6b312ab2bcf0a68373929c6688a26736bd7077a5
Shiraiwa <shiraiwa@psfc.mit.edu>
2016-09-30 14:17:21 -0400

    GradientVectorFEIntegrator
    An integrator to be used to use divJ constraint to a frequency domain
    Maxwell problem

--- fem/bilininteg.cpp
+++ fem/bilininteg.cpp
@@ -1565,6 +1565,84 @@ void VectorFEMassIntegrator::AssembleElementMatrix2(
       }
    }
 }
+  
+void GradientVectorFEIntegrator::AssembleElementMatrix2(const FiniteElement &trial_el,
+                                    const FiniteElement &test_el,
+                                    ElementTransformation &Trans,
+     			            DenseMatrix &elmat){
+   // test  H1
+   // trial ND
+   int dim = test_el.GetDim();
+   int trial_dof = trial_el.GetDof();
+   int test_dof = test_el.GetDof();
+   int spaceDim = Trans.GetSpaceDim();   
+   double w;
+   
+   elmat.SetSize(test_dof, trial_dof);
+   Jinv.  SetSize(dim);
+   dshape.SetSize(test_dof, dim);
+   gshape.SetSize(test_dof, dim);
+#ifdef MFEM_THREAD_SAFE
+   Vector D(VQ ? VQ->GetVDim() : 0);
+   DenseMatrix trial_vshape(trial_dof, spaceDim);
+   DenseMatrix K(MQ ? MQ->GetVDim() : 0, MQ ? MQ->GetVDim() : 0);
+#else
+   trial_vshape.SetSize(trial_dof, spaceDim);
+   D.SetSize(VQ ? VQ->GetVDim() : 0);
+   K.SetSize(MQ ? MQ->GetVDim() : 0, MQ ? MQ->GetVDim() : 0);
+#endif
+   DenseMatrix tmp(trial_vshape.Height(), K.Width());
+   DenseMatrix tmp2(test_dof, trial_dof);
+
+   const IntegrationRule *ir = IntRule;
+   if (ir == NULL)
+   {
+      int order = Trans.OrderGrad(&test_el) + trial_el.GetOrder() + Trans.OrderW();
+      if (test_el.Space() == FunctionSpace::rQk)
+      {
+         ir = &RefinedIntRules.Get(test_el.GetGeomType(), order);
+      }
+      else
+      {
+         ir = &IntRules.Get(test_el.GetGeomType(), order);
+      }
+   }
+   for (int i = 0; i < ir -> GetNPoints(); i++)
+   {
+      const IntegrationPoint &ip = ir->IntPoint(i);
+
+      test_el.CalcDShape (ip, dshape);
+      Trans.SetIntPoint (&ip);     
+      CalcInverse (Trans.Jacobian(), Jinv);
+      Mult (dshape, Jinv, gshape);
+      Trans.SetIntPoint (&ip);
+
+      trial_el.CalcVShape(Trans, trial_vshape);
+      w = ip.weight * Trans.Weight();
+      if (MQ)
+      {
+         MQ->Eval(K, Trans, ip);
+         K *= w;
+         Mult(gshape, K, tmp);
+         AddMultABt(tmp,trial_vshape,elmat);
+      }
+      else if (VQ)
+      {
+         VQ->Eval(D, Trans, ip);
+         D *= w;
+         MultADBt(gshape, D, trial_vshape, tmp2);	 
+         elmat += tmp2;
+      }
+      else
+      {
+         if (Q){
+            w *= Q -> Eval (Trans, ip);
+         }
+         gshape *= w;
+         AddMultABt(gshape, trial_vshape, elmat);
+      }
+   }
+}
 
 void VectorDivergenceIntegrator::AssembleElementMatrix2(
    const FiniteElement &trial_fe,
