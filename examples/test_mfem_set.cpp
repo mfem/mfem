6f7651c252a370c2b0b85f0966a1d4887e6bb179
Veselin Dobrev <dobrev@llnl.gov>
2017-03-05 00:39:49 -0800

    Add a test code for the experimental class Set.
    In this test, Set is 3-4 times faster than std::unordered_set. I suspect
    that the main reason is the slower memory allocation with the default
    allocator in std::unordered_set, and a small contribution from the more
    expensive default hashing function.

---
+++ examples/test_mfem_set.cpp
@@ -0,0 +1,161 @@
+
+#include "mfem.hpp"
+#include <ctime>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
+using namespace mfem;
+
+int main()
+{
+   // const int num_entries = 4*1024*1024; // avg bin size = 2 (with defaults)
+   // const int num_entries = 6*1024*1024; // avg bin size = 1.5 (with defaults)
+   const int num_entries = 5*1024*1024; // avg bin size = 1.25 (with defaults)
+   // const int num_entries = 50*1024*1024;
+
+   time_t seed = time(NULL);
+
+   Set<int> my_set;
+   // cheating ... speeds up Insert(), slows down Find():
+   // Set<int> my_set(num_entries/2);
+   // cheating ... speeds up Insert():
+   // Set<int> my_set((num_entries/4)*3);
+   // cheating ... speeds up both Insert() and Find():
+   // Set<int> my_set(num_entries);
+
+   cout << "Inserting " << num_entries << " random entries in a mfem::Set ..."
+        << flush;
+   tic_toc.Clear();
+   tic_toc.Start();
+   // cheating ... does not affect speed much, just the final MemoryUsage():
+   // my_set.Reserve(num_entries);
+   srand(seed);
+   for (int i = 0; i < num_entries; i++)
+   {
+      my_set.Insert(rand());
+   }
+   tic_toc.Stop();
+   cout << " done (" << tic_toc.RealTime() << " sec).\n" << endl;
+
+   cout << "Inserting the same random entries again ..."
+        << flush;
+   tic_toc.Clear();
+   tic_toc.Start();
+   srand(seed);
+   for (int i = 0; i < num_entries; i++)
+   {
+      my_set.Insert(rand());
+   }
+   tic_toc.Stop();
+   cout << " done (" << tic_toc.RealTime() << " sec).\n" << endl;
+
+   cout << "Verifying all entries in the Set ..." << flush;
+   tic_toc.Clear();
+   tic_toc.Start();
+   srand(seed);
+   for (int i = 0; i < num_entries; i++)
+   {
+      MFEM_VERIFY(my_set.Find(rand()) != -1, "entry not found");
+   }
+   tic_toc.Stop();
+   cout << " done (" << tic_toc.RealTime() << " sec).\n" << endl;
+
+   my_set.PrintStats();
+
+   cout << "\nOptimizing the Set ..." << flush;
+   tic_toc.Clear();
+   tic_toc.Start();
+   my_set.Optimize();
+   tic_toc.Stop();
+   cout << " done (" << tic_toc.RealTime() << " sec).\n" << endl;
+
+   cout << "Inserting the same random entries again ..."
+        << flush;
+   tic_toc.Clear();
+   tic_toc.Start();
+   srand(seed);
+   for (int i = 0; i < num_entries; i++)
+   {
+      my_set.Insert(rand());
+   }
+   tic_toc.Stop();
+   cout << " done (" << tic_toc.RealTime() << " sec).\n" << endl;
+
+   cout << "Verifying all entries in the Set ..." << flush;
+   tic_toc.Clear();
+   tic_toc.Start();
+   srand(seed);
+   for (int i = 0; i < num_entries; i++)
+   {
+      MFEM_VERIFY(my_set.Find(rand()) != -1, "entry not found");
+   }
+   tic_toc.Stop();
+   cout << " done (" << tic_toc.RealTime() << " sec).\n" << endl;
+
+   my_set.PrintStats();
+   my_set.Clear();
+
+
+   //----------------------------------------------------------------
+   //  std::set
+   //----------------------------------------------------------------
+
+   std::set<int> std_set;
+   cout << "\nInserting " << num_entries
+        << " random entries in an std::set ..." << flush;
+   tic_toc.Clear();
+   tic_toc.Start();
+   srand(seed);
+   for (int i = 0; i < num_entries; i++)
+   {
+      std_set.insert(rand());
+   }
+   tic_toc.Stop();
+   cout << " done (" << tic_toc.RealTime() << " sec).\n" << endl;
+
+   cout << "Verifying all entries in the std::set ..." << flush;
+   tic_toc.Clear();
+   tic_toc.Start();
+   srand(seed);
+   for (int i = 0; i < num_entries; i++)
+   {
+      MFEM_VERIFY(std_set.find(rand()) != std_set.end(), "entry not found");
+   }
+   tic_toc.Stop();
+   cout << " done (" << tic_toc.RealTime() << " sec).\n" << endl;
+   std_set.clear();
+
+
+   //----------------------------------------------------------------
+   //  std::unordered_set
+   //----------------------------------------------------------------
+
+   std::unordered_set<int> uo_set;
+   cout << "\nInserting " << num_entries
+        << " random entries in an std::unordered_set ..." << flush;
+   tic_toc.Clear();
+   tic_toc.Start();
+   srand(seed);
+   for (int i = 0; i < num_entries; i++)
+   {
+      uo_set.insert(rand());
+   }
+   tic_toc.Stop();
+   cout << " done (" << tic_toc.RealTime() << " sec).\n" << endl;
+
+   cout << "Verifying all entries in the std::unordered_set ..." << flush;
+   tic_toc.Clear();
+   tic_toc.Start();
+   srand(seed);
+   for (int i = 0; i < num_entries; i++)
+   {
+      MFEM_VERIFY(uo_set.find(rand()) != uo_set.end(), "entry not found");
+   }
+   tic_toc.Stop();
+   cout << " done (" << tic_toc.RealTime() << " sec).\n" << endl;
+   uo_set.clear();
+
+
+   return 0;
+}
