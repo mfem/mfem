22470082126cfeaebd476fa769fea8ba9f246267
VSingh <vikramsingh001@gmail.com>
2017-03-21 19:59:15 +0200

    Simplified common solution calculations and tested

---
+++ examples/test/t1.cpp
@@ -0,0 +1,138 @@
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+using namespace mfem;
+
+// Choice for the problem setup. The fluid velocity, initial condition and
+// inflow boundary condition are chosen based on this parameter.
+int problem;
+
+// Velocity coefficient
+void velocity_function(const Vector &x, Vector &v);
+
+void dir_velocity_function(const Vector &x, Vector &v);
+
+// Initial condition
+double u0_function(const Vector &x);
+
+// Inflow boundary condition
+double inflow_function(const Vector &x);
+
+// Mesh bounding box
+Vector bb_min, bb_max;
+
+
+
+int main(int argc, char *argv[])
+{
+   const char *mesh_file = "periodic-square.mesh";
+   int order = 1;
+   double t_final = 0.01;
+   double dt = 0.01;
+
+   int precision = 8;
+   cout.precision(precision);
+
+   // 2. Read the mesh from the given mesh file. We can handle geometrically
+   //    periodic meshes in this code.
+   Mesh *mesh = new Mesh(mesh_file, 1, 1);
+   int dim = mesh->Dimension();
+
+   // 5. Define the discontinuous DG finite element space of the given
+   //    polynomial order on the refined mesh.
+   DG_FECollection fec(order, dim);
+   FiniteElementSpace fes(mesh, &fec);
+
+   cout << "Number of unknowns: " << fes.GetVSize() << endl;
+
+   // 6. Set up and assemble the bilinear and linear forms corresponding to the
+   //    DG discretization. The DGTraceIntegrator involves integrals over mesh
+   //    interior faces.
+   VectorFunctionCoefficient x_dir(dim, dir_velocity_function);
+   FunctionCoefficient u0(u0_function);
+
+   BilinearForm m(&fes);
+   m.AddDomainIntegrator(new MassIntegrator);
+   BilinearForm k(&fes);
+   k.AddDomainIntegrator(new ConvectionIntegrator(x_dir, -1.0));
+
+   m.Assemble();
+   m.Finalize();
+   int skip_zeros = 1;
+   k.Assemble(skip_zeros);
+   k.Finalize(skip_zeros);
+
+   SparseMatrix &K = k.SpMat();
+
+   GridFunction u_sol(&fes);
+   u_sol.ProjectCoefficient(u0);
+
+   GridFunction v(&fes);
+   K.Mult(u_sol, v);
+
+   VectorFunctionCoefficient velocity(dim, velocity_function);
+
+   FiniteElementSpace fes_v(mesh, &fec, dim);
+   GridFunction u(&fes_v);
+   u.ProjectCoefficient(velocity);
+
+   VectorGridFunctionCoefficient u_vec(&u);
+
+   LinearForm b(&fes);
+   b.AddFaceIntegrator(
+      new DGRiemIntegrator(u_vec, -1, 0));
+   b.Assemble();
+
+
+   // We need a vector finite element space for nodes
+   FiniteElementSpace fes_nodes(mesh, &fec, dim);
+   // Print all nodes in the finite element space 
+   GridFunction nodes(&fes_nodes);
+   mesh->GetNodes(nodes);
+
+   for (int i = 0; i < nodes.Size()/dim; i++)
+   {
+       int sub1 = i, sub2 = nodes.Size()/dim + i;
+//       cout << nodes(sub1) << '\t' << nodes(sub2) << '\t' << m.SpMat().GetRowEntries(sub1)[0] << endl;   
+//       cout << i << '\t' << nodes(sub1) << '\t' << nodes(sub2) << '\t' << u_sol(sub1) << '\t' << v(sub1) << endl;   
+//       cout << nodes(sub1) << '\t' << nodes(sub2) << '\t' << u_sol(sub1) << '\t' << v(sub1)/m.SpMat().GetRowEntries(sub1)[0] << endl;   
+//       cout << nodes(sub1) << '\t' << nodes(sub2) << '\t' << u_sol(sub1) << '\t' << b[sub1]/m.SpMat().GetRowEntries(sub1)[0] << endl;      
+       cout << nodes(sub1) << '\t' << nodes(sub2) << '\t' << (v(sub1) + b[sub1])/m.SpMat().GetRowEntries(sub1)[0] << endl;      
+   }
+
+
+   return 0;
+}
+
+
+
+// Velocity coefficient
+void dir_velocity_function(const Vector &x, Vector &v)
+{
+   int dim = x.Size();
+
+   v(0) = 1.0; v(1) = 0.0; 
+}
+
+
+// Velocity coefficient
+void velocity_function(const Vector &x, Vector &v)
+{
+   int dim = x.Size();
+
+   v(0) = 1 + 0.2*sin(M_PI*x(0)); v(1) = 0.0; 
+//   v(0) = 1 ; v(1) = 0.0; 
+}
+
+// Initial condition
+double u0_function(const Vector &x)
+{
+   int dim = x.Size();
+
+   const double f = M_PI;
+   return 1 + 0.2*sin(f*x(0)) ;
+}
+
