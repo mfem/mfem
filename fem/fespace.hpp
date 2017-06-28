fe6c92ab360c88e0e33cabc7cc1df961b0de3af8
Vladimir Tomov <tomov2@llnl.gov>
2017-06-27 18:09:00 -0700

    Update fespace.hpp
    Added a comment.

--- fem/fespace.hpp
+++ fem/fespace.hpp
@@ -305,13 +305,17 @@ public:
    const FiniteElement *GetTraceElement(int i, int geom_type) const;
 
    /** Mark degrees of freedom associated with boundary elements with
-       the specified boundary attributes (marked in 'bdr_attr_is_ess'). */
+       the specified boundary attributes (marked in 'bdr_attr_is_ess').
+       For spaces with 'vdim' > 1, the 'component' parameter can be used
+       to restricts the marked vDOFs to the specified component. */
    virtual void GetEssentialVDofs(const Array<int> &bdr_attr_is_ess,
                                   Array<int> &ess_vdofs,
                                   int component = -1) const;
 
    /** Get a list of essential true dofs, ess_tdof_list, corresponding to the
-       boundary attributes marked in the array bdr_attr_is_ess. */
+       boundary attributes marked in the array bdr_attr_is_ess.
+       For spaces with 'vdim' > 1, the 'component' parameter can be used
+       to restricts the marked tDOFs to the specified component. */
    virtual void GetEssentialTrueDofs(const Array<int> &bdr_attr_is_ess,
                                      Array<int> &ess_tdof_list,
                                      int component = -1);
