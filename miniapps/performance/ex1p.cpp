8d319988644ce670b1d50ade6c216690b20bf328
Tzanio <tzanio@llnl.gov>
2017-10-17 11:15:35 -0700

    make style

--- miniapps/performance/ex1p.cpp
+++ miniapps/performance/ex1p.cpp
@@ -62,7 +62,7 @@ typedef FieldEvaluator<sol_fes_t,ScalarLayout,int_rule_t> sol_Field_Eval;
 
 // Static bilinear form type, combining the above types
 typedef TBilinearForm<mesh_t,sol_fes_t,int_rule_t,integ_t,
-sol_Shape_Eval,sol_Field_Eval> HPCBilinearForm;
+        sol_Shape_Eval,sol_Field_Eval> HPCBilinearForm;
 
 int main(int argc, char *argv[])
 {
