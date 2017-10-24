4691ac153908fe50fdac787d08e24c525ce8636c
Veselin Dobrev <dobrev@llnl.gov>
2017-10-24 12:33:50 -0700

    In tmop.[ch]pp, remove the old implementation of the matrix invariants.

--- fem/tmop.hpp
+++ fem/tmop.hpp
@@ -31,47 +31,6 @@ protected:
        because it is not used. */
    void SetTransformation(ElementTransformation &) { }
 
-   /// First invariant of the given 2x2 matrix @a M.
-   static double Dim2Invariant1(const DenseMatrix &M);
-   /// Second invariant of the given 2x2 matrix @a M.
-   static double Dim2Invariant2(const DenseMatrix &M);
-
-   /// 1st derivative of the first invariant for the given 2x2 matrix @a M.
-   static void Dim2Invariant1_dM(const DenseMatrix &M, DenseMatrix &dM);
-   /// 1st derivative of the second invariant for the given 2x2 matrix @a M.
-   static void Dim2Invariant2_dM(const DenseMatrix &M, DenseMatrix &dM);
-
-   /// 2nd derivative of the first invariant for the given 2x2 matrix @a M.
-   static void Dim2Invariant1_dMdM(const DenseMatrix &M, int i, int j,
-                                   DenseMatrix &dMdM);
-   /// 2nd derivative of the second invariant for the given 2x2 matrix @a M.
-   static void Dim2Invariant2_dMdM(const DenseMatrix &M, int i, int j,
-                                   DenseMatrix &dMdM);
-
-   /// First invariant of the given 3x3 matrix @a M.
-   static double Dim3Invariant1(const DenseMatrix &M);
-   /// Second invariant of the given 3x3 matrix @a M.
-   static double Dim3Invariant2(const DenseMatrix &M);
-   /// Third invariant of the given 3x3 matrix @a M.
-   static double Dim3Invariant3(const DenseMatrix &M);
-
-   /// 1st derivative of the first invariant for the given 3x3 matrix @a M.
-   static void Dim3Invariant1_dM(const DenseMatrix &M, DenseMatrix &dM);
-   /// 1st derivative of the second invariant for the given 3x3 matrix @a M.
-   static void Dim3Invariant2_dM(const DenseMatrix &M, DenseMatrix &dM);
-   /// 1st derivative of the third invariant for the given 3x3 matrix @a M.
-   static void Dim3Invariant3_dM(const DenseMatrix &M, DenseMatrix &dM);
-
-   /// 2nd derivative of the first invariant for the given 3x3 matrix @a M.
-   static void Dim3Invariant1_dMdM(const DenseMatrix &M, int i, int j,
-                                   DenseMatrix &dMdM);
-   /// 2nd derivative of the second invariant for the given 3x3 matrix @a M.
-   static void Dim3Invariant2_dMdM(const DenseMatrix &M, int i, int j,
-                                   DenseMatrix &dMdM);
-   /// 2nd derivative of the third invariant for the given 3x3 matrix @a M.
-   static void Dim3Invariant3_dMdM(const DenseMatrix &M, int i, int j,
-                                   DenseMatrix &dMdM);
-
 public:
    TMOP_QualityMetric() : Jtr(NULL) { }
    virtual ~TMOP_QualityMetric() { }
