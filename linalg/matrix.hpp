7154064ecbc8a99a3356899856ce77e8865d1264
Alexander Grayver <agrayver@erdw.ethz.ch>
2017-11-06 15:39:56 +0100

    Avoid explicit numbering in the enum

--- linalg/matrix.hpp
+++ linalg/matrix.hpp
@@ -32,9 +32,9 @@ public:
        */
    enum DiagonalPolicy
    {
-      DIAG_KEEP   = 0, ///< keep the value
-      DIAG_ZERO   = 1, ///< set diagonal value to zero
-      DIAG_ONE    = 2, ///< set diagonal value to one
+     DIAG_ZERO, ///< set diagonal value to zero
+     DIAG_ONE,  ///< set diagonal value to one
+     DIAG_KEEP, ///< keep the value
    };
 
    /// Creates a square matrix of size s.
