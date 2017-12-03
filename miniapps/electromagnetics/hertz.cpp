eab8008f5a987887fe86240909b185f0d61f628b
Stowell, Mark L <stowell1@llnl.gov>
2017-12-03 14:15:46 -0800

    Copy-n-paste from Tesla

--- miniapps/electromagnetics/hertz.cpp
+++ miniapps/electromagnetics/hertz.cpp
@@ -22,6 +22,24 @@ using namespace std;
 using namespace mfem;
 using namespace mfem::electromagnetics;
 
+// Permittivity Functions
+Coefficient * SetupPermittivityCoefficient();
+
+static Vector pw_eps_(0);     // Piecewise permittivity values
+static Vector ds_params_(0);  // Center, Radius, and Permittivity
+//                               of dielectric sphere
+double dielectric_sphere(const Vector &);
+
+// Permeability Function
+Coefficient * SetupInvPermeabilityCoefficient();
+
+static Vector pw_mu_(0);      // Piecewise permeability values
+static Vector pw_mu_inv_(0);  // Piecewise inverse permeability values
+static Vector ms_params_(0);  // Center, Inner and Outer Radii, and
+//                               Permeability of magnetic shell
+double magnetic_shell(const Vector &);
+double magnetic_shell_inv(const Vector & x) { return 1.0/magnetic_shell(x); }
+
 // Prints the program's logo to the given output stream
 void display_banner(ostream & os);
 
@@ -30,6 +48,265 @@ int main(int argc, char *argv[])
    MPI_Session mpi(argc, argv);
 
    if ( mpi.Root() ) { display_banner(cout); }
+
+   // Parse command-line options.
+   const char *mesh_file = "../../data/ball-nurbs.mesh";
+   int order = 1;
+   int maxit = 100;
+   int serial_ref_levels = 0;
+   int parallel_ref_levels = 0;
+   bool visualization = true;
+   bool visit = true;
+
+   Array<int> kbcs;
+   Array<int> vbcs;
+
+   Vector vbcv;
+
+   OptionsParser args(argc, argv);
+   args.AddOption(&mesh_file, "-m", "--mesh",
+                  "Mesh file to use.");
+   args.AddOption(&order, "-o", "--order",
+                  "Finite element order (polynomial degree).");
+   args.AddOption(&serial_ref_levels, "-rs", "--serial-ref-levels",
+                  "Number of serial refinement levels.");
+   args.AddOption(&parallel_ref_levels, "-rp", "--parallel-ref-levels",
+                  "Number of parallel refinement levels.");
+   args.AddOption(&pw_eps_, "-pwe", "--piecewise-eps",
+                  "Piecewise values of Permittivity");
+   args.AddOption(&ds_params_, "-ds", "--dielectric-sphere-params",
+                  "Center, Radius, and Permittivity of Dielectric Sphere");
+   args.AddOption(&pw_mu_, "-pwm", "--piecewise-mu",
+                  "Piecewise values of Permeability");
+   args.AddOption(&ms_params_, "-ms", "--magnetic-shell-params",
+                  "Center, Inner Radius, Outer Radius, and Permeability of Magnetic Shell");
+   /*
+   args.AddOption(&dbcs, "-dbcs", "--dirichlet-bc-surf",
+                  "Dirichlet Boundary Condition Surfaces");
+   args.AddOption(&dbcv, "-dbcv", "--dirichlet-bc-vals",
+                  "Dirichlet Boundary Condition Values");
+   args.AddOption(&dbcg, "-dbcg", "--dirichlet-bc-gradient",
+                  "-no-dbcg", "--no-dirichlet-bc-gradient",
+                  "Dirichlet Boundary Condition Gradient (phi = -z)");
+   args.AddOption(&nbcs, "-nbcs", "--neumann-bc-surf",
+                  "Neumann Boundary Condition Surfaces");
+   args.AddOption(&nbcv, "-nbcv", "--neumann-bc-vals",
+                  "Neumann Boundary Condition Values");
+   args.AddOption(&kbcs, "-kbcs", "--surface-current-bc",
+                  "Surfaces for the Surface Current (K) Boundary Condition");
+   args.AddOption(&vbcs, "-vbcs", "--voltage-bc-surf",
+                  "Voltage Boundary Condition Surfaces (to drive K)");
+   args.AddOption(&vbcv, "-vbcv", "--voltage-bc-vals",
+                  "Voltage Boundary Condition Values (to drive K)");
+   */
+   args.AddOption(&maxit, "-maxit", "--max-amr-iterations",
+                  "Max number of iterations in the main AMR loop.");
+   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
+                  "--no-visualization",
+                  "Enable or disable GLVis visualization.");
+   args.AddOption(&visit, "-visit", "--visit", "-no-visit", "--no-visit",
+                  "Enable or disable VisIt visualization.");
+   args.Parse();
+   if (!args.Good())
+   {
+      if (mpi.Root())
+      {
+         args.PrintUsage(cout);
+      }
+      return 1;
+   }
+   if (mpi.Root())
+   {
+      args.PrintOptions(cout);
+   }
+
+   // Read the (serial) mesh from the given mesh file on all processors.  We
+   // can handle triangular, quadrilateral, tetrahedral, hexahedral, surface
+   // and volume meshes with the same code.
+   Mesh *mesh = new Mesh(mesh_file, 1, 1);
+
+   if (mpi.Root())
+   {
+      cout << "Starting initialization." << endl;
+   }
+
+   // Project a NURBS mesh to a piecewise-quadratic curved mesh
+   if (mesh->NURBSext)
+   {
+      mesh->UniformRefinement();
+      if (serial_ref_levels > 0) { serial_ref_levels--; }
+
+      mesh->SetCurvature(2);
+   }
+
+   // Ensure that quad and hex meshes are treated as non-conforming.
+   mesh->EnsureNCMesh();
+
+   // Refine the serial mesh on all processors to increase the resolution. In
+   // this example we do 'ref_levels' of uniform refinement.
+   for (int l = 0; l < serial_ref_levels; l++)
+   {
+      mesh->UniformRefinement();
+   }
+
+   // Define a parallel mesh by a partitioning of the serial mesh. Refine
+   // this mesh further in parallel to increase the resolution. Once the
+   // parallel mesh is defined, the serial mesh can be deleted.
+   ParMesh pmesh(MPI_COMM_WORLD, *mesh);
+   delete mesh;
+
+   // Refine this mesh in parallel to increase the resolution.
+   int par_ref_levels = parallel_ref_levels;
+   for (int l = 0; l < par_ref_levels; l++)
+   {
+      pmesh.UniformRefinement();
+   }
+
+   // If values for Voltage BCs were not set issue a warning and exit
+   if ( ( vbcs.Size() > 0 && kbcs.Size() == 0 ) ||
+        ( kbcs.Size() > 0 && vbcs.Size() == 0 ) ||
+        ( vbcv.Size() < vbcs.Size() ) )
+   {
+      if ( mpi.Root() )
+      {
+         cout << "The surface current (K) boundary condition requires "
+              << "surface current boundary condition surfaces (with -kbcs), "
+              << "voltage boundary condition surface (with -vbcs), "
+              << "and voltage boundary condition values (with -vbcv)."
+              << endl;
+      }
+      return 3;
+   }
+
+   // Create a coefficient describing the dielectric permittivity
+   Coefficient * epsCoef = SetupPermittivityCoefficient();
+
+   // Create a coefficient describing the magnetic permeability
+   Coefficient * muInvCoef = SetupInvPermeabilityCoefficient();
+
+   // Create the Magnetostatic solver
+   HertzSolver Hertz(pmesh, order, kbcs, vbcs, vbcv, *epsCoef, *muInvCoef);
+                     //(b_uniform_.Size() > 0 ) ? a_bc_uniform  : NULL,
+                     //(cr_params_.Size() > 0 ) ? current_ring  : NULL,
+                     //(bm_params_.Size() > 0 ) ? bar_magnet    :
+                     //(ha_params_.Size() > 0 ) ? halbach_array : NULL);
+
+   // Initialize GLVis visualization
+   if (visualization)
+   {
+      Hertz.InitializeGLVis();
+   }
+
+   // Initialize VisIt visualization
+   VisItDataCollection visit_dc("Hertz-AMR-Parallel", &pmesh);
+
+   if ( visit )
+   {
+      Hertz.RegisterVisItFields(visit_dc);
+   }
+   if (mpi.Root()) { cout << "Initialization done." << endl; }
+
+   // The main AMR loop. In each iteration we solve the problem on the current
+   // mesh, visualize the solution, estimate the error on all elements, refine
+   // the worst elements and update all objects to work with the new mesh. We
+   // refine until the maximum number of dofs in the Nedelec finite element
+   // space reaches 10 million.
+   const int max_dofs = 10000000;
+   for (int it = 1; it <= maxit; it++)
+   {
+      if (mpi.Root())
+      {
+         cout << "\nAMR Iteration " << it << endl;
+      }
+
+      // Display the current number of DoFs in each finite element space
+      Hertz.PrintSizes();
+
+      // Assemble all forms
+      Hertz.Assemble();
+
+      // Solve the system and compute any auxiliary fields
+      Hertz.Solve();
+
+      // Determine the current size of the linear system
+      int prob_size = Hertz.GetProblemSize();
+
+      // Write fields to disk for VisIt
+      if ( visit )
+      {
+         Hertz.WriteVisItFields(it);
+      }
+
+      // Send the solution by socket to a GLVis server.
+      if (visualization)
+      {
+         Hertz.DisplayToGLVis();
+      }
+
+      if (mpi.Root())
+      {
+         cout << "AMR iteration " << it << " complete." << endl;
+      }
+
+      // Check stopping criteria
+      if (prob_size > max_dofs)
+      {
+         if (mpi.Root())
+         {
+            cout << "Reached maximum number of dofs, exiting..." << endl;
+         }
+         break;
+      }
+      if ( it == maxit )
+      {
+         break;
+      }
+
+      // Wait for user input. Ask every 10th iteration.
+      char c = 'c';
+      if (mpi.Root() && (it % 10 == 0))
+      {
+         cout << "press (q)uit or (c)ontinue --> " << flush;
+         cin >> c;
+      }
+      MPI_Bcast(&c, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+      if (c != 'c')
+      {
+         break;
+      }
+
+      // Estimate element errors using the Zienkiewicz-Zhu error estimator.
+      Vector errors(pmesh.GetNE());
+      Hertz.GetErrorEstimates(errors);
+
+      double local_max_err = errors.Max();
+      double global_max_err;
+      MPI_Allreduce(&local_max_err, &global_max_err, 1,
+                    MPI_DOUBLE, MPI_MAX, pmesh.GetComm());
+
+      // Refine the elements whose error is larger than a fraction of the
+      // maximum element error.
+      const double frac = 0.5;
+      double threshold = frac * global_max_err;
+      if (mpi.Root()) { cout << "Refining ..." << endl; }
+      pmesh.RefineByError(errors, threshold);
+
+      // Update the magnetostatic solver to reflect the new state of the mesh.
+      Hertz.Update();
+
+      if (pmesh.Nonconforming() && mpi.WorldSize() > 1)
+      {
+         if (mpi.Root()) { cout << "Rebalancing ..." << endl; }
+         pmesh.Rebalance();
+
+         // Update again after rebalancing
+         Hertz.Update();
+      }
+   }
+
+   delete epsCoef;
+   delete muInvCoef;
 
    return 0;
 }
@@ -60,4 +337,95 @@ void display_banner(ostream & os)
       << "  /   __    / \\  ___/|  | \\/|  |  /   _/  " << endl
       << " /___/ /_  /   \\___  >__|   |__| /_____ \\ " << endl
       << "         \\/        \\/                  \\/ " << endl << flush;
+}
+
+// The Permittivity is a required coefficient which may be defined in
+// various ways so we'll determine the appropriate coefficient type here.
+Coefficient *
+SetupPermittivityCoefficient()
+{
+   Coefficient * coef = NULL;
+
+   if ( ds_params_.Size() > 0 )
+   {
+      coef = new FunctionCoefficient(dielectric_sphere);
+   }
+   else if ( pw_eps_.Size() > 0 )
+   {
+      coef = new PWConstCoefficient(pw_eps_);
+   }
+   else
+   {
+      coef = new ConstantCoefficient(epsilon0_);
+   }
+
+   return coef;
+}
+
+// The Permeability is a required coefficient which may be defined in
+// various ways so we'll determine the appropriate coefficient type here.
+Coefficient *
+SetupInvPermeabilityCoefficient()
+{
+   Coefficient * coef = NULL;
+
+   if ( ms_params_.Size() > 0 )
+   {
+      coef = new FunctionCoefficient(magnetic_shell_inv);
+   }
+   else if ( pw_mu_.Size() > 0 )
+   {
+      pw_mu_inv_.SetSize(pw_mu_.Size());
+      for (int i = 0; i < pw_mu_.Size(); i++)
+      {
+         MFEM_ASSERT( pw_mu_[i] > 0.0, "permeability values must be positive" );
+         pw_mu_inv_[i] = 1.0/pw_mu_[i];
+      }
+      coef = new PWConstCoefficient(pw_mu_inv_);
+   }
+   else
+   {
+      coef = new ConstantCoefficient(1.0/mu0_);
+   }
+
+   return coef;
+}
+
+// A sphere with constant permittivity.  The sphere has a radius,
+// center, and permittivity specified on the command line and stored
+// in ds_params_.
+double dielectric_sphere(const Vector &x)
+{
+   double r2 = 0.0;
+
+   for (int i=0; i<x.Size(); i++)
+   {
+      r2 += (x(i)-ds_params_(i))*(x(i)-ds_params_(i));
+   }
+
+   if ( sqrt(r2) <= ds_params_(x.Size()) )
+   {
+      return ds_params_(x.Size()+1) * epsilon0_;
+   }
+   return epsilon0_;
+}
+
+// A spherical shell with constant permeability.  The sphere has inner
+// and outer radii, center, and relative permeability specified on the
+// command line and stored in ms_params_.
+double magnetic_shell(const Vector &x)
+{
+   double r2 = 0.0;
+
+   for (int i = 0; i < x.Size(); i++)
+   {
+      r2 += (x(i) - ms_params_(i))*(x(i) - ms_params_(i));
+   }
+
+   if ( sqrt(r2) >= ms_params_(x.Size()) &&
+        sqrt(r2) <= ms_params_(x.Size()+1) )
+   {
+      return mu0_*ms_params_(x.Size()+2);
+   }
+   return mu0_;
 }
--- miniapps/electromagnetics/hertz_solver.hpp
+++ miniapps/electromagnetics/hertz_solver.hpp
@@ -22,8 +22,148 @@
 namespace mfem
 {
 
+using miniapps::H1_ParFESpace;
+using miniapps::ND_ParFESpace;
+using miniapps::RT_ParFESpace;
+using miniapps::ParDiscreteGradOperator;
+using miniapps::ParDiscreteCurlOperator;
+using miniapps::DivergenceFreeProjector;
+
 namespace electromagnetics
 {
+
+// Physical Constants
+// Permittivity of Free Space (units F/m)
+static double epsilon0_ = 8.8541878176e-12;
+// Permeability of Free Space (units H/m)
+static double mu0_ = 4.0e-7*M_PI;
+
+class SurfaceCurrent;
+class HertzSolver
+{
+public:
+   HertzSolver(ParMesh & pmesh, int order, Array<int> & kbcs,
+               Array<int> & vbcs, Vector & vbcv,
+               Coefficient & epsCoef,
+               Coefficient & muInvCoef/*,
+               void   (*a_bc )(const Vector&, Vector&),
+               void   (*j_src)(const Vector&, Vector&),
+               void   (*m_src)(const Vector&, Vector&)*/);
+   ~HertzSolver();
+
+   HYPRE_Int GetProblemSize();
+
+   void PrintSizes();
+
+   void Assemble();
+
+   void Update();
+
+   void Solve();
+
+   void GetErrorEstimates(Vector & errors);
+
+   void RegisterVisItFields(VisItDataCollection & visit_dc);
+
+   void WriteVisItFields(int it = 0);
+
+   void InitializeGLVis();
+
+   void DisplayToGLVis();
+
+   const ParGridFunction & GetVectorPotential() { return *a_; }
+
+private:
+
+   int myid_;
+   int num_procs_;
+   int order_;
+
+   ParMesh * pmesh_;
+
+   VisItDataCollection * visit_dc_;
+
+   H1_ParFESpace * H1FESpace_;
+   ND_ParFESpace * HCurlFESpace_;
+   RT_ParFESpace * HDivFESpace_;
+
+   ParBilinearForm * curlMuInvCurl_;
+   ParBilinearForm * hCurlMass_;
+   ParMixedBilinearForm * hDivHCurlMuInv_;
+   ParMixedBilinearForm * weakCurlMuInv_;
+
+   ParDiscreteGradOperator * grad_;
+   ParDiscreteCurlOperator * curl_;
+
+   ParGridFunction * a_;  // Vector Potential (HCurl)
+   ParGridFunction * b_;  // Magnetic Flux (HDiv)
+   ParGridFunction * h_;  // Magnetic Field (HCurl)
+   ParGridFunction * jr_; // Raw Volumetric Current Density (HCurl)
+   ParGridFunction * j_;  // Volumetric Current Density (HCurl)
+   ParGridFunction * k_;  // Surface Current Density (HCurl)
+   ParGridFunction * m_;  // Magnetization (HDiv)
+   ParGridFunction * bd_; // Dual of B (HCurl)
+   ParGridFunction * jd_; // Dual of J, the rhs vector (HCurl)
+
+   DivergenceFreeProjector * DivFreeProj_;
+   SurfaceCurrent          * SurfCur_;
+
+   Coefficient       * epsCoef_;   // Dielectric Material Coefficient
+   Coefficient       * muInvCoef_; // Dia/Paramagnetic Material Coefficient
+   VectorCoefficient * aBCCoef_;   // Vector Potential BC Function
+   VectorCoefficient * jCoef_;     // Volume Current Density Function
+   VectorCoefficient * mCoef_;     // Magnetization Vector Function
+
+   void   (*a_bc_ )(const Vector&, Vector&);
+   void   (*j_src_)(const Vector&, Vector&);
+   void   (*m_src_)(const Vector&, Vector&);
+
+   Array<int> ess_bdr_;
+   Array<int> ess_bdr_tdofs_;
+   Array<int> non_k_bdr_;
+
+   std::map<std::string,socketstream*> socks_;
+};
+
+class SurfaceCurrent
+{
+public:
+   SurfaceCurrent(ParFiniteElementSpace & H1FESpace,
+                  ParDiscreteGradOperator & Grad,
+                  Array<int> & kbcs, Array<int> & vbcs, Vector & vbcv);
+   ~SurfaceCurrent();
+
+   void InitSolver() const;
+
+   void ComputeSurfaceCurrent(ParGridFunction & k);
+
+   void Update();
+
+   ParGridFunction * GetPsi() { return psi_; }
+
+private:
+   int myid_;
+
+   ParFiniteElementSpace   * H1FESpace_;
+   ParDiscreteGradOperator * grad_;
+   Array<int>              * kbcs_;
+   Array<int>              * vbcs_;
+   Vector                  * vbcv_;
+
+   ParBilinearForm * s0_;
+   ParGridFunction * psi_;
+   ParGridFunction * rhs_;
+
+   HypreParMatrix  * S0_;
+   mutable Vector Psi_;
+   mutable Vector RHS_;
+
+   mutable HypreBoomerAMG  * amg_;
+   mutable HyprePCG        * pcg_;
+
+   Array<int> ess_bdr_, ess_bdr_tdofs_;
+   Array<int> non_k_bdr_;
+};
 
 } // namespace electromagnetics
 
