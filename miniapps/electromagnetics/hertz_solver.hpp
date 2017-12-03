bef80ef7007f1bbe52de69606f0c3d0e45decfe2
Stowell, Mark L <stowell1@llnl.gov>
2017-12-02 22:59:28 -0800

    Adding initial miniapp files

---
+++ miniapps/electromagnetics/hertz_solver.hpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2010, Lawrence Livermore National Security, LLC. Produced at
+// the Lawrence Livermore National Laboratory. LLNL-CODE-443211. All Rights
+// reserved. See file COPYRIGHT for details.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability see http://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License (as published by the Free
+// Software Foundation) version 2.1 dated February 1999.
+
+#ifndef MFEM_HERTZ_SOLVER
+#define MFEM_HERTZ_SOLVER
+
+#include "../common/pfem_extras.hpp"
+
+#ifdef MFEM_USE_MPI
+
+#include <string>
+#include <map>
+
+namespace mfem
+{
+
+namespace electromagnetics
+{
+
+} // namespace electromagnetics
+
+} // namespace mfem
+
+#endif // MFEM_USE_MPI
+
+#endif // MFEM_HERTZ_SOLVER
