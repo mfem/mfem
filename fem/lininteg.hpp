9cd3bcda47556895e522218e9faef7ec75b52aec
camierjs <camier1@llnl.gov>
2017-12-12 17:35:39 -0800

    [tlinalg] CMakeLists to install *.[ht]pp

--- fem/lininteg.hpp
+++ fem/lininteg.hpp
@@ -32,6 +32,7 @@ public:
    virtual void AssembleRHSElementVect(const FiniteElement &el,
                                        ElementTransformation &Tr,
                                        Vector &elvect) = 0;
+  
    virtual void AssembleRHSElementVect(const FiniteElement &el,
                                        FaceElementTransformations &Tr,
                                        Vector &elvect);
