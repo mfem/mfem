f0877ee13ec980bf5c8b5c987b3552a3b23d0484
Cyrus Harrison <cyrush@llnl.gov>
2017-12-16 13:40:27 -0800

    fix for non-cmake build sys

--- config/defaults.mk
+++ config/defaults.mk
@@ -104,6 +104,7 @@ MFEM_USE_NETCDF      = NO
 MFEM_USE_PETSC       = NO
 MFEM_USE_MPFR        = NO
 MFEM_USE_SIDRE       = NO
+MFEM_USE_CONDUIT     = NO
 
 LIBUNWIND_OPT = -g
 LIBUNWIND_LIB = $(if $(NOTMAC),-lunwind -ldl,)
