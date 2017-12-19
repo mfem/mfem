87b232de9977e4e5da52c8d622bb80f42395f4a8
Veselin Dobrev <dobrev@llnl.gov>
2017-12-18 21:24:55 -0800

    Add a comment in Example 5p that shows how to save the mesh in parallel format with the VisItDataCollection.

--- examples/ex5p.cpp
+++ examples/ex5p.cpp
@@ -317,6 +317,7 @@ int main(int argc, char *argv[])
    VisItDataCollection visit_dc("Example5-Parallel", pmesh);
    visit_dc.RegisterField("velocity", u);
    visit_dc.RegisterField("pressure", p);
+   // visit_dc.SetFormat(1); // save the mesh using MFEM's parallel mesh format
    visit_dc.Save();
 
    // 16. Send the solution by socket to a GLVis server.
