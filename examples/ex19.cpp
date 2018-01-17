61f8b09fdab3ab180b82ed8b2be34c383f84b572
Jamie A. Bramwell <bramwell1@llnl.gov>
2018-01-17 09:33:18 -0800

    Added documentation for the specialized preconditioner

--- examples/ex19.cpp
+++ examples/ex19.cpp
@@ -38,7 +38,20 @@ using namespace std;
 using namespace mfem;
 
 // Custom block preconditioner for the Jacobian of the incompressible nonlinear
-// elasticity operator.
+// elasticity operator. It has the form
+//
+// P^-1 = [ K^-1 0 ][ I -B^T ][ I  0           ]
+//        [ 0    I ][ 0  I   ][ 0 -\gamma S^-1 ]
+//
+// where the original Jacobian has the form
+//
+// J = [ K B^T ]
+//     [ B 0   ]
+//     
+// and K^-1 is an approximation of the inverse of the displacement part of the 
+// Jacobian and S^-1 is an approximation of the inverse of the Schur 
+// complement S = B K^-1 B^T. The Schur complement is approximiated using
+// a mass matrix of the pressure variables.
 class JacobianPreconditioner : public Solver
 {
 protected:
