1eeb2f4465dd460ecba97a6e7b3a0962020cacab
Ido Akkerman <I.Akkerman@tudelft.nl>
2018-04-09 16:38:26 +0200

    Adding Generalized alpha ODE integrator

--- linalg/ode.cpp
+++ linalg/ode.cpp
@@ -485,4 +485,83 @@ void SDIRK33Solver::Step(Vector &x, double &t, double &dt)
    t += dt;
 }
 
+
+void GeneralizedAlphaSolver::Init(TimeDependentOperator &_f)
+{
+   ODESolver::Init(_f);
+   k.SetSize(f->Width());
+   y.SetSize(f->Width());
+   xdot.SetSize(f->Width());
+   xdot = 0.0;
+   first = true;
+}
+
+void GeneralizedAlphaSolver::SetRhoInf(double rho_inf)
+{
+   rho_inf = (rho_inf > 1.0) ? 1.0 : rho_inf;
+   rho_inf = (rho_inf < 0.0) ? 0.0 : rho_inf;
+
+   alpha_m = 0.5*(3.0 - rho_inf)/(1.0 + rho_inf);
+   alpha_f = 1.0/(1.0 + rho_inf);
+   gamma = 0.5 + alpha_m - alpha_f;
+}
+
+void GeneralizedAlphaSolver::PrintProperties(std::ostream &out)
+{
+   out<<"Generalized alpha time integrator:"<<std::endl;
+   out<<"alpha_m = "<<alpha_m<<std::endl;
+   out<<"alpha_f = "<<alpha_f<<std::endl;
+   out<<"gamma   = "<<gamma  <<std::endl;
+
+   if (gamma == 0.5 + alpha_m - alpha_f)
+   {
+      out<<"Second order"<<" and ";
+   }
+   else
+   {
+      out<<"First order"<<" and ";
+   }
+
+   if ((alpha_m >= alpha_f)&&(alpha_f >= 0.5))
+   {
+      out<<"Stable"<<std::endl;
+   }
+   else
+   {
+      out<<"Unstable"<<std::endl;
+   }
+
+}
+
+
+/** This routine assumes xdot is initialized
+*/
+void GeneralizedAlphaSolver::Step(Vector &x, double &t, double &dt)
+{
+   double dt_fac1 = alpha_f*(1.0 - gamma/alpha_m);
+   double dt_fac2 = alpha_f*gamma/alpha_m;
+   double dt_fac3 = 1.0/alpha_m;
+
+   // In the first pass xdot is not yet computed
+   // If parameter choices requires xdot
+   // midpoint rule is used instead for the first step only.
+   if (first && (dt_fac1 != 0.0))
+   {
+      dt_fac1 = 0.0;
+      dt_fac2 = 0.5;
+      dt_fac3 = 2.0;
+      first = false;
+   }
+
+   add(x, dt_fac1*dt, xdot, y);
+   f->SetTime(t + dt_fac2*dt);
+   f->ImplicitSolve(dt_fac2*dt, y, k);
+
+   add(y, dt_fac2*dt, k, x);
+   k.Add(-1.0, xdot);
+   xdot.Add(dt_fac3, k);
+
+   t += dt;
+}
+
 }
