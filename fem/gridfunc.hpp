bba14e664e2406e5ff03ef1390eab7a6fcf394cc
Jan Nikl <nikljan@fjfi.cvut.cz>
2018-05-21 18:26:17 +0200

    Added constant qualifiers to the QuadratureFunction and QuadratureSpace where applicable and added new const versions of the GetElementValues methods.

--- fem/gridfunc.hpp
+++ fem/gridfunc.hpp
@@ -486,7 +486,7 @@ public:
    QuadratureFunction &operator=(const QuadratureFunction &v);
 
    /// Get the IntegrationRule associated with mesh element @a idx.
-   const IntegrationRule &GetElementIntRule(int idx)
+   const IntegrationRule &GetElementIntRule(int idx) const
    { return qspace->GetElementIntRule(idx); }
 
    /// Return all values associated with mesh element @a idx in a Vector.
@@ -498,6 +498,15 @@ public:
     */
    inline void GetElementValues(int idx, Vector &values);
 
+   /// Return all values associated with mesh element @a idx in a Vector.
+   /** The result is stored in the Vector @a values as a copy of the
+       global values.
+
+       Inside the Vector @a values, the index `i+vdim*j` corresponds to the
+       `i`-th vector component at the `j`-th quadrature point.
+    */
+   inline void GetElementValues(int idx, Vector &values) const;
+
    /// Return all values associated with mesh element @a idx in a DenseMatrix.
    /** The result is stored in the DenseMatrix @a values as a reference to the
        global values.
@@ -506,6 +515,15 @@ public:
        `i`-th vector component at the `j`-th quadrature point.
     */
    inline void GetElementValues(int idx, DenseMatrix &values);
+
+   /// Return all values associated with mesh element @a idx in a const DenseMatrix.
+   /** The result is stored in the DenseMatrix @a values as a copy of the
+       global values.
+
+       Inside the DenseMatrix @a values, the `(i,j)` entry corresponds to the
+       `i`-th vector component at the `j`-th quadrature point.
+    */
+   inline void GetElementValues(int idx, DenseMatrix &values) const;
 
    /// Write the QuadratureFunction to the stream @a out.
    void Save(std::ostream &out) const;
@@ -580,11 +598,37 @@ inline void QuadratureFunction::GetElementValues(int idx, Vector &values)
    values.NewDataAndSize(data + vdim*s_offset, vdim*sl_size);
 }
 
+inline void QuadratureFunction::GetElementValues(int idx, Vector &values) const
+{
+   const int s_offset = qspace->element_offsets[idx];
+   const int sl_size = qspace->element_offsets[idx+1] - s_offset;
+   values.SetSize(vdim*sl_size);
+   double *q = data + vdim*s_offset;
+   for (int i = 0; i<values.Size(); i++)
+   {
+      values(i) = *(q++);
+   }
+}
+
 inline void QuadratureFunction::GetElementValues(int idx, DenseMatrix &values)
 {
    const int s_offset = qspace->element_offsets[idx];
    const int sl_size = qspace->element_offsets[idx+1] - s_offset;
    values.Reset(data + vdim*s_offset, vdim, sl_size);
+}
+
+inline void QuadratureFunction::GetElementValues(int idx,
+                                                 DenseMatrix &values) const
+{
+   const int s_offset = qspace->element_offsets[idx];
+   const int sl_size = qspace->element_offsets[idx+1] - s_offset;
+   values.SetSize(vdim, sl_size);
+   double *q = data + vdim*s_offset;
+   for (int i = 0; i<vdim; i++)
+      for (int j = 0; j<sl_size; j++)
+      {
+         values(i,j) = *(q++);
+      }
 }
 
 } // namespace mfem
