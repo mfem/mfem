ff9fef0fd13c975309bbdf9bdd7392d8f9676105
Aaron Fisher <fisher47@llnl.gov>
2018-07-26 16:12:36 -0700

    Some cleanup for the petsc advection app for atpesc.

--- examples/atpesc/petsc/advection-ode.cpp
+++ examples/atpesc/petsc/advection-ode.cpp
@@ -141,9 +141,8 @@ int main(int argc, char *argv[])
    double t_final = 10.0;
    double dt = 0.01;
    bool visualization = true;
-   bool visit = false;
-   bool binary = false;
-   int vis_steps = 5;
+   bool visit = true;
+   int vis_steps = 50;
    bool implicit = false;
    bool use_step = true;
 
@@ -174,9 +173,6 @@ int main(int argc, char *argv[])
    args.AddOption(&visit, "-visit", "--visit-datafiles", "-no-visit",
                   "--no-visit-datafiles",
                   "Save data files for VisIt (visit.llnl.gov) visualization.");
-   args.AddOption(&binary, "-binary", "--binary-datafiles", "-ascii",
-                  "--ascii-datafiles",
-                  "Use binary (Sidre) or ascii format for VisIt data files.");
    args.AddOption(&vis_steps, "-vs", "--visualization-steps",
                   "Visualize every n-th timestep.");
    args.AddOption(&use_step, "-usestep", "--usestep", "-no-step",
@@ -301,24 +297,12 @@ int main(int argc, char *argv[])
       u->Save(osol);
    }
 
-   // Create data collection for solution output: either VisItDataCollection for
-   // ascii data files, or SidreDataCollection for binary data files.
+   // Create data collection for solution output VisItDataCollection 
    DataCollection *dc = NULL;
    if (visit)
    {
-      if (binary)
-      {
-#ifdef MFEM_USE_SIDRE
-         dc = new SidreDataCollection("Example9-Parallel", pmesh);
-#else
-         MFEM_ABORT("Must build with MFEM_USE_SIDRE=YES for binary output.");
-#endif
-      }
-      else
-      {
-         dc = new VisItDataCollection("Example9-Parallel", pmesh);
-         dc->SetPrecision(precision);
-      }
+      dc = new VisItDataCollection("adv", pmesh);
+      dc->SetPrecision(precision);
       dc->RegisterField("solution", u);
       dc->SetCycle(0);
       dc->SetTime(0.0);
