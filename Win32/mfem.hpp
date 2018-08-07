77f956eb950d289cdcbdf35d00d60cb26bb106b5
Jamie Bramwell <bramwell1@llnl.gov>
2018-08-07 16:15:22 -0700

    First cut at mfem windows build

---
+++ Win32/mfem.hpp
@@ -0,0 +1,3 @@
+// Auto-generated file.
+#define MFEM_BUILD_DIR C:/Users/bramwell1/Documents/Repos/ale3d/imports/mfem/Win32
+#include "C:/Users/bramwell1/Documents/Repos/ale3d/imports/mfem/mfem.hpp"
