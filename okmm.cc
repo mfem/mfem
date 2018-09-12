b230e57eee6b73950bfc5ab62feee05ea05269d3
camierjs <camier1@llnl.gov>
2018-09-12 10:54:43 -0700

    clean

--- okmm.cc
+++ okmm.cc
@@ -1,150 +1,131 @@
 #include <dlfcn.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <strings.h>
-//#include <unordered_map>
+#include <cassert>
+#include <unordered_map>
 
 // *****************************************************************************
-static char tmpbuf[1024];
-static size_t tmppos = 0;
-static size_t tmpallocs = 0;
-
-static void* dummy_malloc(size_t size) {
-   if (tmppos + size >= sizeof(tmpbuf))
-      exit(1|fprintf(stderr,"\033[31;1merror\033[m\n"));
-   void *retptr = tmpbuf + tmppos;
-   tmppos += size;
-   ++tmpallocs;
-   return retptr;
-}
-
-static void* dummy_calloc(size_t nmemb, size_t size) {
-   void *ptr = dummy_malloc(nmemb * size);
-   bzero(ptr,size);
-   return ptr;
-}
-
-static void dummy_free(void *ptr) {}
+typedef void *malloc_t(size_t);
+typedef void *calloc_t(size_t, size_t);
+typedef void free_t(void*);
+typedef void *realloc_t(void*, size_t);
+typedef void *memalign_t(size_t, size_t);
+typedef std::unordered_map<void*,size_t> mm_t;
 
 // *****************************************************************************
 static bool hooked = false;
-static void  (*__free)(void*) = NULL;
-static void* (*__malloc)(size_t) = NULL;
-static void* (*__calloc)(size_t,size_t) = NULL;
-static void* (*__realloc)(void*,size_t) = NULL;
-static void* (*__memalign)(size_t,size_t) = NULL;
-
-static void  (*_free)(void*) = NULL;
-static void* (*_malloc)(size_t) = NULL;
-static void* (*_calloc)(size_t,size_t) = NULL;
-static void* (*_realloc)(void*,size_t) = NULL;
-static void* (*_memalign)(size_t,size_t) = NULL;
-//static std::unordered_map<void*,size_t> *_mm = NULL;
+static bool dlsymd = false;
 
 // *****************************************************************************
-void __attribute__((constructor)) _init(void){  
-   //_malloc = dummy_malloc;
-   //_calloc = dummy_calloc;
-   //_free = dummy_free;
-   fprintf(stdout,"\033[32;1m[here]\033[m\n");
+static mm_t *_mm = NULL;
+static free_t *_free = NULL;
+static malloc_t *_malloc = NULL;
+static calloc_t *_calloc = NULL;
+static realloc_t *_realloc = NULL;
+static memalign_t *_memalign = NULL;
 
-   __malloc = (void* (*)(size_t)) dlsym(RTLD_NEXT, "malloc");
-   //__calloc = (void* (*)(size_t, size_t)) dlsym(RTLD_NEXT, "calloc");
-   __realloc =(void* (*)(void*, size_t)) dlsym(RTLD_NEXT, "realloc");
-   __free =  (void  (*)(void*)) dlsym(RTLD_NEXT, "free");
-   __memalign = (void* (*)(size_t, size_t)) dlsym(RTLD_NEXT, "memalign");
-   
-   const bool fmc = __free and __malloc and __calloc and __realloc and __memalign;
-   if (!fmc)
-      exit(1|fprintf(stderr,"\033[31;1merror\033[m\n"));
-   
-   _free = __free;
-   _malloc = __malloc;
-   //_calloc = __calloc;
-   _realloc = __realloc;
-   _memalign = __memalign;
-   //_mm = new std::unordered_map<void*,size_t>();
-   //const bool dls = _free and _malloc and _calloc and _realloc and _memalign;
-   //assert(dls);// and _mm);
-   hooked = false;
+// *****************************************************************************
+static void _init(void){
+   _free = (free_t*) dlsym(RTLD_NEXT, "free");
+   _calloc = (calloc_t*) dlsym(RTLD_NEXT, "calloc");
+   _malloc = (malloc_t*) dlsym(RTLD_NEXT, "malloc");
+   _realloc = (realloc_t*) dlsym(RTLD_NEXT, "realloc");
+   _memalign = (memalign_t*) dlsym(RTLD_NEXT, "memalign");
+   _mm = new mm_t();
+   assert(_free and _malloc and _calloc and _realloc and _memalign and _mm);
+   hooked = dlsymd = true;
 }
 
 // *****************************************************************************
-void *malloc(size_t size){
-   //if (!hooked) return _malloc(size);
+void *malloc(size_t size){ // Red
+   if (!_malloc) _init();
+   if (!hooked) return _malloc(size);
    hooked = false;
    void *ptr = _malloc(size);
-   //assert(ptr);
-   /*if (!(*_mm)[ptr]) {
-      printf("\033[32m%p(%ld)\033[m", ptr, size);
-      (*_mm)[ptr] = size;
+   assert(ptr);
+   if ((*_mm)[ptr]) {
+      printf("\033[31m%p(%ld)\033[m", ptr, size);
    }else{
-      printf("\033[32;7m%p(%ld)\033[m", ptr, size);
-      }*/
+      printf("\033[31;7m%p(%ld)\033[m", ptr, size);
+      (*_mm)[ptr] = size;
+   }
    hooked = true;
    return ptr;
 }
 
 // *****************************************************************************
-void free(void *ptr){
-   //if (!hooked) _free(ptr);
+void free(void *ptr){ // Green
+   if (!_free) _init();
+   if (!hooked) return _free(ptr);
    hooked = false;
-   /* if (ptr){
+   if (ptr){
       if ((*_mm)[ptr]){
-      printf("\033[33;7m%p\033[m", ptr);
+         printf("\033[32m%p\033[m", ptr);
       }else{
-      printf("\033[33m%p\033[m", ptr);
+         printf("\033[32;7m%p\033[m", ptr);
       }
-      }*
-      _free(ptr);
-   */
+   }
+   _free(ptr);
    hooked = true;
 }
 
 // *****************************************************************************
-void *realloc(void *ptr, size_t size){
-   //if (!hooked) return _realloc(ptr, size);
-   hooked = false;
-   void *rptr = _realloc(ptr, size);
-   /*if (rptr){
-     if ((*_mm)[rptr]){
-     printf("\033[33;7m%p(%ld)\033[m", rptr, size);
-     }else{
-     printf("\033[33m%p(%ld)\033[m", rptr, size);
-     }
-     }*/
-   hooked = true;
-   return rptr;
-}
-
-// *****************************************************************************
-/*void *calloc(size_t nmemb, size_t size){
+void *calloc(size_t nmemb, size_t size){ // Yellow
+   if (!dlsymd) { // if we are not yet dlsym'ed, just do it ourselves
+      static const size_t MEM_MAX = 4096;
+      static char mem[MEM_MAX];
+      static size_t m = 0;
+      const size_t bytes = nmemb*size;
+      void *ptr = &mem[m];
+      m += bytes;
+      assert(m<MEM_MAX);
+      for(size_t k=0;k<bytes;k+=1) *(((char*)ptr)+k) = 0;
+      return ptr;
+   }
    if (!hooked) return _calloc(nmemb, size);
    hooked = false;
    void *ptr = _calloc(nmemb, size);
    if (ptr){
-   if ((*_mm)[ptr]){
-   printf("\033[35;7m%p(%ld)\033[m", ptr, size);
-   }else{
-   printf("\033[35m%p(%ld)\033[m", ptr, size);
-   }
+      if ((*_mm)[ptr]){
+         printf("\033[33m%p(%ld)\033[m", ptr, size);
+      }else{
+         printf("\033[33;7m%p(%ld)\033[m", ptr, size);
+      }
    }
    hooked = true;
    return ptr;
-}*/
+}
 
 // *****************************************************************************
-void *memalign(size_t alignment, size_t size){
-   //if (!hooked) return _memalign(alignment, size);
+void *realloc(void *ptr, size_t size){ // Blue
+   if (!_realloc) _init();
+   if (!hooked) return _realloc(ptr, size);
+   hooked = false;
+   void *nptr = _realloc(ptr, size);
+   assert(nptr);
+   if (ptr){
+      if ((*_mm)[ptr]){
+         printf("\033[34m%p(%ld)\033[m", nptr, size);
+      }else{
+         printf("\033[34;7m%p(%ld)\033[m", nptr, size);
+      }
+   }
+   hooked = true;
+   return nptr;
+}
+
+// *****************************************************************************
+void *memalign(size_t alignment, size_t size){ // Magenta
+   if (!_memalign) _init();
+   if (!hooked) return _memalign(alignment, size);
    hooked = false;
    void *ptr = _memalign(alignment, size);
-   /*if (ptr){
-     if ((*_mm)[ptr]){
-     printf("\033[37;7m%p(%ld)\033[m", ptr, size);
-     }else{
-     printf("\033[37m%p(%ld)\033[m", ptr, size);
-     }
-     }*/
+   assert(ptr);
+   if (ptr){
+      if ((*_mm)[ptr]){
+         printf("\033[35m%p(%ld)\033[m", ptr, size);
+      }else{
+         printf("\033[35;7m%p(%ld)\033[m", ptr, size);
+      }
+   }
    hooked = true;
    return ptr;
 }
