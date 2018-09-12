aa1cb6f8401393c8b383c07235ae4427dec3e571
aschaf <andreas.schafelner@dk-compmath.jku.at>
2018-09-12 17:13:41 +0200

    Changed MFEM_VERIFY to include 4D

--- fem/fe_coll.cpp
+++ fem/fe_coll.cpp
@@ -1659,7 +1659,7 @@ const
 H1_FECollection::H1_FECollection(const int p, const int dim, const int btype)
 {
    MFEM_VERIFY(p >= 1, "H1_FECollection requires order >= 1.");
-   MFEM_VERIFY(dim >= 0 && dim <= 3, "H1_FECollection requires 0 <= dim <= 3.");
+   MFEM_VERIFY(dim >= 0 && dim <= 4, "H1_FECollection requires 0 <= dim <= 4.");
 
    const int pm1 = p - 1, pm2 = pm1 - 1, pm3 = pm2 - 1, pm4 = pm3 - 1;
 
