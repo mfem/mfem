16b98c21dd7cefd9033cd832e529eb388b78b28e
Stowell, Mark L <stowell1@llnl.gov>
2018-10-10 20:49:42 -0700

    Adding ParGridFunction::operator=(ParGridFunction&) method just to be certain

--- fem/pgridfunc.hpp
+++ fem/pgridfunc.hpp
@@ -82,6 +82,10 @@ public:
    ParGridFunction &operator=(const Vector &v)
    { GridFunction::operator=(v); return *this; }
 
+   /// Copy the data from a ParGridFunction to the ParGridFunction data.
+   ParGridFunction &operator=(const ParGridFunction &v)
+   { GridFunction::operator=(v); return *this; }
+
    ParFiniteElementSpace *ParFESpace() const { return pfes; }
 
    virtual void Update();
