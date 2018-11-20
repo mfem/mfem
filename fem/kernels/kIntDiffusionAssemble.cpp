d766e2755d3d1b65b6383711955c7209e6e26362
camierjs <camier1@llnl.gov>
2018-11-19 16:05:44 -0800

    Occa NEW_OCCA_KERNEL macro

--- fem/kernels/kIntDiffusionAssemble.cpp
+++ fem/kernels/kIntDiffusionAssemble.cpp
@@ -28,22 +28,8 @@ static void oAssemble2D(const int NUM_QUAD_1D,
   GET_OCCA_CONST_MEMORY(J);
   GET_OCCA_MEMORY(oper);
   
-  static bool setup = false;
-  static occa::kernel Assemble2D;
-  if (not setup){
-    OCCAdevice device = config::Get().OccaDevice();
-    const std::string fdk = "occa://fem/oIntDiffusionAssemble.okl";
-    const std::string odk = occa::io::occaFileOpener().expand(fdk);
-    Assemble2D = device.buildKernel(odk, "Assemble2D");
-    
-    const size_t W_SZ = 4;
-    const size_t J_SZ = 327680;
-    const size_t O_SZ = 245760;
-    o_quadWeights = device.malloc(W_SZ*sizeof(double));
-    o_J = device.malloc(J_SZ*sizeof(double));
-    o_oper = device.malloc(O_SZ*sizeof(double));
-    
-  }
+  NEW_OCCA_KERNEL(Assemble2D,fem,oIntDiffusionAssemble.okl);
+  
   o_quadWeights.copyFrom(quadWeights);
   o_J.copyFrom(J);  
   Assemble2D(NUM_QUAD_1D, numElements, o_quadWeights, o_J, COEFF, o_oper);
