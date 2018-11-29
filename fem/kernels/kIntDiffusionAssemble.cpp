4496c27f0e04ed983b69f717a62bfc4f62f3d8ca
camierjs <camier1@llnl.gov>
2018-11-28 18:42:50 -0800

    Occa Diffusion CPU & GPU kernels drag'n droped from mfem/occa-dev

--- fem/kernels/kIntDiffusionAssemble.cpp
+++ fem/kernels/kIntDiffusionAssemble.cpp
@@ -24,13 +24,14 @@ static void oAssemble2D(const int NUM_QUAD_1D,
                         const double COEFF,
                         double* __restrict oper)
 {
+   const int NUM_QUAD_2D = NUM_QUAD_1D*NUM_QUAD_1D;
+   
    GET_OCCA_CONST_MEMORY(quadWeights);
    GET_OCCA_CONST_MEMORY(J);
    GET_OCCA_MEMORY(oper);
    
    NEW_OCCA_PROPERTY(props);
    SET_OCCA_PROPERTY(props, NUM_QUAD_1D);
-   const int NUM_QUAD_2D = NUM_QUAD_1D*NUM_QUAD_1D;
    SET_OCCA_PROPERTY(props, NUM_QUAD_2D);
    
    NEW_OCCA_KERNEL(Assemble2D, fem, oIntDiffusionAssemble.okl, props);
