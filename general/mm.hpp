857833b670e76be8a89d0361084b7e232cb03adb
Jean-Sylvain Camier <camier1@llnl.gov>
2018-12-18 20:34:24 -0800

    Remove maps usage when unnecessary

--- general/mm.hpp
+++ general/mm.hpp
@@ -27,7 +27,7 @@ public:
    void* const h_adrs;
    void* d_adrs;
    OccaMemory o_adrs;
-   std::list<const alias_t* const> aliases;
+   std::list<const alias_t*> aliases;
    memory_t(void* const h, const size_t b):
       host(true), bytes(b), h_adrs(h), d_adrs(NULL), aliases() {}
 };
