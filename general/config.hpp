24934a4f405914ae42ac163fc686f5abb4135935
camierjs <camier1@llnl.gov>
2018-12-20 12:52:03 -0800

    mm cleanup with new API and logic and Mesh::EnsureNodes

--- general/config.hpp
+++ general/config.hpp
@@ -22,25 +22,25 @@ namespace mfem
 class config
 {
 private:
-   enum BACKENDS{CUDA, OCCA};
-   enum MODES{HOST_MODE, DEVICE_MODE};
+   //enum BACKENDS{CUDA, OCCA};
+   enum MODES{CPU, GPU};
 private:
+   MODES mode;
+   int dev = 0;
+   int ngpu = -1;
    bool pa = false;
-   MODES mode = HOST_MODE;
    bool cuda = false;
    bool occa = false;
    bool sync = false;
    bool nvvp = false;
-   int dev = 0;
-   int gpu_count = 0;
    CUdevice cuDevice;
+   CUstream *cuStream;
    CUcontext cuContext;
-   CUstream *cuStream = NULL;
    OccaDevice occaDevice;
 
 private:
    // **************************************************************************
-   config() {}
+   config(): mode{config::CPU} {}
    config(config const&);
    void operator=(config const&);
 
@@ -54,8 +54,8 @@ private:
 
 private:
    // **************************************************************************
-   void MfemDeviceSetup(const int device =0);
-   void CudaDeviceSetup(const int device =0);
+   void MfemDeviceSetup(const int dev =0);
+   void CudaDeviceSetup(const int dev =0);
    void OccaDeviceSetup(const CUdevice cu_dev, const CUcontext cu_ctx);
 
 public:
@@ -63,20 +63,22 @@ public:
    constexpr static inline bool usingNvcc() { return usingNvccCompiler(); }
 
    // **************************************************************************
-   static inline void enableDevice(const int dev =0) { Get().MfemDeviceSetup(dev); }
-   static inline bool deviceEnabled() { return Get().gpu_count > 0; }
+   static inline void enableGpu(const int dev =0) { Get().MfemDeviceSetup(dev); }
+   static inline bool gpuEnabled() { return Get().ngpu > 0; }
+   static inline bool gpuDisabled() { return Get().ngpu == 0; }
+   static inline bool gpuHasBeenEnabled() { return Get().ngpu >= 0; }
    
-   static inline bool usingGpu() { return deviceEnabled() and Get().mode == DEVICE_MODE; }
+   static inline bool usingGpu() { return gpuEnabled() and Get().mode == GPU; }
    static inline bool usingCpu() { return not usingGpu(); }
 
-   static inline void SwitchToDevice(){ Get().mode = DEVICE_MODE; }
-   static inline void SwitchToHost(){ Get().mode = HOST_MODE; }
+   static inline void SwitchToGpu(){ Get().mode = config::GPU; }
+   static inline void SwitchToCpu(){ Get().mode = config::CPU; }
 
 
    static inline bool usingPA() { return Get().pa; }
    static inline void usePA(const bool mode) { Get().pa = mode; }
 
-   //static inline bool usingCuda() { return Get().cuda; }
+   static inline bool usingCuda() { return Get().cuda; }
    static inline void useCuda() { Get().cuda = true; }
    static inline CUstream Stream() { return *Get().cuStream; }
 
--- general/mm.cpp
+++ general/mm.cpp
@@ -47,7 +47,6 @@ static const void* InsertAlias(const mm_t *maps,
                                const void *base,
                                const void *adrs)
 {
-   assert(adrs > base);
    memory_t *mem = maps->memories->at(base);
    const size_t offset = (char*)adrs - (char*)base;
    const alias_t *alias = new alias_t(mem, offset);
@@ -73,10 +72,16 @@ static bool Alias(const mm_t *maps, const void *adrs)
 // *****************************************************************************
 __attribute__((unused))
 static void debugMode(void){
-   dbg("%sNvcc %sGPU ??Cuda %sOcca",
+   dbg("\033[1K\r%sNvcc %sHasBeenEnabled %sEnabled %sDisabled \
+%sCPU %sGPU %sPA %sCUDA %sOCCA",
        config::usingNvcc()?"\033[32m":"\033[31m",
+       config::gpuHasBeenEnabled()?"\033[32m":"\033[31m",
+       config::gpuEnabled()?"\033[32m":"\033[31m",
+       config::gpuDisabled()?"\033[32m":"\033[31m",
+       config::usingCpu()?"\033[32m":"\033[31m",
        config::usingGpu()?"\033[32m":"\033[31m",
-       //config::usingCuda()?"\033[32m":"\033[31m",
+       config::usingPA()?"\033[32m":"\033[31m",
+       config::usingCuda()?"\033[32m":"\033[31m",
        config::usingOcca()?"\033[32m":"\033[31m");
 }
 
@@ -86,6 +91,7 @@ static void debugMode(void){
 void* mm::Insert(void *adrs, const size_t bytes)
 {
    if (not config::usingNvcc()) return adrs;
+   if (config::gpuDisabled()) return adrs;
    const bool known = Known(maps, adrs);
    MFEM_ASSERT(not known, "Trying to add already present address!");
    dbg("\033[33m%p \033[35m(%ldb)", adrs, bytes);
@@ -99,10 +105,12 @@ void* mm::Insert(void *adrs, const size_t bytes)
 void *mm::Erase(void *adrs)
 {
    if (not config::usingNvcc()) return adrs;
+   if (config::gpuDisabled()) return adrs;
    const bool known = Known(maps, adrs);
    if (not known) { BUILTIN_TRAP; }
    MFEM_ASSERT(known, "Trying to remove an unknown address!");
    const memory_t *mem = memories->at(adrs);
+   dbg("\033[33m %p \033[35m(%ldb)", adrs,mem->bytes);
    for (const alias_t* const alias : mem->aliases) {
       aliases->erase(alias);
       delete alias;
@@ -121,10 +129,8 @@ static void* AdrsKnown(const mm_t *maps, void* adrs){
    if (host and not gpu) { return adrs; }
    if (not base->d_adrs) { cuMemAlloc(&base->d_adrs, bytes); }
    if (device and gpu) { return base->d_adrs; }
-   // Pull
-   if (device and not gpu)
+   if (device and not gpu) // Pull
    {
-      assert(base->d_adrs);
       cuMemcpyDtoH(adrs, base->d_adrs, bytes);
       base->host = true;
       return adrs;
@@ -140,27 +146,25 @@ static void* AdrsKnown(const mm_t *maps, void* adrs){
 static void* AdrsAlias(mm_t *maps, void* adrs){
    const bool gpu = config::usingGpu();
    const alias_t *alias = maps->aliases->at(adrs);
-   memory_t *base = alias->mem;   
+   const memory_t *base = alias->mem;
    const bool host = base->host;
    const bool device = not base->host;
    const size_t bytes = base->bytes;
    if (host and not gpu) { return adrs; }
-   if (not base->d_adrs) { cuMemAlloc(&base->d_adrs, bytes); }
-   void *d_adrs = base->d_adrs;
-   void *a_adrs = (char*)d_adrs + alias->offset;
+   if (not base->d_adrs) { cuMemAlloc(&alias->mem->d_adrs, bytes); }
+   void *a_adrs = (char*)base->d_adrs + alias->offset;
    if (device and gpu) { return a_adrs; }
-   // Pull
-   if (device and not gpu)
+   if (device and not gpu) // Pull
    {
-      assert(d_adrs);
-      cuMemcpyDtoH(base->h_adrs, d_adrs, bytes);
-      base->host = true;
+      assert(base->d_adrs);
+      cuMemcpyDtoH(base->h_adrs, base->d_adrs, bytes);
+      alias->mem->host = true;
       return adrs;
    }
    // Push
    assert(host and gpu);
-   cuMemcpyHtoD(d_adrs, base->h_adrs, bytes);
-   base->host = false;
+   cuMemcpyHtoD(base->d_adrs, base->h_adrs, bytes);
+   alias->mem->host = false;
    return a_adrs;
 }
 
@@ -169,8 +173,9 @@ static void* AdrsAlias(mm_t *maps, void* adrs){
 // *****************************************************************************
 void* mm::Adrs(void *adrs)
 {
+   if (config::gpuDisabled()) return adrs;
    if (not config::usingNvcc()) return adrs;
-   if (not config::deviceEnabled()) return adrs;
+   if (not config::gpuHasBeenEnabled()) return adrs;
    if (Known(maps, adrs)) return AdrsKnown(maps, adrs);
    const bool alias = Alias(maps, adrs);
    if (not alias) { BUILTIN_TRAP; }   
@@ -191,7 +196,6 @@ static OccaMemory occaMemory(const mm_t *maps, const void *adrs)
       OccaMemory o_adrs = occaWrapMemory(occaDevice, (void*)adrs, 0);
       return o_adrs;
    }
-   //assert(not config::usingOcca());
    const bool known = Known(maps, adrs);
    if (not known) { BUILTIN_TRAP; }
    MFEM_ASSERT(known, "Unknown address!");
@@ -240,16 +244,15 @@ static void PushKnown(mm_t *maps, const void *adrs, const size_t bytes)
 static void PushAlias(const mm_t *maps, const void *adrs, const size_t bytes)
 {
    const alias_t *alias = maps->aliases->at(adrs);
-   memory_t *base = alias->mem;
-   if (not base->d_adrs){ cuMemAlloc(&base->d_adrs, base->bytes); }
-   cuMemcpyHtoD((char*)base->d_adrs + alias->offset, adrs, bytes);
+   cuMemcpyHtoD((char*)alias->mem->d_adrs + alias->offset, adrs, bytes);
 }
 
 // *****************************************************************************
 void mm::Push(const void *adrs, const size_t bytes)
 {
+   if (config::gpuDisabled()) return;
    if (not config::usingNvcc()) return;
-   if (not config::usingGpu()) return;
+   if (not config::gpuHasBeenEnabled()) return;
    if (Known(maps, adrs)) return PushKnown(maps, adrs, bytes);
    assert(not config::usingOcca());
    const bool alias = Alias(maps, adrs);
@@ -261,8 +264,7 @@ void mm::Push(const void *adrs, const size_t bytes)
 // *****************************************************************************
 static void PullKnown(const mm_t *maps, const void *adrs, const size_t bytes)
 {
-   memory_t *base = maps->memories->at(adrs);
-   if (not base->d_adrs){ cuMemAlloc((void**)&base->d_adrs, base->bytes); }
+   const memory_t *base = maps->memories->at(adrs);
    cuMemcpyDtoH(base->h_adrs, base->d_adrs, bytes==0?base->bytes:bytes);
 }
 
@@ -270,16 +272,15 @@ static void PullKnown(const mm_t *maps, const void *adrs, const size_t bytes)
 static void PullAlias(const mm_t *maps, const void *adrs, const size_t bytes)
 {
    const alias_t *alias = maps->aliases->at(adrs);
-   const memory_t *base = alias->mem;
-   assert(base->d_adrs);
-   cuMemcpyDtoH((void*)adrs, (char*)base->d_adrs + alias->offset, bytes);
+   cuMemcpyDtoH((void*)adrs, (char*)alias->mem->d_adrs + alias->offset, bytes);
 }
 
 // *****************************************************************************
 void mm::Pull(const void *adrs, const size_t bytes)
 {
+   if (config::gpuDisabled()) return;
    if (not config::usingNvcc()) return;
-   if (not config::usingGpu()) return;
+   if (not config::gpuHasBeenEnabled()) return;
    if (Known(maps, adrs)) return PullKnown(maps, adrs, bytes);
    assert(not config::usingOcca());
    const bool alias = Alias(maps, adrs);
@@ -303,7 +304,8 @@ static void Dump(mm_t *maps){
       if (not d_adrs){
          printf("\n[%ld] \033[33m%p \033[35m(%ld)", k, h_adrs, bytes);
       }else{
-         printf("\n[%ld] \033[33m%p \033[35m (%ld) \033[32 -> %p", k, h_adrs, bytes, d_adrs);
+         printf("\n[%ld] \033[33m%p \033[35m (%ld) \033[32 -> %p",
+                k, h_adrs, bytes, d_adrs);
       }
       fflush(0);
       k++;
@@ -313,7 +315,8 @@ static void Dump(mm_t *maps){
       const void *adrs = a->first;
       const size_t offset = a->second->offset;
       const void *base = a->second->mem->h_adrs;
-      printf("\n[%ld] \033[33m%p < (\033[37m%ld) < \033[33m%p",k , base, offset, adrs);
+      printf("\n[%ld] \033[33m%p < (\033[37m%ld) < \033[33m%p",
+             k , base, offset, adrs);
       fflush(0);
       k++;
    }
@@ -325,12 +328,10 @@ static void Dump(mm_t *maps){
 static void* d2d(void *dst, const void *src, size_t bytes, const bool async){
    GET_ADRS(src);
    GET_ADRS(dst);
-   assert(bytes>0);
    const bool cpu = config::usingCpu();
-   if (cpu) { return std::memcpy(d_dst, d_src, bytes); }
-   if (not async) { cuMemcpyDtoD(d_dst, (void*)d_src, bytes); }
-   else { cuMemcpyDtoDAsync(d_dst, (void*)d_src, bytes, config::Stream()); }
-   return dst;
+   if (cpu) return std::memcpy(d_dst, d_src, bytes);
+   if (not async) return cuMemcpyDtoD(d_dst, (void*)d_src, bytes);
+   return cuMemcpyDtoDAsync(d_dst, (void*)d_src, bytes, config::Stream());
 }
 
 // *****************************************************************************
