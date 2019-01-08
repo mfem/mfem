2f2efd6b99eb483f61672e38dd5c8e54c45a2df9
camierjs <camier1@llnl.gov>
2019-01-07 16:47:57 -0800

    Offset cleanup

--- general/okina.hpp
+++ general/okina.hpp
@@ -79,13 +79,17 @@ uint32_t LOG2(uint32_t);
 #define MFEM_GPU_CANNOT_PASS {assert(FILE_LINE && !config::usingGpu());}
 
 // Offsets *********************************************************************
-#define ijN(i,j,N) (i)+(N)*(j)
-#define ijkN(i,j,k,N) (i)+(N)*((j)+(N)*(k))
-#define ijklN(i,j,k,l,N) (i)+(N)*((j)+(N)*((k)+(N)*(l)))
-
-#define ijNMt(i,j,N,M,t) (t)?((i)+(N)*(j)):((j)+(M)*(i))
-#define ijkNM(i,j,k,N,M) (i)+(N)*((j)+(M)*(k))
-#define ijklNM(i,j,k,l,N,M) (i)+(N)*((j)+(N)*((k)+(M)*(l)))
+#define ijN(i,j,N) (i)+N*(j)
+#define ijkN(i,j,k,N) (i)+N*((j)+N*(k))
+#define ijklN(i,j,k,l,N) (i)+N*((j)+N*((k)+N*(l)))
+#define ijNMt(i,j,N,M,t) (t)?((i)+N*(j)):((j)+M*(i))
+#define ijkNM(i,j,k,N,M) (i)+N*((j)+M*(k))
+#define ijklNM(i,j,k,l,N,M) (i)+N*((j)+N*((k)+M*(l)))
+// External offsets
+#define jkliNM(i,j,k,l,N,M) (j)+N*((k)+N*((l)+M*(i)))
+#define jklmiNM(i,j,k,l,m,N,M) (j)+N*((k)+N*((l)+N*((m)+M*(i))))
+#define xyeijDQE(i,j,x,y,e,D,Q,E) (x)+Q*((y)+Q*((e)+E*((i)+D*j)))
+#define xyzeijDQE(i,j,x,y,z,e,D,Q,E) (x)+Q*((y)+Q*((z)+Q*((e)+E*((i)+D*j))))
 
 // *****************************************************************************
 const char *strrnchr(const char*, const unsigned char, const int);
