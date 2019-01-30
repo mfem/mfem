70653ee1e53488bc09b543ff29657c0a308f32d2
Stefano Zampini <stefano.zampini@gmail.com>
2019-01-30 13:30:14 +0300

    BilinearIntegrators: made all parameters (scalars and coefficients) protected to make them accessible to derived class
    For all public integrators, make coefficients usage consistent and store a pointer instead of a reference
    This affected Convection, Derivative and *ProductInterpolator integrators

--- fem/bilininteg.hpp
+++ fem/bilininteg.hpp
@@ -273,9 +273,9 @@ protected:
                                       Vector & shape)
    { trial_fe.CalcPhysShape(Trans, shape); }
 
-private:
-
    Coefficient *Q;
+
+private:
 
 #ifndef MFEM_THREAD_SAFE
    Vector test_shape;
@@ -347,12 +347,12 @@ protected:
                                       DenseMatrix & shape)
    { trial_fe.CalcVShape(Trans, shape); }
 
-private:
-
    Coefficient *Q;
    VectorCoefficient *VQ;
    VectorCoefficient *DQ;
    MatrixCoefficient *MQ;
+
+private:
 
 #ifndef MFEM_THREAD_SAFE
    Vector V;
@@ -428,11 +428,11 @@ protected:
                                  Vector & shape)
    { scalar_fe.CalcPhysShape(Trans, shape); }
 
-private:
-
    VectorCoefficient *VQ;
    bool transpose;
    bool cross_2d;  // In 2D use a cross product rather than a dot product
+
+private:
 
 #ifndef MFEM_THREAD_SAFE
    Vector V;
@@ -1626,14 +1626,16 @@ protected:
     can be a scalar or a matrix coefficient. */
 class DiffusionIntegrator: public BilinearFormIntegrator
 {
+protected:
+   Coefficient *Q;
+   MatrixCoefficient *MQ;
+
 private:
    Vector vec, pointflux, shape;
 #ifndef MFEM_THREAD_SAFE
    DenseMatrix dshape, dshapedxt, invdfdx, mq;
    DenseMatrix te_dshape, te_dshapedxt;
 #endif
-   Coefficient *Q;
-   MatrixCoefficient *MQ;
 
 public:
    /// Construct a diffusion integrator with coefficient Q = 1
@@ -1715,17 +1717,19 @@ public:
 /// alpha (q . grad u, v)
 class ConvectionIntegrator : public BilinearFormIntegrator
 {
+protected:
+   VectorCoefficient *Q;
+   double alpha;
+
 private:
 #ifndef MFEM_THREAD_SAFE
    DenseMatrix dshape, adjJ, Q_ir;
    Vector shape, vec2, BdFidxT;
 #endif
-   VectorCoefficient &Q;
-   double alpha;
 
 public:
    ConvectionIntegrator(VectorCoefficient &q, double a = 1.0)
-      : Q(q) { alpha = a; }
+      : Q(&q) { alpha = a; }
    virtual void AssembleElementMatrix(const FiniteElement &,
                                       ElementTransformation &,
                                       DenseMatrix &);
@@ -1734,15 +1738,17 @@ public:
 /// alpha (q . grad u, v) using the "group" FE discretization
 class GroupConvectionIntegrator : public BilinearFormIntegrator
 {
+protected:
+   VectorCoefficient *Q;
+   double alpha;
+
 private:
    DenseMatrix dshape, adjJ, Q_nodal, grad;
    Vector shape;
-   VectorCoefficient &Q;
-   double alpha;
 
 public:
    GroupConvectionIntegrator(VectorCoefficient &q, double a = 1.0)
-      : Q(q) { alpha = a; }
+      : Q(&q) { alpha = a; }
    virtual void AssembleElementMatrix(const FiniteElement &,
                                       ElementTransformation &,
                                       DenseMatrix &);
@@ -1807,11 +1813,14 @@ public:
     does NOT depend on the ElementTransformation Trans. */
 class VectorFEDivergenceIntegrator : public BilinearFormIntegrator
 {
-private:
+protected:
    Coefficient *Q;
+
+private:
 #ifndef MFEM_THREAD_SAFE
    Vector divshape, shape;
 #endif
+
 public:
    VectorFEDivergenceIntegrator() { Q = NULL; }
    VectorFEDivergenceIntegrator(Coefficient &q) { Q = &q; }
@@ -1829,14 +1838,17 @@ public:
     This is equivalent to a weak divergence of the Nedelec basis functions. */
 class VectorFEWeakDivergenceIntegrator: public BilinearFormIntegrator
 {
-private:
+protected:
    Coefficient *Q;
+
+private:
 #ifndef MFEM_THREAD_SAFE
    DenseMatrix dshape;
    DenseMatrix dshapedxt;
    DenseMatrix vshape;
    DenseMatrix invdfdx;
 #endif
+
 public:
    VectorFEWeakDivergenceIntegrator() { Q = NULL; }
    VectorFEWeakDivergenceIntegrator(Coefficient &q) { Q = &q; }
@@ -1853,13 +1865,16 @@ public:
     test spaces are switched, assembles the form (u, curl v). */
 class VectorFECurlIntegrator: public BilinearFormIntegrator
 {
-private:
+protected:
    Coefficient *Q;
+
+private:
 #ifndef MFEM_THREAD_SAFE
    DenseMatrix curlshapeTrial;
    DenseMatrix vshapeTest;
    DenseMatrix curlshapeTrial_dFT;
 #endif
+
 public:
    VectorFECurlIntegrator() { Q = NULL; }
    VectorFECurlIntegrator(Coefficient &q) { Q = &q; }
@@ -1872,17 +1887,19 @@ public:
                                        DenseMatrix &elmat);
 };
 
-
 /// Class for integrating (Q D_i(u), v); u and v are scalars
 class DerivativeIntegrator : public BilinearFormIntegrator
 {
+protected:
+   Coefficient* Q;
+
 private:
-   Coefficient & Q;
    int xi;
    DenseMatrix dshape, dshapedxt, invdfdx;
    Vector shape, dshapedxi;
+
 public:
-   DerivativeIntegrator(Coefficient &q, int i) : Q(q), xi(i) { }
+   DerivativeIntegrator(Coefficient &q, int i) : Q(&q), xi(i) { }
    virtual void AssembleElementMatrix(const FiniteElement &el,
                                       ElementTransformation &Trans,
                                       DenseMatrix &elmat)
@@ -1902,6 +1919,8 @@ private:
    DenseMatrix curlshape, curlshape_dFt, M;
    DenseMatrix vshape, projcurl;
 #endif
+
+protected:
    Coefficient *Q;
    MatrixCoefficient *MQ;
 
@@ -1935,6 +1954,8 @@ private:
 #ifndef MFEM_THREAD_SAFE
    DenseMatrix dshape_hat, dshape, curlshape, Jadj, grad_hat, grad;
 #endif
+
+protected:
    Coefficient *Q;
 
 public:
@@ -1994,9 +2015,10 @@ public:
     scalar FE space; p is also in a (different) scalar FE space.  */
 class VectorDivergenceIntegrator : public BilinearFormIntegrator
 {
-private:
+protected:
    Coefficient *Q;
 
+private:
    Vector shape;
    Vector divshape;
    DenseMatrix dshape;
@@ -2017,9 +2039,10 @@ public:
 /// (Q div u, div v) for RT elements
 class DivDivIntegrator: public BilinearFormIntegrator
 {
-private:
+protected:
    Coefficient *Q;
 
+private:
 #ifndef MFEM_THREAD_SAFE
    Vector divshape;
 #endif
@@ -2041,9 +2064,10 @@ public:
     diffusion matrix in each diagonal block. */
 class VectorDiffusionIntegrator : public BilinearFormIntegrator
 {
-private:
+protected:
    Coefficient *Q;
 
+private:
    DenseMatrix Jinv;
    DenseMatrix dshape;
    DenseMatrix gshape;
@@ -2068,10 +2092,11 @@ public:
     using multiple copies of a scalar FE space. */
 class ElasticityIntegrator : public BilinearFormIntegrator
 {
-private:
+protected:
    double q_lambda, q_mu;
    Coefficient *lambda, *mu;
 
+private:
 #ifndef MFEM_THREAD_SAFE
    Vector shape;
    DenseMatrix dshape, gshape, pelmat;
@@ -2128,11 +2153,12 @@ public:
     points. */
 class DGTraceIntegrator : public BilinearFormIntegrator
 {
-private:
+protected:
    Coefficient *rho;
    VectorCoefficient *u;
    double alpha, beta;
 
+private:
    Vector shape1, shape2;
 
 public:
@@ -2419,7 +2445,7 @@ public:
 class ScalarProductInterpolator : public DiscreteInterpolator
 {
 public:
-   ScalarProductInterpolator(Coefficient & sc) : Q(sc) { }
+   ScalarProductInterpolator(Coefficient & sc) : Q(&sc) { }
 
    virtual void AssembleElementMatrix2(const FiniteElement &dom_fe,
                                        const FiniteElement &ran_fe,
@@ -2427,7 +2453,7 @@ public:
                                        DenseMatrix &elmat);
 
 protected:
-   Coefficient &Q;
+   Coefficient *Q;
 };
 
 /** Interpolator of a scalar coefficient multiplied by a vector field onto
@@ -2437,14 +2463,14 @@ class ScalarVectorProductInterpolator : public DiscreteInterpolator
 {
 public:
    ScalarVectorProductInterpolator(Coefficient & sc)
-      : Q(sc) { }
+      : Q(&sc) { }
 
    virtual void AssembleElementMatrix2(const FiniteElement &dom_fe,
                                        const FiniteElement &ran_fe,
                                        ElementTransformation &Trans,
                                        DenseMatrix &elmat);
 protected:
-   Coefficient &Q;
+   Coefficient *Q;
 };
 
 /** Interpolator of a vector coefficient multiplied by a scalar field onto
@@ -2454,14 +2480,14 @@ class VectorScalarProductInterpolator : public DiscreteInterpolator
 {
 public:
    VectorScalarProductInterpolator(VectorCoefficient & vc)
-      : VQ(vc) { }
+      : VQ(&vc) { }
 
    virtual void AssembleElementMatrix2(const FiniteElement &dom_fe,
                                        const FiniteElement &ran_fe,
                                        ElementTransformation &Trans,
                                        DenseMatrix &elmat);
 protected:
-   VectorCoefficient &VQ;
+   VectorCoefficient *VQ;
 };
 
 /** Interpolator of the cross product between a vector coefficient and an
@@ -2471,14 +2497,14 @@ class VectorCrossProductInterpolator : public DiscreteInterpolator
 {
 public:
    VectorCrossProductInterpolator(VectorCoefficient & vc)
-      : VQ(vc) { }
+      : VQ(&vc) { }
 
    virtual void AssembleElementMatrix2(const FiniteElement &nd_fe,
                                        const FiniteElement &rt_fe,
                                        ElementTransformation &Trans,
                                        DenseMatrix &elmat);
 protected:
-   VectorCoefficient &VQ;
+   VectorCoefficient *VQ;
 };
 
 /** Interpolator of the inner product between a vector coefficient and an
@@ -2487,14 +2513,14 @@ protected:
 class VectorInnerProductInterpolator : public DiscreteInterpolator
 {
 public:
-   VectorInnerProductInterpolator(VectorCoefficient & vc) : VQ(vc) { }
+   VectorInnerProductInterpolator(VectorCoefficient & vc) : VQ(&vc) { }
 
    virtual void AssembleElementMatrix2(const FiniteElement &rt_fe,
                                        const FiniteElement &l2_fe,
                                        ElementTransformation &Trans,
                                        DenseMatrix &elmat);
 protected:
-   VectorCoefficient &VQ;
+   VectorCoefficient *VQ;
 };
 
 }
