5b4619e19bf21ca328473eb37a328bd887435971
Jean-Sylvain Charles Camier <camier1@llnl.gov>
2019-02-17 11:52:44 -0800

    AppVeyor test

--- fem/bilininteg_ext.cpp
+++ fem/bilininteg_ext.cpp
@@ -127,7 +127,8 @@ void MassIntegrator::Assemble(const FiniteElementSpace &fes)
       const kernels::Vector x(3,NQ,geo->X.GetData());
       const kernels::Vector J(2,2,NQ,geo->J.GetData());
       kernels::Vector v(NQ,NE,vec.GetData());
-      MFEM_FORALL(e, NE,
+      const size_t NS = 0,
+      MFEM_FORALL_SHARED(e, NE, Ns
       {
          for (int q = 0; q < NQ; ++q)
          {
