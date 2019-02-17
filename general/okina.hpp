00f2823bb7a9f1365c893911473028e895a33922
Jean-Sylvain Charles Camier <camier1@llnl.gov>
2019-02-17 13:59:02 -0800

    AppVeyor local variable

--- general/okina.hpp
+++ general/okina.hpp
@@ -70,7 +70,8 @@ void wrapWithShared(const size_t N, const size_t Nspt,
    }
    else
    {
-      double cpu_mem_s[Nspt];
+      const int nspt = Nspt;
+      double cpu_mem_s[nspt];
       for (size_t k = 0; k < N; k += 1)
       {
          h_body(k, cpu_mem_s);
