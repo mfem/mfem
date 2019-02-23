fc13bc7e1f8a9c26ac0a3df088756dbb469f84c3
Tzanio <tzanio@llnl.gov>
2019-02-23 10:29:54 -0800

    make style

--- linalg/densemat.hpp
+++ linalg/densemat.hpp
@@ -731,7 +731,7 @@ public:
    double *GetData(int k) { return tdata+k*Mk.Height()*Mk.Width(); }
 
    double *Data() { return tdata; }
-   
+
    const double *Data() const { return tdata; }
 
    /** Matrix-vector product from unassembled element matrices, assuming both
