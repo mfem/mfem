90d032135a0eb085f47a6b7a55a4fd97be1c5b42
Stowell, Mark L <stowell1@llnl.gov>
2019-02-24 14:57:39 -0800

    Adding inline functions for mean collision times

--- miniapps/plasma/plasma.hpp
+++ miniapps/plasma/plasma.hpp
@@ -31,9 +31,45 @@ static const double mu0_ = 4.0e-7 * M_PI;
 // Speed of light in Free Space (units m/s)
 static const double c0_ = 1.0 / sqrt(epsilon0_ * mu0_);
 
-static const double q_  = 1.60217662e-19;  // Elementary charge in coulombs
+static const double q_  = 1.602176634e-19; // Elementary charge in coulombs
 static const double u_  = 1.660539040e-27; // Atomic mass unit in kilograms
 static const double me_ = 9.10938356e-31;  // Mass of electron in kilograms
+
+/**
+   Returns the mean Electron-Ion collision time in seconds
+   Te is the electron temperature in eV
+   ni is the density of ions (assuming ni=ne) in particles per meter^3
+   zi is the charge number of the ion species
+   lnLambda is the Coulomb Logarithm
+*/
+inline double meanElectronIonCollisionTime(double Te, double ni, int zi,
+                                           double lnLambda)
+{
+   // The factor of q_^{3/2} is included to convert Te from eV to Joules
+   return 0.75 * pow(4.0 * M_PI * epsilon0_, 2) *
+          sqrt(0.5 * me_ * pow(q_ * Te, 3) / M_PI) /
+          (lnLambda * pow(q_, 4) * zi * zi * ni);
+}
+
+/**
+   Returns the mean Ion-Ion collision time in seconds
+   In the following 'a' refers to the species whose mean collision time is
+   being computed and 'b' refers to the species being collided with.
+   ma is the ion mass in a.m.u.
+   Ta is the ion temperature in eV
+   nb is the density of ions in particles per meter^3
+   za is the charge number of the ion species
+   zb is the charge number of the ion species
+   lnLambda is the Coulomb Logarithm
+*/
+inline double meanIonIonCollisionTime(double ma, double Ta, double nb,
+                                      int za, int zb, double lnLambda)
+{
+   // The factor of q_^{3/2} is included to convert Ti from eV to Joules
+   return 0.75 * pow(4.0 * M_PI * epsilon0_, 2) *
+          sqrt(ma * u_ * pow(q_ * Ta, 3) / M_PI) /
+          (lnLambda * pow(q_, 4) * za * zb * nb);
+}
 
 } // namespace plasma
 
