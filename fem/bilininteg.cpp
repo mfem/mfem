bf90f2b68753695a4d0a48db1c7987742939d9db
Will Pazner <pazner1@llnl.gov>
2019-03-06 13:23:19 -0800

    Bug fix
    Did not multiply eta stability parameter in BR2 method by number
    of faces neighboring each element, as is required for the
    stability proof. This is now fixed.

--- fem/bilininteg.cpp
+++ fem/bilininteg.cpp
@@ -2886,7 +2886,7 @@ void DGDiffusionBR2Integrator::AssembleFaceMatrix(
          el2.CalcShape(eip2, shape2);
       }
 
-      double w = eta*ip.weight;
+      double w = factor*eta*ip.weight;
       if (ndof2)
       {
          w /= 2;
