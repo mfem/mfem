33606607b13af223d8fc7af2a8c9e0239b24f61c
Qi Tang <tangqi@msu.edu>
2019-03-11 09:33:54 -0600

    qt

---
+++ miniapps/mhd/myCoefficent.hpp
@@ -0,0 +1,49 @@
+//write my own coefficient class
+//coefficient:=[-Dy u, Dx u]
+#include "mfem.hpp"
+
+namespace mfem
+{
+    class MyCoefficient : public VectorGridFunctionCoefficient
+    {
+      private:
+         GridFunction &u;
+      public:
+         MyCoefficient(GridFunction &_u);
+         virtual void Eval(Vector &V, ElementTransformation &T,
+                     const IntegrationPoint &ip);
+         virtual void Eval(DenseMatrix &M, ElementTransformation &T,
+                     const IntegrationRule &ir);
+
+         virtual void Eval(ElementTransformation &T, const IntegrationPoint &ip);
+         virtual ~MyCoefficient() { }
+    };
+
+    MyCoefficient::MyCoefficient( GridFunction &_u ) : u(_u) { }
+
+    void MyCoefficient::Eval(Vector &V, ElementTransformation &T,
+                     const IntegrationPoint &ip);
+    {
+        V.SetSize(dim);
+        Vector grad;
+        u.GetGradient(T.ElementNo, ip, grad);
+
+        V(0)=-grad(1);
+        V(1)= grad(0);
+    }
+
+    void MyCoefficient::Eval(DenseMatrix &M, ElementTransformation &T, 
+                         const IntegrationRule &ir)
+    {
+        M.SetSize(vdim, ir.GetNPoints());
+        DenseMatrix grad;
+        u.GetGradients(T, ir, grad);
+
+        for (j=0, j<ir.GetNPoints(), j++)
+        {
+            M(0,j)=-grad(1,j);
+            M(1,j)= grad(0,j);
+        }
+    }
+    
+}
