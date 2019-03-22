510fe9b604c38331f2d66978952c6df34e2ff055
Andrew T. Barker <barker29@llnl.gov>
2019-03-22 10:03:31 -0700

    pmesh: delete faces_tbl in UniformRefinement3D()

--- mesh/pmesh.cpp
+++ mesh/pmesh.cpp
@@ -3616,6 +3616,7 @@ void ParMesh::UniformRefinement3D()
    // update the groups
    UniformRefineGroups3D(old_nv, old_nedges, v_to_v, *faces_tbl,
                          f2qf.Size() ? &f2qf : NULL);
+   delete faces_tbl;
 
    UpdateNodes();
 }
