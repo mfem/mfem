7231c032cf9f874b642c895ae1a9ad658db94e87
Stowell, Mark L <stowell1@llnl.gov>
2019-03-27 20:05:12 -0700

    Overlooked operator deletions in maxwell miniapp

--- miniapps/electromagnetics/maxwell_solver.cpp
+++ miniapps/electromagnetics/maxwell_solver.cpp
@@ -321,7 +321,9 @@ MaxwellSolver::~MaxwellSolver()
 
    delete M1Losses_;
    delete M2MuInv_;
-
+   delete NegCurl_;
+   delete WeakCurlMuInv_;
+   
    delete hDivMassMuInv_;
    delete hCurlLosses_;
    delete weakCurlMuInv_;
