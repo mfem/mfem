957b35e1bd59932e96710ebd31185528cc49dfe9
Stowell, Mark L <stowell1@llnl.gov>
2019-04-09 16:08:16 -0700

    Removing debugging info

--- fem/complex_fem.cpp
+++ fem/complex_fem.cpp
@@ -661,23 +661,15 @@ ParSesquilinearForm::FormLinearSystem(const Array<int> &ess_tdof_list,
    ParFiniteElementSpace * pfes = pblfr_->ParFESpace();
 
    int tvs = pfes->TrueVSize();
-   cout << "TrueVSize returns " << tvs << endl;
-   cout << "GetVSize returns " << pfes->GetVSize() << endl;
 
    int vsize = x.Size() / 2;
-   // int vsize  = pfes->GetVSize();
-   // int tvsize = pfes->GetTrueVSize();
-
-   cout << "x.Size/2 returns " << vsize << endl;
 
    double s = (conv_ == ComplexOperator::HERMITIAN)?1.0:-1.0;
 
    // Allocate temporary vectors
    Vector b_0(vsize);  b_0 = 0.0;
-   // Vector B_0(tvsize); B_0 = 0.0;
 
    // Extract the real and imaginary parts of the input vectors
-   // MFEM_ASSERT(x.Size() == 2 * vsize, "Input GridFunction of incorrect size!");
    Vector x_r(x.GetData(), vsize);
    Vector x_i(&(x.GetData())[vsize], vsize);
 
@@ -685,18 +677,10 @@ ParSesquilinearForm::FormLinearSystem(const Array<int> &ess_tdof_list,
    Vector b_r(b.GetData(), vsize);
    Vector b_i(&(b.GetData())[vsize], vsize);
    b_i *= s;
-   /*
-   X.SetSize(2 * tvsize);
-   Vector X_r(X.GetData(), tvsize);
-   Vector X_i(&(X.GetData())[tvsize], tvsize);
 
-   B.SetSize(2 * tvsize);
-   Vector B_r(B.GetData(), tvsize);
-   Vector B_i(&(B.GetData())[tvsize], tvsize);
-   */
    OperatorHandle A_r, A_i;
    Vector X_0, B_0;
-   cout << "pblfr fls 1" << endl << flush;
+
    b_0 = b_r;
    pblfr_->FormLinearSystem(ess_tdof_list, x_r, b_0, A_r, X_0, B_0, ci);
 
@@ -708,15 +692,15 @@ ParSesquilinearForm::FormLinearSystem(const Array<int> &ess_tdof_list,
    Vector B_r(B.GetData(), tvsize);
    Vector B_i(&(B.GetData())[tvsize], tvsize);
    X_r = X_0; B_r = B_0;
-   cout << "pblfi fls 1" << endl << flush;
+
    b_0 = 0.0;
    pblfi_->FormLinearSystem(ess_tdof_list, x_i, b_0, A_i, X_0, B_0, false);
    B_r -= B_0;
-   cout << "pblfr fls 2" << endl << flush;
+
    b_0 = b_i;
    pblfr_->FormLinearSystem(ess_tdof_list, x_i, b_0, A_r, X_0, B_0, ci);
    X_i = X_0; B_i = B_0;
-   cout << "pblfi fls 2" << endl << flush;
+
    b_0 = 0.0;
    pblfi_->FormLinearSystem(ess_tdof_list, x_r, b_0, A_i, X_0, B_0, false);
    B_i += B_0;
