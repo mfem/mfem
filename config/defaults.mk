fa679612f32bed243f667507ef2f6bafea20ad8b
Yohann Dudouit <yohann.dudouit@gmail.com>
2019-04-19 15:59:55 -0700

    Add MFEM_USE_CEED option

--- config/defaults.mk
+++ config/defaults.mk
@@ -124,6 +124,7 @@ MFEM_USE_PUMI          = NO
 MFEM_USE_CUDA          = NO
 MFEM_USE_RAJA          = NO
 MFEM_USE_OCCA          = NO
+MFEM_USE_CEED          = NO
 
 # Compile and link options for zlib.
 ZLIB_DIR =
@@ -307,6 +308,11 @@ CUDA_LIB =
 OCCA_DIR = @MFEM_DIR@/../occa
 OCCA_OPT = -I$(OCCA_DIR)/include
 OCCA_LIB = $(XLINKER)-rpath,$(OCCA_DIR)/lib -L$(OCCA_DIR)/lib -locca
+
+# CEED library configuration
+CEED_DIR ?= @MFEM_DIR@/../libCEED
+CEED_OPT = -I$(CEED_DIR)/include
+CEED_LIB = $(XLINKER)-rpath,$(CEED_DIR)/lib -L$(CEED_DIR)/lib -lceed
 
 # RAJA library configuration
 RAJA_DIR = @MFEM_DIR@/../raja
