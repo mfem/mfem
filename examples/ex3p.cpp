6948882371976d59ffc9d0bc9fa1f037b836c602
Stowell, Mark L <stowell1@llnl.gov>
2019-05-18 10:54:22 -0700

    Replacing ReorientTetMesh with ExchangeFaceNbrData (also changing related comments)

--- examples/ex3p.cpp
+++ examples/ex3p.cpp
@@ -118,9 +118,9 @@ int main(int argc, char *argv[])
 
    // 5. Define a parallel mesh by a partitioning of the serial mesh. Refine
    //    this mesh further in parallel to increase the resolution. Once the
-   //    parallel mesh is defined, the serial mesh can be deleted. Tetrahedral
-   //    meshes need to be reoriented before we can define high-order Nedelec
-   //    spaces on them.
+   //    parallel mesh is defined, the serial mesh can be deleted. Face
+   //    orientation information must be exchanged with neighboring processors
+   //    before we can define high-order Nedelec spaces.
    ParMesh *pmesh = new ParMesh(MPI_COMM_WORLD, *mesh);
    delete mesh;
    {
