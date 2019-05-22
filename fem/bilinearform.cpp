c3999ba78b6955e7503612e0ec01887d84a5788f
Tzanio <tzanio@llnl.gov>
2019-05-22 08:16:12 -0700

    A few shortcuts for Vector + Memory

--- fem/bilinearform.cpp
+++ fem/bilinearform.cpp
@@ -717,7 +717,7 @@ void BilinearForm::RecoverFEMSolution(const Vector &X,
 
          // If the validity flags of X's Memory were changed (e.g. if it was
          // moved to device memory) then we need to tell x about that.
-         x.GetMemory().SyncWith(X.GetMemory());
+         x.SyncMemory(X);
       }
    }
    else // non-conforming space
