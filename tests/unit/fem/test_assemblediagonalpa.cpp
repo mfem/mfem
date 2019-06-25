21ccf19b0ceba128df74e09aaaa4e4dc5ba6f6c3
Andrew T. Barker <barker29@llnl.gov>
2019-06-25 15:57:17 -0700

    BilinearIntegrator: implement 2D diffusion diagonal assembly kernel

--- tests/unit/fem/test_assemblediagonalpa.cpp
+++ tests/unit/fem/test_assemblediagonalpa.cpp
@@ -65,4 +65,52 @@ TEST_CASE("massdiag")
    }
 }
 
+TEST_CASE("diffusiondiag")
+{
+   for (int dimension = 2; dimension < 4; ++dimension)
+   {
+      for (int ne = 1; ne < 3; ++ne)
+      {
+         std::cout << "Testing " << dimension << "D partial assembly diffusion diagonal: "
+                   << std::pow(ne, dimension) << " elements." << std::endl;
+         for (int order = 1; order < 5; ++order)
+         {
+            Mesh * mesh;
+            if (dimension == 2)
+            {
+               mesh = new Mesh(ne, ne, Element::QUADRILATERAL, 1, 1.0, 1.0);
+            }
+            else
+            {
+               mesh = new Mesh(ne, ne, ne, Element::HEXAHEDRON, 1, 1.0, 1.0, 1.0);
+            }
+            FiniteElementCollection *h1_fec = new H1_FECollection(order, dimension);
+            FiniteElementSpace h1_fespace(mesh, h1_fec);
+            BilinearForm paform(&h1_fespace);
+            ConstantCoefficient one(1.0);
+            paform.SetAssemblyLevel(AssemblyLevel::PARTIAL);
+            paform.AddDomainIntegrator(new DiffusionIntegrator(one));
+            paform.Assemble();
+            Vector pa_diag(h1_fespace.GetVSize());
+            paform.AssembleDiagonal(pa_diag);
+
+            BilinearForm assemblyform(&h1_fespace);
+            assemblyform.AddDomainIntegrator(new DiffusionIntegrator(one));
+            assemblyform.Assemble();
+            assemblyform.Finalize();
+            Vector assembly_diag(h1_fespace.GetVSize());
+            assemblyform.SpMat().GetDiag(assembly_diag);
+
+            assembly_diag -= pa_diag;
+            double error = assembly_diag.Norml2();
+            std::cout << "    order: " << order << ", error norm: " << error << std::endl;
+            REQUIRE(assembly_diag.Norml2() < 1.e-12);
+
+            delete mesh;
+            delete h1_fec;
+         }
+      }
+   }
+}
+
 } // namespace assemblediagonalpa
