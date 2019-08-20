d0d0dc03567d87838c5e0709243a7b90c640f40f
Vladimir Tomov <tomov2@llnl.gov>
2019-08-20 16:59:51 -0700

    Improved the description of the hiop examples.

--- examples/hiop/ex9p.cpp
+++ examples/hiop/ex9p.cpp
@@ -1,4 +1,4 @@
-//                       MFEM Example 9 - Parallel Version
+//  MFEM Example 9 with Nonlinear Constrained Optimization - Parallel Version
 //
 // Compile with: make ex9p
 //
@@ -28,8 +28,15 @@
 //    mpirun -np 4 ex9p -m ../../data/periodic-cube.mesh -p 0 -rs 2 -o 2 -dt 0.02 -tf 8 -opt 1
 //    mpirun -np 4 ex9p -m ../../data/periodic-cube.mesh -p 0 -rs 2 -o 2 -dt 0.02 -tf 8 -opt 2
 
+// Description:  This example modifies the standard MFEM ex9 by adding nonlinear
+//               constrained optimization capabilities through the SLBQP and
+//               HIOP solvers. It demonstrates how a user can define a custom
+//               class OptimizationProblem that includes linear/nonlinear
+//               equality/inequality constraints. This optimization is applied
+//               as post-processing to the solution of the transport equation.
 //
-// Description:  This example code solves the time-dependent advection equation
+//               Description of ex9:
+//               This example code solves the time-dependent advection equation
 //               du/dt + v.grad(u) = 0, where v is a given fluid velocity, and
 //               u0(x)=u(0,x) is a given initial condition.
 //
@@ -566,7 +573,8 @@ int main(int argc, char *argv[])
 
 // Implementation of class FE_Evolution
 FE_Evolution::FE_Evolution(HypreParMatrix &_M, HypreParMatrix &_K,
-                           const Vector &_b, ParBilinearForm &_pbf, Vector &M_rs)
+                           const Vector &_b, ParBilinearForm &_pbf,
+                           Vector &M_rs)
    : TimeDependentOperator(_M.Height()),
      M(_M), K(_K), b(_b), M_solver(M.GetComm()), z(_M.Height()),
      pbf(_pbf), M_rowsums(M_rs)
