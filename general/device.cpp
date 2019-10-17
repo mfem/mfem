e4fd305c25b7b12c28d74f00a72492b0eac33291
Yohann Dudouit <yohann.dudouit@gmail.com>
2019-10-17 15:21:28 -0700

    Default cpu backend to `/cpu/self` since it picks automatically the best cpu backend available.

--- general/device.cpp
+++ general/device.cpp
@@ -292,7 +292,7 @@ void Device::Setup(const int device)
    {
       if (!ceed_option)
       {
-         CeedDeviceSetup("/cpu/self/opt/blocked");
+         CeedDeviceSetup("/cpu/self");
       }
       else
       {
