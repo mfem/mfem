097519ca2dd7464bd260d62e7874ac92aedf9c68
Jakub Červený <cerveny1@llnl.gov>
2019-11-06 16:55:21 +0100

    Removed double Update().

--- mesh/pncmesh.cpp
+++ mesh/pncmesh.cpp
@@ -1313,9 +1313,9 @@ void ParNCMesh::Refine(const Array<Refinement> &refinements)
    if (NRanks == 1)
    {
       NCMesh::Refine(refinements);
-      Update();
       return;
    }
+
    for (int i = 0; i < refinements.Size(); i++)
    {
       const Refinement &ref = refinements[i];
