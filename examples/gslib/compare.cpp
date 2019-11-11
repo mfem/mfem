2d74c9e0f10c7b9afe8f8e71c8e27a31b9c4d2a0
Ketan Mittal <ketan.mittal@gmail.com>
2019-11-11 10:18:21 -0800

    update CHANGELOG and compare.cpp style to match miniapps

--- examples/gslib/compare.cpp
+++ examples/gslib/compare.cpp
@@ -1,4 +1,22 @@
-﻿//          Serial example of utilizing GSLib's FindPoints methods
+﻿// Copyright (c) 2010, Lawrence Livermore National Security, LLC. Produced at
+// the Lawrence Livermore National Laboratory. LLNL-CODE-443211. All Rights
+// reserved. See file COPYRIGHT for details.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability see http://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License (as published by the Free
+// Software Foundation) version 2.1 dated February 1999.
+//
+//          ------------------------------------------------------
+//          Serial example of utilizing GSLib's FindPoints methods
+//          ------------------------------------------------------
+// This example utilizes GSLib's high-order off-grid interpolation utility 
+// FindPoints to compare solution on two different meshes. FindPoints uses 
+// GSLib's highly optimized communication kernels to first find arbitrary 
+// number of points (given in physical-space) in a mesh in serial/parallel
+// and then interpolate a GridFunction/ParGridFunction at those points. 
 //
 // Compile with: make compare
 // Sample run  : ./compare
