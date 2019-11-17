24207731576052c619c2273a719c632215376eea
Tzanio <tzanio@llnl.gov>
2019-11-16 17:03:36 -0800

    make style

--- fem/gslib.hpp
+++ fem/gslib.hpp
@@ -83,7 +83,7 @@ public:
                               Note: the gslib reference frame is [-1,1].
        @param[in] field_in    Function values that will be interpolated on the
                               reference positions. Note: it is assumed that
-                              @a field_in is in H1 and in the same space as the 
+                              @a field_in is in H1 and in the same space as the
                               mesh that was given to Setup().
        @param[out] field_out  Interpolated values. */
    void Interpolate(Array<uint> &codes, Array<uint> &proc_ids,
