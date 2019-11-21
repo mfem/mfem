b2d3703b1dd70464b967e85da7d74db1a934b5a2
camierjs <camier1@llnl.gov>
2019-11-20 17:11:22 -0800

    Re-enable ctest cfg

--- tests/unit/miniapps/test_sedov.cpp
+++ tests/unit/miniapps/test_sedov.cpp
@@ -10,8 +10,9 @@
 // Software Foundation) version 2.1 dated February 1999.
 
 #ifdef _WIN32
-#define _USE_MATH_DEFINES
-#include <cmath>
+#define M_PI 3.14159265358979323846
+//#define _USE_MATH_DEFINES
+//#include <cmath>
 #endif
 
 #include "catch.hpp"
