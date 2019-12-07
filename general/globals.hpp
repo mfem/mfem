3cd6448a9af88e2a69ebebf323df614cb4dcda66
Veselin Dobrev <dobrev@llnl.gov>
2019-12-06 19:01:59 -0800

    Improve the messages printed by the method Mesh::GeneratePartitioning() so that only rank 0 prints the message when running in parallel.
    Fix inaccurate warnings (in DEBUG mode) about inverted elements which
    were generated when refining a periodic parallel mesh in 2D.
    
    Some formatting improvements.

--- general/globals.hpp
+++ general/globals.hpp
@@ -90,7 +90,7 @@ std::string MakeParFilename(const std::string &prefix, const int myid,
     Functions for getting and setting the MPI communicator used by the library
     as the "global" communicator.
 
-    Currently, the MFEM "global" communicator is used only by the function
+    One place where the MFEM "global" communicator is used by the function
     mfem_error(), invoked when an error is detected - the "global" communicator
     is used as a parameter to MPI_Abort() to terminate all "global" tasks. */
 ///@{
