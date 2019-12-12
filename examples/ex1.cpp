7e8d95c4e618c8abf61e160cacdf018e1ce10f1b
Will Pazner <will.e.p@gmail.com>
2019-12-12 13:22:56 -0800

    Whitespace

--- examples/ex1.cpp
+++ examples/ex1.cpp
@@ -197,7 +197,6 @@ int main(int argc, char *argv[])
    else // No preconditioning for now in partial assembly mode.
    {
       CG(*A, B, X, 1, 2000, 1e-12, 0.0);
-
    }
 
    // 12. Recover the solution as a finite element grid function.
