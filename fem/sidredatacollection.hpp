897a931d88e6d5140d6cc12c503bcd8b906adb9c
Tzanio <tzanio@llnl.gov>
2019-12-30 09:36:42 -0800

    Removed potentially confusing comment.

--- fem/sidredatacollection.hpp
+++ fem/sidredatacollection.hpp
@@ -29,7 +29,7 @@
 #endif
 #include <axom/sidre.hpp>
 #ifdef MFEM_HAVE_GCC_PRAGMA_DIAGNOSTIC
-# pragma GCC diagnostic pop // remove for older GCC versions, e.g. 5.4.0
+# pragma GCC diagnostic pop
 #endif
 
 namespace mfem
