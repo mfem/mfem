df467933c9b9df1baa60ba72723e3ef245be1001
Arturo Vargas <artv3.89@gmail.com>
2020-01-03 18:09:42 -0800

    added unit test

--- fem/bilininteg_convection.cpp
+++ fem/bilininteg_convection.cpp
@@ -468,7 +468,7 @@ static void PAConvectionApply(const int dim,
          case 0x33: PAConvectionApply2D<3,3>(NE, B, G, Bt, Gt, op, x, y); break;
          case 0x44: PAConvectionApply2D<4,4>(NE, B, G, Bt, Gt, op, x, y); break;
          case 0x55: PAConvectionApply2D<5,5>(NE, B, G, Bt, Gt, op, x, y); break;
-         default: mfem_error("convection kernel order not supported \n"); break;
+         default: PAConvectionApply2D(NE, B, G, Bt, Gt, op, x, y); break;
     }
     return;
   }
@@ -480,7 +480,7 @@ static void PAConvectionApply(const int dim,
         case 0x34: PAConvectionApply3D<3,4>(NE, B, G, Bt, G, op, x, y); break;
         case 0x45: PAConvectionApply3D<4,5>(NE, B, G, Bt, G, op, x, y); break;
         case 0x56: PAConvectionApply3D<5,6>(NE, B, G, Bt, G, op, x, y); break;
-         default: mfem_error("convection kernel order not supported \n"); break;
+         default: PAConvectionApply3D(NE, B, G, Bt, G, op, x, y); break;
     }
     return;
   }
