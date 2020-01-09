fdf8c41b7b9c011e5f03d8478f69cafac3988819
Adrien M. Bernede <bernede1@llnl.gov>
2020-01-09 14:54:43 -0800

    Checking for large commits

--- tests/scripts/history_checks.bash
+++ tests/scripts/history_checks.bash
@@ -1,36 +1,62 @@
-#! /bin/bash
+# Copyright (c) 2010, Lawrence Livermore National Security, LLC. Produced at the
+# Lawrence Livermore National Laboratory. LLNL-CODE-443211. All Rights reserved.
+# See file COPYRIGHT for details.
+#
+# This file is part of the MFEM library. For more information and source code
+# availability see http://mfem.googlecode.com.
+#
+# MFEM is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License (as published by the Free
+# Software Foundation) version 2.1 dated February 1999.
 
-function check_bin_in_commit() {
-
+# Check the number of changes introduced by the last commit
+# (sum of # of files changed, lines inserted and deleted).
+function check_commit_size()
+{
     binary_files=""
 
-    for file in $(git diff --name-only HEAD^)
-    do
-        if [[ -e $file ]] && (! file --mime $file | grep -q text)
-        then
-            binary_files="${binary_files}   ${file}\n"
-        fi
-    done
+    # Compute the number of total changes (files+insertions+deletions)
+    # in the commit, e.g.
+    # "3 files changed, 66 insertions(+), 45 deletions(-)" -> 111
+    changes=$(git diff --shortstat HEAD^ \
+              | sed -e 's/[^0-9,]//g' -e 's/,/+/g' | bc)
 
-    if [[ -n ${binary_files} ]]
+    if (( $changes > 50000 ))
     then
         msg "$(git log -1 --format=%H)"
-        msg "ATTENTION: binary file(s) added or modified"
-        echo "${binary_files}"
-        #[[ ! $1 == "--safe" ]] && exit 1
+        msg "ATTENTION: This commit is unusually large! (${changes} changes)"
         return 1
     fi
 
     return 0
 }
 
-function check_bin_in_branch() {
+# Check if files modified in the last commit are binaries.
+function check_bin_in_commit()
+{
+    binary_files=""
 
+    if [[ -n ${binary_files} ]]
+    then
+        msg "$(git log -1 --format=%H)"
+        msg "ATTENTION: binary file(s) added or modified"
+        echo "${binary_files}"
+        return 1
+    fi
+
+    return 0
+}
+
+# Check all the commits in the current branch, starting with the common ancestor
+# with master, for large and binary files
+function check_branch()
+{
     current_branch="$(git symbolic-ref --short HEAD)"
     reference_branch="master"
     common_ancestor="$(git merge-base ${current_branch} ${reference_branch})"
 
     binary_found=0
+    large_commit=0
 
     while read -r rev; do
         git checkout -q "$rev"
@@ -39,17 +65,23 @@ function check_bin_in_branch() {
         then
             binary_found=1
         fi
+        if ! check_commit_size
+        then
+            large_commit=1
+        fi
     done < <(git rev-list --reverse ${common_ancestor}..${current_branch})
 
     git checkout -q ${current_branch}
 
-    return $binary_found
+    return $binary_found || $large_commit
 }
 
-function msg() {
+function msg()
+{
     echo "history_checks: $*"
 }
 
-function err() {
+function err()
+{
     msg $* 1>&2
 }
