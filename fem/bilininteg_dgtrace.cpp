fc5c8ef404ad1ae44997529728aee4aafca2c26c
Yohann Dudouit <yohann.dudouit@gmail.com>
2020-01-14 11:07:50 -0800

    Weird potential fix for AppVeyor

--- fem/bilininteg_dgtrace.cpp
+++ fem/bilininteg_dgtrace.cpp
@@ -265,6 +265,7 @@ void PADGTraceApply2D(const int NF,
 
    MFEM_FORALL(f, NF,
    {
+      const int VDIM = 1;
       const int D1D = T_D1D ? T_D1D : d1d;
       const int Q1D = T_Q1D ? T_Q1D : q1d;
       // the following variables are evaluated at compile time
