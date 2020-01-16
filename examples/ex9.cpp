51b0d78ab2efdef74aee9b5d5484ebfdf45b1c57
Vladimir Tomov <tomov2@llnl.gov>
2020-01-15 17:07:37 -0800

    Valgrind catch.

--- examples/ex9.cpp
+++ examples/ex9.cpp
@@ -246,8 +246,8 @@ int main(int argc, char *argv[])
       int skip_zeros = 0;
       k.Assemble(skip_zeros);
       k.Finalize(skip_zeros);
-      b.Assemble();
    }
+   b.Assemble();
 
    // 8. Define the initial conditions, save the corresponding grid function to
    //    a file and (optionally) save data in the VisIt format and initialize
