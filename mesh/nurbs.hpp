0a6e34c9878337bf3c4d344a3c053086ad7561ba
Ido Akkerman <I.Akkerman@tudelft.nl>
2020-01-27 21:35:26 +0100

    make style

--- mesh/nurbs.hpp
+++ mesh/nurbs.hpp
@@ -64,7 +64,7 @@ public:
    void CalcShape  (Vector &shape, int i, double xi) const;
    void CalcDShape (Vector &grad,  int i, double xi) const;
    void CalcDnShape(Vector &gradn, int n, int i, double xi) const;
-   void CalcD2Shape(Vector &grad2, int i, double xi) const 
+   void CalcD2Shape(Vector &grad2, int i, double xi) const
    { CalcDnShape(grad2, 2, i, xi); }
 
    void Difference(const KnotVector &kv, Vector &diff) const;
