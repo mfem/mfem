759ab90cdc95200c43df4d38f911f40afc3ec5bd
camierjs <camier1@llnl.gov>
2020-01-31 11:19:00 -0800

    Set static intrules to use MemoryType::HOST Remove mfem::out-puts in unit tests

--- tests/unit/general/test_mem.cpp
+++ tests/unit/general/test_mem.cpp
@@ -90,13 +90,13 @@ TEST_CASE("MemoryManager", "[MemoryManager]")
       for (int n = 1; n < 2*pagesize; n+=7)
       {
          Aliases(n);
-         mm.PrintPtrs();
-         mm.PrintAliases();
+         //mm.PrintPtrs();
+         //mm.PrintAliases();
       }
       MmuCatch();
       ScanMemoryTypes();
-      mm.PrintPtrs();
-      mm.PrintAliases();
+      //mm.PrintPtrs();
+      //mm.PrintAliases();
    }
 }
 
