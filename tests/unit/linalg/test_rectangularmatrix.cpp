f3444fb2e2b18e1816758de6965ee903aaf38562
Julian Andrej <andrej1@llnl.gov>
2020-02-01 12:01:36 -0800

    enabled parallel unit tests and added parallel version of test for FormRectangularSystemMatrix

--- tests/unit/linalg/test_rectangularmatrix.cpp
+++ tests/unit/linalg/test_rectangularmatrix.cpp
@@ -36,19 +36,19 @@ void gradf1(const Vector &x, Vector &u)
 
 TEST_CASE("FormRectangular", "[FormRectangularSystemMatrix]")
 {
-   Mesh mesh(10, 10, Element::QUADRILATERAL, 0, 1.0, 1.0);
-   int dim = mesh.Dimension();
-   int order = 2;
-
-   int nattr = mesh.bdr_attributes.Max();
-   Array<int> ess_trial_tdof_list, ess_test_tdof_list;
-
-   Array<int> ess_bdr(nattr);
-   ess_bdr = 0;
-   ess_bdr[0] = 1;
-
    SECTION("MixedBilinearForm::FormRectangularSystemMatrix")
    {
+      Mesh mesh(10, 10, Element::QUADRILATERAL, 0, 1.0, 1.0);
+      int dim = mesh.Dimension();
+      int order = 2;
+
+      int nattr = mesh.bdr_attributes.Max();
+      Array<int> ess_trial_tdof_list, ess_test_tdof_list;
+
+      Array<int> ess_bdr(nattr);
+      ess_bdr = 0;
+      ess_bdr[0] = 1;
+
       // Scalar
       H1_FECollection fec1(order, dim);
       FiniteElementSpace fes1(&mesh, &fec1);
@@ -91,4 +91,72 @@ TEST_CASE("FormRectangular", "[FormRectangularSystemMatrix]")
       REQUIRE(field2.Norml2() == Approx(0.0));
    }
 }
+
+#ifdef MFEM_USE_MPI
+
+TEST_CASE("ParallelFormRectangular", "[Parallel], [FormRectangularSystemMatrix]")
+{
+   SECTION("ParMixedBilinearForm::FormRectangularSystemMatrix")
+   {
+      Mesh mesh(10, 10, Element::QUADRILATERAL, 0, 1.0, 1.0);
+      int dim = mesh.Dimension();
+      int order = 2;
+
+      int nattr = mesh.bdr_attributes.Max();
+      Array<int> ess_trial_tdof_list, ess_test_tdof_list;
+
+      Array<int> ess_bdr(nattr);
+      ess_bdr = 0;
+      ess_bdr[0] = 1;
+
+      ParMesh pmesh(MPI_COMM_WORLD, mesh);
+
+      // Scalar
+      H1_FECollection fec1(order, dim);
+      ParFiniteElementSpace fes1(&pmesh, &fec1);
+
+      // Vector valued
+      H1_FECollection fec2(order, dim);
+      ParFiniteElementSpace fes2(&pmesh, &fec2, dim);
+
+      fes1.GetEssentialTrueDofs(ess_bdr, ess_trial_tdof_list);
+      fes2.GetEssentialTrueDofs(ess_bdr, ess_test_tdof_list);
+
+      ParGridFunction field(&fes1), field2(&fes2);
+
+      ParMixedBilinearForm gform(&fes1, &fes2);
+      gform.AddDomainIntegrator(new GradientIntegrator);
+      gform.Assemble();
+
+      // Project u = f1
+      FunctionCoefficient fcoeff1(f1);
+      field.ProjectCoefficient(fcoeff1);
+
+      VectorFunctionCoefficient fcoeff2(dim, gradf1);
+      ParLinearForm lf(&fes2);
+      lf.AddDomainIntegrator(new VectorDomainLFIntegrator(fcoeff2));
+      lf.Assemble();
+
+      OperatorHandle G;
+      Vector X, B;
+      gform.FormRectangularLinearSystem(ess_trial_tdof_list,
+                                        ess_test_tdof_list,
+                                        field,
+                                        lf,
+                                        G,
+                                        X,
+                                        B);
+
+      Vector *field_tdof = field.ParallelProject();
+      Vector *field2_tdof = field2.ParallelProject();
+
+      G->Mult(*field_tdof, *field2_tdof);
+
+      subtract(B, *field2_tdof, *field2_tdof);
+      REQUIRE(field2_tdof->Norml2() == Approx(0.0));
+   }
+}
+
+#endif
+
 } // namespace mfem
