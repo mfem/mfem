52f5069859bb4ecab187cbb8f103585cfa78c3fc
Julian Andrej <andrej1@llnl.gov>
2020-02-05 17:23:16 -0800

    unit test changes and see if windows build works

--- tests/unit/unit_test_main.cpp
+++ tests/unit/unit_test_main.cpp
@@ -9,20 +9,17 @@
 // terms of the GNU Lesser General Public License (as published by the Free
 // Software Foundation) version 2.1 dated February 1999.
 
-// #define CATCH_CONFIG_RUNNER
-// #include "catch.hpp"
-// #include "mfem.hpp"
-
-// int main(int argc, char *argv[])
-// {
-// #ifdef MFEM_USE_MPI
-//    mfem::MPI_Session mpi;
-// #endif
-
-//    // There must be exactly one instance
-//    int result = Catch::Session().run(argc, argv);
-//    return result;
-// }
-
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
+#include "mfem.hpp"
 #include "catch.hpp"
+
+int main(int argc, char *argv[])
+{
+#ifdef MFEM_USE_MPI
+   mfem::MPI_Session mpi;
+#endif
+
+   // There must be exactly one instance
+   int result = Catch::Session().run(argc, argv);
+   return result;
+}
