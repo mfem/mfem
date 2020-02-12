4e4cc0eef3914612999edac58bd1e1a7752d71c3
Stowell, Mark L <stowell1@llnl.gov>
2020-02-12 15:36:24 -0800

    Adding tests of the DoFTransformation classes

---
+++ tests/unit/fem/test_doftrans.cpp
@@ -0,0 +1,122 @@
+// Copyright (c) 2010, Lawrence Livermore National Security, LLC. Produced at
+// the Lawrence Livermore National Laboratory. LLNL-CODE-443211. All Rights
+// reserved. See file COPYRIGHT for details.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability see http://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License (as published by the Free
+// Software Foundation) version 2.1 dated February 1999.
+
+#include "mfem.hpp"
+#include "catch.hpp"
+
+using namespace mfem;
+
+namespace doftrans
+{
+
+TEST_CASE("DoF Transformations"
+          "[ND_TetDofTransformation]")
+{
+   int p = 4;
+   int seed = 123;
+
+   double tol = 1e-13;
+
+   SECTION("Nedelec Tetrahedral Transformations")
+   {
+      ND_TetDofTransformation T(p);
+
+      Array<int> ori(4);
+      ori[0] = 1;
+      ori[1] = 3;
+      ori[2] = 5;
+      ori[3] = 1;
+
+      T.SetFaceOrientations(ori);
+
+      Vector u(T.Width());
+      Vector v(T.Width());
+      Vector f(T.Width());
+      Vector ut(T.Height());
+      Vector vt(T.Height());
+      Vector ft(T.Height());
+
+      u.Randomize(seed);
+      v.Randomize(seed+1);
+      f.Randomize(seed+2);
+
+      SECTION("Inverse DoF transformation")
+      {
+         Vector w(T.Width());
+
+         T.TransformPrimal(u, ut);
+         T.InvTransformPrimal(ut, w);
+
+         w -= u;
+
+         REQUIRE(w.Norml2() < tol * u.Norml2());
+      }
+
+      SECTION("Inner product with linear form f(u)")
+      {
+         T.TransformPrimal(u, ut);
+         T.TransformDual(f, ft);
+
+         REQUIRE(fabs(f * u - ft * ut) < tol);
+      }
+
+      DenseMatrix A(T.Width());
+      {
+         Vector Ac;
+         for (int i=0; i<A.Width(); i++)
+         {
+            A.GetColumnReference(i, Ac);
+            Ac.Randomize(seed+i);
+         }
+      }
+
+      SECTION("Inner product of two primal vectors")
+      {
+         DenseMatrix tA(T.Height(), T.Width());
+         DenseMatrix At(T.Width(), T.Height());
+         DenseMatrix tAt(T.Height());
+
+         T.TransformPrimal(u, ut);
+         T.TransformPrimal(v, vt);
+
+         T.TransformDualRows(A, At);
+         T.TransformDualCols(A, tA);
+         T.TransformDual(A, tAt);
+
+         double uAv = A.InnerProduct(v, u);
+
+         REQUIRE(fabs(uAv -  At.InnerProduct(vt, u )) < tol * fabs(uAv));
+         REQUIRE(fabs(uAv -  tA.InnerProduct(v , ut)) < tol * fabs(uAv));
+         REQUIRE(fabs(uAv - tAt.InnerProduct(vt, ut)) < tol * fabs(uAv));
+      }
+      SECTION("Inner product of a primal vector and a dual vector")
+      {
+         DenseMatrix tA(T.Height(), T.Width());
+         DenseMatrix At(T.Width(), T.Height());
+         DenseMatrix tAt(T.Height());
+
+         T.TransformDual(f, ft);
+         T.TransformPrimal(v, vt);
+
+         T.TransformDualRows(A, At);
+         T.TransformPrimalCols(A, tA);
+         T.TransformPrimalCols(At, tAt);
+
+         double fAv = A.InnerProduct(v, f);
+
+         REQUIRE(fabs(fAv -  At.InnerProduct(vt, f )) < tol * fabs(fAv));
+         REQUIRE(fabs(fAv -  tA.InnerProduct(v , ft)) < tol * fabs(fAv));
+         REQUIRE(fabs(fAv - tAt.InnerProduct(vt, ft)) < tol * fabs(fAv));
+      }
+   }
+}
+
+} // namespace doftrans
