7d75b885f88a585b9a7bd5975cef2c7105f8f077
ben-s-southworth <ben.s.southworth@gmail.com>
2020-02-13 21:11:22 -0800

    Added bool to copy row starts/col starts when using ParMult()

--- linalg/hypre.hpp
+++ linalg/hypre.hpp
@@ -556,7 +556,10 @@ HypreParMatrix *Add(double alpha, const HypreParMatrix &A,
                     double beta,  const HypreParMatrix &B);
 
 /// Returns the matrix A * B
-HypreParMatrix * ParMult(const HypreParMatrix *A, const HypreParMatrix *B);
+/** Returned matrix does not own row or column starts unless the bool
+    own_matrix is set to true. */
+HypreParMatrix * ParMult(const HypreParMatrix *A, const HypreParMatrix *B,
+                         bool own_matrix=false);
 /// Returns the matrix A + B
 /** It is assumed that both matrices use the same row and column partitions and
     the same col_map_offd arrays. */
