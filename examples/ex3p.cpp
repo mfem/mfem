8514657bf5642bf68bde040ea0d3dda7338b17cc
Stowell, Mark L <stowell1@llnl.gov>
2020-02-15 14:49:12 -0800

    Reverting to standard refinement strategies

--- examples/ex3p.cpp
+++ examples/ex3p.cpp
@@ -57,8 +57,6 @@ int main(int argc, char *argv[])
 
    // 2. Parse command-line options.
    const char *mesh_file = "../data/beam-tet.mesh";
-   int ser_ref_levels = -1;
-   int par_ref_levels = 2;
    int order = 1;
    bool static_cond = false;
    bool visualization = 1;
@@ -68,10 +66,6 @@ int main(int argc, char *argv[])
                   "Mesh file to use.");
    args.AddOption(&order, "-o", "--order",
                   "Finite element order (polynomial degree).");
-   args.AddOption(&ser_ref_levels, "-rs", "--refine-serial",
-                  "Number of times to refine the mesh uniformly in serial.");
-   args.AddOption(&par_ref_levels, "-rp", "--refine-parallel",
-                  "Number of times to refine the mesh uniformly in parallel.");
    args.AddOption(&freq, "-f", "--frequency", "Set the frequency for the exact"
                   " solution.");
    args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
@@ -108,8 +102,7 @@ int main(int argc, char *argv[])
    //    'ref_levels' to be the largest number that gives a final mesh with no
    //    more than 1,000 elements.
    {
-      int ref_levels = (ser_ref_levels >= 0) ? ser_ref_levels :
-                       (int)floor(log(1000./mesh->GetNE())/log(2.)/dim);
+      int ref_levels = (int)floor(log(1000./mesh->GetNE())/log(2.)/dim);
       for (int l = 0; l < ref_levels; l++)
       {
          mesh->UniformRefinement();
@@ -124,6 +117,7 @@ int main(int argc, char *argv[])
    ParMesh *pmesh = new ParMesh(MPI_COMM_WORLD, *mesh);
    delete mesh;
    {
+      int par_ref_levels = 2;
       for (int l = 0; l < par_ref_levels; l++)
       {
          pmesh->UniformRefinement();
