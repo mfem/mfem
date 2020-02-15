19961d1493bc3e28585263988dd2480c3a6d3ec9
Stowell, Mark L <stowell1@llnl.gov>
2020-02-15 14:53:37 -0800

    Removing similar confusing comment

--- examples/ex4p.cpp
+++ examples/ex4p.cpp
@@ -116,10 +116,7 @@ int main(int argc, char *argv[])
 
    // 5. Define a parallel mesh by a partitioning of the serial mesh. Refine
    //    this mesh further in parallel to increase the resolution. Once the
-   //    parallel mesh is defined, the serial mesh can be deleted. Face
-   //    orientation information must be exchanged with neighboring processors
-   //    before we can define high-order Nedelec spaces (this is needed in the
-   //    ADS solver below).
+   //    parallel mesh is defined, the serial mesh can be deleted.
    ParMesh *pmesh = new ParMesh(MPI_COMM_WORLD, *mesh);
    delete mesh;
    {
