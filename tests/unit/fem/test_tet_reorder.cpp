0f6f1ef1ebbcb0053b9589a6d2b23950f58de2af
Stowell, Mark L <stowell1@llnl.gov>
2020-02-21 14:51:29 -0800

    Adding unit test for reordering of high order tetrahedra

--- tests/unit/fem/test_tet_reorder.cpp
+++ tests/unit/fem/test_tet_reorder.cpp
@@ -19,38 +19,49 @@ namespace tet_reorder
 
 TEST_CASE("Tetrahedron Reordering")
 {
+   typedef Geometry::Constants<Geometry::TETRAHEDRON> g_const;
 
-  Mesh mesh(3, 4, 1);
+   int p = 7;
+   double tol = 1e-6;
 
-  double c[3];
-  c[0] = 0.0; c[1] = 0.0; c[2] = 0.0;
-  mesh.AddVertex(c);
-  c[0] = 1.0; c[1] = 0.0; c[2] = 0.0;
-  mesh.AddVertex(c);
-  c[0] = 0.0; c[1] = 2.0; c[2] = 0.0;
-  mesh.AddVertex(c);
-  c[0] = 0.0; c[1] = 0.0; c[2] = 3.0;
-  mesh.AddVertex(c);
+   SECTION("Geometry order " + std::to_string(p))
+   {
+      for (int nd=0; nd<=1; nd++)
+         SECTION("Discontinuous flag set to " + std::to_string(nd))
+      {
+         for (int no=0; no<=1; no++)
+            SECTION("VDim ordering type set to " + std::to_string(no))
+         {
+            for (int o=0; o<g_const::NumOrient; o++)
+               SECTION("Initial orientation set to " + std::to_string(o))
+            {
+               Mesh mesh(3, 4, 1);
 
-  int v[4];
-  v[0] = 0; v[1] = 1; v[2] = 2; v[3] = 3;
-  mesh.AddTet(v);
-  mesh.FinalizeMesh(0, false);
+               double c[3];
+               c[0] = 0.0; c[1] = 0.0; c[2] = 3.0;
+               mesh.AddVertex(c);
+               c[0] = 0.0; c[1] = 2.0; c[2] = 0.0;
+               mesh.AddVertex(c);
+               c[0] = 1.0; c[1] = 0.0; c[2] = 0.0;
+               mesh.AddVertex(c);
+               c[0] = 0.0; c[1] = 0.0; c[2] = 0.0;
+               mesh.AddVertex(c);
 
-  mesh.SetCurvature(5);
-  std::cout << "Element Volume: " << mesh.GetElementVolume(0) << std::endl;
+               const int * vo = g_const::Orient[o];
+               mesh.AddTet(vo);
+               mesh.FinalizeMesh(0, false);
 
-  mesh.Finalize(true, true);
-  std::cout << "Element Volume: " << mesh.GetElementVolume(0) << std::endl;
-  mesh.Finalize(true, true);
-  std::cout << "Element Volume: " << mesh.GetElementVolume(0) << std::endl;
-  //mesh.SetCurvature(5);
-  //std::cout << "Element Volume: " << mesh.GetElementVolume(0) << std::endl;
-  /*
-  int * vi = mesh.GetElement(0)->GetVertices();
-  std::swap(vi[2],vi[3]);
-  std::cout << "Element Volume: " << mesh.GetElementVolume(0) << std::endl;
-  */
+               mesh.SetCurvature(p, nd, 3, no);
+               double vol0 = mesh.GetElementVolume(0);
+               REQUIRE(fabs(vol0 - 1.0 + 2.0 * (o % 2)) < tol);
+
+               mesh.Finalize(true, true);
+               double vol1 = mesh.GetElementVolume(0);
+               REQUIRE(fabs(vol1 - 1.0) < tol);
+            }
+         }
+      }
+   }
 }
-  
+
 } // namespace tet_reorder
