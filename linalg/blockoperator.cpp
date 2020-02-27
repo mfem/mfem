ff6f3fc6be35a7369839eb2dd18a7d0eb994abc6
Ben Southworth <ben.s.southworth@gmail.com>
2020-02-27 15:03:08 -0800

    Debugging

--- linalg/blockoperator.cpp
+++ linalg/blockoperator.cpp
@@ -495,11 +495,11 @@ void BlockLDUPreconditioner::Mult(const Vector & x, Vector & y) const
 
    op(1,1) -> Mult(tmp, yblock.GetBlock(1));
    
-   // tmp.SetSize(offsets[1] - offsets[0]);
-   // tmp2.SetSize(offsets[1] - offsets[0]);
-   // op(0,1) -> Mult(yblock.GetBlock(1), tmp);
-   // op(0,0) -> Mult(tmp, tmp2);
-   // yblock.GetBlock(0) -= tmp2;
+   tmp.SetSize(offsets[1] - offsets[0]);
+   tmp2.SetSize(offsets[1] - offsets[0]);
+   op(0,1) -> Mult(yblock.GetBlock(1), tmp);
+   op(0,0) -> Mult(tmp, tmp2);
+   yblock.GetBlock(0) -= tmp2;
 }
 
 BlockLDUPreconditioner::~BlockLDUPreconditioner()
