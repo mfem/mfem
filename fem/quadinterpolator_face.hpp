8632451697c59aa11861cb4792a3e003c7c6a86e
camierjs <camier1@llnl.gov>
2020-03-07 14:06:39 -0800

    Move 'Face' classes from fem/quadinterpolator to fem/quadinterpolator_face

--- fem/quadinterpolator_face.hpp
+++ fem/quadinterpolator_face.hpp
@@ -9,33 +9,36 @@
 // terms of the BSD-3 license.  We welcome feedback and contributions, see file
 // CONTRIBUTING.md for details.
 
-#ifndef MFEM_QUADINTERP
-#define MFEM_QUADINTERP
+#ifndef MFEM_QUADINTERPOLATOR_FACE
+#define MFEM_QUADINTERPOLATOR_FACE
 
 #include "fespace.hpp"
 
 namespace mfem
 {
 
-/** @brief A class that performs interpolation from an E-vector to quadrature
-    point values and/or derivatives (Q-vectors). */
-/** An E-vector represents the element-wise discontinuous version of the FE
-    space and can be obtained, for example, from a GridFunction using the
-    Operator returned by FiniteElementSpace::GetElementRestriction().
-
-    The target quadrature points in the elements can be described either by an
-    IntegrationRule (all mesh elements must be of the same type in this case) or
-    by a QuadratureSpace. */
-class QuadratureInterpolator
+/** @brief A class that performs interpolation from a face E-vector to
+    quadrature point values and/or derivatives (Q-vectors) on the faces. */
+/** A face E-vector represents the face-wise discontinuous version of the trace
+    FE space and can be obtained, for example, from a GridFunction using the
+    Operator returned by FiniteElementSpace::GetFaceRestriction(). */
+class FaceQuadratureInterpolator
 {
+private:
+   FaceType type;
+   int nf;
+   Array<bool> signs;
 protected:
    friend class FiniteElementSpace; // Needs access to qspace and IntRule
 
    const FiniteElementSpace *fespace;  ///< Not owned
-   const QuadratureSpace *qspace;      ///< Not owned
    const IntegrationRule *IntRule;     ///< Not owned
 
    mutable bool use_tensor_products;
+
+   static const int MAX_NQ1D = 10;
+   static const int MAX_ND1D = 10;
+   static const int MAX_VDIM1D = 1;
 
    static const int MAX_NQ2D = 100;
    static const int MAX_ND2D = 100;
@@ -46,21 +49,19 @@ protected:
    static const int MAX_VDIM3D = 3;
 
 public:
-   enum EvalFlags
+   enum FaceEvalFlags
    {
       VALUES       = 1 << 0,  ///< Evaluate the values at quadrature points
       DERIVATIVES  = 1 << 1,  ///< Evaluate the derivatives at quadrature points
       /** @brief Assuming the derivative at quadrature points form a matrix,
           this flag can be used to compute and store their determinants. This
           flag can only be used in Mult(). */
-      DETERMINANTS = 1 << 2
+      DETERMINANTS = 1 << 2,
+      NORMALS      = 1 << 3
    };
 
-   QuadratureInterpolator(const FiniteElementSpace &fes,
-                          const IntegrationRule &ir);
-
-   QuadratureInterpolator(const FiniteElementSpace &fes,
-                          const QuadratureSpace &qs);
+   FaceQuadratureInterpolator(const FiniteElementSpace &fes,
+                              const IntegrationRule &ir, FaceType type);
 
    /** @brief Disable the use of tensor product evaluations, for tensor-product
        elements, e.g. quads and hexes. */
@@ -70,7 +71,7 @@ public:
    { use_tensor_products = !disable; }
 
    /// Interpolate the E-vector @a e_vec to quadrature points.
-   /** The @a eval_flags are a bitwise mask of constants from the EvalFlags
+   /** The @a eval_flags are a bitwise mask of constants from the FaceEvalFlags
        enumeration. When the VALUES flag is set, the values at quadrature points
        are computed and stored in the Vector @a q_val. Similarly, when the flag
        DERIVATIVES is set, the derivatives are computed and stored in @a q_der.
@@ -79,49 +80,37 @@ public:
        FiniteElementSpace is a vector space) and their determinants are computed
        and stored in @a q_det. */
    void Mult(const Vector &e_vec, unsigned eval_flags,
-             Vector &q_val, Vector &q_der, Vector &q_det) const;
-
-   /// Interpolate the values of the E-vector @a e_vec at quadrature points.
-   /// This computation uses tensor product structure.
-   void Values(const Vector &e_vec, Vector &q_val) const;
-
-   /// Interpolate the derivatives of the E-vector @a e_vec at quadrature
-   /// points. This computation uses tensor product structure.
-   void Derivatives(const Vector &e_vec, Vector &q_der) const;
-
-   /// Interpolate the derivatives in physical space of the E-vector @a e_vec at
-   /// quadrature points. This computation uses tensor product structure.
-   void PhysDerivatives(const Vector &e_vec, Vector &q_der) const;
-
-   /// Perform the transpose operation of Mult(). (TODO)
-   void MultTranspose(unsigned eval_flags, const Vector &q_val,
-                      const Vector &q_der, Vector &e_vec) const;
+             Vector &q_val, Vector &q_der, Vector &q_det, Vector &q_nor) const;
 
    // Compute kernels follow (cannot be private or protected with nvcc)
 
    /// Template compute kernel for 2D.
    template<const int T_VDIM = 0, const int T_ND = 0, const int T_NQ = 0>
-   static void Eval2D(const int NE,
+   static void Eval2D(const int NF,
                       const int vdim,
                       const DofToQuad &maps,
+                      const Array<bool> &signs,
                       const Vector &e_vec,
                       Vector &q_val,
                       Vector &q_der,
                       Vector &q_det,
+                      Vector &q_nor,
                       const int eval_flags);
 
    /// Template compute kernel for 3D.
    template<const int T_VDIM = 0, const int T_ND = 0, const int T_NQ = 0>
-   static void Eval3D(const int NE,
+   static void Eval3D(const int NF,
                       const int vdim,
                       const DofToQuad &maps,
+                      const Array<bool> &signs,
                       const Vector &e_vec,
                       Vector &q_val,
                       Vector &q_der,
                       Vector &q_det,
+                      Vector &q_nor,
                       const int eval_flags);
 };
 
-}
+} // mfem namespace
 
-#endif
+#endif // MFEM_QUADINTERPOLATOR_FACE
