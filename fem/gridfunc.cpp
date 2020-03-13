c81a8dcaa71cbc07c6569e38f035063f7e509113
Stowell, Mark L <stowell1@llnl.gov>
2020-03-12 21:20:44 -0700

    Fixing implementation that was overwritten by merge with master

--- fem/gridfunc.cpp
+++ fem/gridfunc.cpp
@@ -831,16 +831,51 @@ void GridFunction::GetVectorValues(ElementTransformation &T,
       T.Transform(ir, *tr);
    }
 
-   int vdim = fes->GetVDim();
+   const FiniteElement *FElem = fes->GetFE(T.ElementNo);
+   int dof = FElem->GetDof();
+
+   Array<int> vdofs;
+   fes->GetElementVDofs(T.ElementNo, vdofs);
+   
+   Vector loc_data;
+   GetSubVector(vdofs, loc_data);
    int nip = ir.GetNPoints();
-   vals.SetSize(vdim, nip);
-   Vector val;
-   for (int j = 0; j < nip; j++)
+
+   if (FElem->GetRangeType() == FiniteElement::SCALAR)
    {
-      const IntegrationPoint &ip = ir.IntPoint(j);
-      T.SetIntPoint(&ip);
-      GetVectorValue(T, ip, val);
-      vals.SetCol(j, val);
+      MFEM_ASSERT(FElem->GetMapType() == FiniteElement::VALUE,
+                  "invalid FE map type");
+      Vector shape(dof);
+      int vdim = fes->GetVDim();
+      vals.SetSize(vdim, nip);
+      for (int j = 0; j < nip; j++)
+      {
+         const IntegrationPoint &ip = ir.IntPoint(j);
+         FElem->CalcShape(ip, shape);
+
+         for (int k = 0; k < vdim; k++)
+         {
+            vals(k,j) = shape * ((const double *)loc_data + dof * k);
+         }
+      }
+   }
+   else
+   {
+      int spaceDim = fes->GetMesh()->SpaceDimension();
+      DenseMatrix vshape(dof, spaceDim);
+
+      vals.SetSize(spaceDim, nip);
+      Vector val_j;
+
+      for (int j = 0; j < nip; j++)
+      {
+         const IntegrationPoint &ip = ir.IntPoint(j);
+         T.SetIntPoint(&ip);
+         FElem->CalcVShape(T, vshape);
+
+         vals.GetColumnReference(j, val_j);
+         vshape.MultTranspose(loc_data, val_j);
+      }
    }
 }
 
