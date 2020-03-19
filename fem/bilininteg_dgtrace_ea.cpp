720ed921be50e4096a0433090c4f4fb2fb5a4527
Yohann Dudouit <yohann.dudouit@gmail.com>
2020-03-18 18:44:51 -0700

    Integrate change for boundary vs interior faces.

--- fem/bilininteg_dgtrace_ea.cpp
+++ fem/bilininteg_dgtrace_ea.cpp
@@ -21,21 +21,30 @@ static void EADGTraceAssemble1D(const int NF,
                                 const Array<double> &basis,
                                 const Vector &padata,
                                 Vector &eadata_int,
-                                Vector &eadata_ext)
+                                Vector &eadata_ext,
+                                FaceType type)
 {
+   const bool bdr = type==FaceType::Boundary;
    auto D = Reshape(padata.Read(), 2, 2, NF);
    auto A_int = Reshape(eadata_int.Write(), 2, NF);
    auto A_ext = Reshape(eadata_ext.Write(), 2, NF);
    MFEM_FORALL(f, NF,
    {
-      double val_int0  = D(0, 0, f);
-      double val_int1  = D(1, 1, f);
-      double val_ext01 = D(1, 0, f);
-      double val_ext10 = D(0, 1, f);
-      A_int(0, f) = val_int0;
-      A_int(1, f) = val_int1;
-      A_ext(0, f) = val_ext01;
-      A_ext(1, f) = val_ext10;
+      double val_int0, val_int1, val_ext01, val_ext10;
+      val_int0  = D(0, 0, f);
+      if (bdr)
+      {
+         val_ext01 = D(1, 0, f);
+         val_ext10 = D(0, 1, f);
+      }
+      val_int1  = D(1, 1, f);
+         A_int(0, f) = val_int0;
+         A_int(1, f) = val_int1;
+      if (bdr)
+      {
+         A_ext(0, f) = val_ext01;
+         A_ext(1, f) = val_ext10;
+      }
    });
 }
 
@@ -45,9 +54,11 @@ static void EADGTraceAssemble2D(const int NF,
                                 const Vector &padata,
                                 Vector &eadata_int,
                                 Vector &eadata_ext,
+                                FaceType type,
                                 const int d1d = 0,
                                 const int q1d = 0)
 {
+   const bool bdr = type==FaceType::Boundary;
    const int D1D = T_D1D ? T_D1D : d1d;
    const int Q1D = T_Q1D ? T_Q1D : q1d;
    MFEM_VERIFY(D1D <= MAX_D1D, "");
@@ -79,14 +90,20 @@ static void EADGTraceAssemble2D(const int NF,
             for (int k1 = 0; k1 < Q1D; ++k1)
             {
                val_int0  += r_Bi[k1] * r_Bj[k1] * D(k1, 0, 0, f);
+               if (bdr)
+               {
+                  val_ext01 += r_Bi[k1] * r_Bj[k1] * D(k1, 1, 0, f);
+                  val_ext10 += r_Bi[k1] * r_Bj[k1] * D(k1, 0, 1, f);
+               }
                val_int1  += r_Bi[k1] * r_Bj[k1] * D(k1, 1, 1, f);
-               val_ext01 += r_Bi[k1] * r_Bj[k1] * D(k1, 1, 0, f);
-               val_ext10 += r_Bi[k1] * r_Bj[k1] * D(k1, 0, 1, f);
             }
             A_int(i1, j1, 0, f) = val_int0;
             A_int(i1, j1, 1, f) = val_int1;
-            A_ext(i1, j1, 0, f) = val_ext01;
-            A_ext(i1, j1, 1, f) = val_ext10;
+            if (bdr)
+            {
+               A_ext(i1, j1, 0, f) = val_ext01;
+               A_ext(i1, j1, 1, f) = val_ext10;
+            }
          }
       }
    });
@@ -98,9 +115,11 @@ static void EADGTraceAssemble3D(const int NF,
                                 const Vector &padata,
                                 Vector &eadata_int,
                                 Vector &eadata_ext,
+                                FaceType type,
                                 const int d1d = 0,
                                 const int q1d = 0)
 {
+   const bool bdr = type==FaceType::Boundary;
    const int D1D = T_D1D ? T_D1D : d1d;
    const int Q1D = T_Q1D ? T_Q1D : q1d;
    MFEM_VERIFY(D1D <= MAX_D1D, "");
@@ -160,18 +179,24 @@ static void EADGTraceAssemble3D(const int NF,
                         val_int1 += r_B[k1][i1] * r_B[k1][j1]
                                   * r_B[k2][i2] * r_B[k2][j2]
                                   * s_D[k1][k2][1][1];
-                        val_ext01+= r_B[k1][i1] * r_B[k1][j1]
-                                  * r_B[k2][i2] * r_B[k2][j2]
-                                  * s_D[k1][k2][1][0];
-                        val_ext10+= r_B[k1][i1] * r_B[k1][j1]
-                                  * r_B[k2][i2] * r_B[k2][j2]
-                                  * s_D[k1][k2][0][1];
+                        if (bdr)
+                        {
+                           val_ext01+= r_B[k1][i1] * r_B[k1][j1]
+                                    * r_B[k2][i2] * r_B[k2][j2]
+                                    * s_D[k1][k2][1][0];
+                           val_ext10+= r_B[k1][i1] * r_B[k1][j1]
+                                    * r_B[k2][i2] * r_B[k2][j2]
+                                    * s_D[k1][k2][0][1];
+                        }
                      }
                   }
                   A_int(i1, i2, j1, j2, 0, f) = val_int0;
                   A_int(i1, i2, j1, j2, 1, f) = val_int1;
-                  A_ext(i1, i2, j1, j2, 0, f) = val_ext01;
-                  A_ext(i1, i2, j1, j2, 1, f) = val_ext10;
+                  if (bdr)
+                  {
+                     A_ext(i1, i2, j1, j2, 0, f) = val_ext01;
+                     A_ext(i1, i2, j1, j2, 1, f) = val_ext10;
+                  }
                }
             }
          }
@@ -189,35 +214,35 @@ void DGTraceIntegrator::SetupEA(const FiniteElementSpace &fes,
    const Array<double> &B = maps->B;
    if (dim == 1)
    {
-      return EADGTraceAssemble1D(nf,B,pa_data,ea_data_int,ea_data_ext);
+      return EADGTraceAssemble1D(nf,B,pa_data,ea_data_int,ea_data_ext,type);
    }
    else if (dim == 2)
    {
       switch ((dofs1D << 4 ) | quad1D)
       {
-         case 0x22: return EADGTraceAssemble2D<2,2>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x33: return EADGTraceAssemble2D<3,3>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x44: return EADGTraceAssemble2D<4,4>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x55: return EADGTraceAssemble2D<5,5>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x66: return EADGTraceAssemble2D<6,6>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x77: return EADGTraceAssemble2D<7,7>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x88: return EADGTraceAssemble2D<8,8>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x99: return EADGTraceAssemble2D<9,9>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         default:   return EADGTraceAssemble2D(nf,B,pa_data,ea_data_int,ea_data_ext,dofs1D,quad1D);
+         case 0x22: return EADGTraceAssemble2D<2,2>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x33: return EADGTraceAssemble2D<3,3>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x44: return EADGTraceAssemble2D<4,4>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x55: return EADGTraceAssemble2D<5,5>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x66: return EADGTraceAssemble2D<6,6>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x77: return EADGTraceAssemble2D<7,7>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x88: return EADGTraceAssemble2D<8,8>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x99: return EADGTraceAssemble2D<9,9>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         default:   return EADGTraceAssemble2D(nf,B,pa_data,ea_data_int,ea_data_ext,type,dofs1D,quad1D);
       }
    }
    else if (dim == 3)
    {
       switch ((dofs1D << 4 ) | quad1D)
       {
-         case 0x23: return EADGTraceAssemble3D<2,3>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x34: return EADGTraceAssemble3D<3,4>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x45: return EADGTraceAssemble3D<4,5>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x56: return EADGTraceAssemble3D<5,6>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x67: return EADGTraceAssemble3D<6,7>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x78: return EADGTraceAssemble3D<7,8>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         case 0x89: return EADGTraceAssemble3D<8,9>(nf,B,pa_data,ea_data_int,ea_data_ext);
-         default:   return EADGTraceAssemble3D(nf,B,pa_data,ea_data_int,ea_data_ext,dofs1D,quad1D);
+         case 0x23: return EADGTraceAssemble3D<2,3>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x34: return EADGTraceAssemble3D<3,4>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x45: return EADGTraceAssemble3D<4,5>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x56: return EADGTraceAssemble3D<5,6>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x67: return EADGTraceAssemble3D<6,7>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x78: return EADGTraceAssemble3D<7,8>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         case 0x89: return EADGTraceAssemble3D<8,9>(nf,B,pa_data,ea_data_int,ea_data_ext,type);
+         default:   return EADGTraceAssemble3D(nf,B,pa_data,ea_data_int,ea_data_ext,type,dofs1D,quad1D);
       }
    }
    MFEM_ABORT("Unknown kernel.");
@@ -233,7 +258,6 @@ void DGTraceIntegrator::AssembleEAInteriorFaces(const FiniteElementSpace& fes,
 void DGTraceIntegrator::AssembleEABoundaryFaces(const FiniteElementSpace& fes,
                                                 Vector &ea_data_bdr)
 {
-   //TODO this is wrong
    SetupEA(fes, ea_data_bdr, ea_data_bdr, FaceType::Boundary);
 }
 
