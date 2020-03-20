ec38800dddd05bddd5cc00065d9032e4e85072b6
Arturo Vargas <artvar3@yahoo.com>
2020-03-20 09:21:25 -0700

    0x1->VALUES

--- fem/quadinterpolator_face.cpp
+++ fem/quadinterpolator_face.cpp
@@ -499,7 +499,7 @@ void FaceQuadratureInterpolator::Values(
    const Vector &e_vec, Vector &q_val) const
 {
    Vector q_der, q_det, q_nor;
-   Mult(e_vec, 0x1, q_val, q_der, q_det, q_nor);
+   Mult(e_vec, VALUES, q_val, q_der, q_det, q_nor);
 }
 
 } // namespace mfem
