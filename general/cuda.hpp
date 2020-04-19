f6c889850632ea60a41de843c29299e4f02d093c
camierjs <camier1@llnl.gov>
2020-04-18 17:39:07 -0700

    Not Cuda or Hip logic

--- general/cuda.hpp
+++ general/cuda.hpp
@@ -59,7 +59,7 @@
 #define MFEM_STREAM_SYNC
 #endif
 
-#if !((defined(MFEM_USE_CUDA) && defined(__CUDA_ARCH__)) || \
+#if !((defined(MFEM_USE_CUDA) && defined(__CUDA_ARCH__)) && \
       (defined(MFEM_USE_HIP)  && defined(__HIP_DEVICE_COMPILE__)))
 #define MFEM_SHARED
 #define MFEM_SYNC_THREAD
