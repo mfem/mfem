02caf2cdf017c69d229c55b4f515c31b1f13a9f9
Robert <rac428@cornell.edu>
2020-04-20 15:01:29 -0700

    All L2 projection methods now work but still missing parallel implementations

--- fem/field_interpolant.cpp
+++ fem/field_interpolant.cpp
@@ -13,7 +13,7 @@
 
 #include "field_interpolant.hpp"
 #include "../linalg/densemat.hpp"
-#include "gridfunc.hpp"
+#include "fem.hpp"
 
 namespace mfem
 {
@@ -75,6 +75,7 @@ void QuadratureIntegrator::AssembleRHSElementVect(const FiniteElement &fe,
 //If that isn't the case we might be able to still do things but things will most likely be slower.
 void FieldInterpolant::ProjectQuadratureDiscCoefficient(GridFunction &gf,
                                                         VectorQuadratureFunctionCoefficient &vqfc,
+                                                        FiniteElementSpace &tr_fes,
                                                         FiniteElementSpace &fes)
 {
    int ndofs;
@@ -104,18 +105,18 @@ void FieldInterpolant::ProjectQuadratureDiscCoefficient(GridFunction &gf,
    VectorQuadratureIntegrator qi(vqfc);
    qi.SetIntRule(ir);
 
-   if (!setup)
+   if (!setup_disc)
    {
       m_all_data.SetSize(ndofs * ndofs * NE);
       double* data = m_all_data.HostReadWrite();
       for (int e = 0; e < NE; e++)
       {
-         const FiniteElement &fe = *fes.GetFE(e);
-         ElementTransformation &eltr = *fes.GetElementTransformation(e);
+         const FiniteElement &fe = *tr_fes.GetFE(e);
+         ElementTransformation &eltr = *tr_fes.GetElementTransformation(e);
          mi.UseExternalData((data + (ndofs * ndofs * e)), ndofs, ndofs);
          mass_int.AssembleElementMatrix(fe, eltr, mi);
       }
-      setup = true;
+      setup_disc = true;
    }
 
    double* data = m_all_data.HostReadWrite();
@@ -126,8 +127,8 @@ void FieldInterpolant::ProjectQuadratureDiscCoefficient(GridFunction &gf,
          qfv = 0.0;
          mi.UseExternalData((data + (ndofs * ndofs * e)), ndofs, ndofs);
          inv.Factor(mi);
-         const FiniteElement &fe = *fes.GetFE(e);
-         ElementTransformation &eltr = *fes.GetElementTransformation(e);
+         const FiniteElement &fe = *tr_fes.GetFE(e);
+         ElementTransformation &eltr = *tr_fes.GetElementTransformation(e);
          qi.AssembleRHSElementVect(fe, eltr, rhs);
          for (int ind = 0; ind < vdim; ind++)
          {
@@ -146,8 +147,8 @@ void FieldInterpolant::ProjectQuadratureDiscCoefficient(GridFunction &gf,
       {
          mi.UseExternalData((data + (ndofs * ndofs * e)), ndofs, ndofs);
          inv.Factor(mi);
-         const FiniteElement &fe = *fes.GetFE(e);
-         ElementTransformation &eltr = *fes.GetElementTransformation(e);
+         const FiniteElement &fe = *tr_fes.GetFE(e);
+         ElementTransformation &eltr = *tr_fes.GetElementTransformation(e);
          qi.AssembleRHSElementVect(fe, eltr, rhs);
          for (int ind = 0; ind < vdim; ind++)
          {
@@ -165,7 +166,7 @@ void FieldInterpolant::ProjectQuadratureDiscCoefficient(GridFunction &gf,
                qfv((nd * vdim) + ind) = tmp(nd + ind * ndofs);
             }
          }
-         fes.GetElementDofs(e, dofs);
+         fes.GetElementVDofs(e, dofs);
          gf.SetSubVector(dofs, qfv);
       }
    }
@@ -173,6 +174,7 @@ void FieldInterpolant::ProjectQuadratureDiscCoefficient(GridFunction &gf,
 
 void FieldInterpolant::ProjectQuadratureDiscCoefficient(GridFunction &gf,
                                                         QuadratureFunctionCoefficient &qfc,
+                                                        FiniteElementSpace &tr_fes,
                                                         FiniteElementSpace &fes)
 {
    int ndofs;
@@ -200,32 +202,133 @@ void FieldInterpolant::ProjectQuadratureDiscCoefficient(GridFunction &gf,
    QuadratureIntegrator qi(qfc);
    qi.SetIntRule(ir);
 
-   if (!setup)
+   if (!setup_disc)
    {
       m_all_data.SetSize(ndofs * ndofs * NE);
       double* data = m_all_data.HostReadWrite();
       for (int e = 0; e < NE; e++)
       {
-         const FiniteElement &fe = *fes.GetFE(e);
-         ElementTransformation &eltr = *fes.GetElementTransformation(e);
+         const FiniteElement &fe = *tr_fes.GetFE(e);
+         ElementTransformation &eltr = *tr_fes.GetElementTransformation(e);
          mi.UseExternalData((data + (ndofs * ndofs * e)), ndofs, ndofs);
          mass_int.AssembleElementMatrix(fe, eltr, mi);
       }
-      setup = true;
+      setup_disc = true;
    }
+
+
 
    double* data = m_all_data.HostReadWrite();
    for (int e = 0; e < NE; e++)
    {
       mi.UseExternalData((data + (ndofs * ndofs * e)), ndofs, ndofs);
       inv.Factor(mi);
-      const FiniteElement &fe = *fes.GetFE(e);
-      ElementTransformation &eltr = *fes.GetElementTransformation(e);
+      const FiniteElement &fe = *tr_fes.GetFE(e);
+      ElementTransformation &eltr = *tr_fes.GetElementTransformation(e);
       qi.AssembleRHSElementVect(fe, eltr, rhs);
       inv.Mult(rhs, qfv);
       fes.GetElementDofs(e, dofs);
       gf.SetSubVector(dofs, qfv);
    }
+}
+
+void FieldInterpolant::ProjectQuadratureCoefficient(GridFunction &gf,
+                                                    VectorQuadratureFunctionCoefficient &vqfc,
+                                                    FiniteElementSpace &fes)
+{
+   const IntegrationRule* ir;
+   {
+      // This is the best way I can think of to make sure the IntegrationRule in the FiniteElementSpace
+      // and the QuadratureSpace correspond to the same
+      const FiniteElement &el = *fes.GetFE(0);
+      ir = &(IntRules.Get(el.GetGeomType(), 2 * el.GetOrder() + 1));
+      const QuadratureFunction* qf = vqfc.GetQuadFunction();
+      const IntegrationRule* ir_qf = &qf->GetSpace()->GetElementIntRule(0);
+      MFEM_VERIFY((ir->GetOrder() == ir_qf->GetOrder()) &&
+                  (ir->GetNPoints() == ir_qf->GetNPoints()),
+                  "IntegrationRule in FiniteElementSpace and in QuadratureFunction appear to be different");
+   }
+
+   LinearForm *b = new LinearForm(&fes);
+   b->AddDomainIntegrator(new VectorQuadratureIntegrator(vqfc, ir));
+   b->Assemble();
+
+   // We need this fes to only have a vdim of 1 which is not the case here so we need to make a new fespace
+   // Potential fix me with a better implementation
+   const FiniteElementCollection *fec = fes.FEColl();
+   Mesh *mesh = fes.GetMesh();
+   FiniteElementSpace fes_v1(mesh, fec, 1);
+
+   BilinearForm *L2 = new BilinearForm(&fes_v1);
+   L2->AddDomainIntegrator(new MassIntegrator(ir));
+   L2->Assemble();
+
+   GridFunction x(&fes);
+   x = 0.0;
+   OperatorPtr A;
+   Vector B, b_sub, X_sub, X;
+
+   Array<int> ess_tdof_list;
+
+   int vdim = vqfc.GetVDim();
+   int size = b->Size() / vdim;
+
+   for(int ind = 0; ind < vdim; ind++) {
+      int offset = ind * size;
+      b_sub.MakeRef(*b, offset, size);
+      X_sub.MakeRef(x, offset, size);
+      L2->FormLinearSystem(ess_tdof_list, X_sub, b_sub, A, X, B);
+      // Fix this to be more efficient
+      // OperatorJacobiSmoother M(*L2, ess_tdof_list);
+      CG(*A, B, X, 0, 2000, 1e-25, 0.0);
+      // Recover the solution as a finite element grid function.
+      L2->RecoverFEMSolution(X, *b, X_sub);
+   }
+   gf = x;
+
+   delete L2;
+   delete b;
+}
+void FieldInterpolant::ProjectQuadratureCoefficient(GridFunction &gf,
+                                                    QuadratureFunctionCoefficient &qfc,
+                                                    FiniteElementSpace &fes)
+{
+   const IntegrationRule* ir;
+   {
+      // This is the best way I can think of to make sure the IntegrationRule in the FiniteElementSpace
+      // and the QuadratureSpace correspond to the same
+      const FiniteElement &el = *fes.GetFE(0);
+      ir = &(IntRules.Get(el.GetGeomType(), 2 * el.GetOrder() + 1));
+      const QuadratureFunction* qf = qfc.GetQuadFunction();
+      const IntegrationRule* ir_qf = &qf->GetSpace()->GetElementIntRule(0);
+      MFEM_VERIFY((ir->GetOrder() == ir_qf->GetOrder()) &&
+                  (ir->GetNPoints() == ir_qf->GetNPoints()),
+                  "IntegrationRule in FiniteElementSpace and in QuadratureFunction appear to be different");
+   }
+   LinearForm *b = new LinearForm(&fes);
+   b->AddDomainIntegrator(new QuadratureIntegrator(qfc, ir));
+   b->Assemble();
+
+   BilinearForm *L2 = new BilinearForm(&fes);
+   L2->AddDomainIntegrator(new MassIntegrator(ir));
+   L2->Assemble();
+
+   GridFunction x(&fes);
+   x = 0.0;
+   OperatorPtr A;
+   Vector B, X;
+   Array<int> ess_tdof_list;
+
+   L2->FormLinearSystem(ess_tdof_list, x, *b, A, X, B);
+   // Fix this to be more efficient
+   // OperatorJacobiSmoother M(*L2, ess_tdof_list);
+   CG(*A, B, X, 0, 2000, 1e-25, 0.0);
+   // Recover the solution as a finite element grid function.
+   L2->RecoverFEMSolution(X, *b, x);
+   gf = x;
+
+   delete L2;
+   delete b;
 }
 
 }
--- fem/field_interpolant.hpp
+++ fem/field_interpolant.hpp
@@ -28,19 +28,41 @@ namespace mfem
 class FieldInterpolant
 {
 private:
-   bool setup;
+   bool setup_disc;
    Vector m_all_data;
    MassIntegrator mass_int;
    int NE;
 public:
    FieldInterpolant(const IntegrationRule* ir) { mass_int.SetIntRule(ir); }
+   // This function takes a vector quadrature function coefficient and projects it onto a GridFunction that lives
+   // in L2 space. This function requires tr_fes to be the finite element space that the VectorQuadratureFunctionCoefficient lives on
+   // and fes is the L2 finite element space that we're projecting onto.
    void ProjectQuadratureDiscCoefficient(GridFunction &gf,
                                          VectorQuadratureFunctionCoefficient &vqfc,
+                                         FiniteElementSpace &tr_fes,
                                          FiniteElementSpace &fes);
+   // This function takes a quadrature function coefficient and projects it onto a GridFunction that lives
+   // in L2 space. This function requires tr_fes to be the finite element space that the QuadratureFunctionCoefficient lives on
+   // and fes is the L2 finite element space that we're projecting onto.
    void ProjectQuadratureDiscCoefficient(GridFunction &gf,
                                          QuadratureFunctionCoefficient &qfc,
+                                         FiniteElementSpace &tr_fes,
                                          FiniteElementSpace &fes);
-   void SetupReset() { setup = false; }
+   //Parallel versions of the ProjectQuadratureCoefficient will need to be created once the serial version works
+   
+   // This function takes a vector quadrature function coefficient and projects it onto a GridFunction of the same space as vector
+   // quadrature function coefficient.
+   void ProjectQuadratureCoefficient(GridFunction &gf,
+                                     VectorQuadratureFunctionCoefficient &vqfc,
+                                     FiniteElementSpace &fes);
+   // This function takes a quadrature function coefficient and projects it onto a GridFunction of the same space as 
+   // quadrature function coefficient.
+   void ProjectQuadratureCoefficient(GridFunction &gf,
+                                     QuadratureFunctionCoefficient &qfc,
+                                     FiniteElementSpace &fes);
+   //Tells the ProjectQuadratureDiscCoefficient that they need to recalculate the data.
+   void SetupDiscReset() { setup_disc = false; }
+   ~FieldInterpolant() {}
 };
 
 class VectorQuadratureIntegrator : public LinearFormIntegrator
@@ -50,6 +72,8 @@ private:
 public:
    VectorQuadratureIntegrator(VectorQuadratureFunctionCoefficient &vqfc) : vqfc(
          vqfc) { }
+   VectorQuadratureIntegrator(VectorQuadratureFunctionCoefficient &vqfc, const IntegrationRule *ir) : vqfc(
+         vqfc), LinearFormIntegrator(ir) { }
    using LinearFormIntegrator::AssembleRHSElementVect;
    void AssembleRHSElementVect(const FiniteElement &fe,
                                ElementTransformation &Tr,
@@ -62,6 +86,7 @@ private:
    QuadratureFunctionCoefficient &qfc;
 public:
    QuadratureIntegrator(QuadratureFunctionCoefficient &qfc) : qfc(qfc) { }
+   QuadratureIntegrator(QuadratureFunctionCoefficient &qfc, const IntegrationRule *ir) : qfc(qfc), LinearFormIntegrator(ir) { }
    using LinearFormIntegrator::AssembleRHSElementVect;
    void AssembleRHSElementVect(const FiniteElement &fe,
                                ElementTransformation &Tr,
