8e17d5d2e63baa3f715d56f06de471183f1da8be
Yohann Dudouit <dudouit1@llnl.gov>
2020-04-21 19:10:42 -0700

    Change CuKernel to use a for loop instead of if.

--- general/forall.hpp
+++ general/forall.hpp
@@ -187,25 +187,34 @@ void RajaSeqWrap(const int N, HBODY &&h_body)
 template <typename BODY> __global__ static
 void CuKernel1D(const int N, BODY body)
 {
-   const int k = blockDim.x*blockIdx.x + threadIdx.x;
-   if (k >= N) { return; }
-   body(k);
+   for (int k = blockIdx.x * blockDim.x + threadIdx.x; 
+        k < N; 
+        k += blockDim.x * gridDim.x)
+   {
+      body(k);
+   }
 }
 
 template <typename BODY> __global__ static
 void CuKernel2D(const int N, BODY body, const int BZ)
 {
-   const int k = blockIdx.x*BZ + threadIdx.z;
-   if (k >= N) { return; }
-   body(k);
+   for (int k = blockIdx.x * BZ + threadIdx.z; 
+        k < N; 
+        k += blockDim.x * blockDim.y * blockDim.z * gridDim.x)
+   {
+      body(k);
+   }
 }
 
 template <typename BODY> __global__ static
 void CuKernel3D(const int N, BODY body)
 {
-   const int k = blockIdx.x;
-   if (k >= N) { return; }
-   body(k);
+   for (int k = blockIdx.x; 
+        k < N; 
+        k += blockDim.x * blockDim.y * blockDim.z * gridDim.x)
+   {
+      body(k);
+   }
 }
 
 template <const int BLCK = MFEM_CUDA_BLOCKS, typename DBODY>
