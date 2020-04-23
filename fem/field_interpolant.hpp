c822f960834b273dd8d0422ac12143d53ecc47f7
Robert <rac428@cornell.edu>
2020-04-23 13:01:07 -0700

    Refactor class FieldInterpolant to seperate out parallel implementation and not require the formation of bilinearform and cg objects to be repeatedly created each time a function is called.

--- fem/field_interpolant.hpp
+++ fem/field_interpolant.hpp
@@ -20,10 +20,12 @@
 #include "eltrans.hpp"
 #include "coefficient.hpp"
 #include "bilininteg.hpp"
+#include "bilinearform.hpp"
 #include "lininteg.hpp"
 #include "gridfunc.hpp"
 #ifdef MFEM_USE_MPI
 #include "pgridfunc.hpp"
+#include "pbilinearform.hpp"
 #endif
 
 namespace mfem
@@ -31,13 +33,28 @@ namespace mfem
 
 class FieldInterpolant
 {
-private:
+protected:
    bool setup_disc;
+   bool setup_full;
    Vector m_all_data;
-   MassIntegrator mass_int;
+   BilinearForm *L2;
+   CGSolver *cg;
    int NE;
 public:
-   FieldInterpolant(const IntegrationRule* ir) : setup_disc(false) { mass_int.SetIntRule(ir); }
+   // The FiniteElementSpace passed into here should have a vdim set to 1 in order for the
+   // MassIntegrator to work properly if the ProjectQuadratureCoefficient method is used with
+   // a VectorQuadratureFunctionCoefficient.
+   FieldInterpolant(FiniteElementSpace *fes) : setup_disc(false), setup_full(false)
+   {
+      L2 = new BilinearForm(fes);
+
+      const FiniteElement &el = *fes->GetFE(0);
+      const IntegrationRule *ir = &(IntRules.Get(el.GetGeomType(),
+                                                 2 * el.GetOrder() + 1));
+
+      L2->AddDomainIntegrator(new MassIntegrator(ir));
+      L2->Assemble();
+   }
    // This function takes a vector quadrature function coefficient and projects it onto a GridFunction that lives
    // in L2 space. This function requires tr_fes to be the finite element space that the VectorQuadratureFunctionCoefficient lives on
    // and fes is the L2 finite element space that we're projecting onto.
@@ -52,8 +69,6 @@ public:
                                          QuadratureFunctionCoefficient &qfc,
                                          FiniteElementSpace &tr_fes,
                                          FiniteElementSpace &fes);
-   //Parallel versions of the ProjectQuadratureCoefficient will need to be created once the serial version works
-
    // This function takes a vector quadrature function coefficient and projects it onto a GridFunction of the same space as vector
    // quadrature function coefficient.
    void ProjectQuadratureCoefficient(GridFunction &gf,
@@ -64,21 +79,80 @@ public:
    void ProjectQuadratureCoefficient(GridFunction &gf,
                                      QuadratureFunctionCoefficient &qfc,
                                      FiniteElementSpace &fes);
-#ifdef MFEM_USE_MPI
-   // This function takes a vector quadrature function coefficient and projects it onto a GridFunction of the same space as vector
-   // quadrature function coefficient.
-   void ProjectQuadratureCoefficient(ParGridFunction &gf,
-                                     VectorQuadratureFunctionCoefficient &vqfc,
-                                     ParFiniteElementSpace &fes);
-   // This function takes a quadrature function coefficient and projects it onto a GridFunction of the same space as
-   // quadrature function coefficient.
-   void ProjectQuadratureCoefficient(ParGridFunction &gf,
-                                     QuadratureFunctionCoefficient &qfc,
-                                     ParFiniteElementSpace &fes);
-#endif
-   //Tells the ProjectQuadratureDiscCoefficient that they need to recalculate the data.
+   // Tells the ProjectQuadratureDiscCoefficient that they need to recalculate the data.
    void SetupDiscReset() { setup_disc = false; }
-   ~FieldInterpolant() {}
+   // Tells the ProjectQuadratureCoefficient that they need to recalculate the data.
+   virtual void FullReset()
+   {
+      L2->Update();
+      L2->Assemble();
+   }
+   virtual void SetupCG()
+   {
+      cg = new CGSolver();
+      cg->SetPrintLevel(0);
+      cg->SetMaxIter(2000);
+      cg->SetRelTol(sqrt(1e-30));
+      cg->SetAbsTol(sqrt(0.0));
+   }
+   ~FieldInterpolant()
+   {
+      delete L2;
+      delete cg;
+   }
+};
+
+class ParFieldInterpolant : public FieldInterpolant
+{
+protected:
+   ParBilinearForm *ParL2;
+public:
+   // The ParFiniteElementSpace passed into here should have a vdim set to 1 in order for the
+   // MassIntegrator to work properly if the ProjectQuadratureCoefficient method is used with
+   // a VectorQuadratureFunctionCoefficient.
+   ParFieldInterpolant(ParFiniteElementSpace *pfes) : FieldInterpolant(pfes)
+   {
+      ParL2 = new ParBilinearForm(pfes);
+
+      const FiniteElement &el = *pfes->GetFE(0);
+      const IntegrationRule *ir = &(IntRules.Get(el.GetGeomType(),
+                                                 2 * el.GetOrder() + 1));
+
+      ParL2->AddDomainIntegrator(new MassIntegrator(ir));
+      ParL2->Assemble();
+   }
+   // This function takes a vector quadrature function coefficient and projects it onto a GridFunction of the same space as vector
+   // quadrature function coefficient.
+   void ProjectQuadratureCoefficient(ParGridFunction &gf,
+                                     VectorQuadratureFunctionCoefficient &vqfc,
+                                     ParFiniteElementSpace &fes);
+   // This function takes a quadrature function coefficient and projects it onto a GridFunction of the same space as
+   // quadrature function coefficient.
+   void ProjectQuadratureCoefficient(ParGridFunction &gf,
+                                     QuadratureFunctionCoefficient &qfc,
+                                     ParFiniteElementSpace &fes);
+   // Tells the internal bilinearform needs to be reset in order to reset the sparse matrix
+   virtual void FullReset() override
+   {
+      FieldInterpolant::FullReset();
+      ParL2->Update();
+      ParL2->Assemble();
+   }
+   using FieldInterpolant::SetupCG;
+   // Setup the CG solver with an MPI communicator
+   virtual void SetupCG(MPI_Comm _comm)
+   {
+      cg = new CGSolver(_comm);
+      cg->SetPrintLevel(0);
+      cg->SetMaxIter(2000);
+      cg->SetRelTol(sqrt(1e-30));
+      cg->SetAbsTol(sqrt(0.0));
+   }
+
+   ~ParFieldInterpolant()
+   {
+      delete ParL2;
+   }
 };
 
 class VectorQuadratureIntegrator : public LinearFormIntegrator
