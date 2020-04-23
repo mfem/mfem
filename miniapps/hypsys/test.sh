5bf4712efd928049d8f78cc5aba36bac659da6a0
HennesHajduk <hajduk1@llnl.gov>
2020-04-23 15:28:10 +0200

    wip, updated test.sh

--- miniapps/hypsys/test.sh
+++ miniapps/hypsys/test.sh
@@ -1,39 +1,49 @@
-make clean && make -j;
+# make clean && make -j;
 
-SCHEME=0
+SCHEME=1
 
 ## Advection
 
 # Solid Body Roatation
-./hypsys -e $SCHEME -r 0
-./phypsys -e $SCHEME -r 0
-mpirun -np 4 ./phypsys -e $SCHEME -r 0
+# ./hypsys -r 0 -e $SCHEME
+# ./phypsys -r 0 -e $SCHEME
+# mpirun -np 4 ./phypsys -r 0 -e $SCHEME
 
 # Steady Circular Convection
-./hypsys -e $SCHEME -vs 1000 -m data/inline-4quad.mesh -dt 0.0001 -o 2 -s 1 -r 3 -c 0
-./phypsys -e $SCHEME -vs 1000 -m data/inline-4quad.mesh -dt 0.0001 -o 2 -s 1 -r 3 -c 0
-mpirun -np 4 ./phypsys -e $SCHEME -vs 1000 -m data/inline-4quad.mesh -dt 0.0001 -o 2 -s 1 -r 3 -c 0
+# ./hypsys -vs 1000 -m data/inline-4quad.mesh -dt 0.0001 -o 2 -s 1 -r 3 -c 0 -e $SCHEME
+# ./phypsys -vs 1000 -m data/inline-4quad.mesh -dt 0.0001 -o 2 -s 1 -r 3 -c 0 -e $SCHEME
+# mpirun -np 4 ./phypsys -vs 1000 -m data/inline-4quad.mesh -dt 0.0001 -o 2 -s 1 -r 3 -c 0 -e $SCHEME
+
+# Translation
+CONFIG="-p 0 -c 2 -vs 100 -tf 0.4 -s 3 -dt 0.004 -m data/periodic-tri.mesh -o 2 -r 3 -e $SCHEME"
+./hypsys $CONFIG
+./phypsys $CONFIG
+mpirun -np 4 phypsys $CONFIG
 
 ## Burgers
-./hypsys -e $SCHEME -p 1 -o 0 -tf 0.5 -r 4 -m data/inline-4quad.mesh
-./phypsys -e $SCHEME -p 1 -o 0 -tf 0.5 -r 4 -m data/inline-4quad.mesh
-mpirun -np 4 ./phypsys -e $SCHEME -p 1 -o 0 -tf 0.5 -r 4 -m data/inline-4quad.mesh
+CONFIG="-p 1 -c 1 -vs 100 -tf 0.5 -s 3 -dt 0.005 -m data/inline-4quad.mesh -o 1 -r 3 -e $SCHEME"
+./hypsys $CONFIG
+./phypsys $CONFIG
+mpirun -np 3 phypsys $CONFIG
 
 ## KPP
-./hypsys -e $SCHEME -m data/inline-4quad.mesh -r 5 -o 0 -p 2 -tf 0.25 -dt 0.004
-./phypsys -e $SCHEME -m data/inline-4quad.mesh -r 5 -o 0 -p 2 -tf 0.25 -dt 0.004
-mpirun -np 4 ./phypsys -e $SCHEME -m data/inline-4quad.mesh -r 5 -o 0 -p 2 -tf 0.25 -dt 0.004
+CONFIG="-p 2 -c 1 -vs 50 -tf 0.25 -s 3 -dt 0.005 -m data/inline-4tri.mesh -o 0 -r 4 -e $SCHEME"
+./hypsys $CONFIG
+./phypsys $CONFIG
+mpirun -np 1 phypsys $CONFIG
 
 ## Shallow-Water
 
 # Dam break
-./hypsys -e $SCHEME -p 3 -m data/wall-bdr-4tri.mesh -r 3 -o 1 -tf 0.25
-./phypsys -e $SCHEME -p 3 -m data/wall-bdr-4tri.mesh -r 3 -o 1 -tf 0.25
-mpirun -np 4 ./phypsys -e $SCHEME -p 3 -m data/wall-bdr-4tri.mesh -r 3 -o 1 -tf 0.25
+CONFIG="-p 3 -c 1 -vs 14 -tf 0.7 -s 3 -dt 0.005 -m data/wall-bdr-4tri.mesh -o 1 -r 3 -e $SCHEME"
+./hypsys $CONFIG
+./phypsys $CONFIG
+mpirun -np 2 phypsys $CONFIG
 
 ## Euler
 
 # Smooth vortex
-./hypsys -e $SCHEME -p 4 -c 0 -tf 2 -m data/periodic-square.mesh -r 2
-./phypsys -e $SCHEME -p 4 -c 0 -tf 2 -m data/periodic-square.mesh -r 2
-mpirun -np 4 ./phypsys -e $SCHEME -p 4 -c 0 -tf 2 -m data/periodic-square.mesh -r 2
+CONFIG="-p 4 -c 0 -vs 100 -tf 2 -s 3 -dt 0.004 -m data/periodic-square.mesh -r 2 -e $SCHEME"
+./hypsys $CONFIG
+./phypsys $CONFIG
+mpirun -np 4 phypsys $CONFIG
