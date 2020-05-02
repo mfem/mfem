9bb20409177b57c3a36c2339e8a7e8b0bcaac0e5
Morteza H. Siboni <hakimi1364@gmail.com>
2020-05-02 11:01:51 -0400

    Updates declarations in pumi.hpp to match w/ .cpp

--- mesh/pumi.hpp
+++ mesh/pumi.hpp
@@ -79,22 +79,21 @@ public:
                int refine = 1, bool fix_orientation = true);
 
 
-   /// Returns the permutation (aka rotation, aka orientation) needed to
-   /// convert PUMI tet to an MFEM tet. This represents the change in
-   /// tet-to-vertex connectivity between the PUMI and MFEM meshes.
-   /// e.g.:
-   /// PUMI_tet{v0,v1,v2,v3}  --->  MFEM_tet{v1,v0,v3,v2}
-   /// * Note that change in the orientation can be caused by
-   ///   a) fixing wrong boundary element orientations
-   ///   b) a call to ReorientTetMesh() which is required for Nedelec
+   /// Returns the PUMI-to-MFEM permutation (aka rotation, aka orientation)
+   /** This represents the change in tet-to-vertex connectivity between
+       the PUMI and MFEM meshes. E.g.,
+       PUMI_tet{v0,v1,v2,v3}  --->  MFEM_tet{v1,v0,v3,v2}
+       * Note that change in the orientation can be caused by
+         a) fixing wrong boundary element orientations
+         b) a call to ReorientTetMesh() which is required for Nedelec */
    int RotationPUMItoMFEM(apf::Mesh2* apf_mesh,
                           apf::MeshEntity* tet,
                           int elemId);
    /// Convert the parent coordinate from PUMI to MFEM
-   /// * By default this functions assumes that there is always
-   ///   change in the orientations of some of the elements.
-   /// * In case it is know for sure that there is NO change in
-   ///   the orientation, call the functions with last argument = true
+   /** By default this functions assumes that there is always
+       change in the orientations of some of the elements. In case it
+       is known for sure that there is NO change in the orientation,
+       call the functions with last argument = false */
    IntegrationRule ParentXisPUMItoMFEM(apf::Mesh2* apf_mesh,
                                        apf::MeshEntity* tet,
                                        int elemId,
@@ -102,27 +101,28 @@ public:
                                        bool checkOrientation = true);
    /// Transfer field from MFEM mesh to PUMI mesh [Mixed].
    void FieldMFEMtoPUMI(apf::Mesh2* apf_mesh,
-                        ParGridFunction* Vel,
-                        ParGridFunction* Pr,
-                        apf::Field* VelField,
-                        apf::Field* PrField,
-                        apf::Field* VelMagField);
+                        ParGridFunction* grid_vel,
+                        ParGridFunction* grid_pr,
+                        apf::Field* vel_field,
+                        apf::Field* pr_field,
+                        apf::Field* vel_mag_field);
 
    /// Transfer field from MFEM mesh to PUMI mesh [Scalar].
    void FieldMFEMtoPUMI(apf::Mesh2* apf_mesh,
-                        ParGridFunction* Pr,
-                        apf::Field* PrField,
-                        apf::Field* PrMagField);
+                        ParGridFunction* grid_pr,
+                        apf::Field* pr_field,
+                        apf::Field* pr_mag_field);
 
    /// Transfer field from MFEM mesh to PUMI mesh [Vector].
    void VectorFieldMFEMtoPUMI(apf::Mesh2* apf_mesh,
-                              ParGridFunction* Vel,
-                              apf::Field* VelField,
-                              apf::Field* VelMagField);
+                              ParGridFunction* grid_vel,
+                              apf::Field* vel_field,
+                              apf::Field* vel_mag_field);
 
+   /// Transfer Nedelec field from MFEM mesh to PUMI mesh [Vector].
    void NedelecFieldMFEMtoPUMI(apf::Mesh2* apf_mesh,
                                ParGridFunction* gf,
-                               apf::Field* NedelecField);
+                               apf::Field* nedelec_field);
 
    /// Update the mesh after adaptation.
    void UpdateMesh(const ParMesh* AdaptedpMesh);
