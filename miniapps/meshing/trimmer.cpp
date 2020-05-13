a4f93fbcf8ea66fdb245bd2f140ae0e821fe129a
Stowell, Mark L <stowell1@llnl.gov>
2020-05-13 15:44:48 -0700

    Adding documentation header, changing default, behavior, and adding visualization

--- miniapps/meshing/trimmer.cpp
+++ miniapps/meshing/trimmer.cpp
@@ -9,6 +9,42 @@
 // terms of the BSD-3 license. We welcome feedback and contributions, see file
 // CONTRIBUTING.md for details.
 //
+//   -----------------------------------------------------------------------
+//   Trimmer Miniap: Trim away elements according to their attribute numbers
+//   -----------------------------------------------------------------------
+//
+// This miniapp creates a new mesh consisting of all the elements not
+// possessing a given set of attribute numbers.  The new boundary elements
+// are created with boundary attribute numbers related to the trimmed elements'
+// attribute numbers.
+//
+// By default the new boundary elements will have new attribute
+// numbers so as not to interfere with existing boundaries.  For
+// example, consider a mesh with attriutes given by:
+//
+//   attributes = {a1, a2, a3, a4, a5, a6, ..., amax}
+//   bdr_attributes = {b1, b2, ..., bmax}
+//
+// If we trim away elements with attributes a2 and a4 the new mesh will have
+// attributes:
+//
+//   attributes: {a1, a3, a5, a6, ..., amax}
+//   bdr_attributes = {b1, b2, ..., bmax, bmax + a2, bmax + a4}
+//
+// The user has the option of providing new attribute numbers for each group
+// of elements to be trimmed.  In this case the new boundary elements may have
+// the same attribute numbers as existing boundary elements.
+//
+// The resulting mesh is displayed with GLVis (unless explicitly disabled) and
+// is also written to the file "trimmer.mesh"
+//
+// Compile with: make trimmer
+//
+// Sample runs:
+//    trimmer -a '2' -b '2'
+//    trimmer -m ../../data/beam-hex.mesh -a '2'
+//    trimmer -m ../../data/beam-hex.mesh -a '2' -b '2'
+
 #include "mfem.hpp"
 #include <fstream>
 #include <iostream>
@@ -20,39 +56,51 @@ int main(int argc, char *argv[])
 {
    // Parse command-line options.
    const char *mesh_file = "../../data/beam-tet.vtk";
-   int offset = -1;
    Array<int> attr;
-
+   Array<int> bdr_attr;
+   bool visualization = 1;
+   
    OptionsParser args(argc, argv);
    args.AddOption(&mesh_file, "-m", "--mesh",
                   "Mesh file to use.");
-   args.AddOption(&offset, "-o", "--attr-offset",
-                  "Offset is added to the element attribute to generate "
-                  "the boundary offset.");
    args.AddOption(&attr, "-a", "-attr", "Set of attributes to remove from "
                   "the mesh.");
+   args.AddOption(&bdr_attr, "-b", "-bdr-attr", "Set of boundary attributes "
+		  "to assign to the new boundary elements.");
+   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
+                  "--no-visualization",
+                  "Enable or disable GLVis visualization.");
    args.Parse();
    if (!args.Good())
    {
       args.PrintUsage(cout);
       return 1;
    }
-   if (attr.Size() == 0)
-   {
-      attr.SetSize(1);
-      attr[0] = 2;
-   }
-
+   
    Mesh mesh(mesh_file, 0, 0);
 
-   int max_attr = mesh.attributes.Max();
-   int max_bdr_attr = (offset == -1) ? mesh.bdr_attributes.Max() : offset;
+   int max_attr     = mesh.attributes.Max();
+   int max_bdr_attr = mesh.bdr_attributes.Max();
 
+   if (bdr_attr.Size() == 0)
+   {
+     bdr_attr.SetSize(attr.Size());
+     for (int i=0; i<attr.Size(); i++)
+     {
+       bdr_attr[i] = max_bdr_attr + attr[i];
+     }
+   }
+   MFEM_VERIFY(attr.Size() == bdr_attr.Size(),
+	       "Size mismatch in attribute arguments.");
+   
    Array<int> marker(max_attr);
+   Array<int> attr_inv(max_attr);
    marker = 0;
+   attr_inv = 0;
    for (int i=0; i<attr.Size(); i++)
    {
       marker[attr[i]-1] = 1;
+      attr_inv[attr[i]-1] = i;
    }
 
    // Count the number of elements in the final mesh
@@ -140,13 +188,13 @@ int main(int argc, char *argv[])
          if (marker[a1-1] && !marker[a2-1])
          {
             Element * bel = mesh.GetFace(f)->Duplicate(&trimmed_mesh);
-            bel->SetAttribute(max_bdr_attr + a1);
+	    bel->SetAttribute(bdr_attr[attr_inv[a1-1]]);
             trimmed_mesh.AddBdrElement(bel);
          }
          else if (!marker[a1-1] && marker[a2-1])
          {
             Element * bel = mesh.GetFace(f)->Duplicate(&trimmed_mesh);
-            bel->SetAttribute(max_bdr_attr + a2);
+	    bel->SetAttribute(bdr_attr[attr_inv[a2-1]]);
             trimmed_mesh.AddBdrElement(bel);
          }
       }
@@ -156,7 +204,19 @@ int main(int argc, char *argv[])
    trimmed_mesh.Finalize();
    trimmed_mesh.RemoveUnusedVertices();
 
-   ofstream ofs("trimmed.mesh");
-   trimmed_mesh.Print(ofs);
-   ofs.close();
+   // Save the final mesh
+   ofstream mesh_ofs("trimmer.mesh");
+   mesh_ofs.precision(8);
+   trimmed_mesh.Print(mesh_ofs);
+
+   if (visualization)
+   {
+      // GLVis server to visualize to
+      char vishost[] = "localhost";
+      int  visport   = 19916;
+      socketstream sol_sock(vishost, visport);
+      sol_sock.precision(8);
+      sol_sock << "mesh\n" << trimmed_mesh << flush;
+   }
+
 }
