9b523e3cd9557374ec98866b5a3a1c4b60523631
Tucker Babcock <tbabcock03@gmail.com>
2020-05-14 09:37:02 -0700

    added test for ProjectRevDiff with tet elements

--- tests/unit/fem/test_projection_revdiff.cpp
+++ tests/unit/fem/test_projection_revdiff.cpp
@@ -49,17 +49,32 @@ std::string mesh_str =
    "-8 4"                                "\n"
    "-7 4"                                "\n";
 
-void func(const Vector &x, Vector &y)
+void func2D(const Vector &x, Vector &y)
 {
    y.SetSize(2);
    y(0) = x(0)*x(0) - x(1);
    y(1) = x(0) * exp(x(1));
 }
 
-void funcRevDiff(const Vector &x, const Vector &v_bar, Vector &x_bar)
+void func2DRevDiff(const Vector &x, const Vector &v_bar, Vector &x_bar)
 {
    x_bar(0) = v_bar(0) * 2*x(0) + v_bar(1) * exp(x(1));
    x_bar(1) = -v_bar(0) + v_bar(1) * x(0) * exp(x(1)); 
+}
+
+void func3D(const Vector &x, Vector &y)
+{
+   y.SetSize(3);
+   y(0) = x(0)*x(0) - x(1);
+   y(1) = x(0) * exp(x(1));
+   y(2) = x(2)*x(0) - x(1);
+}
+
+void func3DRevDiff(const Vector &x, const Vector &v_bar, Vector &x_bar)
+{
+   x_bar(0) = v_bar(0) * 2*x(0) + v_bar(1) * exp(x(1)) + v_bar(2)*x(2);
+   x_bar(1) = -v_bar(0) + v_bar(1) * x(0) * exp(x(1)) - v_bar(2); 
+   x_bar(2) = v_bar(2) * x(0); 
 }
 
 } // anonymous namespace
@@ -77,38 +92,32 @@ TEST_CASE("FiniteElement::Project_RevDiff reverse-mode differentiation",
    // Create quadratic mesh with single C-shaped quadrilateral
    std::stringstream meshStr;
    meshStr << mesh_str;
-   Mesh mesh(meshStr);
+   Mesh mesh2D(meshStr);
 
-   // /// Added incase nedelec elements failed because of the curved mesh
-   // Mesh mesh(1, 1, Element::QUADRILATERAL,
-   //           true /* gen. edges */, 1.0,
-   //           1.0, true);
-   // mesh.EnsureNodes();
-
-   REQUIRE( mesh.GetNE() == 1 );
-   REQUIRE( mesh.GetNodes() != NULL );
+   REQUIRE( mesh2D.GetNE() == 1 );
+   REQUIRE( mesh2D.GetNodes() != NULL );
 
    bool dumpMesh = false;
    if (dumpMesh)
    {
-      std::ofstream mesh_ostream("finitelement-project-revdiff-mesh.vtk");
+      std::ofstream mesh_ostream("finitelement-project-revdiff-quad-mesh.vtk");
       mesh_ostream.precision(14);
       int refine = 10;
-      mesh.PrintVTK(mesh_ostream, refine);
+      mesh2D.PrintVTK(mesh_ostream, refine);
    }
 
-   VectorFunctionCoefficient vc(2, func, funcRevDiff);
+   VectorFunctionCoefficient vc2D(2, func2D, func2DRevDiff);
 
    for (int p = 1; p <= 4; ++p)
    {
-      SECTION("Project_RT_RevDiff for degree p = " + std::to_string(p))
+      SECTION("Project_RT_RevDiff (quad) for degree p = " + std::to_string(p))
       {
          RT_FECollection fec(p, 2);
-         FiniteElementSpace fes(&mesh, &fec);
+         FiniteElementSpace fes(&mesh2D, &fec);
 
          const FiniteElement &el = *fes.GetFE(0);
          IsoparametricTransformation trans;
-         mesh.GetElementTransformation(0, &trans);
+         mesh2D.GetElementTransformation(0, &trans);
 
          DenseMatrix &coords = trans.GetPointMat();
          DenseMatrix coords_bar(coords.Height(), coords.Width());
@@ -127,7 +136,7 @@ TEST_CASE("FiniteElement::Project_RevDiff reverse-mode differentiation",
       
          // reverse-mode differentiation of projection
          coords_bar = 0.0;
-         el.Project_RevDiff(P_bar, vc, trans, coords_bar);
+         el.Project_RevDiff(P_bar, vc2D, trans, coords_bar);
 
          // get the weighted derivatives using finite difference method
          for (int n = 0; n < coords.Width(); ++n)
@@ -135,9 +144,9 @@ TEST_CASE("FiniteElement::Project_RevDiff reverse-mode differentiation",
             for (int di = 0; di < coords.Height(); ++di)
             {
                coords(di, n) += eps_fd;
-               el.Project(vc, trans, dofs_fd);
+               el.Project(vc2D, trans, dofs_fd);
                coords(di, n) -= 2.0*eps_fd;
-               el.Project(vc, trans, dofs_pert);
+               el.Project(vc2D, trans, dofs_pert);
                dofs_fd -= dofs_pert;
                dofs_fd *= 1.0/(2.0*eps_fd);
                coords(di, n) += eps_fd;
@@ -151,14 +160,14 @@ TEST_CASE("FiniteElement::Project_RevDiff reverse-mode differentiation",
 
    for (int p = 1; p <= 4; ++p)
    {
-      SECTION("Project_ND_RevDiff for degree p = " + std::to_string(p))
+      SECTION("Project_ND_RevDiff (quad) for degree p = " + std::to_string(p))
       {
          ND_FECollection fec(p, 2);
-         FiniteElementSpace fes(&mesh, &fec);
+         FiniteElementSpace fes(&mesh2D, &fec);
 
          const FiniteElement &el = *fes.GetFE(0);
          IsoparametricTransformation trans;
-         mesh.GetElementTransformation(0, &trans);
+         mesh2D.GetElementTransformation(0, &trans);
 
          DenseMatrix &coords = trans.GetPointMat();
          DenseMatrix coords_bar(coords.Height(), coords.Width());
@@ -177,7 +186,7 @@ TEST_CASE("FiniteElement::Project_RevDiff reverse-mode differentiation",
       
          // reverse-mode differentiation of projection
          coords_bar = 0.0;
-         el.Project_RevDiff(P_bar, vc, trans, coords_bar);
+         el.Project_RevDiff(P_bar, vc2D, trans, coords_bar);
 
          // get the weighted derivatives using finite difference method
          for (int n = 0; n < coords.Width(); ++n)
@@ -185,9 +194,124 @@ TEST_CASE("FiniteElement::Project_RevDiff reverse-mode differentiation",
             for (int di = 0; di < coords.Height(); ++di)
             {
                coords(di, n) += eps_fd;
-               el.Project(vc, trans, dofs_fd);
+               el.Project(vc2D, trans, dofs_fd);
                coords(di, n) -= 2.0*eps_fd;
-               el.Project(vc, trans, dofs_pert);
+               el.Project(vc2D, trans, dofs_pert);
+               dofs_fd -= dofs_pert;
+               dofs_fd *= 1.0/(2.0*eps_fd);
+               coords(di, n) += eps_fd;
+               double x_bar_fd = P_bar * dofs_fd;
+
+               REQUIRE(coords_bar(di, n) == Approx(x_bar_fd));
+            }
+         }
+      }
+   }
+
+   Mesh mesh3D(1, 1, 1, Element::TETRAHEDRON,
+             true /* gen. edges */, 1.0,
+             1.0, 1.0, true);
+   mesh3D.EnsureNodes();
+
+   if (dumpMesh)
+   {
+      std::ofstream mesh_ostream("finitelement-project-revdiff-tet-mesh.vtk");
+      mesh_ostream.precision(14);
+      int refine = 10;
+      mesh3D.PrintVTK(mesh_ostream, refine);
+   }
+
+   VectorFunctionCoefficient vc3D(3, func3D, func3DRevDiff);
+
+   for (int p = 1; p <= 4; ++p)
+   {
+      SECTION("Project_RT_RevDiff (tet) for degree p = " + std::to_string(p))
+      {
+         RT_FECollection fec(p, 3);
+         FiniteElementSpace fes(&mesh3D, &fec);
+
+         const FiniteElement &el = *fes.GetFE(0);
+         IsoparametricTransformation trans;
+         mesh3D.GetElementTransformation(0, &trans);
+
+         DenseMatrix &coords = trans.GetPointMat();
+         DenseMatrix coords_bar(coords.Height(), coords.Width());
+
+         const int dof = el.GetDof();
+
+         // P_bar is the vector contrated with the derivative of the projection
+         // the values are not important for this test
+         Vector P_bar(dof);
+         for (int i = 0; i < P_bar.Size(); ++i)
+         {
+            P_bar(i) = distribution(generator);
+         }
+
+         Vector dofs_fd(dof), dofs_pert(dof);
+      
+         // reverse-mode differentiation of projection
+         coords_bar = 0.0;
+         el.Project_RevDiff(P_bar, vc3D, trans, coords_bar);
+
+         // get the weighted derivatives using finite difference method
+         for (int n = 0; n < coords.Width(); ++n)
+         {
+            for (int di = 0; di < coords.Height(); ++di)
+            {
+               coords(di, n) += eps_fd;
+               el.Project(vc3D, trans, dofs_fd);
+               coords(di, n) -= 2.0*eps_fd;
+               el.Project(vc3D, trans, dofs_pert);
+               dofs_fd -= dofs_pert;
+               dofs_fd *= 1.0/(2.0*eps_fd);
+               coords(di, n) += eps_fd;
+               double x_bar_fd = P_bar * dofs_fd;
+
+               REQUIRE(coords_bar(di, n) == Approx(x_bar_fd));
+            }
+         }
+      }
+   }
+
+   for (int p = 1; p <= 4; ++p)
+   {
+      SECTION("Project_ND_RevDiff (tet) for degree p = " + std::to_string(p))
+      {
+         ND_FECollection fec(p, 3);
+         FiniteElementSpace fes(&mesh3D, &fec);
+
+         const FiniteElement &el = *fes.GetFE(0);
+         IsoparametricTransformation trans;
+         mesh3D.GetElementTransformation(0, &trans);
+
+         DenseMatrix &coords = trans.GetPointMat();
+         DenseMatrix coords_bar(coords.Height(), coords.Width());
+
+         const int dof = el.GetDof();
+
+         // P_bar is the vector contrated with the derivative of the projection
+         // the values are not important for this test
+         Vector P_bar(dof);
+         for (int i = 0; i < P_bar.Size(); ++i)
+         {
+            P_bar(i) = distribution(generator);
+         }
+
+         Vector dofs_fd(dof), dofs_pert(dof);
+      
+         // reverse-mode differentiation of projection
+         coords_bar = 0.0;
+         el.Project_RevDiff(P_bar, vc3D, trans, coords_bar);
+
+         // get the weighted derivatives using finite difference method
+         for (int n = 0; n < coords.Width(); ++n)
+         {
+            for (int di = 0; di < coords.Height(); ++di)
+            {
+               coords(di, n) += eps_fd;
+               el.Project(vc3D, trans, dofs_fd);
+               coords(di, n) -= 2.0*eps_fd;
+               el.Project(vc3D, trans, dofs_pert);
                dofs_fd -= dofs_pert;
                dofs_fd *= 1.0/(2.0*eps_fd);
                coords(di, n) += eps_fd;
