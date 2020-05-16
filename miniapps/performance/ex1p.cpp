b68643cbecb1820fb63ce5df4a28e1efa404b5e4
Veselin Dobrev <dobrev@llnl.gov>
2020-05-16 14:04:54 -0700

    In the performance miniapps, print the MFEM SIMD width.
    In the miniapps/performance makefile, print the auto-detected compiler
    and if that fails, the print the output used for auto-dection.

--- miniapps/performance/ex1p.cpp
+++ miniapps/performance/ex1p.cpp
@@ -144,6 +144,11 @@ int main(int argc, char *argv[])
       return 3;
    }
 
+   if (myid == 0)
+   {
+      cout << "\nMFEM SIMD width: " << MFEM_SIMD_BYTES << " bytes\n" << endl;
+   }
+
    // See class BasisType in fem/fe_coll.hpp for available basis types
    int basis = BasisType::GetType(basis_type[0]);
    if (myid == 0)
