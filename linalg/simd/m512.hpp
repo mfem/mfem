74a892bb781e85a3eaff0d8dfb29d8a1618442bd
Veselin Dobrev <dobrev@llnl.gov>
2020-05-16 13:21:13 -0700

    Add support for x86/x64 intrinsics with MSVC.

--- linalg/simd/m512.hpp
+++ linalg/simd/m512.hpp
@@ -15,7 +15,12 @@
 #ifdef __AVX512F__
 
 #include "../../config/tconfig.hpp"
+#if defined(__x86_64__)
 #include <x86intrin.h>
+#else // assiming MSVC with _M_X64 or _M_IX86
+#include <intrin.h>
+#endif
+
 
 namespace mfem
 {
