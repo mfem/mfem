9968706da001d1a8598d50a28614d454d51c504b
Yohann Dudouit <dudouit1@llnl.gov>
2020-05-18 14:03:32 -0700

    Fix some bugs

--- fem/bilinearform_ext.hpp
+++ fem/bilinearform_ext.hpp
@@ -119,11 +119,11 @@ public:
 
    /// TODO
    void Assemble();
-   void FormSystemMatrix(const Array<int> &ess_tdof_list, OperatorHandle &A);
-   void FormLinearSystem(const Array<int> &ess_tdof_list,
-                         Vector &x, Vector &b,
-                         OperatorHandle &A, Vector &X, Vector &B,
-                         int copy_interior = 0);
+   // void FormSystemMatrix(const Array<int> &ess_tdof_list, OperatorHandle &A);
+   // void FormLinearSystem(const Array<int> &ess_tdof_list,
+   //                       Vector &x, Vector &b,
+   //                       OperatorHandle &A, Vector &X, Vector &B,
+   //                       int copy_interior = 0);
    void Mult(const Vector &x, Vector &y) const;
    void MultTranspose(const Vector &x, Vector &y) const;
    void Update();
