bac7df083adf1373a8001ed1450a4a400e292971
Yohann Dudouit <dudouit1@llnl.gov>
2020-05-26 14:20:11 -0700

    Set LEGACYFULL to 0.

--- fem/bilinearform.hpp
+++ fem/bilinearform.hpp
@@ -31,7 +31,7 @@ enum class AssemblyLevel
 {
    /// Legacy fully assembled form, i.e. a global sparse matrix in MFEM,
    /// Hypre or PETSC format.
-   LEGACYFULL,
+   LEGACYFULL = 0,
    /// Fully assembled form, i.e. a global sparse matrix in MFEM format.
    FULL,
    /// Form assembled at element level, which computes and stores dense element
