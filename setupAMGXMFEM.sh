9e6a94f96c2073ceb9fcea31f2a64b8e0e05c861
Arturo Vargas <artv3.89@gmail.com>
2020-06-18 12:05:42 -0700

    clone with https

--- setupAMGXMFEM.sh
+++ setupAMGXMFEM.sh
@@ -6,13 +6,13 @@
 #cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/../../ ../ && make -j && make install &&
 
 cd ../ &&
-git clone --recursive git@github.com:hypre-space/hypre.git &&
+git clone --recursive https://github.com/hypre-space/hypre.git &&
 cd hypre/src/ && env ./configure  && make -j 16 &&
 cd ../../ &&
 wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz &&
 tar -zxvf metis-4.0.3.tar.gz && cd metis-4.0.3 &&
 make -j 3 && cd .. &&
 ln -s metis-4.0.3 metis-4.0 &&
-git clone --recursive git@github.com:NVIDIA/AMGX.git && cd AMGX &&
+git clone --recursive https://github.com/NVIDIA/AMGX.git && cd AMGX &&
 mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/../ ../ && make -j all && make install && cd ../../ &&
 cd mfem && make pcuda CUDA_ARCH=sm_70 MFEM_USE_AMGX=YES MFEM_USE_SIMD=NO -j
