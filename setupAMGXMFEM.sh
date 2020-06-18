60f1ec320a1bb86c6ff102351f982ec17fb7c48e
Arturo Vargas <artv3.89@gmail.com>
2020-06-18 12:02:13 -0700

    update script to use hypres github repo

--- setupAMGXMFEM.sh
+++ setupAMGXMFEM.sh
@@ -1,10 +1,14 @@
+#cd ../ &&
+#wget https://computing.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/download/hypre-2.10.0b.tar.gz &&
+#tar -zxvf hypre-2.10.0b.tar.gz &&
+#mv hypre-2.10.0b hypre &&
+#cd hypre/src/ && mkdir build && cd build &&
+#cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/../../ ../ && make -j && make install &&
+
 cd ../ &&
-wget https://computing.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/download/hypre-2.10.0b.tar.gz &&
-tar -zxvf hypre-2.10.0b.tar.gz &&
-mv hypre-2.10.0b hypre &&
-cd hypre/src/ && mkdir build && cd build &&
-cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/../../ ../ && make -j && make install &&
-cd ../../../ &&
+git clone --recursive git@github.com:hypre-space/hypre.git &&
+cd hypre/src/ && env ./configure  && make -j 16 &&
+cd ../../ &&
 wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz &&
 tar -zxvf metis-4.0.3.tar.gz && cd metis-4.0.3 &&
 make -j 3 && cd .. &&
