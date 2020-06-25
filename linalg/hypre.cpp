4f4a9f0cdc17ad3829dc5b2afe7147e2755cf13f
Veselin Dobrev <dobrev@llnl.gov>
2020-06-25 01:17:30 -0700

    Add support for 64bit HYPRE_Int in HypreParMatrixFromBlocks() and GatherBlockOffsetData().
    Add overflow check in GatherBlockOffsetData().
    
    In the SuperLURowLocMatrix constructor from HypreParMatrix,
    remove the check for the number of columns and add a note why it is
    not necessary at the moment.
    
    Add a compile-time check when building with SuperLU_DIST support
    that HYPRE_Int is int which is required by the current implementation.

--- linalg/hypre.cpp
+++ linalg/hypre.cpp
@@ -20,7 +20,6 @@
 #include <iomanip>
 #include <cmath>
 #include <cstdlib>
-#include <climits> // INT_MAX
 
 using namespace std;
 
@@ -1668,12 +1667,12 @@ HypreParMatrix * RAP(const HypreParMatrix * Rt, const HypreParMatrix *A,
 // Helper function for HypreParMatrixFromBlocks. Note that scalability to
 // extremely large processor counts is limited by the use of MPI_Allgather.
 void GatherBlockOffsetData(MPI_Comm comm, const int rank, const int nprocs,
-                           const int num_loc, Array<int> &offsets,
+                           const int num_loc, const Array<int> &offsets,
                            std::vector<int> &all_num_loc, const int numBlocks,
-                           std::vector<std::vector<int>> &blockProcOffsets,
-                           std::vector<int> &procOffsets,
+                           std::vector<std::vector<HYPRE_Int>> &blockProcOffsets,
+                           std::vector<HYPRE_Int> &procOffsets,
                            std::vector<std::vector<int>> &procBlockOffsets,
-                           int &firstLocal, int &globalNum)
+                           HYPRE_Int &firstLocal, HYPRE_Int &globalNum)
 {
    std::vector<std::vector<int>> all_block_num_loc(numBlocks);
 
@@ -1701,6 +1700,10 @@ void GatherBlockOffsetData(MPI_Comm comm, const int rank, const int nprocs,
    for (int i = 0; i < nprocs; ++i)
    {
       globalNum += all_num_loc[i];
+      if (rank == 0)
+      {
+         MFEM_VERIFY(globalNum >= 0, "overflow in global size");
+      }
       if (i < rank)
       {
          firstLocal += all_num_loc[i];
@@ -1809,14 +1812,14 @@ HypreParMatrix * HypreParMatrixFromBlocks(Array2D<HypreParMatrix*> &blocks,
 
    std::vector<int> all_num_loc_rows(nprocs);
    std::vector<int> all_num_loc_cols(nprocs);
-   std::vector<int> procRowOffsets(nprocs);
-   std::vector<int> procColOffsets(nprocs);
-   std::vector<std::vector<int>> blockRowProcOffsets(numBlockRows);
-   std::vector<std::vector<int>> blockColProcOffsets(numBlockCols);
+   std::vector<HYPRE_Int> procRowOffsets(nprocs);
+   std::vector<HYPRE_Int> procColOffsets(nprocs);
+   std::vector<std::vector<HYPRE_Int>> blockRowProcOffsets(numBlockRows);
+   std::vector<std::vector<HYPRE_Int>> blockColProcOffsets(numBlockCols);
    std::vector<std::vector<int>> procBlockRowOffsets(nprocs);
    std::vector<std::vector<int>> procBlockColOffsets(nprocs);
 
-   int first_loc_row, glob_nrows, first_loc_col, glob_ncols;
+   HYPRE_Int first_loc_row, glob_nrows, first_loc_col, glob_ncols;
    GatherBlockOffsetData(comm, rank, nprocs, num_loc_rows, rowOffsets,
                          all_num_loc_rows, numBlockRows, blockRowProcOffsets,
                          procRowOffsets, procBlockRowOffsets, first_loc_row,
@@ -1851,18 +1854,7 @@ HypreParMatrix * HypreParMatrixFromBlocks(Array2D<HypreParMatrix*> &blocks,
          }
          else
          {
-            {
-               hypre_ParCSRMatrix *parcsr_op = (hypre_ParCSRMatrix*)
-                                               const_cast<HypreParMatrix&>
-                                               (*(blocks(i, j)));
-               MFEM_ASSERT(parcsr_op != NULL, "const_cast failed");
-               csr_blocks(i, j) = hypre_MergeDiagAndOffd(parcsr_op);
-#if MFEM_HYPRE_VERSION >= 21600
-               MFEM_VERIFY(csr_blocks(i, j)->big_j != NULL ||
-                           csr_blocks(i, j)->num_cols < INT_MAX,"Number of "
-                           "columns is too large to store as an integer.");
-#endif
-            }
+            csr_blocks(i, j) = hypre_MergeDiagAndOffd(*blocks(i, j));
 
             for (int k = 0; k < csr_blocks(i, j)->num_rows; ++k)
             {
@@ -1907,13 +1899,16 @@ HypreParMatrix * HypreParMatrixFromBlocks(Array2D<HypreParMatrix*> &blocks,
                {
                   // Find the column process offset for the block.
 #if MFEM_HYPRE_VERSION >= 21600
-                  const int bcol = usingBigJ ? csr_blocks(i, j)->big_j[osk + l]
-                                   : csr_blocks(i, j)->j[osk + l];
+                  const HYPRE_Int bcol = usingBigJ ?
+                                         csr_blocks(i, j)->big_j[osk + l] :
+                                         csr_blocks(i, j)->j[osk + l];
 #else
-                  const int bcol = csr_blocks(i, j)->j[osk + l];
+                  const HYPRE_Int bcol = csr_blocks(i, j)->j[osk + l];
 #endif
                   int bcolproc = 0;
 
+                  // TODO: use binary search to find bcolproc, e.g. using
+                  //       std::upper_bound.
                   for (int p = 1; p < nprocs; ++p)
                   {
                      if (blockColProcOffsets[j][p] > bcol)
@@ -1958,11 +1953,12 @@ HypreParMatrix * HypreParMatrixFromBlocks(Array2D<HypreParMatrix*> &blocks,
    colStarts2[0] = first_loc_col;
    colStarts2[1] = first_loc_col + all_num_loc_cols[rank];
 
+   MFEM_VERIFY(HYPRE_AssumedPartitionCheck(),
+               "only 'assumed partition' mode is supported");
+
    return new HypreParMatrix(comm, num_loc_rows, glob_nrows, glob_ncols,
-                             (int *)opI.data(), (HYPRE_Int *)opJ.data(),
-                             (double *)data.data(),
-                             (HYPRE_Int *)rowStarts2.data(),
-                             (HYPRE_Int *)colStarts2.data());
+                             opI.data(), opJ.data(), data.data(),
+                             rowStarts2.data(), colStarts2.data());
 }
 
 void EliminateBC(HypreParMatrix &A, HypreParMatrix &Ae,
--- linalg/superlu.cpp
+++ linalg/superlu.cpp
@@ -24,6 +24,11 @@
 #error "SuperLUDist has been built with 64bit integers. This is not supported"
 #endif
 
+// For now, it is assumed that HYPRE_Int is int.
+#ifdef HYPRE_BIGINT
+#error "SuperLUDist support requires HYPRE_Int == int, for now."
+#endif
+
 using namespace std;
 
 namespace mfem
@@ -135,8 +140,10 @@ SuperLURowLocMatrix::SuperLURowLocMatrix( const HypreParMatrix & hypParMat )
    hypre_CSRMatrix * csr_op = hypre_MergeDiagAndOffd(parcsr_op);
    hypre_CSRMatrixSetDataOwner(csr_op,0);
 #if MFEM_HYPRE_VERSION >= 21600
-   MFEM_VERIFY(csr_blocks(i, j)->big_j != NULL || csr_op->num_cols < INT_MAX,
-               "SuperLU: number of columns is too large to store as an integer.");
+   // For now, this method assumes that HYPRE_Int is int. Also, csr_op->num_cols
+   // is of type HYPRE_Int, so if we want to check for big indices in
+   // csr_op->big_j, we'll have to check all entries and that check will only be
+   // necessary in HYPRE_MIXEDINT mode which is not supported at the moment.
    hypre_CSRMatrixBigJtoJ(csr_op);
 #endif
 
