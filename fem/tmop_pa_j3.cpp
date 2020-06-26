279938afe557b91ab9982fe04054d88f8a673424
camierjs <camier1@llnl.gov>
2020-06-25 18:08:35 -0700

    Check det(Jpr) > 0 @ GPU

---
+++ fem/tmop_pa_j3.cpp
@@ -0,0 +1,109 @@
+// Copyright (c) 2010-2020, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#include "tmop.hpp"
+#include "tmop_pa.hpp"
+#include "linearform.hpp"
+#include "tmop_tools.hpp"
+#include "../general/forall.hpp"
+#include "../linalg/kernels.hpp"
+#include "../linalg/dtensor.hpp"
+
+namespace mfem
+{
+
+MFEM_REGISTER_TMOP_KERNELS(int, CheckDetJpr_Kernel_3D,
+                           const int NE,
+                           const Array<double> &b_,
+                           const Array<double> &g_,
+                           const Vector &x_,
+                           const Vector &ones,
+                           Vector &DetJOk,
+                           const int d1d,
+                           const int q1d)
+{
+   constexpr int DIM = 3;
+   const int D1D = T_D1D ? T_D1D : d1d;
+   const int Q1D = T_Q1D ? T_Q1D : q1d;
+
+   const auto b = Reshape(b_.Read(), Q1D, D1D);
+   const auto g = Reshape(g_.Read(), Q1D, D1D);
+   const auto X = Reshape(x_.Read(), D1D, D1D, D1D, DIM, NE);
+
+   auto E = Reshape(DetJOk.Write(), Q1D, Q1D, Q1D, NE);
+
+   MFEM_FORALL_3D(e, NE, Q1D, Q1D, Q1D,
+   {
+      const int D1D = T_D1D ? T_D1D : d1d;
+      const int Q1D = T_Q1D ? T_Q1D : q1d;
+      constexpr int MQ1 = T_Q1D ? T_Q1D : T_MAX;
+      constexpr int MD1 = T_D1D ? T_D1D : T_MAX;
+
+      MFEM_SHARED double BG[2][MQ1*MD1];
+      MFEM_SHARED double DDD[3][MD1*MD1*MD1];
+      MFEM_SHARED double DDQ[6][MD1*MD1*MQ1];
+      MFEM_SHARED double DQQ[9][MD1*MQ1*MQ1];
+      MFEM_SHARED double QQQ[9][MQ1*MQ1*MQ1];
+
+      kernels::LoadX<MD1>(e,D1D,X,DDD);
+      kernels::LoadBG<MD1,MQ1>(D1D,Q1D,b,g,BG);
+
+      kernels::GradX<MD1,MQ1>(D1D,Q1D,BG,DDD,DDQ);
+      kernels::GradY<MD1,MQ1>(D1D,Q1D,BG,DDQ,DQQ);
+      kernels::GradZ<MD1,MQ1>(D1D,Q1D,BG,DQQ,QQQ);
+
+      MFEM_FOREACH_THREAD(qz,z,Q1D)
+      {
+         MFEM_FOREACH_THREAD(qy,y,Q1D)
+         {
+            MFEM_FOREACH_THREAD(qx,x,Q1D)
+            {
+               double J[9];
+               kernels::PullGradXYZ<MQ1>(qx,qy,qz, QQQ, J);
+               const double detJ = kernels::Det<3>(J);
+               E(qx,qy,qz,e) = (detJ <= 0.0) ? 0.0 : 1.0;
+            }
+         }
+      }
+   });
+   const double N = ones * ones;
+   const double D = DetJOk * ones;
+   return D < N ? 0 : 1;
+}
+
+int TMOPNewtonSolver::CheckDetJpr_3D(const FiniteElementSpace *fes,
+                                     const IntegrationRule &ir,
+                                     const Vector &X) const
+{
+   const ElementDofOrdering ordering = ElementDofOrdering::LEXICOGRAPHIC;
+   const Operator *R = fes->GetElementRestriction(ordering);
+   Vector XE(R->Height(), Device::GetDeviceMemoryType());
+   XE.UseDevice(true);
+   R->Mult(X, XE);
+
+   const DofToQuad &maps = fes->GetFE(0)->GetDofToQuad(ir, DofToQuad::TENSOR);
+   const int NE = fes->GetMesh()->GetNE();
+   const int NQ = ir.GetNPoints();
+   const int D1D = maps.ndof;
+   const int Q1D = maps.nqpt;
+   const int id = (D1D << 4 ) | Q1D;
+   const Array<double> &B = maps.B;
+   const Array<double> &G = maps.G;
+
+   Vector O(NE*NQ), E(NE*NQ);
+   E.UseDevice(true);
+   O.UseDevice(true);
+   O = 1.0;
+
+   MFEM_LAUNCH_TMOP_KERNEL(CheckDetJpr_Kernel_3D,id,NE,B,G,XE,O,E);
+}
+
+} // namespace mfem
