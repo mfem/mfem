68813167edc3afa99e58db4d5469b63b05a8add1
Tzanio <tzanio@llnl.gov>
2020-06-27 18:15:43 -0700

    Small adjustment in ceed-cuda skipped sample runs

--- examples/ex1p.cpp
+++ examples/ex1p.cpp
@@ -32,7 +32,7 @@
 //               mpirun -np 4 ex1p -pa -d occa-cuda
 //               mpirun -np 4 ex1p -pa -d raja-omp
 //               mpirun -np 4 ex1p -pa -d ceed-cpu
-//               * mpirun -np 4 ex1p -pa -d ceed-cuda
+//             * mpirun -np 4 ex1p -pa -d ceed-cuda
 //               mpirun -np 4 ex1p -pa -d ceed-cuda:/gpu/cuda/shared
 //               mpirun -np 4 ex1p -m ../data/beam-tet.mesh -pa -d ceed-cpu
 //
