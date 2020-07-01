bf24259fda81ca3b8e5e9b59f339ef5c4345fe75
Stowell, Mark L <stowell1@llnl.gov>
2020-07-01 15:21:02 -0700

    Gmsh hexahedron mapping

--- mesh/gmsh.cpp
+++ mesh/gmsh.cpp
@@ -45,6 +45,72 @@ int CartesianToGmshQuad(int idx_in[], int ref)
    }
 }
 
+int CartesianToGmshHex(int idx_in[], int ref)
+{
+   int i = idx_in[0];
+   int j = idx_in[1];
+   int k = idx_in[2];
+   // Do we lie on any of the edges
+   bool ibdr = (i == 0 || i == ref);
+   bool jbdr = (j == 0 || j == ref);
+   bool kbdr = (k == 0 || k == ref);
+   if (ibdr && jbdr && kbdr) // Vertex DOF
+   {
+      return (i ? (j ? (k ? 6 : 2) : (k ? 5 : 1)) :
+              (j ? (k ? 7 : 3) : (k ? 4 : 0)));
+   }
+   int offset = 8;
+   if (jbdr && kbdr) // Edge DOF on x-directed edge
+   {
+      return offset + (j ? (k ? 12*ref-12-i: 6*ref-6-i) :
+                       (k ? 8*ref-9+i: i-1));
+   }
+   else if (ibdr && kbdr) // Edge DOF on y-directed edge
+   {
+      return offset + (k ? (i ? 10*ref-11+j: 9*ref-10+j) :
+                       (i ? 3*ref-4+j: ref-2+j));
+   }
+   else if (ibdr && jbdr) // Edge DOF on z-directed edge
+   {
+      return offset + (i ? (j ? 6*ref-7+k: 4*ref-5+k) :
+                       (j ? 7*ref-8+k: 2*ref-3+k));
+   }
+   else if (ibdr) // Face DOF on x-directed face
+   {
+      int idx_out[2];
+      idx_out[0] = i ? j-1 : k-1;
+      idx_out[1] = i ? k-1 : j-1;
+      offset += (12 + (i ? 3 : 2) * (ref - 1)) * (ref - 1);
+      return offset + CartesianToGmshQuad(idx_out, ref-2);
+   }
+   else if (jbdr) // Face DOF on y-directed face
+   {
+      int idx_out[2];
+      idx_out[0] = j ? ref-i-1 : i-1;
+      idx_out[1] = j ? k-1 : k-1;
+      offset += (12 + (j ? 4 : 1) * (ref - 1)) * (ref - 1);
+      return offset + CartesianToGmshQuad(idx_out, ref-2);
+   }
+   else if (kbdr) // Face DOF on z-directed face
+   {
+      int idx_out[2];
+      idx_out[0] = k ? i-1 : j-1;
+      idx_out[1] = k ? j-1 : i-1;
+      offset += (12 + (k ? 5 : 0) * (ref - 1)) * (ref - 1);
+      return offset + CartesianToGmshQuad(idx_out, ref-2);
+   }
+   else // Recursive numbering for interior
+   {
+      int idx_out[3];
+      idx_out[0] = i-1;
+      idx_out[1] = j-1;
+      idx_out[2] = k-1;
+
+      offset += (12 + 6 * (ref - 1)) * (ref - 1);
+      return offset + CartesianToGmshHex(idx_out, ref-2);
+   }
+}
+
 void GmshHOSegmentMapping(int order, int *map)
 {
    map[0] = 0;
@@ -90,7 +156,22 @@ void GmshHOTetrahedronMapping(int order, int *map)
 {}
 
 void GmshHOHexahedronMapping(int order, int *map)
-{}
+{
+   int b[3];
+   int o = 0;
+   for (b[2]=0; b[2]<=order; b[2]++)
+   {
+      for (b[1]=0; b[1]<=order; b[1]++)
+      {
+         for (b[0]=0; b[0]<=order; b[0]++)
+         {
+            int o_gmsh = CartesianToGmshHex(b, order);
+            map[o] = o_gmsh;
+            o++;
+         }
+      }
+   }
+}
 
 void GmshHOWedgeMapping(int order, int *map)
 {}
