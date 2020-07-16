a0406460893d8682f52a972deafdd21978cecc2c
camierjs <camier1@llnl.gov>
2020-07-15 17:37:21 -0700

    Cleanup

--- miniapps/meshing/pmesh-optimizer.cpp
+++ miniapps/meshing/pmesh-optimizer.cpp
@@ -715,13 +715,13 @@ int main (int argc, char *argv[])
    a.GetGradient(x);
    he_nlf_integ->AssembleDiagonalPA(diag);
    dbg("diag: %.15e", diag*diag);
-   //std::cout << "PA:     " << diag.Size() << " " << diag.Norml1() << std::endl;
+   std::cout << "PA:     " << diag.Size() << " " << diag.Norml1() << std::endl;
 
    const SparseMatrix &s = a_fa.GetLocalGradient(x);
    Vector dsp;
    s.GetDiag(dsp);
    dbg("dsp:  %.15e", dsp*dsp);
-   //std::cout << "LEGACY: " << dsp.Size() << " " << dsp.Norml1() << std::endl;
+   std::cout << "LEGACY: " << dsp.Size() << " " << dsp.Norml1() << std::endl;
 
    MFEM_ABORT("test");
 
