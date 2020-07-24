5909a99b3e336eddc2559548dfd3a05c832485b4
camierjs <camier1@llnl.gov>
2020-07-24 16:32:30 -0700

    Debug H2D

--- general/mem_manager.cpp
+++ general/mem_manager.cpp
@@ -9,6 +9,8 @@
 // terms of the BSD-3 license. We welcome feedback and contributions, see file
 // CONTRIBUTING.md for details.
 
+#define MFEM_DEBUG_COLOR 201
+#include "debug.hpp"
 #include "forall.hpp"
 #include "mem_manager.hpp"
 
@@ -453,7 +455,10 @@ public:
    void AliasUnprotect(const void *ptr, size_t bytes)
    { MmuAllow(MmuAddrP(ptr), MmuLengthP(ptr, bytes)); }
    void *HtoD(void *dst, const void *src, size_t bytes)
-   { return std::memcpy(dst, src, bytes); }
+   {
+      dbg("0x%x",bytes);
+      return std::memcpy(dst, src, bytes);
+   }
    void *DtoD(void *dst, const void *src, size_t bytes)
    { return std::memcpy(dst, src, bytes); }
    void *DtoH(void *dst, const void *src, size_t bytes)
