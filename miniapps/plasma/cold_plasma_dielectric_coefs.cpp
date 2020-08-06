1667c89a4f2ba7220f982122e98e27cb2e8a70e2
stefanhenneking <stefan.henneking@gmail.com>
2020-08-06 13:39:10 -0500

    Use MFEM_VERIFY to avoid unused private field without debug mode.

--- miniapps/plasma/cold_plasma_dielectric_coefs.cpp
+++ miniapps/plasma/cold_plasma_dielectric_coefs.cpp
@@ -745,7 +745,7 @@ void SPDDielectricTensor::Eval(DenseMatrix &epsilon, ElementTransformation &T,
 PlasmaProfile::PlasmaProfile(Type type, const Vector & params)
    : type_(type), p_(params), x_(3)
 {
-   MFEM_ASSERT(params.Size() == np_[type],
+   MFEM_VERIFY(params.Size() == np_[type],
                "Incorrect number of parameters, " << params.Size()
                << ", for profile of type: " << type << ".");
 }
