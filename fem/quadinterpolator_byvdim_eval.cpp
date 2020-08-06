552bd17416ceb5c90500d0faff30bc0b1c15df6b
camierjs <camier1@llnl.gov>
2020-08-06 13:05:32 -0700

    Melding quadinterpolator and rename fem/tmop_pa_j*

--- fem/quadinterpolator_byvdim_eval.cpp
+++ fem/quadinterpolator_byvdim_eval.cpp
@@ -20,23 +20,25 @@
 namespace mfem
 {
 
-template<int T_VDIM = 0, int T_D1D = 0, int T_Q1D = 0, int T_NBZ = 0>
-static void D2QValues2D(const int NE,
-                        const Array<double> &b_,
-                        const Vector &x_,
-                        Vector &y_,
-                        const int vdim = 1,
-                        const int d1d = 0,
-                        const int q1d = 0)
+template<int T_VDIM = 0, int T_D1D = 0, int T_Q1D = 0, int T_NBZ = 0,
+         int MAX_D1D = 0, int MAX_Q1D = 0>
+static void EvalByVDim2D(const int NE,
+                         const double *b_,
+                         const double *x_,
+                         double *y_,
+                         const int vdim = 0,
+                         const int d1d = 0,
+                         const int q1d = 0)
 {
+   constexpr int NBZ = T_NBZ ? T_NBZ : 1;
+
    const int D1D = T_D1D ? T_D1D : d1d;
    const int Q1D = T_Q1D ? T_Q1D : q1d;
-   constexpr int NBZ = T_NBZ ? T_NBZ : 1;
    const int VDIM = T_VDIM ? T_VDIM : vdim;
 
-   auto b = Reshape(b_.Read(), Q1D, D1D);
-   auto x = Reshape(x_.Read(), D1D, D1D, VDIM, NE);
-   auto y = Reshape(y_.Write(), VDIM, Q1D, Q1D, NE);
+   const auto b = Reshape(b_, Q1D, D1D);
+   const auto x = Reshape(x_, D1D, D1D, VDIM, NE);
+   auto y = Reshape(y_, VDIM, Q1D, Q1D, NE);
 
    MFEM_FORALL_2D(e, NE, Q1D, Q1D, NBZ,
    {
@@ -46,22 +48,24 @@ static void D2QValues2D(const int NE,
       constexpr int MQ1 = T_Q1D ? T_Q1D : MAX_Q1D;
       constexpr int MD1 = T_D1D ? T_D1D : MAX_D1D;
       constexpr int NBZ = T_NBZ ? T_NBZ : 1;
-      const int zid = MFEM_THREAD_ID(z);
-      MFEM_SHARED double B[MQ1][MD1];
+      const int tidz = MFEM_THREAD_ID(z);
 
-      MFEM_SHARED double DDz[NBZ][MD1*MD1];
-      double (*DD)[MD1] = (double (*)[MD1])(DDz + zid);
+      MFEM_SHARED double s_B[MQ1*MD1];
+      DeviceTensor<2,double> B(s_B, Q1D, D1D);
 
-      MFEM_SHARED double DQz[NBZ][MD1*MQ1];
-      double (*DQ)[MQ1] = (double (*)[MQ1])(DQz + zid);
+      MFEM_SHARED double s_DD[NBZ][MD1*MD1];
+      DeviceTensor<2,double> DD((double*)(s_DD+tidz), MD1, MD1);
 
-      if (zid == 0)
+      MFEM_SHARED double s_DQ[NBZ][MD1*MQ1];
+      DeviceTensor<2,double> DQ((double*)(s_DQ+tidz), MD1, MQ1);
+
+      if (tidz == 0)
       {
          MFEM_FOREACH_THREAD(d,y,D1D)
          {
             MFEM_FOREACH_THREAD(q,x,Q1D)
             {
-               B[q][d] = b(q,d);
+               B(q,d) = b(q,d);
             }
          }
       }
@@ -69,11 +73,11 @@ static void D2QValues2D(const int NE,
 
       for (int c = 0; c < VDIM; c++)
       {
-         MFEM_FOREACH_THREAD(dy,y,D1D)
+         MFEM_FOREACH_THREAD(dx,x,D1D)
          {
-            MFEM_FOREACH_THREAD(dx,x,D1D)
+            MFEM_FOREACH_THREAD(dy,y,D1D)
             {
-               DD[dy][dx] = x(dx,dy,c,e);
+               DD(dx,dy) = x(dx,dy,c,e);
             }
          }
          MFEM_SYNC_THREAD;
@@ -81,12 +85,12 @@ static void D2QValues2D(const int NE,
          {
             MFEM_FOREACH_THREAD(qx,x,Q1D)
             {
-               double dq = 0.0;
+               double u = 0.0;
                for (int dx = 0; dx < D1D; ++dx)
                {
-                  dq += B[qx][dx] * DD[dy][dx];
+                  u += B(qx,dx) *  DD(dx,dy);
                }
-               DQ[dy][qx] = dq;
+               DQ(dy,qx) = u;
             }
          }
          MFEM_SYNC_THREAD;
@@ -94,12 +98,12 @@ static void D2QValues2D(const int NE,
          {
             MFEM_FOREACH_THREAD(qx,x,Q1D)
             {
-               double qq = 0.0;
+               double u = 0.0;
                for (int dy = 0; dy < D1D; ++dy)
                {
-                  qq += DQ[dy][qx] * B[qy][dy];
+                  u += DQ(dy,qx) * B(qy,dy);
                }
-               y(c,qx,qy,e) = qq;
+               y(c,qx,qy,e) = u;
             }
          }
          MFEM_SYNC_THREAD;
@@ -108,38 +112,41 @@ static void D2QValues2D(const int NE,
 }
 
 template<int T_VDIM = 0, int T_D1D = 0, int T_Q1D = 0,
-         int MAX_D = 0, int MAX_Q = 0>
-static void D2QValues3D(const int NE,
-                        const Array<double> &b_,
-                        const Vector &x_,
-                        Vector &y_,
-                        const int vdim = 1,
-                        const int d1d = 0,
-                        const int q1d = 0)
+         int MAX_D1D = 0, int MAX_Q1D = 0>
+static void EvalByVDim3D(const int NE,
+                         const double *b_,
+                         const double *x_,
+                         double *y_,
+                         const int vdim = 0,
+                         const int d1d = 0,
+                         const int q1d = 0)
 {
    const int D1D = T_D1D ? T_D1D : d1d;
    const int Q1D = T_Q1D ? T_Q1D : q1d;
    const int VDIM = T_VDIM ? T_VDIM : vdim;
 
-   auto b = Reshape(b_.Read(), Q1D, D1D);
-   auto x = Reshape(x_.Read(), D1D, D1D, D1D, VDIM, NE);
-   auto y = Reshape(y_.Write(), VDIM, Q1D, Q1D, Q1D, NE);
+   const auto b = Reshape(b_, Q1D, D1D);
+   const auto x = Reshape(x_, D1D, D1D, D1D, VDIM, NE);
+   auto y = Reshape(y_, VDIM, Q1D, Q1D, Q1D, NE);
 
    MFEM_FORALL_3D(e, NE, Q1D, Q1D, Q1D,
    {
       const int D1D = T_D1D ? T_D1D : d1d;
       const int Q1D = T_Q1D ? T_Q1D : q1d;
       const int VDIM = T_VDIM ? T_VDIM : vdim;
-      constexpr int MQ1 = T_Q1D ? T_Q1D : MAX_Q;
-      constexpr int MD1 = T_D1D ? T_D1D : MAX_D;
+      constexpr int MQ1 = T_Q1D ? T_Q1D : MAX_Q1D;
+      constexpr int MD1 = T_D1D ? T_D1D : MAX_D1D;
       constexpr int MDQ = (MQ1 > MD1) ? MQ1 : MD1;
       const int tidz = MFEM_THREAD_ID(z);
-      MFEM_SHARED double B[MQ1][MD1];
+
+      MFEM_SHARED double s_B[MQ1*MD1];
+      DeviceTensor<2,double> B(s_B, Q1D, D1D);
+
       MFEM_SHARED double sm0[MDQ*MDQ*MDQ];
       MFEM_SHARED double sm1[MDQ*MDQ*MDQ];
-      double (*X)[MD1][MD1]   = (double (*)[MD1][MD1]) sm0;
-      double (*DDQ)[MD1][MQ1] = (double (*)[MD1][MQ1]) sm1;
-      double (*DQQ)[MQ1][MQ1] = (double (*)[MQ1][MQ1]) sm0;
+      DeviceTensor<3,double> DDD(sm0, MD1, MD1, MD1);
+      DeviceTensor<3,double> DDQ(sm1, MD1, MD1, MQ1);
+      DeviceTensor<3,double> DQQ(sm0, MD1, MQ1, MQ1);
 
       if (tidz == 0)
       {
@@ -147,7 +154,7 @@ static void D2QValues3D(const int NE,
          {
             MFEM_FOREACH_THREAD(q,x,Q1D)
             {
-               B[q][d] = b(q,d);
+               B(q,d) = b(q,d);
             }
          }
       }
@@ -161,7 +168,7 @@ static void D2QValues3D(const int NE,
             {
                MFEM_FOREACH_THREAD(dx,x,D1D)
                {
-                  X[dz][dy][dx] = x(dx,dy,dz,c,e);
+                  DDD(dx,dy,dz) = x(dx,dy,dz,c,e);
                }
             }
          }
@@ -175,9 +182,9 @@ static void D2QValues3D(const int NE,
                   double u = 0.0;
                   for (int dx = 0; dx < D1D; ++dx)
                   {
-                     u += B[qx][dx] * X[dz][dy][dx];
+                     u += B(qx,dx) * DDD(dx,dy,dz);
                   }
-                  DDQ[dz][dy][qx] = u;
+                  DDQ(dz,dy,qx) = u;
                }
             }
          }
@@ -191,9 +198,9 @@ static void D2QValues3D(const int NE,
                   double u = 0.0;
                   for (int dy = 0; dy < D1D; ++dy)
                   {
-                     u += DDQ[dz][dy][qx] * B[qy][dy];
+                     u += DDQ(dz,dy,qx) * B(qy,dy);
                   }
-                  DQQ[dz][qy][qx] = u;
+                  DQQ(dz,qy,qx) = u;
                }
             }
          }
@@ -207,7 +214,7 @@ static void D2QValues3D(const int NE,
                   double u = 0.0;
                   for (int dz = 0; dz < D1D; ++dz)
                   {
-                     u += DQQ[dz][qy][qx] * B[qz][dz];
+                     u +=  DQQ(dz,qy,qx) * B(qz,dz);
                   }
                   y(c,qx,qy,qz,e) = u;
                }
@@ -218,79 +225,65 @@ static void D2QValues3D(const int NE,
    });
 }
 
-static void D2QValues(const FiniteElementSpace &fes,
-                      const DofToQuad *maps,
-                      const Vector &e_vec,
-                      Vector &q_val)
-{
-   const int dim = fes.GetMesh()->Dimension();
-   const int vdim = fes.GetVDim();
-   const int NE = fes.GetNE();
-   const int D1D = maps->ndof;
-   const int Q1D = maps->nqpt;
-   const int id = (vdim<<8) | (D1D<<4) | Q1D;
-
-   if (dim == 2)
-   {
-      switch (id)
-      {
-         case 0x124: return D2QValues2D<1,2,4,8>(NE, maps->B, e_vec, q_val);
-         case 0x136: return D2QValues2D<1,3,6,4>(NE, maps->B, e_vec, q_val);
-         case 0x148: return D2QValues2D<1,4,8,2>(NE, maps->B, e_vec, q_val);
-         case 0x224: return D2QValues2D<2,2,4,8>(NE, maps->B, e_vec, q_val);
-         case 0x234: return D2QValues2D<2,3,4,8>(NE, maps->B, e_vec, q_val);
-         case 0x236: return D2QValues2D<2,3,6,4>(NE, maps->B, e_vec, q_val);
-         case 0x248: return D2QValues2D<2,4,8,2>(NE, maps->B, e_vec, q_val);
-         default:
-         {
-            dbg("Using standard kernel #id 0x%x", id);
-            MFEM_VERIFY(D1D <= MAX_D1D, "Orders higher than " << MAX_D1D-1
-                        << " are not supported!");
-            MFEM_VERIFY(Q1D <= MAX_Q1D, "Quadrature rules with more than "
-                        << MAX_Q1D << " 1D points are not supported!");
-            D2QValues2D(NE, maps->B, e_vec, q_val, vdim, D1D, Q1D);
-            return;
-         }
-      }
-   }
-   if (dim == 3)
-   {
-      switch (id)
-      {
-         case 0x124: return D2QValues3D<1,2,4>(NE, maps->B, e_vec, q_val);
-         case 0x136: return D2QValues3D<1,3,6>(NE, maps->B, e_vec, q_val);
-         case 0x148: return D2QValues3D<1,4,8>(NE, maps->B, e_vec, q_val);
-         case 0x324: return D2QValues3D<3,2,4>(NE, maps->B, e_vec, q_val);
-         case 0x336: return D2QValues3D<3,3,6>(NE, maps->B, e_vec, q_val);
-         case 0x348: return D2QValues3D<3,4,8>(NE, maps->B, e_vec, q_val);
-         default:
-         {
-            constexpr int MD = 8;
-            constexpr int MQ = 8;
-            dbg("Using standard kernel #id 0x%x", id);
-            MFEM_VERIFY(D1D <= MD, "Orders higher than " << MD-1
-                        << " are not supported!");
-            MFEM_VERIFY(Q1D <= MQ, "Quadrature rules with more than " << MQ
-                        << " 1D points are not supported!");
-            D2QValues3D<0,0,0,MD,MQ>(NE, maps->B, e_vec, q_val, vdim, D1D, Q1D);
-            return;
-         }
-      }
-   }
-   mfem::out << "Unknown kernel 0x" << std::hex << id << std::endl;
-   MFEM_ABORT("Unknown kernel");
-}
-
 template<>
 void QuadratureInterpolator::Values<QVectorLayout::byVDIM>(
    const Vector &e_vec, Vector &q_val) const
 {
-   MFEM_VERIFY(q_layout == QVectorLayout::byVDIM, "");
-   if (fespace->GetNE() == 0) { return; }
-   const IntegrationRule &ir = *IntRule;
-   const DofToQuad::Mode mode = DofToQuad::TENSOR;
-   const DofToQuad &d2q = fespace->GetFE(0)->GetDofToQuad(ir, mode);
-   D2QValues(*fespace, &d2q, e_vec, q_val);
+   const int NE = fespace->GetNE();
+   if (NE == 0) { return; }
+   const int vdim = fespace->GetVDim();
+   const int dim = fespace->GetMesh()->Dimension();
+   const FiniteElement *fe = fespace->GetFE(0);
+   const IntegrationRule *ir =
+      IntRule ? IntRule : &qspace->GetElementIntRule(0);
+   const DofToQuad &maps = fe->GetDofToQuad(*ir, DofToQuad::TENSOR);
+   const int D1D = maps.ndof;
+   const int Q1D = maps.nqpt;
+   const double *B = maps.B.Read();
+   const double *X = e_vec.Read();
+   double *Y = q_val.Write();
+
+   const int id = (dim<<12) | (vdim<<8) | (D1D<<4) | Q1D;
+
+   switch (id)
+   {
+      case 0x2124: return EvalByVDim2D<1,2,4,8>(NE,B,X,Y);
+      case 0x2136: return EvalByVDim2D<1,3,6,4>(NE,B,X,Y);
+      case 0x2148: return EvalByVDim2D<1,4,8,2>(NE,B,X,Y);
+      case 0x2224: return EvalByVDim2D<2,2,4,8>(NE,B,X,Y);
+      case 0x2234: return EvalByVDim2D<2,3,4,8>(NE,B,X,Y);
+      case 0x2236: return EvalByVDim2D<2,3,6,4>(NE,B,X,Y);
+      case 0x2248: return EvalByVDim2D<2,4,8,2>(NE,B,X,Y);
+
+      case 0x3124: return EvalByVDim3D<1,2,4>(NE,B,X,Y);
+      case 0x3136: return EvalByVDim3D<1,3,6>(NE,B,X,Y);
+      case 0x3148: return EvalByVDim3D<1,4,8>(NE,B,X,Y);
+      case 0x3324: return EvalByVDim3D<3,2,4>(NE,B,X,Y);
+      case 0x3336: return EvalByVDim3D<3,3,6>(NE,B,X,Y);
+      case 0x3348: return EvalByVDim3D<3,4,8>(NE,B,X,Y);
+
+      default:
+      {
+         constexpr int MD1 = 8;
+         constexpr int MQ1 = 8;
+         dbg("Using standard kernel #id 0x%x", id);
+         MFEM_VERIFY(D1D <= MD1, "Orders higher than " << MD1-1
+                     << " are not supported!");
+         MFEM_VERIFY(Q1D <= MQ1, "Quadrature rules with more than "
+                     << MQ1 << " 1D points are not supported!");
+         if (dim == 2)
+         {
+            EvalByVDim2D<0,0,0,0,MD1,MQ1>(NE,B,X,Y,vdim,D1D,Q1D);
+         }
+         if (dim == 3)
+         {
+            EvalByVDim3D<0,0,0,MD1,MQ1>(NE,B,X,Y,vdim,D1D,Q1D);
+         }
+         return;
+      }
+   }
+   mfem::out << "Unknown kernel 0x" << std::hex << id << std::endl;
+   MFEM_ABORT("Kernel not supported yet");
 }
 
 } // namespace mfem
