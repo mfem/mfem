2a063c64d5ed2e2df39d59a31cdc5a667acc09e4
Yohann Dudouit <dudouit1@llnl.gov>
2020-08-13 00:32:47 -0700

    Reorganize read functions.

--- linalg/tensor/readmatrix.hpp
+++ linalg/tensor/readmatrix.hpp
@@ -13,15 +13,16 @@
 #define MFEM_TENSOR_READ_MATRIX
 
 #include "tensor.hpp"
-#include "../general/backends.hpp"
+#include "../../general/backends.hpp"
 #include "../dtensor.hpp"
+#include <utility>
 
 namespace mfem
 {
 
 // Functions to read a matrix, for example the dofs to quad matrix
-template<int P, int Q> MFEM_HOST_DEVICE inline
-const dTensor<Q,P>&& ReadMatrix(const DeviceTensor<2> &d_B)
+template<int Q, int P> MFEM_HOST_DEVICE inline
+const dTensor<Q,P>&& ReadMatrix(const DeviceBasis<Q,P> &d_B)
 {
    dTensor<Q,P> s_B;
    for (int p = 0; p < P; p++)
@@ -32,7 +33,7 @@ const dTensor<Q,P>&& ReadMatrix(const DeviceTensor<2> &d_B)
       }    
    }
    MFEM_SYNC_THREAD;
-   return s_B;
+   return std::move(s_B);
 }
 
 // Functions to read dofs to derivatives matrix
@@ -51,27 +52,76 @@ const Tensor<dTensor<Dim>,Q,P>&& ReadMatrix(const DeviceTensor<3> &d_G)
       }    
    }
    MFEM_SYNC_THREAD;
-   return s_G;
+   return std::move(s_G);
 }
 
-// Non-tensor read with VDIMxVDIM components
-template<int P, int VDIM> MFEM_HOST_DEVICE inline
-const Tensor<dTensor<VDIM,VDIM>,P>&& ReadMatrix(const DeviceTensor<4> &e_vec,
+// Non-tensor and 1D read with VDIMxVDIM components
+template<int D, int VDIM> MFEM_HOST_DEVICE inline
+const Tensor<dTensor<VDIM,VDIM>,D>&& ReadMatrix(const DeviceTensor<4> &e_vec,
                                                 const int e)
 {
-   Tensor<dTensor<VDIM,VDIM>,P> u;
+   Tensor<dTensor<VDIM,VDIM>,D> u;
    for (int w = 0; w < VDIM; w++)
    {   
       for (int h = 0; h < VDIM; h++)
       {
-         MFEM_FOREACH_THREAD(p,x,P)
+         MFEM_FOREACH_THREAD(d,x,D)
          {
-            u(p)(h,w) = e_vec(p, h, w, e);
+            u(d)(h,w) = e_vec(d, h, w, e);
          }
       }
    }
    MFEM_SYNC_THREAD;
-   return u;
+   return std::move(u);
+}
+
+// 3D tensor read with VDIMxVDIM components
+template <int D1d, int VDIM> MFEM_HOST_DEVICE inline
+const Tensor<dTensor<VDIM,VDIM>,D1d,D1d,D1d>&& ReadMatrix(const DeviceTensor<6> &e_vec,
+                                                          const int e)
+{
+   Tensor<dTensor<VDIM,VDIM>,D1d,D1d,D1d> u;
+   for (int w = 0; w < VDIM; w++)
+   {
+      for (int h = 0; h < VDIM; h++)
+      {
+         for (int dz = 0; dz < D1d; dz++)
+         {
+            MFEM_FOREACH_THREAD(dy,y,D1d)
+            {
+               MFEM_FOREACH_THREAD(dx,x,D1d)
+               {
+                  u(dx,dy,dz)(h,w) = e_vec(dx,dy,dz,h,w,e);
+               }
+            }
+         }
+      }
+   }
+   MFEM_SYNC_THREAD;
+   return std::move(u);
+}
+
+// 2D tensor read with VDIMxVDIM components
+template <int D1d, int VDIM> MFEM_HOST_DEVICE inline
+const Tensor<dTensor<VDIM,VDIM>,D1d,D1d>&& ReadMatrix(const DeviceTensor<5> &e_vec,
+                                                      const int e)
+{
+   Tensor<dTensor<VDIM,VDIM>,D1d,D1d> u;
+   for (int w = 0; w < VDIM; w++)
+   {
+      for (int h = 0; h < VDIM; h++)
+      {
+         MFEM_FOREACH_THREAD(dy,y,D1d)
+         {
+            MFEM_FOREACH_THREAD(dx,x,D1d)
+            {
+               u(dx,dy)(h,w) = e_vec(dx,dy,h,w,e);
+            }
+         }
+      }
+   }
+   MFEM_SYNC_THREAD;
+   return std::move(u);
 }
 
 } // namespace mfem
