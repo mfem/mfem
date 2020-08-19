ce3e27fc992ba3d9dad59c4d4b1aa26ee4172add
jeremylt <thompson.jeremy.luke@gmail.com>
2020-08-19 13:50:28 -0600

    minor style

--- fem/libceed/ceed.cpp
+++ fem/libceed/ceed.cpp
@@ -368,9 +368,9 @@ void InitCeedBasisAndRestriction(const FiniteElementSpace &fes,
    const int P = fe->GetDof();
    const int Q = irm.GetNPoints();
    const int nelem = mesh->GetNE();
-   CeedBasisKey basis_key (&fes, &irm, P, Q);
+   CeedBasisKey basis_key(&fes, &irm, P, Q);
    auto basis_itr = internal::ceed_basis_map.find(basis_key);
-   CeedRestrKey restr_key (&fes, P, nelem);
+   CeedRestrKey restr_key(&fes, P, nelem);
    auto restr_itr = internal::ceed_restr_map.find(restr_key);
 
    // Init or retreive key values
