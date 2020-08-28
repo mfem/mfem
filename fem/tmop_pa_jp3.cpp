08b8331ef5e45522f16cc3391341154d9d1b1557
Ketan Mittal <ketan.mittal@gmail.com>
2020-08-28 09:48:17 -0700

    refactor mindetj and checkdetj

--- fem/tmop_pa_jp3.cpp
+++ fem/tmop_pa_jp3.cpp
@@ -18,12 +18,12 @@
 namespace mfem
 {
 
-MFEM_REGISTER_TMOP_KERNELS(int, CheckDetJpr_Kernel_3D,
+MFEM_REGISTER_TMOP_KERNELS(double, MinDetJpr_Kernel_3D,
                            const int NE,
                            const Array<double> &b_,
                            const Array<double> &g_,
                            const Vector &x_,
-                           Vector &DetJOk,
+                           Vector &DetJ,
                            const int d1d,
                            const int q1d)
 {
@@ -35,7 +35,7 @@ MFEM_REGISTER_TMOP_KERNELS(int, CheckDetJpr_Kernel_3D,
    const auto g = Reshape(g_.Read(), Q1D, D1D);
    const auto X = Reshape(x_.Read(), D1D, D1D, D1D, DIM, NE);
 
-   auto E = Reshape(DetJOk.Write(), Q1D, Q1D, Q1D, NE);
+   auto E = Reshape(DetJ.Write(), Q1D, Q1D, Q1D, NE);
 
    MFEM_FORALL_3D(e, NE, Q1D, Q1D, Q1D,
    {
@@ -65,93 +65,12 @@ MFEM_REGISTER_TMOP_KERNELS(int, CheckDetJpr_Kernel_3D,
             {
                double J[9];
                kernels::PullGrad<MQ1>(qx,qy,qz, QQQ, J);
-               const double detJ = kernels::Det<3>(J);
-               E(qx,qy,qz,e) = (detJ <= 0.0) ? 0.0 : 1.0;
+               E(qx,qy,qz,e) = kernels::Det<3>(J);
             }
          }
       }
    });
-   const double N = DetJOk.Size();
-   const double D = DetJOk * DetJOk;
-   return D < N ? 0 : 1;
-}
 
-int TMOPNewtonSolver::CheckDetJpr_3D(const FiniteElementSpace *fes,
-                                     const Vector &X) const
-{
-   const ElementDofOrdering ordering = ElementDofOrdering::LEXICOGRAPHIC;
-   const Operator *R = fes->GetElementRestriction(ordering);
-   Vector XE(R->Height(), Device::GetDeviceMemoryType());
-   XE.UseDevice(true);
-   R->Mult(X, XE);
-
-   const DofToQuad &maps = fes->GetFE(0)->GetDofToQuad(ir, DofToQuad::TENSOR);
-   const int NE = fes->GetMesh()->GetNE();
-   const int NQ = ir.GetNPoints();
-   const int D1D = maps.ndof;
-   const int Q1D = maps.nqpt;
-   const int id = (D1D << 4 ) | Q1D;
-   const Array<double> &B = maps.B;
-   const Array<double> &G = maps.G;
-
-   Vector E(NE*NQ);
-   E.UseDevice(true);
-
-   MFEM_LAUNCH_TMOP_KERNEL(CheckDetJpr_Kernel_3D,id,NE,B,G,XE,E);
-}
-
-MFEM_REGISTER_TMOP_KERNELS(double, MinDetJpr_Kernel_3D,
-                           const int NE,
-                           const Array<double> &b_,
-                           const Array<double> &g_,
-                           const Vector &x_,
-                           Vector &DetJ,
-                           const int d1d,
-                           const int q1d)
-{
-   constexpr int DIM = 3;
-   const int D1D = T_D1D ? T_D1D : d1d;
-   const int Q1D = T_Q1D ? T_Q1D : q1d;
-
-   const auto b = Reshape(b_.Read(), Q1D, D1D);
-   const auto g = Reshape(g_.Read(), Q1D, D1D);
-   const auto X = Reshape(x_.Read(), D1D, D1D, D1D, DIM, NE);
-
-   auto D = Reshape(DetJ.Write(), Q1D, Q1D, Q1D, NE);
-
-   MFEM_FORALL_3D(e, NE, Q1D, Q1D, Q1D,
-   {
-      const int D1D = T_D1D ? T_D1D : d1d;
-      const int Q1D = T_Q1D ? T_Q1D : q1d;
-      constexpr int MQ1 = T_Q1D ? T_Q1D : T_MAX;
-      constexpr int MD1 = T_D1D ? T_D1D : T_MAX;
-
-      MFEM_SHARED double BG[2][MQ1*MD1];
-      MFEM_SHARED double DDD[3][MD1*MD1*MD1];
-      MFEM_SHARED double DDQ[6][MD1*MD1*MQ1];
-      MFEM_SHARED double DQQ[9][MD1*MQ1*MQ1];
-      MFEM_SHARED double QQQ[9][MQ1*MQ1*MQ1];
-
-      kernels::LoadX<MD1>(e,D1D,X,DDD);
-      kernels::LoadBG<MD1,MQ1>(D1D,Q1D,b,g,BG);
-
-      kernels::GradX<MD1,MQ1>(D1D,Q1D,BG,DDD,DDQ);
-      kernels::GradY<MD1,MQ1>(D1D,Q1D,BG,DDQ,DQQ);
-      kernels::GradZ<MD1,MQ1>(D1D,Q1D,BG,DQQ,QQQ);
-
-      MFEM_FOREACH_THREAD(qz,z,Q1D)
-      {
-         MFEM_FOREACH_THREAD(qy,y,Q1D)
-         {
-            MFEM_FOREACH_THREAD(qx,x,Q1D)
-            {
-               double Jpr[9];
-               kernels::PullGrad<MQ1>(qx,qy,qz, QQQ, Jpr);
-               D(qx,qy,qz,e) = kernels::Det<3>(Jpr);
-            }
-         }
-      }
-   });
    return DetJ.Min();
 }
 
@@ -178,4 +97,12 @@ double TMOPNewtonSolver::MinDetJpr_3D(const FiniteElementSpace *fes,
 
    MFEM_LAUNCH_TMOP_KERNEL(MinDetJpr_Kernel_3D,id,NE,B,G,XE,E);
 }
+
+int TMOPNewtonSolver::CheckDetJpr_3D(const FiniteElementSpace *fes,
+                                     const Vector &X) const
+{
+   double DetJMin = MinDetJpr_3D(fes, X);
+   return (DetJMin <= 0.) ? 0 : 1;
+}
+
 } // namespace mfem
