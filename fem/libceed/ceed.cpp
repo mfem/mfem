b03d1766306f7aed12f98b5d493e6d7b32fa0cca
Yohann Dudouit <dudouit1@llnl.gov>
2020-09-11 10:24:39 -0700

    move things around

--- fem/libceed/ceed.cpp
+++ fem/libceed/ceed.cpp
@@ -77,8 +77,6 @@ void CeedPAAssemble(const CeedPAOperator& op,
    mfem::Mesh *mesh = fes.GetMesh();
    CeedInt nqpts, nelem = mesh->GetNE();
    CeedInt dim = mesh->SpaceDimension(), vdim = fes.GetVDim();
-   CeedMemType mem;
-   CeedGetPreferredMemType(ceed, &mem);
 
    mesh->EnsureNodes();
    InitCeedBasisAndRestriction(fes, irm, ceed, &ceedData.basis, &ceedData.restr);
@@ -97,6 +95,7 @@ void CeedPAAssemble(const CeedPAOperator& op,
 
    CeedVectorCreate(ceed, mesh->GetNodes()->Size(), &ceedData.node_coords);
    CeedScalar *nodes_ptr;
+   CeedMemType mem;
    CeedGetPreferredMemType(ceed, &mem);
    if ( Device::Allows(Backend::DEVICE_MASK) && mem==CEED_MEM_DEVICE )
    {
