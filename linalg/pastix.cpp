29e6d7ef9d1709245b815274a50f445f12e2e47d
Josh Essman <essman1@llnl.gov>
2020-09-18 16:54:54 -0500

    fix: use hypre local to global row mappings

--- linalg/pastix.cpp
+++ linalg/pastix.cpp
@@ -16,33 +16,9 @@
 
 #include "pastix.hpp"
 
+// #include <unistd.h>
+
 namespace mfem {
-
-static inline spm_int_t
-spm_scatter_create_loc2glob( spmatrix_t *spm, spm_int_t baseval )
-{
-    spm_int_t i, size, begin, end, *loc2glob;
-    int       clustnum, clustnbr;
-
-    clustnum = spm->clustnum;
-    clustnbr = spm->clustnbr;
-
-    size  = spm->gN / clustnbr;
-    begin = size *  clustnum    + spm_imin( clustnum,   spm->gN % clustnbr );
-    end   = size * (clustnum+1) + spm_imin( clustnum+1, spm->gN % clustnbr );
-    size  = end - begin;
-
-    spm->n        = size;
-    spm->loc2glob = (spm_int_t*)malloc( size * sizeof(spm_int_t) );
-    loc2glob = spm->loc2glob;
-
-    for ( i=begin; i<end; i++, loc2glob++ )
-    {
-        *loc2glob = i+baseval;
-    }
-
-    return size;
-}
 
 PastixSparseMatrix::PastixSparseMatrix(const HypreParMatrix & hypParMat)
 {
@@ -77,10 +53,17 @@ PastixSparseMatrix::PastixSparseMatrix(const HypreParMatrix & hypParMat)
   spmAlloc(&matrix_);
 
   // TODO: memcpy?
-  for (int i = 0; i <csr->num_rows; i++) {
+  for (int i = 0; i < csr->num_rows + 1; i++) {
     matrix_.rowptr[i] = (csr->i)[i];
   }
 
+  #if MFEM_HYPRE_VERSION >= 21600
+   // For now, this method assumes that HYPRE_Int is int. Also, csr_op->num_cols
+   // is of type HYPRE_Int, so if we want to check for big indices in
+   // csr_op->big_j, we'll have to check all entries and that check will only be
+   // necessary in HYPRE_MIXEDINT mode which is not supported at the moment.
+   hypre_CSRMatrixBigJtoJ(csr);
+  #endif
   for (int i = 0; i < csr->num_nonzeros; i++) {
     matrix_.colptr[i] = (csr->j)[i];
     (static_cast<double*>(matrix_.values))[i] = (csr->data)[i];
@@ -90,10 +73,38 @@ PastixSparseMatrix::PastixSparseMatrix(const HypreParMatrix & hypParMat)
   MPI_Comm_size( MPI_COMM_WORLD, &matrix_.clustnbr ); // Number of MPI nodes
   MPI_Comm_rank( MPI_COMM_WORLD, &matrix_.clustnum ); // Rank of MPI node
 
-  spm_scatter_create_loc2glob(&matrix_, 0);
+  // {
+  //   volatile int i = 0;
+  //   printf("sudo gdb -p %d <- RUN THIS\n", getpid());
+  //   fflush(stdout);
+  //   while (0 == i)
+  //       sleep(5);
+  // }
+  matrix_.loc2glob = (spm_int_t*)malloc( matrix_.n * sizeof(spm_int_t) );
+  const int start = par_csr->row_starts[matrix_.clustnum];
+  for (int i = 0; i < matrix_.n; i++)
+  {
+    printf("Setting loc2glob[%d] to %d on rank %d\n", i, i + start, matrix_.clustnum);
+    matrix_.loc2glob[i] = start + i;
+  }
 
   spmatrix_t* spmg = spmGather( &matrix_, 0 );
-  
+
+  // if (matrix_.clustnum == 0)
+  // {
+  //   printf("Attempting to open file\n");
+  //   FILE* fp = fopen("test.matrix", "w");
+  //   printf("Could open file\n");
+  //   spmPrint(&matrix_, fp);
+  //   fclose(fp);
+  // }
+
+  if (matrix_.clustnum == 0 && spmg)
+  {
+    spmExit(spmg);
+    free(spmg);
+  }
+  const_cast<HypreParMatrix*>(&hypParMat)->Print("hp.matrix");
 }
 
 PastixSparseMatrix::~PastixSparseMatrix()
