b74d9c6bd9d560acce812440250ae04a10a13b64
Yohann Dudouit <dudouit1@llnl.gov>
2020-09-21 11:19:13 -0700

    Change GetFullAssemblySparseMatrix to return a rectangular SparseMatrix.

--- fem/prestriction.hpp
+++ fem/prestriction.hpp
@@ -34,13 +34,23 @@ public:
                         L2FaceValues m = L2FaceValues::DoubleValued);
    void Mult(const Vector &x, Vector &y) const;
    /** Fill the I array of SparseMatrix corresponding to the sparsity pattern
-       given by this ParL2FaceRestriction. */
-   void FillI(SparseMatrix &mat, SparseMatrix &face_mat) const;
+       given by this L2FaceRestriction. */
+   virtual void FillI(SparseMatrix &mat) const;
+   /** Fill the I array of SparseMatrix corresponding to the sparsity pattern
+       given by this L2FaceRestriction. @a mat contains the interior dofs
+       contribution, the @a face_mat contains the shared dofs contribution.*/
+   virtual void FillI(SparseMatrix &mat, SparseMatrix &face_mat) const;
    /** Fill the J and Data arrays of SparseMatrix corresponding to the sparsity
-       pattern given by this ParL2FaceRestriction, and the values of ea_data. */
-   void FillJAndData(const Vector &ea_data,
-                     SparseMatrix &mat,
-                     SparseMatrix &face_mat) const;
+       pattern given by this L2FaceRestriction, and the values of ea_data. */
+   virtual void FillJAndData(const Vector &ea_data,
+                             SparseMatrix &mat) const;
+   /** Fill the J and Data arrays of SparseMatrix corresponding to the sparsity
+       pattern given by this L2FaceRestriction, and the values of ea_data.
+       @a mat contains the interior dofs contribution, the @a face_mat contains
+       the shared dofs contribution.*/
+   virtual void FillJAndData(const Vector &ea_data,
+                             SparseMatrix &mat,
+                             SparseMatrix &face_mat) const;
 };
 
 }
