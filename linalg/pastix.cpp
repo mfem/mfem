1cc2107cea9c4c2c9b0b682f1502dd4800b0257c
Josh Essman <essman1@llnl.gov>
2020-09-22 13:31:47 -0500

    docs: Doxygen comments for PastixSolver

--- linalg/pastix.cpp
+++ linalg/pastix.cpp
@@ -20,7 +20,7 @@
 
 namespace mfem {
 
-PastixSparseMatrix::PastixSparseMatrix(const HypreParMatrix & hypParMat)
+PastixSparseMatrix::PastixSparseMatrix(const HypreParMatrix & hypParMat) : comm_(hypParMat.GetComm())
 {
   const hypre_ParCSRMatrix* par_csr = hypParMat;
 
@@ -64,20 +64,44 @@ PastixSparseMatrix::PastixSparseMatrix(const HypreParMatrix & hypParMat)
   #else
   int* jptr = csr->j;
   #endif
+
   for (int i = 0; i < csr->num_nonzeros; i++) {
     matrix_.colptr[i] = static_cast<int>(jptr[i]);
     (static_cast<double*>(matrix_.values))[i] = (csr->data)[i];
   }
 
-  matrix_.comm = hypParMat.GetComm();
-  MPI_Comm_size( MPI_COMM_WORLD, &matrix_.clustnbr ); // Number of MPI nodes
-  MPI_Comm_rank( MPI_COMM_WORLD, &matrix_.clustnum ); // Rank of MPI node
+  int num_procs = 0;
+  int rank = 0;
+  MPI_Comm_size(comm_, &num_procs);
+  MPI_Comm_rank(comm_, &rank);
 
-  matrix_.loc2glob = (spm_int_t*)malloc( matrix_.n * sizeof(spm_int_t) );
-  const int start = par_csr->row_starts[matrix_.clustnum];
-  for (int i = 0; i < matrix_.n; i++)
+  matrix_.comm = comm_;
+  matrix_.clustnbr = num_procs;
+  matrix_.clustnum = rank;
+
+  // Only build the loc2glob mapping if multi-process
+  if (num_procs > 1)
   {
-    matrix_.loc2glob[i] = start + i;
+    matrix_.loc2glob = (spm_int_t*)malloc( matrix_.n * sizeof(spm_int_t) );
+    const int start = par_csr->row_starts[rank];
+    for (int i = 0; i < matrix_.n; i++)
+    {
+      matrix_.loc2glob[i] = start + i;
+    }
+  }
+
+  // // Convert to IJV first, as distributed CSR -> CSC is not supported
+  // // i.e. CSR -> IJV -> CSC
+  // if (spmConvert(SpmIJV, &matrix_) != 0)
+  // {
+  //   MFEM_WARNING("Failed to convert CSR to IJV");
+  // }
+
+  spmatrix_t checked_matrix;
+  int rc = spmCheckAndCorrect(&matrix_, &checked_matrix);
+  if ( rc != 0 ) {
+      spmExit(&matrix_);
+      matrix_ = checked_matrix;
   }
 
   hypre_CSRMatrixDestroy(csr);
@@ -93,20 +117,21 @@ void PastixSparseMatrix::Mult(const Vector &x, Vector &y) const
   spmMatVec(SpmNoTrans, 1.0, &matrix_, static_cast<const double*>(x), 0.0, static_cast<double*>(y));
 }
 
-PastixSolver::PastixSolver(MPI_Comm comm) : comm_(comm)
+PastixSolver::PastixSolver()
 {
   pastixInitParam(integer_params_, double_params_);
+  integer_params_[IPARM_ORDERING] = PastixOrderMetis;
   // Hold off on initializing until parameters have been set by the user
 }
 
 PastixSolver::~PastixSolver()
 {
   if(pastix_data_ != nullptr) {
-    pastixFinalize( &pastix_data_ );
+    pastixFinalize(&pastix_data_);
   }
 }
 
-void PastixSolver::Mult( const Vector& x, Vector& y) const
+void PastixSolver::Mult(const Vector& b, Vector& x) const
 {
   MFEM_ASSERT(matrix_ != nullptr,
                "PastixSolver Error: The operator must be set before"
@@ -115,24 +140,32 @@ void PastixSolver::Mult( const Vector& x, Vector& y) const
   // Only solving for one RHS vector
   const int nrhs = 1;
   // The solution is stored in place of the right-hand-side vector
-  y = x;
-  pastix_task_solve(pastix_data_, nrhs, y, spm.n);
+  x = b;
+  pastix_task_solve(pastix_data_, nrhs, x, spm.n);
+
+  // Apply iterative refinement
+  // The refinement may use b as a scratchpad so it needs to be copied
+  Vector scratchpad_b = b;
+  pastix_task_refine(pastix_data_, spm.n, nrhs, scratchpad_b, spm.n, x, spm.n);
 }
 
 void PastixSolver::SetOperator(const Operator& op)
 {
-  // We assume the options have been set, so now we can initialize
-  pastixInit( &pastix_data_, comm_, integer_params_, double_params_);
 
   matrix_ = dynamic_cast<const PastixSparseMatrix*>(&op);
   if (matrix_ == nullptr)
   {
     MFEM_ABORT("PastixSolver::SetOperator: Must be PastixSparseMatrix");
   }
+
+  // We assume the options have been set, so now we can initialize
+  pastixInit( &pastix_data_, matrix_->GetComm(), integer_params_, double_params_);
+
   width = op.Width();
   height = op.Height();
   const spmatrix_t& spm = matrix_->InternalData();
-  pastix_task_analyze(pastix_data_, &spm);
+  // This is also non-modifying, fixed in pastix@master
+  pastix_task_analyze(pastix_data_, const_cast<spmatrix_t*>(&spm));
   // Non-modifying
   pastix_task_numfact(pastix_data_, const_cast<spmatrix_t*>(&spm));
 }
--- linalg/pastix.hpp
+++ linalg/pastix.hpp
@@ -9,6 +9,14 @@
 // terms of the BSD-3 license. We welcome feedback and contributions, see file
 // CONTRIBUTING.md for details.
 
+/**
+ * @file pastix.hpp
+ *
+ * @brief This file contains a RAII wrapper for the PaStiX sparse matrix class
+ * and an associated PaStiX direct solver wrapper
+ */
+
+
 #ifndef MFEM_PASTIX
 #define MFEM_PASTIX
 
@@ -26,33 +34,101 @@
 namespace mfem
 {
 
+/**
+ * @brief A memory-safe wrapper for INRIA's Sparse Matrix Package
+ */
 class PastixSparseMatrix : public Operator
 {
   public:
+    /**
+     * @brief Constructs a distributed sparse PaStiX matrix
+     * @param[in] hypParMat The HYPRE distributed matrix to 
+     * @b copy into the PaStiX matrix structure
+     * @note The source matrix is not modified in any way
+     */
     PastixSparseMatrix(const HypreParMatrix & hypParMat);
+
+    /**
+     * @brief De-allocates all heap-allocated memory in the 
+     * sparse matrix structure
+     */
     ~PastixSparseMatrix();
 
+    /**
+     * @brief Matrix-vector multiplication
+     * @param[in] x The vector operand
+     * @param[out] y The result A*x, where A is the calling object
+     */
     void Mult(const Vector &x, Vector &y) const override;
 
+    /**
+     * @brief Returns a reference to the underlying structure
+     */
     spmatrix_t& InternalData() {return matrix_;}
     const spmatrix_t& InternalData() const {return matrix_;}
+
+    /**
+     * @brief Returns the internal MPI communicator
+     */
+    // MPI_Comm GetComm() const {return matrix_.comm;}
+    MPI_Comm GetComm() const {return comm_;}
+
   private:
+    /**
+     * @brief The underlying matrix structure
+     * @see spmatrix_t
+     */
     spmatrix_t matrix_;
+    MPI_Comm comm_;
 };
 
+/**
+ * @brief A wrapper for the PaStiX direct solver that conforms to the
+ * mfem::Solver interface
+ */
 class PastixSolver : public Solver
 {
   public:
-    PastixSolver(MPI_Comm comm);
+    /**
+     * @brief Constructs a new solver object
+     */
+    PastixSolver();
 
     ~PastixSolver();
 
-    void Mult( const Vector& x, Vector& y) const override;
+    /**
+     * @brief Solves the system Ax=b for x
+     * @param[in] b The right-hand-side vector
+     * @param[out] x The solution vector
+     * @pre The matrix A has been configured with SetOperator
+     */
+    void Mult( const Vector& b, Vector& x) const override;
 
+    /**
+     * @brief Sets the operator A for the solver
+     * @param[in] op The system matrix A
+     * @pre op must be of derived type PastixSparseMatrix
+     */
     void SetOperator(const Operator& op) override;
 
+    // Set various solver options. Refer to PaStiX documentation for details.
+
+    /**
+     * @brief Sets the verbosity (print level) for the solver
+     * @param[in] verb Verbosity level
+     * @note Options are PastixVerboseNot = 0 (nothing),
+     * PastixVerboseNo = 1 (default), and PastixVerboseYes = 2 (extended)
+     */
+    void SetVerbosity(const pastix_verbose_e verb) {integer_params_[IPARM_VERBOSE] = verb;}
+
+    /**
+     * @brief Sets the iterative refinement method to use
+     * @param[in] refine The method
+     * @note Options are PastixRefineGMRES, PastixRefineCG, PastixRefineSR, PastixRefineBiCGSTAB
+     */
+    void SetIterativeRefinementMethod(const pastix_refine_e refine) {integer_params_[IPARM_REFINEMENT] = refine;}
+
   private:
-    MPI_Comm      comm_;
     pastix_int_t    integer_params_[IPARM_SIZE];
     double          double_params_[DPARM_SIZE];
     pastix_data_t  *pastix_data_ = nullptr;
