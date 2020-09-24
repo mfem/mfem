05c81ea16031891c5ddcaad5803e0a4a4baa4b3c
Yohann Dudouit <dudouit1@llnl.gov>
2020-09-24 10:21:05 -0700

    Rename Assemble to Setup.

--- fem/nonlinearform_ext.hpp
+++ fem/nonlinearform_ext.hpp
@@ -25,7 +25,7 @@ protected:
    NonlinearForm *n; ///< Not owned
 public:
    NonlinearFormExtension(NonlinearForm *form);
-   virtual void Assemble() = 0;
+   virtual void Setup() = 0;
 };
 
 /// Data and methods for partially-assembled nonlinear forms
@@ -37,7 +37,7 @@ protected:
    const Operator *elem_restrict_lex; // Not owned
 public:
    PANonlinearFormExtension(NonlinearForm*);
-   void Assemble();
+   void Setup();
    void Mult(const Vector &x, Vector &y) const;
 };
 
@@ -50,7 +50,7 @@ protected:
    const Operator *elem_restrict_lex; // Not owned
 public:
    MFNonlinearFormExtension(NonlinearForm*);
-   void Assemble();
+   void Setup();
    void Mult(const Vector &x, Vector &y) const;
 };
 
