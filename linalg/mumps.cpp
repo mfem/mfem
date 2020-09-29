430b11ca59957850b7fab76f963dbce2da362434
psocratis <petrides1@llnl.gov>
2020-09-28 17:47:08 -0700

    cleanup

--- linalg/mumps.cpp
+++ linalg/mumps.cpp
@@ -16,11 +16,6 @@
 
 #include "mumps.hpp"
 
-// mumps headers
-#define USE_COMM_WORLD -987654
-using namespace std;
-
-
 namespace mfem
 {
 
@@ -32,31 +27,62 @@ MUMPSSolver::~MUMPSSolver()
 {
    id->job=-2;
    dmumps_c(id);
+   delete [] J;
+   delete [] I;
 }
 
 void MUMPSSolver::SetParameters()
 {
-   id->ICNTL(1) = -1; // output messages
-   id->ICNTL(2) = -1; // Diagnosting printing
-   id->ICNTL(3) = -1;  // Global info on host
-   id->ICNTL(4) =  0; // Level of error printing
-   id->ICNTL(5) =  0; //inpute matrix format (distributed)
-   id->ICNTL(9) =  1; // Use A or A^T
-   id->ICNTL(10) = 0; // Iterative refinement (disabled)
-   id->ICNTL(11) = 0; // Error analysis-statistics (disabled)
-   id->ICNTL(13) = 0; // Use of ScaLAPACK (Parallel factorization on root)
-   id->ICNTL(14) =
-      20; // Percentage increase of estimated workspace (default = 20%)
-   id->ICNTL(16) = 0; // Number of OpenMP threads (default)
-   id->ICNTL(18) = 3; // Matrix input format (distributed)
-   id->ICNTL(19) = 0; // Schur complement (no Schur complement matrix returned)
-   id->ICNTL(20) = (dist_rhs) ? 10 :
-                   0; // RHS input format (distributed or only on host)
-   id->ICNTL(21) = (dist_sol) ? 1  :
-                   0; // Sol input format (distributed or only on host)
-   id->ICNTL(22) = 0; // Out of core factorization and solve (disabled)
-   id->ICNTL(23) =
-      0; // Maximum size of working memory (default = based on estimates)
+   // output messages
+   id->ICNTL(1) = -1;
+
+   // Diagnosting printing
+   id->ICNTL(2) = -1;
+
+   // Global info on host
+   id->ICNTL(3) = -1;
+
+   // Level of error printing
+   id->ICNTL(4) =  0;
+
+   //inpute matrix format (distributed)
+   id->ICNTL(5) =  0;
+
+   // Use A or A^T
+   id->ICNTL(9) =  1;
+
+   // Iterative refinement (disabled)
+   id->ICNTL(10) = 0;
+
+   // Error analysis-statistics (disabled)
+   id->ICNTL(11) = 0;
+
+   // Use of ScaLAPACK (Parallel factorization on root)
+   id->ICNTL(13) = 0;
+
+   // Percentage increase of estimated workspace (default = 20%)
+   id->ICNTL(14) = 20;
+
+   // Number of OpenMP threads (default)
+   id->ICNTL(16) = 0;
+
+   // Matrix input format (distributed)
+   id->ICNTL(18) = 3;
+
+   // Schur complement (no Schur complement matrix returned)
+   id->ICNTL(19) = 0;
+
+   // RHS input format (distributed or only on host)
+   id->ICNTL(20) = (dist_rhs) ? 10 : 0;
+
+   // Sol input format (distributed or only on host)
+   id->ICNTL(21) = (dist_sol) ? 1  : 0;
+
+   // Out of core factorization and solve (disabled)
+   id->ICNTL(22) = 0;
+
+   // Max size of working memory (default = based on estimates)
+   id->ICNTL(23) = 0;
 }
 
 void MUMPSSolver::Init()
@@ -66,8 +92,7 @@ void MUMPSSolver::Init()
 
    hypre_ParCSRMatrix * parcsr_op = (hypre_ParCSRMatrix *)
                                     const_cast<HypreParMatrix&>(*APtr);
-
-   csr_op = hypre_MergeDiagAndOffd(parcsr_op);
+   hypre_CSRMatrix * csr_op = hypre_MergeDiagAndOffd(parcsr_op);
 #if MFEM_HYPRE_VERSION >= 21600
    hypre_CSRMatrixBigJtoJ(csr_op);
 #endif
@@ -86,22 +111,25 @@ void MUMPSSolver::Init()
    {
       for (int j = Iptr[i]; j<Iptr[i+1]; j++)
       {
-         // "tdof offsets" can be determined by parcsr_op -> rowstarts
+         // Global I and J indices in 1-based index (for fortran)
          I[k] = row_start + i + 1;
-         J[k] = Jptr[k]+1; // This can be avoided
+         J[k] = Jptr[k]+1;
          k++;
       }
    }
 
    // new MUMPS object
    id = new DMUMPS_STRUC_C;
-   // Initialize a MUMPS instance. Use MPI_COMM_WORLD
-   id->comm_fortran=USE_COMM_WORLD;
+
+   // C to Fortran communicator
+   id->comm_fortran = (MUMPS_INT) MPI_Comm_c2f(comm);
 
    // Host is involved in computation
    id->par=1;
+
    // Unsymmetric matrix
    id->sym=0;
+
    // Mumps init
    id->job=-1;
    dmumps_c(id);
@@ -109,9 +137,8 @@ void MUMPSSolver::Init()
    SetParameters(); // Set MUMPS default parameters
 
    id->n = parcsr_op->global_num_rows;
-   // on all procs
    id->nnz_loc = nnz;
-   id->irn_loc = I; // Distributed rows array
+   id->irn_loc = I; // Distributed row array
    id->jcn_loc = J; // Distributed column array
    id->a_loc = csr_op->data; // Distributed data array
 
@@ -123,19 +150,17 @@ void MUMPSSolver::Init()
    id->job=2;
    dmumps_c(id);
 
-
-   if (!(dist_rhs && dist_sol)) // if at least one is on host
+   if (!dist_rhs || !dist_sol) // if at least one is on host
    {
       if (myid == 0)
       {
          rhs_glob.SetSize(parcsr_op->global_num_rows); rhs_glob = 0.0;
          recv_counts.SetSize(numProcs);
-         displs.SetSize(numProcs);
       }
       MPI_Gather(&n_loc,1,MPI_INT,recv_counts,1,MPI_INT,0,comm);
       if (myid == 0)
       {
-         displs[0] = 0;
+         displs.SetSize(numProcs); displs[0] = 0;
          for (int k=0; k<numProcs-1; k++)
          {
             displs[k+1] = displs[k] + recv_counts[k];
@@ -154,19 +179,20 @@ void MUMPSSolver::Init()
 
    if (dist_sol)
    {
-      row_starts.resize(numProcs);
-      MPI_Allgather(&row_start,1,MPI_INT,&row_starts[0],1,MPI_INT,comm);
+      row_starts.SetSize(numProcs);
+      MPI_Allgather(&row_start,1,MPI_INT,row_starts,1,MPI_INT,comm);
       sol_loc.SetSize(id->INFO(23));
-      isol_loc = new int[id->INFO(23)];
+      isol_loc.SetSize(id->INFO(23));
    }
+   delete csr_op;
 }
 
 void MUMPSSolver::Mult( const Vector & x, Vector & y ) const
 {
-   // cases for the RHS
+   // cases for the load vector
    if (dist_rhs)
    {
-      // distribute RHS
+      // distributed
       id->nloc_rhs = x.Size();
       id->lrhs_loc = x.Size();
       id->rhs_loc = x.GetData();
@@ -174,7 +200,7 @@ void MUMPSSolver::Mult( const Vector & x, Vector & y ) const
    }
    else
    {
-      // RHS gathered on the host
+      // on host
       MPI_Gatherv(x.GetData(),x.Size(),MPI_DOUBLE,rhs_glob.GetData(),
                   recv_counts,displs,MPI_DOUBLE,0,comm);
       if (myid == 0)
@@ -183,35 +209,30 @@ void MUMPSSolver::Mult( const Vector & x, Vector & y ) const
       }
    }
 
-   // cases for the SOL
+   // cases for the solution vector
    if (dist_sol)
    {
+      // distributed
       id->sol_loc = sol_loc.GetData();
       id->lsol_loc = id->INFO(23);
-      id->isol_loc = isol_loc;
+      id->isol_loc = const_cast<int*>(isol_loc.GetData());
    }
    else
    {
-      // if sol on host on the host
+      // on host
       if (myid == 0)
       {
          id->rhs = rhs_glob.GetData();
       }
    }
 
-
+   // Solve
    id->job = 3;
    dmumps_c(id);
 
    if (dist_sol)
    {
-      // sol_loc.Print();
-      Array<int> temp(sol_loc.Size());
-      for (int i = 0; i<sol_loc.Size(); i++)
-      {
-         temp[i] = isol_loc[i]-1;
-      }
-      RedistributeSol(temp, sol_loc, y);
+      RedistributeSol(isol_loc, sol_loc, y);
    }
    else
    {
@@ -222,24 +243,20 @@ void MUMPSSolver::Mult( const Vector & x, Vector & y ) const
 
 void MUMPSSolver::SetOperator( const Operator & op )
 {
-   // Verify that we have a compatible operator
+   // Verify that the operator is compatible
    APtr = dynamic_cast<const HypreParMatrix*>(&op);
    if ( APtr == NULL )
    {
-      mfem_error("MUMPSSolver::SetOperator : not HypreParMatrix!");
+      mfem_error("MUMPSSolver::SetOperator: not HypreParMatrix!");
    }
    height = op.Height();
    width  = op.Width();
    this->Init();
-
-   Array<int> test;
-
 }
 
-int MUMPSSolver::GetRowRank(int i, const std::vector<int> & row_starts_) const
+int MUMPSSolver::GetRowRank(int i, const Array<int> & row_starts_) const
 {
-   int size = row_starts_.size();
-   if (size == 1) { return 0; }
+   if (row_starts_.Size() == 1) { return 0; }
    auto up=std::upper_bound(row_starts_.begin(), row_starts_.end(),i);
    return std::distance(row_starts_.begin(),up)-1;
 }
@@ -250,23 +267,22 @@ void MUMPSSolver::RedistributeSol(const Array<int> & row_map, const Vector & x,
    MFEM_VERIFY(row_map.Size() == x.Size(), "Inconcistent sizes");
    int size = x.Size();
 
-
-   // workspace for MPI_ALLtoALL
-   Array<int> send_count(numProcs); send_count = 0;
-   Array<int> send_displ(numProcs); send_displ = 0;
-   Array<int> recv_count(numProcs); recv_count = 0;
-   Array<int> recv_displ(numProcs); recv_displ = 0;
-
    // compute send_count
+   Array<int> send_count(numProcs); send_count = 0;
    for (int i = 0; i<size; i++)
    {
-      int j = row_map[i];
+      int j = row_map[i]-1; //fix to 0-based indexing
       int row_rank = GetRowRank(j,row_starts);
-      send_count[row_rank]++; // the dof value
+      send_count[row_rank]++; // both for val and global index
    }
 
    // compute recv_count
+   Array<int> recv_count(numProcs);
    MPI_Alltoall(send_count,1,MPI_INT,recv_count,1,MPI_INT,comm);
+
+   // compute offsets
+   Array<int> send_displ(numProcs); send_displ[0] = 0;
+   Array<int> recv_displ(numProcs); recv_displ[0] = 0;
    for (int k=0; k<numProcs-1; k++)
    {
       send_displ[k+1] = send_displ[k] + send_count[k];
@@ -282,7 +298,7 @@ void MUMPSSolver::RedistributeSol(const Array<int> & row_map, const Vector & x,
    // Fill in send buffers
    for (int i = 0; i<size; i++)
    {
-      int j = row_map[i];
+      int j = row_map[i]-1; //fix to 0-based indexing
       int row_rank = GetRowRank(j,row_starts);
       int k = send_displ[row_rank] + soffs[row_rank];
       sendbuf_index[k] = j;
@@ -302,12 +318,9 @@ void MUMPSSolver::RedistributeSol(const Array<int> & row_map, const Vector & x,
    for (int i = 0; i<rbuff_size; i++)
    {
       int local_index = recvbuf_index[i] - row_start;
-      double val = recvbuf_value[i];
-      y(local_index) = val;
+      y(local_index) = recvbuf_value[i];
    }
 }
-
-
 
 } // mfem namespace
 
--- linalg/mumps.hpp
+++ linalg/mumps.hpp
@@ -32,12 +32,6 @@ public:
    // Constructor with MPI_Comm parameter.
    MUMPSSolver( MPI_Comm comm_ );
 
-   // Constructor with HypreParMatrix Object.
-   MUMPSSolver( HypreParMatrix & A);
-
-   // Default destructor.
-   ~MUMPSSolver( void );
-
    // Factor and solve the linear system y = Op^{-1} x.
    void Mult( const Vector & x, Vector & y ) const;
 
@@ -45,48 +39,69 @@ public:
    void SetOperator( const Operator & op );
 
    void UseDistributedRHS(bool dist_rhs_) { dist_rhs = dist_rhs_; }
+
    void UseDistributedSol(bool dist_sol_) { dist_sol = dist_sol_; }
 
+   // Default destructor.
+   ~MUMPSSolver();
+
 private:
+
+   MPI_Comm      comm;
+
+   int           numProcs;
+
+   int           myid;
+
+   // Distributed matrix workspace for COO-storage format
+   const HypreParMatrix * APtr;
+
+   int row_start;
+
+   int n_loc;
+
+   int * I;
+
+   int * J;
+
+   Vector rhs_glob;
+
+   Vector sol_loc;
+
+   Array<int> row_starts;
+   Array<int> recv_counts;
+
+   Array<int> displs;
+
+   Array<int> irhs_loc;
+
+   Array<int> isol_loc;
+
+   // MUMPS workspace
    // macro s.t. indices match MUMPS documentation
 #define ICNTL(I) icntl[(I)-1]
 #define INFO(I) info[(I)-1]
+
+   DMUMPS_STRUC_C * id;
+
    void SetParameters();
+
    void Init();
-   int GetRowRank(int i, const std::vector<int> & row_starts_) const;
+
+   int GetRowRank(int i, const Array<int> & row_starts_) const;
+
    void RedistributeSol(const Array<int> & tdof_map, const Vector & x,
                         Vector &y) const;
+
    // flag for distributed rhs
    bool dist_rhs = false;
    // flag for distributed sol
    bool dist_sol = false;
 
-   std::vector<int> row_starts;
 
 
-protected:
-   MPI_Comm      comm;
-   int           numProcs;
-   int           myid;
-   const HypreParMatrix * APtr;
 
-   hypre_CSRMatrix *csr_op;
-   int n_global;
-   int n_loc;
-   // coordinate format storage
-   int * I;
-   int * J;
-   double * data;
 
-   DMUMPS_STRUC_C * id;
-
-   int row_start;
-   Vector rhs_glob;
-   Array<int> recv_counts;
-   Array<int> displs;
-   Array<int> irhs_loc;
-   Vector sol_loc;
-   int * isol_loc;
 
 
 }; // mfem::MUMPSSolver class
