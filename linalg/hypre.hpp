e8ff92fe3dbc473f6a13c19f0388e1905696d475
Will Pazner <will.e.p@gmail.com>
2020-10-05 11:31:52 -0700

    Remove HypreBoomerAMG::Mult from header

--- linalg/hypre.hpp
+++ linalg/hypre.hpp
@@ -1160,7 +1160,6 @@ public:
    virtual HYPRE_PtrToParSolverFcn SolveFcn() const
    { return (HYPRE_PtrToParSolverFcn) HYPRE_BoomerAMGSolve; }
 
-   virtual void Mult (const HypreParVector &b, HypreParVector &x) const;
    using HypreSolver::Mult;
 
    virtual ~HypreBoomerAMG();
