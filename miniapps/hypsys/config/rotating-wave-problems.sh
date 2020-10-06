bb584f04f8d6ce622f86ccbfb83e9cea1a2b00a6
Hennes Hajduk <hhajduk@o-ray.mathematik.tu-dortmund.de>
2020-10-06 16:51:01 +0200

    minor

--- miniapps/hypsys/config/rotating-wave-problems.sh
+++ miniapps/hypsys/config/rotating-wave-problems.sh
@@ -2,7 +2,7 @@ EXEC="mpirun -np 7 phypsys"
 MESH=data/inline-4quad.mesh
 
 # KPP
-# $EXEC -p 2 -c 1 -vf 100 -tf 0.25 -s 3 -dt 0.0005 -r 9 -o 0 -m $MESH -es 0
+# $EXEC -p 2 -c 1 -vf 100 -tf 0.25 -s 3 -dt 0.00025 -r 9 -o 0 -m $MESH -es 0
 $EXEC -p 2 -c 1 -vf 100 -tf 0.25 -s 3 -dt 0.002 -r 6 -o 0 -m $MESH -es 0
 $EXEC -p 2 -c 1 -vf 100 -tf 0.25 -s 3 -dt 0.0008 -r 5 -o 1 -m $MESH -es 0
 $EXEC -p 2 -c 1 -vf 100 -tf 0.25 -s 3 -dt 0.0008 -r 5 -o 1 -m $MESH -es 1
