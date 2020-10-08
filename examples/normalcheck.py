1d4b7f5be4f13c19d05929829d1b5d21e779911c
Andrew T. Barker <barker29@llnl.gov>
2020-10-08 16:14:44 -0700

    Try a diffusion integrator; but this certainly needs boundary conditions.

--- examples/normalcheck.py
+++ examples/normalcheck.py
@@ -17,39 +17,40 @@ def check_vectors(filea="elimination.vector",
     return np.linalg.norm(veca - vecb) / np.linalg.norm(vecb)
 
 
+def run(fd, t, mesh="../data/square-disc-p3.mesh", diffusion=False):
+    commandline = ["./ex-normal",
+                   "--reltol", "1.e-10",
+                   "--mesh", mesh]
+    if diffusion:
+        commandline.append("--diffusion")
+    if t[0] == "p":
+        commandline = commandline + ["--penalty", t[1:]]
+    elif t == "elimination":
+        commandline = commandline + ["--elimination"]
+    p = subprocess.Popen(commandline, stdout=fd, stderr=fd)
+    p.communicate()
+
+
 def main():
     with open("normalcheck.log", "w") as fd:
         for mesh in ["../data/square-disc-p3.mesh",
                      "icf.mesh",
                      "sphere_hex27.mesh"]:
             print(mesh)
-            p = subprocess.Popen(["./ex-normal",
-                                  "--reltol", "1.e-10",
-                                  "--mesh", mesh],
-                                 stdout=fd, stderr=fd)
-            p.communicate()
-            p = subprocess.Popen(["./ex-normal",
-                                  "--reltol", "1.e-10",
-                                  "--elimination",
-                                  "--mesh", mesh],
-                                 stdout=fd, stderr=fd)
-            p.communicate()
-            print("  ", check_vectors())
+            for d in [False]:
+                print("  diffusion:", d)
+                run(fd, "schur", mesh=mesh, diffusion=d)
+                run(fd, "elimination", mesh=mesh, diffusion=d)
+                print("    ", check_vectors())
 
 
-def penalty():
+def penalty(diffusion=False):
     print("Check penalty sanity, error should scale like 1/penalty:")
     with open("penalty.log", "w") as fd:
-        p = subprocess.Popen(["./ex-normal",
-                              "--reltol", "1.e-10"],
-                             stdout=fd, stderr=fd)
-        p.communicate()
+        run(fd, "schur")
+        # run(fd, "elimination")
         for penalty in [1.0, 1.e+2, 1.e+4, 1.e+6]:
-            p = subprocess.Popen(["./ex-normal",
-                                  "--reltol", "1.e-10",
-                                  "--penalty", str(penalty)],
-                                 stdout=fd, stderr=fd)
-            p.communicate()
+            run(fd, "p" + str(penalty), diffusion=diffusion)
             print("  penalty parameter:", penalty, ", error:",
                   check_vectors(filea="penalty.vector"))
 
@@ -57,3 +58,4 @@ def penalty():
 if __name__ == "__main__":
     main()
     penalty()
+    # penalty(True)
