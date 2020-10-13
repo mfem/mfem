a7039439a7671a41ca75552bbbee6f9b9d5665f8
Yohann Dudouit <dudouit1@llnl.gov>
2020-10-13 15:37:05 -0700

    Update comment.

--- fem/libceed/ceed.hpp
+++ fem/libceed/ceed.hpp
@@ -84,7 +84,6 @@ struct CeedData
    {
       CeedOperatorDestroy(&build_oper);
       CeedOperatorDestroy(&oper);
-      CeedElemRestrictionDestroy(&restr_i);
       CeedQFunctionDestroy(&apply_qfunc);
       CeedQFunctionDestroy(&build_qfunc);
       CeedQFunctionContextDestroy(&build_ctx);
@@ -103,7 +102,6 @@ struct CeedData
       else if (coeff_type==CeedCoeff::Quad)
       {
          CeedQuadCoeff* c = static_cast<CeedQuadCoeff*>(coeff);
-         CeedElemRestrictionDestroy(&c->restr);
          CeedVectorDestroy(&c->coeffVector);
          delete c;
       }
@@ -226,8 +224,10 @@ void RemoveCeedBasisAndRestriction(const FiniteElementSpace *fes);
 void InitCeedVector(const Vector &v, CeedVector &cv);
 
 /// Initialize a strided CeedElemRestriction
-void InitCeedStridedRestriction(CeedInt nelem, CeedInt nqpts, CeedInt qdatasize,
-                                const CeedInt *strides, CeedElemRestriction *restr);
+void InitCeedStridedRestriction(const FiniteElementSpace &fes,
+                                CeedInt nelem, CeedInt nqpts, CeedInt qdatasize,
+                                const CeedInt *strides,
+                                CeedElemRestriction *restr);
 
 /// Initialize a CeedBasis and a CeedElemRestriction
 void InitCeedBasisAndRestriction(const FiniteElementSpace &fes,
