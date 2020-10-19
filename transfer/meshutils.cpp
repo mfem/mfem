292fb0fa94bc634067f1eb4d9add38b0187cddaf
zulianp <patrick.zulian@gmail.com>
2020-10-19 11:34:43 +0200

    applied style and removed prints

--- transfer/meshutils.cpp
+++ transfer/meshutils.cpp
@@ -5,43 +5,46 @@
 #include <iostream>
 
 
-namespace mfem {
+namespace mfem
+{
 
-   Element * NewElem(const int type, const int *cells_data, const int attr)
+Element * NewElem(const int type, const int *cells_data, const int attr)
+{
+   switch (type)
    {
-      switch (type)
-      {
-         case Geometry::TRIANGLE:      return new Triangle(cells_data, attr);
-         case Geometry::TETRAHEDRON:   return new Tetrahedron(cells_data, attr);
-         case Geometry::SQUARE:        return new Quadrilateral(cells_data, attr);
-         case Geometry::CUBE:          return new Hexahedron(cells_data, attr);
+      case Geometry::TRIANGLE:      return new Triangle(cells_data, attr);
+      case Geometry::TETRAHEDRON:   return new Tetrahedron(cells_data, attr);
+      case Geometry::SQUARE:        return new Quadrilateral(cells_data, attr);
+      case Geometry::CUBE:          return new Hexahedron(cells_data, attr);
 
-         default: {
-            assert(false && "unknown type");
-            mfem::err <<  "NewElem: unknown type " << type << std::endl;
-            return nullptr;
-         }
+      default:
+      {
+         assert(false && "unknown type");
+         mfem::err <<  "NewElem: unknown type " << type << std::endl;
+         return nullptr;
       }
    }
+}
 
-   void Finalize(Mesh &mesh, const bool generate_edges)
+void Finalize(Mesh &mesh, const bool generate_edges)
+{
+   //based on the first element
+   int type = mesh.GetElement(0)->GetGeometryType();
+
+   switch (type)
    {
-      //based on the first element
-      int type = mesh.GetElement(0)->GetGeometryType();
+      case Geometry::TRIANGLE:      return mesh.FinalizeTriMesh(generate_edges);
+      case Geometry::SQUARE:        return mesh.FinalizeQuadMesh(generate_edges);
+      case Geometry::CUBE:          return mesh.FinalizeHexMesh(generate_edges);
+      case Geometry::TETRAHEDRON:   return mesh.FinalizeTetMesh(generate_edges);
 
-      switch (type)
+      default:
       {
-         case Geometry::TRIANGLE:      return mesh.FinalizeTriMesh(generate_edges);
-         case Geometry::SQUARE:        return mesh.FinalizeQuadMesh(generate_edges);
-         case Geometry::CUBE:          return mesh.FinalizeHexMesh(generate_edges);
-         case Geometry::TETRAHEDRON:   return mesh.FinalizeTetMesh(generate_edges);
-
-         default: {
-            assert(false && "unknown type");
-            mfem::err <<  "Finalize: unknown type " << type << std::endl;
-            return;
-         }
+         assert(false && "unknown type");
+         mfem::err <<  "Finalize: unknown type " << type << std::endl;
+         return;
       }
    }
+}
 
 }
