0590c89b80e3e2ab48e56c22fd523008ac9bcafa
Andrew T. Barker <barker29@llnl.gov>
2020-10-19 17:02:29 -0700

    Basic unit test of very simple 3 by 3 matrix.

---
+++ tests/unit/linalg/test_constrainedsolver.cpp
@@ -0,0 +1,136 @@
+// Copyright (c) 2010-2020, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#include "mfem.hpp"
+#include "unit_tests.hpp"
+
+using namespace mfem;
+
+#ifdef MFEM_USE_MPI
+
+class IdentitySolver : public Solver
+{
+public:
+   IdentitySolver(int n) : Solver(n) { }
+   void Mult(const Vector& x, Vector& y) const { y = x; }
+   void SetOperator(const Operator& op) { }
+};
+
+class SimpleSaddle
+{
+public:
+   SimpleSaddle(double alpha, double beta);
+   ~SimpleSaddle();
+
+   void Schur(Vector& serr, Vector& lerr);
+   void Penalty(double pen, Vector& serr, Vector& lerr);
+   void Elimination(Vector &serr, Vector& lerr);
+
+private:
+   SparseMatrix A, B;
+   HypreParMatrix * hA;
+   Vector rhs, sol, lambda;
+   double truex, truey, truelambda;
+};
+
+SimpleSaddle::SimpleSaddle(double alpha, double beta)
+   :
+   A(2, 2), B(1, 2), rhs(2), sol(2), lambda(1)
+{
+   truex = 0.5 * alpha - 0.5 * beta;
+   truey = -0.5 * alpha + 0.5 * beta;
+   truelambda = 0.5 * (alpha + beta);
+
+   A.Add(0, 0, 1.0);
+   A.Add(1, 1, 1.0);
+   A.Finalize();
+   B.Add(0, 0, 1.0);
+   B.Add(0, 1, 1.0);
+   B.Finalize();
+
+   int row_starts[2] = {0, 2};
+   hA = new HypreParMatrix(MPI_COMM_WORLD, 2, row_starts, &A);
+   hA->CopyRowStarts();
+
+   rhs(0) = alpha;
+   rhs(1) = beta;
+}
+
+SimpleSaddle::~SimpleSaddle()
+{
+   delete hA;
+}
+
+void SimpleSaddle::Schur(Vector& serr, Vector& lerr)
+{
+   ConstrainedSolver solver(*hA, B);
+   IdentitySolver prec(2);
+   solver.SetSchur(prec);
+   solver.Mult(rhs, sol);
+   solver.GetDualSolution(lambda);
+   serr(0) = truex - sol(0);
+   serr(1) = truey - sol(1);
+   lerr(0) = truelambda - lambda(0);
+}
+
+void SimpleSaddle::Elimination(Vector& serr, Vector& lerr)
+{
+   ConstrainedSolver solver(*hA, B);
+   Array<int> primary(1);
+   primary[0] = 0;
+   Array<int> secondary(1);
+   secondary[0] = 1;
+   solver.SetElimination(primary, secondary);
+   solver.Mult(rhs, sol);
+   solver.GetDualSolution(lambda);
+   serr(0) = truex - sol(0);
+   serr(1) = truey - sol(1);
+   lerr(0) = truelambda - lambda(0);
+}
+
+void SimpleSaddle::Penalty(double pen, Vector& serr, Vector& lerr)
+{
+   ConstrainedSolver solver(*hA, B);
+   solver.SetPenalty(pen);
+   solver.Mult(rhs, sol);
+   solver.GetDualSolution(lambda);
+   serr(0) = truex - sol(0);
+   serr(1) = truey - sol(1);
+   lerr(0) = truelambda - lambda(0);
+}
+
+TEST_CASE("ConstrainedSolver", "[Parallel], [ConstrainedSolver]")
+{
+   Vector serr(2);
+   Vector lerr(1);
+
+   SimpleSaddle problem(4.0, -2.0);
+
+   problem.Schur(serr, lerr);
+   REQUIRE(serr(0) == MFEM_Approx(0.0));
+   REQUIRE(serr(1) == MFEM_Approx(0.0));
+   REQUIRE(lerr(0) == MFEM_Approx(0.0));
+
+   problem.Elimination(serr, lerr);
+   REQUIRE(serr(0) == MFEM_Approx(0.0));
+   REQUIRE(serr(1) == MFEM_Approx(0.0));
+   REQUIRE(lerr(0) == MFEM_Approx(0.0));
+
+   for (auto pen : {1.e+3, 1.e+4, 1.e+6})
+   {
+      problem.Penalty(pen, serr, lerr);
+      REQUIRE(std::abs(serr(0)) < pen);
+      REQUIRE(std::abs(serr(1)) < pen);
+      REQUIRE(std::abs(lerr(0)) < pen);
+   }
+}
+
+#endif
