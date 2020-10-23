bc49e70e17e953fabacabf386fdbafb5f0b64c76
Vladimir Z Tomov <tomov2@llnl.gov>
2020-10-23 16:50:03 -0700

    Redistancing through a time-dependent PDE. WIP.

--- examples/distance.cpp
+++ examples/distance.cpp
@@ -2,10 +2,16 @@
 //
 // Compile with: make distance
 //
-// Sample runs: mpirun -np 4 distance -m ../data/inline-segment.mesh -rs 3 -t 0.5
-//              mpirun -np 4 distance -m ../data/inline-quad.mesh -rs 1 -t 0.1
-//              mpirun -np 4 distance -m ./cir.msh -t 0.01
-//              mpirun -np 4 distance -m ../data/star.mesh
+// Sample runs:
+//   Problem 0: exact boundary alignment:
+//     mpirun -np 4 distance -m ../data/inline-segment.mesh -rs 3 -t 0.5
+//     mpirun -np 4 distance -m ../data/inline-quad.mesh -rs 1 -t 0.1
+//     mpirun -np 4 distance -m ./cir.msh -t 0.01
+//     mpirun -np 4 distance -m ../data/star.mesh
+//
+//    Problem 1: level set enclosing a volume:
+//     mpirun -np 4 distance -m ../data/inline-quad.mesh -p 1 -rs 3
+//
 //
 
 // Sample runs:  mpirun -np 4 ex1p -m ../data/square-disc.mesh
@@ -33,16 +39,6 @@
 //               mpirun -np 4 ex1p -m ../data/mobius-strip.mesh
 //               mpirun -np 4 ex1p -m ../data/mobius-strip.mesh -o -1 -sc
 //
-// Device sample runs:
-//               mpirun -np 4 ex1p -pa -d cuda
-//               mpirun -np 4 ex1p -pa -d occa-cuda
-//               mpirun -np 4 ex1p -pa -d raja-omp
-//               mpirun -np 4 ex1p -pa -d ceed-cpu
-//             * mpirun -np 4 ex1p -pa -d ceed-cuda
-//             * mpirun -np 4 ex1p -pa -d ceed-hip
-//               mpirun -np 4 ex1p -pa -d ceed-cuda:/gpu/cuda/shared
-//               mpirun -np 4 ex1p -m ../data/beam-tet.mesh -pa -d ceed-cpu
-//
 // Description:  This example code demonstrates the use of MFEM to define a
 //               simple finite element discretization of the Laplace problem
 //               -Delta u = 1 with homogeneous Dirichlet boundary conditions.
@@ -57,6 +53,44 @@
 
 using namespace std;
 using namespace mfem;
+
+/** A time-dependent operator for the right-hand side of the ODE. The DG weak
+ form of du/dt = v.grad(u) is M du/dt = K u + b, where M and K are the mass
+ and advection matrices, and b describes the flow on the boundary. This can
+ be written as a general ODE, du/dt = M^{-1} (K u + b), and this class is
+ used to evaluate the right-hand side. */
+class FE_Evolution : public TimeDependentOperator
+{
+private:
+    SparseMatrix &M, &K;
+    const Vector &b;
+    DSmoother M_prec;
+    CGSolver M_solver;
+
+    mutable Vector z;
+
+public:
+    FE_Evolution(SparseMatrix &_M, SparseMatrix &_K, const Vector &_b)
+       : TimeDependentOperator(_M.Size()), M(_M), K(_K), b(_b), z(_M.Size())
+    {
+        M_solver.SetPreconditioner(M_prec);
+        M_solver.SetOperator(M);
+
+        M_solver.iterative_mode = false;
+        M_solver.SetRelTol(1e-9);
+        M_solver.SetAbsTol(0.0);
+        M_solver.SetMaxIter(100);
+        M_solver.SetPrintLevel(0);
+    }
+
+    virtual void Mult(const Vector &x, Vector &y) const
+    {
+        // y = M^{-1} (K x + b)
+        K.Mult(x, z);
+        z += b;
+        M_solver.Mult(z, y);
+    }
+};
 
 class ExactSegmentDistCoeff : public Coefficient
 {
@@ -89,6 +123,199 @@ class ExactCircleDistCoeff : public Coefficient
    }
 };
 
+class GradientCoefficient : public VectorCoefficient
+{
+private:
+   GridFunction &u;
+public:
+   GradientCoefficient(GridFunction &u_, int dim)
+      : VectorCoefficient(dim), u(u_) { }
+   void Eval(Vector &V, ElementTransformation &T, const IntegrationPoint &ip)
+   {
+      T.SetIntPoint(&ip);
+      u.GetGradient(T, V);
+   }
+};
+
+// Coefficient for redistancing: S(w) * grad_w / |grad_w|.
+class RedistVelocityCoefficient : public VectorCoefficient
+{
+private:
+    GridFunction &w;
+    double max_velocity, dx;
+
+public:
+    RedistVelocityCoefficient(GridFunction &w_, int dim, double delta_x)
+       : VectorCoefficient(dim), w(w_), max_velocity(0.0), dx(delta_x) { }
+
+    void Eval(Vector &V, ElementTransformation &T, const IntegrationPoint &ip)
+    {
+        T.SetIntPoint(&ip);
+
+        Vector grad_w;
+        w.GetGradient(T, grad_w);
+
+        const double gnorm = grad_w.Norml2();
+        if (gnorm > 0.0) { grad_w /= gnorm; }
+
+        const double val = w.GetValue(T.ElementNo, ip);
+        grad_w *= val / sqrt(val * val + gnorm * gnorm * dx * dx);
+
+        if (gnorm > max_velocity) { max_velocity = grad_w.Norml2(); }
+
+        V = grad_w;
+    }
+
+    double GetMagVelocity() const { return max_velocity; }
+};
+
+class BoundaryCoefficient : public Coefficient
+{
+private:
+   GridFunction &w;
+
+public:
+    BoundaryCoefficient(GridFunction &w_): w(w_){ }
+    double Eval(ElementTransformation &T, const IntegrationPoint &ip)
+    {
+       // The incoming boundary should just decrease, nothing is coming in.
+       return 0.0;
+       //return u.GetValue(T.ElementNo, ip);
+    }
+};
+
+// Computes S(w) at a point.
+class SCoefficient : public Coefficient
+{
+private:
+   GridFunction &w;
+   double dx;
+
+public:
+   SCoefficient(GridFunction &w_, double delta_x) : w(w_), dx(delta_x) { }
+
+   double Eval(ElementTransformation &T, const IntegrationPoint &ip)
+   {
+      T.SetIntPoint(&ip);
+      Vector grad_w;
+      w.GetGradient(T, grad_w);
+
+      const double gnorm = grad_w.Norml2();
+      const double val = w.GetValue(T.ElementNo, ip);
+
+      return val / sqrt(val * val + gnorm * gnorm * dx * dx);
+   }
+};
+
+double surface_level_set(const Vector &x)
+{
+   const double sine = 0.25 * std::sin(4 * M_PI * x(0));
+   return (x(1) >= sine + 0.5) ? 1.0 : -1.0;
+}
+
+void redistance_level_set(ParGridFunction &w, int num_steps, double dx)
+{
+   const int order = w.ParFESpace()->GetFE(0)->GetOrder();
+   const int dim = w.ParFESpace()->GetMesh()->Dimension();
+   const int myid = w.ParFESpace()->GetMyRank();
+   const bool skip_zeros = false;
+   const double dt = 0.001;
+   ParMesh *pmesh = w.ParFESpace()->GetParMesh();
+
+   DG_FECollection L2fec(order, dim);
+   ParFiniteElementSpace l2_fes(pmesh, &L2fec);
+   ParFiniteElementSpace l2_fes_vec(pmesh, &L2fec, dim);
+   ParGridFunction grad_w_l2(&l2_fes_vec);
+
+   ParGridFunction w_dg(&l2_fes);
+   w_dg.ProjectGridFunction(w);
+
+   ParLinearForm b(&l2_fes);
+
+   BoundaryCoefficient bc(w_dg);
+   GradientCoefficient grad_w_coef(w_dg, dim);
+   RedistVelocityCoefficient vel_l2_rd_coef(w_dg, dim, dx);
+
+   // Redistancing mass.
+   ParBilinearForm m(&l2_fes);
+   m.AddDomainIntegrator(new MassIntegrator);
+   m.Assemble();
+   m.Finalize();
+
+   // Redistancing advection.
+   ParBilinearForm k(&l2_fes);
+   k.AddDomainIntegrator(new ConvectionIntegrator(vel_l2_rd_coef, -1.0));
+   k.AddInteriorFaceIntegrator(
+      new TransposeIntegrator(new DGTraceIntegrator(vel_l2_rd_coef, 1.0, -0.5)));
+   k.AddBdrFaceIntegrator(
+      new TransposeIntegrator(new DGTraceIntegrator(vel_l2_rd_coef, 1.0, -0.5)));
+   k.Assemble(skip_zeros);
+   k.Finalize(skip_zeros);
+
+   // Redistancing RHS.
+   SCoefficient rhs_l2_coef(w_dg, dx);
+   b.AddDomainIntegrator(new DomainLFIntegrator(rhs_l2_coef));
+   b.AddBdrFaceIntegrator(
+      new BoundaryFlowIntegrator(bc, vel_l2_rd_coef, -1.0, -0.5));
+   b.Assemble();
+
+   FE_Evolution redist(m.SpMat(), k.SpMat(), b);
+
+   RK2Solver ode_solver(1.0);
+   ode_solver.Init(redist);
+
+   double norm_old = 0.0;
+   for (int r = 0; r < num_steps; r++)
+   {
+      grad_w_l2.ProjectCoefficient(grad_w_coef);
+      const double norm = grad_w_l2.Normlinf();
+
+      if (myid == 0) { std::cout << "Step " << r << ": "<< norm << std::endl; }
+
+      if ((norm < 1.1 && norm > .9) || fabs(norm_old - norm) < 1e-5)
+      {
+         cout << "Redistancing converged in " << r << " steps." << endl;
+         break;
+      }
+
+      k.Update();
+      k.Assemble(skip_zeros);
+      k.Finalize(skip_zeros);
+      b.Update();
+      b.Assemble();
+
+      double t_red = dt * num_steps; // Shouldn't matter.
+      double dt_rd = 5.0 * dt;
+      ode_solver.Step(w_dg, t_red, dt_rd);
+      norm_old = norm;
+   }
+   w.ProjectGridFunction(w_dg);
+}
+
+void DiffuseField(ParGridFunction &field, int smooth_steps)
+{
+   // Setup the Laplacian operator.
+   ParBilinearForm *Lap = new ParBilinearForm(field.ParFESpace());
+   Lap->AddDomainIntegrator(new DiffusionIntegrator());
+   Lap->Assemble();
+   Lap->Finalize();
+   HypreParMatrix *A = Lap->ParallelAssemble();
+
+   HypreSmoother *S = new HypreSmoother(*A,0,smooth_steps);
+   S->iterative_mode = true;
+
+   Vector tmp(A->Width());
+   field.SetTrueVector();
+   Vector fieldtrue = field.GetTrueVector();
+   tmp = 0.0;
+   S->Mult(tmp, fieldtrue);
+
+   field.SetFromTrueDofs(fieldtrue);
+
+   delete S;
+   delete Lap;
+}
+
 int main(int argc, char *argv[])
 {
    // 1. Initialize MPI.
@@ -99,27 +326,26 @@ int main(int argc, char *argv[])
 
    // 2. Parse command-line options.
    const char *mesh_file = "../data/star.mesh";
+   int problem = 0;
    int rs_levels = 0;
    int order = 1;
    double t_param = 1.0;
-   bool static_cond = false;
-   bool pa = false;
    const char *device_config = "cpu";
    bool visualization = true;
 
    OptionsParser args(argc, argv);
    args.AddOption(&mesh_file, "-m", "--mesh",
                   "Mesh file to use.");
+   args.AddOption(&problem, "-p", "--problem",
+                  "Problem type:\n\t"
+                  "0: exact alignment with the mesh boundary\n\t"
+                  "1: zero level set enclosing a volume");
    args.AddOption(&rs_levels, "-rs", "--refine-serial",
                   "Number of times to refine the mesh uniformly in serial.");
    args.AddOption(&order, "-o", "--order",
                   "Finite element order (polynomial degree) or -1 for"
                   " isoparametric space.");
    args.AddOption(&t_param, "-t", "--t-param", "Varadhan's t constant");
-   args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
-                  "--no-static-condensation", "Enable static condensation.");
-   args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
-                  "--no-partial-assembly", "Enable Partial Assembly.");
    args.AddOption(&device_config, "-d", "--device",
                   "Device configuration string, see Device::Configure().");
    args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
@@ -147,18 +373,27 @@ int main(int argc, char *argv[])
    const int dim = mesh.Dimension();
    for (int lev = 0; lev < rs_levels; lev++) { mesh.UniformRefinement(); }
 
-   // 6. Define a parallel mesh by a partitioning of the serial mesh. Refine
-   //    this mesh further in parallel to increase the resolution. Once the
-   //    parallel mesh is defined, the serial mesh can be deleted.
+   // Compute average mesh size (assumes similar cells).
+   double area = 0.0, dx;
+   const int zones_cnt = mesh.GetNE();
+   for (int i = 0; i < zones_cnt; i++) { area += mesh.GetElementVolume(i); }
+   switch (mesh.GetElementBaseGeometry(0))
+   {
+      case Geometry::SQUARE:
+         dx = sqrt(area / zones_cnt); break;
+      case Geometry::TRIANGLE:
+         dx = sqrt(2.0 * area / zones_cnt); break;
+      case Geometry::CUBE:
+         dx = pow(area / zones_cnt, 1.0/3.0); break;
+      case Geometry::TETRAHEDRON:
+         dx = pow(6.0 * area / zones_cnt, 1.0/3.0); break;
+      default: MFEM_ABORT("Unknown zone type!");
+   }
+   dx /= order;
+
+   // MPI distribution.
    ParMesh pmesh(MPI_COMM_WORLD, mesh);
    mesh.Clear();
-   {
-      int par_ref_levels = 2;
-      for (int l = 0; l < par_ref_levels; l++)
-      {
-         pmesh.UniformRefinement();
-      }
-   }
 
    // 7. Define a parallel finite element space on the parallel mesh. Here we
    //    use continuous Lagrange finite elements of the specified order. If
@@ -213,34 +448,17 @@ int main(int argc, char *argv[])
 
    // Diffusion and mass terms in the LHS.
    ParBilinearForm a(&fespace);
-   if (pa) { a.SetAssemblyLevel(AssemblyLevel::PARTIAL); }
    a.AddDomainIntegrator(new MassIntegrator(one));
    ConstantCoefficient t_coeff(t_param);
    a.AddDomainIntegrator(new DiffusionIntegrator(t_coeff));
-
-   // 12. Assemble the parallel bilinear form and the corresponding linear
-   //     system, applying any necessary transformations such as: parallel
-   //     assembly, eliminating boundary conditions, applying conforming
-   //     constraints for non-conforming AMR, static condensation, etc.
-   if (static_cond) { a.EnableStaticCondensation(); }
    a.Assemble();
 
    OperatorPtr A;
    Vector B, X;
    a.FormLinearSystem(ess_tdof_list, w, b, A, X, B);
 
-   // 13. Solve the linear system A X = B.
-   //     * With full assembly, use the BoomerAMG preconditioner from hypre.
-   //     * With partial assembly, use Jacobi smoothing, for now.
-   Solver *prec = NULL;
-   if (pa)
-   {
-      if (UsesTensorBasis(fespace))
-      {
-         prec = new OperatorJacobiSmoother(a, ess_tdof_list);
-      }
-   }
-   else { prec = new HypreBoomerAMG; }
+   // Solve the linear system A X = B; CG + BoomerAMG.
+   Solver *prec = new HypreBoomerAMG;
 
    CGSolver cg(MPI_COMM_WORLD);
    cg.SetRelTol(1e-12);
@@ -255,24 +473,47 @@ int main(int argc, char *argv[])
    //     local finite element solution on each processor.
    a.RecoverFEMSolution(X, b, w);
 
-   // Varadhan transformation.
    ParGridFunction u(&fespace);
-   for (int i = 0; i < u.Size(); i++)
+   if (problem == 1)
    {
-      u(i) = - sqrt(t_param) * log(w(i));
+      FunctionCoefficient c(surface_level_set);
+      w.ProjectCoefficient(c);
+      DiffuseField(w, 1);
+
+      u = w;
+      redistance_level_set(u, 500, dx);
+
+      /*
+      // Transform so that the peak is at 0.
+      for (int i = 0; i < w.Size(); i++)
+      {
+         double x = w(i);
+         x += 1.0;
+         x /= 2.0;
+         w(i) = 4.0 * x * (1.0 - x);
+      } */
+   }
+
+   // Varadhan transformation.
+   if (problem == 0)
+   {
+      for (int i = 0; i < u.Size(); i++)
+      {
+         if (problem == 0) { u(i) = - sqrt(t_param) * log(w(i)); }
+      }
    }
 
    Coefficient *exact_dist = NULL;
    ParGridFunction u_error(&fespace);
-   if (strcmp(mesh_file, "../data/inline-segment.mesh") == 0)
+   if (problem == 0 && strcmp(mesh_file, "../data/inline-segment.mesh") == 0)
    {
       exact_dist = new ExactSegmentDistCoeff;
    }
-   if (strcmp(mesh_file, "../data/inline-quad.mesh") == 0)
+   if (problem == 0 && strcmp(mesh_file, "../data/inline-quad.mesh") == 0)
    {
       exact_dist = new ExactQuadDistCoeff;
    }
-   if (strcmp(mesh_file, "./cir.msh") == 0)
+   if (problem == 0 && strcmp(mesh_file, "./cir.msh") == 0)
    {
       exact_dist = new ExactCircleDistCoeff;
    }
@@ -343,7 +584,6 @@ int main(int argc, char *argv[])
       }
    }
 
-
    ParaViewDataCollection paraview_dc("Dist", &pmesh);
    paraview_dc.SetPrefixPath("ParaView");
    paraview_dc.SetLevelsOfDetail(order);
@@ -354,8 +594,6 @@ int main(int argc, char *argv[])
    paraview_dc.RegisterField("w",&w);
    paraview_dc.RegisterField("u",&u);
    paraview_dc.Save();
-
-
 
    if (delete_fec) { delete fec; }
 
