8ee602607300d795fb468905fe9025ef49084dbc
Jakub Červený <cerveny1@llnl.gov>
2020-11-14 11:01:40 +0100

    Reverted to star.mesh in ex6p (star-hilbert is better but it would probably throw off testing).

--- examples/ex6p.cpp
+++ examples/ex6p.cpp
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
    // 2. Parse command-line options.
-   const char *mesh_file = "../data/star-hilbert.mesh";
+   const char *mesh_file = "../data/star.mesh";
    int order = 1;
    bool pa = false;
    const char *device_config = "cpu";
