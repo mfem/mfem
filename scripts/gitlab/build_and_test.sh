399fa195065df2130bccfa4ead2dde63425742a2
Adrien M. Bernede <bernede1@llnl.gov>
2020-11-18 16:01:41 -0800

    First attempt at using external uberenv

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -16,6 +16,9 @@ py_env_path=${PYTHON_ENVIRONMENT_PATH:-""}
 
 threads=${THREADS:-""}
 
+uberenv_url="ssh://git@czgitlab.llnl.gov:7999/bernede1/mfem-uberenv.git"
+uberenv_ref="71d85ce03411259ec145197b6155801277f49229"
+
 # Dependencies
 if [[ "${option}" != "--build-only" && "${option}" != "--test-only" ]]
 then
@@ -44,6 +47,12 @@ then
         echo ${spec} > spec.txt
         prefix_opt="--prefix=${prefix}"
     fi
+
+    git clone ${uberenv_url} scripts/uberenv
+    cd scripts/uberenv
+    git fetch origin ${uberenv_ref}
+    git checkout ${uberenv_ref}
+    cd -
 
     python scripts/uberenv/uberenv.py --spec="${spec}" "${upstream_opt}" "${prefix_opt}"
 
