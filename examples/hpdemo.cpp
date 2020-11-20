d90ba7b8f10861e45eec4b5bd0a5a20d097a14ae
Jakub Červený <cerveny1@llnl.gov>
2020-11-20 15:24:35 +0100

    Moved ex1 demo to "hpdemo"; fixed NC check; cleaned up

--- examples/hpdemo.cpp
+++ examples/hpdemo.cpp
@@ -1,60 +1,18 @@
-//                                MFEM Example 1
+//                     hp refinement demo (NOT TO BE MERGED)
 //
-// Compile with: make ex1
+// Compile with: make hpdemo
 //
-// Sample runs:  ex1 -m ../data/square-disc.mesh
-//               ex1 -m ../data/star.mesh
-//               ex1 -m ../data/star-mixed.mesh
-//               ex1 -m ../data/escher.mesh
-//               ex1 -m ../data/fichera.mesh
-//               ex1 -m ../data/fichera-mixed.mesh
-//               ex1 -m ../data/toroid-wedge.mesh
-//               ex1 -m ../data/periodic-annulus-sector.msh
-//               ex1 -m ../data/periodic-torus-sector.msh
-//               ex1 -m ../data/square-disc-p2.vtk -o 2
-//               ex1 -m ../data/square-disc-p3.mesh -o 3
-//               ex1 -m ../data/square-disc-nurbs.mesh -o -1
-//               ex1 -m ../data/star-mixed-p2.mesh -o 2
-//               ex1 -m ../data/disc-nurbs.mesh -o -1
-//               ex1 -m ../data/pipe-nurbs.mesh -o -1
-//               ex1 -m ../data/fichera-mixed-p2.mesh -o 2
-//               ex1 -m ../data/star-surf.mesh
-//               ex1 -m ../data/square-disc-surf.mesh
-//               ex1 -m ../data/inline-segment.mesh
-//               ex1 -m ../data/amr-quad.mesh
-//               ex1 -m ../data/amr-hex.mesh
-//               ex1 -m ../data/fichera-amr.mesh
-//               ex1 -m ../data/mobius-strip.mesh
-//               ex1 -m ../data/mobius-strip.mesh -o -1 -sc
+// Sample runs:  hpdemo
+//               hpdemo -m ../data/inline-hex.mesh
 //
-// Device sample runs:
-//               ex1 -pa -d cuda
-//               ex1 -pa -d raja-cuda
-//               ex1 -pa -d occa-cuda
-//               ex1 -pa -d raja-omp
-//               ex1 -pa -d occa-omp
-//               ex1 -pa -d ceed-cpu
-//             * ex1 -pa -d ceed-cuda
-//             * ex1 -pa -d ceed-hip
-//               ex1 -pa -d ceed-cuda:/gpu/cuda/shared
-//               ex1 -m ../data/beam-hex.mesh -pa -d cuda
-//               ex1 -m ../data/beam-tet.mesh -pa -d ceed-cpu
-//               ex1 -m ../data/beam-tet.mesh -pa -d ceed-cuda:/gpu/cuda/ref
+// Description:  Demonstrates creation of a variable order space and solution
+//               of the Laplace problem -Delta u = 1. The mesh may contain
+//               nonconforming refinements. Works in both 2D and 3D.
+//               Serial FiniteElementSpace only, so far.
 //
-// Description:  This example code demonstrates the use of MFEM to define a
-//               simple finite element discretization of the Laplace problem
-//               -Delta u = 1 with homogeneous Dirichlet boundary conditions.
-//               Specifically, we discretize using a FE space of the specified
-//               order, or if order < 1 using an isoparametric/isogeometric
-//               space (i.e. quadratic for quadratic curvilinear mesh, NURBS for
-//               NURBS mesh, etc.)
-//
-//               The example highlights the use of mesh refinement, finite
-//               element grid functions, as well as linear and bilinear forms
-//               corresponding to the left-hand side and right-hand side of the
-//               discrete linear system. We also cover the explicit elimination
-//               of essential boundary conditions, static condensation, and the
-//               optional connection to the GLVis tool for visualization.
+//               As there is no support in GLVis yet (mainly variable-order
+//               space serialization), we need to prolong the solution to the
+//               maximum order before visualization.
 
 #include "mfem.hpp"
 #include <fstream>
@@ -63,15 +21,21 @@
 using namespace std;
 using namespace mfem;
 
+GridFunction* ProlongToMaxOrder(const GridFunction *x);
+
 int main(int argc, char *argv[])
 {
    // 1. Parse command-line options.
    const char *mesh_file = "../data/star.mesh";
    int order = 1;
+   int ref_levels = 1;
+   int seed = 1;
    bool static_cond = false;
    bool pa = false;
    const char *device_config = "cpu";
    bool visualization = true;
+   bool vis_orders = true, vis_basis = true;
+   bool relaxed_hp = false;
 
    OptionsParser args(argc, argv);
    args.AddOption(&mesh_file, "-m", "--mesh",
@@ -79,15 +43,27 @@ int main(int argc, char *argv[])
    args.AddOption(&order, "-o", "--order",
                   "Finite element order (polynomial degree) or -1 for"
                   " isoparametric space.");
+   args.AddOption(&ref_levels, "-r", "--ref-levels",
+                  "Number of mesh refinement levels.");
+   args.AddOption(&seed, "-s", "--seed", "Random seed");
    args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
                   "--no-static-condensation", "Enable static condensation.");
    args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
                   "--no-partial-assembly", "Enable Partial Assembly.");
    args.AddOption(&device_config, "-d", "--device",
                   "Device configuration string, see Device::Configure().");
+   args.AddOption(&relaxed_hp, "-x", "--relaxed-hp", "-no-x",
+                  "--no-relaxed-hp", "Set relaxed hp conformity.");
    args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
                   "--no-visualization",
-                  "Enable or disable GLVis visualization.");
+                  "Enable or disable GLVis visualization of the solution.");
+   args.AddOption(&vis_orders, "-vis-o", "--visualize-orders", "-no-vis-o",
+                  "--dont-visualize_orders",
+                  "Enable or disable visualization of element orders.");
+   args.AddOption(&vis_basis, "-vis-b", "--visualize-bases", "-no-vis-b",
+                  "--dont-visualize-bases",
+                  "Enable or disable visualization of the basis functions.");
+
    args.Parse();
    if (!args.Good())
    {
@@ -106,17 +82,17 @@ int main(int argc, char *argv[])
    //    the same code.
    Mesh mesh(mesh_file, 1, 1);
    int dim = mesh.Dimension();
+   mesh.EnsureNCMesh();
 
    // 4. Refine the mesh to increase the resolution. In this example we do
    //    'ref_levels' of uniform refinement. We choose 'ref_levels' to be the
    //    largest number that gives a final mesh with no more than 50,000
    //    elements.
+   srand(seed);
    {
-      int ref_levels =
-         (int)floor(log(50000./mesh.GetNE())/log(2.)/dim);
       for (int l = 0; l < ref_levels; l++)
       {
-         mesh.UniformRefinement();
+         mesh.RandomRefinement(0.5, true);
       }
    }
 
@@ -141,7 +117,19 @@ int main(int argc, char *argv[])
       fec = new H1_FECollection(order = 1, dim);
       delete_fec = true;
    }
+
    FiniteElementSpace fespace(&mesh, fec);
+   fespace.SetRelaxedHpConformity(relaxed_hp);
+
+   // 6. At this point all elements have the default order (specified when
+   //    construction the FECollection). Now we can p-refine some of them to
+   //    obtain a variable-order space...
+   for (int i = 0; i < mesh.GetNE(); i++)
+   {
+      fespace.SetElementOrder(i, order + (rand()%5));
+   }
+   fespace.Update(false);
+
    cout << "Number of finite element unknowns: "
         << fespace.GetTrueVSize() << endl;
 
@@ -156,6 +144,7 @@ int main(int argc, char *argv[])
       ess_bdr = 1;
       fespace.GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
    }
+   cout << "Essential DOFs: " << ess_tdof_list.Size() << endl;
 
    // 7. Set up the linear form b(.) which corresponds to the right-hand side of
    //    the FEM linear system, which in this case is (1,phi_i) where phi_i are
@@ -194,17 +183,9 @@ int main(int argc, char *argv[])
    // 11. Solve the linear system A X = B.
    if (!pa)
    {
-#ifndef MFEM_USE_SUITESPARSE
       // Use a simple symmetric Gauss-Seidel preconditioner with PCG.
       GSSmoother M((SparseMatrix&)(*A));
       PCG(*A, M, B, X, 1, 200, 1e-12, 0.0);
-#else
-      // If MFEM was compiled with SuiteSparse, use UMFPACK to solve the system.
-      UMFPackSolver umf_solver;
-      umf_solver.Control[UMFPACK_ORDERING] = UMFPACK_ORDERING_METIS;
-      umf_solver.SetOperator(*A);
-      umf_solver.Mult(B, X);
-#endif
    }
    else // Jacobi preconditioning in partial assembly mode
    {
@@ -231,17 +212,61 @@ int main(int argc, char *argv[])
    sol_ofs.precision(8);
    x.Save(sol_ofs);
 
+   const char vishost[] = "localhost";
+   const int  visport   = 19916;
+
    // 14. Send the solution by socket to a GLVis server.
    if (visualization)
    {
-      char vishost[] = "localhost";
-      int  visport   = 19916;
+      // Prolong the solution vector onto L2 space of max order (for GLVis)
+      GridFunction *vis_x = ProlongToMaxOrder(&x);
       socketstream sol_sock(vishost, visport);
       sol_sock.precision(8);
-      sol_sock << "solution\n" << mesh << x << flush;
+      sol_sock << "solution\n" << mesh << *vis_x;
+      if (dim < 3) { sol_sock << "keys Rjlm\n"; }
+      delete vis_x;
    }
 
-   // 15. Free the used memory.
+   // 15. Visualize element orders
+   if (vis_orders)
+   {
+      L2_FECollection l2fec(0, dim);
+      FiniteElementSpace l2fes(&mesh, &l2fec);
+      GridFunction orders(&l2fes);
+
+      for (int i = 0; i < orders.Size(); i++)
+      {
+         orders(i) = fespace.GetElementOrder(i);
+      }
+
+      socketstream ord_sock(vishost, visport);
+      ord_sock.precision(8);
+      ord_sock << "solution\n" << mesh << orders;
+      if (dim < 3) { ord_sock << "keys Rjlmc\n"; }
+   }
+
+   // 16. Visualize the basis functions
+   if (vis_basis)
+   {
+      socketstream b_sock(vishost, visport);
+      b_sock.precision(8);
+      cout << "Press SPACE to cycle through basis functions..." << endl;
+
+      for (int i = 0; i < X.Size(); i++)
+      {
+         X = 0.0;
+         X(i) = 1.0;
+         a.RecoverFEMSolution(X, b, x);
+
+         GridFunction *vis_x = ProlongToMaxOrder(&x);
+         b_sock << "solution\n" << mesh << *vis_x << flush;
+         if (!i) { b_sock << "keys miIMA\n"; }
+         b_sock << "pause\n" << flush;
+         delete vis_x;
+      }
+   }
+
+   // 17. Free the used memory.
    if (delete_fec)
    {
       delete fec;
@@ -249,3 +274,50 @@ int main(int argc, char *argv[])
 
    return 0;
 }
+
+
+GridFunction* ProlongToMaxOrder(const GridFunction *x)
+{
+   const FiniteElementSpace *fespace = x->FESpace();
+   const FiniteElementCollection *fec = fespace->FEColl();
+   Mesh *mesh = fespace->GetMesh();
+
+   int max_order = fespace->GetMaxElementOrder();
+
+   // Create a visualization space of max order for all elements
+   FiniteElementCollection *vis_fec =
+      new L2_FECollection(max_order, mesh->Dimension(), BasisType::GaussLobatto);
+   FiniteElementSpace *vis_space =
+      new FiniteElementSpace(mesh, vis_fec);
+
+   IsoparametricTransformation T;
+   DenseMatrix I;
+
+   GridFunction *visualization_x = new GridFunction(vis_space);
+
+   // Interpolate solution vector in the visualization space
+   for (int i = 0; i < mesh->GetNE(); i++)
+   {
+      Geometry::Type geometry = mesh->GetElementGeometry(i);
+      T.SetIdentityTransformation(geometry);
+
+      Array<int> dofs;
+      fespace->GetElementDofs(i, dofs);
+      Vector elemvect, visualization_vect;
+      x->GetSubVector(dofs, elemvect);
+
+      const auto *fe = fec->GetFE(geometry, fespace->GetElementOrder(i));
+      const auto *visualization_fe = vis_fec->GetFE(geometry, max_order);
+
+      visualization_fe->GetTransferMatrix(*fe, T, I);
+      vis_space->GetElementDofs(i, dofs);
+      visualization_vect.SetSize(dofs.Size());
+
+      I.Mult(elemvect, visualization_vect);
+      visualization_x->SetSubVector(dofs, visualization_vect);
+   }
+
+   visualization_x->MakeOwner(vis_fec);
+   return visualization_x;
+}
+
