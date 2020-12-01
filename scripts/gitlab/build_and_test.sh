05f7915cfb49d5236389be449cd886e283dfc74f
Adrien M. Bernede <bernede1@llnl.gov>
2020-12-01 10:28:31 -0800

    Fix use shared upstream

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -32,7 +32,7 @@ then
     upstream_opt=""
     if [[ ${CI_COMMIT_BRANCH:-"master"} != "master" && ${sys_type} != "" ]]
     then
-        upstream_opt="--upstream=/usr/workspace/mfem/mfem-spack-upstream/uberenv_libs"
+        upstream_opt="--upstream=/usr/workspace/mfem/spack-upstream/uberenv_libs"
     fi
 
     prefix_opt=""
