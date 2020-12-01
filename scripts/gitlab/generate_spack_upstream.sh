524473ee55107ca16699ce2430d4af58b97ab485
Adrien M. Bernede <bernede1@llnl.gov>
2020-12-01 14:55:28 -0800

    Helper to generate spack upstream of MFEM dependencies

---
+++ scripts/gitlab/generate_spack_upstream.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script is meant be launch in the root directory of MFEM do compile all
+# the dependencies of MFEM for all the specs in CI. By doing so, we obtain a
+# Spack instance that can be used as an upstream.
+
+# This patch applies shared permissions to Spack installations.
+git apply scripts/gitlab/upstream-permission.patch
+
+# call uberenv for all specs in CI
+git grep -e "^[^#]" .gitlab | grep "SPEC" \
+                            | cut -d' ' -f6- \
+                            | sed 's/"//g' \
+                            | while read -r line; do
+  python ./scripts/uberenv/uberenv.py --spec="$line"
+done
+
+# We revert the patch to leave the repo as found.
+git apply -R scripts/gitlab/upstream-permission.patch
