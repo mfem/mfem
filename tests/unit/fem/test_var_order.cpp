0c92937d5644185d0254c1c8cc60ce83a5b8009b
Jakub Červený <cerveny1@llnl.gov>
2020-12-04 15:28:11 +0100

    Unit tests for quad/hex meshes. Improved docs.

--- tests/unit/fem/test_var_order.cpp
+++ tests/unit/fem/test_var_order.cpp
@@ -15,20 +15,21 @@
 namespace mfem
 {
 
-// Check basic functioning of variable order spaces plus some corner cases.
+// Check basic functioning of variable order spaces, hp interpolation and
+// some corner cases.
 //
 TEST_CASE("Variable Order FiniteElementSpace",
           "[FiniteElementCollection]"
           "[FiniteElementSpace]"
           "[NCMesh]")
 {
-   SECTION("Quad mesh basics")
+   SECTION("Quad mesh")
    {
       // 2-element quad mesh
       Mesh mesh(2, 1, Element::QUADRILATERAL);
       mesh.EnsureNCMesh();
 
-      // standard H1 conforming space (order 1 elements)
+      // standard H1 space with order 1 elements
       H1_FECollection fec(1, mesh.Dimension());
       FiniteElementSpace fespace(&mesh, &fec);
 
@@ -46,8 +47,7 @@ TEST_CASE("Variable Order FiniteElementSpace",
       Array<Refinement> refs;
       refs.Append(Refinement(0, 2));
       mesh.GeneralRefinement(refs);
-
-      fespace.Update(true);
+      fespace.Update();
 
       REQUIRE(fespace.GetNDofs() == 13);
       REQUIRE(fespace.GetNConformingDofs() == 11);
@@ -59,18 +59,105 @@ TEST_CASE("Variable Order FiniteElementSpace",
       REQUIRE(fespace.GetNConformingDofs() == 12);
 
       // increase order
+      for (int i = 0; i < mesh.GetNE(); i++)
+      {
+         fespace.SetElementOrder(i, fespace.GetElementOrder(i) + 1);
+      }
+      fespace.Update(false);
 
-      // refine
+      // 15 quadratic + 16 cubic DOFs - 2 shared vertices:
+      REQUIRE(fespace.GetNDofs() == 29);
+      // 3 constrained DOFs on slave side, inexact interpolation
+      REQUIRE(fespace.GetNConformingDofs() == 26);
 
       // relaxed off
+      fespace.SetRelaxedHpConformity(false);
 
+      // new quadratic DOF on master edge:
+      REQUIRE(fespace.GetNDofs() == 30);
+      // 3 constrained DOFs on slave side, 2 on master side:
+      REQUIRE(fespace.GetNConformingDofs() == 25);
+
+      // refine
+      mesh.UniformRefinement();
+      fespace.Update();
+
+      REQUIRE(fespace.GetNDofs() == 93);
+      REQUIRE(fespace.GetNConformingDofs() == 83);
    }
 
-   // TODO: hex mesh
+   SECTION("Hex mesh")
+   {
+      // 2-element hex mesh
+      Mesh mesh(2, 1, 1, Element::HEXAHEDRON);
+      mesh.EnsureNCMesh();
 
-   // TODO: hex mesh with min order propagation
+      // standard H1 space with order 1 elements
+      H1_FECollection fec(1, mesh.Dimension());
+      FiniteElementSpace fespace(&mesh, &fec);
+
+      REQUIRE(fespace.GetNDofs() == 12);
+      REQUIRE(fespace.GetNConformingDofs() == 12);
+
+      // convert to variable order space: p-refine second element
+      fespace.SetElementOrder(1, 2);
+      fespace.Update(false);
+
+      REQUIRE(fespace.GetNDofs() == 31);
+      REQUIRE(fespace.GetNConformingDofs() == 26);
+
+      // h-refine first element in the z axis
+      Array<Refinement> refs;
+      refs.Append(Refinement(0, 4));
+      mesh.GeneralRefinement(refs);
+      fespace.Update();
+
+      REQUIRE(fespace.GetNDofs() == 35);
+      REQUIRE(fespace.GetNConformingDofs() == 28);
+
+      // relax the master face to be quadratic
+      fespace.SetRelaxedHpConformity(true);
+
+      REQUIRE(fespace.GetNDofs() == 35);
+      REQUIRE(fespace.GetNConformingDofs() == 31);
+
+      // increase order
+      for (int i = 0; i < mesh.GetNE(); i++)
+      {
+         fespace.SetElementOrder(i, fespace.GetElementOrder(i) + 1);
+      }
+      fespace.Update(false);
+
+      REQUIRE(fespace.GetNDofs() == 105);
+      REQUIRE(fespace.GetNConformingDofs() == 92);
+
+      // relaxed off
+      fespace.SetRelaxedHpConformity(false);
+
+      REQUIRE(fespace.GetNDofs() == 108);
+      REQUIRE(fespace.GetNConformingDofs() == 87);
+
+      // refine one of the small elements into four
+      refs[0].ref_type = 3;
+      mesh.GeneralRefinement(refs);
+      fespace.Update();
+
+      REQUIRE(fespace.GetNDofs() == 162);
+      REQUIRE(fespace.GetNConformingDofs() == 115);
+
+      // lower the order of one of the four new elements to 1 - this minimum
+      // order will propagate through two master faces and severely constrain
+      // the space (since relaxed hp is off)
+      fespace.SetElementOrder(0, 1);
+      fespace.Update(false);
+
+      REQUIRE(fespace.GetNDofs() == 152);
+      REQUIRE(fespace.GetNConformingDofs() == 92);
+   }
 
    // TODO: prism mesh with edge-face constraint
+
+   // TODO: mixed mesh 2D, 3D
 }
 
 
