02152f6ff66e2f947e2298c72bb81413bb6c6fd8
Jakub Červený <cerveny1@llnl.gov>
2020-12-04 17:18:22 +0100

    Added prism test (edge-face constraint)

--- tests/unit/fem/test_var_order.cpp
+++ tests/unit/fem/test_var_order.cpp
@@ -155,11 +155,45 @@ TEST_CASE("Variable Order FiniteElementSpace",
       REQUIRE(fespace.GetNConformingDofs() == 92);
    }
 
-   // TODO: prism mesh with edge-face constraint
+   SECTION("Prism mesh")
+   {
+      // 2-element prism mesh
+      Mesh mesh(1, 1, 1, Element::WEDGE);
+      mesh.EnsureNCMesh();
 
-   // TODO: mixed mesh 2D, 3D
+      // standard H1 space with order 2 elements
+      H1_FECollection fec(2, mesh.Dimension());
+      FiniteElementSpace fespace(&mesh, &fec);
+
+      REQUIRE(fespace.GetNDofs() == 27);
+      REQUIRE(fespace.GetNConformingDofs() == 27);
+
+      // convert to variable order space: p-refine first element
+      fespace.SetElementOrder(0, 3);
+      fespace.Update(false);
+
+      REQUIRE(fespace.GetNDofs() == 54);
+      REQUIRE(fespace.GetNConformingDofs() == 42);
+
+      // refine to form an edge-face constraint similar to
+      // https://github.com/mfem/mfem/pull/713#issuecomment-495786362
+      Array<Refinement> refs;
+      refs.Append(Refinement(1, 3));
+      mesh.GeneralRefinement(refs);
+      fespace.Update(false);
+
+      refs[0].ref_type = 4;
+      refs.Append(Refinement(2, 4));
+      mesh.GeneralRefinement(refs);
+      fespace.Update(false);
+
+      REQUIRE(fespace.GetNDofs() == 113);
+      REQUIRE(fespace.GetNConformingDofs() == 67);
+   }
+
+   // TODO mixed meshes 2D, 3D?
+   // TODO try to solve with known exact solution
 }
-
 
 
 }
