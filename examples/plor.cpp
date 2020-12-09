ca1e9df5be44952f18858c3a9677d8b6e2721be1
Will Pazner <will.e.p@gmail.com>
2020-12-09 13:39:55 -0800

    Minor

--- examples/plor.cpp
+++ examples/plor.cpp
@@ -1,6 +1,7 @@
 #include "mfem.hpp"
 #include <fstream>
 #include <iostream>
+#include <memory>
 
 using namespace std;
 using namespace mfem;
@@ -14,6 +15,7 @@ int main(int argc, char *argv[])
    int ref_levels = 0;
    int order = 3;
    bool simplex = true;
+   const char *device_config = "cpu";
 
    OptionsParser args(argc, argv);
    args.AddOption(&mesh_file, "-m", "--mesh", "Mesh file to use.");
@@ -21,6 +23,8 @@ int main(int argc, char *argv[])
    args.AddOption(&order, "-o", "--order", "Polynomial degree.");
    args.AddOption(&simplex, "-s", "--simplex", "-no-s", "--no-simplex",
                   "Simplex LOR?");
+   args.AddOption(&device_config, "-d", "--device",
+                  "Device configuration string, see Device::Configure().");
    args.Parse();
    if (!args.Good())
    {
@@ -29,30 +33,45 @@ int main(int argc, char *argv[])
    }
    if (mpi.Root()) { args.PrintOptions(cout); }
 
+   Device device(device_config);
+   if (mpi.Root()) { device.Print(); }
+
    Mesh mesh_serial(mesh_file, 1, 1);
-   ParMesh mesh(MPI_COMM_WORLD, mesh_serial);
-   mesh_serial.Clear();
-
-   int dim = mesh.Dimension();
-
-   mesh.EnsureNodes();
-
    for (int l = 0; l < ref_levels; l++)
    {
-      mesh.UniformRefinement();
+      mesh_serial.UniformRefinement();
    }
+
+   ParMesh mesh(MPI_COMM_WORLD, mesh_serial);
+   mesh_serial.Clear();
+   int dim = mesh.Dimension();
+   mesh.EnsureNodes();
 
    H1_FECollection fec_ho(order, dim);
    ParFiniteElementSpace fes_ho(&mesh, &fec_ho);
 
-   ParMesh mesh_lor_tensor(&mesh, order, BasisType::GaussLobatto);
-   ParMesh mesh_lor;
-   mesh_lor.MakeSimplicial(mesh_lor_tensor);
-   mesh_lor.PrintBdrVTU("bdr");
-   mesh_lor.PrintVTU("mesh");
+   
+   std::shared_ptr<ParMesh> mesh_lor_tensor(new ParMesh(&mesh, order, BasisType::GaussLobatto));
+   std::shared_ptr<ParMesh> mesh_lor;
+   if (simplex)
+   {
+      mesh_lor.reset(new ParMesh);
+      mesh_lor->MakeSimplicial(*mesh_lor_tensor);
+   }
+   else
+   {
+      mesh_lor = mesh_lor_tensor;
+   }
+
+   bool vis = false;
+   if (vis)
+   {
+      mesh_lor->PrintBdrVTU("bdr");
+      mesh_lor->PrintVTU("mesh");
+   }
 
    H1_FECollection fec_lor(1, dim);
-   ParFiniteElementSpace fes_lor(&mesh_lor, &fec_lor);
+   ParFiniteElementSpace fes_lor(mesh_lor.get(), &fec_lor);
 
    Array<int> ess_tdof_list;
    if (mesh.bdr_attributes.Size())
@@ -66,7 +85,7 @@ int main(int argc, char *argv[])
 
    ParBilinearForm a_ho(&fes_ho);
    a_ho.AddDomainIntegrator(new DiffusionIntegrator(one));
-   // a_ho.SetAssemblyLevel(AssemblyLevel::PARTIAL);
+   a_ho.SetAssemblyLevel(AssemblyLevel::PARTIAL);
    a_ho.Assemble();
    ParLinearForm b_ho(&fes_ho);
    b_ho.AddDomainIntegrator(new DomainLFIntegrator(one));
@@ -94,31 +113,41 @@ int main(int argc, char *argv[])
    CGSolver cg(MPI_COMM_WORLD);
    cg.SetAbsTol(0.0);
    cg.SetRelTol(1e-12);
-   cg.SetMaxIter(100);
-   cg.SetPrintLevel(1);
+   cg.SetMaxIter(200);
+   cg.SetPrintLevel(2);
    cg.SetOperator(*A_ho);
    cg.SetPreconditioner(solv_lor);
+
+   if (mpi.Root()) { std::cout << "HO system:  "; }
+   X_ho = 0.0;
    cg.Mult(B_ho, X_ho);
    a_ho.RecoverFEMSolution(X_ho, b_ho, x_ho);
 
-   ParaViewDataCollection dc("LOR", &mesh);
-   dc.SetPrefixPath("ParaView");
-   dc.SetHighOrderOutput(true);
-   dc.SetLevelsOfDetail(order);
-   dc.RegisterField("u", &x_ho);
-   dc.SetCycle(0);
-   dc.SetTime(0.0);
-   dc.Save();
-
-   solv_lor.Mult(B_lor, X_lor);
+   if (mpi.Root()) { std::cout << "LOR system: "; }
+   cg.SetOperator(*A_lor);
+   X_lor = 0.0;
+   cg.Mult(B_lor, X_lor);
    a_lor.RecoverFEMSolution(X_lor, b_lor, x_lor);
-   dc.SetMesh(&mesh_lor);
-   dc.DeregisterField("u");
-   dc.RegisterField("u", &x_lor);
-   dc.SetLevelsOfDetail(1);
-   dc.SetCycle(1);
-   dc.SetTime(1.0);
-   dc.Save();
+
+   if (vis)
+   {
+      ParaViewDataCollection dc("LOR", &mesh);
+      dc.SetPrefixPath("ParaView");
+      dc.SetHighOrderOutput(true);
+      dc.SetLevelsOfDetail(order);
+      dc.RegisterField("u", &x_ho);
+      dc.SetCycle(0);
+      dc.SetTime(0.0);
+      dc.Save();
+
+      dc.SetMesh(mesh_lor.get());
+      dc.DeregisterField("u");
+      dc.RegisterField("u", &x_lor);
+      dc.SetLevelsOfDetail(1);
+      dc.SetCycle(1);
+      dc.SetTime(1.0);
+      dc.Save();
+   }
 
    return 0;
 }
