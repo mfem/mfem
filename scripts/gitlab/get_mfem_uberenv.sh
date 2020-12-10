d270f5cca5523bd0adbcd3915d28262cd378614b
Adrien M. Bernede <bernede1@llnl.gov>
2020-12-09 17:50:05 -0800

    Temp: use https on gitlab + fix case were directory already exist (out of CI)

--- scripts/gitlab/get_mfem_uberenv.sh
+++ scripts/gitlab/get_mfem_uberenv.sh
@@ -3,10 +3,10 @@
 set -o errexit
 set -o nounset
 
-uberenv_url="ssh://git@czgitlab.llnl.gov:7999/mfem/mfem-uberenv.git"
+uberenv_url="https://lc.llnl.gov/gitlab/mfem/mfem-uberenv.git"
 uberenv_ref="c2f3497e9a392885058dd2ba93e2f8c071655726"
 
-git clone ${uberenv_url} scripts/uberenv
+[[ ! -d scripts/uberenv ]] && git clone ${uberenv_url} scripts/uberenv
 cd scripts/uberenv
 git fetch origin ${uberenv_ref}
 git checkout ${uberenv_ref}
