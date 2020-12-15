4c6078f32f4286b6a1dfb812b414e40f8db84510
Yohann Dudouit <dudouit1@llnl.gov>
2020-12-14 18:19:59 -0800

    Add a class to encapsulate qData in a Tensor.

---
+++ linalg/tensor/qdata.hpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2010-2020, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#ifndef MFEM_TENSOR_QDATA
+#define MFEM_TENSOR_QDATA
+
+#include "util.hpp"
+#include "tensor.hpp"
+#include "config.hpp"
+
+namespace mfem
+{
+/// A class to encapsulate quadrature data in a Tensor.
+template <int Dim, int DimComp = 0, bool IsTensor = false, int Q = 0>
+class QData;
+
+// TODO
+
+} // mfem namespace
+
+#endif // MFEM_TENSOR_QDATA
