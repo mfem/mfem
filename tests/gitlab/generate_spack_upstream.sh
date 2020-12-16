7def3bfe04ea2d1baa27022bde1797d002b361e4
Adrien M. Bernede <bernede1@llnl.gov>
2020-12-16 09:59:22 -0800

    Moving gitlab scripts to tests dir and patch to uberenv repo

--- tests/gitlab/generate_spack_upstream.sh
+++ tests/gitlab/generate_spack_upstream.sh
@@ -4,15 +4,15 @@
 # Spack instance that can be used as an upstream.
 
 # This patch applies shared permissions to Spack installations.
-git apply scripts/gitlab/upstream-permission.patch
+git apply tests/gitlab/upstream-permission.patch
 
 # call uberenv for all specs in CI
 git grep -e "^[^#]" .gitlab | grep "SPEC" \
                             | cut -d' ' -f6- \
                             | sed 's/"//g' \
                             | while read -r line; do
-  python ./scripts/uberenv/uberenv.py --spec="$line"
+  python ./tests/uberenv/uberenv.py --spec="$line"
 done
 
 # We revert the patch to leave the repo as found.
-git apply -R scripts/gitlab/upstream-permission.patch
+git apply -R tests/uberenv/upstream-permission.patch
--- tests/gitlab/get_mfem_uberenv.sh
+++ tests/gitlab/get_mfem_uberenv.sh
@@ -6,8 +6,8 @@ set -o nounset
 uberenv_url="https://github.com/mfem/mfem-uberenv.git"
 uberenv_ref="c2f3497e9a392885058dd2ba93e2f8c071655726"
 
-[[ ! -d scripts/uberenv ]] && git clone ${uberenv_url} scripts/uberenv
-cd scripts/uberenv
+[[ ! -d scripts/uberenv ]] && git clone ${uberenv_url} tests/uberenv
+cd tests/uberenv
 git fetch origin ${uberenv_ref}
 git checkout ${uberenv_ref}
 cd -
