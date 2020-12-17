58e6db4118ec6e33490e50dd50e0dd2ebb16f752
Robert <rac428@cornell.edu>
2020-12-17 10:52:18 -0800

    make style

--- fem/fespace.hpp
+++ fem/fespace.hpp
@@ -755,7 +755,7 @@ public:
 
    /// Return the total number of quadrature points.
    int GetSize() const { return size; }
-    
+
    /// Return the order used by the global rules from #IntRules.
    int GetOrder() const { return order; }
 
