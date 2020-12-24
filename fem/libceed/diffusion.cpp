bd9163a91fe51fe9447e6c768eb8704fab4c4837
Will Pazner <will.e.p@gmail.com>
2020-12-23 21:20:43 -0800

    Workaround for GCC 4.8 bug 60199
    error: field initializer is not constant
    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=60199

--- fem/libceed/diffusion.cpp
+++ fem/libceed/diffusion.cpp
@@ -27,17 +27,23 @@ struct CeedDiffusionInfo
    static constexpr const char *apply_func = ":f_apply_diff";
    static constexpr const char *apply_func_mf_const = ":f_apply_diff_mf_const";
    static constexpr const char *apply_func_mf_quad = ":f_apply_diff_mf_quad";
-   static constexpr CeedQFunctionUser build_qf_const = f_build_diff_const;
-   static constexpr CeedQFunctionUser build_qf_quad = f_build_diff_quad;
-   static constexpr CeedQFunctionUser apply_qf = f_apply_diff;
-   static constexpr CeedQFunctionUser apply_qf_mf_const = f_apply_diff_mf_const;
-   static constexpr CeedQFunctionUser apply_qf_mf_quad = f_apply_diff_mf_quad;
+   static const CeedQFunctionUser build_qf_const;
+   static const CeedQFunctionUser build_qf_quad;
+   static const CeedQFunctionUser apply_qf;
+   static const CeedQFunctionUser apply_qf_mf_const;
+   static const CeedQFunctionUser apply_qf_mf_quad;
    static constexpr EvalMode trial_op = EvalMode::Grad;
    static constexpr EvalMode test_op = EvalMode::Grad;
    const int qdatasize;
    DiffusionContext ctx;
    CeedDiffusionInfo(int dim) : qdatasize(dim*(dim+1)/2) { }
 };
+
+const CeedQFunctionUser CeedDiffusionInfo::build_qf_const = f_build_diff_const;
+const CeedQFunctionUser CeedDiffusionInfo::build_qf_quad = f_build_diff_quad;
+const CeedQFunctionUser CeedDiffusionInfo::apply_qf = f_apply_diff;
+const CeedQFunctionUser CeedDiffusionInfo::apply_qf_mf_const = f_apply_diff_mf_const;
+const CeedQFunctionUser CeedDiffusionInfo::apply_qf_mf_quad = f_apply_diff_mf_quad;
 
 CeedPADiffusionIntegrator::CeedPADiffusionIntegrator(
    const FiniteElementSpace &fes,
