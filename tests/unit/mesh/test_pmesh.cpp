defa968cb21eec0012f28e09e7b51c4336a3296b
Dylan Copeland <dylancopeland@gmail.com>
2020-12-29 10:10:20 -0800

    Adding some AMR tests for element global indices, and more documentation.

--- tests/unit/mesh/test_pmesh.cpp
+++ tests/unit/mesh/test_pmesh.cpp
@@ -22,75 +22,99 @@ TEST_CASE("ParMeshGlobalIndices",  "[Parallel], [ParMesh]")
 
    for (int dimension = 1; dimension < 4; ++dimension)
    {
-      Mesh* mesh;
-      if (dimension == 1)
+      for (int amr=0; amr < 1 + (dimension > 1); ++amr)
       {
-         mesh = new Mesh(ne, 1.0);
-      }
-      else if (dimension == 2)
-      {
-         mesh = new Mesh(ne, ne, Element::QUADRILATERAL, 1, 1.0, 1.0);
-      }
-      else
-      {
-         mesh = new Mesh(ne, ne, ne, Element::HEXAHEDRON, 1, 1.0, 1.0, 1.0);
-      }
-
-      ParMesh pmesh(MPI_COMM_WORLD, *mesh);
-
-      int globalN = 0;
-
-      enum EntityType { VERTEX, EDGE, FACE, ELEMENT };
-      // Loop over all types of mesh entities
-      for (int e=EntityType::VERTEX; e<=EntityType::ELEMENT; ++e)
-      {
-         Array<HYPRE_Int> gi;
-
-         switch (e)
+         Mesh* mesh;
+         if (dimension == 1)
          {
-            case EntityType::VERTEX:
-               globalN = mesh->GetNV();
-               pmesh.GetGlobalVertexIndices(gi);
-               break;
-            case EntityType::EDGE:
-               globalN = dimension == 1 ? mesh->GetNV() : mesh->GetNEdges();
-               pmesh.GetGlobalEdgeIndices(gi);
-               break;
-            case EntityType::FACE:
-               globalN = mesh->GetNumFaces();
-               pmesh.GetGlobalFaceIndices(gi);
-               break;
-            case EntityType::ELEMENT:
-               globalN = mesh->GetNE();
-               pmesh.GetGlobalElementIndices(gi);
-               break;
+            mesh = new Mesh(ne, 1.0);
+         }
+         else if (dimension == 2)
+         {
+            if (amr)
+            {
+               const char *mesh_file = "../../data/amr-quad.mesh";
+               mesh = new Mesh(mesh_file, 1, 1);
+            }
+            else
+            {
+               mesh = new Mesh(ne, ne, Element::QUADRILATERAL, 1, 1.0, 1.0);
+            }
+         }
+         else
+         {
+            if (amr)
+            {
+               const char *mesh_file = "../../data/amr-hex.mesh";
+               mesh = new Mesh(mesh_file, 1, 1);
+            }
+            else
+            {
+               mesh = new Mesh(ne, ne, ne, Element::HEXAHEDRON, 1, 1.0, 1.0, 1.0);
+            }
          }
 
-         // Verify that the local entities do not share a global index.
+         ParMesh pmesh(MPI_COMM_WORLD, *mesh);
+
+         int globalN = 0;
+
+         enum EntityType { VERTEX, EDGE, FACE, ELEMENT };
+         // Loop over all types of mesh entities
+         for (int e=EntityType::VERTEX; e<=EntityType::ELEMENT; ++e)
          {
-            std::set<HYPRE_Int> localGI;
-            for (int i=0; i<gi.Size(); ++i)
+            if (amr && dimension > 1 && e != EntityType::ELEMENT)
             {
-               localGI.insert(gi[i]);
+               continue;
             }
 
-            REQUIRE(localGI.size() == (std::size_t) gi.Size());
+            Array<HYPRE_Int> gi;
+
+            switch (e)
+            {
+               case EntityType::VERTEX:
+                  globalN = mesh->GetNV();
+                  pmesh.GetGlobalVertexIndices(gi);
+                  break;
+               case EntityType::EDGE:
+                  globalN = dimension == 1 ? mesh->GetNV() : mesh->GetNEdges();
+                  pmesh.GetGlobalEdgeIndices(gi);
+                  break;
+               case EntityType::FACE:
+                  globalN = mesh->GetNumFaces();
+                  pmesh.GetGlobalFaceIndices(gi);
+                  break;
+               case EntityType::ELEMENT:
+                  globalN = mesh->GetNE();
+                  pmesh.GetGlobalElementIndices(gi);
+                  break;
+            }
+
+            // Verify that the local entities do not share a global index.
+            {
+               std::set<HYPRE_Int> localGI;
+               for (int i=0; i<gi.Size(); ++i)
+               {
+                  localGI.insert(gi[i]);
+               }
+
+               REQUIRE(localGI.size() == (std::size_t) gi.Size());
+            }
+
+            // Verify that the global indices range from 0 to globalN-1.
+            {
+               const HYPRE_Int localMin = gi.Min();
+               const HYPRE_Int localMax = gi.Max();
+
+               HYPRE_Int globalMin, globalMax;
+               MPI_Allreduce(&localMin, &globalMin, 1, HYPRE_MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+               MPI_Allreduce(&localMax, &globalMax, 1, HYPRE_MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+
+               REQUIRE((globalMin == 0 && globalMax == globalN-1));
+            }
          }
 
-         // Verify that the global indices range from 0 to globalN-1.
-         {
-            const HYPRE_Int localMin = gi.Min();
-            const HYPRE_Int localMax = gi.Max();
-
-            HYPRE_Int globalMin, globalMax;
-            MPI_Allreduce(&localMin, &globalMin, 1, HYPRE_MPI_INT, MPI_MIN, MPI_COMM_WORLD);
-            MPI_Allreduce(&localMax, &globalMax, 1, HYPRE_MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-
-            REQUIRE((globalMin == 0 && globalMax == globalN-1));
-         }
+         delete mesh;
       }
-
-      delete mesh;
    }
 }
 
