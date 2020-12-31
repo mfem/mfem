9d87c4efcd3867c71ab5d688950cb79b03a00c0f
Stowell, Mark L <stowell1@llnl.gov>
2020-12-31 12:48:10 -0800

    Expanding product interpolator tests to include 1D and 2D tests

--- tests/unit/fem/test_lin_interp.cpp
+++ tests/unit/fem/test_lin_interp.cpp
@@ -74,15 +74,44 @@ void CurlF3(const Vector & x, Vector & df)
 double DivF3(const Vector & x)
 { return 1.234 + 4.321 + 3.234; }
 
+double g1(const Vector & x) { return 4.234 * x[0]; }
+double g2(const Vector & x) { return 4.234 * x[0] + 3.357 * x[1]; }
 double g3(const Vector & x)
 { return 4.234 * x[0] + 3.357 * x[1] + 1.572 * x[2]; }
 
+void G2(const Vector & x, Vector & v)
+{
+   v.SetSize(2);
+   v[0] = 4.234 * x[0] + 3.357 * x[1];
+   v[1] = 4.537 * x[0] + 1.321 * x[1];
+}
 void G3(const Vector & x, Vector & v)
 {
    v.SetSize(3);
    v[0] = 4.234 * x[0] + 3.357 * x[1] + 1.572 * x[2];
    v[1] = 4.537 * x[0] + 1.321 * x[1] + 2.234 * x[2];
    v[2] = 1.572 * x[0] + 2.321 * x[1] + 3.234 * x[2];
+}
+
+double fg1(const Vector & x) { return f1(x) * g1(x); }
+
+double fg2(const Vector & x) { return f2(x) * g2(x); }
+void   fG2(const Vector & x, Vector & v) { G2(x, v); v *= f2(x); }
+void   Fg2(const Vector & x, Vector & v) { F2(x, v); v *= g2(x); }
+
+void FcrossG2(const Vector & x, Vector & FxG)
+{
+   Vector F; F2(x, F);
+   Vector G; G2(x, G);
+   FxG.SetSize(1);
+   FxG(0) = F(0) * G(1) - F(1) * G(0);
+}
+
+double FdotG2(const Vector & x)
+{
+   Vector F; F2(x, F);
+   Vector G; G2(x, G);
+   return F * G;
 }
 
 double fg3(const Vector & x) { return f3(x) * g3(x); }
@@ -696,56 +725,77 @@ TEST_CASE("Derivative Linear Interpolators",
    }
 }
 
-TEST_CASE("3D Product Linear Interpolators",
+TEST_CASE("Product Linear Interpolators",
           "[ScalarProductInterpolator]"
           "[VectorScalarProductInterpolator]"
           "[ScalarVectorProductInterpolator]"
+          "[ScalarCrossProductInterpolator]"
           "[VectorCrossProductInterpolator]"
           "[VectorInnerProductInterpolator]")
 {
-   int order_h1 = 1, order_nd = 2, order_rt = 2, n = 3, dim = 3;
+   int order_h1 = 1, order_nd = 2, order_rt = 2, n = 3, dim = -1;
    double tol = 1e-9;
 
-   FunctionCoefficient       fCoef(f3);
-   VectorFunctionCoefficient FCoef(dim, F3);
-
-   FunctionCoefficient       gCoef(g3);
-   VectorFunctionCoefficient GCoef(dim, G3);
-
-   FunctionCoefficient        fgCoef(fg3);
-   FunctionCoefficient        FGCoef(FdotG3);
-   VectorFunctionCoefficient  fGCoef(dim, fG3);
-   VectorFunctionCoefficient  FgCoef(dim, Fg3);
-   VectorFunctionCoefficient FxGCoef(dim, FcrossG3);
-
-   for (int type = (int)Element::TETRAHEDRON;
+   for (int type = (int)Element::SEGMENT;
         type <= (int)Element::HEXAHEDRON; type++)
    {
-      Mesh mesh(n, n, n, (Element::Type)type, 1, 2.0, 3.0, 5.0);
+      Mesh *mesh = NULL;
 
-      if (type == Element::TETRAHEDRON)
+      if (type < (int)Element::TRIANGLE)
       {
-         mesh.ReorientTetMesh();
+         dim = 1;
+         mesh = new Mesh(n, (Element::Type)type, 1, 2.0);
+
       }
+      else if (type < (int)Element::TETRAHEDRON)
+      {
+         dim = 2;
+         mesh = new Mesh(n, n, (Element::Type)type, 1, 2.0, 3.0);
+      }
+      else
+      {
+         dim = 3;
+         mesh = new Mesh(n, n, n, (Element::Type)type, 1, 2.0, 3.0, 5.0);
+
+         if (type == Element::TETRAHEDRON)
+         {
+            mesh->ReorientTetMesh();
+         }
+      }
+
+      FunctionCoefficient        fCoef((dim==1) ? f1 :
+                                       ((dim==2) ? f2 : f3));
+      FunctionCoefficient        gCoef((dim==1) ? g1 :
+                                       ((dim==2) ? g2 : g3));
+      FunctionCoefficient        fgCoef((dim==1) ? fg1 :
+                                        ((dim==2) ? fg2 : fg3));
+
+      VectorFunctionCoefficient   FCoef(dim,
+                                        (dim==2) ? F2 : F3);
+      VectorFunctionCoefficient   GCoef(dim,
+                                        (dim==2) ? G2 : G3);
+
+      FunctionCoefficient        FGCoef((dim==2) ? FdotG2 : FdotG3);
+      VectorFunctionCoefficient  fGCoef(dim, (dim==2) ? fG2 : fG3);
+      VectorFunctionCoefficient  FgCoef(dim, (dim==2) ? Fg2 : Fg3);
+      VectorFunctionCoefficient FxGCoef(dim, (dim==2) ? FcrossG2 : FcrossG3);
 
       SECTION("Operators on H1 for element type " + std::to_string(type))
       {
          H1_FECollection    fec_h1(order_h1, dim);
-         FiniteElementSpace fespace_h1(&mesh, &fec_h1);
+         FiniteElementSpace fespace_h1(mesh, &fec_h1);
 
          GridFunction g0(&fespace_h1);
          g0.ProjectCoefficient(gCoef);
 
          SECTION("Mapping H1 to H1")
          {
-            GridFunction f0(&fespace_h1);
-            f0.ProjectCoefficient(fCoef);
-
             H1_FECollection    fec_h1p(2*order_h1, dim);
-            FiniteElementSpace fespace_h1p(&mesh, &fec_h1p);
+            FiniteElementSpace fespace_h1p(mesh, &fec_h1p);
 
             DiscreteLinearOperator Opf0(&fespace_h1,&fespace_h1p);
-            Opf0.AddDomainInterpolator(new ScalarProductInterpolator(fCoef));
+            Opf0.AddDomainInterpolator(
+               new ScalarProductInterpolator(fCoef));
             Opf0.Assemble();
 
             GridFunction fg0(&fespace_h1p);
@@ -753,120 +803,137 @@ TEST_CASE("3D Product Linear Interpolators",
 
             REQUIRE( fg0.ComputeL2Error(fgCoef) < tol );
          }
-         SECTION("Mapping to HCurl")
+         if (dim > 1)
          {
-            ND_FECollection    fec_nd(order_nd, dim);
-            FiniteElementSpace fespace_nd(&mesh, &fec_nd);
+            SECTION("Mapping to HCurl")
+            {
+               ND_FECollection    fec_nd(order_nd, dim);
+               FiniteElementSpace fespace_nd(mesh, &fec_nd);
 
-            GridFunction F1(&fespace_nd);
-            F1.ProjectCoefficient(FCoef);
+               ND_FECollection    fec_ndp(order_h1+order_nd, dim);
+               FiniteElementSpace fespace_ndp(mesh, &fec_ndp);
 
-            ND_FECollection    fec_ndp(order_h1+order_nd, dim);
-            FiniteElementSpace fespace_ndp(&mesh, &fec_ndp);
+               DiscreteLinearOperator OpF1(&fespace_h1,&fespace_ndp);
+               OpF1.AddDomainInterpolator(
+                  new VectorScalarProductInterpolator(FCoef));
+               OpF1.Assemble();
 
-            DiscreteLinearOperator OpF1(&fespace_h1,&fespace_ndp);
-            OpF1.AddDomainInterpolator(new VectorScalarProductInterpolator(FCoef));
-            OpF1.Assemble();
+               GridFunction Fg1(&fespace_ndp);
+               OpF1.Mult(g0,Fg1);
 
-            GridFunction Fg1(&fespace_ndp);
-            OpF1.Mult(g0,Fg1);
-
-            REQUIRE( Fg1.ComputeL2Error(FgCoef) < tol );
+               REQUIRE( Fg1.ComputeL2Error(FgCoef) < tol );
+            }
          }
       }
-      SECTION("Operators on HCurl for element type " + std::to_string(type))
+      if (dim > 1)
       {
-         ND_FECollection    fec_nd(order_nd, dim);
-         FiniteElementSpace fespace_nd(&mesh, &fec_nd);
-
-         GridFunction G1(&fespace_nd);
-         G1.ProjectCoefficient(GCoef);
-
-         SECTION("Mapping HCurl to HCurl")
+         SECTION("Operators on HCurl for element type " + std::to_string(type))
          {
-            H1_FECollection    fec_h1(order_h1, dim);
-            FiniteElementSpace fespace_h1(&mesh, &fec_h1);
+            ND_FECollection    fec_nd(order_nd, dim);
+            FiniteElementSpace fespace_nd(mesh, &fec_nd);
 
-            GridFunction f0(&fespace_h1);
-            f0.ProjectCoefficient(fCoef);
+            GridFunction G1(&fespace_nd);
+            G1.ProjectCoefficient(GCoef);
 
-            ND_FECollection    fec_ndp(order_nd+order_h1, dim);
-            FiniteElementSpace fespace_ndp(&mesh, &fec_ndp);
+            SECTION("Mapping HCurl to HCurl")
+            {
+               H1_FECollection    fec_h1(order_h1, dim);
+               FiniteElementSpace fespace_h1(mesh, &fec_h1);
 
-            DiscreteLinearOperator Opf0(&fespace_nd,&fespace_ndp);
-            Opf0.AddDomainInterpolator(new ScalarVectorProductInterpolator(fCoef));
-            Opf0.Assemble();
+               ND_FECollection    fec_ndp(order_nd+order_h1, dim);
+               FiniteElementSpace fespace_ndp(mesh, &fec_ndp);
 
-            GridFunction fG1(&fespace_ndp);
-            Opf0.Mult(G1,fG1);
+               DiscreteLinearOperator Opf0(&fespace_nd,&fespace_ndp);
+               Opf0.AddDomainInterpolator(
+                  new ScalarVectorProductInterpolator(fCoef));
+               Opf0.Assemble();
 
-            REQUIRE( fG1.ComputeL2Error(fGCoef) < tol );
+               GridFunction fG1(&fespace_ndp);
+               Opf0.Mult(G1,fG1);
+
+               REQUIRE( fG1.ComputeL2Error(fGCoef) < tol );
+            }
+            if (dim == 2)
+            {
+               SECTION("Mapping to L2")
+               {
+                  L2_FECollection    fec_l2p(2*order_nd-1, dim);
+                  FiniteElementSpace fespace_l2p(mesh, &fec_l2p);
+
+                  DiscreteLinearOperator OpF1(&fespace_nd,&fespace_l2p);
+                  OpF1.AddDomainInterpolator(
+                     new ScalarCrossProductInterpolator(FCoef));
+                  OpF1.Assemble();
+
+                  GridFunction FxG2(&fespace_l2p);
+                  OpF1.Mult(G1,FxG2);
+
+                  REQUIRE( FxG2.ComputeL2Error(FxGCoef) < tol );
+               }
+            }
+            else
+            {
+               SECTION("Mapping to HDiv")
+               {
+                  RT_FECollection    fec_rtp(2*order_nd-1, dim);
+                  FiniteElementSpace fespace_rtp(mesh, &fec_rtp);
+
+                  DiscreteLinearOperator OpF1(&fespace_nd,&fespace_rtp);
+                  OpF1.AddDomainInterpolator(
+                     new VectorCrossProductInterpolator(FCoef));
+                  OpF1.Assemble();
+
+                  GridFunction FxG2(&fespace_rtp);
+                  OpF1.Mult(G1,FxG2);
+
+                  REQUIRE( FxG2.ComputeL2Error(FxGCoef) < tol );
+               }
+            }
+            SECTION("Mapping to L2")
+            {
+               RT_FECollection    fec_rt(order_rt, dim);
+               FiniteElementSpace fespace_rt(mesh, &fec_rt);
+
+               L2_FECollection    fec_l2p(order_nd+order_rt, dim);
+               FiniteElementSpace fespace_l2p(mesh, &fec_l2p);
+
+               DiscreteLinearOperator OpF2(&fespace_nd,&fespace_l2p);
+               OpF2.AddDomainInterpolator(
+                  new VectorInnerProductInterpolator(FCoef));
+               OpF2.Assemble();
+
+               GridFunction FG3(&fespace_l2p);
+               OpF2.Mult(G1,FG3);
+
+               REQUIRE( FG3.ComputeL2Error(FGCoef) < tol );
+            }
          }
-         SECTION("Mapping to HDiv")
-         {
-            GridFunction F1(&fespace_nd);
-            F1.ProjectCoefficient(FCoef);
-
-            RT_FECollection    fec_rtp(2*order_nd, dim);
-            FiniteElementSpace fespace_rtp(&mesh, &fec_rtp);
-
-            DiscreteLinearOperator OpF1(&fespace_nd,&fespace_rtp);
-            OpF1.AddDomainInterpolator(new VectorCrossProductInterpolator(FCoef));
-            OpF1.Assemble();
-
-            GridFunction FxG2(&fespace_rtp);
-            OpF1.Mult(G1,FxG2);
-
-            REQUIRE( FxG2.ComputeL2Error(FxGCoef) < tol );
-         }
-         SECTION("Mapping to L2")
+         SECTION("Operators on HDiv for element type " + std::to_string(type))
          {
             RT_FECollection    fec_rt(order_rt, dim);
-            FiniteElementSpace fespace_rt(&mesh, &fec_rt);
+            FiniteElementSpace fespace_rt(mesh, &fec_rt);
 
-            GridFunction F2(&fespace_rt);
-            F2.ProjectCoefficient(FCoef);
+            GridFunction G2(&fespace_rt);
+            G2.ProjectCoefficient(GCoef);
 
-            L2_FECollection    fec_l2p(order_nd+order_rt, dim);
-            FiniteElementSpace fespace_l2p(&mesh, &fec_l2p);
+            SECTION("Mapping to L2")
+            {
+               ND_FECollection    fec_nd(order_nd, dim);
+               FiniteElementSpace fespace_nd(mesh, &fec_nd);
 
-            DiscreteLinearOperator OpF2(&fespace_nd,&fespace_l2p);
-            OpF2.AddDomainInterpolator(new VectorInnerProductInterpolator(FCoef));
-            OpF2.Assemble();
+               L2_FECollection    fec_l2p(order_nd+order_rt, dim);
+               FiniteElementSpace fespace_l2p(mesh, &fec_l2p);
 
-            GridFunction FG3(&fespace_l2p);
-            OpF2.Mult(G1,FG3);
+               DiscreteLinearOperator OpF1(&fespace_rt,&fespace_l2p);
+               OpF1.AddDomainInterpolator(
+                  new VectorInnerProductInterpolator(FCoef));
+               OpF1.Assemble();
 
-            REQUIRE( FG3.ComputeL2Error(FGCoef) < tol );
-         }
-      }
-      SECTION("Operators on HDiv for element type " + std::to_string(type))
-      {
-         RT_FECollection    fec_rt(order_rt, dim);
-         FiniteElementSpace fespace_rt(&mesh, &fec_rt);
+               GridFunction FG3(&fespace_l2p);
+               OpF1.Mult(G2,FG3);
 
-         GridFunction G2(&fespace_rt);
-         G2.ProjectCoefficient(GCoef);
-
-         SECTION("Mapping to L2")
-         {
-            ND_FECollection    fec_nd(order_nd, dim);
-            FiniteElementSpace fespace_nd(&mesh, &fec_nd);
-
-            GridFunction F1(&fespace_nd);
-            F1.ProjectCoefficient(FCoef);
-
-            L2_FECollection    fec_l2p(order_nd+order_rt, dim);
-            FiniteElementSpace fespace_l2p(&mesh, &fec_l2p);
-
-            DiscreteLinearOperator OpF1(&fespace_rt,&fespace_l2p);
-            OpF1.AddDomainInterpolator(new VectorInnerProductInterpolator(FCoef));
-            OpF1.Assemble();
-
-            GridFunction FG3(&fespace_l2p);
-            OpF1.Mult(G2,FG3);
-
-            REQUIRE( FG3.ComputeL2Error(FGCoef) < tol );
+               REQUIRE( FG3.ComputeL2Error(FGCoef) < tol );
+            }
          }
       }
    }
