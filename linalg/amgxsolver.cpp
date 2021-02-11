5bb9067c435a006436e839740d4d824674e112a7
Arturo Vargas <artv3.89@gmail.com>
2021-02-10 16:41:09 -0800

    remove null pointer setting

--- linalg/amgxsolver.cpp
+++ linalg/amgxsolver.cpp
@@ -625,9 +625,7 @@ void AmgXSolver::SetMatrix(const HypreParMatrix &A, const bool update_mat)
       return SetMatrixMPITeams(A, loc_A, loc_I, loc_J, update_mat);
    }
 
-   // Set A_ptr to NULL
    // Free A_csr data from hypre_MergeDiagAndOffd method
-   A_ptr = NULL;
    hypre_CSRMatrixDestroy(A_csr);
 
    mfem_error("Unsupported MPI_GPU combination \n");
