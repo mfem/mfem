e1e154c92be0f11507d3e02badf3a4b43d428563
Veselin Dobrev <dobrev@llnl.gov>
2021-02-17 23:48:01 -0800

    Add a method mfem::Device::GetId().
    this way we can tell PETSc to use the same device.

--- linalg/petsc.cpp
+++ linalg/petsc.cpp
@@ -168,6 +168,12 @@ void MFEMInitializePetsc(int *argc,char*** argv)
 void MFEMInitializePetsc(int *argc,char ***argv,const char rc_file[],
                          const char help[])
 {
+   if (mfem::Device::Allows(mfem::Backend::CUDA_MASK))
+   {
+      // Tell PETSc to use the same CUDA device as MFEM:
+      ierr = PetscOptionsSetValue(NULL,"-cuda_device", to_string(mfem::Device::GetId()).c_str());
+      MFEM_VERIFY(!ierr,"Unable to set initial option value to PETSc");
+   }
    ierr = PetscInitialize(argc,argv,rc_file,help);
    MFEM_VERIFY(!ierr,"Unable to initialize PETSc");
 }
