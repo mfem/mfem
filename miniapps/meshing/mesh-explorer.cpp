33315f7cfd0d65ee7e837bcf67c36a8e7b94c9a9
Will Pazner <will.e.p@gmail.com>
2021-02-18 19:46:31 -0800

    Change mesh constructors in mesh-explorer

--- miniapps/meshing/mesh-explorer.cpp
+++ miniapps/meshing/mesh-explorer.cpp
@@ -404,10 +404,7 @@ int main (int argc, char *argv[])
                if (ref_factor <= 1 || ref_factor > 32) { break; }
                int ref_type = (sk == 'u') ? BasisType::ClosedUniform :
                               BasisType::GaussLobatto;
-               Mesh *rmesh = new Mesh(
-                  Mesh::MakeRefined(*mesh, ref_factor, ref_type));
-               delete mesh;
-               mesh = rmesh;
+               *mesh = Mesh::MakeRefined(*mesh, ref_factor, ref_type);
                break;
             }
             case 'l':
