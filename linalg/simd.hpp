9a01d9f9a92260b90847bd4cf0e10a8beba5904a
camierjs <camier1@llnl.gov>
2021-02-23 17:38:53 -0800

    Add SVE intrinsics

--- linalg/simd.hpp
+++ linalg/simd.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010-2020, Lawrence Livermore National Security, LLC. Produced
+// Copyright (c) 2010-2021, Lawrence Livermore National Security, LLC. Produced
 // at the Lawrence Livermore National Laboratory. All Rights reserved. See files
 // LICENSE and NOTICE for details. LLNL-CODE-806117.
 //
@@ -16,10 +16,14 @@
 
 // --- AutoSIMD + specializations with intrinsics
 #include "simd/auto.hpp"
+
 #ifdef MFEM_USE_SIMD
-#if defined(__VSX__)
+#if defined(__aarch64__) && defined(__ARM_FEATURE_SVE)
+#warning simd/sve.hpp
+#include "simd/sve.hpp"
+#elif defined(__VSX__)
 #include "simd/vsx.hpp"
-#elif defined (__bgq__)
+#elif defined(__bgq__)
 #include "simd/qpx.hpp"
 #elif defined(__x86_64__) || defined(_M_X64) || defined(_M_IX86)
 #include "simd/x86.hpp"
@@ -27,8 +31,10 @@
 #warning Unknown SIMD architecture
 #else
 #pragma message("warning: Unknown SIMD architecture")
-#endif
-#endif
+#endif // ARCHITECTURE
+#else
+#warning !MFEM_USE_SIMD
+#endif // MFEM_USE_SIMD
 
 // MFEM_SIMD_BYTES is the default SIMD size used by MFEM, see e.g. class
 // TBilinearForm and the default traits class AutoSIMDTraits.
@@ -40,6 +46,9 @@
 #define MFEM_SIMD_BYTES 8
 #define MFEM_ALIGN_BYTES 32
 #elif defined(__AVX512F__)
+#define MFEM_SIMD_BYTES 64
+#define MFEM_ALIGN_BYTES 64
+#elif defined(__aarch64__) && defined(__ARM_FEATURE_SVE)
 #define MFEM_SIMD_BYTES 64
 #define MFEM_ALIGN_BYTES 64
 #elif defined(__AVX__) || defined(__VECTOR4DOUBLE__)
@@ -61,21 +70,21 @@
 namespace mfem
 {
 
-template<typename complex_t, typename real_t>
+template <typename complex_t, typename real_t>
 struct AutoSIMDTraits
 {
    static const int block_size = MFEM_TEMPLATE_BLOCK_SIZE;
 
    // Alignment for arrays of vcomplex_t and vreal_t
-   static const int align_bytes = MFEM_SIMD_BYTES;
+   static const int align_bytes = MFEM_ALIGN_BYTES;
 
    static const int batch_size = 1;
 
-   static const int simd_size = MFEM_SIMD_BYTES/sizeof(real_t);
+   static const int simd_size = MFEM_SIMD_BYTES / sizeof(real_t);
 
-   typedef AutoSIMD<complex_t, simd_size, MFEM_SIMD_BYTES> vcomplex_t;
-   typedef AutoSIMD<real_t, simd_size, MFEM_SIMD_BYTES> vreal_t;
-   typedef AutoSIMD<int, simd_size, simd_size*sizeof(int)> vint_t;
+   typedef AutoSIMD<complex_t, simd_size, MFEM_ALIGN_BYTES> vcomplex_t;
+   typedef AutoSIMD<real_t, simd_size, MFEM_ALIGN_BYTES> vreal_t;
+   typedef AutoSIMD<int, simd_size, simd_size * sizeof(int)> vint_t;
 };
 
 template<typename complex_t, typename real_t>
---
+++ linalg/simd/sve.hpp
@@ -0,0 +1,289 @@
+// Copyright (c) 2010-2020, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#ifndef MFEM_SIMD_SVE_HPP
+#define MFEM_SIMD_SVE_HPP
+
+#if defined(__ARM_FEATURE_SVE) //||1
+#warning SVE AutoSIMD<double,8,64>
+
+#include "../../config/tconfig.hpp"
+
+#include <arm_sve.h>
+#define PG svptrue_b64()
+
+namespace mfem
+{
+
+// Use this macro as a workaround for astyle formatting issue with 'alignas'
+#define MFEM_AUTOSIMD_ALIGN_SVE alignas(64)
+
+template <typename,int,int> struct AutoSIMD;
+
+template <> struct MFEM_AUTOSIMD_ALIGN_SVE AutoSIMD<double,8,64>
+{
+   typedef double scalar_type;
+   static constexpr int size = 8;
+   static constexpr int align_bytes = 64;
+
+   double vec[size];
+
+   inline MFEM_ALWAYS_INLINE double &operator[](int i)
+   {
+      return vec[i];
+   }
+
+   inline MFEM_ALWAYS_INLINE const double &operator[](int i) const
+   {
+      return vec[i];
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &operator=(const AutoSIMD &v)
+   {
+      svst1_f64(PG, vec, svld1_f64(PG, v.vec));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &operator=(const double &e)
+   {
+      svst1_f64(PG, vec, svdup_n_f64(e));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &operator+=(const AutoSIMD &v)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, vec, svadd_f64_z(PG, vd, vvd));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &operator+=(const double &e)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      svst1_f64(PG, vec, svadd_f64_z(PG, vd, svdup_n_f64(e)));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &operator-=(const AutoSIMD &v)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, vec, svsub_f64_z(PG, vd, vvd));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &operator-=(const double &e)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      svst1_f64(PG, vec, svsub_f64_z(PG, vd, svdup_n_f64(e)));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &operator*=(const AutoSIMD &v)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, vec, svmul_f64_z(PG, vd, vvd));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &operator*=(const double &e)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      svst1_f64(PG, vec, svmul_f64_z(PG, vd, svdup_n_f64(e)));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &operator/=(const AutoSIMD &v)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, vec, svdiv_f64_z(PG, vd, vvd));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &operator/=(const double &e)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      svst1_f64(PG, vec, svdiv_f64_z(PG, vd, svdup_n_f64(e)));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD operator-() const
+   {
+      AutoSIMD r;
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      svst1_f64(PG, r.vec, svneg_f64_z(PG, vd));
+      return r;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD operator+(const AutoSIMD &v) const
+   {
+      AutoSIMD r;
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, r.vec, svadd_f64_z(PG, vd, vvd));
+      return r;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD operator+(const double &e) const
+   {
+      AutoSIMD r;
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      svst1_f64(PG, r.vec, svadd_f64_z(PG, vd, svdup_n_f64(e)));
+      return r;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD operator-(const AutoSIMD &v) const
+   {
+      AutoSIMD r;
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, r.vec, svsub_f64_z(PG, vd, vvd));
+      return r;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD operator-(const double &e) const
+   {
+      AutoSIMD r;
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      svst1_f64(PG, r.vec, svsub_f64_z(PG, vd, svdup_n_f64(e)));
+      return r;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD operator*(const AutoSIMD &v) const
+   {
+      AutoSIMD r;
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, r.vec, svmul_f64_z(PG, vd, vvd));
+      return r;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD operator*(const double &e) const
+   {
+      AutoSIMD r;
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      svst1_f64(PG, r.vec, svmul_f64_z(PG, vd, svdup_n_f64(e)));
+      return r;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD operator/(const AutoSIMD &v) const
+   {
+      AutoSIMD r;
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, r.vec, svdiv_f64_z(PG, vd, vvd));
+      return r;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD operator/(const double &e) const
+   {
+      AutoSIMD r;
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      svst1_f64(PG, r.vec, svdiv_f64_z(PG, vd, svdup_n_f64(e)));
+      return r;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &fma(const AutoSIMD &v, const AutoSIMD &w)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      const svfloat64_t wvd = svld1_f64(PG, w.vec);
+      svst1_f64(PG, vec, svmad_f64_z(PG, wvd, vd, vvd));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &fma(const AutoSIMD &v, const double &e)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, vec, svmad_f64_z(PG, vvd, svdup_n_f64(e),vd));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &fma(const double &e, const AutoSIMD &v)
+   {
+      const svfloat64_t vd = svld1_f64(PG, vec);
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, vec, svmad_f64_z(PG, svdup_n_f64(e),vvd,vd));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &mul(const AutoSIMD &v, const AutoSIMD &w)
+   {
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      const svfloat64_t wvd = svld1_f64(PG, w.vec);
+      svst1_f64(PG, vec, svmul_f64_z(PG, vvd, wvd));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &mul(const AutoSIMD &v, const double &e)
+   {
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, vec, svmul_f64_z(PG, vvd, svdup_n_f64(e)));
+      return *this;
+   }
+
+   inline MFEM_ALWAYS_INLINE AutoSIMD &mul(const double &e, const AutoSIMD &v)
+   {
+      const svfloat64_t vvd = svld1_f64(PG, v.vec);
+      svst1_f64(PG, vec, svmul_f64_z(PG, svdup_n_f64(e), vvd));
+      return *this;
+   }
+};
+
+inline MFEM_ALWAYS_INLINE
+AutoSIMD<double,8,64> operator+(const double &e,
+                                const AutoSIMD<double,8,64> &v)
+{
+   AutoSIMD<double,8,64> r;
+   const svfloat64_t vvd = svld1_f64(PG, v.vec);
+   svst1_f64(PG, r.vec, svadd_f64_z(PG, svdup_n_f64(e), vvd));
+   return r;
+}
+
+inline MFEM_ALWAYS_INLINE
+AutoSIMD<double,8,64> operator-(const double &e,
+                                const AutoSIMD<double,8,64> &v)
+{
+   AutoSIMD<double,8,64> r;
+   const svfloat64_t vvd = svld1_f64(PG, v.vec);
+   svst1_f64(PG, r.vec, svsub_f64_z(PG, svdup_n_f64(e), vvd));
+   return r;
+}
+
+inline MFEM_ALWAYS_INLINE
+AutoSIMD<double,8,64> operator*(const double &e,
+                                const AutoSIMD<double,8,64> &v)
+{
+   AutoSIMD<double,8,64> r;
+   const svfloat64_t vvd = svld1_f64(PG, v.vec);
+   svst1_f64(PG, r.vec, svmul_f64_z(PG, svdup_n_f64(e), vvd));
+   return r;
+}
+
+inline MFEM_ALWAYS_INLINE
+AutoSIMD<double,8,64> operator/(const double &e,
+                                const AutoSIMD<double,8,64> &v)
+{
+   AutoSIMD<double,8,64> r;
+   const svfloat64_t vvd = svld1_f64(PG, v.vec);
+   svst1_f64(PG, r.vec, svdiv_f64_z(PG, svdup_n_f64(e), vvd));
+   return r;
+}
+
+} // namespace mfem
+
+#endif // __aarch64__
+
+#endif // MFEM_SIMD_SVE_HPP
