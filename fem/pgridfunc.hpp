a99ab59f3bbe9b1a17b3f5be5bc1c40a3f9a48f7
Will Pazner <will.e.p@gmail.com>
2021-03-22 10:57:08 -0700

    Make ParGridFunction::SaveAsOne const

--- fem/pgridfunc.hpp
+++ fem/pgridfunc.hpp
@@ -425,7 +425,7 @@ public:
 #endif
 
    /// Merge the local grid functions
-   void SaveAsOne(std::ostream &out = mfem::out);
+   void SaveAsOne(std::ostream &out = mfem::out) const;
 
    virtual ~ParGridFunction() { }
 };
