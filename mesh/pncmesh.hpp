a8b28f96ae07bc79cde209501da2da2c65784e27
Jakub Červený <cerveny1@llnl.gov>
2021-03-29 10:09:22 +0200

    Documented NCMesh::NCMesh(istream &, ...).

--- mesh/pncmesh.hpp
+++ mesh/pncmesh.hpp
@@ -69,7 +69,8 @@ public:
        passed in 'part', where part[i] is the desired MPI rank for element i. */
    ParNCMesh(MPI_Comm comm, const NCMesh& ncmesh, int* part = NULL);
 
-   /// Load from a stream. The id header is assumed to have been read already.
+   /** Load from a stream, parallel version. See the serial NCMesh::NCMesh
+       counterpart for a description of the parameters. */
    ParNCMesh(MPI_Comm comm, std::istream &input,
              int version, int &curved, int &is_nc);
 
