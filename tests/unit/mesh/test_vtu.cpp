fe0e7bfa41acb70a0d682dbc5cefeb10e1505ec9
Will Pazner <will.e.p@gmail.com>
2021-04-11 08:25:28 -0700

    make style

--- tests/unit/mesh/test_vtu.cpp
+++ tests/unit/mesh/test_vtu.cpp
@@ -16,13 +16,15 @@ using namespace mfem;
 
 TEST_CASE("VTU XML Reader", "[Mesh][VTU][XML]")
 {
-   std::vector<std::string> mesh_filenames = {
+   std::vector<std::string> mesh_filenames =
+   {
       "quad_append_b64.vtu", "quad_append_raw.vtu",
       "quad_ascii.vtu", "quad_binary.vtu"
    };
 #ifdef MFEM_USE_ZLIB
    // Test VTU meshes with binary compression only if compiled with zlib enabled
-   mesh_filenames.insert(mesh_filenames.end(), {
+   mesh_filenames.insert(mesh_filenames.end(),
+   {
       "quad_append_b64_compress.vtu", "quad_append_raw_compress.vtu",
       "quad_binary_compress.vtu"
    });
