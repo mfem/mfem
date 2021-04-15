d199fa9e58e0bae9c0314972865fd087fa6a28fa
Will Pazner <will.e.p@gmail.com>
2021-04-15 16:50:10 -0700

    Add simplified plor2 example

--- examples/plor2.cpp
+++ examples/plor2.cpp
@@ -84,47 +84,64 @@ void f_exact(const Vector &xvec, Vector &f)
 
 int main(int argc, char *argv[])
 {
+   MPI_Session mpi;
+
    // 1. Parse command-line options.
    const char *mesh_file = "../data/star.mesh";
    int ref_levels = 0;
    int order = 3;
    const char *fe = "n";
+   bool hybridization = false;
 
    OptionsParser args(argc, argv);
    args.AddOption(&mesh_file, "-m", "--mesh", "Mesh file to use.");
    args.AddOption(&ref_levels, "-r", "--refine", "Uniform refinements.");
    args.AddOption(&order, "-o", "--order", "Polynomial degree.");
-   args.AddOption(&exact_sol, "-p", "--problem", "Exact solution.");
    args.AddOption(&fe, "-fe", "--fe-type", "FE type. n for Hcurl, r for Hdiv");
-   args.ParseCheck();
+   args.AddOption(&hybridization, "-hb", "--hybridization", "-no-hb",
+                  "--no-hybridization", "Enable hybridization.");
+   args.Parse();
+   if (!args.Good())
+   {
+      args.PrintUsage(cout);
+      return 1;
+   }
+   args.PrintOptions(cout);
 
-   bool ND;
+   bool ND = false;
    if (string(fe) == "n") { ND = true; }
    else if (string(fe) == "r") { ND = false; }
    else { MFEM_ABORT("Bad FE type. Must be 'n' or 'r'."); }
+   bool RT = !ND;
 
-   Mesh mesh(mesh_file, 1, 1);
-   dim = mesh.Dimension();
-   for (int l = 0; l < ref_levels; l++) { mesh.UniformRefinement(); }
+   Mesh serial_mesh(mesh_file, 1, 1);
+   int dim = serial_mesh.Dimension();
+   for (int l = 0; l < ref_levels; l++) { serial_mesh.UniformRefinement(); }
+   ParMesh mesh(MPI_COMM_WORLD, serial_mesh);
+   serial_mesh.Clear();
 
    int b1 = BasisType::GaussLobatto, b2 = BasisType::Integrated;
+   ParMesh mesh_lor = ParMesh::MakeRefined(mesh, order, b1);
+
    unique_ptr<FiniteElementCollection> fec;
    if (ND) { fec.reset(new ND_FECollection(order, dim, b1, b2)); }
    else { fec.reset(new RT_FECollection(order-1, dim, b1, b2)); }
 
-   FiniteElementSpace fes(&mesh, fec.get());
+   ParFiniteElementSpace fes(&mesh, fec.get());
    Array<int> ess_dofs;
    fes.GetBoundaryTrueDofs(ess_dofs);
 
-   BilinearForm a_ho(&fes);
-   a_ho.AddDomainIntegrator(new VectorFEMassIntegrator);
-   if (ND) { a_ho.AddDomainIntegrator(new CurlCurlIntegrator); }
-   else { a_ho.AddDomainIntegrator(new DivDivIntegrator); }
-   a_ho.SetAssemblyLevel(AssemblyLevel::PARTIAL);
-   a_ho.Assemble();
+   VectorFunctionCoefficient f_coeff(dim, f_exact);
+
+   ParBilinearForm a(&fes);
+   // ParBilinearForm a(&fes_lor), a_lor(&fes_lor);
+   a.AddDomainIntegrator(new VectorFEMassIntegrator);
+   if (ND) { a.AddDomainIntegrator(new CurlCurlIntegrator); }
+   else { a.AddDomainIntegrator(new DivDivIntegrator); }
+   a.SetAssemblyLevel(AssemblyLevel::PARTIAL);
+   a.Assemble();
 
    LinearForm b(&fes);
-   VectorFunctionCoefficient f_coeff(dim, f_exact);
    b.AddDomainIntegrator(new VectorFEDomainLFIntegrator(f_coeff));
    b.Assemble();
 
@@ -133,19 +150,36 @@ int main(int argc, char *argv[])
 
    Vector X, B;
    OperatorHandle A;
-   a_ho.FormLinearSystem(ess_dofs, x, b, A, X, B);
+   a.FormLinearSystem(ess_dofs, x, b, A, X, B);
 
-   LORSolver<UMFPackSolver> lor_solver(a_ho, ess_dofs);
+   ParLOR lor(a, ess_dofs);
+   ParFiniteElementSpace &fes_lor = lor.GetParFESpace();
 
-   CGSolver cg;
+   unique_ptr<Solver> solv_lor;
+   if (RT && dim == 3)
+   {
+      solv_lor.reset(new LORSolver<HypreADS>(lor, &fes_lor));
+   }
+   else
+   {
+      solv_lor.reset(new LORSolver<HypreAMS>(lor, &fes_lor));
+   }
+
+   CGSolver cg(MPI_COMM_WORLD);
    cg.SetAbsTol(0.0);
    cg.SetRelTol(1e-12);
    cg.SetMaxIter(100);
    cg.SetPrintLevel(1);
+
    cg.SetOperator(*A);
-   cg.SetPreconditioner(lor_solver);
+   cg.SetPreconditioner(*solv_lor);
    cg.Mult(B, X);
-   a_ho.RecoverFEMSolution(X, b, x);
+
+   // cg.SetOperator(*A_lor);
+   // cg.SetPreconditioner(*amg);
+   // cg.Mult(B_lor, X_lor);
+
+   a.RecoverFEMSolution(X, b, x);
 
    VectorFunctionCoefficient exact_coeff(dim, E_exact);
    double er = x.ComputeL2Error(exact_coeff);
