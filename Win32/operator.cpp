3300334f58045ffbfbb2e6e7255218303d1554b1
Benjamin T. Liu <liu15@llnl.gov>
2021-04-16 08:26:46 -0700

    Fix hypre and mfem projects

---
+++ Win32/operator.cpp
@@ -0,0 +1,4 @@
+// The current Windows build system cannot handle multiple files
+// with the same name in a project (operator.cpp), so this file is
+// used as a workaround.
+#include "../fem/ceed/operator.cpp"
