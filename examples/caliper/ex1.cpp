08da01b3add00e73721a65d59ef8d1a09288e74f
Boyan Lazarov <32148077+bslazarov@users.noreply.github.com>
2021-04-19 11:05:04 -0700

    Update examples/caliper/ex1.cpp
    Co-authored-by: Veselin Dobrev <v-dobrev@users.noreply.github.com>

--- examples/caliper/ex1.cpp
+++ examples/caliper/ex1.cpp
@@ -106,7 +106,7 @@ int main(int argc, char *argv[])
    Device device(device_config);
    device.Print();
    
-   //Caliper configuration
+   // Caliper configuration
    mgr.add(cali_config);
    mgr.start();
 
