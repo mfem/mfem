267991ef210e02c2ebaaf4370a62b8da011faca8
Boyan Lazarov <32148077+bslazarov@users.noreply.github.com>
2021-04-19 11:04:15 -0700

    Update examples/caliper/ex1.cpp
    Co-authored-by: Veselin Dobrev <v-dobrev@users.noreply.github.com>

--- examples/caliper/ex1.cpp
+++ examples/caliper/ex1.cpp
@@ -65,6 +65,7 @@ int main(int argc, char *argv[])
    cali::ConfigManager mgr;
    // Caliper instrumentation
    MFEM_PERF_FUNCTION;
+
    // 1. Parse command-line options.
    const char *mesh_file = "../../data/star.mesh";
    int order = 1;
