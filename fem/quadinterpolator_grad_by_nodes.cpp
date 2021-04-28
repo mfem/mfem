db81731de0299e835b4459c73c766305d010fd2b
Veselin Dobrev <dobrev@llnl.gov>
2021-04-28 00:10:19 -0700

    Tweak catch.hpp to print double values in more standard format.
    Update a little the QuadratureInterpolator unit test.
    
    Update error messages in TensorDerivatives<QVectorLayout::byNODES>
    to be similar to the byVDIM version.

--- fem/quadinterpolator_grad_by_nodes.cpp
+++ fem/quadinterpolator_grad_by_nodes.cpp
@@ -112,10 +112,10 @@ void TensorDerivatives<QVectorLayout::byNODES>(const int NE,
          {
             constexpr int MD = 8;
             constexpr int MQ = 8;
-            if (D1D > MD || Q1D > MQ)
-            {
-               MFEM_ABORT("");
-            }
+            MFEM_VERIFY(D1D <= MD, "Orders higher than " << MD-1
+                        << " are not supported!");
+            MFEM_VERIFY(Q1D <= MQ, "Quadrature rules with more than "
+                        << MQ << " 1D points are not supported!");
             Derivatives3D<L,P,0,0,0,MD,MQ>(NE,B,G,J,X,Y,vdim,D1D,Q1D);
             return;
          }
