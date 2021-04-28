c9c181d25bb100f3d588f7c757e3c70a1da0156e
psocratis <petrides1@llnl.gov>
2021-04-27 19:08:17 -0700

    adding entry-wise mult operator *=

--- linalg/vector.hpp
+++ linalg/vector.hpp
@@ -271,6 +271,9 @@ public:
 
    Vector &operator+=(const Vector &v);
 
+   /// entry-wise mult
+   Vector &operator*=(const Vector &v);
+
    /// (*this) += a * Va
    Vector &Add(const double a, const Vector &Va);
 
