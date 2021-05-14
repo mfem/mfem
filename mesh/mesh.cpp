5619dd468bbaa8bca1c0a450f32fd234ca56bc7b
Arturo Vargas <artvar3@yahoo.com>
2021-05-13 22:21:02 -0700

    nodes-> nodes.

--- mesh/mesh.cpp
+++ mesh/mesh.cpp
@@ -11232,7 +11232,7 @@ GeometricFactors::GeometricFactors(const GridFunction &nodes,
                                    const IntegrationRule &ir,
                                    int flags, MemoryType d_mt)
 {
-   this->mesh = nodes->FESpace()->GetMesh();
+   this->mesh = nodes.FESpace()->GetMesh();
    IntRule = &ir;
    computed_factors = flags;
 
