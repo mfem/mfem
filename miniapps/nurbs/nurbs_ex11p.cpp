bdb618772b1fe672a879777eb98611d9149559b6
Stowell, Mark L <stowell1@llnl.gov>
2021-05-17 11:27:48 -0700

    Missed one

--- miniapps/nurbs/nurbs_ex11p.cpp
+++ miniapps/nurbs/nurbs_ex11p.cpp
@@ -328,7 +328,7 @@ int main(int argc, char *argv[])
       for (int i=0; i<nev; i++)
       {
          // convert eigenvector from HypreParVector to ParGridFunction
-         x = const_cast<const HypreLOBPCG*>(lobpcg)->GetEigenvector(i);
+         x = lobpcg->GetEigenvector(i);
 
          mode_name << "mode_" << setfill('0') << setw(2) << i << "."
                    << setfill('0') << setw(6) << myid;
@@ -357,7 +357,7 @@ int main(int argc, char *argv[])
          }
 
          // convert eigenvector from HypreParVector to ParGridFunction
-         x = const_cast<const HypreLOBPCG*>(lobpcg)->GetEigenvector(i);
+         x = lobpcg->GetEigenvector(i);
 
          mode_sock << "parallel " << num_procs << " " << myid << "\n"
                    << "solution\n" << *pmesh << x << flush
