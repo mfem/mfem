3914176e3754b3630c3721b4e095ff4facd07fbf
Will Pazner <will.e.p@gmail.com>
2021-05-17 18:50:16 -0700

    Remove -no-vis and -paraview from ex9 and ex9p sample runs
    In sample runs tests -no-vis was specified twice, causing an error.

--- examples/ex9p.cpp
+++ examples/ex9p.cpp
@@ -17,7 +17,7 @@
 //    mpirun -np 4 ex9p -m ../data/periodic-square.mesh -p 3 -rp 2 -dt 0.0025 -tf 9 -vs 20
 //    mpirun -np 4 ex9p -m ../data/periodic-cube.mesh -p 0 -o 2 -rp 1 -dt 0.01 -tf 8
 //    mpirun -np 4 ex9p -m ../data/periodic-square.msh -p 0 -rs 2 -dt 0.005 -tf 2
-//    mpirun -np 4 ex9p -m ../data/periodic-cube.msh -p 0 -rs 1 -o 2 -tf 2 -no-vis -paraview
+//    mpirun -np 4 ex9p -m ../data/periodic-cube.msh -p 0 -rs 1 -o 2 -tf 2
 //    mpirun -np 3 ex9p -m ../data/amr-hex.mesh -p 1 -rs 1 -rp 0 -dt 0.005 -tf 0.5
 //
 // Device sample runs:
