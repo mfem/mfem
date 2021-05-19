256aa278aca82c57526033602ff163f6034f8bc4
Will Pazner <will.e.p@gmail.com>
2021-05-19 08:29:16 -0700

    Add copyright header
    Change name of header guard to remove double-underscore

--- miniapps/solvers/lor_mms.hpp
+++ miniapps/solvers/lor_mms.hpp
@@ -1,5 +1,16 @@
-#ifndef __LOR_MMS_HPP__
-#define __LOR_MMS_HPP__
+// Copyright (c) 2010-2021, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#ifndef MFEM_LOR_MMS_HPP
+#define MFEM_LOR_MMS_HPP
 
 extern bool grad_div_problem;
 
