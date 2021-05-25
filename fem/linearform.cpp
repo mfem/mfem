ab3a32b33d01f571abd733659687ac7fd601ecf3
blaz <bslazarov.dev@gmail.com>
2021-05-24 17:33:41 -0700

    Minor changes/comments in the diffusion file and one memory leak fix

--- fem/linearform.cpp
+++ fem/linearform.cpp
@@ -338,6 +338,7 @@ LinearForm::~LinearForm()
       for (k=0; k < dlfi.Size(); k++) { delete dlfi[k]; }
       for (k=0; k < blfi.Size(); k++) { delete blfi[k]; }
       for (k=0; k < flfi.Size(); k++) { delete flfi[k]; }
+      for (k=0; k < iflfi.Size(); k++) { delete iflfi[k]; }
    }
 }
 
