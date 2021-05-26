1db8f2d67f22cc8511af274bd701543a87c6902a
Adrien M. Bernede <bernede1@llnl.gov>
2021-05-26 09:21:28 -0700

    TO REVERT: test license check

--- fem/staticcond.cpp
+++ fem/staticcond.cpp
@@ -6,7 +6,7 @@
 // availability visit https://mfem.org.
 //
 // MFEM is free software; you can redistribute it and/or modify it under the
-// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// terms of the GNU Lesser General Public License. We welcome feedback and contributions, see file
 // CONTRIBUTING.md for details.
 
 #include "staticcond.hpp"
