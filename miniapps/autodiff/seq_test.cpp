353e4d15da30a855cf42717c0219a00f6b46a1bd
blaz <bslazarov.dev@gmail.com>
2021-05-25 21:00:57 -0700

    Example of auto diff

---
+++ miniapps/autodiff/seq_test.cpp
@@ -0,0 +1,82 @@
+#include "admfem.hpp"
+
+template<typename TDataType, typename TParamVector, typename TStateVector
+         , int state_size, int param_size>
+class DiffusionFunctional
+{
+public:
+    TDataType operator() (TParamVector& vparam, TStateVector& uu)
+    {
+        MFEM_ASSERT(state_size==4,"ExampleFunctor state_size should be equal to 4!");
+        MFEM_ASSERT(param_size==2,"ExampleFunctor param_size should be equal to 2!");
+        auto kapa = vparam[0]; //diffusion coefficient
+        auto load = vparam[1]; //volumetric influx
+        TDataType rez = kapa*(uu[0]*uu[0]+uu[1]*uu[1]+uu[2]*uu[2])/2.0 - load*uu[3];
+        return rez;
+    }
+
+};
+
+template<typename TDataType, typename TParamVector, typename TStateVector,
+         int residual_size, int state_size, int param_size>
+class DiffusionResidual
+{
+public:
+    void operator ()(TParamVector& vparam, TStateVector& uu, TStateVector& rr)
+    {
+        MFEM_ASSERT(residual_size==4,"DiffusionResidual residual_size should be equal to 4!")
+        MFEM_ASSERT(state_size==4,"ExampleFunctor state_size should be equal to 4!");
+        MFEM_ASSERT(param_size==2,"ExampleFunctor param_size should be equal to 2!");
+        auto kapa = vparam[0]; //diffusion coefficient
+        auto load = vparam[1]; //volumetric influx
+
+        rr[0] = kapa * uu[0];
+        rr[1] = kapa * uu[1];
+        rr[2] = kapa * uu[2];
+        rr[3] = -load;
+    }
+};
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+    mfem::Vector param(2);
+    param[0]=3.0; //diffusion coefficient
+    param[1]=2.0; //volumetric influx
+
+    mfem::Vector state(4);
+    state[0]=1.0; // grad_x
+    state[1]=2.0; // grad_y
+    state[2]=3.0; // grad_z
+    state[4]=4.0; // state value
+
+    mfem::QFunctionAutoDiff<DiffusionFunctional,4,2> adf;
+    mfem::QResidualAutoDiff<DiffusionResidual,4,4,2> rdf;
+
+    mfem::Vector rr0(4);
+    mfem::DenseMatrix hh0(4,4);
+
+    mfem::Vector rr1(4);
+    mfem::DenseMatrix hh1(4,4);
+
+    adf.QResidual(param,state,rr0);
+    adf.QGradResidual(param, state, hh0);
+    // dump out the results
+    std::cout<<"FunctionAutoDiff"<<std::endl;
+    std::cout<< adf.QEval(param,state)<<std::endl;
+    rr0.Print(std::cout);
+    hh0.Print(std::cout);
+
+    rdf.QResidual(param,state, rr1);
+    rdf.QGradResidual(param, state, hh1);
+
+    std::cout<<"ResidualAutoDiff"<<std::endl;
+    rr1.Print(std::cout);
+    hh1.Print(std::cout);
+
+
+}
