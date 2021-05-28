a413f8fbbcef33186c2b398568ac9bacd7167d66
camierjs <camier1@llnl.gov>
2021-05-28 12:10:21 -0700

    general/xfl.*.* copyright

--- general/xfl.L.cpp
+++ general/xfl.L.cpp
@@ -851,7 +851,7 @@ char *yytext;
 #line 1 "xfl.ll"
 /**INDENT-OFF**/
 /* clang-format off */
-// Copyright (c) 2010-2020, Lawrence Livermore National Security, LLC. Produced
+// Copyright (c) 2010-2021, Lawrence Livermore National Security, LLC. Produced
 // at the Lawrence Livermore National Laboratory. All Rights reserved. See files
 // LICENSE and NOTICE for details. LLNL-CODE-806117.
 //
--- general/xfl.Y.hpp
+++ general/xfl.Y.hpp
@@ -31,7 +31,7 @@
 // version 2.2 of Bison.
 
 
-// Copyright (c) 2010-2020, Lawrence Livermore National Security, LLC. Produced
+// Copyright (c) 2010-2021, Lawrence Livermore National Security, LLC. Produced
 // at the Lawrence Livermore National Laboratory. All Rights reserved. See files
 // LICENSE and NOTICE for details. LLNL-CODE-806117.
 //
