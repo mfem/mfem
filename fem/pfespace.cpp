3970ea1a71251fdbb434b879649374958a42c380
Will Pazner <will.e.p@gmail.com>
2021-06-26 18:27:47 -0700

    Handle LOR permutation construction in variable-order case

--- fem/pfespace.cpp
+++ fem/pfespace.cpp
@@ -2939,8 +2939,11 @@ void ParFiniteElementSpace::CopyProlongationAndRestriction(
    SparseMatrix *perm_mat = NULL, *perm_mat_tr = NULL;
    if (perm)
    {
+      // Note: although n and fes.GetVSize() are typically equal, in
+      // variable-order spaces they may differ, since nonconforming edges/faces
+      // my have fictitious DOFs.
       int n = perm->Size();
-      perm_mat = new SparseMatrix(n, n);
+      perm_mat = new SparseMatrix(n, fes.GetVSize());
       for (int i=0; i<n; ++i)
       {
          double s;
