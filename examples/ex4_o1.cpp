f0ba213ba58d24dd5cbbb819f60b7b6f1c71645d
Stowell, Mark L <stowell1@llnl.gov>
2021-07-19 10:07:44 -0700

    Adding two H1 and RT examples for testing purposes only

---
+++ examples/ex4_o1.cpp
@@ -0,0 +1,359 @@
+//                                MFEM Example 4
+//
+// Compile with: make ex4
+//
+// Sample runs:  ex4 -m ../data/square-disc.mesh
+//               ex4 -m ../data/star.mesh
+//               ex4 -m ../data/beam-tet.mesh
+//               ex4 -m ../data/beam-hex.mesh
+//               ex4 -m ../data/beam-hex.mesh -o 2 -pa
+//               ex4 -m ../data/escher.mesh
+//               ex4 -m ../data/fichera.mesh -o 2 -hb
+//               ex4 -m ../data/fichera-q2.vtk
+//               ex4 -m ../data/fichera-q3.mesh -o 2 -sc
+//               ex4 -m ../data/square-disc-nurbs.mesh
+//               ex4 -m ../data/beam-hex-nurbs.mesh
+//               ex4 -m ../data/periodic-square.mesh -no-bc
+//               ex4 -m ../data/periodic-cube.mesh -no-bc
+//               ex4 -m ../data/amr-quad.mesh
+//               ex4 -m ../data/amr-hex.mesh
+//               ex4 -m ../data/amr-hex.mesh -o 2 -hb
+//               ex4 -m ../data/fichera-amr.mesh -o 2 -sc
+//               ex4 -m ../data/star-surf.mesh -o 1
+//
+// Device sample runs:
+//               ex4 -m ../data/star.mesh -pa -d cuda
+//               ex4 -m ../data/star.mesh -pa -d raja-cuda
+//               ex4 -m ../data/star.mesh -pa -d raja-omp
+//               ex4 -m ../data/beam-hex.mesh -pa -d cuda
+//
+// Description:  This example code solves a simple 2D/3D H(div) diffusion
+//               problem corresponding to the second order definite equation
+//               -grad(alpha div F) + beta F = f with boundary condition F dot n
+//               = <given normal field>. Here, we use a given exact solution F
+//               and compute the corresponding r.h.s. f.  We discretize with
+//               Raviart-Thomas finite elements.
+//
+//               The example demonstrates the use of H(div) finite element
+//               spaces with the grad-div and H(div) vector finite element mass
+//               bilinear form, as well as the computation of discretization
+//               error when the exact solution is known. Bilinear form
+//               hybridization and static condensation are also illustrated.
+//
+//               We recommend viewing examples 1-3 before viewing this example.
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using namespace mfem;
+
+// Exact solution, F, and r.h.s., f. See below for implementation.
+void F_exact(const Vector &, Vector &);
+double dF_exact(const Vector &p);
+void f_exact(const Vector &, Vector &);
+double freq = 1.0, kappa, alpha = 1.0, beta = 1.0;
+
+int main(int argc, char *argv[])
+{
+   // 1. Parse command-line options.
+   const char *mesh_file = "../data/star.mesh";
+   int ser_ref_levels = 0;
+   int order = 1;
+   bool set_bc = true;
+   bool static_cond = false;
+   bool hybridization = false;
+   bool pa = false;
+   const char *device_config = "cpu";
+   bool visualization = 1;
+
+   OptionsParser args(argc, argv);
+   args.AddOption(&mesh_file, "-m", "--mesh",
+                  "Mesh file to use.");
+   args.AddOption(&ser_ref_levels, "-rs", "--refine-serial",
+                  "Number of times to refine the mesh uniformly in serial.");
+   args.AddOption(&order, "-o", "--order",
+                  "Finite element order (polynomial degree).");
+   args.AddOption(&set_bc, "-bc", "--impose-bc", "-no-bc", "--dont-impose-bc",
+                  "Impose or not essential boundary conditions.");
+   args.AddOption(&freq, "-f", "--frequency", "Set the frequency for the exact"
+                  " solution.");
+   args.AddOption(&alpha, "-a", "--alpha", "Diffusion coefficient.");
+   args.AddOption(&beta, "-b", "--beta", "Mass coefficient.");
+   args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
+                  "--no-static-condensation", "Enable static condensation.");
+   args.AddOption(&hybridization, "-hb", "--hybridization", "-no-hb",
+                  "--no-hybridization", "Enable hybridization.");
+   args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
+                  "--no-partial-assembly", "Enable Partial Assembly.");
+   args.AddOption(&device_config, "-d", "--device",
+                  "Device configuration string, see Device::Configure().");
+   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
+                  "--no-visualization",
+                  "Enable or disable GLVis visualization.");
+   args.Parse();
+   if (!args.Good())
+   {
+      args.PrintUsage(cout);
+      return 1;
+   }
+   args.PrintOptions(cout);
+   kappa = freq * M_PI;
+
+   // 2. Enable hardware devices such as GPUs, and programming models such as
+   //    CUDA, OCCA, RAJA and OpenMP based on command line options.
+   Device device(device_config);
+   device.Print();
+
+   // 3. Read the mesh from the given mesh file. We can handle triangular,
+   //    quadrilateral, tetrahedral, hexahedral, surface and volume, as well as
+   //    periodic meshes with the same code.
+   Mesh *mesh = new Mesh(mesh_file, 1, 1);
+   int dim = mesh->Dimension();
+   int sdim = mesh->SpaceDimension();
+
+   // 4. Refine the mesh to increase the resolution. In this example we do
+   //    'ref_levels' of uniform refinement. We choose 'ref_levels' to be the
+   //    largest number that gives a final mesh with no more than 25,000
+   //    elements.
+   {
+      int ref_levels = ser_ref_levels;
+      // (int)floor(log(25000./mesh->GetNE())/log(2.)/dim);
+      for (int l = 0; l < ref_levels; l++)
+      {
+         mesh->UniformRefinement();
+      }
+   }
+
+   // 5. Define a finite element space on the mesh. Here we use the
+   //    Raviart-Thomas finite elements of the specified order.
+   FiniteElementCollection *fec = new RT0_3DFECollection();
+   FiniteElementSpace *fespace = new FiniteElementSpace(mesh, fec);
+   cout << "Number of finite element unknowns: "
+        << fespace->GetTrueVSize() << endl;
+
+   Const3DFECollection dfec;
+   FiniteElementSpace dfes(mesh, &dfec);
+   cout << "Number of L2 finite element unknowns: "
+        << dfes.GetTrueVSize() << endl;
+
+   // 6. Determine the list of true (i.e. conforming) essential boundary dofs.
+   //    In this example, the boundary conditions are defined by marking all
+   //    the boundary attributes from the mesh as essential (Dirichlet) and
+   //    converting them to a list of true dofs.
+   Array<int> ess_tdof_list;
+   if (mesh->bdr_attributes.Size())
+   {
+      Array<int> ess_bdr(mesh->bdr_attributes.Max());
+      ess_bdr = set_bc ? 1 : 0;
+      fespace->GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
+   }
+
+   // 7. Set up the linear form b(.) which corresponds to the right-hand side
+   //    of the FEM linear system, which in this case is (f,phi_i) where f is
+   //    given by the function f_exact and phi_i are the basis functions in the
+   //    finite element fespace.
+   VectorFunctionCoefficient f(sdim, f_exact);
+   LinearForm *b = new LinearForm(fespace);
+   b->AddDomainIntegrator(new VectorFEDomainLFIntegrator(f));
+   b->Assemble();
+
+   // 8. Define the solution vector x as a finite element grid function
+   //    corresponding to fespace. Initialize x by projecting the exact
+   //    solution. Note that only values from the boundary faces will be used
+   //    when eliminating the non-homogeneous boundary condition to modify the
+   //    r.h.s. vector b.
+   GridFunction x(fespace);
+   VectorFunctionCoefficient F(sdim, F_exact);
+   x.ProjectCoefficient(F);
+
+   cout << "\n|| F_h - F ||_{L^2} = " << x.ComputeL2Error(F) << '\n' << endl;
+
+   GridFunction dx(&dfes);
+   FunctionCoefficient dF(dF_exact);
+   dx.ProjectCoefficient(dF);
+   cout << "\n|| dF_h - dF ||_{L^2} = " << dx.ComputeL2Error(dF) << '\n' << endl;
+
+   DivergenceGridFunctionCoefficient dF_gfc(&x);
+   dx.ProjectCoefficient(dF_gfc);
+   cout << "\n|| dF_h - dF ||_{L^2} = " << dx.ComputeL2Error(dF) << '\n' << endl;
+
+   DiscreteLinearOperator div(fespace, &dfes);
+   div.AddDomainInterpolator(new DivergenceInterpolator);
+   div.Assemble();
+   div.Finalize();
+   if (false)
+   {
+      cout << "\nDiscrete Divergence:" << endl;
+      div.SpMat().Print(cout, 6);
+      cout << endl;
+   }
+
+   // 9. Set up the bilinear form corresponding to the H(div) diffusion operator
+   //    grad alpha div + beta I, by adding the div-div and the mass domain
+   //    integrators.
+   Coefficient *alphaCoef = new ConstantCoefficient(alpha);
+   Coefficient *betaCoef  = new ConstantCoefficient(beta);
+   BilinearForm *a = new BilinearForm(fespace);
+   if (pa) { a->SetAssemblyLevel(AssemblyLevel::PARTIAL); }
+   a->AddDomainIntegrator(new DivDivIntegrator(*alphaCoef));
+   a->AddDomainIntegrator(new VectorFEMassIntegrator(*betaCoef));
+
+   // 10. Assemble the bilinear form and the corresponding linear system,
+   //    applying any necessary transformations such as: eliminating boundary
+   //    conditions, applying conforming constraints for non-conforming AMR,
+   //    static condensation, hybridization, etc.
+   FiniteElementCollection *hfec = NULL;
+   FiniteElementSpace *hfes = NULL;
+   if (static_cond)
+   {
+      a->EnableStaticCondensation();
+   }
+   else if (hybridization)
+   {
+      hfec = new DG_Interface_FECollection(order-1, dim);
+      hfes = new FiniteElementSpace(mesh, hfec);
+      a->EnableHybridization(hfes, new NormalTraceJumpIntegrator(),
+                             ess_tdof_list);
+   }
+   a->Assemble();
+
+   if (true)
+   {
+      ofstream ofsA("A.mat");
+      a->SpMat().Print(ofsA);
+      ofsA.close();
+   }
+
+   OperatorPtr A;
+   Vector B, X;
+   a->FormLinearSystem(ess_tdof_list, x, *b, A, X, B);
+
+   cout << "Size of linear system: " << A->Height() << endl;
+
+   // 11. Solve the linear system A X = B.
+   if (!pa)
+   {
+#ifndef MFEM_USE_SUITESPARSE
+      // Use a simple symmetric Gauss-Seidel preconditioner with PCG.
+      GSSmoother M((SparseMatrix&)(*A));
+      PCG(*A, M, B, X, 1, 10000, 1e-20, 0.0);
+#else
+      // If MFEM was compiled with SuiteSparse, use UMFPACK to solve the system.
+      UMFPackSolver umf_solver;
+      umf_solver.Control[UMFPACK_ORDERING] = UMFPACK_ORDERING_METIS;
+      umf_solver.SetOperator(*A);
+      umf_solver.Mult(B, X);
+#endif
+   }
+   else // Jacobi preconditioning in partial assembly mode
+   {
+      if (UsesTensorBasis(*fespace))
+      {
+         OperatorJacobiSmoother M(*a, ess_tdof_list);
+         PCG(*A, M, B, X, 1, 10000, 1e-20, 0.0);
+      }
+      else
+      {
+         CG(*A, B, X, 1, 10000, 1e-20, 0.0);
+      }
+   }
+
+   // 12. Recover the solution as a finite element grid function.
+   a->RecoverFEMSolution(X, *b, x);
+
+   // 13. Compute and print the L^2 norm of the error.
+   cout << "\n|| F_h - F ||_{L^2} = " << x.ComputeL2Error(F) << '\n' << endl;
+
+   dx.ProjectCoefficient(dF_gfc);
+   cout << "\n|| dF_h - dF ||_{L^2} = " << dx.ComputeL2Error(dF) << '\n' << endl;
+
+   // 14. Save the refined mesh and the solution. This output can be viewed
+   //     later using GLVis: "glvis -m refined.mesh -g sol.gf".
+   {
+      ofstream mesh_ofs("refined.mesh");
+      mesh_ofs.precision(8);
+      mesh->Print(mesh_ofs);
+      ofstream sol_ofs("sol.gf");
+      sol_ofs.precision(8);
+      x.Save(sol_ofs);
+   }
+
+   // 15. Send the solution by socket to a GLVis server.
+   if (visualization)
+   {
+      char vishost[] = "localhost";
+      int  visport   = 19916;
+      socketstream sol_sock(vishost, visport);
+      sol_sock.precision(8);
+      sol_sock << "solution\n" << *mesh << x << flush;
+
+      socketstream dsol_sock(vishost, visport);
+      dsol_sock.precision(8);
+      dsol_sock << "solution\n" << *mesh << dx << flush;
+   }
+
+   // 16. Free the used memory.
+   delete hfes;
+   delete hfec;
+   delete a;
+   delete alphaCoef;
+   delete betaCoef;
+   delete b;
+   delete fespace;
+   delete fec;
+   delete mesh;
+
+   return 0;
+}
+
+
+// The exact solution (for non-surface meshes)
+void F_exact(const Vector &p, Vector &F)
+{
+   int dim = p.Size();
+
+   double x = p(0);
+   double y = p(1);
+   double z = (dim == 3) ? p(2) : 0.0; // Uncomment if F is changed to depend on z
+
+   F(0) = cos(kappa*x)*sin(kappa*y);
+   F(1) = cos(kappa*y)*sin(kappa*x);
+   if (dim == 3)
+   {
+      F(2) = cos(kappa*z);
+   }
+}
+
+// The divergence of the exact solution (for non-surface meshes)
+double dF_exact(const Vector &p)
+{
+   int dim = p.Size();
+
+   double x = p(0);
+   double y = p(1);
+   double z = (dim == 3) ? p(2) : 0.0; // Uncomment if F is changed to depend on z
+
+   return -kappa*(2.0*sin(kappa*x)*sin(kappa*y)+sin(kappa*z));
+}
+
+// The right hand side
+void f_exact(const Vector &p, Vector &f)
+{
+   int dim = p.Size();
+
+   double x = p(0);
+   double y = p(1);
+   double z = (dim == 3) ? p(2) : 0.0; // Uncomment if f is changed to depend on z
+
+   double temp1 = beta + alpha*kappa*kappa;
+   double temp2 = beta + 2.*alpha*kappa*kappa;
+
+   f(0) = temp2*cos(kappa*x)*sin(kappa*y);
+   f(1) = temp2*cos(kappa*y)*sin(kappa*x);
+   if (dim == 3)
+   {
+      f(2) = temp1*cos(kappa*z);
+   }
+}
