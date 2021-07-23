f776d6bd23d3880410ad2be46cba6cc17c490c0a
Joseph Ciurej <ciurej1@llnl.gov>
2021-07-23 15:52:29 -0700

    extend 'ConduitDataCollection' adjacency information to include shared boundaries

--- fem/conduitdatacollection.hpp
+++ fem/conduitdatacollection.hpp
@@ -167,7 +167,8 @@ public:
                                    const std::string &coordset_name = "coords",
                                    const std::string &main_topology_name = "main",
                                    const std::string &boundary_topology_name = "boundary",
-                                   const std::string &main_adjset_name = "main_adjset");
+                                   const std::string &main_adjset_name = "main_adjset",
+                                   const std::string &boundary_adjset_name = "boundary_adjset");
 
    /// Describes a MFEM grid function using the mesh blueprint
    /** Sets up passed conduit::Node out to describe the given grid function
