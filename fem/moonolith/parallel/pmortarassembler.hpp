6dc21c77b0c506dab532c31d113ef24f1f4b7e91
zulianp <patrick.zulian@gmail.com>
2021-08-03 12:13:58 +0200

    fix on docs

--- fem/moonolith/parallel/pmortarassembler.hpp
+++ fem/moonolith/parallel/pmortarassembler.hpp
@@ -30,6 +30,12 @@ namespace mfem
  * interaction, or even visulization of average quanties within subvolumes. This
  * particular code is also used with LLNL for large scale multilevel Monte Carlo
  * simulations.
+ * This algorithm allows to perform quadrature in the intersection of elements
+ * of two separate, unrelated, and arbitrarily distributed meshes.
+ * It generates quadrature rules in the intersection which allows us to
+ * integrate with to machine precision using the mfem::MortarIntegrator
+ * interface. See https://doi.org/10.1137/15M1008361 for and in-depth
+ * explanation.
  */
 class ParMortarAssembler
 {
