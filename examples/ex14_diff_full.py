2a50dba6b34571f6f0c646ffab4b470dc96e2924
pguthrey <piersonguthrey@gmail.com>
2021-08-19 16:25:51 -0700

    mods to testing script

--- examples/ex14_diff_full.py
+++ examples/ex14_diff_full.py
@@ -5,9 +5,12 @@ base_str = "./ex14 -pa "
 
 # NURBS are not tensor
 
-meshes = ["../data/inline-hex.mesh",
-      #"../data/inline-quad.mesh",  
-           # "../data/star.mesh"
+tolerance = 1e-04
+
+
+meshes = ["../data/inline-quad.mesh",  
+            "../data/inline-hex.mesh",
+            "../data/star.mesh",
            ]#,
 #            "../data/escher.mesh",
  #           "../data/fichera.mesh",
@@ -19,16 +22,15 @@ maxrs = [3,3,3,2,2,2]
 
 #print("b s k int     bdy    basis i r p err")
 #print(" ")
-
 def get_data(line):
    return repr(line).split('\\')[0].split("=")[1].strip()
 
 for bas in ['--lob']:#,'--pos']:
    for thismesh in meshes:#list(range(0, len(meshes))):
-      for o in [1,2]:#[1,2,3]:
-         for r in [0,1]:#list(range(0, maxrs[o-1]+1)):
-            for I in ['--int']: #['--noint','--int']:
-               for B in ['--bdy']: #['--nobdy','--bdy']:
+      for o in [1,2,3]:
+         for r in list(range(0, maxrs[o-1]+1)):
+            for I in ['--noint','--int']:
+               for B in ['--nobdy','--bdy']:
                   if( not ((I == '--noint') and (B == '--nobdy'))):
                      for sigma in [0, -1, 1]:
                         for kappa in [0, -1, 1]:
@@ -65,7 +67,7 @@ for bas in ['--lob']:#,'--pos']:
                                                 quit()
                                                 
 
-                                          if( float(error) > 1.0e-10 ):
+                                          if( float(error) > tolerance ):
                                              #s = [str(mesh),str(beta),str(sigma),str(kappa),str(I),str(B),str(bas),str(init),str(r),str(o),error,patime,fulltime]
                                              #print(" ".join(s))
                                              errors = errors + 1
@@ -85,6 +87,6 @@ for bas in ['--lob']:#,'--pos']:
                                                 " -o ",str(o)]
                                           print(" ".join(s))
 
-                                          if( errors >= 5 ):
+                                          if( errors >= 1 ):
                                              print("too many fails ")
                                              quit()
