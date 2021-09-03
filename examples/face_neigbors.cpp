d48fe60de2fc513ab48c20082184b167059697d9
Socratis Petrides <petrides1@llnl.gov>
2021-09-03 14:01:48 +0300

    initial commit, amr - get face neighbors

---
+++ examples/face_neigbors.cpp
@@ -0,0 +1,45 @@
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using namespace mfem;
+
+int main(int argc, char *argv[])
+{
+   // 1. Parse command-line options.
+   const char *mesh_file = "../data/inline-quad.mesh";
+   int order = 1;
+   bool visualization = true;
+
+   OptionsParser args(argc, argv);
+   args.AddOption(&mesh_file, "-m", "--mesh",
+                  "Mesh file to use.");
+   args.AddOption(&order, "-o", "--order",
+                  "Finite element order (polynomial degree) or -1 for"
+                  " isoparametric space.");
+   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
+                  "--no-visualization",
+                  "Enable or disable GLVis visualization.");
+   args.Parse();
+   if (!args.Good())
+   {
+      args.PrintUsage(cout);
+      return 1;
+   }
+   args.PrintOptions(cout);
+
+   Mesh mesh(mesh_file, 1, 1);
+   int dim = mesh.Dimension();
+
+   Array<int> ref_elems(1);
+   ref_elems = 2;
+   mesh.GeneralRefinement(ref_elems,-1,0);
+
+   const Table & e2e = mesh.ElementToElementTable();
+
+   e2e.Print();
+
+   return 0;
+}
