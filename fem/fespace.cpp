61e9c368d79730522a8d81d411edb7759318e1cb
Jakub Červený <cerveny1@llnl.gov>
2021-09-17 12:52:20 +0200

    DerefinementOperator: uses original GetCoarseToFineMap, now in fespace.cpp.

--- fem/fespace.cpp
+++ fem/fespace.cpp
@@ -1723,7 +1723,10 @@ void FiniteElementSpace::RefinementOperator
    }
 }
 
-// Used in DerefinementOperator::DerefinementOperator() below.
+namespace internal
+{
+
+// Used in GetCoarseToFineMap() below.
 struct RefType
 {
    Geometry::Type geom;
@@ -1747,6 +1750,94 @@ struct RefType
       return false; // everything is equal
    }
 };
+
+void GetCoarseToFineMap(const CoarseFineTransformations &cft,
+                        const mfem::Mesh &fine_mesh,
+                        Table &coarse_to_fine,
+                        Array<int> &coarse_to_ref_type,
+                        Table &ref_type_to_matrix,
+                        Array<Geometry::Type> &ref_type_to_geom)
+{
+   const int fine_ne = cft.embeddings.Size();
+   int coarse_ne = -1;
+   for (int i = 0; i < fine_ne; i++)
+   {
+      coarse_ne = std::max(coarse_ne, cft.embeddings[i].parent);
+   }
+   coarse_ne++;
+
+   coarse_to_ref_type.SetSize(coarse_ne);
+   coarse_to_fine.SetDims(coarse_ne, fine_ne);
+
+   Array<int> cf_i(coarse_to_fine.GetI(), coarse_ne+1);
+   Array<Pair<int,int> > cf_j(fine_ne);
+   cf_i = 0;
+   for (int i = 0; i < fine_ne; i++)
+   {
+      cf_i[cft.embeddings[i].parent+1]++;
+   }
+   cf_i.PartialSum();
+   MFEM_ASSERT(cf_i.Last() == cf_j.Size(), "internal error");
+   for (int i = 0; i < fine_ne; i++)
+   {
+      const Embedding &e = cft.embeddings[i];
+      cf_j[cf_i[e.parent]].one = e.matrix; // used as sort key below
+      cf_j[cf_i[e.parent]].two = i;
+      cf_i[e.parent]++;
+   }
+   std::copy_backward(cf_i.begin(), cf_i.end()-1, cf_i.end());
+   cf_i[0] = 0;
+   for (int i = 0; i < coarse_ne; i++)
+   {
+      std::sort(&cf_j[cf_i[i]], cf_j.GetData() + cf_i[i+1]);
+   }
+   for (int i = 0; i < fine_ne; i++)
+   {
+      coarse_to_fine.GetJ()[i] = cf_j[i].two;
+   }
+
+   using std::map;
+   using std::pair;
+
+   map<RefType,int> ref_type_map;
+   for (int i = 0; i < coarse_ne; i++)
+   {
+      const int num_children = cf_i[i+1]-cf_i[i];
+      MFEM_ASSERT(num_children > 0, "");
+      const int fine_el = cf_j[cf_i[i]].two;
+      // Assuming the coarse and the fine elements have the same geometry:
+      const Geometry::Type geom = fine_mesh.GetElementBaseGeometry(fine_el);
+      const RefType ref_type(geom, num_children, &cf_j[cf_i[i]]);
+      pair<map<RefType,int>::iterator,bool> res =
+         ref_type_map.insert(
+            pair<const RefType,int>(ref_type, (int)ref_type_map.size()));
+      coarse_to_ref_type[i] = res.first->second;
+   }
+
+   ref_type_to_matrix.MakeI((int)ref_type_map.size());
+   ref_type_to_geom.SetSize((int)ref_type_map.size());
+   for (map<RefType,int>::iterator it = ref_type_map.begin();
+        it != ref_type_map.end(); ++it)
+   {
+      ref_type_to_matrix.AddColumnsInRow(it->second, it->first.num_children);
+      ref_type_to_geom[it->second] = it->first.geom;
+   }
+
+   ref_type_to_matrix.MakeJ();
+   for (map<RefType,int>::iterator it = ref_type_map.begin();
+        it != ref_type_map.end(); ++it)
+   {
+      const RefType &rt = it->first;
+      for (int j = 0; j < rt.num_children; j++)
+      {
+         ref_type_to_matrix.AddConnection(it->second, rt.children[j].one);
+      }
+   }
+   ref_type_to_matrix.ShiftUpI();
+}
+
+} // namespace internal
+
 
 /// TODO: Implement DofTransformation support
 FiniteElementSpace::DerefinementOperator::DerefinementOperator(
@@ -1788,50 +1879,11 @@ FiniteElementSpace::DerefinementOperator::DerefinementOperator(
       }
    }
 
-   rtrans.MakeCoarseToFineTable(coarse_to_fine, true);
-   MFEM_ASSERT(coarse_to_fine.Size() == c_fes->GetNE(), "");
-
-   using std::map;
-   using std::pair;
-
-   // create coarse_to_ref_type
-   map<RefType,int> ref_type_map;
-   coarse_to_ref_type.SetSize(coarse_to_fine.Size());
-   for (int i = 0; i < coarse_ne; i++)
-   {
-      const int num_children = coarse_to_fine.RowSize(i);
-      const int *children = coarse_to_fine.GetRow(i);
-      MFEM_ASSERT(num_children > 0, "");
-      // Assuming the coarse and the fine elements have the same geometry:
-      const Geometry::Type geom = new_mesh.GetElementBaseGeometry(children[0]);
-      const RefType ref_type(geom, num_children, children);
-      auto res = ref_type_map.insert(
-         pair<const RefType,int>(ref_type, (int)ref_type_map.size()));
-      coarse_to_ref_type[i] = res.first->second;
-   }
-
-   // create ref_type_to_geom, ref_type_to_matrix
    Table ref_type_to_matrix;
-   ref_type_to_matrix.MakeI((int)ref_type_map.size());
-   ref_type_to_geom.SetSize((int)ref_type_map.size());
-   for (map<RefType,int>::iterator it = ref_type_map.begin();
-        it != ref_type_map.end(); ++it)
-   {
-      ref_type_to_matrix.AddColumnsInRow(it->second, it->first.num_children);
-      ref_type_to_geom[it->second] = it->first.geom;
-   }
-
-   ref_type_to_matrix.MakeJ();
-   for (map<RefType,int>::iterator it = ref_type_map.begin();
-        it != ref_type_map.end(); ++it)
-   {
-      const RefType &rt = it->first;
-      for (int j = 0; j < rt.num_children; j++)
-      {
-         ref_type_to_matrix.AddConnection(it->second, rt.children[j].one);
-      }
-   }
-   ref_type_to_matrix.ShiftUpI();
+   internal::GetCoarseToFineMap(rtrans, *f_mesh, coarse_to_fine,
+                                coarse_to_ref_type, ref_type_to_matrix,
+                                ref_type_to_geom);
+   MFEM_ASSERT(coarse_to_fine.Size() == c_fes->GetNE(), "");
 
    const int total_ref_types = ref_type_to_geom.Size();
    int num_ref_types[Geometry::NumGeom], num_fine_elems[Geometry::NumGeom];
@@ -1847,7 +1899,6 @@ FiniteElementSpace::DerefinementOperator::DerefinementOperator(
       num_ref_types[g]++;
       num_fine_elems[g] += ref_type_to_matrix.RowSize(i);
    }
-
    DenseTensor localPtMP[Geometry::NumGeom];
    for (int g = 0; g < Geometry::NumGeom; g++)
    {
