87ca1ab0c47a00f49800e49e485fac4bdb3a55fe
termi-official <termi-official@users.noreply.github.com>
2021-09-29 01:16:36 +0200

    Forgot a file.

--- general/globals.hpp
+++ general/globals.hpp
@@ -129,21 +129,21 @@ void SetGlobalMPI_Comm(MPI_Comm comm);
 // backwards compatibility.
 // Code derived from https://www.fluentcpp.com/2019/08/30/how-to-disable-a-warning-in-cpp/
 #if defined(_MSC_VER)
-#define DISABLE_WARNING_PUSH           __pragma(warning( push ))
-#define DISABLE_WARNING_POP            __pragma(warning( pop ))
-#define DISABLE_WARNING(warningNumber) __pragma(warning( disable : warningNumber ))
-#define DISABLE_WARNING_DEPRECATED     DISABLE_WARNING(4996)
+#define MFEM_DISABLE_WARNING_PUSH           __pragma(warning( push ))
+#define MFEM_DISABLE_WARNING_POP            __pragma(warning( pop ))
+#define MFEM_DISABLE_WARNING(warningNumber) __pragma(warning( disable : warningNumber ))
+#define MFEM_DISABLE_WARNING_DEPRECATED     DISABLE_WARNING(4996)
 #elif defined(__GNUC__) || defined(__clang__)
-#define DO_PRAGMA(X) _Pragma(#X)
-#define DISABLE_WARNING_PUSH           DO_PRAGMA(GCC diagnostic push)
-#define DISABLE_WARNING_POP            DO_PRAGMA(GCC diagnostic pop)
-#define DISABLE_WARNING(warningName)   DO_PRAGMA(GCC diagnostic ignored #warningName)
-#define DISABLE_WARNING_DEPRECATED     DISABLE_WARNING(-Wdeprecated-declarations)
+#define MFEM_DO_PRAGMA(X) _Pragma(#X)
+#define MFEM_DISABLE_WARNING_PUSH           MFEM_DO_PRAGMA(GCC diagnostic push)
+#define MFEM_DISABLE_WARNING_POP            MFEM_DO_PRAGMA(GCC diagnostic pop)
+#define MFEM_DISABLE_WARNING(warningName)   MFEM_DO_PRAGMA(GCC diagnostic ignored #warningName)
+#define MFEM_DISABLE_WARNING_DEPRECATED     MFEM_DISABLE_WARNING(-Wdeprecated-declarations)
 #else
 #pragma message("WARNING: You need to implement DISABLE_WARNING_* for this compiler")
-#define DISABLE_WARNING_PUSH
-#define DISABLE_WARNING_POP
-#define DISABLE_WARNING_DEPRECATED
+#define MFEM_DISABLE_WARNING_PUSH
+#define MFEM_DISABLE_WARNING_POP
+#define MFEM_DISABLE_WARNING_DEPRECATED
 #endif
 
 #endif
