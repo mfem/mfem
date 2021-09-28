3cbfee625a7e5f2e95956e640af5df5d0b927aac
termi-official <termi-official@users.noreply.github.com>
2021-09-28 21:42:18 +0200

    Add portable way to skip warnings generated by internal usage of deprecated variables.

--- general/globals.hpp
+++ general/globals.hpp
@@ -125,4 +125,25 @@ void SetGlobalMPI_Comm(MPI_Comm comm);
 #define MFEM_DEPRECATED
 #endif
 
+// Sometimes we have to ignore deprecation warnings internally to guarantee
+// backwards compatibility.
+// Code derived from https://www.fluentcpp.com/2019/08/30/how-to-disable-a-warning-in-cpp/
+#if defined(_MSC_VER)
+    #define DISABLE_WARNING_PUSH           __pragma(warning( push ))
+    #define DISABLE_WARNING_POP            __pragma(warning( pop ))
+    #define DISABLE_WARNING(warningNumber) __pragma(warning( disable : warningNumber ))
+    #define DISABLE_WARNING_DEPRECATED     DISABLE_WARNING(4996)
+#elif defined(__GNUC__) || defined(__clang__)
+    #define DO_PRAGMA(X) _Pragma(#X)
+    #define DISABLE_WARNING_PUSH           DO_PRAGMA(GCC diagnostic push)
+    #define DISABLE_WARNING_POP            DO_PRAGMA(GCC diagnostic pop)
+    #define DISABLE_WARNING(warningName)   DO_PRAGMA(GCC diagnostic ignored #warningName)
+    #define DISABLE_WARNING_DEPRECATED     DISABLE_WARNING(-Wdeprecated-declarations)
+#else
+   #pragma message("WARNING: You need to implement DISABLE_WARNING_* for this compiler")
+   #define DISABLE_WARNING_PUSH
+   #define DISABLE_WARNING_POP
+   #define DISABLE_WARNING_DEPRECATED
+#endif
+
 #endif
