c63b84d08686891043a0fbabf1c72609d08bf004
Socratis Petrides <petrides1@llnl.gov>
2021-09-29 16:03:43 -0700

    minor typo fix

--- mesh/mesh.hpp
+++ mesh/mesh.hpp
@@ -1194,7 +1194,7 @@ public:
    void GetFaceInfos (int Face, int *Inf1, int *Inf2, int *NCFace) const;
 
    /** Return all elements adjacent to the given Face
-       For an NCMesh and a Master face the functio will return more than 2 elements.
+       For an NCMesh and a master face the function will return more than 2 elements.
        For the other two cases (conforming or slave face) it falls back to the
        original GetFaceElements function above */
    void GetFaceElements (int Face, Array<int> & elems) const;
