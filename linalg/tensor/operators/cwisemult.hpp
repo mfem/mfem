64ac60a20cc4f84c12fbe0619191805f0089f053
Yohann Dudouit <yohann.dudouit@gmail.com>
2021-10-19 18:27:32 -0700

    Add cwisemult with vdim.

--- linalg/tensor/operators/cwisemult.hpp
+++ linalg/tensor/operators/cwisemult.hpp
@@ -90,8 +90,7 @@ auto operator*(const DiagonalTensor &D, const Tensor &u)
    return Du;
 }
 
-// With VDim
-// 1D
+// 1D with VDim
 template <typename DiagonalTensor,
           typename Tensor,
           std::enable_if_t<
@@ -105,10 +104,11 @@ auto operator*(const DiagonalTensor &D, const Tensor &u)
 {
    constexpr int Quads = 0;
    constexpr int VDim = 1;
-   constexpr int VD = get_tensor_size<VDim,Tensor>;
-   constexpr int Q_c = get_tensor_size<0,Tensor>;
-   const int Q_r = u.template Size<0>();
-   StaticResultTensor<Tensor,Q_c,VD> Du(Q_r,VD);
+   constexpr int Q_c = get_tensor_size<Quads,Tensor>;
+   constexpr int VD_c = get_tensor_size<VDim,Tensor>;
+   const int Q_r = u.template Size<Quads>();
+   const int VD_r = u.template Size<VDim>();
+   StaticResultTensor<Tensor,Q_c,VD_c> Du(Q_r,VD_r);
    Foreach<Quads>(u, [&](int q)
    {
       auto d = D(q);
@@ -119,7 +119,7 @@ auto operator*(const DiagonalTensor &D, const Tensor &u)
    return Du;
 }
 
-// 2D
+// 2D with VDim
 template <typename DiagonalTensor,
           typename Tensor,
           std::enable_if_t<
@@ -134,10 +134,13 @@ auto operator*(const DiagonalTensor &D, const Tensor &u)
    constexpr int QuadsX = 0;
    constexpr int QuadsY = 1;
    constexpr int VDim = 2;
-   constexpr int VD = get_tensor_size<VDim,Tensor>;
-   constexpr int Q_c = get_tensor_size<0,Tensor>;
-   const int Q_r = u.template Size<0>();
-   StaticResultTensor<Tensor,Q_c,Q_c,VD> Du(Q_r,Q_r,VD);
+   constexpr int QX_c = get_tensor_size<QuadsX,Tensor>;
+   constexpr int QY_c = get_tensor_size<QuadsY,Tensor>;
+   constexpr int VD_c = get_tensor_size<VDim,Tensor>;
+   const int QX_r = u.template Size<QuadsX>();
+   const int QY_r = u.template Size<QuadsY>();
+   const int VD_r = u.template Size<VDim>();
+   StaticResultTensor<Tensor,QX_c,QY_c,VD_c> Du(QX_r,QY_r,VD_r);
    Foreach<QuadsX>(u, [&](int qx)
    {
       Foreach<QuadsY>(u, [&](int qy)
@@ -151,7 +154,7 @@ auto operator*(const DiagonalTensor &D, const Tensor &u)
    return Du;
 }
 
-// 3D
+// 3D with VDim
 template <typename DiagonalTensor,
           typename Tensor,
           std::enable_if_t<
@@ -167,10 +170,15 @@ auto operator*(const DiagonalTensor &D, const Tensor &u)
    constexpr int QuadsY = 1;
    constexpr int QuadsZ = 2;
    constexpr int VDim = 3;
-   constexpr int VD = get_tensor_size<VDim,Tensor>;
-   constexpr int Q_c = get_tensor_size<0,Tensor>;
-   const int Q_r = u.template Size<0>();
-   StaticResultTensor<Tensor,Q_c,Q_c,Q_c,VD> Du(Q_r,Q_r,Q_r,VD);
+   constexpr int QX_c = get_tensor_size<QuadsX,Tensor>;
+   constexpr int QY_c = get_tensor_size<QuadsY,Tensor>;
+   constexpr int QZ_c = get_tensor_size<QuadsZ,Tensor>;
+   constexpr int VD_c = get_tensor_size<VDim,Tensor>;
+   const int QX_r = u.template Size<QuadsX>();
+   const int QY_r = u.template Size<QuadsY>();
+   const int QZ_r = u.template Size<QuadsZ>();
+   const int VD_r = u.template Size<VDim>();
+   StaticResultTensor<Tensor,QX_c,QY_c,QZ_c,VD_c> Du(QX_r,QY_r,QZ_r,VD_r);
    Foreach<QuadsX>(u, [&](int qx)
    {
       Foreach<QuadsY>(u, [&](int qy)
@@ -306,18 +314,119 @@ auto operator*(const DiagonalSymmTensor &D, const Tensor &u)
       const double D00 = D(q...,0);
       const double D01 = D(q...,1);
       const double D02 = D(q...,2);
-      const double D10 = D01;
       const double D11 = D(q...,3);
       const double D12 = D(q...,4);
+      const double D22 = D(q...,5);
+      const double D10 = D01;
       const double D20 = D02;
       const double D21 = D12;
-      const double D22 = D(q...,5);
       const double u0 = u(q...,0);
       const double u1 = u(q...,1);
       const double u2 = u(q...,2);
       Du(q...,0) = D00 * u0 + D01 * u1 + D02 * u2;
       Du(q...,1) = D10 * u0 + D11 * u1 + D12 * u2;
       Du(q...,2) = D20 * u0 + D21 * u1 + D22 * u2;
+   });
+   return Du;
+}
+
+// 2D with VDim
+template <typename DiagonalSymmTensor,
+          typename Tensor,
+          std::enable_if_t<
+             is_diagonal_symmetric_tensor<DiagonalSymmTensor> &&
+             get_diagonal_symmetric_tensor_diagonal_rank<DiagonalSymmTensor> == 2 &&
+             get_diagonal_symmetric_tensor_values_rank<DiagonalSymmTensor> == 1 &&
+             get_tensor_rank<Tensor> == 4,
+             bool> = true >
+MFEM_HOST_DEVICE inline
+auto operator*(const DiagonalSymmTensor &D, const Tensor &u)
+{
+   constexpr int QuadsX = 0;
+   constexpr int QuadsY = 1;
+   constexpr int CompDim = 2;
+   constexpr int VDim = 3;
+   constexpr int QX_c = get_tensor_size<QuadsX,Tensor>;
+   constexpr int QY_c = get_tensor_size<QuadsY,Tensor>;
+   constexpr int Dim_c = get_tensor_size<CompDim,Tensor>;
+   constexpr int VD_c = get_tensor_size<VDim,Tensor>;
+   const int QX_r = u.template Size<QuadsX>();
+   const int QY_r = u.template Size<QuadsY>();
+   const int Dim_r = u.template Size<CompDim>();
+   const int VD_r = u.template Size<VDim>();
+   StaticResultTensor<Tensor,QX_c,QY_c,Dim_c,VD_c> Du(QX_r,QY_r,Dim_r,VD_r);
+   Foreach<QuadsX>(u, [&](int qx)
+   {
+      Foreach<QuadsY>(u, [&](int qy)
+      {
+         const double D00 = D(qx,qy,0);
+         const double D01 = D(qx,qy,1);
+         const double D11 = D(qx,qy,2);
+         const double D10 = D01;
+         Foreach<VDim>(u, [&](int vd){
+            const double u0 = u(qx,qy,0,vd);
+            const double u1 = u(qx,qy,1,vd);
+            Du(qx,qy,0,vd) = D00 * u0 + D01 * u1;
+            Du(qx,qy,1,vd) = D10 * u0 + D11 * u1;
+         });
+      });
+   });
+   return Du;
+}
+
+// 3D
+template <typename DiagonalSymmTensor,
+          typename Tensor,
+          std::enable_if_t<
+             is_diagonal_symmetric_tensor<DiagonalSymmTensor> &&
+             get_diagonal_symmetric_tensor_diagonal_rank<DiagonalSymmTensor> == 3 &&
+             get_diagonal_symmetric_tensor_values_rank<DiagonalSymmTensor> == 1 &&
+             get_tensor_rank<Tensor> == 5,
+             bool> = true >
+MFEM_HOST_DEVICE inline
+auto operator*(const DiagonalSymmTensor &D, const Tensor &u)
+{
+   constexpr int QuadsX = 0;
+   constexpr int QuadsY = 1;
+   constexpr int QuadsZ = 2;
+   constexpr int CompDim = 3;
+   constexpr int VDim = 4;
+   constexpr int QX_c = get_tensor_size<QuadsX,Tensor>;
+   constexpr int QY_c = get_tensor_size<QuadsY,Tensor>;
+   constexpr int QZ_c = get_tensor_size<QuadsZ,Tensor>;
+   constexpr int Dim_c = get_tensor_size<CompDim,Tensor>;
+   constexpr int VD_c = get_tensor_size<VDim,Tensor>;
+   const int QX_r = u.template Size<QuadsX>();
+   const int QY_r = u.template Size<QuadsY>();
+   const int QZ_r = u.template Size<QuadsZ>();
+   const int Dim_r = u.template Size<CompDim>();
+   const int VD_r = u.template Size<VDim>();
+   StaticResultTensor<Tensor,QX_c,QY_c,QZ_c,Dim_c,VD_c> Du(QX_r,QY_r,QZ_r,Dim_r,VD_r);
+   Foreach<QuadsX>(u, [&](int qx)
+   {
+      Foreach<QuadsY>(u, [&](int qy)
+      {
+         Foreach<QuadsZ>(u, [&](int qz)
+         {
+            const double D00 = D(qx,qy,qz,0);
+            const double D01 = D(qx,qy,qz,1);
+            const double D02 = D(qx,qy,qz,2);
+            const double D11 = D(qx,qy,qz,3);
+            const double D12 = D(qx,qy,qz,4);
+            const double D22 = D(qx,qy,qz,5);
+            const double D10 = D01;
+            const double D20 = D02;
+            const double D21 = D12;
+            Foreach<VDim>(u, [&](int vd){
+               const double u0 = u(qx,qy,qz,0,vd);
+               const double u1 = u(qx,qy,qz,1,vd);
+               const double u2 = u(qx,qy,qz,2,vd);
+               Du(qx,qy,qz,0,vd) = D00 * u0 + D01 * u1 + D02 * u2;
+               Du(qx,qy,qz,1,vd) = D10 * u0 + D11 * u1 + D12 * u2;
+               Du(qx,qy,qz,2,vd) = D20 * u0 + D21 * u1 + D22 * u2;
+            });
+         });
+      });
    });
    return Du;
 }
