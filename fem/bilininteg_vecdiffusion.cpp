47d58f11fc7823cbbe650e98aa804fcb9b8074db
Yohann Dudouit <yohann.dudouit@gmail.com>
2021-10-19 18:46:26 -0700

    Instantiate same vecdiff kernels as diffusion.

--- fem/bilininteg_vecdiffusion.cpp
+++ fem/bilininteg_vecdiffusion.cpp
@@ -618,10 +618,11 @@ void VectorDiffusionIntegrator::AddMultPA(const Vector &x, Vector &y) const
       const Array<double> &Bt = maps->Bt;
       const Array<double> &Gt = maps->Gt;
       const Vector &D = pa_data;
+      const int ID = (D1D << 4) | Q1D;
 
       if (dim == 2 && sdim == 3)
       {
-         switch ((dofs1D << 4 ) | quad1D)
+         switch (ID)
          {
             // case 0x22: return PAVectorDiffusionApply2D<2,2,3>(ne,B,G,Bt,Gt,D,x,y);
             // case 0x33: return PAVectorDiffusionApply2D<3,3,3>(ne,B,G,Bt,Gt,D,x,y);
@@ -636,10 +637,36 @@ void VectorDiffusionIntegrator::AddMultPA(const Vector &x, Vector &y) const
          }
       }
       if (dim == 2 && sdim == 2)
-      { return PAVectorDiffusionApply2D(ne,B,G,Bt,Gt,D,x,y,D1D,Q1D,sdim); }
+      {
+         switch (ID)
+         {
+            case 0x22: return ApplyDiff<2,2,true,2,2,16>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x33: return ApplyDiff<2,2,true,3,3,16>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x44: return ApplyDiff<2,2,true,4,4,8>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x55: return ApplyDiff<2,2,true,5,5,8>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x66: return ApplyDiff<2,2,true,6,6,4>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x77: return ApplyDiff<2,2,true,7,7,4>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x88: return ApplyDiff<2,2,true,8,8,2>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x99: return ApplyDiff<2,2,true,9,9,2>(ne,B,G,Bt,Gt,D,x,y);
+            default:   return PAVectorDiffusionApply2D(ne,B,G,Bt,Gt,D,x,y,D1D,Q1D,sdim);
+         }
+      }
 
       if (dim == 3 && sdim == 3)
-      { return PAVectorDiffusionApply3D(ne,B,G,Bt,Gt,D,x,y,D1D,Q1D); }
+      {
+         switch (ID)
+         {
+            case 0x23: return ApplyDiff<3,3,true,2,3>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x34: return ApplyDiff<3,3,true,3,4>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x45: return ApplyDiff<3,3,true,4,5>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x46: return ApplyDiff<3,3,true,4,6>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x56: return ApplyDiff<3,3,true,5,6>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x67: return ApplyDiff<3,3,true,6,7>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x78: return ApplyDiff<3,3,true,7,8>(ne,B,G,Bt,Gt,D,x,y);
+            case 0x89: return ApplyDiff<3,3,true,8,9>(ne,B,G,Bt,Gt,D,x,y);
+            default:   return PAVectorDiffusionApply3D(ne,B,G,Bt,Gt,D,x,y,D1D,Q1D);
+         }
+      }
 
       MFEM_ABORT("Unknown kernel.");
    }
