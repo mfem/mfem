28021f10ee7f22edd468f7019c316ea83ff09082
Yohann Dudouit <dudouit1@llnl.gov>
2021-10-22 22:57:59 -0700

    More code for determinants.

--- linalg/tensor/operators/determinant.hpp
+++ linalg/tensor/operators/determinant.hpp
@@ -20,11 +20,16 @@ namespace mfem
 // Determinant
 template <typename Tensor,
           std::enable_if_t<
-             is_dynamic_matrix<Tensor>,
+             is_dynamic_matrix<Tensor> &&
+             !is_threaded_tensor_dim<Tensor,0>,
           bool> = true>
 MFEM_HOST_DEVICE inline
 auto Determinant(const Tensor &J)
 {
+   using T = get_tensor_value_type<Tensor>;
+   MFEM_ASSERT_KERNEL(
+      J.template Size<0>()== J.template Size<1>(),
+      "The matrix must be square.");
    if (J.template Size<0>()==3 && J.template Size<1>()==3)
    {
       return J(0,0)*J(1,1)*J(2,2)-J(0,2)*J(1,1)*J(2,0)
@@ -41,14 +46,96 @@ auto Determinant(const Tensor &J)
    }
    else
    {
-      // TODO abort
-      return 0;
+      MFEM_ABORT_KERNEL("Not yet supported.");
+      // T det{};
+      // Foreach<0>(J,[&](int row){
+      //    det += pow(-1,row) * J(row,0) * Determinant(Sub<0>(row,Sub<1>(0,J)));
+      // });
+      // return det;
+      return T{};
    }
 }
 
 template <typename Tensor,
           std::enable_if_t<
-             is_static_matrix<3,3,Tensor>,
+             is_dynamic_matrix<Tensor> &&
+             is_threaded_tensor_dim<Tensor,0>,
+          bool> = true>
+MFEM_HOST_DEVICE inline
+auto Determinant(const Tensor &J)
+{
+   using T = get_tensor_value_type<Tensor>;
+   MFEM_ASSERT_KERNEL(
+      J.template Size<0>()== J.template Size<1>(),
+      "The matrix must be square.");
+   if (J.template Size<0>()==1 && J.template Size<1>()==1)
+   {
+      return J(0,0);
+   }
+   else
+   {
+      MFEM_ABORT_KERNEL("Not yet supported.");
+      // Is this correct?
+      // MFEM_SHARED T det{};
+      // Foreach<0>(J,[&](int row){
+      //    det += pow(-1,row) * J(row,0) * Determinant(Sub<0>(row,Sub<1>(0,J))); // TODO: Use AtomicAdd
+      // });
+      // MFEM_SYNC_THREAD;
+      // return det;
+      return T{};
+   }
+}
+
+template <typename Tensor,
+          std::enable_if_t<
+             is_static_tensor<Tensor> &&
+             get_tensor_rank<Tensor> == 2 &&
+             get_tensor_size<0,Tensor> == get_tensor_size<1,Tensor> &&
+             (get_tensor_size<0,Tensor> > 1) &&
+             !is_threaded_tensor_dim<Tensor,0>,
+          bool> = true>
+MFEM_HOST_DEVICE inline
+auto Determinant(const Tensor &J)
+{
+   MFEM_ABORT_KERNEL("Not yet supported.");
+   using T = get_tensor_value_type<Tensor>;
+   // MFEM_SHARED T det{};
+   // Foreach<0>(J,[&](int row){
+   //    AtomicAdd(det,
+   //              pow(-1,row) * J(row,0) * Determinant(
+   //                                          Sub<0>(row,
+   //                                             Sub<1>(0, J ))));
+   // });
+   // MFEM_SYNC_THREAD;
+   // return det;
+   return T{};
+}
+
+template <typename Tensor,
+          std::enable_if_t<
+             is_static_tensor<Tensor> &&
+             get_tensor_rank<Tensor> == 2 &&
+             get_tensor_size<0,Tensor> == get_tensor_size<1,Tensor> &&
+             (get_tensor_size<0,Tensor> > 3) &&
+             is_threaded_tensor_dim<Tensor,0>,
+          bool> = true>
+MFEM_HOST_DEVICE inline
+auto Determinant(const Tensor &J)
+{
+   MFEM_ABORT_KERNEL("Not yet supported.");
+   using T = get_tensor_value_type<Tensor>;
+   // T det{};
+   // Foreach<0>(J,[&](int row){
+   //    det += pow(-1,row) * J(row,0) * Determinant(Sub<0>(row,Sub<1>(0,J)));
+   // });
+   // return det;
+   return T{};
+}
+
+template <typename Tensor,
+          std::enable_if_t<
+             is_static_matrix<3,3,Tensor> &&
+             !is_threaded_tensor_dim<Tensor,0>,
           bool> = true>
 MFEM_HOST_DEVICE inline
 auto Determinant(const Tensor &J)
@@ -60,7 +147,8 @@ auto Determinant(const Tensor &J)
 
 template <typename Tensor,
           std::enable_if_t<
-             is_static_matrix<2,2,Tensor>,
+             is_static_matrix<2,2,Tensor> &&
+             !is_threaded_tensor_dim<Tensor,0>,
           bool> = true>
 MFEM_HOST_DEVICE inline
 auto Determinant(const Tensor &J)
