c413c5674ce9655280ceb33fc57ff4d474d01508
Yohann Dudouit <dudouit1@llnl.gov>
2021-11-04 12:46:32 -0700

    GetLayout prototype

---
+++ linalg/tensor/utilities/get_layout.hpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2010-2020, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#ifndef MFEM_TENSOR_UTIL_LAYOUT
+#define MFEM_TENSOR_UTIL_LAYOUT
+
+#include "../tensor.hpp"
+
+namespace mfem
+{
+
+template <typename Container, typename Layout>
+auto GetLayout(const Tensor<Container,Layout>& t)
+{
+   return static_cast<const Layout&>(t);
+}
+
+} // mfem namespace
+
+#endif // MFEM_TENSOR_UTIL_LAYOUT
