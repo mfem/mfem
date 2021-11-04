42f4573a20ca5acd53ad1cbb0dbd2b4c49cdadc8
Yohann Dudouit <dudouit1@llnl.gov>
2021-11-04 12:43:19 -0700

    Refactor DynamicLayout.

--- linalg/tensor/layouts/dynamic_layout.hpp
+++ linalg/tensor/layouts/dynamic_layout.hpp
@@ -12,31 +12,37 @@
 #ifndef MFEM_DYNAMIC_LAYOUT
 #define MFEM_DYNAMIC_LAYOUT
 
+#include "../utilities/helper_constants.hpp"
 #include "layout_impl.hpp"
 #include "../../../general/error.hpp"
 #include "layout_traits.hpp"
 
 namespace mfem
 {
+template <int... Sizes>
+class SizedDynamicLayout;
+
+template <int Rank>
+using DynamicLayout = instantiate<SizedDynamicLayout,int_repeat<Dynamic,Rank>>;
 
 /// A dynamic layout with first index fastest
 template <int... Sizes>
-class DynamicLayout
+class SizedDynamicLayout
 {
 private:
    static constexpr int Rank = sizeof...(Sizes);
    int sizes[Rank];
 
 public:
-   template <typename... Sizes> MFEM_HOST_DEVICE
-   DynamicLayout(int arg0, Sizes... args)
+   template <typename... Dims> MFEM_HOST_DEVICE
+   SizedDynamicLayout(int arg0, Dims... args)
    {
       // TODO Add Sizes checks
       InitDynamicLayout<Rank>::result(sizes, arg0, args...);
    }
 
    template <typename Layout> MFEM_HOST_DEVICE
-   DynamicLayout(const Layout &rhs)
+   SizedDynamicLayout(const Layout &rhs)
    {
       // TODO Add Sizes checks
       InitDynamicLayout<Rank>::result(sizes,rhs);
@@ -62,46 +68,60 @@ public:
 };
 
 // get_layout_rank
-template <int Rank>
-struct get_layout_rank_v<DynamicLayout<Rank>>
+template <int... Sizes>
+struct get_layout_rank_v<SizedDynamicLayout<Sizes...>>
 {
-   static constexpr int value = Rank;
+   static constexpr int value = sizeof...(Sizes);
 };
 
 // is_dynamic_layout
-template<int Rank>
-struct is_dynamic_layout_v<DynamicLayout<Rank>>
+template <int... Sizes>
+struct is_dynamic_layout_v<SizedDynamicLayout<Sizes...>>
 {
    static constexpr bool value = true;
 };
 
 // is_serial_layout
-template <int Rank>
-struct is_serial_layout_v<DynamicLayout<Rank>>
+template <int... Sizes>
+struct is_serial_layout_v<SizedDynamicLayout<Sizes...>>
 {
    static constexpr bool value = true;
 };
 
 // get_layout_size
-template <int N, int Rank>
-struct get_layout_size_v<N, DynamicLayout<Rank>>
+template <int N, int... Sizes>
+struct get_layout_size_v<N, SizedDynamicLayout<Sizes...>>
 {
-   static constexpr int value = Dynamic;
+   static constexpr int value = get_value<N,Sizes...>;
 };
 
 // get_layout_sizes
-template <int Rank>
-struct get_layout_sizes_t<DynamicLayout<Rank>>
+template <int... Sizes>
+struct get_layout_sizes_t<SizedDynamicLayout<Sizes...>>
 {
-   using type = int_repeat<Dynamic,Rank>;
+   using type = int_list<Sizes...>;//int_repeat<Dynamic,Rank>;
+};
+
+// get_layout_capacity
+template <int First, int... Rest>
+struct get_layout_capacity_v<SizedDynamicLayout<First,Rest...>>
+{
+   static constexpr int value = (First==Dynamic? DynamicMaxSize : First) *
+                                get_layout_capacity_v<SizedDynamicLayout<Rest...>>::value;
+};
+
+template <int Size>
+struct get_layout_capacity_v<SizedDynamicLayout<Size>>
+{
+   static constexpr int value = (Size==Dynamic? DynamicMaxSize : Size);
 };
 
 // get_layout_result_type
-template <int Rank>
-struct get_layout_result_type<DynamicLayout<Rank>>
+template <int... Sizes>
+struct get_layout_result_type<SizedDynamicLayout<Sizes...>>
 {
-   template <int myRank>
-   using type = DynamicLayout<myRank>;
+   template <int... mySizes>
+   using type = SizedDynamicLayout<mySizes...>;
 };
 
 } // namespace mfem
