61b2526cd1b6702c9eccf19364358e848348d784
Yohann Dudouit <dudouit1@llnl.gov>
2021-11-04 12:44:55 -0700

    Refactor Dynamic3dThreadLayout

--- linalg/tensor/layouts/dynamic_3dthread_layout.hpp
+++ linalg/tensor/layouts/dynamic_3dthread_layout.hpp
@@ -19,19 +19,37 @@
 namespace mfem
 {
 
+template <int BatchSize, int... Sizes>
+class SizedDynamic3dThreadLayout;
+
 template <int Rank, int BatchSize>
-class Dynamic3dThreadLayout
+using Dynamic3dThreadLayout = instantiate<SizedDynamic3dThreadLayout,
+                                          append<
+                                             int_list<BatchSize>,
+                                             int_repeat<Dynamic,Rank> > >;
+
+template <int BatchSize, int FirstSize, int SecondSize, int ThirdSize, int... Sizes>
+class SizedDynamic3dThreadLayout<BatchSize,FirstSize,SecondSize,ThirdSize,Sizes...>
 {
 private:
    const int size0;
    const int size1;
    const int size2;
-   DynamicLayout<Rank-3> layout;
+   SizedDynamicLayout<Sizes...> layout;
 public:
-   template <typename... Sizes> MFEM_HOST_DEVICE inline
-   Dynamic3dThreadLayout(int size0, int size1, int size2, Sizes... sizes)
+   template <typename... Ss> MFEM_HOST_DEVICE inline
+   SizedDynamic3dThreadLayout(int size0, int size1, int size2, Ss... sizes)
    : size0(size0), size1(size1), size2(size2), layout(sizes...)
    {
+      MFEM_ASSERT_KERNEL(
+         FirstSize==Dynamic || FirstSize==size0,
+         "Compilation time and runtime sizes must be the same.");
+      MFEM_ASSERT_KERNEL(
+         SecondSize==Dynamic || SecondSize==size1,
+         "Compilation time and runtime sizes must be the same.");
+      MFEM_ASSERT_KERNEL(
+         ThirdSize==Dynamic || ThirdSize==size2,
+         "Compilation time and runtime sizes must be the same.");
       MFEM_ASSERT_KERNEL(
          size0<MFEM_THREAD_SIZE(x),
          "The first dimension exceeds the number of x threads.");
@@ -43,23 +61,36 @@ public:
          "The third dimension exceeds the number of z threads.");
    }
 
+   template <typename Layout> MFEM_HOST_DEVICE
+   SizedDynamic3dThreadLayout(const Layout &rhs)
+   : size0(rhs.template Size<0>()),
+     size1(rhs.template Size<1>()),
+     size2(rhs.template Size<2>()),
+     layout( Get<0>( 0, Get<0>( 0, Get<0>( 0, rhs ) ) ) )
+   {
+      constexpr int Rank = sizeof...(Sizes) + 3;
+      static_assert(
+         Rank == get_layout_rank<Layout>,
+         "Can't copy-construct a layout of different rank.");
+   }
+
    template <typename... Idx> MFEM_HOST_DEVICE inline
    int index(int idx0, int idx1, int idx2, Idx... idx) const
    {
       MFEM_ASSERT_KERNEL(
          idx0==MFEM_THREAD_ID(x),
          "The first index must be equal to the x thread index"
-         " when using Dynamic3dThreadLayout. Use shared memory"
+         " when using SizedDynamic3dThreadLayout. Use shared memory"
          " to access values stored in a different thread.");
       MFEM_ASSERT_KERNEL(
          idx1==MFEM_THREAD_ID(y),
          "The second index must be equal to the y thread index"
-         " when using Dynamic3dThreadLayout. Use shared memory"
+         " when using SizedDynamic3dThreadLayout. Use shared memory"
          " to access values stored in a different thread.");
       MFEM_ASSERT_KERNEL(
          idx2==MFEM_THREAD_ID(z),
          "The third index must be equal to the z thread index"
-         " when using Dynamic3dThreadLayout. Use shared memory"
+         " when using SizedDynamic3dThreadLayout. Use shared memory"
          " to access values stored in a different thread.");
       return layout.index(idx...);
    }
@@ -68,6 +99,7 @@ public:
    template <int N> MFEM_HOST_DEVICE inline
    constexpr int Size() const
    {
+      constexpr int Rank = sizeof...(Sizes) + 3;
       static_assert(
          N>=0 && N<Rank,
          "Accessed size is higher than the rank of the Tensor.");
@@ -75,19 +107,31 @@ public:
    }
 };
 
-template <int BatchSize>
-class Dynamic3dThreadLayout<1,BatchSize>
+template <int BatchSize, int FirstSize>
+class SizedDynamic3dThreadLayout<BatchSize,FirstSize>
 {
 private:
    const int size0;
 public:
    MFEM_HOST_DEVICE inline
-   Dynamic3dThreadLayout(int size0)
+   SizedDynamic3dThreadLayout(int size0)
    : size0(size0)
    {
       MFEM_ASSERT_KERNEL(
+         FirstSize==Dynamic || FirstSize==size0,
+         "Compilation time and runtime sizes must be the same.");
+      MFEM_ASSERT_KERNEL(
          size0<MFEM_THREAD_SIZE(x),
          "The first dimension exceeds the number of x threads.");
+   }
+
+   template <typename Layout> MFEM_HOST_DEVICE
+   SizedDynamic3dThreadLayout(const Layout &rhs)
+   : size0(rhs.template Size<0>())
+   {
+      static_assert(
+         1 == get_layout_rank<Layout>,
+         "Can't copy-construct a layout of different rank.");
    }
 
    MFEM_HOST_DEVICE inline
@@ -96,7 +140,7 @@ public:
       MFEM_ASSERT_KERNEL(
          idx==MFEM_THREAD_ID(x),
          "The first index must be equal to the x thread index"
-         " when using Dynamic3dThreadLayout. Use shared memory"
+         " when using SizedDynamic3dThreadLayout. Use shared memory"
          " to access values stored in a different thread.");
       return 0;
    }
@@ -112,17 +156,23 @@ public:
    }
 };
 
-template <int BatchSize>
-class Dynamic3dThreadLayout<2,BatchSize>
+template <int BatchSize, int FirstSize, int SecondSize>
+class SizedDynamic3dThreadLayout<BatchSize,FirstSize,SecondSize>
 {
 private:
    const int size0;
    const int size1;
 public:
    MFEM_HOST_DEVICE inline
-   Dynamic3dThreadLayout(int size0, int size1)
+   SizedDynamic3dThreadLayout(int size0, int size1)
    : size0(size0), size1(size1)
    {
+      MFEM_ASSERT_KERNEL(
+         FirstSize==Dynamic || FirstSize==size0,
+         "Compilation time and runtime sizes must be the same.");
+      MFEM_ASSERT_KERNEL(
+         SecondSize==Dynamic || SecondSize==size1,
+         "Compilation time and runtime sizes must be the same.");
       MFEM_ASSERT_KERNEL(
          size0<MFEM_THREAD_SIZE(x),
          "The first dimension exceeds the number of x threads.");
@@ -131,18 +181,28 @@ public:
          "The second dimension exceeds the number of y threads.");
    }
 
+   template <typename Layout> MFEM_HOST_DEVICE
+   SizedDynamic3dThreadLayout(const Layout &rhs)
+   : size0(rhs.template Size<0>()),
+     size1(rhs.template Size<1>())
+   {
+      static_assert(
+         2 == get_layout_rank<Layout>,
+         "Can't copy-construct a layout of different rank.");
+   }
+
    MFEM_HOST_DEVICE inline
    int index(int idx0, int idx1) const
    {
       MFEM_ASSERT_KERNEL(
          idx0==MFEM_THREAD_ID(x),
          "The first index must be equal to the x thread index"
-         " when using Dynamic3dThreadLayout. Use shared memory"
+         " when using SizedDynamic3dThreadLayout. Use shared memory"
          " to access values stored in a different thread.");
       MFEM_ASSERT_KERNEL(
          idx1==MFEM_THREAD_ID(y),
          "The second index must be equal to the y thread index"
-         " when using Dynamic3dThreadLayout. Use shared memory"
+         " when using SizedDynamic3dThreadLayout. Use shared memory"
          " to access values stored in a different thread.");
       return 0;
    }
@@ -157,8 +217,8 @@ public:
    }
 };
 
-template <int BatchSize>
-class Dynamic3dThreadLayout<3,BatchSize>
+template <int BatchSize, int FirstSize, int SecondSize, int ThirdSize>
+class SizedDynamic3dThreadLayout<BatchSize,FirstSize,SecondSize,ThirdSize>
 {
 private:
    const int size0;
@@ -166,9 +226,18 @@ private:
    const int size2;
 public:
    MFEM_HOST_DEVICE inline
-   Dynamic3dThreadLayout(int size0, int size1, int size2)
+   SizedDynamic3dThreadLayout(int size0, int size1, int size2)
    : size0(size0), size1(size1), size2(size2)
    {
+      MFEM_ASSERT_KERNEL(
+         FirstSize==Dynamic || FirstSize==size0,
+         "Compilation time and runtime sizes must be the same.");
+      MFEM_ASSERT_KERNEL(
+         SecondSize==Dynamic || SecondSize==size1,
+         "Compilation time and runtime sizes must be the same.");
+      MFEM_ASSERT_KERNEL(
+         ThirdSize==Dynamic || ThirdSize==size2,
+         "Compilation time and runtime sizes must be the same.");
       MFEM_ASSERT_KERNEL(
          size0<MFEM_THREAD_SIZE(x),
          "The first dimension exceeds the number of x threads.");
@@ -180,23 +249,34 @@ public:
          "The third dimension exceeds the number of z threads.");
    }
 
+   template <typename Layout> MFEM_HOST_DEVICE
+   SizedDynamic3dThreadLayout(const Layout &rhs)
+   : size0(rhs.template Size<0>()),
+     size1(rhs.template Size<1>()),
+     size2(rhs.template Size<2>())
+   {
+      static_assert(
+         3 == get_layout_rank<Layout>,
+         "Can't copy-construct a layout of different rank.");
+   }
+
    MFEM_HOST_DEVICE inline
    int index(int idx0, int idx1, int idx2) const
    {
       MFEM_ASSERT_KERNEL(
          idx0==MFEM_THREAD_ID(x),
          "The first index must be equal to the x thread index"
-         " when using Dynamic3dThreadLayout. Use shared memory"
+         " when using SizedDynamic3dThreadLayout. Use shared memory"
          " to access values stored in a different thread.");
       MFEM_ASSERT_KERNEL(
          idx1==MFEM_THREAD_ID(y),
          "The second index must be equal to the y thread index"
-         " when using Dynamic3dThreadLayout. Use shared memory"
+         " when using SizedDynamic3dThreadLayout. Use shared memory"
          " to access values stored in a different thread.");
       MFEM_ASSERT_KERNEL(
          idx2==MFEM_THREAD_ID(z),
          "The third index must be equal to the z thread index"
-         " when using Dynamic3dThreadLayout. Use shared memory"
+         " when using SizedDynamic3dThreadLayout. Use shared memory"
          " to access values stored in a different thread.");
       return 0;
    }
@@ -213,91 +293,116 @@ public:
 };
 
 // get_layout_rank
-template <int Rank, int BatchSize>
-struct get_layout_rank_v<Dynamic3dThreadLayout<Rank, BatchSize>>
+template <int BatchSize, int... Sizes>
+struct get_layout_rank_v<SizedDynamic3dThreadLayout<BatchSize,Sizes...>>
 {
-   static constexpr int value = Rank;
+   static constexpr int value = sizeof...(Sizes);
 };
 
 // is_dynamic_layout
-template <int Rank, int BatchSize>
-struct is_dynamic_layout_v<Dynamic3dThreadLayout<Rank,BatchSize>>
+template <int BatchSize, int... Sizes>
+struct is_dynamic_layout_v<SizedDynamic3dThreadLayout<BatchSize,Sizes...>>
 {
    static constexpr bool value = true;
 };
 
 // is_3d_threaded_layout
-template <int Rank, int BatchSize>
-struct is_3d_threaded_layout_v<Dynamic3dThreadLayout<Rank,BatchSize>>
+template <int BatchSize, int... Sizes>
+struct is_3d_threaded_layout_v<SizedDynamic3dThreadLayout<BatchSize,Sizes...>>
 {
    static constexpr bool value = true;
 };
 
 // is_serial_layout_dim
-template <int Rank, int BatchSize>
-struct is_serial_layout_dim_v<Dynamic3dThreadLayout<Rank,BatchSize>, 0>
+template <int BatchSize, int... Sizes>
+struct is_serial_layout_dim_v<SizedDynamic3dThreadLayout<BatchSize,Sizes...>, 0>
 {
    static constexpr bool value = false;
 };
 
-template <int Rank, int BatchSize>
-struct is_serial_layout_dim_v<Dynamic3dThreadLayout<Rank,BatchSize>, 1>
+template <int BatchSize, int... Sizes>
+struct is_serial_layout_dim_v<SizedDynamic3dThreadLayout<BatchSize,Sizes...>, 1>
 {
    static constexpr bool value = false;
 };
 
-template <int Rank, int BatchSize>
-struct is_serial_layout_dim_v<Dynamic3dThreadLayout<Rank,BatchSize>, 2>
+template <int BatchSize, int... Sizes>
+struct is_serial_layout_dim_v<SizedDynamic3dThreadLayout<BatchSize,Sizes...>, 2>
 {
    static constexpr bool value = false;
 };
 
 // is_threaded_layout_dim
-template <int Rank, int BatchSize>
-struct is_threaded_layout_dim_v<Dynamic3dThreadLayout<Rank,BatchSize>, 0>
+template <int BatchSize, int... Sizes>
+struct is_threaded_layout_dim_v<SizedDynamic3dThreadLayout<BatchSize,Sizes...>, 0>
 {
    static constexpr bool value = true;
 };
 
-template <int Rank, int BatchSize>
-struct is_threaded_layout_dim_v<Dynamic3dThreadLayout<Rank,BatchSize>, 1>
+template <int BatchSize, int... Sizes>
+struct is_threaded_layout_dim_v<SizedDynamic3dThreadLayout<BatchSize,Sizes...>, 1>
 {
    static constexpr bool value = true;
 };
 
-template <int Rank, int BatchSize>
-struct is_threaded_layout_dim_v<Dynamic3dThreadLayout<Rank,BatchSize>, 2>
+template <int BatchSize, int... Sizes>
+struct is_threaded_layout_dim_v<SizedDynamic3dThreadLayout<BatchSize,Sizes...>, 2>
 {
    static constexpr bool value = true;
 };
 
 // get_layout_size
-template <int N, int Rank, int BatchSize>
-struct get_layout_size_v<N, Dynamic3dThreadLayout<Rank, BatchSize>>
+template <int N, int BatchSize, int... Sizes>
+struct get_layout_size_v<N, SizedDynamic3dThreadLayout<BatchSize,Sizes...>>
 {
-   static constexpr int value = Dynamic;
+   static constexpr int value = get_value<N,Sizes...>;
 };
 
 // get_layout_sizes
-template <int Rank, int BatchSize>
-struct get_layout_sizes_t<Dynamic3dThreadLayout<Rank,BatchSize>>
+template <int BatchSize, int... Sizes>
+struct get_layout_sizes_t<SizedDynamic3dThreadLayout<BatchSize,Sizes...>>
 {
-   using type = int_repeat<Dynamic,Rank>;
+   using type = int_list<Sizes...>;
+};
+
+// get_layout_capacity
+template <int BatchSize, int First, int Second, int Third, int... Rest>
+struct get_layout_capacity_v<SizedDynamic3dThreadLayout<BatchSize,First,Second,Third,Rest...>>
+{
+   static constexpr int value = get_layout_capacity<SizedDynamicLayout<Rest...>>;
+};
+
+template <int BatchSize, int First, int Second, int Third>
+struct get_layout_capacity_v<SizedDynamic3dThreadLayout<BatchSize,First,Second,Third>>
+{
+   static constexpr int value = 1;
+};
+
+template <int BatchSize, int First, int Second>
+struct get_layout_capacity_v<SizedDynamic3dThreadLayout<BatchSize,First,Second>>
+{
+   static constexpr int value = 1;
+};
+
+template <int BatchSize, int First>
+struct get_layout_capacity_v<SizedDynamic3dThreadLayout<BatchSize,First>>
+{
+   static constexpr int value = 1;
 };
 
 // get_layout_batch_size
-template <int Rank, int BatchSize>
-struct get_layout_batch_size_v<Dynamic3dThreadLayout<Rank, BatchSize>>
+template <int BatchSize, int... Sizes>
+struct get_layout_batch_size_v<SizedDynamic3dThreadLayout<BatchSize,Sizes...>>
 {
    static constexpr int value = BatchSize;
 };
 
 // get_layout_result_type
-template <int Rank, int BatchSize>
-struct get_layout_result_type<Dynamic3dThreadLayout<Rank,BatchSize>>
+template <int BatchSize, int... Sizes>
+struct get_layout_result_type<SizedDynamic3dThreadLayout<BatchSize,Sizes...>>
 {
    template <int myRank>
-   using type = Dynamic3dThreadLayout<myRank,BatchSize>;
+   using type = SizedDynamic3dThreadLayout<myRank,BatchSize>;
 };
 
 } // namespace mfem
