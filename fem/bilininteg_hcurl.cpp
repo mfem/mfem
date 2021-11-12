fcbf5c5f3b52a2fb66f28b34db635d48c54b6be1
Yohann Dudouit <dudouit1@llnl.gov>
2021-11-12 14:48:43 -0800

    Try to mute warnings.

--- fem/bilininteg_hcurl.cpp
+++ fem/bilininteg_hcurl.cpp
@@ -4424,6 +4424,7 @@ static void SmemPAHcurlL2Apply3DTranspose(const int D1D,
                                           const Vector &x,
                                           Vector &y)
 {
+#ifdef MFEM_DEVICE_COMPILE
    MFEM_VERIFY(D1D <= MAX_D1D, "Error: D1D > MAX_D1D");
    MFEM_VERIFY(Q1D <= MAX_Q1D, "Error: Q1D > MAX_Q1D");
 
@@ -4633,6 +4634,9 @@ static void SmemPAHcurlL2Apply3DTranspose(const int D1D,
          }
       } // qz
    }); // end of element loop
+#else
+   MFEM_ABORT("This kernel should only be used on GPU.");
+#endif
 }
 
 void MixedVectorWeakCurlIntegrator::AddMultPA(const Vector &x, Vector &y) const
