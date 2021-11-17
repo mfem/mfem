43be5a98bd4430767f5926b6438885d595c06ab3
Yohann Dudouit <dudouit1@llnl.gov>
2021-11-16 19:48:59 -0800

    Fix SquaredNorm for threaded tensors.

--- linalg/tensor/operators/norms.hpp
+++ linalg/tensor/operators/norms.hpp
@@ -12,14 +12,18 @@
 #ifndef MFEM_TENSOR_NORMS
 #define MFEM_TENSOR_NORMS
 
-#include "../../../general/forall.hpp"
+#include "../../../general/backends.hpp"
 #include "../tensor_traits.hpp"
 #include "../utilities/foreach.hpp"
 
 namespace mfem
 {
 
-template <typename Tensor> MFEM_HOST_DEVICE inline
+template <typename Tensor,
+          std::enable_if_t<
+             is_serial_tensor<Tensor>,
+             bool> = true >
+MFEM_HOST_DEVICE inline
 auto SquaredNorm(const Tensor& t)
 {
    using Scalar = get_tensor_type<Tensor>;
@@ -29,6 +33,30 @@ auto SquaredNorm(const Tensor& t)
       norm += val*val;
    });
    return norm;
+}
+
+template <typename Tensor,
+          std::enable_if_t<
+             !is_serial_tensor<Tensor>,
+             bool> = true >
+MFEM_HOST_DEVICE inline
+auto SquaredNorm(const Tensor& t)
+{
+   using Scalar = get_tensor_type<Tensor>;
+   MFEM_SHARED Scalar res;
+   if (MFEM_THREAD_ID(x)==0 && MFEM_THREAD_ID(y)==0 && MFEM_THREAD_ID(z)==0)
+   {
+      res = 0.0;
+   }
+   MFEM_SYNC_THREAD;
+   Scalar norm = 0;
+   ForallDims<Tensor>::Apply(t, [&](auto... idx){
+      const Scalar& val = t(idx...);
+      norm += val*val;
+   });
+   AtomicAdd(res, norm);
+   MFEM_SYNC_THREAD;
+   return res;
 }
 
 } // namespace mfem
