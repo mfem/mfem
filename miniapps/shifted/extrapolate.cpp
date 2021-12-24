7c9558bb503bf25bf7f0e4ec6580923979dc001c
Vladimir Z Tomov <tomov2@llnl.gov>
2021-12-23 23:51:06 -0800

    Some code reorg.

--- miniapps/shifted/extrapolate.cpp
+++ miniapps/shifted/extrapolate.cpp
@@ -93,14 +93,36 @@ public:
    }
 };
 
+void PrintNorm(int myid, Vector &v, std::string text)
+{
+   double norm = v.Norml1();
+   MPI_Allreduce(MPI_IN_PLACE, &norm, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+   if (myid == 0)
+   {
+      std::cout << std::setprecision(12) << std::fixed
+                << text << norm << std::endl;
+   }
+}
+
+void PrintIntegral(int myid, ParGridFunction &g, std::string text)
+{
+   ConstantCoefficient zero(0.0);
+   double norm = g.ComputeL1Error(zero);
+   if (myid == 0)
+   {
+      std::cout << std::setprecision(12) << std::fixed
+                << text << norm << std::endl;
+   }
+}
+
 class NormalGradCoeff : public Coefficient
 {
 private:
    const ParGridFunction &u_gf;
-   LevelSetNormalGradCoeff &n_coeff;
+   VectorCoefficient &n_coeff;
 
 public:
-   NormalGradCoeff(const ParGridFunction &u, LevelSetNormalGradCoeff &n) :
+   NormalGradCoeff(const ParGridFunction &u, VectorCoefficient &n) :
       u_gf(u), n_coeff(n) { }
 
    virtual double Eval(ElementTransformation &T, const IntegrationPoint &ip)
@@ -114,7 +136,7 @@ public:
 
 };
 
-class Extrapolator : public TimeDependentOperator
+class AdvectionOper : public TimeDependentOperator
 {
 private:
    Array<bool> &active_zones;
@@ -122,17 +144,14 @@ private:
    const Vector &b;
 
 public:
-   Extrapolator(Array<bool> &zones,
-                ParBilinearForm &Mbf, ParBilinearForm &Kbf, const Vector &rhs)
+   AdvectionOper(Array<bool> &zones,
+                 ParBilinearForm &Mbf, ParBilinearForm &Kbf, const Vector &rhs)
       : TimeDependentOperator(Mbf.Size()),
         active_zones(zones),
         M(Mbf), K(Kbf), b(rhs) { }
 
    virtual void Mult(const Vector &x, Vector &dx) const
    {
-      K.BilinearForm::operator=(0.0);
-      K.Assemble();
-
       ParFiniteElementSpace &pfes = *M.ParFESpace();
       const int NE = pfes.GetNE();
       const int nd = pfes.GetFE(0)->GetDof();
@@ -165,6 +184,180 @@ public:
    }
 };
 
+class Extrapolator
+{
+private:
+
+   void TimeLoop(ParGridFunction &sltn, ODESolver &ode_solver,
+                 double dt, int vis_x_pos)
+   {
+      char vishost[] = "localhost";
+      int  visport   = 19916, wsize = 500;
+      socketstream sock;
+
+      const int myid  = sltn.ParFESpace()->GetMyRank();
+      const double t_final = 0.4;
+      bool done = false;
+      double t = 0.0;
+      for (int ti = 0; !done;)
+      {
+         double dt_real = min(dt, t_final - t);
+         ode_solver.Step(sltn, t, dt_real);
+         ti++;
+
+         done = (t >= t_final - 1e-8*dt);
+         if (done || ti % vis_steps == 0)
+         {
+            if (myid == 0)
+            {
+               cout << "time step: " << ti << ", time: " << t << endl;
+            }
+            if (visualization)
+            {
+               common::VisualizeField(sock, vishost, visport, sltn,
+                                      "Solution", vis_x_pos, 570, wsize, wsize,
+                                      "rRjmm********A");
+               MPI_Barrier(sltn.ParFESpace()->GetComm());
+            }
+         }
+      }
+   }
+
+public:
+   bool visualization = false;
+   int vis_steps      = 5;
+   int extr_order = 1;
+
+   Extrapolator() { }
+
+   // The known values taken from elements where level_set > 0, and extrapolated
+   // to all other elements. The known values are not changed.
+   void Extrapolate(Coefficient &level_set, const ParGridFunction &input,
+                    ParGridFunction &xtrap)
+   {
+      ParMesh &pmesh = *input.ParFESpace()->GetParMesh();
+      const int order = input.ParFESpace()->GetOrder(0),
+                dim   = pmesh.Dimension(), NE = pmesh.GetNE();
+
+      char vishost[] = "localhost";
+      int  visport   = 19916, wsize = 500;
+
+      // Get a ParGridFunction and mark elements.
+      H1_FECollection fec(order, dim);
+      ParFiniteElementSpace pfes_H1(&pmesh, &fec);
+      ParGridFunction ls_gf(&pfes_H1);
+      ls_gf.ProjectCoefficient(level_set);
+      if (visualization)
+      {
+         socketstream sock;
+         common::VisualizeField(sock, vishost, visport, ls_gf,
+                                "Domain level set", 0, 0, wsize, wsize,
+                                "rRjmm********A");
+         MPI_Barrier(pmesh.GetComm());
+      }
+      // Mark elements.
+      Array<int> elem_marker;
+      ShiftedFaceMarker marker(pmesh, pfes_H1, false);
+      ls_gf.ExchangeFaceNbrData();
+      marker.MarkElements(ls_gf, elem_marker);
+
+      // The active zones are where we extrapolate (where the PDE is solved).
+      Array<bool> active_zones(NE);
+      for (int k = 0; k < NE; k++)
+      {
+         // Extrapolation is done in zones that are CUT or OUTSIDE.
+         active_zones[k] =
+            (elem_marker[k] == ShiftedFaceMarker::INSIDE) ? false : true;
+      }
+
+      // Setup a VectorCoefficient for n = - grad_ls / |grad_ls|.
+      // The sign makes it point out of the known region.
+      // The coefficient must be continuous to have well-defined transport.
+      LevelSetNormalGradCoeff ls_n_coeff_L2(ls_gf);
+      ParFiniteElementSpace pfes_H1_vec(&pmesh, &fec, dim);
+      ParGridFunction lsn_gf(&pfes_H1_vec);
+      ls_gf.ExchangeFaceNbrData();
+      lsn_gf.ProjectDiscCoefficient(ls_n_coeff_L2, GridFunction::ARITHMETIC);
+      VectorGridFunctionCoefficient ls_n_coeff(&lsn_gf);
+
+      // Initial solution.
+      // Trim to the known values (only elements inside the known region).
+      Array<int> dofs;
+      L2_FECollection fec_L2(order, dim);
+      ParFiniteElementSpace pfes_L2(&pmesh, &fec_L2);
+      ParGridFunction u(&pfes_L2);
+      u.ProjectGridFunction(input);
+      for (int k = 0; k < NE; k++)
+      {
+         pfes_L2.GetElementDofs(k, dofs);
+         if (elem_marker[k] != ShiftedFaceMarker::INSIDE)
+         { u.SetSubVector(dofs, 0.0); }
+      }
+      if (visualization)
+      {
+         socketstream sock;
+         common::VisualizeField(sock, vishost, visport, u,
+                                "Fixed (known) u values", wsize, 0, wsize, wsize,
+                                "rRjmm********A");
+      }
+
+      // Normal derivative function.
+      ParGridFunction grad_u_n(&pfes_L2);
+      NormalGradCoeff grad_u_n_coeff(u, ls_n_coeff);
+      grad_u_n.ProjectCoefficient(grad_u_n_coeff);
+      if (visualization)
+      {
+         socketstream sock;
+         common::VisualizeField(sock, vishost, visport, grad_u_n,
+                                "grad_u n", 2*wsize, 0, wsize, wsize,
+                                "rRjmm********A");
+      }
+
+      ParBilinearForm lhs_bf(&pfes_L2), rhs_bf(&pfes_L2);
+      lhs_bf.AddDomainIntegrator(new MassIntegrator);
+      const double alpha = -1.0;
+      rhs_bf.AddDomainIntegrator(new ConvectionIntegrator(ls_n_coeff, alpha));
+      auto trace_i = new NonconservativeDGTraceIntegrator(ls_n_coeff, alpha);
+      rhs_bf.AddInteriorFaceIntegrator(trace_i);
+      rhs_bf.KeepNbrBlock(true);
+
+      ls_gf.ExchangeFaceNbrData();
+      lhs_bf.Assemble();
+      lhs_bf.Finalize();
+      rhs_bf.Assemble(0);
+      rhs_bf.Finalize(0);
+
+      Vector rhs(pfes_L2.GetVSize());
+      rhs = 0.0;
+
+      // Compute a CFL time step.
+      double h_min = std::numeric_limits<double>::infinity();
+      for (int k = 0; k < NE; k++)
+      {
+         h_min = std::min(h_min, pmesh.GetElementSize(k));
+      }
+      MPI_Allreduce(MPI_IN_PLACE, &h_min, 1, MPI_DOUBLE, MPI_MIN,
+                    pfes_L2.GetComm());
+      h_min /= order;
+      // The propagation speed is 1.
+      double dt = 0.25 * h_min / 1.0;
+
+      // Time loop
+      AdvectionOper adv_oper(active_zones, lhs_bf, rhs_bf, rhs);
+      RK2Solver ode_solver(1.0);
+      ode_solver.Init(adv_oper);
+
+      // Constant extrapolation of [grad_u.n].
+      TimeLoop(grad_u_n, ode_solver, dt, 2*wsize);
+
+      // Linear extrapolation of u.
+      lhs_bf.Mult(grad_u_n, rhs);
+      TimeLoop(u, ode_solver, dt, wsize);
+
+      xtrap.ProjectGridFunction(u);
+   }
+};
+
 int main(int argc, char *argv[])
 {
    // Initialize MPI.
@@ -175,7 +368,6 @@ int main(int argc, char *argv[])
    const char *mesh_file = "../../data/inline-quad.mesh";
    int rs_levels = 2;
    int order = 2;
-   int ode_solver_type = 2;
    bool visualization = true;
    int vis_steps = 5;
 
@@ -187,10 +379,6 @@ int main(int argc, char *argv[])
    args.AddOption(&order, "-o", "--order",
                   "Finite element order (polynomial degree) or -1 for"
                   " isoparametric space.");
-   args.AddOption(&ode_solver_type, "-s", "--ode-solver",
-                  "ODE solver: 1 - Forward Euler,\n\t"
-                  "            2 - RK2 SSP,\n\t"
-                  "            3 - RK3 SSP");
    args.AddOption(&problem, "-p", "--problem",
                   "0 - 2D circle,\n\t"
                   "1 - 2D star");
@@ -207,193 +395,35 @@ int main(int argc, char *argv[])
    }
    if (myid == 0) { args.PrintOptions(cout); }
 
-   char vishost[] = "localhost";
-   int  visport   = 19916, wsize = 500;
-   socketstream sock_grad_u_n, sock_u;
 
-   // Refine the mesh.
+   // Refine the mesh and distribute.
    Mesh mesh(mesh_file, 1, 1);
    for (int lev = 0; lev < rs_levels; lev++) { mesh.UniformRefinement(); }
-
-   // MPI distribution.
    ParMesh pmesh(MPI_COMM_WORLD, mesh);
    mesh.Clear();
    const int dim = pmesh.Dimension(), NE = pmesh.GetNE();
 
-   FunctionCoefficient ls_coeff(domainLS), u0_coeff(solution0);
-
-   L2_FECollection fec_L2(order, dim, BasisType::GaussLobatto);
+   // Input function.
+   L2_FECollection fec_L2(order, dim);
    ParFiniteElementSpace pfes_L2(&pmesh, &fec_L2);
    ParGridFunction u(&pfes_L2);
+   FunctionCoefficient u0_coeff(solution0);
    u.ProjectCoefficient(u0_coeff);
 
-   // Initialize the level set.
-   H1_FECollection fec(order, dim);
-   ParFiniteElementSpace pfes_H1(&pmesh, &fec);
-   ParGridFunction level_set(&pfes_H1);
-   level_set.ProjectCoefficient(ls_coeff);
-   if (visualization)
-   {
-      socketstream sol_sock_w;
-      common::VisualizeField(sol_sock_w, vishost, visport, level_set,
-                             "Domain level set", 0, 0, wsize, wsize,
-                             "rRjmm********A");
-      MPI_Barrier(pmesh.GetComm());
-   }
-   // Setup a VectorCoefficient for n = - grad_ls / |grad_ls|.
-   // The sign makes it point out of the known region.
-   LevelSetNormalGradCoeff ls_n_coeff(level_set);
+   // Extrapolate.
+   Extrapolator xtrap;
+   xtrap.visualization = true;
+   xtrap.vis_steps = 5;
+   FunctionCoefficient ls_coeff(domainLS);
+   ParGridFunction ux(&pfes_L2);
+   xtrap.Extrapolate(ls_coeff, u, ux);
 
-   // Mark elements.
-   Array<int> elem_marker, dofs;
-   ShiftedFaceMarker marker(pmesh, pfes_L2, false);
-   level_set.ExchangeFaceNbrData();
-   marker.MarkElements(level_set, elem_marker);
-
-   // Trim to the known values (only elements inside the known region).
-   ParGridFunction u_known(u);
-   for (int k = 0; k < NE; k++)
-   {
-      pfes_L2.GetElementDofs(k, dofs);
-      if (elem_marker[k] != ShiftedFaceMarker::INSIDE)
-      { u_known.SetSubVector(dofs, 0.0); }
-   }
-   if (visualization)
-   {
-      socketstream sol_socks;
-      common::VisualizeField(sol_socks, vishost, visport, u_known,
-                             "Fixed (known) u values", wsize, 0, wsize, wsize,
-                             "rRjmm********A");
-   }
-   u = u_known;
-
-   // Normal derivative function.
-   ParGridFunction grad_u_n(&pfes_L2);
-   NormalGradCoeff grad_u_n_coeff(u, ls_n_coeff);
-   grad_u_n.ProjectCoefficient(grad_u_n_coeff);
-   if (visualization)
-   {
-      socketstream sol_sock;
-      common::VisualizeField(sol_sock, vishost, visport, grad_u_n,
-                             "grad_u n", 2*wsize, 0, wsize, wsize,
-                             "rRjmm********A");
-   }
-
-   // The active zones are where we extrapolate (where the PDE is solved).
-   Array<bool> active_zones(NE);
-   for (int k = 0; k < NE; k++)
-   {
-      // Extrapolation is done in zones that are CUT or OUTSIDE.
-      active_zones[k] =
-         (elem_marker[k] == ShiftedFaceMarker::INSIDE) ? false : true;
-   }
-
-   ParBilinearForm lhs_bf(&pfes_L2), rhs_bf(&pfes_L2);
-   lhs_bf.AddDomainIntegrator(new MassIntegrator);
-   const double alpha = -1.0;
-   rhs_bf.AddDomainIntegrator(new ConvectionIntegrator(ls_n_coeff, alpha));
-   auto trace_i = new NonconservativeDGTraceIntegrator(ls_n_coeff, alpha);
-   rhs_bf.AddInteriorFaceIntegrator(trace_i);
-   rhs_bf.KeepNbrBlock(true);
-
-   lhs_bf.Assemble();
-   lhs_bf.Finalize();
-   rhs_bf.Assemble(0);
-   rhs_bf.Finalize(0);
-
-   Vector rhs(pfes_L2.GetVSize());
-   rhs = 0.0;
-
-   // Compute a CFL time step.
-   double h_min = std::numeric_limits<double>::infinity();
-   for (int k = 0; k < NE; k++)
-   {
-      h_min = std::min(h_min, pmesh.GetElementSize(k));
-   }
-   MPI_Allreduce(MPI_IN_PLACE, &h_min, 1, MPI_DOUBLE, MPI_MIN,
-                 pfes_L2.GetComm());
-   h_min /= order;
-   // The propagation speed is 1.
-   double dt = 0.25 * h_min / 1.0;
-
-   // Time loop
-   double t = 0.0;
-   ODESolver *ode_solver = NULL;
-   switch (ode_solver_type)
-   {
-      case 1: ode_solver = new ForwardEulerSolver; break;
-      case 2: ode_solver = new RK2Solver(1.0); break;
-      case 3: ode_solver = new RK3SSPSolver; break;
-      default:
-      {
-         if (myid == 0)
-         { cout << "Unknown ODE solver type: " << ode_solver_type << '\n'; }
-         return 3;
-      }
-   }
-
-   Extrapolator ext(active_zones, lhs_bf, rhs_bf, rhs);
-   ode_solver->Init(ext);
-
-   bool done = false;
-   const double t_final = 0.4;
-   for (int ti = 0; !done;)
-   {
-      double dt_real = min(dt, t_final - t);
-      ode_solver->Step(grad_u_n, t, dt_real);
-      ti++;
-
-      done = (t >= t_final - 1e-8*dt);
-
-      if (done || ti % vis_steps == 0)
-      {
-         if (myid == 0)
-         {
-            cout << "time step: " << ti << ", time: " << t << endl;
-         }
-
-         if (visualization)
-         {
-            common::VisualizeField(sock_grad_u_n, vishost, visport, grad_u_n,
-                                   "Solution", 2*wsize, 570, wsize, wsize,
-                                   "rRjmm********A");
-            MPI_Barrier(pmesh.GetComm());
-         }
-      }
-   }
-
-   lhs_bf.Mult(grad_u_n, rhs);
-   done = false;
-   t = 0.0;
-   for (int ti = 0; !done;)
-   {
-      double dt_real = min(dt, t_final - t);
-      ode_solver->Step(u, t, dt_real);
-      ti++;
-
-      done = (t >= t_final - 1e-8*dt);
-
-      if (done || ti % vis_steps == 0)
-      {
-         if (myid == 0)
-         {
-            cout << "time step: " << ti << ", time: " << t << endl;
-         }
-
-         if (visualization)
-         {
-            common::VisualizeField(sock_u, vishost, visport, u,
-                                   "Solution", wsize, 570, wsize, wsize,
-                                   "rRjmm********A");
-            MPI_Barrier(pmesh.GetComm());
-         }
-      }
-   }
+   PrintNorm(myid, ux, "final norm: ");
 
    // ParaView output.
    ParaViewDataCollection dacol("ParaViewExtrapolate", &pmesh);
    dacol.SetLevelsOfDetail(order);
-   dacol.RegisterField("filtered_level_set", &level_set);
+   dacol.RegisterField("extrapolated sltn", &ux);
    dacol.SetTime(1.0);
    dacol.SetCycle(1);
    dacol.Save();
