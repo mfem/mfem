6dfc8f6547a1ec3feef8c01942c9a3e1bd42adf1
Vladimir Z Tomov <tomov2@llnl.gov>
2022-01-15 19:33:31 -0800

    Construction of a H1 Q1 smoothness indicator for grad_u.

--- miniapps/meshing/smoother.cpp
+++ miniapps/meshing/smoother.cpp
@@ -27,7 +27,7 @@ double level_set_func(const Vector &coord)
 
    if (problem == 0)
    {
-      // Smooth square
+      // Smooth circle.
       x -= 0.5; y -= 0.5;
       return 0.25 - sqrt(x*x + y*y + 1e-12);
    }
@@ -36,8 +36,50 @@ double level_set_func(const Vector &coord)
       // Vertical line.
       return (x < 0.5) ? 0.5 - x : 5.0 - 10.0 * x;
    }
+   if (problem == 2)
+   {
+      const int num_circ = 3;
+      double rad[num_circ] = {0.3, 0.15, 0.2};
+      double c[num_circ][2] = { {0.6, 0.6}, {0.3, 0.3}, {0.25, 0.75} };
+
+      const double xc = coord(0), yc = coord(1);
+
+      // circle 0
+      double r0 = (xc-c[0][0])*(xc-c[0][0]) + (yc-c[0][1])*(yc-c[0][1]);
+      r0 = (r0 > 0) ? std::sqrt(r0) : 0.0;
+      if (r0 <= 0.2) { return -1.0; }
+
+      for (int i = 0; i < num_circ; i++)
+      {
+         double r = (xc-c[i][0])*(xc-c[i][0]) + (yc-c[i][1])*(yc-c[i][1]);
+         r = (r > 0) ? std::sqrt(r) : 0.0;
+         if (r <= rad[i]) { return 1.0; }
+      }
+
+      // rectangle 1
+      if (0.7 <= xc && xc <= 0.8 && 0.1 <= yc && yc <= 0.8) { return 1.0; }
+
+      // rectangle 2
+      if (0.3 <= xc && xc <= 0.8 && 0.15 <= yc && yc <= 0.2) { return 1.0; }
+      return -1.0;
+   }
    return 1.0;
 }
+
+void MeasureJumps(const ParGridFunction &u, ParGridFunction &jumps);
+
+class GradientCompCoefficient : public Coefficient
+{
+private:
+   const ParGridFunction &u;
+   const int comp;
+
+public:
+   GradientCompCoefficient(const ParGridFunction &u_gf, int c)
+      : u(u_gf), comp(c) { }
+
+   virtual double Eval(ElementTransformation &T, const IntegrationPoint &ip);
+};
 
 class GradientDot11Coefficient : public Coefficient
 {
@@ -93,20 +135,114 @@ int main (int argc, char *argv[])
    u.ProjectCoefficient(ls_coeff);
    socketstream sock_u;
    common::VisualizeField(sock_u, vishost, visport, u, "u",
-                          0, 0, wsize, wsize, "Rjm***");
+                          0, 0, wsize, wsize, "Rjmc***");
 
    L2_FECollection fec_L2(order, dim);
    ParFiniteElementSpace pfes_L2(&pmesh, &fec_L2);
    ParGridFunction grad_u_dot_1(&pfes_L2);
    GradientDot11Coefficient grad_u_dot_1_coeff(u);
    grad_u_dot_1.ProjectCoefficient(grad_u_dot_1_coeff);
-
    socketstream sock_grad;
    common::VisualizeField(sock_grad, vishost, visport, grad_u_dot_1,
-                          "grad_u . 1", 0, wsize, wsize, wsize, "Rjm***");
+                          "grad_u . 1", 0, wsize, wsize, wsize, "Rjmc***");
+
+   H1_FECollection fec_lin_H1(1, dim);
+   ParFiniteElementSpace pfes_lin_H1(&pmesh, &fec_lin_H1);
+   ParGridFunction jumps(&pfes_lin_H1);
+   MeasureJumps(grad_u_dot_1, jumps);
+   socketstream sock_j;
+   common::VisualizeField(sock_j, vishost, visport, jumps, "smooth indicator",
+                          3*wsize, wsize, wsize, wsize, "Rjmc***");
+
+   ConstantCoefficient zero(0.0);
+   double norm = jumps.ComputeL1Error(zero);
+   if (myid == 0)
+   {
+      std::cout << setprecision(12) << "L1 norm = " << norm << std::endl;
+   }
 
    MPI_Finalize();
    return 0;
+}
+
+void MeasureJumps(const ParGridFunction &u, ParGridFunction &jumps)
+{
+   ParFiniteElementSpace &pfes_L2 = *u.ParFESpace();
+   const int dim   = pfes_L2.GetMesh()->Dimension();
+   const int order = pfes_L2.FEColl()->GetOrder();
+
+   // Normalize to [0, 1].
+   ParGridFunction u_n(u);
+   double u_min = u_n.Min();
+   MPI_Allreduce(MPI_IN_PLACE, &u_min, 1, MPI_DOUBLE, MPI_MIN,
+                 u_n.ParFESpace()->GetComm());
+   u_n += fabs(u_min);
+   double u_max = u_n.Max();
+   MPI_Allreduce(MPI_IN_PLACE, &u_max, 1, MPI_DOUBLE, MPI_MAX,
+                 u_n.ParFESpace()->GetComm());
+   u_n /= u_max;
+
+   socketstream sock_un;
+   common::VisualizeField(sock_un, vishost, visport, u_n, "u normalized",
+                          wsize, wsize, wsize, wsize, "Rjmc**");
+
+   // Form min/max at each CG dof, considering element overlaps.
+   H1_FECollection fec_H1(order, dim);
+   ParFiniteElementSpace pfes_H1(pfes_L2.GetParMesh(), &fec_H1);
+   ParGridFunction u_min_H1(&pfes_H1), u_max_H1(&pfes_H1);
+   u_min_H1 =   std::numeric_limits<double>::infinity();
+   u_max_H1 = - std::numeric_limits<double>::infinity();
+   const int NE = pfes_H1.GetNE();
+   Array<int> dofsCG;
+   const TensorBasisElement *fe_tensor =
+      dynamic_cast<const TensorBasisElement *>(pfes_H1.GetFE(0));
+   MFEM_VERIFY(fe_tensor, "TODO - implement for triangles");
+   const Array<int> &dof_map = fe_tensor->GetDofMap();
+   const int ndofs = dof_map.Size();
+   for (int k = 0; k < NE; k++)
+   {
+      pfes_H1.GetElementDofs(k, dofsCG);
+      for (int i = 0; i < ndofs; i++)
+      {
+         u_min_H1(dofsCG[dof_map[i]]) = fmin(u_min_H1(dofsCG[dof_map[i]]),
+                                             u_n(k*ndofs + i));
+         u_max_H1(dofsCG[dof_map[i]]) = fmax(u_max_H1(dofsCG[dof_map[i]]),
+                                             u_n(k*ndofs + i));
+      }
+   }
+   // MPI neighbors.
+   GroupCommunicator &gcomm = pfes_H1.GroupComm();
+   Array<double> minvals(u_min_H1.GetData(), u_min_H1.Size()),
+                 maxvals(u_max_H1.GetData(), u_max_H1.Size());
+   gcomm.Reduce<double>(minvals, GroupCommunicator::Min);
+   gcomm.Bcast(minvals);
+   gcomm.Reduce<double>(maxvals, GroupCommunicator::Max);
+   gcomm.Bcast(maxvals);
+
+   // Compute jumps (and reuse the min H1 function).
+   ParGridFunction &u_jump_H1 = u_min_H1;
+   for (int i = 0; i < u_jump_H1.Size(); i++)
+   {
+      u_jump_H1(i) = fabs(u_max_H1(i) - u_min_H1(i));
+   }
+   socketstream sock_j;
+   common::VisualizeField(sock_j, vishost, visport, u_jump_H1, "u jumps HO",
+                          2*wsize, wsize, wsize, wsize, "Rjmc**");
+
+   // Project the jumps to Q1.
+   jumps.ProjectGridFunction(u_jump_H1);
+   for (int i = 0; i < jumps.Size(); i++) { jumps(i) = fmax(jumps(i), 0.0); }
+   Vector tv(jumps.ParFESpace()->GetTrueVSize());
+   jumps.ParallelAverage(tv);
+   jumps.Distribute(tv);
+}
+
+double GradientCompCoefficient::Eval(ElementTransformation &T,
+                                     const IntegrationPoint &ip)
+{
+   Vector grad_u(T.GetDimension());
+   u.GetGradient(T, grad_u);
+   return grad_u(comp);
 }
 
 double GradientDot11Coefficient::Eval(ElementTransformation &T,
