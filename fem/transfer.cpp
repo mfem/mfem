098f83a942d46d179f0b1a7b9c5d8b1af393f47d
Socratis Petrides <petrides1@llnl.gov>
2022-01-26 10:53:33 -0800

    correcting the R/Rhp choice in TrueTransfer -> fixing the failed unit tests

--- fem/transfer.cpp
+++ fem/transfer.cpp
@@ -1439,7 +1439,8 @@ TrueTransferOperator::TrueTransferOperator(const FiniteElementSpace& lFESpace_,
    localTransferOperator = new TransferOperator(lFESpace_, hFESpace_);
 
    P = lFESpace.GetProlongationMatrix();
-   R = hFESpace.GetHpRestrictionMatrix();
+   R = hFESpace.IsVariableOrder() ? hFESpace.GetHpRestrictionMatrix() :
+       hFESpace.GetRestrictionMatrix();
 
    // P and R can be both null
    // P can be null and R not null
