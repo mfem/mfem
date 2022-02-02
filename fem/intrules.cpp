ec8f2baeecc6776649acf0f543ad9769ad170fae
Stowell, Mark L <stowell1@llnl.gov>
2022-02-01 20:14:24 -0800

    Fixing the higher order integration rules for pyramids

--- fem/intrules.cpp
+++ fem/intrules.cpp
@@ -1675,7 +1675,7 @@ IntegrationRule *IntegrationRules::PyramidIntegrationRule(int Order)
       ipp.x = ipc.x * (1.0 - ipc.z);
       ipp.y = ipc.y * (1.0 - ipc.z);
       ipp.z = ipc.z;
-      ipp.weight = ipc.weight / 3.0;
+      ipp.weight = 4.0 * ipc.weight * pow(1.0 - ipc.z, 2);
    }
    return PyramidIntRules[Order];
 }
