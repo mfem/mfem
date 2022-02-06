6d2b48da1eb4a2a861d27ef2c85e70486d1749be
Veselin Dobrev <dobrev@llnl.gov>
2022-02-06 15:04:17 -0800

    Fix some issues with the tests.
    Sync some tests between GNU make and CMake.
    
    Add tests for the (p)lor_solvers miniapp.

--- miniapps/shifted/diffusion.cpp
+++ miniapps/shifted/diffusion.cpp
@@ -86,7 +86,7 @@ int main(int argc, char *argv[])
 #ifdef HYPRE_USING_GPU
    cout << "\nAs of mfem-4.3 and hypre-2.22.0 (July 2021) this miniapp\n"
         << "is NOT supported with the GPU version of hypre.\n\n";
-   return 255;
+   return 242;
 #endif
 
    // Initialize MPI.
