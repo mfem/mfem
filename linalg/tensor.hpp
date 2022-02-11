e62c4b06ac85a0958e0c70c08d25ad45e9dd6279
Julian Andrej <andrej1@llnl.gov>
2022-02-11 10:24:42 -0800

    copyright notice

--- linalg/tensor.hpp
+++ linalg/tensor.hpp
@@ -1,13 +1,18 @@
-// Copyright (c) 2019-2022, Lawrence Livermore National Security, LLC and
-// other Serac Project Developers. See the top-level LICENSE file for
-// details.
+// Copyright (c) 2010-2022, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
 //
-// SPDX-License-Identifier: (BSD-3-Clause)
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
 
 /**
  * @file tensor.hpp
  *
- * @brief Implementation of the tensor class used by Functional
+ * @brief Implementation of the tensor class
  */
 
 #pragma once
