d82def8b0dac603c905975c7da0bbcb1f3673d76
Veselin Dobrev <dobrev@llnl.gov>
2022-02-11 21:23:46 -0800

    Fix various warnings

--- fem/bilininteg_hcurl.cpp
+++ fem/bilininteg_hcurl.cpp
@@ -1708,9 +1708,8 @@ static void SmemPACurlCurlApply3D(const int D1D,
 
    const int s = symmetric ? 6 : 9;
 
-   MFEM_FORALL_3D(e, NE, Q1D, Q1D, Q1D,
+   auto device_kernel = [=] MFEM_DEVICE (int e)
    {
-#ifdef MFEM_DEVICE_COMPILE
       constexpr int VDIM = 3;
 
       MFEM_SHARED double sBo[MAX_D1D][MAX_Q1D];
@@ -1988,10 +1987,14 @@ static void SmemPACurlCurlApply3D(const int D1D,
             }
          }
       } // qz
-#else
-   MFEM_ABORT_KERNEL("This kernel should only be used on GPU.");
-#endif
-   }); // end of element loop
+   }; // end of element loop
+
+   auto host_kernel = [&] MFEM_LAMBDA (int)
+   {
+      MFEM_ABORT_KERNEL("This kernel should only be used on GPU.");
+   };
+
+   ForallWrap<3>(true, NE, device_kernel, host_kernel, Q1D, Q1D, Q1D);
 }
 
 void CurlCurlIntegrator::AddMultPA(const Vector &x, Vector &y) const
@@ -3322,9 +3325,8 @@ static void SmemPAHcurlL2Apply3D(const int D1D,
    auto X = Reshape(x.Read(), 3*(D1D-1)*D1D*D1D, NE);
    auto Y = Reshape(y.ReadWrite(), 3*(D1D-1)*D1D*D1D, NE);
 
-   MFEM_FORALL_3D(e, NE, Q1D, Q1D, Q1D,
+   auto device_kernel = [=] MFEM_DEVICE (int e)
    {
-#ifdef MFEM_DEVICE_COMPILE
       constexpr int VDIM = 3;
       constexpr int maxCoeffDim = 3;
 
@@ -3584,10 +3586,14 @@ static void SmemPAHcurlL2Apply3D(const int D1D,
             }
          }
       } // qz
-#else
-   MFEM_ABORT_KERNEL("This kernel should only be used on GPU.");
-#endif
-   }); // end of element loop
+   }; // end of element loop
+
+   auto host_kernel = [&] MFEM_LAMBDA (int)
+   {
+      MFEM_ABORT_KERNEL("This kernel should only be used on GPU.");
+   };
+
+   ForallWrap<3>(true, NE, device_kernel, host_kernel, Q1D, Q1D, Q1D);
 }
 
 // Apply to x corresponding to DOF's in H(curl) (trial), whose curl is
@@ -4442,9 +4448,8 @@ static void SmemPAHcurlL2Apply3DTranspose(const int D1D,
    auto X = Reshape(x.Read(), 3*(D1D-1)*D1D*D1D, NE);
    auto Y = Reshape(y.ReadWrite(), 3*(D1D-1)*D1D*D1D, NE);
 
-   MFEM_FORALL_3D(e, NE, Q1D, Q1D, Q1D,
+   auto device_kernel = [=] MFEM_DEVICE (int e)
    {
-#ifdef MFEM_DEVICE_COMPILE
       constexpr int VDIM = 3;
       constexpr int maxCoeffDim = 3;
 
@@ -4641,10 +4646,14 @@ static void SmemPAHcurlL2Apply3DTranspose(const int D1D,
             }
          }
       } // qz
-#else
-   MFEM_ABORT_KERNEL("This kernel should only be used on GPU.");
-#endif
-   }); // end of element loop
+   }; // end of element loop
+
+   auto host_kernel = [&] MFEM_LAMBDA (int)
+   {
+      MFEM_ABORT_KERNEL("This kernel should only be used on GPU.");
+   };
+
+   ForallWrap<3>(true, NE, device_kernel, host_kernel, Q1D, Q1D, Q1D);
 }
 
 void MixedVectorWeakCurlIntegrator::AddMultPA(const Vector &x, Vector &y) const
