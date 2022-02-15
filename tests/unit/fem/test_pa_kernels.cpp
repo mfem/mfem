70fb6d1c664a54bd539a9fa1b14014d2ad44e8cc
Yohann Dudouit <dudouit1@llnl.gov>
2022-02-15 13:26:04 -0800

    Remove most Convection DG PA tests.

--- tests/unit/fem/test_pa_kernels.cpp
+++ tests/unit/fem/test_pa_kernels.cpp
@@ -435,32 +435,38 @@ TEST_CASE("PA Convection", "[PartialAssembly][MFEMData]")
    // - 2: DG discontinuous coeff,
    // - 3: DG Bernstein discontinuous coeff.
    auto prob = GENERATE(0, 1, 2, 3);
-   auto order_2d = GENERATE(2, 3, 4);
+   auto order_2d = GENERATE(2);
    auto order_3d = GENERATE(2);
    // refinement > 0 => Non-conforming mesh
-   auto refinement_2d = GENERATE(0,1,2);
+   auto refinement_2d = GENERATE(0,1);
    auto refinement_3d = GENERATE(0,1);
 
    SECTION("2D")
    {
       test_pa_convection("../../data/periodic-square.mesh", order_2d, prob,
                          refinement_2d);
-      test_pa_convection("../../data/periodic-hexagon.mesh", order_2d, prob,
-                         refinement_2d);
-      test_pa_convection("../../data/star-q3.mesh", order_2d, prob,
-                         refinement_2d);
-      test_pa_convection(mfem_data_dir+"/gmsh/v22/unstructured_quad.v22.msh",
-                         order_2d, prob, refinement_2d);
+      if (launch_all_non_regression_tests)
+      {
+         test_pa_convection("../../data/periodic-hexagon.mesh", order_2d, prob,
+                            refinement_2d);
+         test_pa_convection("../../data/star-q3.mesh", order_2d, prob,
+                            refinement_2d);
+         test_pa_convection(mfem_data_dir+"/gmsh/v22/unstructured_quad.v22.msh",
+                            order_2d, prob, refinement_2d);
+      }
    }
 
    SECTION("3D")
    {
       test_pa_convection("../../data/periodic-cube.mesh", order_3d, prob,
                          refinement_3d);
-      test_pa_convection("../../data/fichera-q3.mesh", order_3d, prob,
-                         refinement_3d);
-      test_pa_convection(mfem_data_dir+"/gmsh/v22/unstructured_hex.v22.msh",
-                         order_3d, prob, refinement_3d);
+      if (launch_all_non_regression_tests)
+      {
+         test_pa_convection("../../data/fichera-q3.mesh", order_3d, prob,
+                            refinement_3d);
+         test_pa_convection(mfem_data_dir+"/gmsh/v22/unstructured_hex.v22.msh",
+                            order_3d, prob, refinement_3d);
+      }
    }
 
 } // test case
