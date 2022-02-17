132f951d39c86d8851e8de051a6c24ad03f66249
Socratis Petrides <petrides1@llnl.gov>
2022-02-16 19:42:50 -0800

    Rational approximation works

--- examples/fpde_aaa.cpp
+++ examples/fpde_aaa.cpp
@@ -24,7 +24,6 @@
 using namespace std;
 using namespace mfem;
 
-
 void RationalApproximation_AAA(const Vector &val,
                                const Vector &pt,           // inputs
                                Array<double> &z, Array<double> &f, Vector &w, // outputs
@@ -33,7 +32,25 @@ void RationalApproximation_AAA(const Vector &val,
 
 int main(int argc, char *argv[])
 {
+   int size = 100;
+   Vector x(size);
+   Vector val(size);
+   for (int i = 0; i<size; i++)
+   {
+      x(i) = (double)i+0.01;
+      val(i) = 1./pow(x(i),0.33);
+   }
 
+   Array<double> z, f;
+   Vector w;
+   RationalApproximation_AAA(val,x,z,f,w,1e-7,10);
+
+   mfem::out << "x = " ; x.Print(cout,size);
+   mfem::out << "val = " ; val.Print(cout,size);
+   mfem::out << endl;
+   mfem::out << "z = " ; z.Print(cout,z.Size());
+   mfem::out << "f = " ; f.Print(cout,f.Size());
+   mfem::out << "w = " ; w.Print(cout,w.Size());
 
 }
 
@@ -42,63 +59,174 @@ void RationalApproximation_AAA(const Vector &val, const Vector &pt,
                                Array<double> &z, Array<double> &f, Vector &w,
                                double tol, int max_order)
 {
-   MFEM_VERIFY(pt.Size() == val.Size(), "size mismatch");
 
+   // number of sample points
+   int size = val.Size();
+   MFEM_VERIFY(pt.Size() == size, "size mismatch");
+
+
+   // Initializations
+   Array<int> J(size);
+   for (int i = 0; i < size; i++) { J[i] = i; }
    z.SetSize(0);
    f.SetSize(0);
-   Vector f_vec;
-
-   int size = val.Size();
-   SparseMatrix SF(val);
    DenseMatrix C;
+   DenseMatrix *Cl = nullptr;
+   DenseMatrix *Cr = nullptr;
+   DenseMatrix *A = nullptr;
+   DenseMatrix *Am = nullptr;
+
+   // auxiliary arrays and vectors
+   Vector f_vec;
    Array<double> C_i;
 
-   double R = val.Sum()/size;
-   Array<int> J(size);
-   for (int i = 0; i < size; i++)
+   // mean of the value vector
+   Vector R(val.Size());
+   double mean_val = val.Sum()/size;
+   for (int i = 0; i<R.Size(); i++)
    {
-      J[i] = i;
+      R(i) = mean_val;
    }
 
-
-
-   for (int i = 0; i < max_order; i++)
+   // mfem::out << "Val mean = " << mean_val << endl;
+   // Array<double> errors;
+   for (int k = 0; k < max_order; k++)
    {
-      // Vector tmp(val);
-      // tmp -= R;
-      int index = 0;
+      // select next support point
+
+      int idx = 0;
       double tmp_max = 0;
       for (int j = 0; j < size; j++)
       {
-         double tmp = abs(val(j)-R);
+         double tmp = abs(val(j)-R(j));
          if (tmp > tmp_max)
          {
             tmp_max = tmp;
-            index = j;
+            idx = j;
          }
       }
-      z.Append(pt(index));
-      f.Append(val(index));
-      J.DeleteFirst(index);
+      // mfem::out << "tmp_max = " << tmp_max << endl;
+      // mfem::out << "idx = " << idx << endl;
 
-      Array<double> C_tmp(size-1);
-      int cnt = 0;
+      // Append support points and data values
+      z.Append(pt(idx));
+      f.Append(val(idx));
+
+      // mfem::out << "z = "; z.Print();
+      // mfem::out << "f = "; f.Print();
+
+      // Update index vector
+      J.DeleteFirst(idx);
+      // mfem::out << "J = "; J.Print(cout, J.Size());
+
+      // next column in Cauchy matrix
+
+      Array<double> C_tmp(size);
+      // we might want to remove the inf
       for (int j = 0; j < size; j++)
       {
-         if (j != index)
-         {
-            C_tmp[cnt] = 1.0/(pt(j)-pt(index));
-         }
-         cnt++;
+         C_tmp[j] = 1.0/(pt(j)-pt(idx));
       }
       C_i.Append(C_tmp);
+      int h_C = C_tmp.Size();
+      int w_C = k+1;
+      C.UseExternalData(C_i.GetData(),h_C,w_C);
+      // mfem::out << "C = " << endl;
+      // C.PrintMatlab();
+
+      // This will need some cleanup
+      // temporary copies to perform the scalings
+      Cl = new DenseMatrix(C);
+      Cr = new DenseMatrix(C);
 
       f_vec.SetDataAndSize(f.GetData(),f.Size());
+      Cl->LeftScaling(val);
+      Cr->RightScaling(f_vec);
+
+      A = new DenseMatrix(C.Height(), C.Width());
+      Add(*Cl,*Cr,-1.0,*A);
+
+      // remove select only the J indexes of the columns of A
+
+      // mfem::out << "A = " << endl;
+      // A->PrintMatlab();
+
+      int h_Am = J.Size();
+      int w_Am = A->Width();
+      Am = new DenseMatrix(h_Am,w_Am);
+      for (int i = 0; i<h_Am; i++)
+      {
+         int ii = J[i];
+         for (int j = 0; j<w_Am; j++)
+         {
+            (*Am)(i,j) = (*A)(ii,j);
+         }
+      }
 
 
+      // mfem::out << "Am = " << endl;
+      // Am->PrintMatlab();
 
+      // SVD to get the eigenvectors of Am
+
+      DenseMatrix AtA(Am->Width());
+      MultAtB(*Am,*Am,AtA);
+
+      DenseMatrixEigensystem eig(AtA);
+      eig.Eval();
+      DenseMatrix & v = eig.Eigenvectors();
+      Vector & svalues = eig.Eigenvalues();
+
+      for (int i = 0; i<svalues.Size(); i++)
+      {
+         svalues(i) = std::sqrt(svalues(i));
+      }
+
+      // The columns of v (right-singular vectors) of A are eigenvectors of AtA
+      // Singular values of A are the square roots of eigenvalues of AtA
+      // mfem::out << "svalues = " << endl;
+      // svalues.Print();
+      // mfem::out << "singular vectors = " << endl;
+      // v.PrintMatlab();
+
+      v.GetColumn(0,w);
+
+      // mfem::out << "w = " ; w.Print();
+
+      // N = C*(w.*f); D = C*w; % numerator and denominator
+      Vector aux(w);
+      aux *= f_vec;
+      Vector N(C.Height()); // Numerator
+      C.Mult(aux,N);
+      Vector D(C.Height()); // Denominator
+      C.Mult(w,D);
+
+      // mfem::out << "Numerator = "; N.Print();
+      // mfem::out << "Denominator = "; D.Print();
+
+      R = val;
+      for (int i = 0; i<J.Size(); i++)
+      {
+         int ii = J[i];
+         R(ii) = N(ii)/D(ii);
+      }
+
+      // mfem::out << "R = "; R.Print();
+
+      Vector verr(val);
+      verr-=R;
+      double err = verr.Normlinf();
+      // errors.Append(err);
+
+
+      delete Cl;
+      delete Cr;
+      delete A;
+      delete Am;
+
+      if (err <= tol*val.Normlinf())
+      {
+         break;
+      }
    }
-
-
-
 }
