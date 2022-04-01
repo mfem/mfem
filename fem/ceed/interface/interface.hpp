a07489832b6f1019b7decb381aa137d1f3281f41
Yohann Dudouit <dudouit1@llnl.gov>
2022-03-31 17:56:49 -0700

    Add documentation to interface.hpp.

--- fem/ceed/interface/interface.hpp
+++ fem/ceed/interface/interface.hpp
@@ -12,14 +12,20 @@
 #ifndef MFEM_LIBCEED_INTERFACE
 #define MFEM_LIBCEED_INTERFACE
 
-#include "ceed.hpp"
-#include "basis.hpp"
-#include "restriction.hpp"
+// Object wrapping a CeedOperator in a mfem::Operator.
 #include "operator.hpp"
-#include "integrator.hpp"
-#include "mixed_integrator.hpp"
+// Functions to initialize CeedBasis objects.
+#include "basis.hpp"
+// Functions to initialize CeedRestriction objects.
+#include "restriction.hpp"
+// Functions to initialize coefficients.
 #include "coefficient.hpp"
+// PA or MF Operator using libCEED.
+#include "integrator.hpp"
+// Utility functions
 #include "util.hpp"
+// Wrapper to include <ceed.h>
+#include "ceed.hpp"
 
 namespace mfem
 {
