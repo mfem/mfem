69c1822084bf4e0e087aae8904ceb10ab5aa317d
Will Pazner <will.e.p@gmail.com>
2022-04-05 15:14:57 -0700

    Add FaceQuadratureSpace and FaceQuadratureFunction
    Also add base classes for QuadratureSpace and QuadratureFunction

--- fem/qspace.hpp
+++ fem/qspace.hpp
@@ -18,36 +18,22 @@
 namespace mfem
 {
 
-/// Class representing the storage layout of a QuadratureFunction.
-/** Multiple QuadratureFunction%s can share the same QuadratureSpace. */
-class QuadratureSpace
+class QuadratureSpaceBase
 {
 protected:
-   friend class QuadratureFunction; // Uses the element_offsets.
+   friend class QuadratureFunctionBase; // Uses the offsets.
 
-   Mesh *mesh;
+   Mesh &mesh;
    int order;
    int size;
+   Array<int> offsets;
 
    const IntegrationRule *int_rule[Geometry::NumGeom];
-   int *element_offsets; // scalar offsets; size = number of elements + 1
 
-   // protected functions
-
-   // Assuming mesh and order are set, construct the members: int_rule,
-   // element_offsets, and size.
-   void Construct();
+   QuadratureSpaceBase(Mesh &mesh_, int order_ = 0)
+      : mesh(mesh_), order(order_) { }
 
 public:
-   /// Create a QuadratureSpace based on the global rules from #IntRules.
-   QuadratureSpace(Mesh *mesh_, int order_)
-      : mesh(mesh_), order(order_) { Construct(); }
-
-   /// Read a QuadratureSpace from the stream @a in.
-   QuadratureSpace(Mesh *mesh_, std::istream &in);
-
-   virtual ~QuadratureSpace() { delete [] element_offsets; }
-
    /// Return the total number of quadrature points.
    int GetSize() const { return size; }
 
@@ -55,17 +41,56 @@ public:
    int GetOrder() const { return order; }
 
    /// Returns the mesh
-   inline Mesh *GetMesh() const { return mesh; }
+   inline Mesh *GetMesh() const { return &mesh; }
+
+   virtual ~QuadratureSpaceBase() { }
+};
+
+/// Class representing the storage layout of a QuadratureFunction.
+/** Multiple QuadratureFunction%s can share the same QuadratureSpace. */
+class QuadratureSpace : public QuadratureSpaceBase
+{
+protected:
+   // Assuming mesh and order are set, construct the members: int_rule,
+   // element_offsets, and size.
+   void Construct();
+public:
+   /// Create a QuadratureSpace based on the global rules from #IntRules.
+   QuadratureSpace(Mesh *mesh_, int order_)
+      : QuadratureSpaceBase(*mesh_, order_) { Construct(); }
+
+   /// Read a QuadratureSpace from the stream @a in.
+   QuadratureSpace(Mesh *mesh_, std::istream &in);
 
    /// Returns number of elements in the mesh.
-   inline int GetNE() const { return mesh->GetNE(); }
+   inline int GetNE() const { return mesh.GetNE(); }
 
    /// Get the IntegrationRule associated with mesh element @a idx.
    const IntegrationRule &GetElementIntRule(int idx) const
-   { return *int_rule[mesh->GetElementBaseGeometry(idx)]; }
+   { return *int_rule[mesh.GetElementBaseGeometry(idx)]; }
 
    /// Write the QuadratureSpace to the stream @a out.
    void Save(std::ostream &out) const;
+};
+
+/// Class representing the storage layout of a FaceQuadratureFunction.
+class FaceQuadratureSpace : public QuadratureSpaceBase
+{
+   // Assuming mesh and order are set, construct the members: int_rule,
+   // element_offsets, and size.
+   void Construct();
+
+public:
+   /// Create a FaceQuadratureSpace based on the global rules from #IntRules.
+   FaceQuadratureSpace(Mesh &mesh_, int order_)
+      : QuadratureSpaceBase(mesh_, order_) { Construct(); }
+
+   /// Returns number of faces in the mesh.
+   inline int GetNumFaces() const { return mesh.GetNumFaces(); }
+
+   /// Get the IntegrationRule associated with mesh element @a idx.
+   const IntegrationRule &GetFaceIntRule(int idx) const
+   { return *int_rule[mesh.GetFaceGeometry(idx)]; }
 };
 
 }
