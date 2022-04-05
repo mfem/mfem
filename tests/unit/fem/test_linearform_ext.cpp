d127d0d0d514ac444ccdb0a3f2f5c8f05425c2fa
camierjs <camier1@llnl.gov>
2022-04-05 15:34:15 -0700

    Cleanup tests/unit/fem/test_linearform_ext

--- tests/unit/fem/test_linearform_ext.cpp
+++ tests/unit/fem/test_linearform_ext.cpp
@@ -17,13 +17,8 @@ using namespace mfem;
 
 struct LinearFormExtTest
 {
-   enum
-   {
-      DomainLF,
-      DomainLFGrad,
-      VectorDomainLF,
-      VectorDomainLFGrad
-   };
+   enum { DomainLF, DomainLFGrad, VectorDomainLF, VectorDomainLFGrad };
+   using vector_fct_t = void(const Vector&, Vector&);
    const double abs_tol = 1e-14, rel_tol = 1e-14;
    const char *mesh_filename;
    Mesh mesh;
@@ -37,11 +32,10 @@ struct LinearFormExtTest
    Vector one_vec, dim_vec, vdim_vec, vdim_dim_vec;
    ConstantCoefficient cst_coeff;
    VectorConstantCoefficient dim_cst_coeff, vdim_cst_coeff, vdim_dim_cst_coeff;
-   std::function<void(const Vector&, Vector&)>
-   vdim_vector_function = [&](const Vector&, Vector &y)
-   { y.SetSize(vdim); y.Randomize(SEED); };
-   std::function<void(const Vector&, Vector&)> vector_fct;
-   VectorFunctionCoefficient vdim_function_coeff;
+   std::function<vector_fct_t> vdim_vec_function =
+   [&](const Vector&, Vector &y) { y.SetSize(vdim); y.Randomize(SEED); };
+   std::function<vector_fct_t> vector_fct;
+   VectorFunctionCoefficient vdim_fct_coeff;
    LinearForm lf_dev, lf_std;
    LinearFormIntegrator *lfi_dev, *lfi_std;
 
@@ -49,35 +43,18 @@ struct LinearFormExtTest
                      int vdim, int ordering, int gll, int problem, int p):
       mesh_filename(mesh_filename),
       mesh(Mesh::LoadFromFile(mesh_filename)),
-      dim(mesh.Dimension()),
-      vdim(vdim),
-      ordering(ordering),
-      gll(gll),
-      problem(problem),
-      p(p),
-      q(2*p + (gll?-1:3)),
-      fec(p, dim),
-      vfes(&mesh, &fec, vdim, ordering),
-      mfes(&mesh, &fec, dim),
+      dim(mesh.Dimension()), vdim(vdim), ordering(ordering), gll(gll),
+      problem(problem), p(p), q(2*p + (gll?-1:3)), fec(p, dim),
+      vfes(&mesh, &fec, vdim, ordering), mfes(&mesh, &fec, dim),
       geom_type(vfes.GetFE(0)->GetGeomType()),
       IntRulesGLL(0, Quadrature1D::GaussLobatto),
-      irGLL(&IntRulesGLL.Get(geom_type, q)),
-      ir(&IntRules.Get(geom_type, q)),
-      elem_marker(),
-      one_vec(1),
-      dim_vec(dim),
-      vdim_vec(vdim),
-      vdim_dim_vec(vdim*dim),
-      cst_coeff(M_PI),
-      dim_cst_coeff((dim_vec.Randomize(SEED), dim_vec)),
+      irGLL(&IntRulesGLL.Get(geom_type, q)), ir(&IntRules.Get(geom_type, q)),
+      elem_marker(), one_vec(1), dim_vec(dim), vdim_vec(vdim), vdim_dim_vec(vdim*dim),
+      cst_coeff(M_PI), dim_cst_coeff((dim_vec.Randomize(SEED), dim_vec)),
       vdim_cst_coeff((vdim_vec.Randomize(SEED), vdim_vec)),
       vdim_dim_cst_coeff((vdim_dim_vec.Randomize(SEED), vdim_dim_vec)),
-      vector_fct(vdim_vector_function),
-      vdim_function_coeff(vdim, vector_fct),
-      lf_dev(&vfes),
-      lf_std(&vfes),
-      lfi_dev(nullptr),
-      lfi_std(nullptr)
+      vector_fct(vdim_vec_function), vdim_fct_coeff(vdim, vector_fct),
+      lf_dev(&vfes), lf_std(&vfes), lfi_dev(nullptr), lfi_std(nullptr)
    {
       REQUIRE(mesh.attributes.Size() == 1);
       for (int e = 0; e < mesh.GetNE(); e++) { mesh.SetAttribute(e, e%2?1:2); }
@@ -99,8 +76,8 @@ struct LinearFormExtTest
       }
       else if (problem == VectorDomainLF)
       {
-         lfi_dev = new VectorDomainLFIntegrator(vdim_function_coeff);
-         lfi_std = new VectorDomainLFIntegrator(vdim_function_coeff);
+         lfi_dev = new VectorDomainLFIntegrator(vdim_fct_coeff);
+         lfi_std = new VectorDomainLFIntegrator(vdim_fct_coeff);
       }
       else if (problem == VectorDomainLFGrad)
       {
@@ -152,8 +129,8 @@ TEST_CASE("Linear Form Extension", "[LinearformExt], [CUDA]")
    const auto mesh =
       all ? GENERATE("../../data/star.mesh", "../../data/star-q3.mesh",
                      "../../data/fichera.mesh", "../../data/fichera-q3.mesh") :
-      GENERATE("../../data/star-q3.mesh", "../../data/fichera-q3.mesh") ;
-   const auto p = all ? GENERATE(1,2,3,4,5,6/*,7*/) : GENERATE(1,3,6);
+      GENERATE("../../data/star-q3.mesh", "../../data/fichera-q3.mesh");
+   const auto p = all ? GENERATE(1,2,3,4,5,6) : GENERATE(1,3);
    const auto gll = GENERATE(0,1);
 
    SECTION("Scalar")
