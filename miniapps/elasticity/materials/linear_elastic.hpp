c9f5103796061785bf0d32d8d4f63cf86619477b
Julian Andrej <andrej1@llnl.gov>
2022-04-05 15:32:37 -0700

    correct includes

--- miniapps/elasticity/materials/linear_elastic.hpp
+++ miniapps/elasticity/materials/linear_elastic.hpp
@@ -33,7 +33,7 @@ template <int dim> struct LinearElasticMaterial
    tensor<double, dim, dim>
    MFEM_HOST_DEVICE stress(const tensor<double, dim, dim> &dudx) const
    {
-      constexpr auto I = IsotropicIdentity<dim>();
+      constexpr auto I = mfem::internal::IsotropicIdentity<dim>();
       auto epsilon = sym(dudx);
       return lambda * tr(epsilon) * I + 2.0 * mu * epsilon;
    }
@@ -62,7 +62,8 @@ template <int dim> struct LinearElasticMaterial
    tensor<double, dim, dim, dim, dim>
    MFEM_HOST_DEVICE gradient(tensor<double, dim, dim> /* dudx */) const
    {
-      return make_tensor<dim, dim, dim, dim>([&](int i, int j, int k, int l)
+      return mfem::internal::make_tensor<dim, dim, dim, dim>([&](int i, int j, int k,
+                                                                 int l)
       {
          return lambda * (i == j) * (k == l) +
                 mu * ((i == l) * (j == k) + (i == k) * (j == l));
