891890fe18d5eb630c7b9f8d294af63b4eddcca0
Socratis Petrides <petrides1@llnl.gov>
2022-04-17 19:07:49 -0700

    fixing precomputed poles and zeros values

--- examples/ex33.cpp
+++ examples/ex33.cpp
@@ -4,7 +4,7 @@
 //
 // Sample runs:  ex33 -m ../data/square-disc.mesh -alpha 0.33 -o 2
 //               ex33 -m ../data/star.mesh -alpha 0.99 -o 3
-//               ex33 -m ../data/inline-quad.mesh -alpha 0.2 -o 3
+//               ex33 -m ../data/inline-quad.mesh -alpha 0.5 -o 3
 //               ex33 -m ../data/disc-nurbs.mesh -alpha 0.33 -o 3
 //               ex33 -m ../data/l-shape.mesh -alpha 0.33 -o 3 -r 4
 //
