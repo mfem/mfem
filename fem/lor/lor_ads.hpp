2b9e2403ea7bf6abf674f045b5242d5087fbea34
Will Pazner <will.e.p@gmail.com>
2022-04-25 16:57:25 -0700

    Batched LOR: Doxygen for ADS

--- fem/lor/lor_ads.hpp
+++ fem/lor/lor_ads.hpp
@@ -30,21 +30,39 @@ namespace mfem
 class BatchedLOR_ADS
 {
 protected:
-   ParFiniteElementSpace &face_fes;
-   static constexpr int dim = 3;
-   const int order;
-   ND_FECollection edge_fec;
-   ParFiniteElementSpace edge_fes;
-   BatchedLOR_AMS ams;
-   HypreParMatrix *C;
+   ParFiniteElementSpace &face_fes; ///< The RT space.
+   static constexpr int dim = 3; ///< Spatial dimension, always 3.
+   const int order; ///< Polynomial degree.
+   ND_FECollection edge_fec; ///< The associated Nedelec collection.
+   ParFiniteElementSpace edge_fes; ///< The associated Nedelec space.
+   BatchedLOR_AMS ams; ///< The associated AMS object.
+   HypreParMatrix *C; ///< The discrete curl matrix.
 
+   /// Form the local elementwise discrete curl matrix.
    void Form3DFaceToEdge(Array<int> &face2edge);
 public:
+   /// @brief Construct the BatchedLOR_AMS object associated with the 3D RT
+   /// space @a pfes_ho_.
+   ///
+   /// The vector @a X_vert represents the LOR mesh coordinates in E-vector
+   /// format, see BatchedLORAssembly::GetLORVertexCoordinates.
    BatchedLOR_ADS(ParFiniteElementSpace &pfes_ho_,
                   const Vector &X_vert);
+   /// @brief Steal ownership of the discrete curl matrix.
+   ///
+   /// The caller assumes ownership and must delete the object. Subsequent calls
+   /// will return nullptr.
    HypreParMatrix *StealCurlMatrix();
+   /// @brief Return the discrete curl matrix.
+   ///
+   /// The caller does not assume ownership, and must not delete the object.
    HypreParMatrix *GetCurlMatrix() const { return C; };
+   /// Return the associated BatchedLOR_AMS object.
    BatchedLOR_AMS &GetAMS() { return ams; }
+
+   // The following should be protected, but contain MFEM_FORALL kernels
+
+   /// Form the discrete curl matrix (not part of the public API).
    void FormCurlMatrix();
    ~BatchedLOR_ADS();
 };
