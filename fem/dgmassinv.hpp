4dfb9ae4214dc30f5d330cc26274755037a9b21d
Will Pazner <will.e.p@gmail.com>
2022-04-26 10:53:41 -0700

    DG mass inverse: Doxygen

--- fem/dgmassinv.hpp
+++ fem/dgmassinv.hpp
@@ -18,6 +18,13 @@
 namespace mfem
 {
 
+/// @brief Solver for the discontinuous Galerkin mass matrix.
+///
+/// This class performs a @a local (diagonally preconditioned) conjugate
+/// gradient iteration for each element. Optionally, a change of basis is
+/// performed to iterate on a better-conditioned system.
+///
+/// This class fully supports execution on device.
 class DGMassInverse : public Solver
 {
 protected:
@@ -29,28 +36,61 @@ protected:
    Vector diag_inv; ///< Jacobi preconditioner.
    double rel_tol = 1e-12; ///< Relative CG tolerance.
    double abs_tol = 1e-12; ///< Absolute CG tolerance.
-   int max_iter = 100; ///> Maximum number of CG iterations;
+   int max_iter = 100; ///< Maximum number of CG iterations;
 
    /// @name Intermediate vectors needed for CG three-term recurrence.
    ///@{
    mutable Vector r_, d_, z_, b2_;
    ///@}
 
+   /// @brief Protected constructor, used internally.
+   ///
+   /// Custom coefficient and integration rule are used in @a coeff and @a ir
+   /// are non-NULL.
    DGMassInverse(FiniteElementSpace &fes_, Coefficient *coeff,
                  const IntegrationRule *ir, int btype);
-
 public:
+   /// @brief Construct the DG inverse mass operator for @a fes_.
+   ///
+   /// The basis type @a btype determines which basis should be used internally
+   /// in the solver. This <b>does not</b> have to be the same basis as @a fes_.
+   /// The best choice is typically BasisType::GaussLegendre because it is
+   /// well-preconditioned by its diagonal.
+   ///
+   /// The solution and right-hand side used for the solver are not affected by
+   /// this basis (they correspond to the basis of @a fes_). @a btype is only
+   /// used internally, and only has an effect on the convergence rate.
+   DGMassInverse(FiniteElementSpace &fes_, int btype=BasisType::GaussLegendre);
+   /// @brief Construct the DG inverse mass operator for @a fes_ with
+   /// Coefficient @a coeff.
+   ///
+   /// @sa DGMassInverse(FiniteElementSpace&, int) for information about @a
+   /// btype.
    DGMassInverse(FiniteElementSpace &fes_, Coefficient &coeff,
                  int btype=BasisType::GaussLegendre);
+   /// @brief Construct the DG inverse mass operator for @a fes_ with
+   /// Coefficient @a coeff and IntegrationRule @a ir.
+   ///
+   /// @sa DGMassInverse(FiniteElementSpace&, int) for information about @a
+   /// btype.
    DGMassInverse(FiniteElementSpace &fes_, Coefficient &coeff,
                  const IntegrationRule &ir, int btype=BasisType::GaussLegendre);
+   /// @brief Construct the DG inverse mass operator for @a fes_ with
+   /// IntegrationRule @a ir.
+   ///
+   /// @sa DGMassInverse(FiniteElementSpace&, int) for information about @a
+   /// btype.
    DGMassInverse(FiniteElementSpace &fes_, const IntegrationRule &ir,
                  int btype=BasisType::GaussLegendre);
-   DGMassInverse(FiniteElementSpace &fes_, int btype=BasisType::GaussLegendre);
-   void Mult(const Vector &Mu, Vector &u) const;
+   /// Solve the system M b = u. Use @a u as an initial guess.
+   void Mult(const Vector &b, Vector &u) const;
+   /// Not implemented. Aborts.
    void SetOperator(const Operator &op);
+   /// Set the relative tolerance.
    void SetRelTol(const double rel_tol_);
+   /// Set the absolute tolerance.
    void SetAbsTol(const double abs_tol_);
+   /// Set the maximum number of iterations.
    void SetMaxIter(const double max_iter_);
    ~DGMassInverse();
 
