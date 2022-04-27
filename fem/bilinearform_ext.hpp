e3297d9e91de7612ba8b17f402b2729850e656ea
Yohann Dudouit <dudouit1@llnl.gov>
2022-04-27 15:20:07 -0700

    Attempt to implement `FormLinearSystem` and `FormSystemMatrix`.

--- fem/bilinearform_ext.hpp
+++ fem/bilinearform_ext.hpp
@@ -125,6 +125,11 @@ public:
    FABilinearFormExtension(BilinearForm *form);
 
    void Assemble();
+   void FormSystemMatrix(const Array<int> &ess_tdof_list, OperatorHandle &A);
+   void FormLinearSystem(const Array<int> &ess_tdof_list,
+                         Vector &x, Vector &b,
+                         OperatorHandle &A, Vector &X, Vector &B,
+                         int copy_interior = 0);
    void Mult(const Vector &x, Vector &y) const;
    void MultTranspose(const Vector &x, Vector &y) const;
 
