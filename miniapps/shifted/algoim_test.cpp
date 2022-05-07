d3e484d67a70fcffd05710765d326838b9c16328
Ketan Mittal <ketan.mittal@gmail.com>
2022-05-06 17:41:51 -0700

    add another level set example

--- miniapps/shifted/algoim_test.cpp
+++ miniapps/shifted/algoim_test.cpp
@@ -1,18 +1,23 @@
-
-
 #include "mfem.hpp"
 #include "integ_algoim.hpp"
 
-
 using namespace mfem;
+using namespace std;
 
-//level set function for sphere in 3D and circle in 2D
+//Level set function for sphere in 3D and circle in 2D
 double sphere_ls(const Vector &x)
 {
    double r2= x*x;
    return -sqrt(r2)+1.0;//the radius is 0.5
 }
 
+//Level set function for a sinusoidal wave.
+//Resulting zero isocontour is at y=0.5-(0.1*sin(3*pi*x+pi/2))
+double sinusoidal_ls(const Vector &x)
+{
+   double a1 = 20., a2 = 2., a3 = 3.;
+   return tanh(a1*(x(1)-0.5) + a2*sin(a3*(x(0)-0.5)*M_PI));
+}
 
 int main(int argc, char *argv[])
 {
@@ -24,7 +29,7 @@ int main(int argc, char *argv[])
    int aorder = 2; //Algoim integration integration points
    bool visualization = true;
    int print_level = 0;
-
+   int ls_type = 1;
 
    OptionsParser args(argc, argv);
    args.AddOption(&mesh_file, "-m", "--mesh", "Mesh file to use.");
@@ -44,6 +49,10 @@ int main(int argc, char *argv[])
                   "-ao",
                   "--aorder",
                   "Algoim Integration order.");
+   args.AddOption(&ls_type,
+                  "-ls",
+                  "--ls-type",
+                  "Level set type: 1: circle, 2 sinusoidal wave");
    args.AddOption(&visualization,
                   "-vis",
                   "--visualization",
@@ -70,7 +79,6 @@ int main(int argc, char *argv[])
       mesh->UniformRefinement();
    }
 
-
    // Define the finite element spaces for the solution
    H1_FECollection fec(order, dim);
    FiniteElementSpace fespace(mesh, &fec, 1, Ordering::byVDIM);
@@ -82,9 +90,34 @@ int main(int argc, char *argv[])
 
    // Define the level set coefficient
    Coefficient *ls_coeff = nullptr;
-   ls_coeff=new FunctionCoefficient(sphere_ls);
+   if (ls_type == 1)
+   {
+      ls_coeff=new FunctionCoefficient(sphere_ls);
+   }
+   else if (ls_type == 2)
+   {
+      ls_coeff=new FunctionCoefficient(sinusoidal_ls);
+   }
+   else
+   {
+      MFEM_ABORT("Level set coefficient not defined");
+   }
+
    // Project the coefficient onto the LS grid function
    x.ProjectCoefficient(*ls_coeff);
+
+   if (visualization)
+   {
+      osockstream sock(19916, "localhost");
+      sock << "solution\n";
+      mesh->Print(sock);
+      x.Save(sock);
+      sock.send();
+      sock << "window_title 'Level set'\n"
+           << "window_geometry "
+           << 1200 << " " << 0 << " " << 600 << " " << 600 << "\n"
+           << "keys jRmmclA" << endl;
+   }
 
    ElementTransformation *trans;
 
@@ -156,14 +189,43 @@ int main(int argc, char *argv[])
       delete air;
    }
 #endif
-   std::cout<<"Volume="<<vol<<" Error="<<vol-M_PI<<std::endl;
-   std::cout<<"Area="<<area<<" Error="<<area-2.0*M_PI<<std::endl;
+   double exact_volume = -10, exact_area = -10;
+   if (ls_type == 1)
+   {
+      if (strncmp(mesh_file,"../../data/star-q3.mesh",100) == 0)
+      {
+         exact_volume = M_PI;
+         exact_area   = M_PI*2;
+      }
+      else if (strncmp(mesh_file, "../../data/inline-quad.mesh",100) == 0)
+      {
+         exact_volume = M_PI/4;
+         exact_area   = M_PI/2;
+      }
+   }
+   else if (ls_type == 2)
+   {
+      if (strncmp(mesh_file, "../../data/inline-quad.mesh",100) == 0)
+      {
+         exact_volume = 0.5;
+         exact_area   = 1.194452300992437;
+      }
+   }
+   if (exact_volume > 0)
+   {
+      std::cout<<"Algoim Volume="<<vol<<" Error="<<vol-exact_volume<<std::endl;
+      std::cout<<"Algoim Area="<<area<<" Error="<<area-exact_area<<std::endl;
+   }
+   else
+   {
+      std::cout<<"Algoim Volume="<<vol<<std::endl;
+      std::cout<<"Algoim Area="<<area<<std::endl;
+   }
 
    //Perform standard MFEM integration
    vol=0.0;
    for (int i=0; i<fespace.GetNE(); i++)
    {
-
       const FiniteElement* el=fespace.GetFE(i);
       //get the element transformation
       trans = fespace.GetElementTransformation(i);
@@ -184,8 +246,14 @@ int main(int argc, char *argv[])
       }
 
    }
-   std::cout<<"Volume="<<vol<<" Error="<<vol-M_PI<<std::endl;
-
+   if (exact_volume > 0.0)
+   {
+      std::cout<<"MFEM Volume="<<vol<<" Error="<<vol-exact_volume<<std::endl;
+   }
+   else
+   {
+      std::cout<<"MFEM Volume="<<vol<<std::endl;
+   }
 
    ParaViewDataCollection dacol("ParaViewDistance", mesh);
    dacol.SetLevelsOfDetail(order);
