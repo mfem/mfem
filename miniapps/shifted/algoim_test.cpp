43a0fb7de16cbb44cbb936dba9103c5ac845f61e
blaz <bslazarov.dev@gmail.com>
2022-05-07 18:29:55 -0700

    added default lines

--- miniapps/shifted/algoim_test.cpp
+++ miniapps/shifted/algoim_test.cpp
@@ -1,3 +1,21 @@
+// Copyright (c) 2010-2022, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+//
+//     ---------------------------------------------------------------------
+//     Miniapp: Integral of implicit domains defined by a level set function
+//     ---------------------------------------------------------------------
+//  
+
+
+
 #include "mfem.hpp"
 #include "integ_algoim.hpp"
 
@@ -8,7 +26,7 @@ using namespace std;
 double sphere_ls(const Vector &x)
 {
    double r2= x*x;
-   return -sqrt(r2)+1.0;//the radius is 0.5
+   return -sqrt(r2)+1.0;//the radius is 1.0
 }
 
 //Level set function for a sinusoidal wave.
