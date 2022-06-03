cd29ce6961cc0021b5a873a84d5a8556e7e8aca5
KJSchwie <kjschwie@mtu.edu>
2022-06-03 15:20:26 -0600

    Adding the fsi mms with assumed fluid solution.

--- miniapps/fsi/shell1d_fluid.cpp
+++ miniapps/fsi/shell1d_fluid.cpp
@@ -1,11 +1,13 @@
 //
-// Compile with: make shell1d
+// Compile with: make shell1d_fluid
 //
-// Description: it solves a 1D equation  
-//                      u_tt = -u_xxxx
-//              using an auxiliary variable V=-u_xx and the Newmark-Beta integrator
-//  MMS
-//  u = cos(t)*sin(x) with x in [0, 2pi]
+// Description: it solves a 1D equation as described in LANL fsi notes assuming the fluid solve is known.
+//
+// TODO: 
+// 1) Adjust need for dt to be passed to the ShellOperator class if possible
+// 2) Account for nonhomogenous BCs.
+// 3) Make a parallel version.
+// 4) Play with convergence rates more.
 
 #include "mfem.hpp"
 #include <fstream>
@@ -14,25 +16,60 @@
 using namespace std;
 using namespace mfem;
 
+// Globally store the problem constants.
+struct
+{
+    double a = 0.5;
+    double ft = 2;
+    double fx = 2;
+    double Tbar = 1;
+    double Kbar = 1;
+    double viscosity = 0.05;
+} ctx;
+
 //1D MMS
 double InitialSolution(const Vector &pt)
 {
    double x = pt(0);
-   return sin(x);
+   return ctx.a / (M_PI * ctx.ft) * sin(ctx.fx * M_PI * x);
 }
 
 double InitialRate(const Vector &pt)
 {
-   return 0.;
+    double x = pt(0);
+    return ctx.a * sin(ctx.fx * M_PI * x);
 }
 
 double ExactSolution(const Vector &pt, const double t)
 {
-   double x = pt(0);
-   return cos(t)*sin(x);
+    double x = pt(0);
+    return ctx.a / (M_PI * ctx.ft) * sin(ctx.fx * M_PI * x) * sin(ctx.ft * M_PI * t) + 1;
 }
 
+double Forcing(const Vector &pt, const double t)
+{
+    double x = pt(0);
 
+    // Compute the normal vector
+    double dEtaDx = ctx.a * ctx.fx / ctx.ft * cos(ctx.fx * M_PI * x) * sin(ctx.ft * M_PI * t);
+    double n2 = 1/sqrt(1 + dEtaDx * dEtaDx);
+    double n1 = -dEtaDx * n2;
+
+    // Compute pressure, structure, velocity, and required derivatives.
+    // Uses a tiny bit more memory, will be easier to debug.
+    // We need to recall that y = eta.
+    double eta = ExactSolution(pt,t);
+    double p = cos(ctx.fx * M_PI * x) * cos(ctx.fx * M_PI *eta) * cos(ctx.ft * M_PI * t);
+    double d2EtaDx2 = -ctx.a * ctx.fx * ctx.fx * M_PI / ctx.ft * sin(ctx.fx * M_PI * x) * sin(ctx.ft * M_PI * t);
+    double d2EtaDt2 = -ctx.a * ctx.ft * ctx.ft * M_PI / ctx.ft * sin(ctx.fx * M_PI * x) * sin(ctx.ft * M_PI * t) + 1;
+    double dUDy = -ctx.a * ctx.fx * M_PI * cos(ctx.fx * M_PI *x) * cos(ctx.ft * M_PI * t);
+    double dVDy = -ctx.a * ctx.fx * M_PI * cos(ctx.fx * M_PI * x) * cos(ctx.ft * M_PI * t) * dEtaDx;
+    double dVDx =  ctx.a * ctx.fx * M_PI * sin(ctx.fx * M_PI * x) * cos(ctx.ft * M_PI * t);
+
+    // We now have everything we need.
+    return ctx.Tbar * d2EtaDt2 + ctx.Kbar * eta - ctx.Tbar * d2EtaDx2 - p*n2 
+         + ctx.viscosity * ((dUDy + dVDx) * n1 + 2 * dVDy * n2);
+}
 
 class ShellOperator : public SecondOrderTimeDependentOperator
 {
@@ -41,24 +78,22 @@ protected:
    Array<int> ess_tdof_list; 
 
    BilinearForm *M, *K;
-   SparseMatrix Mmat, Mmat0, Kmat, *S, *Sc;
+   LinearForm forcing_LF;
+   SparseMatrix Mmat, Mmat0, Kmat, S;
    Solver *invM, *invS;
    double current_dt, fac0old;
    Array<int> block_trueOffsets;
+   FunctionCoefficient forcing_function;
 
-   CGSolver M_solver; // Krylov solver for inverting the mass matrix M
-   DSmoother M_prec;  // Preconditioner for the mass matrix M
+   CGSolver M_solver, S_solver; // Krylov solver for inverting mass and overall system matricies.
+   DSmoother M_prec, S_prec;  // Preconditioner for the mass matrix M
 
-   GMRESSolver solver;  // the block system is not symmetric 
-
-   BlockOperator *BlockSystem;
-   BlockDiagonalPreconditioner *prec;
-   BlockVector *vx, *rhs;
+   double current_time, dt;
 
    mutable Vector z, V; // auxiliary vector
 
 public:
-   ShellOperator(FiniteElementSpace &f, Array<int> &ess_bdr);
+   ShellOperator(FiniteElementSpace &f, Array<int> &ess_bdr, double dt);
 
    using SecondOrderTimeDependentOperator::Mult;
    virtual void Mult(const Vector &u, const Vector &du_dt,
@@ -72,9 +107,9 @@ public:
 };
 
 
-ShellOperator::ShellOperator(FiniteElementSpace &f, Array<int> &ess_bdr)
+ShellOperator::ShellOperator(FiniteElementSpace &f, Array<int> &ess_bdr, double dt_)
    : SecondOrderTimeDependentOperator(f.GetTrueVSize(), 0.0), fespace(f), M(NULL),
-     K(NULL), current_dt(0.0), block_trueOffsets(3), z(height), V(height) 
+     K(NULL), current_dt(0.0), block_trueOffsets(3), forcing_function(Forcing), z(height), V(height) 
 {
    const double rel_tol = 1e-8;
    ConstantCoefficient one(1);
@@ -94,152 +129,86 @@ ShellOperator::ShellOperator(FiniteElementSpace &f, Array<int> &ess_bdr)
    M_solver.iterative_mode = false;
    M_solver.SetRelTol(rel_tol);
    M_solver.SetAbsTol(0.0);
-   M_solver.SetMaxIter(100);
+   M_solver.SetMaxIter(1000);
    M_solver.SetPrintLevel(0);
    M_solver.SetPreconditioner(M_prec);
    M_solver.SetOperator(Mmat);
 
+   forcing_LF.Update(&fespace);
+   dt = dt_;
+   forcing_LF.AddDomainIntegrator(new DomainLFIntegrator(forcing_function));
+
    fac0old=0.;
 
-   block_trueOffsets[0] = 0;
-   block_trueOffsets[1] = height;
-   block_trueOffsets[2] = height;
-   block_trueOffsets.PartialSum();
+   // The overall system matrix does not change. Just build it here.
+   S = Mmat;
+   S *= 1.0 + ctx.Kbar / ctx.Tbar;
+   S += Kmat;
 
-   BlockSystem = new BlockOperator(block_trueOffsets);
-   BlockSystem->SetBlock(0,0, &Mmat);
-   BlockSystem->SetBlock(0,1, &Kmat);
-   BlockSystem->SetBlock(1,0, &Kmat, -1.);
-
-   // Define a block diagonal preconditioner
-   //                 P = [ diag(M)            0             ]
-   //                     [  0       M/fac0 + K diag(M)^-1 K ]
-   prec = new BlockDiagonalPreconditioner(block_trueOffsets);
-
-   Vector Md(height);
-   Mmat.GetDiag(Md);
-   SparseMatrix tmp = Kmat;
-   for (int i = 0; i < Md.Size(); i++)
-   {
-      tmp.ScaleRow(i, 1./Md(i));
-   }
-   Sc = mfem::Mult(Kmat, tmp);
-
-   invM = new DSmoother(Mmat);
-   invM->iterative_mode = false;
-
-   prec->SetDiagonalBlock(0, invM);
-
-   rhs = new BlockVector(block_trueOffsets);
-   vx = new BlockVector(block_trueOffsets);
-   invS=NULL;
+   S_solver.iterative_mode = false;
+   S_solver.SetRelTol(rel_tol);
+   S_solver.SetAbsTol(0.0);
+   S_solver.SetMaxIter(1000);
+   S_solver.SetPrintLevel(0);
+   S_solver.SetPreconditioner(S_prec);
+   S_solver.SetOperator(S);
 }
 
 void ShellOperator::Mult(const Vector &u, const Vector &du_dt,
                         Vector &d2udt2)  const
 {
    // Compute:
-   //    V = M^{-1}*K(v)
-   //    d2udt2 = M^{-1}*[-K V]
+   //    d2EtaDt2 = -Kbar / Tbar eta + M^-1(F - K eta)
    Kmat.Mult(u, z);
+   z *= -1;
+   z += forcing_LF;
    M_solver.Mult(z, V);
 
-   Kmat.Mult(V, z);
-   z.Neg(); // z = -z
-   M_solver.Mult(z, d2udt2);
+   d2udt2 = u;
+   d2udt2 *= -ctx.Kbar / ctx.Tbar;
+   d2udt2 += V;
 }
 
 void ShellOperator::ImplicitSolve(const double fac0, const double fac1,
                                  const Vector &u, const Vector &dudt, Vector &d2udt2)
 {
-   // Solve the equation:
-   //    d2udt2 = M^{-1}*[-K V]
-   // where
-   //         V = M^{-1}*[K(u+fac0*d2udt2)]
-   // 
-   // Need to solve the 2x2 system:
-   // | M   K      ||a| = | 0      |
-   // | -K  M/fac0 ||V|   | Ku/fac0|
+   // Solve the equation
+   //    d2udt2 = S^1/fac0*(-Kbar/Tbar M eta - K eta + F).
+   // We have the system matrix 
+   //    S = ((1+Kbar/Tbar)M + K).
    
-   if (fac0old<1e-14)
-   {
-      fac0old=fac0;
-      Mmat0=Mmat;
-      Mmat0*=(1./fac0);
-      BlockSystem->SetBlock(1,1, &Mmat0);
+   // We first need to form the right hand side.
+   // Assume that ImplicitSolve is called only once per time step.
+   // This is probably bad, but is the case for our solver.
+   // TODO: Figure out how to impose time-dependent forcing without something like this.
+   current_time += dt;
+   forcing_function.SetTime(current_time);
+   forcing_LF.Update();
+   forcing_LF.Assemble();
 
-      S = Add(1.0, Mmat0, 1., *Sc);
-#ifndef MFEM_USE_SUITESPARSE
-      invS = new GSSmoother(*S);
-#else
-      invS = new UMFPackSolver(*S);
-#endif
-      invS->iterative_mode = false;
-      prec->SetDiagonalBlock(1, invS);
+   // Form the part of the rhs that comes from the current postion.
+   Mmat.Mult(u,z);
+   z *= -ctx.Kbar/ctx.Tbar;
+   z += forcing_LF;
+   Kmat.Mult(u,V);
+   V *= -1.0;
+   z += V;
+   z *= 1./fac0;
 
-      solver.iterative_mode = false;
-      solver.SetAbsTol(0.0);
-      solver.SetRelTol(1e-8);
-      solver.SetMaxIter(100);
-      solver.SetOperator(*BlockSystem);
-      solver.SetPreconditioner(*prec);
-      solver.SetPrintLevel(0);
-   }
-   else if(fac0!=fac0old)
-   {
-      cout << " fac0 changes..."<<endl;
-      fac0old=fac0;
-      Mmat0=Mmat;
-      Mmat0*=(1./fac0);
-      BlockSystem->SetBlock(1,1, &Mmat0);
-
-      delete invS;
-      delete S;
-      S = Add(1.0, Mmat0, 1., *Sc);
-#ifndef MFEM_USE_SUITESPARSE
-      invS = new GSSmoother(*S);
-#else
-      invS = new UMFPackSolver(*S);
-#endif
-      invS->iterative_mode = false;
-      prec->SetDiagonalBlock(1, invS);
-
-      solver.SetOperator(*BlockSystem);
-      solver.SetPreconditioner(*prec);
-   }
-
-   Kmat.Mult(u, z);
-   for (int i = 0; i < ess_tdof_list.Size(); i++)
-   {
-      z[ess_tdof_list[i]] = 0.0;
-   }
-   z*=(1./fac0);
-
-   *rhs=0.;
-   rhs->GetBlock(1)=z;
-
-   solver.Mult(*rhs, *vx);
-   d2udt2=vx->GetBlock(0);
+   // Solve the system.
+   S_solver.Mult(z,d2udt2);
 }
 
 ShellOperator::~ShellOperator()
 {
    delete M;
    delete K;
-   delete BlockSystem;
-   delete prec;
-   delete invM;
-   delete invS;
-   delete S;
-   delete Sc;
-   delete vx;
-   delete rhs;
 }
 
 int main(int argc, char *argv[])
 {
    int order = 2;
-   double t_final = 2*M_PI;
+   double t_final = 1.0;
    double dt = 1e-2;
    bool visualization = true;
    int vis_steps = 2;
@@ -269,7 +238,7 @@ int main(int argc, char *argv[])
    args.PrintOptions(cout);
 
    Mesh mesh;
-   mesh = mesh.MakeCartesian1D(8, 2*M_PI);
+   mesh = mesh.MakeCartesian1D(8); // Use default interval [0,1].
    int dim = mesh.Dimension();
 
    for (int l = 0; l < ref_levels; l++)
@@ -286,7 +255,7 @@ int main(int argc, char *argv[])
    Array<int> ess_bdr(mesh.bdr_attributes.Max());
    if (mesh.bdr_attributes.Size())
    {
-      ess_bdr = 1;
+      ess_bdr = 0.0;
    }
 
    SecondOrderODESolver *ode_solver = new NewmarkSolver();
@@ -332,7 +301,7 @@ int main(int argc, char *argv[])
       }
    }         
 
-   ShellOperator oper(fespace, ess_bdr);
+   ShellOperator oper(fespace, ess_bdr, dt);
    ode_solver->Init(oper);
    double t = 0.0;
 
