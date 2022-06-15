542bd7ecd811751cb1d282873189c675aea392ce
Yohann Dudouit <dudouit1@llnl.gov>
2022-06-14 17:24:51 -0700

    Add note to `EliminateRHS`.

--- linalg/operator.hpp
+++ linalg/operator.hpp
@@ -879,7 +879,9 @@ public:
            z = A((0,x_b));  b_i -= z_i;  b_b = x_b;
 
        where the "_b" subscripts denote the essential (boundary) indices/dofs of
-       the vectors, and "_i" -- the rest of the entries. */
+       the vectors, and "_i" -- the rest of the entries.
+
+       @note This method is consistent with `DiagonalPolicy::DIAG_ONE`. */
    void EliminateRHS(const Vector &x, Vector &b) const;
 
    /** @brief Constrained operator action.
