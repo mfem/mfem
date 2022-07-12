fbfa8f22c09092a3a49bbc1cbc52632748f67eba
Dan Serino <serino.daniel@gmail.com>
2022-07-12 09:25:41 -0400

    added notes

--- miniapps/tds-gs/SurfaceCoefficient.hpp
+++ miniapps/tds-gs/SurfaceCoefficient.hpp
@@ -2,6 +2,34 @@
 using namespace std;
 using namespace mfem;
 
+/*
+  Goal: compute the bilinear form matrix
+    
+  int_Gamma int_Gamma v_i(y) M(x, y) psi(x) dS(x) dS(y),    i=1,2,...
+
+  v_i: i'th test function
+  psi: solution that we are solving for
+  M:   Green's function
+
+  Approach 1: (code below)
+    int_Gamma (int_Gamma M(x, y) psi(x) dS(x)) v_i(y) dS(y),    i=1,2,...
+
+    The following is like a bilinear form with the test functions all set to 1?
+    F(y; psi) := int_Gamma M(x, y) psi(x) dS(x)
+    F(y; psi) is a matrix?
+
+    int_Gamma F(y; psi) v_i(y) dS(y)
+
+  Approach 2:
+    int_Gamma (int_Gamma v_i(y) M(x, y) dS(y)) psi(x) dS(x),    i=1,2,...
+
+    f_i(x) := int_Gamma v_i(y) M(x, y) dS(y)
+    Linear form
+
+    int_Gamma f_i(x) psi(x) dS(x)
+    Bilinear form with shape dependent coefficient?
+*/
+
 double greenfunc(Vector X, Vector Y)
 {
    return 1.0/(X(0)*X(0)+X(1)*X(1)+Y(0)*Y(0)+Y(1)*Y(1)); //just an example
@@ -43,7 +71,7 @@ public:
 class YFixSurfaceCoefficient : public Coefficient
 {
 private:
-  GridFunction *Psi;
+  GridFunction *Psi; // this is unknown
   GreenCoefficient Green;
   FiniteElementSpace *fespace;
   GridFunction ones;
@@ -63,6 +91,8 @@ YFixSurfaceCoefficient::YFixSurfaceCoefficient( GridFunction *psi_ )
 double YFixSurfaceCoefficient::Eval(ElementTransformation &T,
                                    const IntegrationPoint &ip)
 {
+
+  
    double x[3], value;
    // Compute y value for the given quaderture ip
    Vector transip(x, 3);
@@ -75,7 +105,7 @@ double YFixSurfaceCoefficient::Eval(ElementTransformation &T,
    IntSurf.AddBoundaryIntegrator(new BoundaryMassIntegrator(Green));
    IntSurf.SetAssemblyLevel(AssemblyLevel::PARTIAL);
    IntSurf.Assemble();
-   IntSurf.Mult(*Psi, IntPhi);
+   IntSurf.Mult(*Psi, IntPhi); // psi is unknown
    value = IntPhi(ones);
    return value;
 }
