29f0c58931c31318ca4e3da2c3d3c0bd6f2ef989
Tobias Duswald <tobias.duswald@cern.ch>
2022-07-18 15:09:42 -0700

    Remove header comments from cpp file

--- miniapps/materials/rational_approximation.cpp
+++ miniapps/materials/rational_approximation.cpp
@@ -6,18 +6,6 @@
 
 namespace mfem {
 
-/** RationalApproximation_AAA: compute the rational approximation (RA) of data
-    @a val [in] at the set of points @a pt [in].
-
-    @param[in]  val        Vector of data values
-    @param[in]  pt         Vector of sample points
-    @param[in]  tol        Relative tolerance
-    @param[in]  max_order  Maximum number of terms (order) of the RA
-    @param[out] z          Support points of the RA in rational barycentric form
-    @param[out] f          Data values at support points @a z
-    @param[out] w          Weights of the RA in rational barycentric form
-
-    See pg. A1501 of Nakatsukasa et al. [1]. */
 void RationalApproximation_AAA(const Vector &val, const Vector &pt,
                                Array<double> &z, Array<double> &f, Vector &w,
                                double tol, int max_order)
@@ -130,17 +118,6 @@ void RationalApproximation_AAA(const Vector &val, const Vector &pt,
    }
 }
 
-/** ComputePolesAndZeros: compute the @a poles [out] and @a zeros [out] of the
-    rational function f(z) = C p(z)/q(z) from its ration barycentric form.
-
-    @param[in]  z      Support points in rational barycentric form
-    @param[in]  f      Data values at support points @a z
-    @param[in]  w      Weights in rational barycentric form
-    @param[out] poles  Array of poles (roots of p(z))
-    @param[out] zeros  Array of zeros (roots of q(z))
-    @param[out] scale  Scaling constant in f(z) = C p(z)/q(z)
-
-    See pg. A1501 of Nakatsukasa et al. [1]. */
 void ComputePolesAndZeros(const Vector &z, const Vector &f, const Vector &w,
                           Array<double> & poles, Array<double> & zeros, double &scale)
 {
@@ -203,14 +180,6 @@ void ComputePolesAndZeros(const Vector &z, const Vector &f, const Vector &w,
    scale = w * f / w.Sum();
 }
 
-/** PartialFractionExpansion: compute the partial fraction expansion of the
-    rational function f(z) = Σ_i c_i / (z - p_i) from its @a poles [in] and
-    @a zeros [in].
-
-    @param[in]  poles   Array of poles (same as p_i above)
-    @param[in]  zeros   Array of zeros
-    @param[in]  scale   Scaling constant
-    @param[out] coeffs  Coefficients c_i */
 void PartialFractionExpansion(double scale, Array<double> & poles,
                               Array<double> & zeros, Array<double> & coeffs)
 {
@@ -243,24 +212,6 @@ void PartialFractionExpansion(double scale, Array<double> & poles,
    }
 }
 
-
-/** ComputePartialFractionApproximation: compute a rational approximation (RA)
-    in partial fraction form, e.g., f(z) ≈ Σ_i c_i / (z - p_i), from sampled
-    values of the function f(z) = z^{-a}, 0 < a < 1.
-
-    @param[in]  alpha         Exponent a in f(z) = z^-a
-    @param[in] lmax, npoints  f(z) is uniformly sampled @a npoints times in the
-                              interval [ 0, @a lmax ]
-    @param[in]  tol           Relative tolerance
-    @param[in]  max_order     Maximum number of terms (order) of the RA
-    @param[out] coeffs        Coefficients c_i
-    @param[out] poles         Poles p_i
-
-    NOTES: When MFEM is not built with LAPACK support, only @a alpha = 0.33,
-           0.5, and 0.99 are possible. In this case, if @a alpha != 0.33 and
-           @a alpha != 0.99, then @a alpha = 0.5 is used by default.
-
-   See pg. A1501 of Nakatsukasa et al. [1]. */
 void ComputePartialFractionApproximation(double & alpha,
                                          Array<double> & coeffs, Array<double> & poles,
                                          double lmax,
