93a82c8e69c1f137271c147a839d00c0e0d0eb11
Tobias Duswald <tobias.duswald@cern.ch>
2022-07-18 15:11:31 -0700

    Fix visualization problem for GLVis

--- miniapps/materials/visualizer.cpp
+++ miniapps/materials/visualizer.cpp
@@ -21,30 +21,36 @@ void Visualizer::SendToGLVis(){
   char vishost[] = "localhost";
   int  visport   = 19916;
   int num_procs = Mpi::WorldSize();
-  int myid = Mpi::WorldRank();
+  int process_rank = Mpi::WorldRank();
   socketstream uout, vout, wout;
   std::ostringstream oss_u, oss_v, oss_w;
   uout.open(vishost, visport);
   uout.precision(8);
-  vout.open(vishost, visport);
-  vout.precision(8);
-  wout.open(vishost, visport);
-  wout.precision(8);
   oss_u.str(""); oss_u.clear();
-  oss_v.str(""); oss_v.clear();
-  oss_w.str(""); oss_w.clear();
   oss_u << "Random Field";
-  oss_v << "Topological Support";
-  oss_w << "Imperfect Topology";
-  uout << "parallel " << num_procs << " " << myid << "\n"
+  uout << "parallel " << num_procs << " " << process_rank << "\n"
         << "solution\n" << *mesh_ << g1_
         << "window_title '" << oss_u.str() << "'" << std::flush;
-  vout << "parallel " << num_procs << " " << myid << "\n"
+  uout.close();
+
+
+  vout.open(vishost, visport);
+  vout.precision(8);
+  oss_v.str(""); oss_v.clear();
+  oss_v << "Topological Support";
+  vout << "parallel " << num_procs << " " << process_rank << "\n"
         << "solution\n" << *mesh_ << g2_
         << "window_title '" << oss_v.str() << "'" << std::flush;
-  wout << "parallel " << num_procs << " " << myid << "\n"
+  vout.close();
+
+  wout.open(vishost, visport);
+  wout.precision(8);
+  oss_w.str(""); oss_w.clear();
+  oss_w << "Imperfect Topology";
+  wout << "parallel " << num_procs << " " << process_rank << "\n"
         << "solution\n" << *mesh_ << g3_
         << "window_title '" << oss_w.str() << "'" << std::flush;
+  wout.close();
 }
 
 } // namespace materials
