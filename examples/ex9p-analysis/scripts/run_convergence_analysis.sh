21d67b35bc00f234ae4a747f32d94343646c7fda
Madison Sheridan <madison.sheridan94@gmail.com>
2022-07-19 14:26:24 -0700

    Testing on 1-9 processors.

--- examples/ex9p-analysis/scripts/run_convergence_analysis.sh
+++ examples/ex9p-analysis/scripts/run_convergence_analysis.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # This is a script to run convergence analysis on ex9p-continuous
-mfem_dir="/Users/sheridan7/Workspace/mfem/examples"
-create_convergence="/Users/sheridan7/Workspace/mfem/examples/ex9p-analysis/scripts/create_convergence_table.py"
-temp_output="/Users/sheridan7/Workspace/mfem/examples/ex9p-analysis/temp_output/"
-analysis_dir="/Users/sheridan7/Workspace/mfem/examples/ex9p-analysis/"
+mfem_dir="/Users/madisonsheridan/Workspace/mfem/examples"
+create_convergence="/Users/madisonsheridan/Workspace/mfem/examples/ex9p-analysis/scripts/create_convergence_table.py"
+temp_output="/Users/madisonsheridan/Workspace/mfem/examples/ex9p-analysis/temp_output/"
+analysis_dir="/Users/madisonsheridan/Workspace/mfem/examples/ex9p-analysis/"
 
 cd $analysis_dir
 rm *.out
@@ -15,16 +15,16 @@ rm *.out
 rm *.txt
 
 cd $mfem_dir
-mpirun -np 4 --oversubscribe ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 1 -rp 0 -p 4 -tf 2.84
-mpirun -np 4 --oversubscribe ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 2 -rp 0 -p 4 -tf 2.84
-mpirun -np 4 --oversubscribe ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 3 -rp 0 -p 4 -tf 2.84
-mpirun -np 4 --oversubscribe ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 4 -rp 0 -p 4 -tf 2.84
-# mpirun -np 4 --oversubscribe ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 5 -rp 0 -p 4 -tf 2.84
-# mpirun -np 4 --oversubscribe ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 6 -rp 0 -p 4 -tf 2.84
-# mpirun -np 4 --oversubscribe ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 7 -rp 0 -p 4 -tf 2.84
-# mpirun -np 4 --oversubscribe ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 8 -rp 0 -p 4 -tf 2.84
-# mpirun -np 4 --oversubscribe ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 9 -rp 0 -p 4 -tf 2.84
-# mpirun -np 4 --oversubscribe ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 10 -rp 0 -p 4 -tf 2.84
+mpirun -np 8 ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 1 -rp 0 -p 4 -tf 2.84
+mpirun -np 8 ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 2 -rp 0 -p 4 -tf 2.84
+mpirun -np 8 ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 3 -rp 0 -p 4 -tf 2.84
+mpirun -np 8 ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 4 -rp 0 -p 4 -tf 2.84
+mpirun -np 8 ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 5 -rp 0 -p 4 -tf 2.84
+mpirun -np 8 ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 6 -rp 0 -p 4 -tf 2.84
+# mpirun -np 4 ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 7 -rp 0 -p 4 -tf 2.84
+# mpirun -np 4 ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 8 -rp 0 -p 4 -tf 2.84
+# mpirun -np 4 ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 9 -rp 0 -p 4 -tf 2.84
+# mpirun -np 4 ex9p-continuous -m ../data/periodic-square.mesh -dt .001 -rs 10 -rp 0 -p 4 -tf 2.84
 
 # mpirun -np 2 ex9p-continuous -m ../data/periodic-square.mesh -ct -rs 1 -rp 0 -p 4 -ots
 # mpirun -np 2 ex9p-continuous -m ../data/periodic-square.mesh -ct -rs 2 -rp 0 -p 4 -ots
@@ -60,4 +60,4 @@ mv *.out temp_output/.
 cd scripts
 
 # python stuff for iterating over files to build convergence table
-python $create_convergence $temp_output
+python3 $create_convergence $temp_output
--- examples/ex9p-analysis/scripts/run_parallelization_analysis.sh
+++ examples/ex9p-analysis/scripts/run_parallelization_analysis.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # This is a script to run convergence analysis on ex9p-continuous
-mfem_dir="/Users/sheridan7/Workspace/mfem/examples"
-create_parallelization="/Users/sheridan7/Workspace/mfem/examples/ex9p-analysis/scripts/create_parallelization_table.py"
-temp_output="/Users/sheridan7/Workspace/mfem/examples/ex9p-analysis/temp_output/"
-analysis_dir="/Users/sheridan7/Workspace/mfem/examples/ex9p-analysis/"
+mfem_dir="/Users/madisonsheridan/Workspace/mfem/examples"
+create_parallelization="/Users/madisonsheridan/Workspace/mfem/examples/ex9p-analysis/scripts/create_parallelization_table.py"
+temp_output="/Users/madisonsheridan/Workspace/mfem/examples/ex9p-analysis/temp_output/"
+analysis_dir="/Users/madisonsheridan/Workspace/mfem/examples/ex9p-analysis/"
 cd $analysis_dir
 rm *.out
 rm *.txt
@@ -18,9 +18,16 @@ cd $mfem_dir
 # mpirun -np 2 ex9p-continuous -m ../data/periodic-segment.mesh -ct -rs 5 -rp 0 -p 4 -tf 2
 # mpirun -np 4 ex9p-continuous -m ../data/periodic-segment.mesh -ct -rs 5 -rp 0 -p 4 -tf 2
 
-mpirun -np 1 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.005 -rs 5 -rp 0 -p 4 -tf 2.84
-mpirun -np 2 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.005 -rs 5 -rp 0 -p 4 -tf 2.84
-mpirun -np 4 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.005 -rs 5 -rp 0 -p 4 -tf 2.84
+mpirun -np 1 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.001 -rs 4 -rp 4 -p 4 -tf 2.84
+mpirun -np 2 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.001 -rs 4 -rp 4 -p 4 -tf 2.84
+mpirun -np 3 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.001 -rs 4 -rp 4 -p 4 -tf 2.84
+mpirun -np 4 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.001 -rs 4 -rp 4 -p 4 -tf 2.84
+# mpirun -np 5 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.001 -rs 4 -rp 4 -p 4 -tf 2.84
+# mpirun -np 6 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.001 -rs 4 -rp 4 -p 4 -tf 2.84
+# mpirun -np 7 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.001 -rs 4 -rp 4 -p 4 -tf 2.84
+# mpirun -np 8 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.001 -rs 4 -rp 4 -p 4 -tf 2.84
+# mpirun -np 9 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.001 -rs 4 -rp 4 -p 4 -tf 2.84
+# mpirun -np 10 ex9p-continuous -m ../data/periodic-square.mesh -dt 0.005 -rs 4 -rp 0 -p 4 -tf 2.84
 
 # mpirun -np 1 ex9p-discontinuous -m ../data/periodic-square.mesh -ct -rs 5 -rp 0 -p 4 -ots
 # mpirun -np 2 ex9p-discontinuous -m ../data/periodic-square.mesh -ct -rs 5 -rp 0 -p 4 -ots
@@ -43,4 +50,4 @@ mv *.out temp_output/.
 cd scripts
 
 # python stuff for iterating over files to build convergence table
-python $create_parallelization $temp_output
+python3 $create_parallelization $temp_output
