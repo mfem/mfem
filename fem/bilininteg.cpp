810effe04f0d117136cf40b2201a0a174e3ebbe4
Greg Teichert <gregteichert@coreform.com>
2022-07-20 15:08:45 -0600

    add: adding NURBS patch integration to additional integrators

--- fem/bilininteg.cpp
+++ fem/bilininteg.cpp
@@ -856,7 +856,6 @@ void DiffusionIntegrator::AssembleElementMatrix
       */
 
       MFEM_VERIFY(kv.Size() == dim, "Sanity check (remove later)");
-
       irn = &patchRule->GetElementRule(NURBSFE->GetElement(), patch, ijk, kv);
    }
 
@@ -2818,11 +2817,31 @@ void ElasticityIntegrator::AssembleElementMatrix(
       ir = &IntRules.Get(el.GetGeomType(), order);
    }
 
+   const NURBSFiniteElement *NURBSFE =
+	   dynamic_cast<const NURBSFiniteElement *>(&el);
+
+   IntegrationRule *irn = nullptr;
+   if (NURBSFE && patchRule)
+   {
+      const int patch = NURBSFE->GetPatch();
+      int ijk[3];
+      NURBSFE->GetIJK(ijk);
+      Array<const KnotVector*>& kv = NURBSFE->KnotVectors();
+
+      MFEM_VERIFY(kv.Size() == dim, "Sanity check (remove later)");
+
+      irn = &patchRule->GetElementRule(NURBSFE->GetElement(), patch, ijk, kv);
+   }
+
+   const int numIP = irn ? irn->GetNPoints() : ir->GetNPoints();
+
    elmat = 0.0;
 
-   for (int i = 0; i < ir -> GetNPoints(); i++)
+   // for (int i = 0; i < ir->GetNPoints(); i++)
+   for (int i = 0; i < numIP; i++)
    {
-      const IntegrationPoint &ip = ir->IntPoint(i);
+      // const IntegrationPoint &ip = ir->IntPoint(i);
+      const IntegrationPoint &ip = irn ? irn->IntPoint(i) : ir->IntPoint(i);
 
       el.CalcDShape(ip, dshape);
 
