50e912cf13ba79f1de7b0498f8c1676920838d56
Qi Tang <tangqi@msu.edu>
2022-07-28 11:59:25 -0600

    init

--- miniapps/mhd/InitialConditions.cpp
+++ miniapps/mhd/InitialConditions.cpp
@@ -198,9 +198,9 @@ double E0rhs7(const Vector &x)
 double InitialPsi7(const Vector &x)
 {
     double lx = 5.0*L0, ly = L0, llong = 50.0*L0;
-    //return -L0*log(cosh(x(1)/L0)) + beta*exp( - (x(0)/lx)*(x(0)/lx) - (x(1)/ly)*(x(1)/ly) );
+    return -L0*log(cosh(x(1)/L0)) + beta*exp( - (x(0)/lx)*(x(0)/lx) - (x(1)/ly)*(x(1)/ly) );
     //return -L0*log(cosh(x(1)/L0)) + beta*sin(M_PI/llong*x(1))*cos(M_PI/lx*x(0));
-    return -L0*log(cosh(x(1)/L0)) + beta*exp(-(x(1)/ly)*(x(1)/ly))*cos(M_PI/lx*x(0));
+    //return -L0*log(cosh(x(1)/L0)) + beta*exp(-(x(1)/ly)*(x(1)/ly))*cos(M_PI/lx*x(0));
 }
 
 double BackPsi7(const Vector &x)
@@ -212,9 +212,9 @@ double InitialJ7(const Vector &x)
 {
     double lx = 5.0*L0, ly = L0, llong = 50.0*L0;
     double exp_xy =exp( - (x(0)/lx)*(x(0)/lx) - (x(1)/ly)*(x(1)/ly) );
-    //return ( tanh(x(1)/L0)*tanh(x(1)/L0) - 1.0 )/L0  - beta*exp_xy*( 2.0/lx/lx + 2.0/ly/ly - 4.0*(x(0)/lx)*(x(0)/lx)/lx/lx - 4.0*(x(1)/ly)*(x(1)/ly)/ly/ly );
+    return ( tanh(x(1)/L0)*tanh(x(1)/L0) - 1.0 )/L0  - beta*exp_xy*( 2.0/lx/lx + 2.0/ly/ly - 4.0*(x(0)/lx)*(x(0)/lx)/lx/lx - 4.0*(x(1)/ly)*(x(1)/ly)/ly/ly );
     //return ( tanh(x(1)/L0)*tanh(x(1)/L0) - 1.0 )/L0  - beta*sin(M_PI/llong*x(1))*cos(M_PI/lx*x(0))*M_PI*M_PI*(1.0/llong/llong+1.0/lx/lx);
-    return ( tanh(x(1)/L0)*tanh(x(1)/L0) - 1.0 )/L0  + beta*exp(-(x(1)/ly)*(x(1)/ly))*cos(M_PI/lx*x(0))*(-M_PI*M_PI/lx/lx - 2/ly/ly + 4*x(1)*x(1)/ly/ly/ly/ly);
+    //return ( tanh(x(1)/L0)*tanh(x(1)/L0) - 1.0 )/L0  + beta*exp(-(x(1)/ly)*(x(1)/ly))*cos(M_PI/lx*x(0))*(-M_PI*M_PI/lx/lx - 2/ly/ly + 4*x(1)*x(1)/ly/ly/ly/ly);
 }
 
 double InitialPsi8(const Vector &x)
