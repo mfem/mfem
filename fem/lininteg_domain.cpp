c94261878a8c491fbaaf193bfa9e1be3089d1209
Will Pazner <will.e.p@gmail.com>
2022-08-12 11:18:51 -0700

    Rename J to DETJ in DomainLFIntegrator

--- fem/lininteg_domain.cpp
+++ fem/lininteg_domain.cpp
@@ -19,13 +19,13 @@ namespace mfem
 template<int T_D1D = 0, int T_Q1D = 0> static
 void DLFEvalAssemble2D(const int vdim, const int ne, const int d, const int q,
                        const int map_type, const int *markers, const double *b,
-                       const double *j, const double *weights,
+                       const double *detj, const double *weights,
                        const Vector &coeff, double *y)
 {
    const auto F = coeff.Read();
    const auto M = Reshape(markers, ne);
    const auto B = Reshape(b, q, d);
-   const auto J = Reshape(j, q, q, ne);
+   const auto DETJ = Reshape(detj, q, q, ne);
    const auto W = Reshape(weights, q, q);
    const bool cst = coeff.Size() == vdim;
    const auto C = cst ? Reshape(F,vdim,1,1,1) : Reshape(F,vdim,q,q,ne);
@@ -55,7 +55,7 @@ void DLFEvalAssemble2D(const int vdim, const int ne, const int d, const int q,
          {
             MFEM_FOREACH_THREAD(y,y,q)
             {
-               const double detJ = (map_type == FiniteElement::VALUE) ? J(x,y,e) : 1.0;
+               const double detJ = (map_type == FiniteElement::VALUE) ? DETJ(x,y,e) : 1.0;
                const double coeff_val = cst ? cst_val : C(c,x,y,e);
                QQ(y,x) = W(x,y) * coeff_val * detJ;
             }
@@ -88,13 +88,13 @@ void DLFEvalAssemble2D(const int vdim, const int ne, const int d, const int q,
 template<int T_D1D = 0, int T_Q1D = 0> static
 void DLFEvalAssemble3D(const int vdim, const int ne, const int d, const int q,
                        const int map_type, const int *markers, const double *b,
-                       const double *j, const double *weights,
+                       const double *detj, const double *weights,
                        const Vector &coeff, double *y)
 {
    const auto F = coeff.Read();
    const auto M = Reshape(markers, ne);
    const auto B = Reshape(b, q,d);
-   const auto J = Reshape(j, q, q, q, ne);
+   const auto DETJ = Reshape(detj, q, q, q, ne);
    const auto W = Reshape(weights, q,q,q);
    const bool cst_coeff = coeff.Size() == vdim;
    const auto C = cst_coeff ? Reshape(F,vdim,1,1,1,1):Reshape(F,vdim,q,q,q,ne);
@@ -126,7 +126,7 @@ void DLFEvalAssemble3D(const int vdim, const int ne, const int d, const int q,
             {
                for (int z = 0; z < q; ++z)
                {
-                  const double detJ = (map_type == FiniteElement::VALUE) ? J(x,y,z,e) : 1.0;
+                  const double detJ = (map_type == FiniteElement::VALUE) ? DETJ(x,y,z,e) : 1.0;
                   const double coeff_val = cst_coeff ? cst_val : C(c,x,y,z,e);
                   QQQ(z,y,x) = W(x,y,z) * coeff_val * detJ;
                }
