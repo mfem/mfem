305466139fc75b3795cb2820c850747214c47d8e
Socratis Petrides <petrides1@llnl.gov>
2022-08-15 16:53:57 -0700

    cmakelist

--- linalg/operator.hpp
+++ linalg/operator.hpp
@@ -267,6 +267,7 @@ public:
       Complex_Operator, ///< ID for class ComplexOperator.
       MFEM_ComplexSparseMat, ///< ID for class ComplexSparseMatrix.
       Complex_Hypre_ParCSR   ///< ID for class ComplexHypreParMatrix.
+      Complex_DenseMat  ///< ID for class ComplexDenseMatrix
    };
 
    /// Return the type ID of the Operator class.
