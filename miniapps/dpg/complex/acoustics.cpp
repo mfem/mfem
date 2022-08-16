dbf709e3db848a1eff7722e62b2b96be428e9962
Socratis Petrides <petrides1@llnl.gov>
2022-08-16 14:59:16 -0700

    Setup directory stracture

---
+++ miniapps/dpg/complex/acoustics.cpp
@@ -0,0 +1,11 @@
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using namespace mfem;
+
+int main(int argc, char *argv[])
+{
+   return 0;
+}
---
+++ miniapps/dpg/real/pdiffusion.cpp
@@ -0,0 +1,12 @@
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using namespace mfem;
+
+int main(int argc, char *argv[])
+{
+   return 0;
+}
