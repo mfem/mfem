caa79bf80147c2b36b3f1070ec890221cbe2f11d
Yohann Dudouit <yohann.dudouit@gmail.com>
2022-08-31 17:17:18 -0700

    feedback from @pazner

--- fem/bilinearform.cpp
+++ fem/bilinearform.cpp
@@ -970,9 +970,6 @@ void BilinearForm::EliminateVDofs(const Array<int> &vdofs_,
                                   DiagonalPolicy dpolicy)
 {
    vdofs_.HostRead();
-   mat->HostReadWriteI();
-   mat->HostReadWriteJ();
-   mat->HostReadWriteData();
    for (int i = 0; i < vdofs_.Size(); i++)
    {
       int vdof = vdofs_[i];
@@ -996,9 +993,6 @@ void BilinearForm::EliminateVDofs(const Array<int> &vdofs_,
    }
 
    vdofs_.HostRead();
-   mat->HostReadWriteI();
-   mat->HostReadWriteJ();
-   mat->HostReadWriteData();
    for (int i = 0; i < vdofs_.Size(); i++)
    {
       int vdof = vdofs_[i];
