e2e34551b95d8d48e89ed3c06e49bf3c09bb85b9
Yohann Dudouit <yohann.dudouit@gmail.com>
2022-08-31 18:00:29 -0700

    Set `ntrials` to `1`.

--- tests/unit/fem/test_fa_determinism.cpp
+++ tests/unit/fem/test_fa_determinism.cpp
@@ -28,7 +28,7 @@ TEST_CASE("FA Determinism", "[PartialAssembly][CUDA]")
 
    Vector x, b_reference;
 
-   const int ntrials = 100 + 1;
+   const int ntrials = 1;
    for (int i = 0; i < ntrials; ++i)
    {
       BilinearForm a(&fes);
