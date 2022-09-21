9d86d75a301fbac7f83dc277c348db9336867c6a
Tzanio Kolev <tzanio@llnl.gov>
2022-09-21 12:50:10 -0700

    Remove config/Win32 (will be maintained externally)

--- config/Win32/fem_ceed_operator.cpp
+++
@@ -1,4 +0,0 @@
-// The current Windows build system cannot handle multiple files
-// with the same name in a project (operator.cpp), so this file is
-// used as a workaround.
-#include "../../fem/ceed/operator.cpp"
