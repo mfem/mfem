12dacdbe7b31002ed79568b902cc37d78282e7c8
Jacob Lotz <j.e.lotz@tudelft.nl>
2022-09-29 14:56:55 +0200

    Error should be absolute

--- mesh/nurbs.cpp
+++ mesh/nurbs.cpp
@@ -392,7 +392,7 @@ void KnotVector::Difference(const KnotVector &kv, Vector &diff) const
    int i = 0;
    for (int j = 0; j < kv.Size(); j++)
    {
-      if (knot(i) - kv[j] < std::numeric_limits<double>::epsilon())
+      if (abs(knot(i) - kv[j]) < std::numeric_limits<double>::epsilon())
       {
          i++;
       }
