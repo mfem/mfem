03eca785cfa277a4846b88c4567bde44431df9b2
Tobias Duswald <tobias.duswald@cern.ch>
2022-10-05 18:49:19 +0200

    Add boundary integration for verification

--- miniapps/materials/boundary.hpp
+++ miniapps/materials/boundary.hpp
@@ -26,17 +26,27 @@ struct Boundary {
 
   /// Print the information specifying the boundary conditions.
   void PrintInfo(std::ostream &os = mfem::out) const;
+  
   /// Verify that all defined boundaries are actually defined on the mesh, i.e.
   /// if the keys of boundary attributes appear in the boundary attributes of 
   /// the mesh.
   void VerifyDefinedBoundaries(const Mesh& mesh) const;
+
+  /// Computes the error for each defined boundary attribute by calling back to 
+  /// the IntegrateBC.
+  void ComputeBoundaryError(const ParGridFunction& solution);
+
+  /// Helper function to compute the coefficients alpha, beta, gamma (see in 
+  /// `IntegrateBC`) for a given boundary attribute.
+  void UpdateIntegrationCoefficients(int i, double& alpha, double& beta, 
+                                     double& gamma);
 
   /// Add a homogeneous boundary condition to the boundary.
   void AddHomogeneousBoundaryCondition(int boundary, BoundaryType type);  
 
   /// Add a inhomogeneous Dirichlet boundary condition to the boundary.
   void AddInhomogeneousDirichletBoundaryCondition(int boundary, 
-                                                  Coefficient* coefficient);
+                                                  double coefficient);
 
   /// Set the robin coefficient for the boundary.
   void SetRobinCoefficient(double coefficient);
@@ -44,11 +54,19 @@ struct Boundary {
   /// Map to assign homogeneous boundary conditions to defined boundary types.
   std::map<int, BoundaryType> boundary_attributes;
   /// Coefficient for inhomogeneous Dirichlet boundary conditions.
-  std::map<int, Coefficient*> dirichlet_coefficients;
+  std::map<int, double> dirichlet_coefficients;
   /// Coefficient for Robin boundary conditions (n.grad(u) + coeff u = 0) on 
   /// defined boundaries.
-  double robin_coefficient; 
+  double robin_coefficient = 1.0; 
 };
+
+/// IntegrateBC function from ex27p.cpp. For boundary verification.
+/// Compute the average value of alpha*n.Grad(sol) + beta*sol over the boundary
+/// attributes marked in bdr_marker. Also computes the L2 norm of
+/// alpha*n.Grad(sol) + beta*sol - gamma over the same boundary.
+double IntegrateBC(const ParGridFunction &x, const Array<int> &bdr,
+                   double alpha, double beta, double gamma,
+                   double &glb_err);
 
 }
 }
