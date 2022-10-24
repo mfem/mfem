bf993a5ab6894119225c9cb0485d2f2c9008ed0b
Jan Nikl <j.nikl@hzdr.de>
2022-10-24 16:37:59 +0200

    Added access to the underlaying functions of the function coefficients.

--- fem/coefficient.hpp
+++ fem/coefficient.hpp
@@ -256,6 +256,9 @@ public:
    /// Evaluate the coefficient at @a ip.
    real_t Eval(ElementTransformation &T,
                const IntegrationPoint &ip) override;
+
+   const std::function<real_t(const Vector &)>& GetFunction() const { return Function; }
+   const std::function<real_t(const Vector &,real_t)>& GetTDFunction() const { return TDFunction; }
 };
 
 /// A common base class for returning individual components of the domain's
@@ -767,6 +770,10 @@ public:
    /// Evaluate the vector coefficient at @a ip.
    void Eval(Vector &V, ElementTransformation &T,
              const IntegrationPoint &ip) override;
+
+   const std::function<void(const Vector &, Vector &)>& GetFunction() const { return Function; }
+   const std::function<void(const Vector &, real_t, Vector &)>& GetTDFunction()
+   const { return TDFunction; }
 
    virtual ~VectorFunctionCoefficient() { }
 };
