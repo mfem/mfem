40e1e7aade3c2db9feb3e08723ca3fb8d8897042
Vladimir Z Tomov <tomov2@llnl.gov>
2022-11-01 16:56:31 -0700

    wip degree elevation

--- miniapps/meshing/orders.cpp
+++ miniapps/meshing/orders.cpp
@@ -23,8 +23,14 @@
 using namespace mfem;
 using namespace std;
 
-void Untangle(ParGridFunction &x, double min_detA, int quad_order);
-void WorstCaseOptimize(ParGridFunction &x, int quad_order);
+double test_func(const Vector &coord)
+{
+   return sin(M_PI*coord(0)) * sin(2.0*M_PI*coord(1));
+}
+
+
+void OptimizeMesh(ParGridFunction &x,
+                  TMOP_QualityMetric &metric, int quad_order);
 
 int main (int argc, char *argv[])
 {
@@ -34,19 +40,12 @@ int main (int argc, char *argv[])
    Hypre::Init();
 
    // Set the method's default parameters.
-   const char *mesh_file = "jagged.mesh";
+   const char *mesh_file = "blade.mesh";
    int rs_levels         = 0;
    int mesh_poly_deg     = 2;
-   bool fdscheme         = false;
-   bool worst_case       = true;
    int solver_iter       = 50;
    int quad_order        = 8;
-   int bg_amr_steps      = 6;
-   double surface_fit_const = 10.0;
-   double surface_fit_adapt = 10.0;
-   double surface_fit_threshold = 1e-7;
    int metric_id         = 2;
-   int target_id         = 1;
 
    // Parse command-line input file.
    OptionsParser args(argc, argv);
@@ -57,26 +56,10 @@ int main (int argc, char *argv[])
                   "Number of times to refine the mesh uniformly in serial.");
    args.AddOption(&solver_iter, "-ni", "--newton-iters",
                   "Maximum number of Newton iterations.");
-   args.AddOption(&fdscheme, "-fd", "--fd_approx", "-no-fd", "--no-fd-approx",
-                  "Enable finite difference based derivative computations.");
-   args.AddOption(&worst_case, "-wc", "--worst-case",
-                               "-no-wc", "--no-worst-case",
-                  "Enable worst case optimization step.");
    args.AddOption(&quad_order, "-qo", "--quad_order",
                   "Order of the quadrature rule.");
-   args.AddOption(&bg_amr_steps, "-amr", "--amr-bg-steps",
-                  "Number of AMR steps on the background mesh.");
-   args.AddOption(&surface_fit_const, "-sfc", "--surface-fit-const",
-                  "Surface preservation constant.");
-   args.AddOption(&surface_fit_adapt, "-sfa", "--adaptive-surface-fit",
-                  "Enable or disable adaptive surface fitting.");
-   args.AddOption(&surface_fit_threshold, "-sft", "--surf-fit-threshold",
-                  "Set threshold for surface fitting. TMOP solver will"
-                  "terminate when max surface fitting error is below this limit");
    args.AddOption(&metric_id, "-mid", "--metric-id",
                   "Mesh optimization metric 1/2/50/58 in 2D:\n\t");
-   args.AddOption(&target_id, "-tid", "--target-id",
-                  "Mesh optimization metric 1/2/3 in 2D:\n\t");
    args.Parse();
    if (!args.Good())
    {
@@ -140,13 +123,8 @@ int main (int argc, char *argv[])
    }
    else { metric = new TMOP_Metric_302; }
 
-   TargetConstructor::TargetType target_t;
-   switch (target_id)
-   {
-   case 1: target_t = TargetConstructor::IDEAL_SHAPE_UNIT_SIZE; break;
-   case 2: target_t = TargetConstructor::IDEAL_SHAPE_EQUAL_SIZE; break;
-   case 3: target_t = TargetConstructor::IDEAL_SHAPE_GIVEN_SIZE; break;
-   }
+   TargetConstructor::TargetType target_t =
+         TargetConstructor::IDEAL_SHAPE_UNIT_SIZE;
    auto target_c = new TargetConstructor(target_t, MPI_COMM_WORLD);
    target_c->SetNodes(x0);
 
@@ -157,56 +135,21 @@ int main (int argc, char *argv[])
    }
 
    // If needed, perform worst-case optimization with fixed boundary.
-   OptimizeMesh(x, quad_order);
+   OptimizeMesh(x, *metric, quad_order);
 
-   // Visualize the starting mesh and metric values.
-   {
-      char title[] = "After Untangl / WC";
-      vis_tmop_metric_p(mesh_poly_deg, *metric, *target_c, *pmesh, title, 0);
-   }
-
-   if (fit_optimize == false) { return 0; }
-
-   // Detect boundary nodes.
-   Array<int> vdofs;
    ParFiniteElementSpace pfes_s(pmesh, &fec);
-   ParGridFunction domain(&pfes_s);
-   domain = 1.0;
-   for (int i = 0; i < pfes_s.GetNBE(); i++)
+   ParGridFunction g(&pfes_s);
+   FunctionCoefficient fc(test_func);
+   g.ProjectCoefficient(fc);
    {
-      pfes_s.GetBdrElementDofs(i, vdofs);
-      for (int j = 0; j < vdofs.Size(); j++) { domain(vdofs[j]) = 0.0; }
+      socketstream vis_b_func;
+      common::VisualizeField(vis_b_func, "localhost", 19916, g,
+                             "Dist on Background", 0, 700, 300, 300, "Rj");
    }
 
 #ifndef MFEM_USE_GSLIB
    MFEM_ABORT("GSLIB needed for this functionality.");
 #endif
-
-   // The background level set function is always linear to avoid oscillations.
-   H1_FECollection bg_fec(mesh_bg_curv, dim);
-   ParFiniteElementSpace bg_pfes(pmesh_bg, &bg_fec);
-   ParGridFunction bg_domain(&bg_pfes);
-
-
-   // Visualize something.
-   {
-      socketstream vis_b_func;
-      common::VisualizeField(vis_b_func, "localhost", 19916, bg_domain,
-                             "Dist on Background", 300, 700, 300, 300, "Rj");
-   }
-
-   // Background mesh FECollection, FESpace, and GridFunction
-   ParFiniteElementSpace *bg_grad_fes = NULL;
-   ParGridFunction *bg_grad = NULL;
-   ParFiniteElementSpace *bg_hess_fes = NULL;
-   ParGridFunction *bg_hess = NULL;
-
-   // If a background mesh is used, we interpolate the Gradient and Hessian
-   // from that mesh to the current mesh being optimized.
-   ParFiniteElementSpace *grad_fes = NULL;
-   ParGridFunction *surf_fit_grad = NULL;
-   ParFiniteElementSpace *surf_fit_hess_fes = NULL;
-   ParGridFunction *surf_fit_hess = NULL;
 
    // Visualize the final mesh and metric values.
    {
@@ -234,43 +177,25 @@ int main (int argc, char *argv[])
       }
    }
 
-   // 20. Free the used memory.
-   delete S;
-   //   delete metric_coeff1;
-   //   delete adapt_lim_eval;
-   //   delete adapt_surface;
    delete target_c;
-   //   delete adapt_coeff;
    delete metric;
-   delete surf_fit_hess;
-   delete surf_fit_hess_fes;
-   delete surf_fit_grad;
-   delete grad_fes;
-   delete bg_hess;
-   delete bg_hess_fes;
-   delete bg_grad;
-   delete bg_grad_fes;
-   delete pmesh_bg;
    delete pmesh;
 
    return 0;
 }
 
-void OptimizeMesh(ParGridFunction &x, int quad_order)
+void OptimizeMesh(ParGridFunction &x,
+                  TMOP_QualityMetric &metric, int quad_order)
 {
    ParFiniteElementSpace &pfes = *x.ParFESpace();
-   const int dim = pfes.GetParMesh()->Dimension();
 
    if (pfes.GetMyRank() == 0) { cout << "*** \nWorst Quality Phase\n***\n"; }
 
    // Metric / target / integrator.
-   TMOP_QualityMetric *metric = NULL;
-   if (dim == 2) { metric = new TMOP_Metric_002; }
-   else          { metric = new TMOP_Metric_304; }
    TargetConstructor::TargetType target =
          TargetConstructor::IDEAL_SHAPE_UNIT_SIZE;
    TargetConstructor target_c(target, pfes.GetComm());
-   auto tmop_integ = new TMOP_Integrator(metric, &target_c, nullptr);
+   auto tmop_integ = new TMOP_Integrator(&metric, &target_c, nullptr);
    tmop_integ->SetIntegrationRules(IntRulesLo, quad_order);
 
    // Nonlinear form.
@@ -307,8 +232,6 @@ void OptimizeMesh(ParGridFunction &x, int quad_order)
    Vector b;
    solver.Mult(b, x.GetTrueVector());
    x.SetFromTrueVector();
-
-   delete metric;
 
    return;
 }
