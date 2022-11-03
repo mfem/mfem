96391c1a6253495e183a396046a03b538a45bd54
Michael C Tyler Stees <stees1@llnl.gov>
2022-11-03 09:44:25 -0700

    Revert changes to pmesh-optimizer miniapp

--- miniapps/meshing/pmesh-optimizer.cpp
+++ miniapps/meshing/pmesh-optimizer.cpp
@@ -873,8 +873,7 @@ int main (int argc, char *argv[])
    // The small_phys_size is relevant only with proper normalization.
    if (normalization) { dist = small_phys_size; }
    ConstantCoefficient lim_coeff(lim_const);
-
-   if (lim_const != 0.0){ tmop_integ->EnableLimiting(x0, dist, lim_coeff); }
+   if (lim_const != 0.0) { tmop_integ->EnableLimiting(x0, dist, lim_coeff); }
 
    // Adaptive limiting.
    ParGridFunction adapt_lim_gf0(&ind_fes);
