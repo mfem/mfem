affa9185bd7eb93b10db3341de7adfbdc55c200f
Yohann Dudouit <dudouit1@llnl.gov>
2022-11-07 11:17:26 -0800

    Set the diagonal policy to `DIAG_ONE` in `ex1` and `ex1p`.

--- examples/ex1.cpp
+++ examples/ex1.cpp
@@ -196,6 +196,7 @@ int main(int argc, char *argv[])
    {
       a.SetAssemblyLevel(AssemblyLevel::FULL);
       a.EnableSparseMatrixSorting( Device::Allows(~Backend::CPU_MASK) );
+      a.SetDiagonalPolicy( Operator::DiagonalPolicy::DIAG_ONE );
    }
    a.AddDomainIntegrator(new DiffusionIntegrator(one));
 
