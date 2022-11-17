48533a819608600e43dee29ccfc1562c7a04c2ff
Aaron Fisher <fisher47@llnl.gov>
2022-11-17 15:52:48 -0800

    Added more mesh_connection methods.

--- mesh/mesh_connections.cpp
+++ mesh/mesh_connections.cpp
@@ -90,50 +90,74 @@ void MeshConnections::ConnectionsOfEntities(const EntityIndices entities,
 
 bool MeshConnections::IsChild(const EntityIndex &parent, const EntityIndex &child) const
 {
-
-   return false;
+   EntityIndices children;
+   children.kind = child.kind;
+   ChildrenOfEntity(parent, children);
+   return children.Find(child.index) > -1;
 }
 
 void MeshConnections::ChildrenOfEntity(const EntityIndex &parent,
                                        EntityIndices &children) const
 {
-
+   MFEM_ASSERT(parent.kind.dim > children.kind.dim, "Dimension of parent must be > child dimension.");
+   ConnectionsOfEntity(parent, children);
 }
 
 void MeshConnections::ChildrenOfEntities(const EntityIndices &parents,
                                          EntityIndices &children) const
 {
-
+   MFEM_ASSERT(parents.kind.dim > children.kind.dim, "Dimension of parent must be > child dimension.");
+   ConnectionsOfEntities(parents, children, false);
 }
 
 void MeshConnections::ParentsOfEntity(const EntityIndex &child,
                                       EntityIndices &parents) const
 {
-
+   MFEM_ASSERT(parents.kind.dim > child.kind.dim, "Dimension of parent must be > child dimension.");
+   ConnectionsOfEntity(child, parents);
 }
 
 void MeshConnections::ParentsOfAnyEntities(const EntityIndices &children,
                                            EntityIndices &parents) const
 {
-
+   MFEM_ASSERT(parents.kind.dim > children.kind.dim, "Dimension of parent must be > child dimension.");
+   ConnectionsOfEntities(children, parents, false);
 }
 
 void MeshConnections::ParentsCoveredByEntities(const EntityIndices &children,
                                                EntityIndices &parents) const
 {
-
+   MFEM_ASSERT(parents.kind.dim > children.kind.dim, "Dimension of parent must be > child dimension.");
+   ConnectionsOfEntities(children, parents, true);
 }
 
 void MeshConnections::NeighborsOfEntity(const EntityIndex &entity, EntityIndexKind shared_kind,
                                         EntityIndices &neighbors) const
 {
-
+   MFEM_ASSERT(entity.kind == neighbors.kind, "Neighbors entity must have the same kind.")
+   Table *t = GetNeighborTable(entity.kind, shared_kind);
+   t->GetRow(entity.index, neighbors.indices);
 }
 
 void MeshConnections::NeighborsOfEntities(const EntityIndices &entities, EntityIndexKind shared_kind, 
                                           EntityIndices &neighbors) const
 {
+   Table *t = GetTable(entities.kind, connected.kind);
 
+   //Find all the connected entities touched by the entities
+   //put it in a set to remove duplicates
+   std::set<int> touched_set;
+   for (int i = 0; i < entities.indices.Size(); ++i)
+   {
+      int row_sz = t->RowSize(entities.indices[i]);
+      const int *row = t->GetRow(entities.indices[i]);
+      for (int j = 0; j < row_sz; ++j)
+      {
+         touched_set.insert(row[j]);
+      }
+   }
+   neighbors.indices.SetSize(touched_set.size());
+   std::copy(touched_set.begin(), touched_set.end(), neighbors.indices.begin());   
 }
 
 int MeshConnections::GetAllIdxFromBdrIdx(int bdr_idx) const
