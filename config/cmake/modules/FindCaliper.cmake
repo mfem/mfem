09331236d2ee145e227ce7dedf434dbba6708e05
Kenneth Weiss <weiss27@llnl.gov>
2022-12-14 12:16:49 -0800

    Adds support for optional caliper dependencies in cmake build system
    If a user's caliper is built against `adiak` or `gotcha`, they can supply
    `ADIAK_DIR` and/or `GOTCHA_DIR` config variables.

--- config/cmake/modules/FindCaliper.cmake
+++ config/cmake/modules/FindCaliper.cmake
@@ -16,7 +16,28 @@
 
 include(MfemCmakeUtilities)
 mfem_find_package(Caliper CALIPER CALIPER_DIR
-       	"include" "caliper/cali.h"
-       	"lib" "caliper"
-  	"Paths to headers required by Caliper."
-       	"Libraries required by Caliper.")
+      "include" "caliper/cali.h"
+      "lib" "caliper"
+      "Paths to headers required by Caliper."
+      "Libraries required by Caliper.")
+
+# Append adiak path/lib if the user provided ADIAK_DIR
+if(ADIAK_DIR AND EXISTS ${ADIAK_DIR})
+    list(APPEND CALIPER_INCLUDE_DIRS ${ADIAK_DIR}/include)
+    
+    unset(_lib)
+    find_library(_lib "adiak" NO_CACHE PATHS ${ADIAK_DIR}/lib)
+    list(APPEND CALIPER_LIBRARIES ${_lib})
+endif()
+
+# Append gotcha path/lib if the user provided GOTCHA_DIR
+if(GOTCHA_DIR AND EXISTS ${GOTCHA_DIR})
+    list(APPEND CALIPER_INCLUDE_DIRS ${GOTCHA_DIR}/include)
+    
+    unset(_lib)
+    find_library(_lib "gotcha" NO_CACHE PATHS ${GOTCHA_DIR}/lib)
+    list(APPEND CALIPER_LIBRARIES ${_lib})
+endif()
+
+unset(_lib)
+
