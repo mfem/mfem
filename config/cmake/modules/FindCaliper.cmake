fcb9f6b4c26fe6cd6614cd9346856a74498532b3
Kenneth Weiss <weiss27@llnl.gov>
2022-12-21 10:29:46 -0800

    Use find_package for adiak and gotcha dependencies in CMake build system
    Per PR suggestion from Veselin.

--- config/cmake/modules/FindCaliper.cmake
+++ config/cmake/modules/FindCaliper.cmake
@@ -23,21 +23,15 @@ mfem_find_package(Caliper CALIPER CALIPER_DIR
 
 # Append adiak path/lib if the user provided ADIAK_DIR
 if(ADIAK_DIR AND EXISTS ${ADIAK_DIR})
-    list(APPEND CALIPER_INCLUDE_DIRS ${ADIAK_DIR}/include)
-    
-    unset(_lib)
-    find_library(_lib "adiak" NO_CACHE PATHS ${ADIAK_DIR}/lib)
-    list(APPEND CALIPER_LIBRARIES ${_lib})
+    find_package(adiak NO_DEFAULT_PATH REQUIRED PATHS ${ADIAK_DIR}/lib/cmake/adiak ${ADIAK_DIR})
+    list(APPEND CALIPER_INCLUDE_DIRS ${adiak_INCLUDE_DIRS})
+    list(APPEND CALIPER_LIBRARIES ${adiak_LIBRARIES})
 endif()
 
 # Append gotcha path/lib if the user provided GOTCHA_DIR
 if(GOTCHA_DIR AND EXISTS ${GOTCHA_DIR})
-    list(APPEND CALIPER_INCLUDE_DIRS ${GOTCHA_DIR}/include)
-    
-    unset(_lib)
-    find_library(_lib "gotcha" NO_CACHE PATHS ${GOTCHA_DIR}/lib)
-    list(APPEND CALIPER_LIBRARIES ${_lib})
+    find_package(gotcha NO_DEFAULT_PATH REQUIRED PATHS ${GOTCHA_DIR}/lib/cmake/gotcha ${GOTCHA_DIR})
+    list(APPEND CALIPER_INCLUDE_DIRS ${gotcha_INCLUDE_DIRS})
+    list(APPEND CALIPER_LIBRARIES ${gotcha_LIBRARIES})
 endif()
-
-unset(_lib)
 
