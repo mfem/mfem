d06b50b52c7be7e8f6a58d5f84c62f57c08fb25b
Robert Carson <carson16@llnl.gov>
2023-01-10 15:04:49 -0500

    Add magma support for libBatchSolver calls

--- linalg/libBatchSolver.cpp
+++ linalg/libBatchSolver.cpp
@@ -20,27 +20,41 @@ void LibBatchMult::Mult(const Vector &b, Vector &x)
 #if defined(MFEM_USE_CUDA) || defined(MFEM_USE_HIP)
    if (Device::Allows(Backend::DEVICE_MASK))
    {
+#if defined(MFEM_USE_MAGMA)
       const double alpha = 1.0, beta = 0.0;
-
-#if defined(MFEM_USE_HIP) || (defined(MFEM_USE_CUDA) && CUDA_VERSION >= 11070)
+#else
+      const double alpha_t = 1.0, beta_t = 0.0;
+      const double* alpha = &alpha_t;
+      const double* beta = &beta_t;
+#endif
+     
+#if defined(MFEM_USE_HIP) || (defined(MFEM_USE_CUDA) && CUDA_VERSION >= 11070) || defined(MFEM_USE_MAGMA)
+#if defined(MFEM_USE_MAGMA)
+      magmablas_dgemv_batched_strided(MagmaNoTrans,
+#else
       MFEM_SUB_cu_or_hip(blasStatus_t)
       status = MFEM_SUB_cu_or_hip(blasDgemvStridedBatched)(MFEM_SUB_Cuda_or_Hip(
-                                                              BLAS::Handle)(),
+                                                           BLAS::Handle)(),
                                                            MFEM_SUB_CU_or_HIP(BLAS_OP_N),
+#endif
                                                            mat_size,
                                                            mat_size,
-                                                           &alpha,
+                                                           alpha,
                                                            MatrixBatch.Read(),
                                                            mat_size,
                                                            mat_size * mat_size,
                                                            b.Read(),
                                                            1,
                                                            mat_size,
-                                                           &beta,
+                                                           beta,
                                                            x.Write(),
                                                            1,
                                                            mat_size,
-                                                           num_mats);
+                                                           num_mats
+#if defined(MFEM_USE_MAGMA)
+                                                           , queue
+#endif
+							   );
 #else
       MFEM_SUB_cu_or_hip(blasStatus_t)
       status = MFEM_SUB_cu_or_hip(blasDgemmStridedBatched)(MFEM_SUB_Cuda_or_Hip(
@@ -50,14 +64,14 @@ void LibBatchMult::Mult(const Vector &b, Vector &x)
                                                            mat_size,
                                                            1,
                                                            mat_size,
-                                                           &alpha,
+                                                           alpha,
                                                            MatrixBatch.Read(),
                                                            mat_size,
                                                            mat_size * mat_size,
                                                            b.Read(),
                                                            mat_size,
                                                            mat_size,
-                                                           &beta,
+                                                           beta,
                                                            x.Write(),
                                                            mat_size,
                                                            mat_size,
@@ -66,8 +80,10 @@ void LibBatchMult::Mult(const Vector &b, Vector &x)
 
 #endif
 
+#if !defined(MFEM_USE_MAGMA)
       MFEM_VERIFY(status == MFEM_SUB_CU_or_HIP(BLAS_STATUS_SUCCESS),
                   "blasDgemvStridedBatched");
+#endif
    }
    else
 #endif
