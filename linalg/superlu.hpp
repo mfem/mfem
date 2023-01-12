fcb0f8da2d14ca86d0abb0d70aca2e5a10eecf60
Sebastian Grimberg <sjg@amazon.com>
2023-01-12 10:05:11 -0800

    Fix missed transpose application

--- linalg/superlu.hpp
+++ linalg/superlu.hpp
@@ -109,7 +109,7 @@ public:
    // Factor and solve the linear system y = Op^{-T} x.
    // Note: Factorization modifies the operator matrix.
    void MultTranspose(const Vector &x, Vector &y) const;
-   void MultTranspose(const Array<const Vector *> &X, Array<Vector *> &Y) const;
+   void ArrayMultTranspose(const Array<const Vector *> &X, Array<Vector *> &Y) const;
 
    // Set various solver options. Refer to SuperLU_DIST documentation for
    // details.
