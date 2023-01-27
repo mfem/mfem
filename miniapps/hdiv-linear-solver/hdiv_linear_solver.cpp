379eaa343cd2508ef9eb63656ab49d1d715a84fc
Will Pazner <will.e.p@gmail.com>
2023-01-26 19:19:11 -0800

    Add H(div) saddle-point solver miniapps
    Included are grad-div and Darcy/Poisson miniapps.

---
+++ miniapps/hdiv-linear-solver/hdiv_linear_solver.cpp
@@ -0,0 +1,288 @@
+// Copyright (c) 2010-2022, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#include "general/forall.hpp"
+#include "hdiv_linear_solver.hpp"
+#include "discrete_divergence.hpp"
+
+namespace mfem
+{
+
+/// Modify diag_vec to hold its reciprocal, and return a new HypreParMatrix
+/// with those entrirs on the diagonal.
+HypreParMatrix *DiagonalInverse(
+   Vector &diag_vec, const ParFiniteElementSpace &fes)
+{
+   diag_vec.HostReadWrite();
+   for (int i=0; i<diag_vec.Size(); ++i) { diag_vec[i] = 1.0/diag_vec[i]; }
+   SparseMatrix diag_inv(diag_vec);
+
+   HYPRE_BigInt global_size = fes.GlobalTrueVSize();
+   HYPRE_BigInt *row_starts = fes.GetTrueDofOffsets();
+   HypreParMatrix D(MPI_COMM_WORLD, global_size, row_starts, &diag_inv);
+   return new HypreParMatrix(D); // make a deep copy
+}
+
+const IntegrationRule &GetMassIntRule(FiniteElementSpace &fes_l2)
+{
+   Mesh *mesh = fes_l2.GetMesh();
+   const FiniteElement *fe = fes_l2.GetFE(0);
+   return MassIntegrator::GetRule(*fe, *fe, *mesh->GetElementTransformation(0));
+}
+
+HdivSaddlePointSolver::HdivSaddlePointSolver(
+   ParMesh &mesh, ParFiniteElementSpace &fes_rt_, ParFiniteElementSpace &fes_l2_,
+   Coefficient &L_coeff_, Coefficient &R_coeff_, const Array<int> &ess_rt_dofs_,
+   Mode mode_)
+   : minres(mesh.GetComm()),
+     order(fes_rt_.GetMaxElementOrder()),
+     fec_l2(order - 1, mesh.Dimension(), b2, mt),
+     fes_l2(&mesh, &fec_l2),
+     fec_rt(order - 1, mesh.Dimension(), b1, b2),
+     fes_rt(&mesh, &fec_rt),
+     ess_rt_dofs(ess_rt_dofs_),
+     basis_l2(fes_l2_, fes_l2),
+     basis_rt(fes_rt_, fes_rt),
+     mass_l2(&fes_l2),
+     mass_rt(&fes_rt),
+     L_coeff(L_coeff_),
+     R_coeff(R_coeff_),
+     mode(mode_),
+     qs(mesh, GetMassIntRule(fes_l2)),
+     qf(qs),
+     l2_qf_coeff(qf)
+{
+   // If the user gives zero L coefficient, switch mode to DARCY_ZERO
+   auto *L_const_coeff = dynamic_cast<ConstantCoefficient*>(&L_coeff);
+   zero_l2_block = (L_const_coeff && L_const_coeff->constant == 0.0);
+
+   if (mode == Mode::GRAD_DIV)
+   {
+      MFEM_VERIFY(!zero_l2_block,
+                  "Mode::GRAD_DIV incompatible with zero coefficient.");
+   }
+
+   mass_l2.AddDomainIntegrator(new MassIntegrator(l2_qf_coeff));
+   mass_l2.SetAssemblyLevel(AssemblyLevel::PARTIAL);
+
+   mass_rt.AddDomainIntegrator(new VectorFEMassIntegrator(&R_coeff));
+   mass_rt.SetAssemblyLevel(AssemblyLevel::PARTIAL);
+
+   D.reset(FormDiscreteDivergenceMatrix(fes_rt, fes_l2, ess_rt_dofs));
+   Dt.reset(D->Transpose());
+
+   // Versions without BCs needed for elimination
+   D_e.reset(FormDiscreteDivergenceMatrix(fes_rt, fes_l2, empty));
+   mass_rt.FormSystemMatrix(empty, R_e);
+
+   offsets.SetSize(3);
+   offsets[0] = 0;
+   offsets[1] = fes_l2.GetTrueVSize();
+   offsets[2] = offsets[1] + fes_rt.GetTrueVSize();
+
+   minres.SetAbsTol(0.0);
+   minres.SetRelTol(1e-12);
+   minres.SetMaxIter(500);
+   minres.SetPrintLevel(IterativeSolver::PrintLevel().None());
+   minres.iterative_mode = false;
+
+   R_diag.SetSize(fes_rt.GetTrueVSize());
+   L_diag.SetSize(fes_l2.GetTrueVSize());
+
+   S_inv.SetPrintLevel(0);
+
+   Setup();
+}
+
+HdivSaddlePointSolver::HdivSaddlePointSolver(
+   ParMesh &mesh, ParFiniteElementSpace &fes_rt_, ParFiniteElementSpace &fes_l2_,
+   Coefficient &R_coeff_, const Array<int> &ess_rt_dofs_)
+   : HdivSaddlePointSolver(mesh, fes_rt_, fes_l2_, zero, R_coeff_, ess_rt_dofs_, Mode::DARCY)
+{ }
+
+void HdivSaddlePointSolver::Setup()
+{
+   if (!zero_l2_block)
+   {
+      L_coeff.Project(qf);
+      if (mode == Mode::DARCY)
+      {
+         L_coeff.Project(qf);
+         // Compute l2_qf_coeff, which is the reciprocal of L_inv.
+         // The data is stored in the QuadratureFunction qf.
+         double *qf_d = qf.ReadWrite();
+         MFEM_FORALL(i, qf.Size(), {
+            qf_d[i] = 1.0/qf_d[i];
+         });
+      }
+      // Form the DG mass inverse with the new coefficient
+      L_inv_1.reset(new DGMassInverse(fes_l2, l2_qf_coeff));
+   }
+
+   if (mode == Mode::GRAD_DIV) { L_inv_2 = L_inv_1; }
+   else { L_inv_2.reset(new DGMassInverse(fes_l2)); }
+
+   // Reassemble the L2 mass diagonal with the new coefficient
+   mass_l2.Assemble();
+   mass_l2.AssembleDiagonal(L_diag);
+   mass_l2.FormSystemMatrix(empty, L);
+
+   // Reassmble the RT mass operator with the new coefficient
+   mass_rt.Update();
+   mass_rt.Assemble();
+   mass_rt.FormSystemMatrix(ess_rt_dofs, R);
+
+   // Form the updated approximate Schur complement
+   mass_rt.AssembleDiagonal(R_diag);
+
+   {
+      const int *d_I = ess_rt_dofs.Read();
+      double *d_R_diag = R_diag.ReadWrite();
+      MFEM_FORALL(i, ess_rt_dofs.Size(), d_R_diag[d_I[i]] = 1.0;);
+   }
+
+   // Form the approximate Schur complement
+   {
+      std::unique_ptr<HypreParMatrix> R_diag_inv(DiagonalInverse(R_diag, fes_rt));
+      if (zero_l2_block)
+      {
+         S.reset(RAP(R_diag_inv.get(), Dt.get()));
+      }
+      else
+      {
+         std::unique_ptr<HypreParMatrix> D_Minv_Dt(RAP(R_diag_inv.get(), Dt.get()));
+         std::unique_ptr<HypreParMatrix> L_diag_inv(DiagonalInverse(L_diag, fes_l2));
+         S.reset(ParAdd(D_Minv_Dt.get(), L_diag_inv.get()));
+      }
+   }
+
+   // Reassemble the preconditioners
+   R_inv.reset(new OperatorJacobiSmoother(mass_rt, ess_rt_dofs));
+   S_inv.SetOperator(*S);
+
+   // Set up the block operators
+   A_block.reset(new BlockOperator(offsets));
+   // Skip L2 mass term if coefficient is zero
+   if (!zero_l2_block)
+   {
+      A_block->SetBlock(0, 0, L_inv_1.get());
+   }
+   A_block->SetBlock(0, 1, D.get());
+   A_block->SetBlock(1, 0, Dt.get());
+   A_block->SetBlock(1, 1, R.Ptr(), -1.0);
+
+   D_prec.reset(new BlockDiagonalPreconditioner(offsets));
+   D_prec->SetDiagonalBlock(0, &S_inv);
+   D_prec->SetDiagonalBlock(1, R_inv.get());
+
+   minres.SetPreconditioner(*D_prec);
+   minres.SetOperator(*A_block);
+}
+
+void HdivSaddlePointSolver::EliminateBC(Vector &b) const
+{
+   const int n_ess_dofs = ess_rt_dofs.Size();
+   if (n_ess_dofs == 0) { return; }
+
+   const int n_l2 = offsets[1];
+   const int n_rt = offsets[2]-offsets[1];
+   Vector bE(b, 0, n_l2);
+   Vector bF(b, n_l2, n_rt);
+
+   // SetBC must be called first
+   MFEM_VERIFY(x_bc.Size() == n_rt, "BCs not set");
+
+   // Create a vector z that has the BC values at essential DOFs, zero elsewhere
+   z.SetSize(n_rt);
+   z.UseDevice(true);
+   z = 0.0;
+   const int *d_I = ess_rt_dofs.Read();
+   const double *d_x_bc = x_bc.Read();
+   double *d_z = z.ReadWrite();
+   MFEM_FORALL(i, n_ess_dofs,
+   {
+      const int j = d_I[i];
+      d_z[j] = d_x_bc[j];
+   });
+
+   // Convert to the IntegratedGLL basis used internally
+   w.SetSize(n_rt);
+   basis_rt.MultInverse(z, w);
+
+   // Eliminate the BCs in the L2 RHS
+   D_e->Mult(-1.0, w, 1.0, bE);
+
+   // Eliminate the BCs in the RT RHS
+   // Flip the sign because the R block appears with multiplier -1
+   z.SetSize(n_rt);
+   R_e->Mult(w, z);
+   bF += z;
+
+   // Insert the RT BCs into the RHS at the essential DOFs.
+   const double *d_w = w.Read();
+   double *d_bF = bF.ReadWrite(); // Need read-write access to set subvector
+   MFEM_FORALL(i, n_ess_dofs,
+   {
+      const int j = d_I[i];
+      d_bF[j] = -d_w[j];
+   });
+
+   // Make sure the monolithic RHS is updated
+   bE.SyncAliasMemory(b);
+   bF.SyncAliasMemory(b);
+}
+
+void HdivSaddlePointSolver::Mult(const Vector &b, Vector &x) const
+{
+   b_prime.SetSize(b.Size());
+   x_prime.SetSize(x.Size());
+
+   // Transform RHS to the IntegratedGLL basis
+   Vector bE_prime(b_prime, offsets[0], offsets[1]-offsets[0]);
+   Vector bF_prime(b_prime, offsets[1], offsets[2]-offsets[1]);
+
+   const Vector bE(const_cast<Vector&>(b), offsets[0], offsets[1]-offsets[0]);
+   const Vector bF(const_cast<Vector&>(b), offsets[1], offsets[2]-offsets[1]);
+
+   z.SetSize(bE.Size());
+   basis_l2.MultTranspose(bE, z);
+   basis_rt.MultTranspose(bF, bF_prime);
+   // Transform by the inverse of the L2 mass matrix
+   L_inv_2->Mult(z, bE_prime);
+
+   // Update the monolithic transformed RHS
+   bE_prime.SyncAliasMemory(b_prime);
+   bF_prime.SyncAliasMemory(b_prime);
+
+   // Eliminate the RT essential BCs
+   EliminateBC(b_prime);
+
+   // Solve the transformed system
+   minres.Mult(b_prime, x_prime);
+
+   // Transform the solution back to the user's basis
+   Vector xE_prime(x_prime, offsets[0], offsets[1]-offsets[0]);
+   Vector xF_prime(x_prime, offsets[1], offsets[2]-offsets[1]);
+
+   Vector xE(x, offsets[0], offsets[1]-offsets[0]);
+   Vector xF(x, offsets[1], offsets[2]-offsets[1]);
+
+   L_inv_2->Mult(xE_prime, z);
+
+   basis_l2.Mult(z, xE);
+   basis_rt.Mult(xF_prime, xF);
+
+   // Update the monolithic solution vector
+   xE.SyncAliasMemory(x);
+   xF.SyncAliasMemory(x);
+}
+
+} // namespace mfem
---
+++ miniapps/hdiv-linear-solver/hdiv_linear_solver.hpp
@@ -0,0 +1,172 @@
+// Copyright (c) 2010-2022, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#ifndef LINEAR_SOLVER_HPP
+#define LINEAR_SOLVER_HPP
+
+#include "mfem.hpp"
+#include "change_basis.hpp"
+#include <memory>
+
+namespace mfem
+{
+
+/// @brief Solve the H(div) saddle-point system using MINRES with matrix-free
+/// block-diagonal preconditioning.
+///
+/// See HdivSaddlePointSolver::HdivSaddlePointSolver for the problem
+/// description.
+class HdivSaddlePointSolver : public Solver
+{
+public:
+   enum Mode
+   {
+      GRAD_DIV,
+      DARCY
+   };
+private:
+   MINRESSolver minres;
+
+   static constexpr int b1 = BasisType::GaussLobatto;
+   static constexpr int b2 = BasisType::IntegratedGLL;
+   static constexpr int mt = FiniteElement::INTEGRAL;
+
+   const int order;
+
+   // L2 and RT spaces, using the interpolation-histopolation bases
+   L2_FECollection fec_l2;
+   ParFiniteElementSpace fes_l2;
+
+   RT_FECollection fec_rt;
+   ParFiniteElementSpace fes_rt;
+
+   const Array<int> &ess_rt_dofs; ///< Essential BCs (in the RT space only).
+
+   // Change of basis operators
+   ChangeOfBasis_L2 basis_l2;
+   ChangeOfBasis_RT basis_rt;
+
+   ParBilinearForm mass_l2, mass_rt;
+
+   // Components needed for the block operator
+   OperatorHandle L, R, R_e; ///< Mass matrices.
+   std::unique_ptr<HypreParMatrix> D, Dt, D_e; ///< Divergence matrices.
+   std::shared_ptr<DGMassInverse> L_inv_1; ///< Inverse of the DG mass matrix.
+   std::shared_ptr<DGMassInverse> L_inv_2;
+
+   /// Diagonals of the mass matrices
+   Vector L_diag, R_diag;
+
+   // Components needed for the preconditioner
+
+   /// Jacobi preconditioner for the RT mass matrix.
+   std::unique_ptr<OperatorJacobiSmoother> R_inv;
+   std::unique_ptr<HypreParMatrix> S; ///< Approximate Schur complement.
+   HypreBoomerAMG S_inv; ///< AMG preconditioner for #S.
+
+   Array<int> offsets, empty;
+   /// The 2x2 block operator.
+   std::unique_ptr<BlockOperator> A_block;
+   /// The block-diagonal preconditioner.
+   std::unique_ptr<BlockDiagonalPreconditioner> D_prec;
+
+   Coefficient &L_coeff, &R_coeff;
+
+   const Mode mode;
+   bool zero_l2_block = false;
+   QuadratureSpace qs;
+   QuadratureFunction qf;
+   QuadratureFunctionCoefficient l2_qf_coeff;
+
+   ConstantCoefficient zero = ConstantCoefficient(0.0);
+
+   // Work vectors
+   mutable Vector b_prime, x_prime, x_bc, w, z;
+public:
+   /// @brief Creates a solver for the H(div) saddle-point system.
+   ///
+   /// The associated matrix is given by
+   ///
+   ///     [  L    B ]
+   ///     [ B^T  -R ]
+   ///
+   /// where L is the L2 mass matrix, R is the RT mass matrix, and B is the
+   /// divergence.
+   ///
+   /// Essential boundary conditions in the RT space are given by @a
+   /// ess_rt_dofs_. (Rows and columns are eliminated from R and columns are
+   /// eliminated from B).
+   ///
+   /// The L block has coefficient @a L_coeff_ and the  R block has coefficient
+   /// @a R_coeff_.
+   ///
+   /// If the @a mode_ is Mode::DARCY_ZERO, then the L block is replaced by zero
+   /// (in this case, @a L_coeff_ should be a zero constant coefficient for
+   /// consistency, see also @link HdivSaddlePointSolver(ParMesh&,
+   /// ParFiniteElementSpace&, ParFiniteElementSpace&, Coefficient&, const
+   /// Array<int>&) the other HdivSaddlePointSolver constructor @endlink).
+   ///
+   /// If @a mode_ is Mode::GRAD_DIV, then the matrices B and B^T are also
+   /// weighted by the coefficient L_coeff_. This corresponds to the grad-div
+   /// problem
+   ///
+   ///     alpha u - grad ( beta div ( u )),
+   ///
+   /// where alpha is @a R_coeff_ and beta is @a L_coeff_.
+   ///
+   /// If @a mode_ is Mode::DARCY or Mode::DARCY_NONZERO, then B and B^T are
+   /// unweighted, which corresponds to the Darcy-type problem
+   ///
+   ///    alpha p - div ( beta grad ( p ))
+   ///
+   /// where alpha is @a L_coeff and beta is @a R_coeff_. This requires
+   /// internally taking the reciprocal of these coefficients.
+   HdivSaddlePointSolver(ParMesh &mesh_,
+                               ParFiniteElementSpace &fes_rt_,
+                               ParFiniteElementSpace &fes_l2_,
+                               Coefficient &L_coeff_,
+                               Coefficient &R_coeff_,
+                               const Array<int> &ess_rt_dofs_,
+                               Mode mode_);
+
+   /// @brief Creates a linear solver for the case when the L2 diagonal block is
+   /// zero (mode Mode::DARCY_ZERO).
+   HdivSaddlePointSolver(ParMesh &mesh_,
+                               ParFiniteElementSpace &fes_rt_,
+                               ParFiniteElementSpace &fes_l2_,
+                               Coefficient &R_coeff_,
+                               const Array<int> &ess_rt_dofs_);
+
+   /// @brief Build the linear operator and solver. Must be called when the
+   /// coefficients change.
+   void Setup();
+   /// Sets the Dirichlet boundary conditions at the RT essential DOFs.
+   void SetBC(const Vector &x_rt) { x_bc = x_rt; }
+   /// @brief Solve the linear system for L2 (scalar) and RT (flux) unknowns.
+   ///
+   /// If the problem has essential boundary conditions (i.e. if @a ess_rt_dofs
+   /// is not empty), then SetBC() must be called before Mult().
+   void Mult(const Vector &b, Vector &x) const override;
+   /// No-op.
+   void SetOperator(const Operator &op) override { }
+   /// Get the number of MINRES iterations.
+   int GetNumIterations() const { return minres.GetNumIterations(); }
+   /// Eliminates the BCs (called internally, not public interface).
+   void EliminateBC(Vector &) const;
+   /// Return the offsets of the block system.
+   const Array<int> &GetOffsets() const { return offsets; }
+   /// Returns the internal MINRES solver.
+   MINRESSolver &GetMINRES() { return minres; }
+};
+
+} // namespace mfem
+
+#endif
