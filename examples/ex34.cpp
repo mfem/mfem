b0360d7438737174f5fb48e197848135db327924
Dohyun Kim <dohyun_kim@brown.edu>
2023-02-10 16:11:54 -0500

    create a new test

---
+++ examples/ex34.cpp
@@ -0,0 +1,34 @@
+//                                MFEM Example 0
+//
+// Compile with: make ex0
+//
+// Sample runs:  ex0
+//               ex0 -m ../data/fichera.mesh
+//               ex0 -m ../data/square-disc.mesh -o 2
+//
+// Description: This example code demonstrates the most basic usage of MFEM to
+//              define a simple finite element discretization of the Laplace
+//              problem -Delta u = 1 with zero Dirichlet boundary conditions.
+//              General 2D/3D mesh files and finite element polynomial degrees
+//              can be specified by command line options.
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+#include "ex34.hpp"
+
+using namespace std;
+using namespace mfem;
+
+int main(int argc, char *argv[])
+{
+   // 1. Parse command line options.
+   const char *mesh_file = "../data/star.mesh";
+   int order = 1;
+
+   OptionsParser args(argc, argv);
+   args.AddOption(&mesh_file, "-m", "--mesh", "Mesh file to use.");
+   args.AddOption(&order, "-o", "--order", "Finite element polynomial degree");
+
+   return 0;
+}
---
+++ examples/ex34.hpp
@@ -0,0 +1,6 @@
+//                  MFEM Example 18 - Serial/Parallel Shared Code
+
+#include "mfem.hpp"
+
+using namespace std;
+using namespace mfem;
