e83bd2cc558d21f94d77a6ba8e72f1ce3a3dd7d1
camierjs <camier1@llnl.gov>
2023-03-25 15:31:58 -0700

    mdspan general header and unit tests

---
+++ general/mdspan.hpp
@@ -0,0 +1,415 @@
+// Copyright (c) 2010-2023, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#ifndef MFEM_MDSPAN_HPP
+#define MFEM_MDSPAN_HPP
+
+#include <list>
+#include <array>
+#include <vector>
+
+#include "device.hpp"
+#include "backends.hpp"
+
+namespace mfem
+{
+
+using md_size_t = int; // could be configured from MFEM config
+
+namespace internal // experimental helper functions for mfem::MDSpan
+{
+
+// md_sequence represents a compile-time sequence of integers
+template <typename T, T... args> struct md_sequence { };
+
+template <typename T, md_size_t N, bool left> struct make_md_sequence;
+
+// make_sequence, specialized for left (default) and right layout
+template <typename T, md_size_t N, bool left = true>
+using make_sequence = typename make_md_sequence<T, N, left>::type;
+
+} // namespace internal
+
+/// @brief The MDOffset class computes the multi-dimensional offsets
+template <md_size_t n, md_size_t N, typename T, typename... Ts>
+struct MDOffset
+{
+   static MFEM_HOST_DEVICE inline
+   T offset(const md_size_t Sd[N], const T nd, Ts... args)
+   { return nd * Sd[n-1] + MDOffset<n+1, N, Ts...>::offset(Sd, args...); }
+};
+
+template <md_size_t N, typename T, typename... Ts>
+struct MDOffset<N, N, T, Ts...>
+{
+   static MFEM_HOST_DEVICE inline
+   T offset(const md_size_t Sd[N], const T nd) { return nd * Sd[N-1]; }
+};
+
+/// @brief The MDTensor class holds a pointer and strides for each dimension
+template<md_size_t N, typename T = double>
+class MDTensor
+{
+   T *ptr;
+   md_size_t Sd[N];
+
+public:
+   /// Default constructor
+   MDTensor() = delete;
+
+   /// Copy constructor (default)
+   MDTensor(const MDTensor&) = default;
+
+   /// Copy assignment (default)
+   MDTensor& operator=(const MDTensor&) = default;
+
+   /// Constructor to initialize a tensor from a pointer and strides
+   template <typename... Args> MFEM_HOST_DEVICE
+   MDTensor(T* ptr, const md_size_t (&sd)[N]): ptr(ptr)
+   { for (md_size_t i = 0; i < N; ++i) { Sd[i] = sd[i]; } }
+
+   /// Accessor for the data
+   template <typename... Ts> MFEM_HOST_DEVICE inline
+   T& operator()(Ts... args) { return ptr[Offset(args...)]; }
+
+   /// Const accessor for the data
+   template <typename... Ts> MFEM_HOST_DEVICE inline
+   T& operator()(Ts... args) const { return ptr[Offset(args...)]; }
+
+   /// Offset computation
+   template <typename... Ts> MFEM_HOST_DEVICE inline
+   md_size_t Offset(Ts... args) const
+   {
+      static_assert(sizeof...(args) == N, "Wrong number of dimensions");
+      return MDOffset<1, N, Ts...>::offset(Sd, args...);
+   }
+};
+
+/// \brief The MDLayout class is by default an column-major (Fortran) ordering
+template<md_size_t N, bool left = true> struct MDLayout
+{
+   /// Create a layout through the internal::md_sequence
+   template <md_size_t... args>
+   static constexpr auto Make(internal::md_sequence<md_size_t, args...>)
+   -> std::array<md_size_t, sizeof...(args)>
+   { return {(static_cast<md_size_t>(args))...}; }
+
+   /// Array holding the permutation set for the layout
+   using perm_type = std::array<md_size_t, N>;
+   perm_type perm = Make(internal::make_sequence<md_size_t, N, left> {});
+
+   /// Default constructor
+   MDLayout() = default;
+
+   /// Copy constructor (default)
+   MDLayout(const MDLayout&) = default;
+
+   /// Copy assignment (default)
+   MDLayout& operator=(const MDLayout&) = default;
+
+   /// Constructor to initialize a layout from an array of indices
+   template <typename... Ts>
+   MDLayout(md_size_t n, Ts... args) noexcept: MDLayout(args...)
+   {
+      constexpr md_size_t k = N - sizeof...(args) - 1;
+      static_assert(0 <= k && k < N, "Index out of bounds!");
+      perm[k] = n;
+   }
+
+   /// Access layout entries using operator()
+   inline md_size_t operator()(md_size_t i) const
+   { return Assert(i), perm[static_cast<typename perm_type::size_type>(i)]; }
+
+   /// Access layout entries using operator[]
+   inline md_size_t operator[](md_size_t i) const
+   { return Assert(i), perm[static_cast<typename perm_type::size_type>(i)]; }
+
+   /// Asserts the given index is valid (only in MFEM_DEBUG)
+   inline void Assert(const md_size_t k) const
+   {
+      MFEM_CONTRACT_VAR(k);
+      MFEM_ASSERT(0 <= k && k < N, "Index should be in [0," << (N-1) << "]");
+   }
+};
+
+/// Left (Column-major (Fortran)) and Right (Row-major (C/C++)) layouts
+template<md_size_t N> using MDLayoutLeft = MDLayout<N, true>;
+template<md_size_t N> using MDLayoutRight = MDLayout<N, false>;
+
+/// \brief The MDSpan base class is a generic non-owning mfem_type's view
+/// that reinterprets it as a multidimensional type.
+/// It is suited for mfem::Array, mfem::Vector and mfem::GridFunction which all
+/// require mfem_type::data and mfem_type::size to exist;
+///
+/// Possible typical use cases are used in tests/unit/general/test_mdspan.cpp.
+template<typename mfem_type, typename T, md_size_t N, class layout_type = MDLayoutLeft<N>>
+class MDSpan : public mfem_type
+{
+protected:
+   md_size_t Nd[N], // sizes of the dimensions
+             Sd[N]; // strides, once the layout has been set
+   layout_type layout; // stored layout, useful for reshapes
+
+   /// Setup sets the dimensions (Nd) and strides (Sd) during the contruction.
+   /// When all the arguments have been processed, SetSize is called on the
+   /// mfem_type with Device::GetMemoryType() as memory type and SetLayout is
+   /// called using the layout.
+   template <typename... Ts> void Setup(md_size_t dim, Ts... args)
+   {
+      constexpr md_size_t k = N - sizeof...(args) - 1;
+      Sd[k] = Nd[k] = dim;
+      if (k > 0) { return; }
+      md_size_t psize = 1;
+      for (md_size_t i = 0; i < N; i++) { psize *= Nd[i]; }
+      mfem_type::SetSize(static_cast<int>(psize), Device::GetMemoryType());
+      SetLayout(layout);
+   }
+
+public:
+
+   /// Default constructor (recursion)
+   MDSpan() noexcept: mfem_type() { }
+
+   /// Recursion constructor
+   template <typename... Ts>
+   MDSpan(md_size_t n, Ts... args): MDSpan(args...) { Setup(n, args...); }
+
+   /// Move constructor (delete)
+   MDSpan(MDSpan&&) = delete;
+
+   /// Copy constructor (delete)
+   MDSpan(const MDSpan&) = delete;
+
+   /// Copy assignment (delete)
+   MDSpan& operator=(MDSpan&&) = delete;
+
+   /// Return the ith dimension
+   md_size_t Extent(md_size_t i) const { return Nd[i]; }
+
+   /// Store and use the given layout to update the strides
+   template<typename Layout> void SetLayout(const Layout &l)
+   {
+      layout = l;
+      Sd[l[0]] = 1;
+      for (md_size_t i = 1; i < N; i++) { Sd[l[i]] = Nd[l[i-1]] * Sd[l[i-1]]; }
+   }
+
+   /// Variadic resize the mfem_type
+   template <typename... Ts> inline void SetSize(md_size_t size, Ts... args)
+   {
+      constexpr md_size_t k = N - sizeof...(args) - 1;
+      Sd[k] = Nd[k] = size;
+      const md_size_t msize = mfem_type::Size();
+      MFEM_VERIFY(size > 0, "Size should be positive!");
+      mfem_type::SetSize(msize > 0 ? msize*size : size, Device::GetMemoryType());
+      MDSpan::SetSize(args...);
+   }
+
+   /// Variadic terminal case of the mfem_type resize
+   inline void SetSize(md_size_t size)
+   {
+      Sd[N-1] = Nd[N-1] = size;
+      const md_size_t msize = mfem_type::Size();
+      MFEM_VERIFY(size > 0, "Size should be positive!");
+      mfem_type::SetSize(msize > 0 ? msize*size : size, Device::GetMemoryType());
+      SetLayout(layout);
+   }
+
+   /// Access mfem_type entries using operator()
+   template <typename... Ts> inline
+   T& operator()(Ts... args) { return mfem_type::data[Offset(args...)]; }
+
+   /// Const access mfem_type entries using operator()
+   template <typename... Ts> inline const T& operator()(Ts... args) const
+   {
+      return mfem_type::data[Offset(args...)];
+   }
+
+   /// Offset computation
+   template <typename... Ts> inline md_size_t Offset(Ts... args) const
+   {
+      static_assert(sizeof...(args) == N, "Wrong number of dimensions");
+      return MDOffset<1,N,Ts...>::offset(Sd, args...);
+   }
+
+   /// Shortcut for mfem::Read(mfem_type::data, mfem_type::size, on_dev)
+   /// and return a MDTensor with the MDSpan's strides and layout
+   const MDTensor<N,const double> MDRead(bool on_dev = true) const
+   {
+      double *ptr = mfem::Read(mfem_type::data, mfem_type::size, on_dev);
+      return MDTensor<N,const double>(ptr, Sd);
+   }
+
+   /// Shortcut for mfem::Read(mfem_type::data, mfem_type::size, false)
+   /// and return a MDTensor with the MDSpan's strides and layout
+   const MDTensor<N,const double> MDHostRead() const
+   {
+      const double *ptr = mfem::Read(mfem_type::data, mfem_type::size, false);
+      return MDTensor<N,const double>(ptr, Sd);
+   }
+
+   /// Shortcut for mfem::Write(mfem_type::data, mfem_type::size, on_dev)
+   /// and return a MDTensor with the MDSpan's strides and layout
+   MDTensor<N> MDWrite(bool on_dev = true)
+   {
+      double *ptr = mfem::Write(mfem_type::data, mfem_type::size, on_dev);
+      return MDTensor<N>(ptr, Sd);
+   }
+
+   /// Shortcut for mfem::Write(mfem_type::data, mfem_type::size, false)
+   /// and return a MDTensor with the MDSpan's strides and layout
+   MDTensor<N> MDHostWrite()
+   {
+      double *ptr = mfem::Write(mfem_type::data, mfem_type::size, false);
+      return MDTensor<N>(ptr, Sd);
+   }
+
+   /// Shortcut for mfem::ReadWrite(mfem_type::data, mfem_type::size, on_dev)
+   /// and return a MDTensor with the MDSpan's strides and layout
+   MDTensor<N> MDReadWrite(bool on_dev = true)
+   {
+      double *ptr = mfem::ReadWrite(mfem_type::data, mfem_type::size, on_dev);
+      return MDTensor<N>(ptr, Sd);
+   }
+
+   /// Shortcut for mfem::ReadWrite(mfem_type::data, mfem_type::size, false)
+   /// and return a MDTensor with the MDSpan's strides and layout
+   MDTensor<N> MDHostReadWrite()
+   {
+      double *ptr = mfem::ReadWrite(mfem_type::data, mfem_type::size, false);
+      return MDTensor<N>(ptr, Sd);
+   }
+
+   /// The MDReshape function allowes to reshape the MDSpan multi-dimentional
+   /// view into a new multi-dimentional one, by std::array blocks.
+   /// For example, if this has three dimensions {N1, N2, N3}, it could do:
+   /// this->MDReshape<4>(ptr, N1, std::array<int,2> {2, N2/2}, N3);
+   ///
+   /// Parameter R could be omitted with c++14 standard's deduced return types
+
+   // first entry point with given data pointer and rest of arguments
+   template <int R, int m = 0, md_size_t M = 0, typename... Ts>
+   inline auto MDReshape(double *ptr, Ts&&... args) -> MDTensor<R>
+   {
+      rNd.clear();
+      reshape_ptr = ptr;
+      reshape_offset = 1, reshape_shifts[0] = reshape_shifts[1] = 0;
+      return MDReshape<R,m,M>(std::forward<Ts>(args)...);
+   }
+
+   // variadic case, where a new block of reshape is given
+   template <int R, int m = 0, md_size_t M = 0, size_t P, typename... Ts>
+   inline auto MDReshape(std::array<int,P> list, Ts&&... args) -> MDTensor<R>
+   {
+      reshape_shifts[0] = layout.perm[m]; // store layout shift begin
+      md_size_t shifted_layout = reshape_shifts[1] + layout.perm[m];
+      for (int dim: list)
+      {
+         rNd.push_back(dim);
+         rLt[m].push_back(sub_layout_pair{shifted_layout,-1});
+         shifted_layout += 1; // default left layout
+      }
+      reshape_shifts[1] += P-1; // update end
+      return MDReshape<R,m+1,M+P>(std::forward<Ts>(args)...);
+   }
+
+   // variadic case, where a new dimension of reshape is given
+   template <int R, int m = 0, md_size_t M = 0, typename... Ts>
+   inline auto MDReshape(md_size_t dim, Ts&&... args) -> MDTensor<R>
+   {
+      rNd.push_back(dim);
+      const md_size_t shift =
+         reshape_shifts[0] < layout.perm[m] ? reshape_shifts[1] : 0;
+      rLt[m].push_back(sub_layout_pair{layout.perm[m] + shift,-1});
+      return MDReshape<R,m+1,M+1>(std::forward<Ts>(args)...);
+   }
+
+   // terminal case which returns the right MDTensor
+   template <int R, int m = 0, md_size_t M = 0>
+   inline MDTensor<R> MDReshape()
+   {
+      md_size_t k = 0, rLt_idx[M], rSd[M];
+      // initialize sub_layout_pair's second
+      for (sub_layout_type &sub: rLt)
+      {
+         for (sub_layout_pair &p: sub) { p.second = k++; }
+      }
+      // scan with the previous layout (N) order the reshaped layout (M)
+      for (md_size_t i = 0, j = 0; i < N; i++)
+      {
+         for (sub_layout_pair &p: rLt[layout[i]])
+         {
+            rLt_idx[j++] = p.second;
+         }
+      }
+      // apply the reshaped layout (M)
+      rSd[rLt_idx[0]] = 1;
+      for (md_size_t i = 1; i < M; i++)
+      {
+         rSd[rLt_idx[i]] = rNd[rLt_idx[i-1]] * rSd[rLt_idx[i-1]];
+      }
+      // construct the MDTensor with the given pointer and reshaped sizes
+      static_assert(R == M, "R != M");
+      return MDTensor<R>(reshape_ptr, rSd);
+   }
+
+private:
+   double *reshape_ptr;
+   std::vector<md_size_t> rNd; // reshape sizes
+   md_size_t reshape_offset, reshape_shifts[2];// shift begin & end
+   using sub_layout_pair = std::pair<md_size_t,int>;
+   using sub_layout_type = std::list<sub_layout_pair>;
+   std::array<sub_layout_type,N> rLt; // layout
+};
+
+namespace internal // implementation could be simplified with c++14 standard
+{
+
+template <typename T, md_size_t N, md_size_t mod, bool left> struct md_extend;
+
+template <typename T, T... args, md_size_t N>
+struct md_extend<md_sequence<T, args...>, N, 0, true>
+{
+   using type = md_sequence<T, args..., (args + N)...>;
+};
+
+template <typename T, T... args, md_size_t N>
+struct md_extend<md_sequence<T, args...>, N, 1, true>
+{
+   using type = md_sequence<T, args..., (args + N)..., 2*N>;
+};
+
+template <typename T, T... args, md_size_t N>
+struct md_extend<md_sequence<T, args...>, N, 0, false>
+{
+   using type = md_sequence<T, (args + N)..., args...>;
+};
+
+template <typename T, T... args, md_size_t N>
+struct md_extend<md_sequence<T, args...>, N, 1, false>
+{
+   using type = md_sequence<T,  2*N, (args + N)..., args...>;
+};
+
+template <typename T, md_size_t N, bool L> struct make_md_sequence
+{
+   using sequence_type = typename make_md_sequence<T,N/2,L>::type;
+   using type = typename md_extend<sequence_type, N/2, N%2, L>::type;
+};
+
+template <typename T, bool L>
+struct make_md_sequence<T,0,L> { using type = md_sequence<T>; };
+
+} //namespace internal
+
+} // namespace mfem
+
+#endif // MFEM_MDSPAN_HPP
---
+++ tests/unit/general/test_mdspan.cpp
@@ -0,0 +1,323 @@
+// Copyright (c) 2010-2023, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#include <list>
+#include "unit_tests.hpp"
+#include "fem/gridfunc.hpp"
+#include "general/array.hpp"
+#include "general/mdspan.hpp"
+#include "general/forall.hpp"
+#include "linalg/dtensor.hpp"
+
+using namespace mfem;
+
+// A possible MDArray class
+template<typename T, md_size_t N, typename L = MDLayoutLeft<N>>
+using MDArray = MDSpan<mfem::Array<T>,T,N,L>;
+
+TEST_CASE("MDArray", "[MDSpan], [MDArray]")
+{
+   SECTION("SetSize")
+   {
+      constexpr int NA = 11, NB = 22, NC = 33;
+      MDArray<int,3> abc;
+      abc.SetSize(NA, NB, NC);
+      REQUIRE(abc.Size() == NA*NB*NC);
+   }
+
+   SECTION("SetLayout")
+   {
+      constexpr int NA = 11, NB = 22, NC = 33;
+      constexpr int na =  0, nb =  1, nc =  2;
+      MDLayout<3> layout_012({na,nb,nc});
+
+      MDArray<int,3> abc(NA, NB, NC);
+      MDArray<int,3, MDLayout<3>> abc_ini(NA, NB, NC);
+      MDArray<int,3, MDLayout<3>> abc_set(NA, NB, NC);
+      abc_set.SetLayout(layout_012);
+
+      REQUIRE(abc_set.Offset(na,nb,nc) == abc.Offset(na,nb,nc));
+      REQUIRE(abc_set.Offset(na,nb,nc) == abc_ini.Offset(na,nb,nc));
+   }
+}
+
+// A possible MDVector class
+template<md_size_t N, typename L = MDLayoutLeft<N>>
+struct MDVector : public MDSpan<mfem::Vector,double,N,L>
+{
+   using base_t = MDSpan<mfem::Vector,double,N,L>;
+
+   MDVector(): base_t() { }
+
+   template <typename... Ts>
+   MDVector(md_size_t n, Ts... as): MDVector(as...) { base_t::Setup(n, as...); }
+};
+
+TEST_CASE("MDVector", "[MDSpan], [MDVector]")
+{
+   SECTION("SetLayout")
+   {
+      constexpr int NA = 11, NB = 22, NC = 33;
+      constexpr int na =  0, nb =  1, nc =  2;
+      MDLayout<3> layout_012({na,nb,nc});
+
+      MDVector<3> abc(NA, NB, NC);
+      MDVector<3, MDLayout<3>> abc_ini(NA, NB, NC);
+      MDVector<3, MDLayout<3>> abc_set(NA, NB, NC);
+      abc_set.SetLayout(layout_012);
+
+      REQUIRE(abc_set.Offset(na,nb,nc) == abc.Offset(na,nb,nc));
+      REQUIRE(abc_set.Offset(na,nb,nc) == abc_ini.Offset(na,nb,nc));
+   }
+
+   SECTION("Offset")
+   {
+      constexpr int NA = 18, NB = 2, NC = 36, ND = 32;
+      // Fortran col major: (N1:18, 2, 36, Nd:32)
+      //                    (    0, 1,  2,     3)
+      // = 0 + 18( 1 + 2( 2 + 36( 3))) = 3978
+      MDVector<4> left(NA,NB,NC,ND); // default layout is LayoutLeft
+      REQUIRE(left.Offset(0,1,2,3) == 3978);
+
+      // C/C++ row major: (N1:18, 2, 36, Nd:32)
+      //                  (    0, 1,  2,     3)
+      // = 3 + 32( 2 + 36( 1 + 2(0))) = 1219
+      // = (((0)*2 + 1) * 36 + 2) * 32 + 3
+      MDVector<4> right(NA, NB, NC, ND);
+      right.SetLayout(MDLayout<4>({3,2,1,0}));
+      REQUIRE(right.Offset(0,1,2,3) == 1219);
+
+      MDVector<4,MDLayoutRight<4>> right4(NA, NB, NC, ND);
+      right4.SetLayout(MDLayoutRight<4>({3,2,1,0}));
+      REQUIRE(right4.Offset(0,1,2,3) == 1219);
+   }
+}
+
+// A possible MDGridFunction class
+template<md_size_t N, class L = MDLayoutLeft<N>>
+class MDGridFunction : public MDSpan<mfem::GridFunction,double,N,L>
+{
+   using base_type = MDSpan<mfem::GridFunction,double,N,L>;
+   using base_type::Nd;
+   using base_type::Sd;
+
+private:
+   mutable md_size_t get_vdofs_offset = 0;
+
+public:
+   MDGridFunction(): base_type() { }
+
+   MDGridFunction(MDGridFunction&&) = delete;
+   MDGridFunction(const MDGridFunction&) = delete;
+   MDGridFunction& operator=(MDGridFunction&&) = delete;
+
+   template <typename... Ts>
+   MDGridFunction(FiniteElementSpace *ifes, Ts... args): MDGridFunction(args...)
+   {
+      this->GridFunction::fes = ifes;
+      base_type::Setup(ifes->GetNDofs(), args...);
+   }
+
+   template <typename... Ts>
+   MDGridFunction(md_size_t n, Ts... args): MDGridFunction(args...)
+   {
+      base_type::Setup(n, args...);
+   }
+
+   template <md_size_t n = 1, typename... Ts>
+   void GetVDofs(mfem::Array<int> &dofs, Ts... args) const
+   {
+      REQUIRE(dofs.Size() == Nd[n-1]);
+      for (int s = 0; s < dofs.Size(); s++)
+      {
+         dofs[s] = get_vdofs_offset;
+         dofs[s] += MDOffset<n,N,md_size_t,Ts...>::offset(Sd, s, args...);
+      }
+      get_vdofs_offset = 0;
+   }
+
+   template <md_size_t n = 1, typename... Ts>
+   void GetVDofs(md_size_t dim, Ts&&... args) const
+   {
+      get_vdofs_offset += dim * Sd[n-1];
+      MDGridFunction::GetVDofs<n+1>(std::forward<Ts>(args)...);
+   }
+};
+
+TEST_CASE("MDGridFunction layouts", "[MDSpan], [MDGridFunction]")
+{
+   constexpr int NE = 18, NG = 2, NA = 32;
+
+   const bool all = launch_all_non_regression_tests;
+   auto p = all ? GENERATE(1,2) : 2;
+   auto nx = all ? GENERATE(3,5) : 2;
+   auto dim = all ? GENERATE(1,2,3) : 2;
+
+   auto MakeCartesian = [](int dim, int nx)
+   {
+      return dim == 2 ? Mesh::MakeCartesian2D(nx, nx, Element::QUADRILATERAL):
+             dim == 3 ? Mesh::MakeCartesian3D(nx, nx, nx, Element::HEXAHEDRON):
+             Mesh::MakeCartesian1D(nx);
+   };
+
+   Mesh mesh = MakeCartesian(dim, nx);
+
+   H1_FECollection fec(p, dim);
+   FiniteElementSpace fes(&mesh, &fec);
+   const int ND = fes.GetNDofs();
+
+   SECTION("E>G>D>A, left")
+   {
+      MDGridFunction<4> gsa(NE, NG, &fes, NA);
+      const int gsa_0123 = gsa.Offset(0, 1, 2, 3);
+      REQUIRE(gsa_0123 ==  0 + 1*(NE) + 2*(NE*NG) + 3*(NE*NG*ND));
+      mfem::Array<int> vdofs(ND);
+      gsa.GetVDofs(0, 1, vdofs, 3);
+      REQUIRE(0 <= vdofs.Min());
+      REQUIRE(vdofs[2] == gsa_0123);
+      REQUIRE(vdofs.Max() < NE*NG*NA*ND);
+
+   }
+
+   SECTION("E>G>D>A, right")
+   {
+      MDGridFunction<4,MDLayoutRight<4>> gsa(NE, NG, &fes, NA);
+      const int gsa_0123 = gsa.Offset(0,1,2,3);
+      REQUIRE(gsa_0123 == 0*(NG*ND*NA) + 1*(ND*NA) + 2*(NA) + 3);
+      mfem::Array<int> vdofs(ND);
+      gsa.GetVDofs(0, 1, vdofs, 3);
+      REQUIRE(0 <= vdofs.Min());
+      REQUIRE(vdofs[2] == gsa_0123);
+      REQUIRE(vdofs.Max() < NE*NG*NA*ND);
+   }
+}
+
+TEST_CASE("MDGridFunction reshapes", "[MDSpan], [MDGridFunction]")
+{
+   auto is_equal = [&](const  Vector &a, const  Vector &b)
+   {
+      REQUIRE(a.Size() == b.Size());
+      for (int i = 0; i < a.Size(); i++)
+      {
+         const double va = a.GetData()[i], vb =  b.GetData()[i];
+         REQUIRE(va == MFEM_Approx(vb));
+      };
+      return true;
+   };
+
+   SECTION("MDReshapes")
+   {
+      constexpr int dim = 3;
+      constexpr int nx = 5, ny = 3, nz = 2;
+      Mesh mesh = Mesh::MakeCartesian3D(nx, ny, nz, mfem::Element::HEXAHEDRON);
+
+      H1_FECollection fec_mesh(1, dim);
+      FiniteElementSpace fes_mesh(&mesh, &fec_mesh, dim);
+      mesh.SetNodalFESpace(&fes_mesh);
+
+      L2_FECollection fec(1, dim, BasisType::GaussLobatto);
+      FiniteElementSpace fes(&mesh, &fec);
+
+      const std::list<MDLayout<3>> layouts =
+      { {0,1,2}, {0,2,1}, {1,0,2}, {1,2,0}, {2,1,0}, {2,0,1} };
+
+      for (auto &layout: layouts)
+      {
+         constexpr int numGroups = 4, numAngles = 7;
+
+         MDGridFunction<3> psi(&fes, numGroups, numAngles);
+         psi.SetLayout(MDLayout<3>(layout));
+
+         const GridFunction *nodes = mesh.GetNodes();
+         const FiniteElementSpace *mfes = mesh.GetNodalFESpace();
+         const int ng = numGroups, na = numAngles, ne = mfes->GetNE();
+         const ElementDofOrdering e_ordering = ElementDofOrdering::LEXICOGRAPHIC;
+         const Operator *R = mfes->GetElementRestriction(e_ordering);
+         REQUIRE(R);
+         const FiniteElement *mfe = mfes->GetFE(0);
+         const int nd = mfe->GetDof(), vdim = mfes->GetVDim();
+         Vector nodes_e(vdim*nd*ne); nodes_e.UseDevice(true);
+         constexpr int D1D = 2;
+         REQUIRE(fes.GetVSize() == D1D*D1D*D1D*ne);
+         nodes_e.Read();
+         REQUIRE(nodes);
+         R->Mult(*nodes, nodes_e);
+         const auto X = mfem::Reshape(nodes_e.Read(), D1D, D1D, D1D, vdim, ne);
+         auto dY = psi.MDWrite();
+
+         MDGridFunction<3> rY1(&fes, numGroups, numAngles);
+         rY1.SetLayout(MDLayout<3>(layout));
+         REQUIRE(rY1.Size() == psi.Size());
+         auto drY1 = rY1.MDWrite();
+
+         MDGridFunction<3> rY2(&fes, numGroups, numAngles);
+         rY2.SetLayout(MDLayout<3>(layout));
+         REQUIRE(ng%2 == 0);
+         auto drY2 = rY2.MDReshape<4>(rY2.Write(),
+                                      D1D*D1D*D1D*ne,
+                                      std::array<int,2> {2, ng/2},
+                                      na);
+
+         MDGridFunction<3> rY3(&fes, numGroups, numAngles);
+         rY3.SetLayout(MDLayout<3>(layout));
+         auto drY3 = rY3.MDReshape<6>(rY3.Write(),
+                                      std::array<int,4> {D1D,D1D,D1D,ne},
+                                      ng, na);
+
+         MDGridFunction<3> rY4(&fes, numGroups, numAngles);
+         rY4.SetLayout(MDLayout<3>(layout));
+         auto drY4 = rY4.MDReshape<7>(rY4.Write(),
+                                      std::array<int,4> {D1D,D1D,D1D,ne},
+                                      std::array<int,2> {1, ng},
+                                      na);
+
+         MDGridFunction<3> rY5(&fes, numGroups, numAngles);
+         rY5.SetLayout(MDLayout<3>(layout));
+         auto drY5 = rY5.MDReshape<7>(rY5.Write(),
+                                      std::array<int,4> {D1D,D1D,D1D,ne},
+                                      std::array<int,2> {2, ng/2},
+                                      na);
+
+         const double exp_m08 = exp(-0.8);
+
+         MFEM_FORALL_3D(ega, ne*ng*na, D1D,D1D,D1D,
+         {
+            const int e = ega/(ng*na), ga = ega%(ng*na), g = ga/na, a = ga%na;
+            MFEM_FOREACH_THREAD(dz,z,D1D)
+            {
+               MFEM_FOREACH_THREAD(dy,y,D1D)
+               {
+                  MFEM_FOREACH_THREAD(dx,x,D1D)
+                  {
+                     const int xyze = dx + D1D*(dy + D1D*(dz + D1D*(e)));
+
+                     const double p0 = X(dx,dy,dz,0,e), p1 = X(dx,dy,dz,1,e);
+                     const double value = 1.0 - exp_m08*cos(M_PI*p0)*cos(M_PI*p1);
+
+                     dY(xyze,g,a) = value;
+                     drY1(xyze,g,a) = value;
+                     drY2(xyze,g%2,g/2,a) = value;
+                     drY3(dx,dy,dz,e, g, a) = value;
+                     drY4(dx,dy,dz,e, 0,g, a) = value;
+                     drY5(dx,dy,dz,e, g%2,g/2, a) = value;
+                  }
+               }
+            }
+         });
+         psi.MDHostRead(); rY1.HostRead();
+         REQUIRE(is_equal(rY1, psi));
+         REQUIRE(is_equal(rY2, psi));
+         REQUIRE(is_equal(rY3, psi));
+         REQUIRE(is_equal(rY4, psi));
+         REQUIRE(is_equal(rY5, psi));
+      }
+   }
+}
