e48bf5ebb3df721fc3bbd51d496917fa9e37714c
Will Pazner <will.e.p@gmail.com>
2023-03-30 12:29:25 -0700

    Change coefficient tests to use tag [Coefficient]

--- tests/unit/fem/test_coefficient.cpp
+++ tests/unit/fem/test_coefficient.cpp
@@ -14,8 +14,7 @@
 
 using namespace mfem;
 
-TEST_CASE("Piecewise Coefficient",
-          "[PWCoefficient]")
+TEST_CASE("Piecewise Coefficient", "[Coefficient]")
 {
    ConstantCoefficient oneCoef(1.0);
    ConstantCoefficient twoCoef(2.0);
@@ -95,8 +94,7 @@ TEST_CASE("Piecewise Coefficient",
    }
 }
 
-TEST_CASE("Piecewise Vector Coefficient",
-          "[PWVectorCoefficient]")
+TEST_CASE("Piecewise Vector Coefficient", "[Coefficient]")
 {
    int d = 3;
 
@@ -201,8 +199,7 @@ TEST_CASE("Piecewise Vector Coefficient",
    }
 }
 
-TEST_CASE("Piecewise Matrix Coefficient",
-          "[PWMatrixCoefficient]")
+TEST_CASE("Piecewise Matrix Coefficient", "[Coefficient]")
 {
    int d = 3;
 
