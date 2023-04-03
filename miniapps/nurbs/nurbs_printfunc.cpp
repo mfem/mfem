0d3d2954a0bdc833e1f7bbacf3e9a5044abc23b2
JacobLotz <jacob.lotz@gmail.com>
2023-04-03 17:13:07 +0200

    Add example of fix

---
+++ miniapps/nurbs/nurbs_printfunc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "mfem.hpp"
+
+using namespace std;
+using namespace mfem;
+
+int main()
+{
+	KnotVector kv(2, 7);
+
+	kv[0] = 0;
+	kv[1] = 0;
+	kv[2] = 0;
+	kv[3] = 0.25;
+	kv[4] = 0.5;
+	kv[5] = 0.5; // Repeated knot
+	kv[6] = 0.75; 
+	kv[7] = 1;
+	kv[8] = 1;
+	kv[9] = 1;
+
+	kv.Print(cout);
+
+	// Count number of elements, required for printing of shapes
+	kv.GetElements();
+
+	kv.PrintFunctions(cout);
+}
