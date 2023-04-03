68a4f186714d5151469fa8664c7b4fd21a68f713
JacobLotz <jacob.lotz@gmail.com>
2023-04-03 17:13:59 +0200

    make style

--- miniapps/nurbs/nurbs_printfunc.cpp
+++ miniapps/nurbs/nurbs_printfunc.cpp
@@ -6,23 +6,23 @@ using namespace mfem;
 
 int main()
 {
-	KnotVector kv(2, 7);
+   KnotVector kv(2, 7);
 
-	kv[0] = 0;
-	kv[1] = 0;
-	kv[2] = 0;
-	kv[3] = 0.25;
-	kv[4] = 0.5;
-	kv[5] = 0.5; // Repeated knot
-	kv[6] = 0.75; 
-	kv[7] = 1;
-	kv[8] = 1;
-	kv[9] = 1;
+   kv[0] = 0;
+   kv[1] = 0;
+   kv[2] = 0;
+   kv[3] = 0.25;
+   kv[4] = 0.5;
+   kv[5] = 0.5; // Repeated knot
+   kv[6] = 0.75;
+   kv[7] = 1;
+   kv[8] = 1;
+   kv[9] = 1;
 
-	kv.Print(cout);
+   kv.Print(cout);
 
-	// Count number of elements, required for printing of shapes
-	kv.GetElements();
+   // Count number of elements, required for printing of shapes
+   kv.GetElements();
 
-	kv.PrintFunctions(cout);
+   kv.PrintFunctions(cout);
 }
