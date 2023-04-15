b297b3a9de797714bc9505d3dd9f02f2d61c74b4
blaz <bslazarov.dev@gmail.com>
2023-04-14 22:39:40 -0700

    fixed headers

--- general/kdtree.hpp
+++ general/kdtree.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010-2022, Lawrence Livermore National Security, LLC. Produced
+// Copyright (c) 2010-2023, Lawrence Livermore National Security, LLC. Produced
 // at the Lawrence Livermore National Laboratory. All Rights reserved. See files
 // LICENSE and NOTICE for details. LLNL-CODE-806117.
 //
@@ -7,7 +7,7 @@
 //
 // MFEM is free software; you can redistribute it and/or modify it under the
 // terms of the BSD-3 license. We welcome feedback and contributions, see file
-// CONTRIBUTING.md for details.
+// CONTRIBUTING.md for details
 
 #ifndef MFEM_KDTREE_HPP
 #define MFEM_KDTREE_HPP
