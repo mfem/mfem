c116a1e3809be20a0d94f800c9004826ecd343a8
Dohyun Kim <dohyun_kim@brown.edu>
2023-05-03 15:54:21 -0400

    revert back ZZ estimator

--- fem/estimators.cpp
+++ fem/estimators.cpp
@@ -15,6 +15,22 @@
 namespace mfem
 {
 
+void ZienkiewiczZhuEstimator::ComputeEstimates()
+{
+   flux_space->Update(false);
+   // In parallel, 'flux' can be a GridFunction, as long as 'flux_space' is a
+   // ParFiniteElementSpace and 'solution' is a ParGridFunction.
+   GridFunction flux(flux_space);
+
+   if (!anisotropic) { aniso_flags.SetSize(0); }
+   total_error = ZZErrorEstimator(integ, solution, flux, error_estimates,
+                                  anisotropic ? &aniso_flags : NULL,
+                                  flux_averaging,
+                                  with_coeff);
+
+   current_sequence = solution.FESpace()->GetMesh()->GetSequence();
+}
+
 void ProjectionErrorEstimator::ComputeEstimates()
 {
    const int nelem = solution.FESpace()->GetNE();
