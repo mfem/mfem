f32e84dd267ed025c6e225f4cca9679e63042254
Dohyun Kim <dohyun_kim@brown.edu>
2023-05-05 10:19:49 -0400

    timing

--- examples/test_estimator.cpp
+++ examples/test_estimator.cpp
@@ -1,11 +1,13 @@
 #include "mfem.hpp"
+#include <chrono>
+
 
 using namespace mfem;
 int main()
 {
    const int order = 2;
    const int dim = 2;
-   for (int i=0; i<7; i++)
+   for (int i=0; i<12; i++)
    {
       Mesh mesh = Mesh::MakeCartesian2D(static_cast<int>(std::pow(2, i)),
                                         static_cast<int>(std::pow(2, i)), Element::Type::QUADRILATERAL);
@@ -25,15 +27,27 @@ int main()
       GridFunction x(fes);
       x.ProjectCoefficient(v);
 
+      out << "Estimation" << std::endl;
+      auto start_1 = std::chrono::high_resolution_clock::now();
       ProjectionErrorEstimator estimator(x);
       auto & estimators = estimator.GetLocalErrors();
+      out << "estimated error: " << estimator.GetTotalError() << std::endl;
+      auto stop_1 = std::chrono::high_resolution_clock::now();
+      auto duration_1 = std::chrono::duration_cast<std::chrono::microseconds>
+                        (stop_1 - start_1);
+      out << "wall time: " << duration_1.count() / 1000.0 << "ms" << std::endl;
 
+      out << "Estimation" << std::endl;
+      auto start_2 = std::chrono::high_resolution_clock::now();
       PRefDiffEstimator estimator_Katen(x, -1);
       auto & estimators_Katen = estimator_Katen.GetLocalErrors();
+      out << "estimated error: " << estimator_Katen.GetTotalError() << std::endl;
+      auto stop_2 = std::chrono::high_resolution_clock::now();
+      auto duration_2 = std::chrono::duration_cast<std::chrono::microseconds>
+                        (stop_2 - start_2);
+      out << "wall time: " << duration_2.count() / 1000.0 <<  "ms" << std::endl;
 
       // double total_error = estimators.Norml2();
-      out << estimator.GetTotalError() << std::endl;
-      out << estimator_Katen.GetTotalError() << std::endl;
       out << x.ComputeL2Error(v) << std::endl;
       out << estimator.GetTotalError() / x.ComputeL2Error(v) << std::endl;
       out << estimator_Katen.GetTotalError() / x.ComputeL2Error(v) << std::endl;
