f35a9df42e2ce128355cdd1ab541ffb7730a5bd8
Brody Bassett <bassett4@llnl.gov>
2023-05-11 03:05:23 -0700

    Added ex18p for RK.

--- examples/ex18p.cpp
+++ examples/ex18p.cpp
@@ -70,6 +70,10 @@ int main(int argc, char *argv[])
    int ser_ref_levels = 0;
    int par_ref_levels = 1;
    int order = 3;
+   bool rk = false;
+   int rk_num_points = 4;
+   double rbf_h = 4.01;
+   int rbf_type = 7;
    int ode_solver_type = 4;
    double t_final = 2.0;
    double dt = -0.01;
@@ -93,6 +97,14 @@ int main(int argc, char *argv[])
                   " partitioning.");
    args.AddOption(&order, "-o", "--order",
                   "Order (degree) of the finite elements.");
+   args.AddOption(&rk, "-rk", "--rk", "-no-rk", "--no-rk",
+                  "Use reproducing kernel functions");
+   args.AddOption(&rk_num_points, "-rkp", "--rk-num-points",
+                  "Number of reproducing kernel points across each dimension of element");
+   args.AddOption(&rbf_h, "-rbfh", "--rbf-h",
+                  "Radial basis function shape parameter (>= RK order)");
+   args.AddOption(&rbf_type, "-rbft", "--rbf-type",
+                  "Radial basis function type: (0-2) global, (3-7) local");
    args.AddOption(&ode_solver_type, "-s", "--ode-solver",
                   "ODE solver: 1 - Forward Euler,\n\t"
                   "            2 - RK2 SSP, 3 - RK3 SSP, 4 - RK4, 6 - RK6.");
@@ -161,13 +173,17 @@ int main(int argc, char *argv[])
 
    // 7. Define the discontinuous DG finite element space of the given
    //    polynomial order on the refined mesh.
-   DG_FECollection fec(order, dim);
+   FiniteElementCollection *fec =
+      rk ?
+      (FiniteElementCollection*)new KernelFECollection(dim, rk_num_points, rbf_h,
+                                                       rbf_type, 2, order, 2) :
+      (FiniteElementCollection*)new DG_FECollection(order, dim);
    // Finite element space for a scalar (thermodynamic quantity)
-   ParFiniteElementSpace fes(&pmesh, &fec);
+   ParFiniteElementSpace fes(&pmesh, fec);
    // Finite element space for a mesh-dim vector quantity (momentum)
-   ParFiniteElementSpace dfes(&pmesh, &fec, dim, Ordering::byNODES);
+   ParFiniteElementSpace dfes(&pmesh, fec, dim, Ordering::byNODES);
    // Finite element space for all variables together (total thermodynamic state)
-   ParFiniteElementSpace vfes(&pmesh, &fec, num_equation, Ordering::byNODES);
+   ParFiniteElementSpace vfes(&pmesh, fec, num_equation, Ordering::byNODES);
 
    // This example depends on this ordering of the space.
    MFEM_ASSERT(fes.GetOrdering() == Ordering::byNODES, "");
@@ -375,6 +391,7 @@ int main(int argc, char *argv[])
 
    // Free the used memory.
    delete ode_solver;
+   delete fec;
 
    return 0;
 }
