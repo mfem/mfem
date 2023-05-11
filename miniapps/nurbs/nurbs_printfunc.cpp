29ecee103b55346d4825bcc58b9c9cb0d4e111c3
JacobLotz <jacob.lotz@gmail.com>
2023-05-11 15:43:11 +0200

    Added documentation and compiling/testing in makefile

--- miniapps/nurbs/nurbs_printfunc.cpp
+++ miniapps/nurbs/nurbs_printfunc.cpp
@@ -1,11 +1,33 @@
+//                          MFEM NURBS knot vector example
+//
+// Compile with: make nurbs_curveint
+//
+// Sample runs:  nurbs_curveint
+//
+// Description:  This example code demonstrates the use of MFEM to define a
+//               simple KnotVector and print its corresponding shape functions.
+
 #include <iostream>
 #include "mfem.hpp"
 
 using namespace std;
 using namespace mfem;
 
-int main()
+int main(int argc, char *argv[])
 {
+   OptionsParser args(argc, argv);
+   bool visualization;
+
+   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
+                  "--no-visualization",
+                  "Enable or disable GLVis visualization. Dummy option to allow testing.");
+   args.Parse();
+   if (!args.Good())
+   {
+      args.PrintUsage(cout);
+      return 1;
+   }
+
    KnotVector kv(2, 7);
 
    kv[0] = 0;
