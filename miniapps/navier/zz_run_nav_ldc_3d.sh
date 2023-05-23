d58a8505a5e7f6c234d2f67b2e54f353f678bf68
Andrew Gillette <gillette7@llnl.gov>
2023-05-23 16:05:06 -0700

    Update script for bsub

--- miniapps/navier/zz_run_nav_ldc_3d.sh
+++ miniapps/navier/zz_run_nav_ldc_3d.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#SBATCH -A asccasc
-#SBATCH -t 4:00:00
-#SBATCH -N 8
-#SBATCH -o yy_%j.out
-#SBATCH -J slurm1
-#SBATCH --mail-type=ALL
-#SBATCH -p pbatch
+#BSUB -G asccasc
+#BSUB -t 4:00:00
+#BSUB -nnodes 8
+#BSUB --mail-type=ALL
 
-# on lasseen: max 4 cores per node i.e. n <= 4*N
+# on lassen: max 4 cores per node i.e. n <= 4*N
 
 cd /usr/WS1/gillette/nfp4va/mfemstuff/mfem/miniapps/navier
-srun -n32 -ppdebug navier_ldc_3d --device cuda
+jsrun --nrs 32 --rs_per_host 4 --np 32 navier_ldc_3d --device cuda
+# srun -n32 -ppdebug navier_ldc_3d --device cuda
 echo 'Done'
