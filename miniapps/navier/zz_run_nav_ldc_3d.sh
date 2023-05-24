095615e52bee13a73800473463167fcd78946784
Andrew Gillette <gillette7@llnl.gov>
2023-05-24 14:35:51 -0700

    Update job script and options

--- miniapps/navier/zz_run_nav_ldc_3d.sh
+++ miniapps/navier/zz_run_nav_ldc_3d.sh
@@ -11,7 +11,7 @@
 # on lassen: max 4 cores per node i.e. n <= 4*N
 
 cd /usr/WS1/gillette/nfp4va/mfemstuff/mfem/miniapps/navier
-srun -n32 navier_ldc_3d --device cuda
+srun -n4 navier_ldc_3d --device cuda
 # jsrun --nrs 32 --rs_per_host 4 --np 32 navier_ldc_3d --device cuda
 # srun -n32 -ppdebug navier_ldc_3d --device cuda
 echo 'Done'
