22606ce124f9c31f0072b77f7c1af2b00f55daf8
termi-official <termi-official@users.noreply.github.com>
2023-05-25 21:12:49 +0200

    Partial rollback of LSZZ test extension.

--- tests/unit/fem/test_estimator.cpp
+++ tests/unit/fem/test_estimator.cpp
@@ -223,7 +223,8 @@ TEST_CASE("Least-squares ZZ estimator on 3D NCMesh", "[NCMesh]")
 {
    // Setup
    const auto order = GENERATE(2, 3);
-   const auto element = GENERATE(Element::TETRAHEDRON, Element::HEXAHEDRON);
+   // const auto element = GENERATE(Element::TETRAHEDRON, Element::HEXAHEDRON);
+   const auto element = Element::HEXAHEDRON;
    Mesh mesh = Mesh::MakeCartesian3D(2, 2, 2, element);
 
    // Make the mesh NC
