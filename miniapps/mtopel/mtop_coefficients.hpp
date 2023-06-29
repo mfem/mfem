3257c417b10d62324ca35f59973e5f6e7edc4882
blaz <bslazarov.dev@gmail.com>
2023-06-28 17:26:44 -0700

    Risk measures

--- miniapps/mtopel/mtop_coefficients.hpp
+++ miniapps/mtopel/mtop_coefficients.hpp
@@ -9,6 +9,496 @@
 
 namespace mfem {
 
+class RiskMeasures{
+public:
+    RiskMeasures(std::vector<double> samples_)
+    {
+        samples.resize(samples_.size());
+        std::copy(samples_.begin(),samples_.end(),samples.begin());
+        prob.resize(0);
+        for(size_t i=0;i<samples_.size();i++){
+            ind.push_back(i);
+        }
+
+        std::sort(ind.begin(),ind.end(),Comp(samples));
+
+        for(size_t i=0;i<samples.size();i++){
+            std::cout<<samples[ind[i]]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+
+    RiskMeasures(std::vector<double> samples_, std::vector<double> prob_)
+    {
+        samples.resize(samples_.size());
+        std::copy(samples_.begin(),samples_.end(),samples.begin());
+        prob.resize(prob_.size());
+        double sp=0.0;
+        for(size_t i=0;i<prob_.size();i++){
+            prob[i]=prob_[i];
+            sp=sp+prob[i];
+        }
+
+        for(size_t i=0;i<prob_.size();i++){
+            prob[i]=prob[i]/sp;
+            ind.push_back(i);
+        }
+
+        std::sort(ind.begin(),ind.end(),Comp(samples));
+
+        for(size_t i=0;i<samples.size();i++){
+            std::cout<<samples[ind[i]]<<" ";
+        }
+        std::cout<<std::endl;
+        for(size_t i=0;i<samples.size();i++){
+            std::cout<<prob[ind[i]]<<" ";
+        }
+        std::cout<<std::endl;
+
+    }
+
+
+    int VaRIndex(double beta)
+    {
+        if(prob.size()!=0){
+            double cp=0.0;
+            int fi=samples.size()-1;
+            for(size_t i=0;i<samples.size()-1;i++){
+                cp=cp+prob[ind[i]];
+                if(cp>(1.0-beta))
+                {
+                    fi=i;
+                    break;
+                }
+            }
+            return ind[fi];
+
+        }else{
+            double sp=1.0/(double(samples.size()));
+            double cp=0.0;
+            int fi=samples.size()-1;
+            for(size_t i=0;i<samples.size()-1;i++){
+                cp=cp+sp;
+                if(cp>(1.0-beta))
+                {
+                    fi=i;
+                    break;
+                }
+            }
+            return ind[fi];
+        }
+    }
+
+    double VaR(double beta){
+        if(prob.size()!=0){
+            double cp=0.0;
+            int fi=samples.size()-1;
+            for(size_t i=0;i<samples.size()-1;i++){
+                cp=cp+prob[ind[i]];
+                if(cp>(1.0-beta))
+                {
+                    fi=i;
+                    break;
+                }
+            }
+            return samples[ind[fi]];
+
+        }else{
+            double sp=1.0/(double(samples.size()));
+            int fi=samples.size()-1;
+            double cp=0.0;
+            for(size_t i=0;i<samples.size()-1;i++){
+                cp=cp+sp;
+                if(cp>(1.0-beta))
+                {
+                    fi=i;
+                    break;
+                }
+            }
+            return samples[ind[fi]];
+        }
+    }
+
+    double CVaR(double beta){
+        if(fabs(beta-1.0)<std::numeric_limits<double>::epsilon()){
+            return Max();
+        }
+
+        //find the VaR index
+        int vi;
+        double cp=0.0;
+        if(prob.size()!=0){
+            vi=samples.size()-1;
+            for(size_t i=0;i<samples.size()-1;i++){
+                if((cp+prob[ind[i]])>(1.0-beta))
+                {
+                    vi=i;
+                    break;
+                }else{
+                    cp=cp+prob[ind[i]];
+                }
+            }
+        }else{
+            double sp=1.0/(double(samples.size()));
+            vi=samples.size()-1;
+            for(size_t i=0;i<samples.size()-1;i++){
+                if((cp+sp)>(1.0-beta))
+                {
+                    vi=i;
+                    break;
+                }else{
+                    cp=cp+sp;}
+            }
+        }
+
+        double vVaR=samples[ind[vi]];
+
+        double rez=0.0;
+        if(prob.size()!=0){
+            for(int i=0;i<vi;i++){
+                rez=rez+prob[ind[i]]*samples[ind[i]];}
+        }else{
+            double lp=1.0/samples.size();
+            for(int i=0;i<vi;i++){
+                rez=rez+lp*samples[ind[i]];}
+        }
+
+        rez=rez+(1.0-beta-cp)*vVaR;
+        rez=rez/(1.0-beta);
+
+        return rez;
+    }
+
+
+    /// Regularized (smooth) CVaR using the
+    /// expression in Beiser2023
+    double CVaRe(double beta, double eps=1e-3)
+    {
+        if(fabs(beta-1.0)<std::numeric_limits<double>::epsilon()){
+            return Max();
+        }
+
+        double t=0.0;
+        //find t
+
+        return CVaRet(beta,eps,t);
+    }
+
+    /// For a given t computes regularized CVaR, i.e., CVaRe
+    double CVaRet(double beta, double eps, double t){
+
+        std::function<double(double)> rf=[&eps](double y)
+        {
+            //return y+eps*std::log(1.0+std::exp(-y/eps));
+            if(y<0.0){
+                return 0.0;
+            }else if(y<eps){
+                return y*y*y/(eps*eps)-y*y*y*y/(2.0*eps*eps*eps);
+            }else{
+                return y-eps/2.0;
+            }
+        };
+
+        std::function<double(double)> rf3=[&eps,&rf](double y)
+        {
+            return   rf(y+eps/2.0);
+        };
+
+        double rez=0.0;
+        if(prob.size()!=0){
+            for(size_t i=0;i<samples.size();i++){
+                rez=rez+prob[i]*rf3(samples[i]-t);}
+        }else{
+            double lp=1.0/samples.size();
+            for(size_t i=0;i<samples.size();i++){
+                rez=rez+lp*rf3(samples[i]-t);}
+        }
+
+        return t+rez/(1.0-beta);
+    }
+
+    /// For a given t computes the gradient of the
+    /// regularized CVaR with respect to t
+    double dCVaRe(double beta, double eps, double t){
+
+        std::function<double(double)> rf=[&eps](double y)
+        {
+            //return 1.0/(1.0+std::exp(-y/eps));
+            if(y<0.0){
+                return 0.0;
+            }else if(y<eps){
+                return 3.0*y*y/(eps*eps)-4.0*y*y*y/(2.0*eps*eps*eps);
+            }else{
+                return 1.0;
+            }
+        };
+
+        std::function<double(double)> rf3=[&eps,&rf](double y)
+        {
+            return rf(y+eps/2.0);
+        };
+
+        double rez=0.0;
+        if(prob.size()!=0){
+            for(size_t i=0;i<samples.size();i++){
+                rez=rez+prob[i]*rf3(samples[i]-t);}
+        }else{
+            double lp=1.0/samples.size();
+            for(size_t i=0;i<samples.size();i++){
+                rez=rez+lp*rf3(samples[i]-t);}
+        }
+
+        rez=1.0-rez/(1.0-beta);
+        return rez;
+    }
+
+
+    double CVaRe_Find_t(double beta, double eps,
+                        double alpha=1, double rerr=1e-8, int max_it=1000)
+    {   if(fabs(beta-1.0)<std::numeric_limits<double>::epsilon()){
+            return Max();
+        }
+        double t=VaR(beta);
+        return CVaRe_Find_t(beta,eps,t, alpha, rerr,max_it);
+    }
+
+    /// Finds the optimal value of t
+    double CVaRe_Find_t(double beta, double eps, double t,
+                        double alpha=1, double rerr=1e-8, int max_it=1000)
+    {
+        if(fabs(beta-1.0)<std::numeric_limits<double>::epsilon()){
+            return Max();
+        }
+
+        double a=t-1.0*eps;
+        double b=t+1.0*eps;
+        double tn;
+        for(int i=0;i<max_it;i++){
+            double dF=dCVaRe(beta,eps,t);
+            std::cout<<"i="<<i<<" dF="<<dF<<" t="<<t<<std::endl;
+            tn=a*(b-t)*std::exp(alpha*dF)+b*(t-a);
+            tn=tn/((b-t)*std::exp(alpha*dF)+t-a);
+            if(fabs(tn-t)<rerr){ t=tn; break;}
+            t=tn;
+        }
+        return t;
+    }
+
+    double Mean(){
+        double rez=0.0;
+        if(prob.size()!=0){
+            for(size_t i=0;i<samples.size();i++){
+                rez=rez+prob[i]*samples[i];}
+        }else{
+            double lp=1.0/samples.size();
+            for(size_t i=0;i<samples.size();i++){
+                rez=rez+lp*samples[i];}
+        }
+        return rez;
+    }
+
+    double EntRisk(double t){
+        double shift=Max();
+
+        double lt=0.0;
+        if(prob.size()!=0){
+            for(size_t i=0;i<samples.size();i++){
+                lt=lt+prob[i]*std::exp((samples[i]-shift)/t);
+            }
+        }else{
+            double sp=1.0/double(samples.size());
+            for(size_t i=0;i<samples.size();i++){
+                lt=lt+sp*std::exp((samples[i]-shift)/t);
+            }
+        }
+
+        lt=std::log(lt);
+        return shift+t*lt;
+    }
+
+    double Max()
+    {
+        return samples[ind[0]];
+    }
+
+    double Min()
+    {
+        return samples[ind[samples.size()-1]];
+    }
+
+    //eval EVaR
+    double EVaR(double beta, double alpha=1.0, double eps=1e-8, int max_it=1000){
+
+
+        if(fabs(beta)<std::numeric_limits<double>::epsilon()){
+            return Mean();
+        }
+
+        if(fabs(beta-1.0)<std::numeric_limits<double>::epsilon()){
+            return samples[ind[0]];
+        }
+
+        double t=fabs(Max());
+        double tm=fabs(Min());
+        if(t<tm){t=tm;}
+
+        //find t
+        t=EVaR_Find_t(beta,t,alpha,eps,max_it);
+        return EVaRt(beta,t);
+    }
+
+    // For given beta and t evaluates EVaR
+    double EVaRt(double beta, double t){
+
+        double shift=Max();
+
+        double lt=0.0;
+        if(prob.size()!=0){
+            for(size_t i=0;i<samples.size();i++){
+                lt=lt+prob[i]*std::exp((samples[i]-shift)/t);
+            }
+        }else{
+            double sp=1.0/double(samples.size());
+            for(size_t i=0;i<samples.size();i++){
+                lt=lt+sp*std::exp((samples[i]-shift)/t);
+            }
+        }
+
+        lt=lt/(1.0-beta);
+        lt=std::log(lt);
+        return shift+t*lt;
+    }
+
+    double EVaR_Find_t(double beta, double alpha=1.0, double eps=1e-8, int max_it=1000)
+    {
+        double t=fabs(Max());
+        double tm=fabs(Min());
+        if(t<tm){t=tm;}
+        return EVaR_Find_t(beta,t,alpha,eps,max_it);
+    }
+
+    double EVaR_Find_t(double beta, double t, double alpha=1.0, double eps=1e-8, int max_it=1000)
+    {
+        //find t
+        double inc=0.0;
+        int iter=0;
+        double dF;
+        while(fabs(inc-1.0)>eps)
+        {
+            dF=dEVaR(beta,t);
+            //std::cout<<" i="<<iter<<" dF="<<dF<<" inc="<<exp(-alpha*dF)<<" t="<<t<<std::endl;
+            inc=exp(-alpha*dF);
+            t=t*inc;
+            iter++;
+            if(iter==max_it){
+                MFEM_WARNING("Maximum number of iterations in EVaR_Find_t has been reached!!!")
+                break;
+            }
+        }
+        //std::cout<<std::endl<<"iter="<<iter<<" t="<<t<<std::endl;
+
+        return t;
+    }
+
+    void Test_EVaR_Grad()
+    {
+        double beta=0.9;
+        double t=0.4;
+        double dt=1.0;
+        double tt;
+        double EReF=EVaRt(beta,t);
+        double EVaR;
+        double dF=dEVaR(beta,t);
+
+        double CReFe=CVaRet(beta,1e-3,t);
+        double CVaRe;
+        double dC=dCVaRe(beta,1e-3,t);
+
+        for(int i=0;i<10;i++){
+            tt=t+dt;
+            EVaR=EVaRt(beta,tt);
+            std::cout<<" FD="<<(EVaR-EReF)/dt<<" DF="
+                    <<dF<<" err="<<fabs(dF-(EVaR-EReF)/dt)<<std::endl;
+
+            CVaRe=CVaRet(beta,1e-3,tt);
+            std::cout<<" FD="<<(CVaRe-CReFe)/dt<<" DF="
+                    <<dC<<" err="<<fabs(dC-(CVaRe-CReFe)/dt)<<std::endl;
+
+
+            dt=dt/10.0;
+        }
+
+    }
+
+    double dEVaR(double beta, double t){
+        double shift=Max();
+        double rez=0.0;
+        double lt=0.0;
+        double nt=0.0;
+        if(prob.size()!=0){
+            for(size_t i=0;i<samples.size();i++){
+                lt=lt+prob[i]*std::exp((samples[i]-shift)/t);
+                nt=nt+prob[i]*samples[i]*std::exp((samples[i]-shift)/t);
+            }
+        }else{
+           double sp=1.0/double(samples.size());
+           for(size_t i=0;i<samples.size();i++){
+               lt=lt+sp*std::exp((samples[i]-shift)/t);
+               nt=nt+sp*samples[i]*std::exp((samples[i]-shift)/t);
+           }
+        }
+
+        //std::cout<<" t1="<<shift/t<<" t2="<<std::log(lt/(1.0-beta))<<" t3="<<nt/(lt*t)<<std::endl;
+        rez=shift/t+std::log(lt/(1.0-beta))-nt/(lt*t);
+        return rez;
+    }
+
+    double STD(){
+        double var=0.0;
+        double rez=0.0;
+        if(prob.size()!=0){
+            for(size_t i=0;i<samples.size();i++){
+                rez=rez+prob[i]*samples[i];
+                var=var+prob[i]*samples[i]*samples[i];
+            }
+        }else{
+            double lp=1.0/samples.size();
+            for(size_t i=0;i<samples.size();i++){
+                rez=rez+lp*samples[i];
+                var=var+lp*samples[i]*samples[i];
+            }
+        }
+        return std::sqrt(var-rez*rez);
+    }
+
+
+
+private:
+
+    std::vector<int> ind;
+    std::vector<double> samples;
+    std::vector<double> prob;
+
+    struct Comp{
+        Comp(std::vector<double>& ss_)
+        {
+            ss=&ss_;
+        }
+
+        bool operator()(int i1,int i2){
+            return (*ss)[i1]>(*ss)[i2];
+        }
+
+    private:
+        std::vector<double>* ss;
+    };
+
+
+};
+
+
+
+
 
 /// Generates a coefficient with value zero or one.
 /// The value is zero if the point is within a ball
@@ -154,6 +644,11 @@ public:
         scale=scale_;
     }
 
+    void SetZeroDirichletBC(int i)
+    {
+        dbc.insert(i);
+    }
+
     void SetCorrelationLen(double l_){
         lx=l_;
         ly=l_;
@@ -173,9 +668,13 @@ public:
         return scale*gfc.Eval(T,ip);
     }
 
-    /// Generates a new random center for the ball.
     void Sample(int seed=std::numeric_limits<int>::max()){
         if(solver==nullptr){
+            if(dbc.size()!=0){
+                for(auto it=dbc.begin();it!=dbc.end();it++){
+                    bc.AddHomogeneousBoundaryCondition(*it,spde::BoundaryType::kDirichlet);
+                }
+            }
             solver=new spde::SPDESolver(nu,bc,fes,pmesh->GetComm(),lx,ly,lz);
         }
         solver->GenerateRandomField(*rf,seed);
@@ -193,6 +692,7 @@ private:
     ParGridFunction* rf;
     spde::Boundary bc;
     GridFunctionCoefficient gfc;
+    std::set<int> dbc;//Dirichlet BC
 };
 
 #endif
