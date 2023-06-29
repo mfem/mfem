67d0d6f9313c14f790a58778faa6d4e8e18cfe1b
Will Pazner <will.e.p@gmail.com>
2023-06-29 13:03:32 -0700

    Change usage of MAX_D1D, MAX_Q1D in QuadratureInterpolator

--- fem/qinterp/grad_phys_by_nodes.cpp
+++ fem/qinterp/grad_phys_by_nodes.cpp
@@ -101,13 +101,13 @@ void TensorPhysDerivatives<QVectorLayout::byNODES>(const int NE,
          case 0x358: return Derivatives3D<L,P,3,5,8>(NE,B,G,J,X,Y);
          default:
          {
-            const int MD = DeviceDofQuadLimits::Get().MAX_D1D;
-            const int MQ = DeviceDofQuadLimits::Get().MAX_Q1D;
+            const int MD = DeviceDofQuadLimits::Get().MAX_D1D - 2;
+            const int MQ = DeviceDofQuadLimits::Get().MAX_Q1D - 2;
             MFEM_VERIFY(D1D <= MD, "Orders higher than " << MD-1
                         << " are not supported!");
             MFEM_VERIFY(Q1D <= MQ, "Quadrature rules with more than "
                         << MQ << " 1D points are not supported!");
-            Derivatives3D<L,P,0,0,0,0,0>(NE,B,G,J,X,Y,vdim,D1D,Q1D);
+            Derivatives3D<L,P>(NE,B,G,J,X,Y,vdim,D1D,Q1D);
             return;
          }
       }
