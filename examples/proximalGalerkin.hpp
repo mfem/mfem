8eec21692e24112a8e6d6c937a2330548a9bdc89
Dohyun Kim <dohyun_kim@brown.edu>
2023-07-03 22:42:04 -0400

    mapped gridfunction

--- examples/proximalGalerkin.hpp
+++ examples/proximalGalerkin.hpp
@@ -40,4 +40,57 @@ double d2simpRuledx2(const double rho, const int exponent, const double rho0)
 }
 
 
+namespace mfem
+{
+class MappedGridFunctionCoefficient :public GridFunctionCoefficient
+{
+   typedef std::__1::function<double (const double)> Mapping;
+public:
+   MappedGridFunctionCoefficient(GridFunction *gf, Mapping f_x, int comp=1)
+      :GridFunctionCoefficient(gf, comp), map(f_x) { }
+
+   /// Evaluate the coefficient at @a ip.
+   virtual double Eval(ElementTransformation &T,
+                       const IntegrationPoint &ip)
+   {
+      return map(GridFunctionCoefficient::Eval(T, ip));
+   }
+
+   inline void SetMapping(Mapping &f_x) { map = f_x; }
+private:
+   Mapping &map;
+};
+
+MappedGridFunctionCoefficient SIMPCoefficient(GridFunction *gf,
+                                              const int exponent, const double rho0)
+{
+   auto map = [exponent, rho0](const double x) {return simpRule(x, exponent, rho0); };
+   return MappedGridFunctionCoefficient(gf, map);
+}
+
+MappedGridFunctionCoefficient DerSIMPCoefficient(GridFunction *gf,
+                                                 const int exponent, const double rho0)
+{
+   auto map = [exponent, rho0](const double x) {return dsimpRuledx(x, exponent, rho0); };
+   return MappedGridFunctionCoefficient(gf, map);
+}
+
+MappedGridFunctionCoefficient Der2SIMPCoefficient(GridFunction *gf,
+                                                  const int exponent, const double rho0)
+{
+   auto map = [exponent, rho0](const double x) {return d2simpRuledx2(x, exponent, rho0); };
+   return MappedGridFunctionCoefficient(gf, map);
+}
+
+MappedGridFunctionCoefficient SigmoidCoefficient(GridFunction *gf)
+{
+   return MappedGridFunctionCoefficient(gf, sigmoid);
+}
+
+MappedGridFunctionCoefficient DerSigmoidCoefficient(GridFunction *gf)
+{
+   return MappedGridFunctionCoefficient(gf, dsigmoiddx);
+}
+} // end of namespace mfem
+
 #endif // end of proximalGalerkin.hpp
