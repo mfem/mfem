6402e7772936aab9b97963fc28b1173e23518715
Dohyun Kim <dohyun_kim@brown.edu>
2023-07-03 22:43:17 -0400

    volume projection

--- examples/proximalGalerkin.hpp
+++ examples/proximalGalerkin.hpp
@@ -91,6 +91,83 @@ MappedGridFunctionCoefficient DerSigmoidCoefficient(GridFunction *gf)
 {
    return MappedGridFunctionCoefficient(gf, dsigmoiddx);
 }
+
+double VolumeProjection(GridFunction &psi, const double target_volume,
+              const double tol=1e-12,
+              const int max_its=10)
+{
+   auto sigmoid_psi = SigmoidCoefficient(&psi);
+   auto der_sigmoid_psi = DerSigmoidCoefficient(&psi);
+
+   LinearForm int_sigmoid_psi(psi.FESpace());
+   int_sigmoid_psi.AddDomainIntegrator(new DomainLFIntegrator(sigmoid_psi));
+   LinearForm int_der_sigmoid_psi(psi.FESpace());
+   int_der_sigmoid_psi.AddDomainIntegrator(new DomainLFIntegrator(
+                                              der_sigmoid_psi));
+   bool done = false;
+   for (int k=0; k<max_its; k++) // Newton iteration
+   {
+      int_sigmoid_psi.Assemble(); // Recompute f(c) with updated ψ
+      const double f = int_sigmoid_psi.Sum() - target_volume;
+
+      int_der_sigmoid_psi.Assemble(); // Recompute df(c) with updated ψ
+      const double df = int_der_sigmoid_psi.Sum();
+
+      const double dc = -f/df;
+      psi += dc;
+      if (abs(dc) < tol) { done = true; break; }
+   }
+   if (!done)
+   {
+      mfem_warning("Projection reached maximum iteration without converging. Result may not be accurate.");
+   }
+   int_sigmoid_psi.Assemble();
+   return int_sigmoid_psi.Sum();
+}
+
+#ifdef MFEM_USE_MPI
+double ParVolumeProjection(ParGridFunction &psi, const double target_volume,
+                 const double tol=1e-12, const int max_its=10)
+{
+   auto sigmoid_psi = SigmoidCoefficient(&psi);
+   auto der_sigmoid_psi = DerSigmoidCoefficient(&psi);
+   auto comm = psi.ParFESpace()->GetComm();
+
+   ParLinearForm int_sigmoid_psi(psi.ParFESpace());
+   int_sigmoid_psi.AddDomainIntegrator(new DomainLFIntegrator(sigmoid_psi));
+   ParLinearForm int_der_sigmoid_psi(psi.ParFESpace());
+   int_der_sigmoid_psi.AddDomainIntegrator(new DomainLFIntegrator(
+                                              der_sigmoid_psi));
+   bool done = false;
+   for (int k=0; k<max_its; k++) // Newton iteration
+   {
+      int_sigmoid_psi.Assemble(); // Recompute f(c) with updated ψ
+      const double myf = int_sigmoid_psi.Sum();
+      double f;
+      MPI_Allreduce(&myf, &f, 1, MPI_DOUBLE, MPI_SUM, comm);
+      f -= target_volume;
+
+      int_der_sigmoid_psi.Assemble(); // Recompute df(c) with updated ψ
+      const double mydf = int_der_sigmoid_psi.Sum();
+      double df;
+      MPI_Allreduce(&mydf, &df, 1, MPI_DOUBLE, MPI_SUM, comm);
+
+      const double dc = -f/df;
+      psi += dc;
+      if (abs(dc) < tol) { done = true; break; }
+   }
+   int rank;
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   if (rank == 0 & !done)
+   {
+      mfem_warning("Projection reached maximum iteration without converging. Result may not be accurate.");
+   }
+   const double myf = int_sigmoid_psi.Sum();
+   double f;
+   MPI_Allreduce(&myf, &f, 1, MPI_DOUBLE, MPI_SUM, comm);
+   return f;
+}
+#endif // end of MFEM_USE_MPI for ParProjit
 } // end of namespace mfem
 
 #endif // end of proximalGalerkin.hpp
