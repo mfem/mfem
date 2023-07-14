ddc12ed1c6ad48369d9b01ebf5b3ae0d09f32024
arotem3 <arotem33@gmail.com>
2023-07-14 11:37:57 -0600

    normal face deriv transpose in 3d

--- tests/unit/fem/test_normal_face_derivative.cpp
+++ tests/unit/fem/test_normal_face_derivative.cpp
@@ -14,7 +14,7 @@
 
 using namespace mfem;
 
-constexpr double __C[] = {1.0, 2.0, 4.0};
+constexpr double __C[] = {1.0, 2.0, 3.1415};
 
 double f(const Vector& x)
 {
@@ -25,6 +25,91 @@ double f(const Vector& x)
    }
    
    return F;
+}
+
+// Constructs a sparse matrix representation of op by applying op to the columns
+// of the identity matrix. SLOW but just for testing.
+SparseMatrix PA2Sparse(const Operator * op);
+
+TEST_CASE("Normal Face Derivative", "[FaceRestriction]")
+{
+   // Set grid function f(x) = dot(c, x) and verify that normal derivatives
+   // computed by L2NormalDerivativeFaceRestriction are dot(c, n).
+
+   const int dim = GENERATE(2, 3);
+   const int nx = 3;
+   const int order = 4;
+
+   Mesh mesh = (dim == 2) ? Mesh::MakeCartesian2D(nx, nx, Element::QUADRILATERAL,
+                                                  true) : Mesh::MakeCartesian3D(nx, nx, nx, Element::HEXAHEDRON);
+   DG_FECollection fec(order, dim, BasisType::GaussLobatto);
+
+   FiniteElementSpace fes(&mesh, &fec);
+
+   GridFunction gf(&fes);
+
+   FaceType face_type = GENERATE(FaceType::Boundary, FaceType::Interior);
+
+   IntegrationRules irs(0, Quadrature1D::GaussLobatto);
+   const IntegrationRule& ir = irs.Get(mesh.GetFaceGeometry(0), 2 * order - 1);
+   FaceQuadratureSpace fqs(mesh, ir, face_type);
+
+   L2NormalDerivativeFaceRestriction dfr(fes, ElementDofOrdering::LEXICOGRAPHIC,
+                                         face_type);
+
+   QuadratureFunction qf(fqs, 2);
+   QuadratureFunction qfref(fqs, 2);
+   QuadratureFunction qff(fqs, 1);
+   FunctionCoefficient coef(f);
+   coef.Project(qff);
+   gf.ProjectCoefficient(coef);
+
+   auto geom = mesh.GetFaceGeometricFactors(ir, FaceGeometricFactors::NORMALS,
+                                            face_type);
+   auto ns = geom->normal.Read();
+
+   const int np = ir.Size();
+
+   for (int f=0; f < fqs.GetNumFaces(); ++f)
+   {
+      for (int p=0; p < np; ++p)
+      {
+         double val = 0.0;
+         for (int d = 0; d < dim; ++d)
+         {
+            double n = std::abs(ns[p + np * (d + dim * f)]);
+
+            val += __C[d] * n;
+         }
+
+         qfref[p + np * (0 + 2 * f)] = val / nx;
+         qfref[p + np * (1 + 2 * f)] = (face_type == FaceType::Interior) ?
+                                              (val / nx) : 0.0;
+      }
+   }
+
+   dfr.Mult(gf, qf);
+   qf -= qfref;
+
+   REQUIRE(qf.Normlinf() == MFEM_Approx(0.0));
+
+   // test transpose
+   SparseMatrix A = PA2Sparse(&dfr);
+   
+   Vector y(qf.Size());
+   y.Randomize(1);
+
+   Vector x(gf.Size());
+   x = 0.0;
+   Vector xref(gf.Size());
+   xref = 0.0;
+
+   dfr.AddMultTranspose(y, x);
+   A.AddMultTranspose(y, xref);
+
+   x -= xref;
+
+   REQUIRE(x.Normlinf() == MFEM_Approx(0.0));
 }
 
 SparseMatrix PA2Sparse(const Operator * op)
@@ -56,91 +141,4 @@ SparseMatrix PA2Sparse(const Operator * op)
 
    A.Finalize();
    return A;
-}
-
-TEST_CASE("Normal Face Derivative", "[FaceRestriction]")
-{
-   // Set grid function f(x) = dot(c, x) and verify that normal derivatives
-   // computed by L2NormalDerivativeFaceRestriction are dot(c, n).
-
-   const int dim = GENERATE(2, 3);
-   const int nx = 1;
-   const int order = 2;
-
-   Mesh mesh = (dim == 2) ? Mesh::MakeCartesian2D(nx, nx, Element::QUADRILATERAL,
-                                                  true) : Mesh::MakeCartesian3D(nx, nx, nx, Element::HEXAHEDRON);
-   // Mesh mesh("../../data/star.mesh");
-   DG_FECollection fec(order, dim, BasisType::GaussLobatto);
-
-   FiniteElementSpace fes(&mesh, &fec);
-
-   GridFunction gf(&fes);
-
-   FaceType face_type = GENERATE(FaceType::Boundary, FaceType::Interior);
-
-   std::cout << dim << " " << ((face_type == FaceType::Boundary) ? "Boundary" : "Interior") << "\n";
-
-   IntegrationRules irs(0, Quadrature1D::GaussLobatto);
-   const IntegrationRule& ir = irs.Get(mesh.GetFaceGeometry(0), 2 * order - 1);
-   FaceQuadratureSpace fqs(mesh, ir, face_type);
-
-   L2NormalDerivativeFaceRestriction dfr(fes, ElementDofOrdering::LEXICOGRAPHIC,
-                                         face_type);
-
-   QuadratureFunction qf(fqs, 2);
-   QuadratureFunction qfref(fqs, 2);
-   QuadratureFunction qff(fqs, 1);
-   FunctionCoefficient coef(f);
-   coef.Project(qff);
-   gf.ProjectCoefficient(coef);
-
-   auto geom = mesh.GetFaceGeometricFactors(ir, FaceGeometricFactors::NORMALS,
-                                            face_type);
-   auto ns = geom->normal.Read();
-
-   const int np = ir.Size();
-   const double rx = (dim == 2) ? (1.0 / nx) : (1.0 / nx / nx);
-
-   for (int f=0; f < fqs.GetNumFaces(); ++f)
-   {
-      for (int p=0; p < np; ++p)
-      {
-         double val = 0.0;
-         for (int d = 0; d < dim; ++d)
-         {
-            double n = std::abs(ns[p + np * (d + dim * f)]);
-
-            val += __C[d] * n;
-         }
-
-         qfref[p + np * (0 + 2 * f)] = val * rx;
-         qfref[p + np * (1 + 2 * f)] = (face_type == FaceType::Interior) ?
-                                              (val * rx) : 0.0;
-      }
-   }
-
-   dfr.Mult(gf, qf);
-   qf -= qfref;
-
-   std::cout << "\t " << ((qf.Normlinf() == MFEM_Approx(0.0)) ? "S" : "F") << "\n";
-
-   REQUIRE(qf.Normlinf() == MFEM_Approx(0.0));
-
-   // test transpose
-   SparseMatrix A = PA2Sparse(&dfr);
-   
-   Vector y(qf.Size());
-   y.Randomize(1);
-
-   Vector x(gf.Size());
-   x = 0.0;
-   Vector xref(gf.Size());
-   xref = 0.0;
-
-   dfr.AddMultTranspose(y, x);
-   A.AddMultTranspose(y, xref);
-
-   x -= xref;
-   
-   REQUIRE(x.Normlinf() == MFEM_Approx(0.0));
 }
