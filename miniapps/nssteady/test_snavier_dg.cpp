b130ba10d2cb26a3d574e280bba521069bb7d6ff
JAU-UEI CHEN <chenju@vpnb-user-128-15-246-121.llnl.gov>
2023-07-19 16:03:44 -0500

    create a template of 2d test for the LDG solver. The example is Kovasznay flow. The structure of the script largely follow ../navier/navier_kovasznay.cpp

--- miniapps/nssteady/test_snavier_dg.cpp
+++ miniapps/nssteady/test_snavier_dg.cpp
@@ -4,6 +4,36 @@
 #include <random>
 #include "snavier_dg.hpp"
 
+using namespace std;
+using namespace mfem;
+
+struct s_NavierContext
+{
+   int ser_ref_levels = 1;
+   int order = 1;
+   bool reduce_order_pressure = true;
+   bool reduce_order_stress = true;
+   double kinvis = 1.0 / 40.0;
+   double reference_pressure = 0.0;
+   double reynolds = 1.0 / kinvis;
+
+   bool iterative=false;
+   bool petsc =true;
+   const char *petscrc_file = "rc_direct";
+   bool use_ksp_solver = false;
+   int max_lin_it = 1000;
+   double lin_it_tol = 1e-8;
+   int setPrintLevel = 2;
+   int max_picard_it = 20;
+   double picard_it_tol = 1e-7;
+   bool ni = false;
+   bool visualization = false;
+   bool checkres = false;
+} ctx;
+
+void vel_kovasznay(const Vector &x, Vector &u);
+double pres_kovasznay(const Vector &x);
+
 int main(int argc, char *argv[])
 {
    // Initialize MPI.
@@ -11,89 +41,129 @@ int main(int argc, char *argv[])
    MPI_Init(&argc, &argv);
    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+   bool verbose = (myrank == 0);
 
    // Parse command-line options.
-   const char *mesh_file = "../../data/star.mesh";
-   int order = 1;
-   int ser_ref_levels = 0;
-   int par_ref_levels = 0;
-   double rel_tol = 1e-7;
-   double abs_tol = 1e-15;
-   int tot_iter = 100;
-   int print_level = 1;
-   bool visualization = false;
-
    mfem::OptionsParser args(argc, argv);
-   args.AddOption(&mesh_file, "-m", "--mesh",
-                     "Mesh file to use.");
-   args.AddOption(&ser_ref_levels,
+   args.AddOption(&ctx.ser_ref_levels,
                      "-rs",
                      "--refine-serial",
                      "Number of times to refine the mesh uniformly in serial.");
-   args.AddOption(&par_ref_levels,
-                     "-rp",
-                     "--refine-parallel",
-                     "Number of times to refine the mesh uniformly in parallel.");
-   args.AddOption(&order, "-o", "--order",
-                     "Finite element order (polynomial degree) or -1 for"
-                     " isoparametric space.");
-   args.AddOption(&visualization,
-                     "-vis",
-                     "--visualization",
-                     "-no-vis",
-                     "--no-visualization",
-                     "Enable or disable GLVis visualization.");
-   args.AddOption(&rel_tol,
-                  "-rel",
-                  "--relative-tolerance",
-                  "Relative tolerance for the Newton solve.");
-   args.AddOption(&abs_tol,
-                  "-abs",
-                  "--absolute-tolerance",
-                  "Absolute tolerance for the Newton solve.");
-   args.AddOption(&tot_iter,
-                  "-it",
-                  "--linear-iterations",
-                  "Maximum iterations for the linear solve.");
+   args.AddOption(&ctx.order, "-o", "--order",
+                     "Finite element order (polynomial degree)");
+   args.AddOption(&ctx.reduce_order_pressure, "-reduce-p", "--reduce-order-p", "-full-p",
+				  	 "--full-order-p",
+					 "Using k-1 order in approximation of pressure.");
+   args.AddOption(&ctx.reduce_order_stress, "-reduce-s", "--reduce-order-s", "-full-s",
+				  	 "--full-order-s",
+					 "Using k-1 order in approximation of gradient of velocity.");
+   args.AddOption(&ctx.petsc, "-petsc", "--use-petsc",
+                  	 "-no-petsc", "--no-use-petsc",
+					 "Enable or disable SC solver.");
+   args.AddOption(&ctx.petscrc_file, "-petscopts", "--petscopts",
+                  	  "PetscOptions file to use.");
+   args.AddOption(&ctx.use_ksp_solver, "-ksp", "--ksp_solver", "-lu",
+                  	  "--lu_solver", "Iterative solver or direct solver setup.");
+   args.AddOption(&ctx.lin_it_tol, "-lin_tol", "--lin_tolerance",
+                  	  "Tolerance in the iteration of the linear solver.");
+   args.AddOption(&ctx.max_lin_it, "-lin_maxit", "--lin_max_nonlin_it",
+                  	  "Maximum number of iterations of the linear solver.");
+   args.AddOption(&ctx.setPrintLevel, "-printl", "--print_level",
+                  	  "Setting the printlevel.");
+   args.AddOption(&ctx.picard_it_tol,
+                  	  "-pic-tol",
+					  "--pictard-tolerance",
+					  "Absolute tolerance for the Newton solve.");
+   args.AddOption(&ctx.max_picard_it,
+                  	  "-pic-it",
+					  "--picard-iterations",
+					  "Maximum iterations for the linear solve.");
+   args.AddOption(&ctx.ni,
+                  	  "-ni",
+					  "--enable-ni",
+					  "-no-ni",
+					  "--disable-ni",
+					  "Enable numerical integration rules.");
+   args.AddOption(&ctx.visualization,
+                      "-vis",
+					  "--visualization",
+					  "-no-vis",
+					  "--no-visualization",
+					  "Enable or disable GLVis visualization.");
+   args.AddOption(
+      &ctx.checkres,
+      "-cr",
+      "--checkresult",
+      "-no-cr",
+      "--no-checkresult",
+      "Enable or disable checking of the result. Returns -1 on failure.");
+
    args.Parse();
    if (!args.Good())
    {
-      if (myrank == 0)
+      if (verbose)
       {
          args.PrintUsage(std::cout);
       }
       MPI_Finalize();
       return 1;
    }
-   if (myrank == 0)
+   if (verbose)
    {
        args.PrintOptions(std::cout);
    }
 
-   // Read the (serial) mesh from the given mesh file on all processors.  We
-   // can handle triangular, quadrilateral, tetrahedral, hexahedral, surface
-   // and volume meshes with the same code.
-   mfem::Mesh mesh(mesh_file, 1, 1);
+   // Create the rectangle domain (0,1.5)x(2.0) that is composed
+   // by 2x4x2 triangle elements.
+   Mesh mesh = Mesh::MakeCartesian2D(2, 4, Element::TRIANGLE, false, 1.5,
+                                     2.0);
+
+   // Shift entire domain to left with 0.5 unit. Now the domain
+   // is (-0.5,1)x(2.0)
+   mesh.EnsureNodes();
+   GridFunction *nodes = mesh.GetNodes();
+   *nodes -= 0.5;
+
    int dim = mesh.Dimension();
-   for (int l = 0; l < ser_ref_levels; l++)
+   for (int l = 0; l < ctx.ser_ref_levels; l++)
    {
        mesh.UniformRefinement();
    }
 
-   // Define a parallel mesh by a partitioning of the serial mesh. Refine
-   // this mesh further in parallel to increase the resolution. Once the
-   // parallel mesh is defined, the serial mesh can be deleted.
-   mfem::ParMesh pmesh(MPI_COMM_WORLD, mesh);
+   ParMesh *pmesh = new ParMesh(MPI_COMM_WORLD, mesh);
    mesh.Clear();
-   {
-       for (int l = 0; l < par_ref_levels; l++)
-       {
-          pmesh.UniformRefinement();
-       }
-   }
 
+   // Define the coefficients (e.g. parameters, analytical solution/s).
+   //TODO
+
+   // Create solver
+   //TODO
+
+   // Set parameters of the Fixed Point Solver
+   //TODO
+
+   // Set parameters of the Linear Solvers
+   //TODO
 
 
    MPI_Finalize();
    return 0;
+}
+
+void vel_kovasznay(const Vector &x, Vector &u)
+{
+   double xi = x(0);
+   double yi = x(1);
+   double lam = 0.5 * ctx.reynolds
+                - sqrt(0.25 * ctx.reynolds * ctx.reynolds + 4.0 * M_PI * M_PI);
+   u(0) = 1.0 - exp(lam * xi) * cos(2.0 * M_PI * yi);
+   u(1) = lam / (2.0 * M_PI) * exp(lam * xi) * sin(2.0 * M_PI * yi);
+}
+
+double pres_kovasznay(const Vector &x)
+{
+   double xi = x(0);
+   double lam = 0.5 * ctx.reynolds
+                - sqrt(0.25 * ctx.reynolds * ctx.reynolds + 4.0 * M_PI * M_PI);
+   return 0.5 * (1.0 - exp(2.0 * lam * xi)) + ctx.reference_pressure;
 }
