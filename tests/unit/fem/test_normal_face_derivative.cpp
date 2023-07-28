f99fd2740dc2ea9b1ab70fb82a891925619dd408
arotem3 <arotem33@gmail.com>
2023-07-28 11:26:16 -0600

    normal restriction test

--- tests/unit/fem/test_normal_face_derivative.cpp
+++ tests/unit/fem/test_normal_face_derivative.cpp
@@ -111,6 +111,137 @@ TEST_CASE("Normal Face Derivative", "[FaceRestriction]")
 
    REQUIRE(x.Normlinf() == MFEM_Approx(0.0));
 }
+void rotate_3d_vertices(int *v, int ref_face, int rot);
+
+Mesh mesh_3d_orientation1(int face_perm_1, int face_perm_2)
+{
+   static const int dim = 3;
+   static const int nv = 12;
+   static const int nel = 2;
+   Mesh mesh(dim, nv, nel);
+   double x[dim];
+   x[0] = 0.0;   x[1] = 0.0;   x[2] = 0.0;
+   mesh.AddVertex(x);
+   x[0] = 1.0;   x[1] = 0.0;   x[2] = 0.0;
+   mesh.AddVertex(x);
+   x[0] = 2.0;   x[1] = 0.0;   x[2] = 0.0;
+   mesh.AddVertex(x);
+   x[0] = 0.0;   x[1] = 1.0;   x[2] = 0.0;
+   mesh.AddVertex(x);
+   x[0] = 1.0;   x[1] = 1.0;   x[2] = 0.0;
+   mesh.AddVertex(x);
+   x[0] = 2.0;   x[1] = 1.0;   x[2] = 0.0;
+   mesh.AddVertex(x);
+   x[0] = 0.0;   x[1] = 0.0;   x[2] = 1.0;
+   mesh.AddVertex(x);
+   x[0] = 1.0;   x[1] = 0.0;   x[2] = 1.0;
+   mesh.AddVertex(x);
+   x[0] = 2.0;   x[1] = 0.0;   x[2] = 1.0;
+   mesh.AddVertex(x);
+   x[0] = 0.0;   x[1] = 1.0;   x[2] = 1.0;
+   mesh.AddVertex(x);
+   x[0] = 1.0;   x[1] = 1.0;   x[2] = 1.0;
+   mesh.AddVertex(x);
+   x[0] = 2.0;   x[1] = 1.0;   x[2] = 1.0;
+   mesh.AddVertex(x);
+
+   int el[8];
+
+   el[0] = 0;
+   el[1] = 1;
+   el[2] = 4;
+   el[3] = 3;
+   el[4] = 6;
+   el[5] = 7;
+   el[6] = 10;
+   el[7] = 9;
+   rotate_3d_vertices(el, face_perm_1/4, face_perm_1%4);
+   mesh.AddHex(el);
+
+   el[0] = 1;
+   el[1] = 2;
+   el[2] = 5;
+   el[3] = 4;
+   el[4] = 7;
+   el[5] = 8;
+   el[6] = 11;
+   el[7] = 10;
+   rotate_3d_vertices(el, face_perm_2/4, face_perm_2%4);
+   mesh.AddHex(el);
+
+   mesh.FinalizeHexMesh(true);
+   mesh.GenerateBoundaryElements();
+   mesh.Finalize();
+   return mesh;
+}
+
+TEST_CASE("normal perm")
+{
+   const int order = 2;
+   const int perm1 = GENERATE(range(0, 24));
+   const int perm2 = GENERATE(range(0, 24));
+
+   Mesh mesh = mesh_3d_orientation1(perm1, perm2);
+   const int dim = mesh.Dimension();
+
+   DG_FECollection fec(order, dim, BasisType::GaussLobatto);
+   FiniteElementSpace fes(&mesh, &fec);
+
+   GridFunction gf(&fes);
+
+   FaceType face_type = FaceType::Interior;
+
+   IntegrationRules irs(0, Quadrature1D::GaussLobatto);
+   const IntegrationRule& ir = irs.Get(mesh.GetFaceGeometry(0), 2 * order - 1);
+   FaceQuadratureSpace fqs(mesh, ir, face_type);
+
+   L2NormalDerivativeFaceRestriction dfr(fes, ElementDofOrdering::LEXICOGRAPHIC,
+                                         face_type);
+
+   QuadratureFunction qf(fqs, 2);
+   QuadratureFunction qfref(fqs, 2);
+   QuadratureFunction qff(fqs, 1);
+   FunctionCoefficient coef([](const Vector& x)->double{return x[0]*x[1]*x[2];});
+   coef.Project(qff);
+   gf.ProjectCoefficient(coef);
+
+   const int np = ir.Size();
+
+   FunctionCoefficient dercoef([](const Vector& x)->double{return x[1]*x[2];});
+   GridFunction dgf(&fes);
+   dgf.ProjectCoefficient(dercoef);
+   qff.ProjectGridFunction(dgf);
+
+   for (int f = 0; f < fqs.GetNumFaces(); ++f)
+   {
+      for (int p = 0; p < np; ++p)
+      {
+         qfref(p + np * (0 + 2 * f)) = qff(p + np * f);
+         qfref(p + np * (1 + 2 * f)) = qff(p + np * f);
+      }
+   }
+
+   dfr.Mult(gf, qf);
+
+   // for (int i=0; i < qf.Size(); ++i)
+   // {
+   //    std::cout << qf(i) << " " << qfref(i) << "\n";
+   // }
+
+   double e = 0;
+
+   for (int i=0; i < qf.Size(); ++i)
+   {
+      e = std::max(e, std::abs( std::abs(qf(i)) - std::abs(qfref(i)) ));
+   }
+
+   // qf -= qfref;
+
+   // REQUIRE(qf.Normlinf() == MFEM_Approx(0.0));
+   REQUIRE(e == MFEM_Approx(0.0));
+
+}
+
 
 SparseMatrix PA2Sparse(const Operator * op)
 {
