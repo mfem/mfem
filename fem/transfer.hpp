56903bd4ddab6a405e6e13afad5757effaa63cc8
Will Pazner <will.e.p@gmail.com>
2023-08-09 11:20:01 -0700

    make style

--- fem/transfer.hpp
+++ fem/transfer.hpp
@@ -305,11 +305,18 @@ protected:
       std::pair<std::unique_ptr<SparseMatrix>,
           std::unique_ptr<SparseMatrix>> ComputeSparseRAndM_LH();
       void GetTDofs(const FiniteElementSpace& fes, const Vector& x, Vector& X) const;
-      void SetFromTDofs(const FiniteElementSpace& fes, const Vector& X, Vector& x) const;
-      void GetTDofsTranspose(const FiniteElementSpace& fes, const Vector& x, Vector& X) const;
-      void SetFromTDofsTranspose(const FiniteElementSpace& fes, const Vector& X, Vector& x) const;
+      void SetFromTDofs(const FiniteElementSpace& fes,
+                        const Vector& X,
+                        Vector& x) const;
+      void GetTDofsTranspose(const FiniteElementSpace& fes,
+                             const Vector& x,
+                             Vector& X) const;
+      void SetFromTDofsTranspose(const FiniteElementSpace& fes,
+                                 const Vector& X,
+                                 Vector& x) const;
       void TDofsListByVDim(const FiniteElementSpace& fes,
-                           int vdim, Array<int>& vdofs_list) const;
+                           int vdim,
+                           Array<int>& vdofs_list) const;
       /// Returns the inverse of an on-rank lumped mass matrix
       void LumpedMassInverse(Vector& ML_inv) const;
 
