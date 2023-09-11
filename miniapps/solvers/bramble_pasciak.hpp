223e4bceb58fd49c6a49507fdedc0904d3761349
Gabriel Esteban Pinochet Soto <pinochetsoto1@llnl.gov>
2023-09-11 11:21:27 -0700

    Cleaner code.
    - Add description to README
    - Remove commentaries in BPCG
    - Move assert into ConstructMassPreconditioner

--- miniapps/solvers/bramble_pasciak.hpp
+++ miniapps/solvers/bramble_pasciak.hpp
@@ -21,13 +21,13 @@
 //        B = -\int_\Omega (div_h u_h) q_h dx,
 //        u_h, v_h \in R_h (Raviart-Thomas finite element space),
 //        q_h \in W_h (piecewise discontinuous polynomials),
-// with a block transformation of the form X = AN - Id
+// with a block transformation of the form X = A*N - Id
 //                  X = [ M*invQ - Id    0   ]
 //                      [     B*invQ    -Id  ]
 // where N is defined by
 //                  N = [ invQ    0 ]
 //                      [   0     0 ]
-// and Q is constructed such that Q and M-Q are both s.p.d.
+// and Q is constructed such that Q and M-Q are both s.p.d..
 //
 // The solution x is then obtained by solving XAx = Xb with PCG as XA is s.p.d.
 //
