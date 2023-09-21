915984cdbf82b4a24e4d112b1db41d79be0721a6
dohyun <dohyun_kim@brown.edu>
2023-09-21 13:46:07 -0400

    additional header for ex38

---
+++ examples/ex38.hpp
@@ -0,0 +1,13 @@
+//                  MFEM Example 38
+//
+//
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+#include <functional>
+
+namespace mfem
+{
+
+} // end of namespace mfem
