245e7dc35c0bb1e2fd5459cb4ad8af0432db5387
Will Pazner <will.e.p@gmail.com>
2023-11-15 16:44:16 -0800

    Use std::vector instead of int* in Cubit mesh reader

--- mesh/mesh.hpp
+++ mesh/mesh.hpp
@@ -339,7 +339,7 @@ protected:
                                      const double *coordx,
                                      const double *coordy,
                                      const double *coordz,
-                                     const int **element_blocks);
+                                     const std::vector<std::vector<int>> &element_blocks);
 
    /// @brief Returns a pointer to a new mfem::Element based on the provided
    /// cubit element type. This is used internally to create the mesh elements
