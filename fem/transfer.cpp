c1bb8f2b73c468834f0051cd06292a10c897b89a
EB Chin <chin23@llnl.gov>
2023-11-15 16:26:39 -0800

    zero elem fix for H1 transfers

--- fem/transfer.cpp
+++ fem/transfer.cpp
@@ -781,7 +781,7 @@ std::unique_ptr<SparseMatrix>>
    int ndof_lor = fes_lor.GetNDofs();
 
    // If the local mesh is empty, skip all computations
-   if (nel_ho == 0) { return {nullptr, nullptr}; }
+   if (nel_ho == 0) { return {std::make_unique<SparseMatrix>(), std::make_unique<SparseMatrix>()}; }
 
    const CoarseFineTransformations& cf_tr = mesh_lor->GetRefinementTransforms();
 
