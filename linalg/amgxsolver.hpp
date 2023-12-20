3c17c6750874caa0bd776641c1171aa3ea749151
Aaron Fisher <fisher47@llnl.gov>
2023-12-20 12:08:10 -0800

    Significant improvements to the amgx solver doxygen.  Fixed a couple doxygen errors in ex37.

--- linalg/amgxsolver.hpp
+++ linalg/amgxsolver.hpp
@@ -28,7 +28,7 @@
 namespace mfem
 {
 
-/**
+/** @brief 
    MFEM wrapper for Nvidia's multigrid library, AmgX (github.com/NVIDIA/AMGX)
 
    AmgX requires building MFEM with CUDA, and AMGX enabled. For distributed
@@ -71,85 +71,119 @@ class AmgXSolver : public Solver
 public:
 
    /// Flags to configure AmgXSolver as a solver or preconditioner
-   enum AMGX_MODE {SOLVER, PRECONDITIONER};
+   enum AMGX_MODE 
+   {
+      /// Use the preconditioned conjugate gradient method with the AMG
+      /// V-cycle used as a proconditioner.  With the default configuration 
+      /// a block Jacobi smoother is used.
+      SOLVER,
+      /// Directly apply iterations of the AMG V cycle to the matrix 
+      /// With the default configuration this will be 2 iterations 
+      /// with block Jacobi smoother.
+      PRECONDITIONER
+   };
 
    /// Flag to check for convergence
    bool ConvergenceCheck;
 
-   /**
+   /** @brief
       Flags to determine whether user solver settings are defined internally in
       the source code or will be read through an external JSON file.
    */
-   enum CONFIG_SRC {INTERNAL, EXTERNAL, UNDEFINED};
+   enum CONFIG_SRC 
+   {
+      /// Configuration with be read directly from a string 
+      INTERNAL,
+      /// Configure will be read from a specified file
+      EXTERNAL, 
+      UNDEFINED};
 
    AmgXSolver();
 
-   /**
+   /** @brief
       Configures AmgX with a default configuration based on the AmgX mode, and
       verbosity. Assumes no MPI parallism.
    */
    AmgXSolver(const AMGX_MODE amgxMode_, const bool verbose);
 
-   /**
-      Once the solver configuration has been established through either the
-      ReadParameters method or the constructor, InitSerial will initialize the
-      library. If configuring with constructor, the constructor will make this
+   /** @brief Initilize the AmgX library for serial execution once
+      the solver configuration has been established through either the
+      ReadParameters method or the constructor.  The constructor will make this
       call.
    */
    void InitSerial();
 
 #ifdef MFEM_USE_MPI
 
-   /**
-      Configures AmgX with a default configuration based on the AmgX mode, and
-      verbosity. Pairs each MPI rank with one GPU.
+   /** @brief
+      Configures AmgX with a default configuration based on the AMGX_MODE
+      (AmgXSolver::SOLVER, AmgXSolver::PRECONDITIONER) 
+      and verbosity. Pairs each MPI rank with one GPU.
    */
    AmgXSolver(const MPI_Comm &comm, const AMGX_MODE amgxMode_, const bool verbose);
 
-   /**
-      Configures AmgX with a default configuration based on the AmgX mode, and
-      verbosity. Creates MPI teams around GPUs to support MPI ranks >
+   /** @brief
+      Configures AmgX with a default configuration based on the AMGX_MODE
+      (AmgXSolver::SOLVER, AmgXSolver::PRECONDITIONER) 
+      and verbosity. Creates MPI teams around GPUs to support more ranks than 
       GPUs. Consolidates linear solver data to avoid multiple ranks sharing
-      GPUs. Requires specifying number of devices in each compute node.
+      GPUs. Requires specifying number the of devices in each compute node as
+      @a nDevs.
    */
    AmgXSolver(const MPI_Comm &comm, const int nDevs,
               const AMGX_MODE amgx_Mode_, const bool verbose);
 
-   /**
-      Once the solver configuration has been established, either through the
-      constructor or the ReadParameters method, InitSerial will initialize the
-      library. If configuring with constructor, the constructor will make this
-      call.
+   /** @brief Initilize the AmgX library in parallel mode with exactly one
+      GPU per rank once the solver configuration has been established, 
+      either through the constructor or the AmgXSolver::ReadParameters 
+      method.  If configuring with constructor, the constructor will make 
+      this call.
    */
    void InitExclusiveGPU(const MPI_Comm &comm);
 
-   /**
-      Once the solver configuration has been established, either through the
-      ReadParameters method, InitMPITeams will initialize the library and create
-      MPI teams based on the number of devices on each node (nDevs). If
-      configuring with constructor, the constructor will make this call.
+   /** @brief Initilize the AmgX library and create MPI teams based on the number
+      of devices on each node @a nDevs.  If configuring with constructor, the 
+      constructor will make this call, otherwise this will need to be called 
+      after solver configuration  been established through the 
+      AmgXSolver::ReadParameters call.  
    */
    void InitMPITeams(const MPI_Comm &comm,
                      const int nDevs);
 #endif
 
-   /**
-      Sets Operator for AmgX library, either MFEM SparseMatrix or HypreParMatrix
+   /** @brief  Sets Operator that is going to be solved via AmgX.  
+      Supports operators based on either an MFEM SparseMatrix or 
+      HypreParMatrix.
    */
    virtual void SetOperator(const Operator &op);
 
-   /**
-      Replaces the matrix coefficients in the AmgX solver.
+   /** @brief Change the input operator that is being solved via AmgX.
+      Supports operators based on either an MFEM SparseMatrix or 
+      HypreParMatrix.
    */
    void UpdateOperator(const Operator &op);
 
+   /** @brief Untilize the AmgX library to solve the linear system
+       where the "matrix" is the AMG approximation to the operator set
+       by AmgXSolver::SetOperator.  If the mode is set to 
+       AmgXSolver::PRECONDITIONER the initial guess for the 
+       @a x vector will be set to zero, otherwise the value of @a x passed 
+       in will be used.
+   */
    virtual void Mult(const Vector& b, Vector& x) const;
 
+   /// Return the number of iterations that were executed during the last solve phase.
    int GetNumIterations();
 
+   /** @brief Read in the AMGx parameters either through a file or directly through a 
+       properly formated string.  If @a source is set to AmgXSolver::EXTERNAL 
+       the parameters are loaded from a filename set by @a config.  If If @a source is set  
+       to AmgXSolver::INTERNAL the parameters are set directly by the string 
+       defined by @a config.
+   */
    void ReadParameters(const std::string config, CONFIG_SRC source);
 
-   /**
+   /** @brief  Set up the AmgX library with the default paramaters.
       @param [in] amgxMode_ AmgXSolver::PRECONDITIONER,
                             AmgXSolver::SOLVER.
 
@@ -168,8 +202,10 @@ public:
    /// Add a check for convergence after applying Mult.
    void SetConvergenceCheck(bool setConvergenceCheck_=true);
 
+   /// Close down the AmgX library and free up any MPI Comms set up for it
    ~AmgXSolver();
 
+   /// Close down the AmgX library and free up any MPI Comms set up for it
    void Finalize();
 
 private:
@@ -181,36 +217,42 @@ private:
    CONFIG_SRC configSrc = UNDEFINED;
 
 #ifdef MFEM_USE_MPI
-   // Consolidates matrix diagonal and off diagonal data and uploads matrix to
-   // AmgX.
+   /** @brief  Consolidates matrix diagonal and off diagonal data and uploads 
+     matrix to AmgX.
+   */
    void SetMatrixMPIGPUExclusive(const HypreParMatrix &A,
                                  const Array<double> &loc_A,
                                  const Array<int> &loc_I, const Array<int64_t> &loc_J,
                                  const bool update_mat = false);
 
-   // Consolidates matrix diagonal and off diagonal data for all ranks in an MPI
-   // team. Root rank of each MPI team holds the the consolidated data and sets
-   // matrix.
+   /** @brief Consolidates matrix diagonal and off diagonal data for all ranks in an MPI
+      team. Root rank of each MPI team holds the the consolidated data and sets
+      matrix.
+   */
    void SetMatrixMPITeams(const HypreParMatrix &A, const Array<double> &loc_A,
                           const Array<int> &loc_I, const Array<int64_t> &loc_J,
                           const bool update_mat = false);
 
-   // The following methods consolidate array data to the root node in a MPI
-   // team.
+   /// Consolidate array data to the root node in a MPI team.
    void GatherArray(const Array<double> &inArr, Array<double> &outArr,
                     const int mpiTeamSz, const MPI_Comm &mpiTeam) const;
 
+   /// Consolidate array data to the root node in a MPI team.
    void GatherArray(const Vector &inArr, Vector &outArr,
                     const int mpiTeamSz, const MPI_Comm &mpiTeam) const;
 
+   /// Consolidate array data to the root node in a MPI team.
    void GatherArray(const Array<int> &inArr, Array<int> &outArr,
                     const int mpiTeamSz, const MPI_Comm &mpiTeam) const;
 
+   /// Consolidate array data to the root node in a MPI team.
    void GatherArray(const Array<int64_t> &inArr, Array<int64_t> &outArr,
                     const int mpiTeamSz, const MPI_Comm &mpiTeam) const;
 
-   // The following methods consolidate array data to the root node in a MPI
-   // team as well as store array partitions and displacements.
+   /** @brief Consolidate array data to the root node in a MPI
+      team as well as store array partitions and displacements in
+      @a Apart and @a Adisp.
+   */
    void GatherArray(const Vector &inArr, Vector &outArr,
                     const int mpiTeamSz, const MPI_Comm &mpiTeamComm,
                     Array<int> &Apart, Array<int> &Adisp) const;
@@ -299,10 +341,10 @@ private:
    // AmgX resource object.
    static AMGX_resources_handle   rsrc;
 
-   // Set the ID of the corresponding GPU used by this process.
+   /// Set the ID of the corresponding GPU used by this process.
    void SetDeviceIDs(const int nDevs);
 
-   // Initialize all MPI communicators.
+   /// Initialize all MPI communicators.
 #ifdef MFEM_USE_MPI
    void InitMPIcomms(const MPI_Comm &comm, const int nDevs);
 #endif
