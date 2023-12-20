163b59145c746a4ff3c516446188df330c958917
Eric B. Chin <chin23@llnl.gov>
2023-12-20 10:55:49 -0800

    buildsys changes for adding tribol

---
+++ miniapps/tribol/ContactPatchTest.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2010-2023, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+
+
+#include "mfem.hpp"
+
+#include "redecomp/redecomp.hpp"
+
+int main(int argc, char *argv[])
+{
+   // Initialize MPI and HYPRE.
+   mfem::Mpi::Init();
+   int num_ranks = mfem::Mpi::WorldSize();
+   int myid = mfem::Mpi::WorldRank();
+
+   return 0;
+}
