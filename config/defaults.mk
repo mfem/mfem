5c731a519ed6c6dee7958f7e12bf5ccd54ed78ad
Eric B. Chin <chin23@llnl.gov>
2023-12-22 16:55:57 -0800

    add checks for patch test

--- config/defaults.mk
+++ config/defaults.mk
@@ -577,8 +577,10 @@ ifeq ($(MFEM_USE_TRIBOL),YES)
 endif
 AXOM_DIR = @MFEM_DIR@/../axom
 TRIBOL_DIR = @MFEM_DIR@/../tribol
-TRIBOL_OPT = -I$(TRIBOL_DIR)/include -I$(AXOM_DIR)/include
-TRIBOL_LIB = -L$(TRIBOL_DIR)/lib -ltribol -lredecomp
+TRIBOL_OPT = -I$(TRIBOL_DIR)/include -I$(AXOM_DIR)/include -I$(CONDUIT_DIR)/include/conduit
+TRIBOL_LIB = -L$(TRIBOL_DIR)/lib -ltribol -lredecomp -L$(AXOM_DIR)/lib -laxom_mint\
+   -laxom_sidre -laxom_slam -laxom_slic -laxom_core -lmfem -L$(CONDUIT_DIR)/lib -lconduit\
+   -lconduit_blueprint -lconduit_blueprint_mpi -lconduit_relay -lconduit_relay_mpi
 
 # Enzyme configuration
 
