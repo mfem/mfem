60281bda7494bc0a8bea02fb5d73cd92b67875d0
dohyun <dohyun_kim@brown.edu>
2024-01-16 16:25:09 -0500

    logger

--- miniapps/topopt/helper.cpp
+++ miniapps/topopt/helper.cpp
@@ -1,77 +1,81 @@
 #include "helper.hpp"
+#include <string>
+#include <vector>
+#include <iostream>
 namespace mfem
 {
 GridFunction *MakeGridFunction(FiniteElementSpace *fes)
 {
-    #ifdef MFEM_USE_MPI
-    auto pfes = dynamic_cast<ParFiniteElementSpace*>(fes);
-    if (pfes)
-    {
+#ifdef MFEM_USE_MPI
+   auto pfes = dynamic_cast<ParFiniteElementSpace*>(fes);
+   if (pfes)
+   {
       return new ParGridFunction(pfes);
-    }
-    else
-    {
+   }
+   else
+   {
       return new GridFunction(fes);
-    }
-    #else
-    return new GridFunction(fes);
-    #endif
+   }
+#else
+   return new GridFunction(fes);
+#endif
 }
 LinearForm *MakeLinearForm(FiniteElementSpace *fes)
 {
-    #ifdef MFEM_USE_MPI
-    auto pfes = dynamic_cast<ParFiniteElementSpace*>(fes);
-    if (pfes)
-    {
+#ifdef MFEM_USE_MPI
+   auto pfes = dynamic_cast<ParFiniteElementSpace*>(fes);
+   if (pfes)
+   {
       return new ParLinearForm(pfes);
-    }
-    else
-    {
+   }
+   else
+   {
       return new LinearForm(fes);
-    }
-    #else
-    return new LinearForm(fes);
-    #endif
+   }
+#else
+   return new LinearForm(fes);
+#endif
 }
 NonlinearForm *MakeNonlinearForm(FiniteElementSpace *fes)
 {
-    #ifdef MFEM_USE_MPI
-    auto pfes = dynamic_cast<ParFiniteElementSpace*>(fes);
-    if (pfes)
-    {
+#ifdef MFEM_USE_MPI
+   auto pfes = dynamic_cast<ParFiniteElementSpace*>(fes);
+   if (pfes)
+   {
       return new ParNonlinearForm(pfes);
-    }
-    else
-    {
+   }
+   else
+   {
       return new NonlinearForm(fes);
-    }
-    #else
-    return new NonlinearForm(fes);
-    #endif
+   }
+#else
+   return new NonlinearForm(fes);
+#endif
 }
 BilinearForm *MakeBilinearForm(FiniteElementSpace *fes)
 {
-    #ifdef MFEM_USE_MPI
-    auto pfes = dynamic_cast<ParFiniteElementSpace*>(fes);
-    if (pfes)
-    {
+#ifdef MFEM_USE_MPI
+   auto pfes = dynamic_cast<ParFiniteElementSpace*>(fes);
+   if (pfes)
+   {
       return new ParBilinearForm(pfes);
-    }
-    else
-    {
+   }
+   else
+   {
       return new BilinearForm(fes);
-    }
-    #else
-    return new BilinearForm(fes);
-    #endif
+   }
+#else
+   return new BilinearForm(fes);
+#endif
 }
-MixedBilinearForm *MakeMixedBilinearForm(FiniteElementSpace *trial_fes, FiniteElementSpace *test_fes)
+MixedBilinearForm *MakeMixedBilinearForm(FiniteElementSpace *trial_fes,
+                                         FiniteElementSpace *test_fes)
 {
-    #ifdef MFEM_USE_MPI
-    auto trial_pfes = dynamic_cast<ParFiniteElementSpace*>(trial_fes);
-    auto test_pfes = dynamic_cast<ParFiniteElementSpace*>(test_fes);
-    if (trial_pfes)
-    {
+#ifdef MFEM_USE_MPI
+   auto trial_pfes = dynamic_cast<ParFiniteElementSpace*>(trial_fes);
+   auto test_pfes = dynamic_cast<ParFiniteElementSpace*>(test_fes);
+   if (trial_pfes)
+   {
       if (test_pfes)
       {
          return new ParMixedBilinearForm(trial_pfes, test_pfes);
@@ -80,13 +84,75 @@ MixedBilinearForm *MakeMixedBilinearForm(FiniteElementSpace *trial_fes, FiniteEl
       {
          return new MixedBilinearForm(trial_fes, test_fes);
       }
-    }
-    else
-    {
+   }
+   else
+   {
       return new MixedBilinearForm(trial_fes, test_fes);
-    }
-    #else
-    return new MixedBilinearForm(trial_fes, test_fes);
-    #endif
+   }
+#else
+   return new MixedBilinearForm(trial_fes, test_fes);
+#endif
+}
+
+
+TableLogger::TableLogger(std::ostream &os): os(os), var_name_printed(false),
+   my_rank(0)
+{
+#ifdef MFEM_USE_MPI
+   int mpi_is_initialized = Mpi::IsInitialized();
+   if (mpi_is_initialized) { my_rank = Mpi::WorldRank(); }
+#endif
+
+}
+
+void TableLogger::Append(const std::string name, double &val)
+{
+   names.push_back(name);
+   monitored_double_data.push_back(&val);
+   data_order.push_back(dtype::DOUBLE);
+}
+
+void TableLogger::Append(const std::string name, int &val)
+{
+   names.push_back(name);
+   monitored_int_data.push_back(&val);
+   data_order.push_back(dtype::INT);
+}
+
+void TableLogger::Print()
+{
+   if (my_rank == 0)
+   {
+      if (!var_name_printed)
+      {
+         var_name_printed = true;
+         for (auto &name : names) { os << std::setw(10) << name << "\t";}
+         os << "\b\b";
+         os << "\n";
+      }
+      int i_double(0), i_int(0);
+      for (auto d: data_order)
+      {
+         switch (d)
+         {
+            case dtype::DOUBLE:
+            {
+               os << std::setw(10) << *monitored_double_data[i_double++] << ",\t";
+               break;
+            }
+            case dtype::INT:
+            {
+               os << std::setw(10) << *monitored_int_data[i_int++] << ",\t";
+               break;
+            }
+            default:
+            {
+               MFEM_ABORT("Unknown data type. See, TableLogger::dtype");
+            }
+         }
+      }
+      os << "\b\b"; // remove the last ,\t
+      os << std::endl;
+   }
 }
 }
--- miniapps/topopt/helper.hpp
+++ miniapps/topopt/helper.hpp
@@ -1,10 +1,43 @@
 #pragma once
 #include "mfem.hpp"
+#include <vector>
+#include <string>
+#include <iostream>
 namespace mfem
 {
 GridFunction *MakeGridFunction(FiniteElementSpace *fes);
 LinearForm *MakeLinearForm(FiniteElementSpace *fes);
 NonlinearForm *MakeNonlinearForm(FiniteElementSpace *fes);
 BilinearForm *MakeBilinearForm(FiniteElementSpace *fes);
-MixedBilinearForm *MakeMixedBilinearForm(FiniteElementSpace *trial_fes, FiniteElementSpace *test_fes);
+MixedBilinearForm *MakeMixedBilinearForm(FiniteElementSpace *trial_fes,
+                                         FiniteElementSpace *test_fes);
+
+class TableLogger
+{
+public:
+   enum dtype {DOUBLE, INT};
+protected:
+   // Array<double*> monitored_double_data;
+   std::vector<double*> monitored_double_data;
+   std::vector<int*> monitored_int_data;
+   std::vector<dtype> data_order;
+   std::vector<std::string> names;
+   std::ostream &os;
+   bool var_name_printed;
+   int my_rank;
+   // Array<double
+   // MixedBilinearForm blabla();
+private:
+
+public:
+   TableLogger(std::ostream &os=mfem::out);
+
+   void Append(const std::string name, double &val);
+
+   void Append(const std::string name, int &val);
+
+   void Print();
+
+
+};
 }
