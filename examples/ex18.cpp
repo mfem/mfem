ec74fa82ecd67d90f7643dd3c210591211c036b4
dohyun <dohyun_kim@brown.edu>
2024-01-23 12:58:10 -0500

    add hyperbolic to changelog and ex18 comment update

--- examples/ex18.cpp
+++ examples/ex18.cpp
@@ -32,7 +32,7 @@
 //               method. An additional factor can be tuned by passing the --cfl
 //               (or -c shorter) flag.
 //
-//               The example demonstrates usage of DGHyperbolicConservationLaws
+//               The example demonstrates usage of user-defined nonlinear form
 //               that wraps NonlinearFormIntegrators containing element and face
 //               integration schemes. In this case the system also involves an
 //               external approximate Riemann solver for the DG interface flux.
