2887738161f203cbb7460ddf5a1af8d62a911022
Eric B. Chin <chin23@llnl.gov>
2024-02-13 16:43:33 -0800

    add deprecated version

--- linalg/hypre.cpp
+++ linalg/hypre.cpp
@@ -3017,6 +3017,20 @@ void GatherBlockOffsetData(MPI_Comm comm, const int rank, const int nprocs,
    }
 }
 
+HypreParMatrix * HypreParMatrixFromBlocks(Array2D<HypreParMatrix*> &blocks,
+                                          Array2D<double> *blockCoeff)
+{
+   Array2D<const HypreParMatrix*> constBlocks(blocks.NumRows(), blocks.NumCols());
+   for (int i = 0; i < blocks.NumRows(); ++i)
+   {
+      for (int j = 0; j < blocks.NumCols(); ++j)
+      {
+         constBlocks(i, j) = blocks(i, j);
+      }
+   }
+   return HypreParMatrixFromBlocks(constBlocks, blockCoeff);
+}
+
 HypreParMatrix * HypreParMatrixFromBlocks(Array2D<const HypreParMatrix*>
                                           &blocks,
                                           Array2D<double> *blockCoeff)
