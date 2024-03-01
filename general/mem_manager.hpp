a14fea50f60412ee3e41f61e43a9e82681a1ac46
Julian Andrej <andrej1@llnl.gov>
2024-03-01 08:50:37 -0800

    Revert "change order of attribute and comment for doxygen"
    This reverts commit fdc724036d66d13457c6946bd3c574df56a9af18.

--- general/mem_manager.hpp
+++ general/mem_manager.hpp
@@ -603,42 +603,42 @@ private:
 
    template <typename T> friend class Memory;
 
-   /// Host memory type set during the Setup.
 #ifdef MFEM_USE_ENZYME
    __attribute__((enzyme_inactive))
 #endif
+   /// Host memory type set during the Setup.
    static MemoryType host_mem_type;
 
-   /// Device memory type set during the Setup.
 #ifdef MFEM_USE_ENZYME
    __attribute__((enzyme_inactive))
 #endif
+   /// Device memory type set during the Setup.
    static MemoryType device_mem_type;
 
-   /// Allow to detect if a global memory manager instance exists.
 #ifdef MFEM_USE_ENZYME
    __attribute__((enzyme_inactive))
 #endif
+   /// Allow to detect if a global memory manager instance exists.
    static bool exists;
 
    /// Return true if the global memory manager instance exists.
    static bool Exists() { return exists; }
 
-   /// Array defining the dual MemoryType for each MemoryType
-   /** The dual of a host MemoryType is a device MemoryType and vice versa: the
-       dual of a device MemoryType is a host MemoryType. */
 #ifdef MFEM_USE_ENZYME
    __attribute__((enzyme_inactive))
 #endif
+   /// Array defining the dual MemoryType for each MemoryType
+   /** The dual of a host MemoryType is a device MemoryType and vice versa: the
+       dual of a device MemoryType is a host MemoryType. */
    static MemoryType dual_map[MemoryTypeSize];
 
    /// Update the dual memory type of @a mt to be @a dual_mt.
    static void UpdateDualMemoryType(MemoryType mt, MemoryType dual_mt);
 
-   /// True if Configure() was called.
 #ifdef MFEM_USE_ENZYME
    __attribute__((enzyme_inactive))
 #endif
+   /// True if Configure() was called.
    static bool configured;
 
    /// Host and device allocator names for Umpire.
@@ -658,11 +658,11 @@ private: // Static methods used by the Memory<T> class
    static void *New_(void *h_tmp, size_t bytes, MemoryType h_mt,
                      MemoryType d_mt, unsigned valid_flags, unsigned &flags);
 
-   /// Register an external pointer of the given MemoryType.
-   /// Return the host pointer.
 #ifdef MFEM_USE_ENZYME
    __attribute__((enzyme_inactive, enzyme_nofree))
 #endif
+   /// Register an external pointer of the given MemoryType.
+   /// Return the host pointer.
    static void *Register_(void *ptr, void *h_ptr, size_t bytes, MemoryType mt,
                           bool own, bool alias, unsigned &flags);
 
@@ -679,10 +679,10 @@ private: // Static methods used by the Memory<T> class
    static void SetDeviceMemoryType_(void *h_ptr, unsigned flags,
                                     MemoryType d_mt);
 
-   /// Un-register and free memory identified by its host pointer.
 #ifdef MFEM_USE_ENZYME
    __attribute__((enzyme_function_like("free")))
 #endif
+   /// Un-register and free memory identified by its host pointer.
    static void Delete_(void *h_ptr, MemoryType mt, unsigned flags);
 
    /// Free device memory identified by its host pointer
@@ -692,12 +692,11 @@ private: // Static methods used by the Memory<T> class
    static bool MemoryClassCheck_(MemoryClass mc, void *h_ptr,
                                  MemoryType h_mt, size_t bytes, unsigned flags);
 
-   /// Return a pointer to the memory identified by the host pointer h_ptr for
-   /// access with the given MemoryClass.
 #ifdef MFEM_USE_ENZYME
    __attribute__((enzyme_function_like("__dynamic_cast")))
 #endif
-
+   /// Return a pointer to the memory identified by the host pointer h_ptr for
+   /// access with the given MemoryClass.
    static void *ReadWrite_(void *h_ptr, MemoryType h_mt, MemoryClass mc,
                            size_t bytes, unsigned &flags);
 
@@ -717,17 +716,17 @@ private: // Static methods used by the Memory<T> class
                           size_t alias_bytes, unsigned base_flags,
                           unsigned &alias_flags);
 
+#ifdef MFEM_USE_ENZYME
+   __attribute__((enzyme_inactive, enzyme_nofree))
+#endif
    /// Return the type the of the currently valid memory.
    /// If more than one types are valid, return a device type.
-#ifdef MFEM_USE_ENZYME
-   __attribute__((enzyme_inactive, enzyme_nofree))
-#endif
    static MemoryType GetDeviceMemoryType_(void *h_ptr, bool alias);
 
-   /// Return the type the of the host memory.
 #ifdef MFEM_USE_ENZYME
    __attribute__((enzyme_inactive, enzyme_nofree))
 #endif
+   /// Return the type the of the host memory.
    static MemoryType GetHostMemoryType_(void *h_ptr);
 
    /// Verify that h_mt and h_ptr's h_mt (memory or alias) are equal.
