46824afc0c21862173a8ca3d4f4d5c232341b593
Tucker Babcock <tbabcock03@gmail.com>
2024-03-01 17:04:36 -0700

    hack to get around lack of a real parallel implementation

--- fem/nonlinearform.cpp
+++ fem/nonlinearform.cpp
@@ -353,9 +353,64 @@ double NonlinearForm::GetGridFunctionEnergy(const Vector &x) const
       }
    }
 
+   // if (internal_boundary_face_integs.Size())
+   // {
+   //    MFEM_ABORT("TODO: add energy contribution from internal boundary face terms");
+   // }
+
    if (internal_boundary_face_integs.Size())
    {
-      MFEM_ABORT("TODO: add energy contribution from internal boundary face terms");
+      // Which internal boundary attributes need to be processed?
+      Array<int> bdr_attr_marker(mesh->bdr_attributes.Size() ?
+                                 mesh->bdr_attributes.Max() : 0);
+      bdr_attr_marker = 0;
+      for (int k = 0; k < internal_boundary_face_integs.Size(); k++)
+      {
+         if (internal_boundary_face_integs_marker[k] == NULL)
+         {
+            bdr_attr_marker = 1;
+            break;
+         }
+         auto &bdr_marker = *internal_boundary_face_integs_marker[k];
+         MFEM_ASSERT(bdr_marker.Size() == bdr_attr_marker.Size(),
+                     "invalid boundary marker for internal boundary face "
+                     "integrator #" << k << ", counting from zero");
+         for (int i = 0; i < bdr_attr_marker.Size(); i++)
+         {
+            bdr_attr_marker[i] |= bdr_marker[i];
+         }
+      }
+
+      Array<int> vdofs2;
+      for (int i = 0; i < mesh->GetNBE(); i++)
+      {
+         const int bdr_attr = mesh->GetBdrAttribute(i);
+         if (bdr_attr_marker[bdr_attr-1] == 0) { continue; }
+
+         auto *tr = mesh->GetInternalBdrFaceTransformations(i);
+         if (tr != nullptr)
+         {
+            fes->GetElementVDofs(tr->Elem1No, vdofs);
+            fes->GetElementVDofs(tr->Elem2No, vdofs2);
+            vdofs.Append(vdofs2);
+
+            x.GetSubVector(vdofs, el_x);
+
+            const auto *fe1 = fes->GetFE(tr->Elem1No);
+            const auto *fe2 = fes->GetFE(tr->Elem2No);
+            for (int k = 0; k < internal_boundary_face_integs.Size(); k++)
+            {
+               if (internal_boundary_face_integs_marker[k] &&
+                   (*internal_boundary_face_integs_marker[k])[bdr_attr - 1] == 0)
+               {
+                  continue;
+               }
+
+               energy += internal_boundary_face_integs[k]->GetFaceEnergy(
+                            *fe1, *fe2, *tr, el_x);
+            }
+         }
+      }
    }
 
    return energy;
