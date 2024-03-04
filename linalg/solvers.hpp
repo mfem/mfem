771ecab05bab8ca4162ba4dfff382167320fd886
Seth Watts <watts24@llnl.gov>
2024-03-04 15:21:41 -0800

    Fix formatting

--- linalg/solvers.hpp
+++ linalg/solvers.hpp
@@ -169,14 +169,14 @@ protected:
 
    ///@}
 
-   /** @brief Return the standard (l2, i.e., Euclidean) inner product of 
+   /** @brief Return the standard (l2, i.e., Euclidean) inner product of
        @a x and @a y
-       @details Overriding this method in a derived class enables a 
+       @details Overriding this method in a derived class enables a
        custom inner product.
       */
    virtual double Dot(const Vector &x, const Vector &y) const;
 
-   /// Return the inner product norm of @a x, using the inner product defined by Dot() 
+   /// Return the inner product norm of @a x, using the inner product defined by Dot()
    double Norm(const Vector &x) const { return sqrt(Dot(x, x)); }
 
    /// Monitor both the residual @a r and the solution @a x
