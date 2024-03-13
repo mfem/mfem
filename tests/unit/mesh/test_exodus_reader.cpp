1b319b593c372b7e683e2771b3ab4949ff574e4b
Edward Palmer <edward.palmer@ukaea.uk>
2024-03-13 10:54:09 +0000

    Added unit tests for Exodus reader.

---
+++ tests/unit/mesh/test_exodus_reader.cpp
@@ -0,0 +1,135 @@
+// Copyright (c) 2010-2024, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#include "mfem.hpp"
+#include "unit_tests.hpp"
+
+using namespace mfem;
+
+TEST_CASE("Exodus Hex8", "[Mesh]")
+{
+#ifdef MFEM_USE_NETCDF
+   const std::string fname = "simple-cube-hex8.e";
+
+   const std::string & fpath = ("data/" + fname);
+
+   Mesh mesh(fpath);
+
+   REQUIRE(mesh.Dimension() == 3);
+   REQUIRE(mesh.GetNE() == 27);
+   REQUIRE(mesh.GetNV() == 64);
+   REQUIRE(mesh.GetNodalFESpace() == nullptr);
+   REQUIRE(mesh.HasGeometry(Geometry::CUBE));
+#endif
+}
+
+TEST_CASE("Exodus Hex27", "[Mesh]")
+{
+#ifdef MFEM_USE_NETCDF
+   const std::string fname = "simple-cube-hex27.e";
+
+   const std::string & fpath = ("data/" + fname);
+
+   Mesh mesh(fpath);
+
+   REQUIRE(mesh.Dimension() == 3);
+   REQUIRE(mesh.GetNE() == 27);
+   REQUIRE(mesh.GetNV() == 64);
+   REQUIRE(mesh.GetNodalFESpace() != nullptr);
+   REQUIRE(mesh.HasGeometry(Geometry::CUBE));
+#endif
+}
+
+TEST_CASE("Exodus Tet4", "[Mesh]")
+{
+#ifdef MFEM_USE_NETCDF
+   const std::string fname = "simple-cube-tet4.e";
+
+   const std::string & fpath = ("data/" + fname);
+
+   Mesh mesh(fpath);
+
+   REQUIRE(mesh.Dimension() == 3);
+   REQUIRE(mesh.GetNE() == 295);
+   REQUIRE(mesh.GetNV() == 98);
+   REQUIRE(mesh.GetNodalFESpace() == nullptr);
+   REQUIRE(mesh.HasGeometry(Geometry::TETRAHEDRON));
+#endif
+}
+
+TEST_CASE("Exodus Tet10", "[Mesh]")
+{
+#ifdef MFEM_USE_NETCDF
+   const std::string fname = "simple-cube-tet10.e";
+
+   const std::string & fpath = ("data/" + fname);
+
+   Mesh mesh(fpath);
+
+   REQUIRE(mesh.Dimension() == 3);
+   REQUIRE(mesh.GetNE() == 295);
+   REQUIRE(mesh.GetNV() == 98);
+   REQUIRE(mesh.GetNodalFESpace() != nullptr);
+   REQUIRE(mesh.HasGeometry(Geometry::TETRAHEDRON));
+#endif
+}
+
+TEST_CASE("Exodus Pyramid5", "[Mesh]")
+{
+#ifdef MFEM_USE_NETCDF
+   const std::string fname = "simple-cube-pyramid5.e";
+
+   const std::string & fpath = ("data/" + fname);
+
+   Mesh mesh(fpath);
+
+   REQUIRE(mesh.Dimension() == 3);
+   REQUIRE(mesh.GetNE() == 750);
+   REQUIRE(mesh.GetNV() == 341);
+   REQUIRE(mesh.GetNodalFESpace() == nullptr);
+   REQUIRE(mesh.HasGeometry(Geometry::PYRAMID));
+#endif
+}
+
+TEST_CASE("Exodus Wedge6", "[Mesh]")
+{
+#ifdef MFEM_USE_NETCDF
+   const std::string fname = "simple-cube-wedge6.e";
+
+   const std::string & fpath = ("data/" + fname);
+
+   Mesh mesh(fpath);
+
+   REQUIRE(mesh.Dimension() == 3);
+   REQUIRE(mesh.GetNE() == 250);
+   REQUIRE(mesh.GetNV() == 216);
+   REQUIRE(mesh.GetNodalFESpace() == nullptr);
+   REQUIRE(mesh.HasGeometry(Geometry::PRISM));
+#endif
+}
+
+TEST_CASE("Exodus Wedge18", "[Mesh]")
+{
+#ifdef MFEM_USE_NETCDF
+   const std::string fname = "simple-cube-wedge18.e";
+
+   const std::string & fpath = ("data/" + fname);
+
+   Mesh mesh(fpath);
+
+   REQUIRE(mesh.Dimension() == 3);
+   REQUIRE(mesh.GetNE() == 250);
+   REQUIRE(mesh.GetNV() == 216);
+   REQUIRE(mesh.GetNodalFESpace() != nullptr);
+   REQUIRE(mesh.HasGeometry(Geometry::PRISM));
+#endif
+}
+
