e53a3df9c58899f73f9c16e1a0514f3b2bd0693c
Eric B. Chin <chin23@llnl.gov>
2024-04-02 10:18:27 -0700

    simplified host configs

--- miniapps/tribol/tribol-gcc-basictpl.cmake
+++ miniapps/tribol/tribol-gcc-basictpl.cmake
@@ -2,10 +2,9 @@
 # CMake executable path: /usr/bin/cmake
 #------------------------------------------------------------------------------
 
-set(COMPILER_HOME "/usr" CACHE PATH "")
-set(CMAKE_C_COMPILER "${COMPILER_HOME}/bin/gcc" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "${COMPILER_HOME}/bin/g++" CACHE PATH "")
-set(CMAKE_Fortran_COMPILER "/usr/bin/gfortran" CACHE PATH "")
+set(CMAKE_C_COMPILER "gcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "gfortran" CACHE PATH "")
 
 #------------------------------------------------------------------------------
 # MPI
@@ -13,17 +12,22 @@ set(CMAKE_Fortran_COMPILER "/usr/bin/gfortran" CACHE PATH "")
 
 set(ENABLE_MPI ON CACHE BOOL "")
 
-set(MPI_HOME             "/usr" CACHE PATH "")
-set(MPI_C_COMPILER       "${MPI_HOME}/bin/mpicc"   CACHE PATH "")
-set(MPI_CXX_COMPILER     "${MPI_HOME}/bin/mpicxx"  CACHE PATH "")
-set(MPI_Fortran_COMPILER "${MPI_HOME}/bin/mpif90" CACHE PATH "")
+set(MPI_C_COMPILER       "mpicc"   CACHE PATH "")
+set(MPI_CXX_COMPILER     "mpicxx"  CACHE PATH "")
+set(MPI_Fortran_COMPILER "mpif90" CACHE PATH "")
 
-set(MPIEXEC              "/usr/bin/mpirun" CACHE PATH "")
+set(MPIEXEC              "mpirun" CACHE PATH "")
 set(MPIEXEC_NUMPROC_FLAG "-np" CACHE PATH "")
 
 #------------------------------------------------------------------------------
 # Options
 #------------------------------------------------------------------------------
+
+set(ENABLE_FORTRAN OFF CACHE BOOL "")
+
+set(TRIBOL_ENABLE_TESTS OFF CACHE BOOL "")
+set(TRIBOL_ENABLE_EXAMPLES OFF CACHE BOOL "")
+set(TRIBOL_ENABLE_DOCS OFF CACHE BOOL "")
 
 set(BUILD_REDECOMP ON CACHE BOOL "")
 
