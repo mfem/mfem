d39c7e9a787767f5020937b2628edf5e4a951201
Eric B. Chin <chin23@llnl.gov>
2024-04-02 10:18:27 -0700

    simplified host configs

--- miniapps/tribol/axom-gcc-notpl.cmake
+++ miniapps/tribol/axom-gcc-notpl.cmake
@@ -2,12 +2,29 @@
 # CMake executable path: /usr/bin/cmake
 #------------------------------------------------------------------------------
 
-set(ENABLE_FORTRAN ON CACHE BOOL "")
+set(CMAKE_C_COMPILER "gcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
+set(CMAKE_Fortran_COMPILER "gfortran" CACHE PATH "")
 
-set(COMPILER_HOME "/usr" CACHE PATH "")
-set(CMAKE_C_COMPILER "${COMPILER_HOME}/bin/gcc" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "${COMPILER_HOME}/bin/g++" CACHE PATH "")
-set(CMAKE_Fortran_COMPILER "/usr/bin/gfortran" CACHE PATH "")
+#set(BLT_EXE_LINKER_FLAGS " -Wl,-rpath,${COMPILER_HOME}/lib" CACHE STRING "Adds a missing libstdc++ rpath")
+
+#------------------------------------------------------------------------------
+# MPI
+#------------------------------------------------------------------------------
+set(ENABLE_MPI ON CACHE BOOL "")
+
+set(MPI_C_COMPILER       "mpicc"   CACHE PATH "")
+set(MPI_CXX_COMPILER     "mpicxx"  CACHE PATH "")
+set(MPI_Fortran_COMPILER "mpif90" CACHE PATH "")
+
+set(MPIEXEC              "mpirun" CACHE PATH "")
+set(MPIEXEC_NUMPROC_FLAG "-np" CACHE PATH "")
+
+#------------------------------------------------------------------------------
+# Options
+#------------------------------------------------------------------------------
+
+set(ENABLE_FORTRAN OFF CACHE BOOL "")
 
 # Sidre requires conduit and hdf5, so disable it in this host-config
 # Inlet and Klee both depend on Sidre, so disable them too
@@ -15,23 +32,8 @@ set(AXOM_ENABLE_SIDRE OFF CACHE BOOL "")
 set(AXOM_ENABLE_INLET OFF CACHE BOOL "")
 set(AXOM_ENABLE_KLEE OFF CACHE BOOL "")
 
-set(BLT_EXE_LINKER_FLAGS " -Wl,-rpath,${COMPILER_HOME}/lib" CACHE STRING "Adds a missing libstdc++ rpath")
-
-#------------------------------------------------------------------------------
-# MPI
-#------------------------------------------------------------------------------
-set(ENABLE_MPI ON CACHE BOOL "")
-
-set(MPI_HOME             "/usr" CACHE PATH "")
-set(MPI_C_COMPILER       "${MPI_HOME}/bin/mpicc"   CACHE PATH "")
-set(MPI_CXX_COMPILER     "${MPI_HOME}/bin/mpicxx"  CACHE PATH "")
-set(MPI_Fortran_COMPILER "${MPI_HOME}/bin/mpif90" CACHE PATH "")
-
-set(MPIEXEC              "/usr/bin/mpirun" CACHE PATH "")
-set(MPIEXEC_NUMPROC_FLAG "-n " CACHE PATH "")
-
 #------------------------------------------------------------------------------
 # Other
 #------------------------------------------------------------------------------
-set(CLANGFORMAT_EXECUTABLE "/usr/bin/clang-format" CACHE PATH "")
+#set(CLANGFORMAT_EXECUTABLE "clang-format" CACHE PATH "")
 
