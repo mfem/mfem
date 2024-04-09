477cc5738f63d6ddd994e88d89c1dddc6f99f39d
Jan Nikl <nikl1@llnl.gov>
2024-04-09 12:34:54 -0700

    Revert "Moved initialization of the shared-local maps to the source."
    This reverts commit 4006af5a564b462330dd2191cf9a0380617e8dc8.

--- mesh/pmesh.hpp
+++ mesh/pmesh.hpp
@@ -106,9 +106,6 @@ protected:
    // Convert the local 'meshgen' to a global one.
    void ReduceMeshGen();
 
-   /// Used in Finalize() after the topology changes
-   void UpdatedTopology() override { FinalizeParTopo(); }
-
    // Determine sedge_ledge and sface_lface.
    void FinalizeParTopo();
 
