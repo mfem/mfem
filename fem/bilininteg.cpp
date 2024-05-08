102d6bb36d673122cd5c2b1a1a5270b52d4b7028
Jan Nikl <nikl1@llnl.gov>
2024-05-08 10:07:34 -0700

    Fixed HDGConvectionCenteredIntegrator dependency on the parameter.

--- fem/bilininteg.cpp
+++ fem/bilininteg.cpp
@@ -3769,7 +3769,7 @@ void HDGConvectionCenteredIntegrator::AssembleHDGFaceMatrix(
 
       un = vu * nor;
       a = alpha * un;
-      b = alpha * fabs(un);
+      b = fabs(alpha * un);
       // note: if |alpha/2|==|beta| then |a|==|b|, i.e. (a==b) or (a==-b)
       //       and therefore two blocks in the element matrix contribution
       //       (from the current quadrature point) are 0
