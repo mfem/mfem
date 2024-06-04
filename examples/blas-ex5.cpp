05f66daa6ecd0dbc309c32618d4088a329fb0b03
Tseng <tseng7@mac-9900613.llnl.gov>
2024-06-03 21:17:33 -0700

    check iterations of SetMatrix

--- examples/blas-ex5.cpp
+++ examples/blas-ex5.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <cuda_runtime.h>
-#include "cublas_v2.h"  // do we need additional headers? especially for gemvBatched
+#include "cublas_v2.h"
 #include "mfem.hpp"
 #include <fstream>
 #include <iostream>
@@ -20,25 +20,24 @@ int main (void){
     cublasHandle_t handle;  // tracks handle into API; can be specified futher but NULL works too
     int i, j, k;
 
-    // batchlinalg uses DenseTensor Mat and Vector x & y 
     DenseTensor A(M,N,W);
     Vector X(N*W);
     Vector Y(N*W);
-
     for (k = 0; k < W; k++) {
         for (j = 0; j < N; j++) {
+            X.GetData()[IDXM(j,k,N)] = IDXM(j,k,N);
             for (i = 0; i < M; i++) {
-                X.GetData()[IDXM(i,j,N)] = IDXM(i,j,N);
                 A.Data()[IDXT(i,j,k,M)] = IDXT(i,j,k,M);
             }
         }
     }
 
-    double* devPtrA[W];  // device array pointer (for W many matrices) for tensor A
-    double* devPtrX[W];  // device array pointer (for W many vectors) for host pointer x
-    double* devPtrY[W];  // device array pointer (for W many vectors) for host pointer y
+    double* devPtrA[W];  // device pointer for a
 
-    double* y = 0;  // host device pointer; used to update Y (there may be a better version than this -- talk to Arturo)
+    double* devPtrX[W];  // device pointer for host pointer x
+
+    double* devPtrY[W];  // device pointer for host pointer y
+    double* y = 0;  // host device pointer
 
     y = (double *)malloc (N * W * sizeof(*Y.GetData()));
     if (!y) {
@@ -56,25 +55,9 @@ int main (void){
     }
     for (k = 0; k < W; k++) {
         for (j = 0; j < N; j++) {
-            printf ("%7.0f", X[IDXM(j,k,N)]);
+            printf ("%7.0f", X.GetData()[IDXM(j,k,N)]);
         }
         printf ("\n");
-    }
-
-    cudaStat = cudaMalloc ((void**)&devPtrA, M*N*W*sizeof(*A.Data()));  // cudaMalloc - give matrix allocation of MxNxW to device pointer on GPU
-    if (cudaStat != cudaSuccess) {
-        printf ("device memory allocation failed for A");
-        return EXIT_FAILURE;
-    }
-    cudaStat = cudaMalloc ((void**)&devPtrX, N*W*sizeof(*X.GetData()));  // cudaMalloc - give matrix allocation of NxW to device pointer on GPU
-    if (cudaStat != cudaSuccess) {
-        printf ("device memory allocation failed for X");
-        return EXIT_FAILURE;
-    }
-    cudaStat = cudaMalloc ((void**)&devPtrY, N*W*sizeof(*Y.GetData()));  // cudaMalloc - give matrix allocation of NxW to device pointer on GPU
-    if (cudaStat != cudaSuccess) {
-        printf ("device memory allocation failed for Y");
-        return EXIT_FAILURE;
     }
 
     // ***START OF DEVICE COMPUTATIONS***
@@ -86,17 +69,32 @@ int main (void){
         return EXIT_FAILURE;
     }
 
-    // (assuming this works...)
     for (k = 0; k < W; k++) {
-        stat = cublasSetMatrix (M, N, sizeof(*A.Data()), &A.Data()[M*N*k], M, devPtrA[k], M);  // fill in the device pointer matrix;
-        // arguments are (rows, cols, bytesize, source_matrix, ld of source, dest_matrix, ld of dest)
+        cudaStat = cudaMalloc ((void**)&devPtrA[k], M*N*sizeof(*A.Data()));  // cudaMalloc - give matrix allocation of MxNxW to device pointer on GPU
+        if (cudaStat != cudaSuccess) {
+            printf ("device memory allocation failed for A");
+            return EXIT_FAILURE;
+        }
+        cudaStat = cudaMalloc ((void**)&devPtrX[k], N*sizeof(*X.GetData()));  // cudaMalloc - give matrix allocation of NxW to device pointer on GPU
+        if (cudaStat != cudaSuccess) {
+            printf ("device memory allocation failed for X");
+            return EXIT_FAILURE;
+        }
+        cudaStat = cudaMalloc ((void**)&devPtrY[k], N*sizeof(*Y.GetData()));  // cudaMalloc - give matrix allocation of NxW to device pointer on GPU
+        if (cudaStat != cudaSuccess) {
+            printf ("device memory allocation failed for Y");
+            return EXIT_FAILURE;
+        }
+
+        stat = cublasSetMatrix (M, N, sizeof(*A.Data()), A.Data()[IDXT(M,N,k,M)], M, devPtrA[k], M);  // fill in the device pointer matrix;
+        // arguments are (rows, cols, NE, elemSize, source_matrix, ld of source, destination matrix, ld dest)
         if (stat != CUBLAS_STATUS_SUCCESS) {
             printf ("data download failed");
             cudaFree (devPtrA);
             cublasDestroy(handle);
             return EXIT_FAILURE;
         }
-        stat = cublasSetMatrix (N, W, sizeof(*X.GetData()), &X.GetData()[N*k], N, devPtrX[k], N);  // fill in the device pointer matrix;
+        stat = cublasSetMatrix (N, W, sizeof(*X.GetData()), X.GetData()[IDXM(N,k,N)], N, devPtrX[k], N);  // fill in the device pointer matrix;
         // arguments are (rows, cols, NE, elemSize, source_matrix, ld of source, destination matrix, ld dest)
         if (stat != CUBLAS_STATUS_SUCCESS) {
             printf ("data download failed");
@@ -106,14 +104,15 @@ int main (void){
         }
     }
 
+    // handles the computations on the device
     double alpha = 1.;
     double beta = 0.;
-    // stat = cublasDgemvBatched (handle, CUBLAS_OP_N, M, N,
-    //                            &alpha, devPtrA, M, devPtrX, 1,
-    //                            &beta, devPtrY, 1);
+    stat = cublasDgemvBatched (handle, CUBLAS_OP_N, M, N,
+                        &alpha, devPtrA, M, devPtrX, 1,
+                        &beta, devPtrY, 1);
 
     // copies device memory to host memory, for output from host
-    stat = cublasGetMatrix (N, W, sizeof(*y), devPtrX, N, y, N);
+    stat = cublasGetMatrix (N, W, sizeof(*y), devPtrY, N, y, N);
     if (stat != CUBLAS_STATUS_SUCCESS) {
         printf ("data upload failed");
         free (y);
@@ -121,7 +120,7 @@ int main (void){
         cublasDestroy(handle);
         return EXIT_FAILURE;
     }
-    // free up memory & end CUBLAS stream
+    // free up memory & end API connection
     cudaFree (devPtrA);
     cudaFree (devPtrX);
     cudaFree (devPtrY);
@@ -132,7 +131,7 @@ int main (void){
     Y.SetData(y);
     for (k = 0; k < W; k++) {
         for (j = 0; j < N; j++) {
-            printf ("%7.0f", Y.GetData()[IDXM(j,k,N)]);  
+            printf ("%7.0f", Y.GetData()[IDXM(j,k,N)]);  // col-major, so prints vectors of columns out together in each row
         }
         printf ("\n");
     }
