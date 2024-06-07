3ad27bb10bd2a5d2c256c2c7d0d5723d23630b14
Christine Tseng <chrissytseng@github.com>
2024-06-07 11:55:39 -0700

    reuploading blas; wrong file before

--- examples/blas.cpp
+++ examples/blas.cpp
@@ -1,283 +1,223 @@
-//                                MFEM Example 1
+//                      MFEM cuBLAS and hipBLAS gemvBatched() Example
 //
-// Compile with: make ex1
-//
-// Sample runs:  ex1 -m ../data/square-disc.mesh
-//               ex1 -m ../data/star.mesh
-//               ex1 -m ../data/star-mixed.mesh
-//               ex1 -m ../data/escher.mesh
-//               ex1 -m ../data/fichera.mesh
-//               ex1 -m ../data/fichera-mixed.mesh
-//               ex1 -m ../data/toroid-wedge.mesh
-//               ex1 -m ../data/octahedron.mesh -o 1
-//               ex1 -m ../data/periodic-annulus-sector.msh
-//               ex1 -m ../data/periodic-torus-sector.msh
-//               ex1 -m ../data/square-disc-p2.vtk -o 2
-//               ex1 -m ../data/square-disc-p3.mesh -o 3
-//               ex1 -m ../data/square-disc-nurbs.mesh -o -1
-//               ex1 -m ../data/star-mixed-p2.mesh -o 2
-//               ex1 -m ../data/disc-nurbs.mesh -o -1
-//               ex1 -m ../data/pipe-nurbs.mesh -o -1
-//               ex1 -m ../data/fichera-mixed-p2.mesh -o 2
-//               ex1 -m ../data/star-surf.mesh
-//               ex1 -m ../data/square-disc-surf.mesh
-//               ex1 -m ../data/inline-segment.mesh
-//               ex1 -m ../data/amr-quad.mesh
-//               ex1 -m ../data/amr-hex.mesh
-//               ex1 -m ../data/fichera-amr.mesh
-//               ex1 -m ../data/mobius-strip.mesh
-//               ex1 -m ../data/mobius-strip.mesh -o -1 -sc
+// Compile with: make blas-ex
 //
 // Device sample runs:
-//               ex1 -pa -d cuda
-//               ex1 -fa -d cuda
-//               ex1 -pa -d raja-cuda
-//             * ex1 -pa -d raja-hip
-//               ex1 -pa -d occa-cuda
-//               ex1 -pa -d raja-omp
-//               ex1 -pa -d occa-omp
-//               ex1 -pa -d ceed-cpu
-//               ex1 -pa -d ceed-cpu -o 4 -a
-//               ex1 -pa -d ceed-cpu -m ../data/square-mixed.mesh
-//               ex1 -pa -d ceed-cpu -m ../data/fichera-mixed.mesh
-//             * ex1 -pa -d ceed-cuda
-//             * ex1 -pa -d ceed-hip
-//               ex1 -pa -d ceed-cuda:/gpu/cuda/shared
-//               ex1 -pa -d ceed-cuda:/gpu/cuda/shared -m ../data/square-mixed.mesh
-//               ex1 -pa -d ceed-cuda:/gpu/cuda/shared -m ../data/fichera-mixed.mesh
-//               ex1 -m ../data/beam-hex.mesh -pa -d cuda
-//               ex1 -m ../data/beam-tet.mesh -pa -d ceed-cpu
-//               ex1 -m ../data/beam-tet.mesh -pa -d ceed-cuda:/gpu/cuda/ref
-//
-// Description:  This example code demonstrates the use of MFEM to define a
-//               simple finite element discretization of the Laplace problem
-//               -Delta u = 1 with homogeneous Dirichlet boundary conditions.
-//               Specifically, we discretize using a FE space of the specified
-//               order, or if order < 1 using an isoparametric/isogeometric
-//               space (i.e. quadratic for quadratic curvilinear mesh, NURBS for
-//               NURBS mesh, etc.)
-//
-//               The example highlights the use of mesh refinement, finite
-//               element grid functions, as well as linear and bilinear forms
-//               corresponding to the left-hand side and right-hand side of the
-//               discrete linear system. We also cover the explicit elimination
-//               of essential boundary conditions, static condensation, and the
-//               optional connection to the GLVis tool for visualization.
+//               blas-ex -d cuda
+//               blas-ex -pa -d cuda
+//               blas-ex -fa -d cuda
+//               blas-ex -d hip
+//               blas-ex -pa -d hip
+//               blas-ex -fa -d hip
+
+// Description:  This example code demonstrates the use of cu or hipBLAS on MFEM 
+//               objects to multiply batched square matrices with batched vectors. 
+//               It utilizes the BLAS functions 
+//                  cublasDgemvBatched()  or  hipblasDgemvBatched().
+//               Note that version cuda/11.7.0 is needed. 
+// 
+//               User can specify the number of rows (cols) n for the (nxn) square 
+//               square matrices stored in DenseTensor A and (nx1) vectors stored in 
+//               Vector X, as well as scalars alpha and beta to transform the operations: 
+//                  alpha * A[i] * X[i] + beta = Y[i]. 
+//               Output also solves the linear system Ab = x as needed: 
+//                  Ainv[i] * X[i] = B[i]. 
+//               This function is specifically included to demonstrate the Batch Linear
+//               Algebra functions ported with cu/hipBLAS (see batchlinalg.cpp).
 
 #include "mfem.hpp"
+#include "../fem/bilinearform.cpp"
 #include <fstream>
 #include <iostream>
+#define IDXT(i,j,k,ld) (((ld)*(ld)*(k))+((j)*(ld))+(i))
+#define IDXM(i,j,ld) ((ld*j)+i)
 
 using namespace std;
 using namespace mfem;
 
-int main(int argc, char *argv[])
-{
-   // 1. Parse command-line options.
-   const char *mesh_file = "../data/star.mesh";
-   int order = 1;
-   bool static_cond = false;
-   bool pa = false;
-   bool fa = false;
-   const char *device_config = "cpu";
-   bool visualization = true;
-   bool algebraic_ceed = false;
+int main (int argc, char *argv[]){
+    // 1. Parse command-line options.
+    int ndof = 4;
+    int num_elem = 3;
+    double alpha = 1.;
+    double beta = 0.;
+    bool inverse = true;
+    bool pa = false;
+    bool fa = false;
+    const char *device_config = "cuda";
 
-   OptionsParser args(argc, argv);
-   args.AddOption(&mesh_file, "-m", "--mesh",
-                  "Mesh file to use.");
-   args.AddOption(&order, "-o", "--order",
-                  "Finite element order (polynomial degree) or -1 for"
-                  " isoparametric space.");
-   args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
-                  "--no-static-condensation", "Enable static condensation.");
-   args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
-                  "--no-partial-assembly", "Enable Partial Assembly.");
-   args.AddOption(&fa, "-fa", "--full-assembly", "-no-fa",
-                  "--no-full-assembly", "Enable Full Assembly.");
-   args.AddOption(&device_config, "-d", "--device",
-                  "Device configuration string, see Device::Configure().");
-#ifdef MFEM_USE_CEED
-   args.AddOption(&algebraic_ceed, "-a", "--algebraic", "-no-a", "--no-algebraic",
-                  "Use algebraic Ceed solver");
-#endif
-   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
-                  "--no-visualization",
-                  "Enable or disable GLVis visualization.");
-   args.Parse();
-   if (!args.Good())
-   {
-      args.PrintUsage(cout);
-      return 1;
-   }
-   args.PrintOptions(cout);
+    OptionsParser args(argc, argv);
+    args.AddOption(&ndof, "-n", "--ndof",
+                    "Number of Rows/Columns of batched square matrices.");
+    args.AddOption(&num_elem, "-ne", "--num_elem",
+                    "Number of matrices in tensor (batch size).");
+    args.AddOption(&alpha, "-a", "--alpha",
+                    "Scalar for batched matrices.");
+    args.AddOption(&beta, "-b", "--beta",
+                    "Scalar to add to multiplication.");
+    args.AddOption(&inverse, "-no-inv", "--no-inverse", "-inv", 
+                    "--inverse", "Disable inverse of batched matrices.");
+    args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
+                    "--no-partial-assembly", "Enable Partial Assembly.");
+    args.AddOption(&fa, "-fa", "--full-assembly", "-no-fa",
+                    "--no-full-assembly", "Enable Full Assembly.");
+    args.AddOption(&device_config, "-d", "--device",
+                    "Device configuration string, see Device::Configure().");
+    args.Parse();
+    if (!args.Good())
+    {
+        args.PrintUsage(cout);
+        return 1;
+    }
+    args.PrintOptions(cout);
 
-   // 2. Enable hardware devices such as GPUs, and programming models such as
-   //    CUDA, OCCA, RAJA and OpenMP based on command line options.
-   Device device(device_config);
-   device.Print();
+    // 2. Specify hardware devices such as GPUs.
+    Device device(device_config);
+    device.Print();
+    printf ("\n");
 
-   // 3. Read the mesh from the given mesh file. We can handle triangular,
-   //    quadrilateral, tetrahedral, hexahedral, surface and volume meshes with
-   //    the same code.
-   Mesh mesh(mesh_file, 1, 1);
-   int dim = mesh.Dimension();
+    // 3. Create a DenseTensor A and Vector X to batch multiply. Create a Vector Y container for the result. 
+    DenseTensor A(ndof,ndof,num_elem);
+    Vector X(ndof*num_elem);
+    Vector Y(ndof*num_elem);
+    printf ("A transpose is \n"); // col-major, so prints vectors of columns of A out together in each row
+    for (int k = 0; k < num_elem; k++) {
+        for (int j = 0; j < ndof; j++) {
+            X.HostReadWrite()[IDXM(j,k,ndof)] = (double) (IDXM(j,k,ndof));
+            for (int i = 0; i < ndof; i++) {
+                if (i==j) {A.HostReadWrite()[IDXT(i,j,k,ndof)] = 1;}
+                else {A.HostReadWrite()[IDXT(i,j,k,ndof)] = (double) (IDXT(j,i,k,ndof));}
+                printf ("%9.3f", A.HostRead()[IDXT(i,j,k,ndof)]);
+            }
+            printf ("\n");  
+        }
+        printf ("\n");
+    }
+    printf ("X transpose is \n");
+    for (int k = 0; k < num_elem; k++) {
+        for (int j = 0; j < ndof; j++) {
+            printf ("%9.3f", X.HostRead()[IDXM(j,k,ndof)]);
+        }
+        printf ("\n");
+    }
+    printf ("\n");
 
-   // 4. Refine the mesh to increase the resolution. In this example we do
-   //    'ref_levels' of uniform refinement. We choose 'ref_levels' to be the
-   //    largest number that gives a final mesh with no more than 50,000
-   //    elements.
-   {
-      int ref_levels =
-         (int)floor(log(50000./mesh.GetNE())/log(2.)/dim);
-      for (int l = 0; l < ref_levels; l++)
-      {
-         mesh.UniformRefinement();
-      }
-   }
+    // 4. Run CUDA or HIP
+    //    Full commands not written out; using MFEM_USE_CUDA_OR_HIP implementation.
+    //    See commented section below for example in cuBLAS and hipBLAS.
+    
+    // gemvBatched() requires array of pointers for each A, X, Y 
+    Array<double *>devPtrA(num_elem);  
+    Array<double *>devPtrX(num_elem);  
+    Array<double *>devPtrY(num_elem);  
 
-   // 5. Define a finite element space on the mesh. Here we use continuous
-   //    Lagrange finite elements of the specified order. If order < 1, we
-   //    instead use an isoparametric/isogeometric space.
-   FiniteElementCollection *fec;
-   bool delete_fec;
-   if (order > 0)
-   {
-      fec = new H1_FECollection(order, dim);
-      delete_fec = true;
-   }
-   else if (mesh.GetNodes())
-   {
-      fec = mesh.GetNodes()->OwnFEC();
-      delete_fec = false;
-      cout << "Using isoparametric FEs: " << fec->Name() << endl;
-   }
-   else
-   {
-      fec = new H1_FECollection(order = 1, dim);
-      delete_fec = true;
-   }
-   FiniteElementSpace fespace(&mesh, fec);
-   cout << "Number of finite element unknowns: "
-        << fespace.GetTrueVSize() << endl;
+    for (int k = 0; k < num_elem; k++) {
+        devPtrA[k] = &A.ReadWrite()[ndof*ndof*k];
+        devPtrX[k] = &X.ReadWrite()[ndof*k];
+        devPtrY[k] = &Y.ReadWrite()[ndof*k];
+    }
+    
+    MFEM_cu_or_hip(blasStatus_t) stat;  // collects generation of cublasStatus_t
+    MFEM_cu_or_hip(blasHandle_t) handle;  // tracks handle into API; can be specified futher but NULL works too
 
-   // 6. Determine the list of true (i.e. conforming) essential boundary dofs.
-   //    In this example, the boundary conditions are defined by marking all
-   //    the boundary attributes from the mesh as essential (Dirichlet) and
-   //    converting them to a list of true dofs.
-   Array<int> ess_tdof_list;
-   if (mesh.bdr_attributes.Size())
-   {
-      Array<int> ess_bdr(mesh.bdr_attributes.Max());
-      ess_bdr = 1;
-      fespace.GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
-   }
+    stat = MFEM_cu_or_hip(blasCreate)(&handle);  // create handle to start CUBLAS work on the device; i.e. initialize CUBLAS
+    if (stat != MFEM_CU_or_HIP(BLAS_STATUS_SUCCESS)) {
+        printf ("BLAS initialization failed\n");
+        return EXIT_FAILURE;
+    }
 
-   // 7. Set up the linear form b(.) which corresponds to the right-hand side of
-   //    the FEM linear system, which in this case is (1,phi_i) where phi_i are
-   //    the basis functions in the finite element fespace.
-   LinearForm b(&fespace);
-   ConstantCoefficient one(1.0);
-   b.AddDomainIntegrator(new DomainLFIntegrator(one));
-   b.Assemble();
+    stat = MFEM_cu_or_hip(blasDgemvBatched) (handle, MFEM_CU_or_HIP(BLAS_OP_N), ndof, ndof,
+                                &alpha, devPtrA.Read(), ndof, devPtrX.Read(), 1,
+                                &beta, devPtrY.ReadWrite(), 1, num_elem);  // version 11.9.0 needs batchCount = W as the last parameter
+    if (stat != MFEM_CU_or_HIP(BLAS_STATUS_SUCCESS)) {
+        printf ("BLAS gemvBatched() failed\n");
+        printf("\n");
+        return EXIT_FAILURE;
+    }
 
-   // 8. Define the solution vector x as a finite element grid function
-   //    corresponding to fespace. Initialize x with initial guess of zero,
-   //    which satisfies the boundary conditions.
-   GridFunction x(&fespace);
-   x = 0.0;
+    MFEM_cu_or_hip(blasDestroy)(handle);  // end API stream
+    
 
-   // 9. Set up the bilinear form a(.,.) on the finite element space
-   //    corresponding to the Laplacian operator -Delta, by adding the Diffusion
-   //    domain integrator.
-   BilinearForm a(&fespace);
-   if (pa) { a.SetAssemblyLevel(AssemblyLevel::PARTIAL); }
-   if (fa)
-   {
-      a.SetAssemblyLevel(AssemblyLevel::FULL);
-      // Sort the matrix column indices when running on GPU or with OpenMP (i.e.
-      // when Device::IsEnabled() returns true). This makes the results
-      // bit-for-bit deterministic at the cost of somewhat longer run time.
-      a.EnableSparseMatrixSorting(Device::IsEnabled());
-   }
-   a.AddDomainIntegrator(new DiffusionIntegrator(one));
+    // 5. Done! We can now output.
+    printf ("Y tranpose is \n");
+    for (int k = 0; k < num_elem; k++) {
+        for (int j = 0; j < ndof; j++) {
+            printf ("%9.3f", Y.HostRead()[IDXM(j,k,ndof)]);  
+        }
+        printf ("\n");
+    }
+    printf ("\n");
 
-   // 10. Assemble the bilinear form and the corresponding linear system,
-   //     applying any necessary transformations such as: eliminating boundary
-   //     conditions, applying conforming constraints for non-conforming AMR,
-   //     static condensation, etc.
-   if (static_cond) { a.EnableStaticCondensation(); }
-   a.Assemble();
 
-   OperatorPtr A;
-   Vector B, X;
-   a.FormLinearSystem(ess_tdof_list, x, b, A, X, B);
+    // 6. If wanted, solve the linear systems
+    if (inverse) {
+        Vector B(ndof*num_elem);
+        DenseTensor Ainv(ndof,ndof,num_elem);
+        BatchSolver batchSolver(BatchSolver::SolveMode::INVERSE);
+        batchSolver.AssignMatrices(A);
+        batchSolver.GetInverse(Ainv);
+        batchSolver.Mult(X, B);
 
-   cout << "Size of linear system: " << A->Height() << endl;
+        printf ("A inverse transpose is \n");
+        for (int k = 0; k < num_elem; k++) {
+            for (int j = 0; j < ndof; j++) {
+                for (int i = 0; i < ndof; i++) {
+                    printf ("%9.3f", Ainv.HostRead()[IDXT(i,j,k,ndof)]);
+                }
+                printf ("\n");  
+            }
+            printf ("\n");
+        }
 
-   // 11. Solve the linear system A X = B.
-   if (!pa)
-   {
-#ifndef MFEM_USE_SUITESPARSE
-      // Use a simple symmetric Gauss-Seidel preconditioner with PCG.
-      GSSmoother M((SparseMatrix&)(*A));
-      PCG(*A, M, B, X, 1, 200, 1e-12, 0.0);
-#else
-      // If MFEM was compiled with SuiteSparse, use UMFPACK to solve the system.
-      UMFPackSolver umf_solver;
-      umf_solver.Control[UMFPACK_ORDERING] = UMFPACK_ORDERING_METIS;
-      umf_solver.SetOperator(*A);
-      umf_solver.Mult(B, X);
-#endif
-   }
-   else
-   {
-      if (UsesTensorBasis(fespace))
-      {
-         if (algebraic_ceed)
-         {
-            ceed::AlgebraicSolver M(a, ess_tdof_list);
-            PCG(*A, M, B, X, 1, 400, 1e-12, 0.0);
-         }
-         else
-         {
-            OperatorJacobiSmoother M(a, ess_tdof_list);
-            PCG(*A, M, B, X, 1, 400, 1e-12, 0.0);
-         }
-      }
-      else
-      {
-         CG(*A, B, X, 1, 400, 1e-12, 0.0);
-      }
-   }
+        printf ("B tranpose is \n");
+        for (int k = 0; k < num_elem; k++) {
+            for (int j = 0; j < ndof; j++) {
+                printf ("%9.3f", B.HostRead()[IDXM(j,k,ndof)]);  // col-major, so prints vectors of columns out together in each row
+            }
+            printf ("\n");
+        }
+        printf ("\n");
+    }
 
-   // 12. Recover the solution as a finite element grid function.
-   a.RecoverFEMSolution(X, b, x);
+    return EXIT_SUCCESS;
 
-   // 13. Save the refined mesh and the solution. This output can be viewed later
-   //     using GLVis: "glvis -m refined.mesh -g sol.gf".
-   ofstream mesh_ofs("refined.mesh");
-   mesh_ofs.precision(8);
-   mesh.Print(mesh_ofs);
-   ofstream sol_ofs("sol.gf");
-   sol_ofs.precision(8);
-   x.Save(sol_ofs);
 
-   // 14. Send the solution by socket to a GLVis server.
-   if (visualization)
-   {
-      char vishost[] = "localhost";
-      int  visport   = 19916;
-      socketstream sol_sock(vishost, visport);
-      sol_sock.precision(8);
-      sol_sock << "solution\n" << mesh << x << flush;
-   }
+    // *****CUBLAS IMPLEMENTATIONS*****
+    // cublasStatus_t stat;  // collects generation of cublasStatus_t
+    // cublasHandle_t handle;  // tracks handle into API; can be specified futher but NULL works too
+    // stat = cublasCreate(&handle);  // create handle to start CUBLAS work on the device; i.e. initialize CUBLAS
+    // if (stat != CUBLAS_STATUS_SUCCESS) {
+    //     printf ("CUBLAS initialization failed\n");
+    //     return EXIT_FAILURE;
+    // }
+    // stat = cublasDgemvBatched (handle, CUBLAS_OP_N, M, N,
+    //                             &alpha, devPtrA.READ(), M, devPtrX.Read(), 1,
+    //                             &beta, devPtrY.ReadWrite(), 1, W);  // version 11.9.0 needs batchCount = W as the last parameter
+    // if (stat != CUBLAS_STATUS_SUCCESS) {
+    //     printf ("CUBLAS gemvBatched() failed\n");
+    //     printf("%s", cublasGetStatusString(stat));
+    //     printf("\n");
+    //     return EXIT_FAILURE;
+    // }
+    // cublasDestroy(handle);  // end API stream
 
-   // 15. Free the used memory.
-   if (delete_fec)
-   {
-      delete fec;
-   }
 
-   return 0;
+    // *****HIPBLAS IMPLEMENTATIONS*****
+    // hipblasStatus_t stat;  // collects generation of hipblasStatus_t
+    // hipblasHandle_t handle = nullptr;  // tracks handle into API; can be specified futher but NULL works too
+    // stat = hipblasCreate(&handle);  // create handle to start HIPBLAS work on the device; i.e. initialize HIPBLAS
+    // if (stat != HIPBLAS_STATUS_SUCCESS) {
+    //     printf ("HIPBLAS initialization failed\n");
+    //     return EXIT_FAILURE;
+    // }
+    // stat = hipblasDgemvBatched (handle, HIPBLAS_OP_N, M, N,
+    //                             &alpha, devPtrA.Read(), M, devPtrX.Read(), 1,
+    //                             &beta, devPtrY.ReadWrite(), 1, W);  
+    // if (stat != HIPBLAS_STATUS_SUCCESS) {
+    //     printf ("HIPBLAS gemvBatched() failed\n");
+    //     printf("%s", hipblasStatusToString(stat));
+    //     printf("\n");
+    //     return EXIT_FAILURE;
+    // }
+    // hipblasDestroy(handle);  // end API stream
 }
