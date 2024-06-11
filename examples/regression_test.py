d411841736ce7df1fecbc9a8f37829384388076b
migliore <migliore.c@husky.neu.edu>
2024-06-11 11:48:00 -0400

    Updates regression testing script

--- examples/regression_test.py
+++ examples/regression_test.py
@@ -15,7 +15,7 @@ class bcolors:
 	FAIL = '\033[91m'
 	RESET = "\033[0m"
 
-tol = 1e-4
+tol = 1e-7
 
 print('Running Regression Testing:')
 path = 'regress_test/'
@@ -28,8 +28,6 @@ for i in range(len(filenames)):
 	dg = int(filenames[i][9])
 	hb = int(filenames[i][10])
 
-	print("  Case "+problem+".........", end="", flush=True)
-
 	dg_com = ''
 	if int(dg) == 1:
 		dg_com = " -dg "
@@ -37,9 +35,9 @@ for i in range(len(filenames)):
 	if int(hb) == 1:
 		hb_com = "-hb "
 
-	p = subprocess.getoutput("grep ' --ncells-x' "+path+filenames[i]+"| awk '{print $2}'")
+	p = subprocess.getoutput("grep ' --ncells-x' "+path+filenames[i]+"| cut -d ' ' -f 5")
 	nx = p.split()[0]
-	p = subprocess.getoutput("grep ' --ncells-y' "+path+filenames[i]+"| awk '{print $2}'")
+	p = subprocess.getoutput("grep ' --ncells-y' "+path+filenames[i]+"| cut -d ' ' -f 5")
 	ny = p.split()[0]
 
 	p = subprocess.getoutput("grep '|| t_h - t_ex || / || t_ex || = ' "+path+filenames[i]+"  | cut -d '=' -f 2-")
@@ -48,13 +46,16 @@ for i in range(len(filenames)):
 	ref_L2_q = float(p.split()[0])
 
 	# Run test case
-	subprocess.call("./ex5-nguyen -nx "+str(nx)+" -ny "+str(ny)+" -p "+problem+" -o "+order+dg_com+hb_com+" > temp_output.txt", shell=True,stdout=subprocess.DEVNULL)
-	p = subprocess.getoutput("grep '|| t_h - t_ex || / || t_ex || = ' temp_output.txt  | cut -d '=' -f 2-")
-	test_L2_t = float(p.split()[0])
-	p = subprocess.getoutput("grep '|| q_h - q_ex || / || q_ex || = ' temp_output.txt  | cut -d '=' -f 2-")
-	test_L2_q = float(p.split()[0])
+	command_line = "./ex5-nguyen -no-vis -nx "+str(nx)+" -ny "+str(ny)+" -p "+problem+" -o "+order+dg_com+hb_com
+	print(command_line+".....", end="", flush=True)
 
-	subprocess.call("rm temp_output.txt",shell=True)
+	p = subprocess.getoutput(command_line)
+	newp = p.splitlines()
+	index_t = newp[-1].find('= ')
+	index_q = newp[-2].find('= ')
+
+	test_L2_t = float(newp[-1][index_t+2::])
+	test_L2_q = float(newp[-2][index_q+2::])
 
 	if abs(ref_L2_t - test_L2_t) < tol and abs(ref_L2_q - test_L2_q) < tol:
 		print(bcolors.OKGREEN + "SUCCESS" + bcolors.RESET)
