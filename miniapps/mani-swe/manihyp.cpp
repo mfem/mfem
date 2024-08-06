a351b3000f4417a8158e76072f717edccdfec137
dohyun <dohyun_kim@brown.edu>
2024-08-06 14:38:33 -0400

    manifold shallow water init

---
+++ miniapps/mani-swe/manihyp.cpp
@@ -0,0 +1,96 @@
+#include "manihyp.hpp"
+
+namespace mfem{
+
+Mesh sphericalMesh(const double r, Element::Type element_type, const int level_serial, const int level_parallel, bool parallel)
+{
+  parallel = parallel || level_parallel > 0;
+  #ifndef MFEM_USE_MPI
+  if (parallel) {mfem_error("Parallel MFEM is not built but parallel flag is on.")}
+  #endif
+
+  VectorFunctionCoefficient sphere_cf(3, [r](const Vector &x, Vector &new_x){new_x = x; new_x *= r/x.Norml2();});
+   int Nvert = 8, Nelem = 6;
+   if (element_type == Element::Type::TRIANGLE)
+   {
+      Nvert = 6;
+      Nelem = 8;
+   }
+   Mesh *mesh = new Mesh(2, Nvert, Nelem, 0, 3);
+
+  switch (element_type)
+  {
+    case Element::Type::TRIANGLE:
+  {
+      const real_t tri_v[6][3] =
+      {
+         { 1,  0,  0}, { 0,  1,  0}, {-1,  0,  0},
+         { 0, -1,  0}, { 0,  0,  1}, { 0,  0, -1}
+      };
+      const int tri_e[8][3] =
+      {
+         {0, 1, 4}, {1, 2, 4}, {2, 3, 4}, {3, 0, 4},
+         {1, 0, 5}, {2, 1, 5}, {3, 2, 5}, {0, 3, 5}
+      };
+
+      for (int j = 0; j < Nvert; j++)
+      {
+         mesh->AddVertex(tri_v[j]);
+      }
+      for (int j = 0; j < Nelem; j++)
+      {
+         int attribute = j + 1;
+         mesh->AddTriangle(tri_e[j], attribute);
+      }
+      mesh->FinalizeTriMesh(1, 1, true);
+           break;
+   }
+           case Element::Type::QUADRILATERAL:
+   {
+      const real_t quad_v[8][3] =
+      {
+         {-1, -1, -1}, {+1, -1, -1}, {+1, +1, -1}, {-1, +1, -1},
+         {-1, -1, +1}, {+1, -1, +1}, {+1, +1, +1}, {-1, +1, +1}
+      };
+      const int quad_e[6][4] =
+      {
+         {3, 2, 1, 0}, {0, 1, 5, 4}, {1, 2, 6, 5},
+         {2, 3, 7, 6}, {3, 0, 4, 7}, {4, 5, 6, 7}
+      };
+
+      for (int j = 0; j < Nvert; j++)
+      {
+         mesh->AddVertex(quad_v[j]);
+      }
+      for (int j = 0; j < Nelem; j++)
+      {
+         int attribute = j + 1;
+         mesh->AddQuad(quad_e[j], attribute);
+      }
+      mesh->FinalizeQuadMesh(1, 1, true);
+        break;
+   }
+    default:
+      mfem_error("Only triangle and quadrilateral are supported.");
+  }
+
+  for (int i=0; i<level_serial; i++)
+  {
+    mesh.UniformRefinement();
+    mesh.GetNodes()->ProjectCoefficient(sphere_cf);
+  }
+  if(!parallel) return mesh;
+
+  #ifdef MFEM_USE_MPI
+  ParMesh pmesh(MPI_COMM_WORLD, mesh);
+  mesh.Clear();
+  for (int i=0; i<level_parallel; i++)
+  {
+    pmesh.UniformRefinement();
+    pmesh.GetNodes()->ProjectCoefficient(sphere_cf);
+  }
+  return pmesh;
+  #endif
+
+}
+} // end of namespace mfem
---
+++ miniapps/mani-swe/swe_sphere.cpp
@@ -0,0 +1,81 @@
+//                                MFEM MINIAPP MANI-SWE
+//
+// Compile with: make swe
+//
+// Sample runs:  swe
+//
+// Description:  This example code demonstrates the use of MFEM to define a
+//               simple finite element discretization of the Laplace problem
+//               -Delta u = 1 with homogeneous Dirichlet boundary conditions.
+//               Specifically, we discretize using a FE space of the specified
+//               order, or if order < 1 using an isoparametric/isogeometric
+//               space (i.e. quadratic for quadratic curvilinear mesh, NURBS for
+//               NURBS mesh, etc.)
+//
+//               The example highlights the use of mesh refinement, finite
+//               element grid functions, as well as linear and bilinear forms
+//               corresponding to the left-hand side and right-hand side of the
+//               discrete linear system. We also cover the explicit elimination
+//               of essential boundary conditions, static condensation, and the
+//               optional connection to the GLVis tool for visualization.
+
+#include "mfem.hpp"
+#include "manihyp.hpp"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using namespace mfem;
+
+
+int main(int argc, char *argv[])
+{
+   // 1. Parse command-line options.
+   int order = 1;
+   bool static_cond = false;
+   bool pa = false;
+   bool fa = false;
+   const char *device_config = "cpu";
+   bool visualization = true;
+   bool algebraic_ceed = false;
+   int level = 2;
+
+   OptionsParser args(argc, argv);
+   args.AddOption(&order, "-o", "--order",
+                  "Finite element order (polynomial degree) or -1 for"
+                  " isoparametric space.");
+   args.AddOption(&level, "-r", "--refine",
+                  "Refinement level");
+   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
+                  "--no-visualization",
+                  "Enable or disable GLVis visualization.");
+   args.Parse();
+   if (!args.Good())
+   {
+      args.PrintUsage(cout);
+      return 1;
+   }
+   args.PrintOptions(cout);
+   
+   Mesh mesh = sphericalMesh(1.0, level);
+   L2_FECollection fec(order, 2);
+   FiniteElementSpace fes_scalar(&mesh, &fec);
+   FiniteElementSpace fes_sdim(&mesh, &fec, 2);
+   FiniteElementSpace fes_vdim(&mesh, &fec, 2+1);
+   
+   GridFunction gf(&fes_scalar);
+   gf = 1.0;
+
+   if (visualization)
+   {
+      char vishost[] = "localhost";
+      int  visport   = 19916;
+      socketstream sol_sock(vishost, visport);
+      sol_sock.precision(8);
+      sol_sock << "solution\n" << mesh << gf << flush;
+   }
+   
+   
+
+   return 0;
+}
