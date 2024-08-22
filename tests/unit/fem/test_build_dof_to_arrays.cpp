4fac872989a6223bbc5c429ea3bddc7e89b3cd20
Joseph Signorelli <jms26@illinois.edu>
2024-08-22 17:07:55 -0500

    style

--- tests/unit/fem/test_build_dof_to_arrays.cpp
+++ tests/unit/fem/test_build_dof_to_arrays.cpp
@@ -270,7 +270,8 @@ TEST_CASE("Build Dof To Arrays (Parallel)",
 
             // Get all boundary ldofs
             Array<int> bdr(1); bdr = 1;
-            static_cast<FiniteElementSpace>(fespace).GetEssentialVDofs(bdr, all_bdr_ldofs_marked);
+            static_cast<FiniteElementSpace>(fespace).GetEssentialVDofs(bdr,
+                                                                       all_bdr_ldofs_marked);
             FiniteElementSpace::MarkerToList(all_bdr_ldofs_marked, all_bdr_ldofs);
 
             for (int i = 0; i < size; i++)
