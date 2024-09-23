17880b06f21d1626ffe77c76f62b20c120eef73b
camierjs <camierjs@gmail.com>
2024-09-23 19:20:58 +0000

    make style

--- examples/ex1p.cpp
+++ examples/ex1p.cpp
@@ -125,9 +125,10 @@ int main(int argc, char *argv[])
    // 3. Enable hardware devices such as GPUs, and programming models such as
    //    CUDA, OCCA, RAJA and OpenMP based on command line options.
    Device device(device_config);
-   if (myid == 0) {
-     device.Print();
-     HYPRE_PrintDeviceInfo();
+   if (myid == 0)
+   {
+      device.Print();
+      HYPRE_PrintDeviceInfo();
    }
 
    // 4. Read the (serial) mesh from the given mesh file on all processors.  We
