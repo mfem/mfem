8b34deecb3760444bf77bdc4b70aa33fe9c4c4b1
Socratis Petrides <petrides1@llnl.gov>
2024-10-02 14:45:34 -0700

    modified two-level prec

--- miniapps/contact/ipsolver/two-level-solver.cpp
+++ miniapps/contact/ipsolver/two-level-solver.cpp
@@ -55,65 +55,177 @@ void TwoLevelAMGSolver::SetContactTransferMap(const Operator & P)
 
 void TwoLevelAMGSolver::SetNonContactTransferMap(const Operator & P)
 {
-    Pnc = dynamic_cast<const HypreParMatrix *>(&P);
+    Pi = dynamic_cast<const HypreParMatrix *>(&P);
+    // Ai = RAP(A, Pi);
+    // Si = new HypreBoomerAMG(*Ai);
+    // Si->SetPrintLevel(0);
+    // Si->SetSystemsOptions(3);
+    // Si->SetRelaxType(relax_type);
 }
 
 void TwoLevelAMGSolver::InitAMG()
 {
-    amg = new HypreBoomerAMG(*A);
-    amg->SetPrintLevel(0);
-    amg->SetSystemsOptions(3);
-    amg->SetRelaxType(relax_type);
+    S = new HypreBoomerAMG(*A);
+    S->SetPrintLevel(0);
+    S->SetSystemsOptions(3);
+    S->SetRelaxType(relax_type);
 }
 
 void TwoLevelAMGSolver::InitMumps()
 {
     Ac = RAP(A, Pc);
-    M = new MUMPSSolver(comm);
-    M->SetPrintLevel(0);
-    M->SetMatrixSymType(MUMPSSolver::MatType::UNSYMMETRIC);
-    M->SetOperator(*Ac);
+    Sc = new MUMPSSolver(comm);
+    Sc->SetPrintLevel(0);
+    Sc->SetMatrixSymType(MUMPSSolver::MatType::UNSYMMETRIC);
+    Sc->SetOperator(*Ac);
 }
-
 
 void TwoLevelAMGSolver::Mult(const Vector & b, Vector & x) const
 {
     MFEM_VERIFY(b.Size() == x.Size(), "Inconsistent x and y size");
-    
 
     x = 0.0;
     Vector z(x);
-    amg->Mult(b, z);
+    S->Mult(b, z);
     x+=z;
     Vector rc(Pc->Width());
     Vector xc(Pc->Width());
-    if (additive)
-    {
-        Pc->MultTranspose(b,rc);
-        M->Mult(rc,xc);
-        Pc->Mult(xc,z);
-    }
-    else
-    {
-        Vector r(b.Size());
-        // 2. Compute Residual r = b - A x
-        A->Mult(x,r);
-        r.Neg(); r+=b;
-        // 3. Restrict to subspace
-        Pc->MultTranspose(r,rc);
-        // 4. Solve on the subspace
-        M->Mult(rc,xc);
-        // 5. Transfer to fine space
-        Pc->Mult(xc,z);
-        // 6. Update Correction
-        x+=z;
-        // 7. Compute Residual r = b - A x
-        A->Mult(x,r);
-        r.Neg(); r+=b;
-        // 8. Post V-Cycle 
-        amg->Mult(r, z);
-    }
+    Vector r(b);
+    // 2. Compute Residual r = b - A x
+    A->AddMult(x,r,-1.0);
+    // 3. Restrict to subspace
+    Pc->MultTranspose(r,rc);
+    // 4. Solve on the subspace
+    Sc->Mult(rc,xc);
+    // 5. Transfer to fine space
+    Pc->Mult(xc,z);
+    // 6. Update Correction
+    x+=z;
+    // 7. Compute Residual r = b - A x = r - A z
+    A->AddMult(z,r,-1.0);
+    // 8. Post V-Cycle 
+    S->Mult(r, z);
     x+= z;
+
+    // Vector z(x.Size());
+    // Vector rc(Pc->Width());
+    // Vector xc(Pc->Width());
+    // Pc->MultTranspose(b,rc);
+    // Sc->Mult(rc,xc);
+    // Pc->Mult(xc,z);
+    // x+=z;
+    // Vector r(b);
+    // A->AddMult(z,r,-1.0);
+    // S->Mult(r, z);
+    // x+=z;
+    // A->AddMult(z,r,-1.0);
+    // Pc->MultTranspose(r,rc);
+    // Sc->Mult(rc,xc);
+    // Pc->Mult(xc,z);
+    // x+=z;
+
+
+
 }
+
+
+TwoLevelContactSolver::TwoLevelContactSolver(MPI_Comm comm_): Solver()
+{
+    Init(comm_);
+}
+
+TwoLevelContactSolver::TwoLevelContactSolver(const Operator & A_, const Operator & D_, const Operator & Pi_, const Operator & Pc_)
+: Solver()
+{
+   auto APtr = dynamic_cast<const HypreParMatrix *>(&A_);
+   MFEM_VERIFY(APtr, "Operator: Not a compatible matrix type");
+   Init(APtr->GetComm());
+
+   A = dynamic_cast<const HypreParMatrix *>(&A_);
+   JtDJ = dynamic_cast<const HypreParMatrix *>(&D_);
+   K = ParAdd(A, JtDJ);  // A + Ju^T D Ju
+   height = A->Height();
+   width = A->Width(); 
+   SetContactTransferMap(Pc_);
+   SetNonContactTransferMap(Pi_);
+
+}
+
+void TwoLevelContactSolver::Init(MPI_Comm comm_)
+{
+    comm=comm_;
+    MPI_Comm_size(comm, &numProcs);
+    MPI_Comm_rank(comm, &myid);
+}
+
+void TwoLevelContactSolver::SetOperator(const Operator & Op) { }
+
+void TwoLevelContactSolver::SetNonContactTransferMap(const Operator & P)
+{
+    Pi = dynamic_cast<const HypreParMatrix *>(&P);
+    Ai = RAP(K,Pi);
+    InitAMG();
+}
+
+
+void TwoLevelContactSolver::SetContactTransferMap(const Operator & P)
+{
+    Pc = dynamic_cast<const HypreParMatrix *>(&P);
+    Ac = RAP(K,Pc);
+    InitMumps();
+}
+
+void TwoLevelContactSolver::InitAMG()
+{
+    Si = new HypreBoomerAMG(*Ai);
+    Si->SetPrintLevel(0);
+    Si->SetSystemsOptions(3);
+    Si->SetRelaxType(relax_type);
+}
+
+void TwoLevelContactSolver::InitMumps()
+{
+    Sc = new MUMPSSolver(comm);
+    Sc->SetPrintLevel(0);
+    Sc->SetMatrixSymType(MUMPSSolver::MatType::SYMMETRIC_POSITIVE_DEFINITE);
+    Sc->SetOperator(*Ac);
+}
+
+void TwoLevelContactSolver::Mult(const Vector & b, Vector & x) const
+{
+    MFEM_VERIFY(b.Size() == x.Size(), "Inconsistent x and y size");
+
+    x = 0.0;
+    Vector z(x);
+    Vector r(b);
+    Vector ri(Pi->Width());
+    Vector zi(Pi->Width()); zi = 0.0;
+    Pi->MultTranspose(r,ri);
+    Si->Mult(ri, zi);
+    Pi->Mult(zi,z);
+    x+=z;
+
+    // // 2. Compute Residual r = b - A x
+    K->AddMult(z,r,-1.0);
+    Vector rc(Pc->Width());
+    Vector zc(Pc->Width());
+    // 3. Restrict to subspace
+    Pc->MultTranspose(r,rc);
+    // 4. Solve on the subspace
+    Sc->Mult(rc,zc);
+    // 5. Transfer to fine space
+    Pc->Mult(zc,z);
+    // 6. Update Correction
+    x+=z;
+    // 7. Compute Residual r = b - A x
+    K->AddMult(z,r,-1.0);
+    // 8. Post V-Cycle 
+    Pi->MultTranspose(r,ri);
+    Si->Mult(ri, zi);
+    Pi->Mult(zi,z);
+    x+=z;
+}
+
+
 
 } // namespace
--- miniapps/contact/ipsolver/two-level-solver.hpp
+++ miniapps/contact/ipsolver/two-level-solver.hpp
@@ -18,12 +18,16 @@ private:
     MPI_Comm comm;
     int numProcs, myid;
     const HypreParMatrix * A = nullptr;
+    HypreBoomerAMG * S = nullptr;
+
+    const HypreParMatrix * Pi = nullptr;
+    HypreParMatrix * Ai = nullptr;
+    HypreBoomerAMG * Si = nullptr;
+
     const HypreParMatrix * Pc = nullptr;
-    const HypreParMatrix * Pnc = nullptr;
-    HypreBoomerAMG * amg = nullptr;
     HypreParMatrix * Ac = nullptr;
-    MUMPSSolver * M = nullptr;
-    bool additive = false;
+    MUMPSSolver * Sc = nullptr;
+
     int relax_type = 88;
     void Init(MPI_Comm comm_);
     void InitAMG();
@@ -34,19 +38,62 @@ public:
     void SetOperator(const Operator &op);
     void SetContactTransferMap(const Operator & P);
     void SetNonContactTransferMap(const Operator & P);
-    void EnableAdditiveCoupling() { additive = true; }
-    void EnableMultiplicativeCoupling() { additive = false; }
     void SetAMGRelaxType(int relax_type_) { relax_type = relax_type_;  }
 
     virtual void Mult(const Vector & y, Vector & x) const; 
 
     ~TwoLevelAMGSolver()
     {
-        delete amg;
+        delete S;
+        delete Ai;
+        delete Si;
         delete Ac;
-        delete M;
+        delete Sc;
     }
 };
+
+class TwoLevelContactSolver : public Solver
+{
+private:
+    MPI_Comm comm;
+    int numProcs, myid;
+    const HypreParMatrix * K = nullptr;
+    const HypreParMatrix * A = nullptr;
+    const HypreParMatrix * JtDJ = nullptr;
+    HypreBoomerAMG * S = nullptr;
+
+    const HypreParMatrix * Pi = nullptr;
+    HypreParMatrix * Ai = nullptr;
+    HypreBoomerAMG * Si = nullptr;
+
+    const HypreParMatrix * Pc = nullptr;
+    HypreParMatrix * Ac = nullptr;
+    MUMPSSolver * Sc = nullptr;
+
+    int relax_type = 88;
+    void Init(MPI_Comm comm_);
+    void InitAMG();
+    void InitMumps();
+public:
+    TwoLevelContactSolver(MPI_Comm comm_);
+    TwoLevelContactSolver(const Operator & A_, const Operator & D_, const Operator & Pi_, const Operator & Pc_);
+    void SetOperator(const Operator &K_);
+    void SetContactTransferMap(const Operator & P);
+    void SetNonContactTransferMap(const Operator & P);
+    void SetAMGRelaxType(int relax_type_) { relax_type = relax_type_;  }
+
+    virtual void Mult(const Vector & y, Vector & x) const; 
+
+    ~TwoLevelContactSolver()
+    {
+        delete S;
+        delete Ai;
+        delete Si;
+        delete Ac;
+        delete Sc;
+    }
+};
+
 
 }
 
