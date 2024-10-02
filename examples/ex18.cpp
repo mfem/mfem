607775421ce19916471c49091e30c1226b0deb16
Jan Nikl <nikl1@llnl.gov>
2024-10-02 15:23:48 -0700

    Added a note about component-wise Godunov flux to example 18.

--- examples/ex18.cpp
+++ examples/ex18.cpp
@@ -91,7 +91,7 @@ int main(int argc, char *argv[])
    args.AddOption(&order, "-o", "--order",
                   "Order (degree) of the finite elements.");
    args.AddOption(&rsolver_type, "-rs", "--riemann-solver",
-                  "Riemann solver: 1 - Rusanov, 2 - Godunov.");
+                  "Riemann solver: 1 - Rusanov, 2 - Godunov (component-wise).");
    args.AddOption(&ode_solver_type, "-s", "--ode-solver",
                   "ODE solver: 1 - Forward Euler,\n\t"
                   "            2 - RK2 SSP, 3 - RK3 SSP, 4 - RK4, 6 - RK6.");
@@ -193,7 +193,9 @@ int main(int argc, char *argv[])
    switch (rsolver_type)
    {
       case 1: rsolver = new RusanovFlux(flux); break;
-      case 2: rsolver = new GodunovFlux(flux); break;
+      case 2: rsolver = new GodunovFlux(flux);
+         cout << "Warning: Godunov flux is implemented only component-wise." << endl;
+         break;
       default:
          cout << "Unknown Riemann solver type: " << rsolver_type << '\n';
          return 3;
