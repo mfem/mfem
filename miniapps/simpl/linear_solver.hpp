e68ecf5853ca6a62501a229650bd209a36ed4308
dohyun <dohyun_kim@brown.edu>
2024-10-02 23:25:52 -0400

    makefile and preliminary code not yet work

---
+++ miniapps/simpl/linear_solver.hpp
@@ -0,0 +1,36 @@
+#include "mfem.hpp"
+
+namespace mfem
+{
+
+class EllipticSolver
+{
+private:
+   BilinearForm &a;
+   Array<int> ess_tdof_list;
+   std::unique_ptr<CGSolver> solver;
+   std::unique_ptr<GSSmoother> prec;
+   OperatorHandle A;
+   Vector B;
+   Vector X;
+   bool parallel;
+#ifdef MFEM_USE_MPI
+   ParBilinearForm *par_a;
+   MPI_Comm comm;
+   std::unique_ptr<HyprePCG> par_solver;
+   std::unique_ptr<HypreBoomerAMG> par_prec;
+#endif
+public:
+private:
+   void BuildEssTdofList();
+   void SetupSolver();
+   public:
+   EllipticSolver(BilinearForm &a, Array<int> &ess_bdr);
+   EllipticSolver(BilinearForm &a, Array2D<int> &ess_bdr);
+   void Solve(LinearForm &b, GridFunction &x);
+   Solver* GetSolver();
+
+
+};
+
+}// namespace mfem
