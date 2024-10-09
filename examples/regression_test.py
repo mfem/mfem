ab47164b472fdeceec6e3cdaa92bce4cd3c4fbf5
Jan Nikl <nikl1@llnl.gov>
2024-10-08 20:10:38 -0700

    Refactored regression tests script a bit.

--- examples/regression_test.py
+++ examples/regression_test.py
@@ -46,12 +46,12 @@ for i in range(len(filenames)):
 		else:
 			return default
 
-	problem = get_ref_param(filename, '--problem')
-	order = get_ref_param(filename, '--order')
-	nx = get_ref_param(filename, '--ncells-x')
-	ny = get_ref_param(filename, '--ncells-y')
-	kappa = get_ref_param(filename, '--kappa', "1")
-	hdg = get_ref_param(filename, '--hdg_scheme', "1")
+	problem = int(get_ref_param(filename, '--problem'))
+	order = int(get_ref_param(filename, '--order'))
+	nx = int(get_ref_param(filename, '--ncells-x'))
+	ny = int(get_ref_param(filename, '--ncells-y'))
+	kappa = float(get_ref_param(filename, '--kappa', "1"))
+	hdg = int(get_ref_param(filename, '--hdg_scheme', "1"))
 
 	ref_out = subprocess.getoutput("grep '|| t_h - t_ex || / || t_ex || = ' "+filename+"  | cut -d '=' -f 2-")
 	ref_L2_t = float(ref_out.split()[0])
@@ -64,25 +64,29 @@ for i in range(len(filenames)):
 	precond_ref = ref_out.split()[0]
 
 	# Run test case
-	command_line = "./ex5-nguyen -no-vis -nx "+str(nx)+" -ny "+str(ny)+" -p "+problem+" -o "+order
+	command_line = "./ex5-nguyen -no-vis"
+	command_line += " -nx " + str(nx)
+	command_line += " -ny " + str(ny)
+	command_line += " -p " + str(problem)
+	command_line += " -o " + str(order)
 	if dg:
-		command_line = command_line+' -dg'
+		command_line += ' -dg'
 	if bcn:
-		command_line = command_line+' -bcn'
+		command_line += ' -bcn'
 	if hb:
-		command_line = command_line+' -hb'
+		command_line += ' -hb'
 	if upwind:
-		command_line = command_line+' -up'
+		command_line += ' -up'
 	if nonlin:
-		command_line = command_line+' -nl'
+		command_line += ' -nl'
 	if nonlin_conv:
-		command_line = command_line+' -nlc'
+		command_line += ' -nlc'
 	if nonlin_diff:
-		command_line = command_line+' -nld'
-	if kappa != str(1):
-		command_line = command_line+' -k '+kappa
-	if hdg != str(1):
-		command_line = command_line+' -hdg '+hdg
+		command_line += ' -nld'
+	if kappa != 1.:
+		command_line += ' -k ' + str(kappa)
+	if hdg != 1:
+		command_line += ' -hdg ' + str(hdg)
 
 	cmd_out = subprocess.getoutput(command_line)
 	split_cmd_out = cmd_out.splitlines()
@@ -91,21 +95,27 @@ for i in range(len(filenames)):
 	precond_test_idx_s = split_cmd_out[-4].find('+')
 	precond_test_idx_e = split_cmd_out[-4].find(' ')
 
-	test_L2_t = float(split_cmd_out[-1][indx_t+2::])
-	test_L2_q = float(split_cmd_out[-2][indx_q+2::])
-	precond_test = split_cmd_out[-4][precond_test_idx_s+1:precond_test_idx_e]
+	fail = False
+
+	try:
+		test_L2_t = float(split_cmd_out[-1][indx_t+2::])
+		test_L2_q = float(split_cmd_out[-2][indx_q+2::])
+		precond_test = split_cmd_out[-4][precond_test_idx_s+1:precond_test_idx_e]
+	except:
+		fail = True
 
 	if precond_test == precond_ref:
 		if abs(ref_L2_t - test_L2_t) < tol and abs(ref_L2_q - test_L2_q) < tol:
-			print(bcolors.OKGREEN + "SUCCESS: " + bcolors.RESET, end="", flush=True)
-			print(command_line)
+			print(bcolors.OKGREEN + "SUCCESS: " + bcolors.RESET + command_line, flush=True)
 		else:
-			print(bcolors.FAIL + "FAIL: " + bcolors.RESET, end="", flush=True)
-			print(command_line)
-			print(cmd_out)
-			failed += 1
+			fail = True
 	else:
-		print(bcolors.HEADER + "SKIPPING: "+ bcolors.RESET +command_line+" → incompatible preconditioner")
+		print(bcolors.HEADER + "SKIPPING: "+ bcolors.RESET + command_line + " → incompatible preconditioner")
+	
+	if fail:
+		print(bcolors.FAIL + "FAIL: " + bcolors.RESET + command_line, flush=True)
+		print(cmd_out)
+		failed += 1
 
 print("----------------------------------------------------------------")
 if failed == 0:
