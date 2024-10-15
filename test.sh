6df9fc2c9d96c5594a5e0534a1405a5e3745bfdf
Jan-Phillip Baecker:MAIL:TERMINAL:CLOUD <jbaecker@lws2024-05.mathematik.tu-dortmund.de>
2024-10-15 14:00:25 +0200

    Deleted testing file

--- test.sh
+++
@@ -1,4 +0,0 @@
-make serial MFEM_USE=SUITESPARSE=YES SUITESPARSE_OPT=-I/usr/include/suitesparse MFEM_USE_LAPACK=YES LAPACK_LIB='-lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -L/sfw/intel/2024.0.1/compiler/latest/lib -liomp5 -lpthread' -j 11
-cd examples
-make ex38
-./ex38 -i volumetric3d -r 3
