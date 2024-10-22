3c2e847e0a4bfcf1774de25683b264a9e4507e6d
Will Pazner <will.e.p@gmail.com>
2024-10-22 14:12:20 -0700

    Factor out test same matrix unit test utils

--- tests/unit/fem/test_lor_batched.cpp
+++ tests/unit/fem/test_lor_batched.cpp
@@ -11,6 +11,7 @@
 
 #include "mfem.hpp"
 #include "unit_tests.hpp"
+#include "../linalg/test_same_matrices.hpp"
 #include "../../fem/lor/lor_ads.hpp"
 #include "../../fem/lor/lor_ams.hpp"
 #include <memory>
@@ -24,47 +25,6 @@ using namespace mfem;
 
 namespace lor_batched
 {
-
-void TestSameMatrices(SparseMatrix &A1, const SparseMatrix &A2,
-                      HYPRE_BigInt *cmap1=nullptr,
-                      std::unordered_map<HYPRE_BigInt,int> *cmap2inv=nullptr)
-{
-   REQUIRE(A1.Height() == A2.Height());
-   int n = A1.Height();
-
-   const int *I1 = A1.HostReadI();
-   const int *J1 = A1.HostReadJ();
-   const real_t *V1 = A1.HostReadData();
-
-   A2.HostReadI();
-   A2.HostReadJ();
-   A2.HostReadData();
-
-   real_t error = 0.0;
-
-   for (int i=0; i<n; ++i)
-   {
-      for (int jj=I1[i]; jj<I1[i+1]; ++jj)
-      {
-         int j = J1[jj];
-         if (cmap1)
-         {
-            if (cmap2inv->count(cmap1[j]) > 0)
-            {
-               j = (*cmap2inv)[cmap1[j]];
-            }
-            else
-            {
-               error = std::max(error, std::fabs(V1[jj]));
-               continue;
-            }
-         }
-         error = std::max(error, std::fabs(V1[jj] - A2(i,j)));
-      }
-   }
-
-   REQUIRE(error == MFEM_Approx(0.0, 1e-10));
-}
 
 template <typename FE_COLL>
 FE_COLL *NewLOR_FE_Collection(int order, int dim)
@@ -156,30 +116,6 @@ TEST_CASE("LOR Batched RT", "[LOR][BatchedLOR][CUDA]")
 }
 
 #ifdef MFEM_USE_MPI
-
-void TestSameMatrices(HypreParMatrix &A1, const HypreParMatrix &A2)
-{
-   HYPRE_BigInt *cmap1, *cmap2;
-   SparseMatrix diag1, offd1, diag2, offd2;
-
-   A1.GetDiag(diag1);
-   A2.GetDiag(diag2);
-   A1.GetOffd(offd1, cmap1);
-   A2.GetOffd(offd2, cmap2);
-
-   TestSameMatrices(diag1, diag2);
-
-   if (cmap1)
-   {
-      std::unordered_map<HYPRE_BigInt,int> cmap2inv;
-      for (int i=0; i<offd2.Width(); ++i) { cmap2inv[cmap2[i]] = i; }
-      TestSameMatrices(offd1, offd2, cmap1, &cmap2inv);
-   }
-   else
-   {
-      TestSameMatrices(offd1, offd2);
-   }
-}
 
 template <typename FE_COLL, typename INTEG_1, typename INTEG_2>
 void ParTestBatchedLOR()
