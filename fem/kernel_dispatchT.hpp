93d771284825531c5f37c838a861820b31ed06e2
camierjs <camierjs@Io>
2024-10-22 16:36:07 -0700

    wip with T

---
+++ fem/kernel_dispatchT.hpp
@@ -0,0 +1,140 @@
+// Copyright (c) 2010-2024, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#ifndef MFEM_KERNEL_DISPATCH_T_HPP
+#define MFEM_KERNEL_DISPATCH_T_HPP
+
+#include "../config/config.hpp"
+#include "kernel_reporter.hpp"
+#include <unordered_map>
+#include <tuple>
+#include <cstddef>
+
+namespace mfem
+{
+
+#define MFEM_PARAM_LIST(...) __VA_ARGS__
+
+// P1 are the parameters, P2 are the optional (non-dispatch parameters), and P3
+// is the concatenation of P1 and P2. We need to pass it as a separate argument
+// to avoid a trailing comma in the case that P2 is empty.
+#define MFEM_REGISTER_KERNELS_T(KernelName, KernelType, T, P1)         \
+   class KernelName : public KernelDispatchTable<                      \
+                         KernelName, KernelType, T,                    \
+                         internal::KernelTypeList<MFEM_PARAM_LIST P1>> \
+   {                                                                   \
+   public:                                                             \
+      const char *kernel_name = MFEM_KERNEL_NAME(KernelName);          \
+      using KernelSignature = KernelType;                              \
+      template <typename T, MFEM_PARAM_LIST P1>                        \
+      static KernelSignature Kernel();                                 \
+      static KernelSignature Fallback(T, MFEM_PARAM_LIST P1);          \
+      static KernelName &Get()                                         \
+      {                                                                \
+         static KernelName table;                                      \
+         return table;                                                 \
+      }                                                                \
+   }
+
+/// @brief Hashes variadic packs for which each type contained in the variadic
+/// pack has a specialization of `std::hash` available.
+///
+/// For example, packs containing int, bool, enum values, etc.
+template <typename... KernelParameters>
+struct KernelDispatchKeyHash
+{
+private:
+   template <int N>
+   size_t operator()(std::tuple<KernelParameters...> value) const
+   {
+      return 0;
+   }
+
+   // The hashing formula here is taken directly from the Boost library, with
+   // the magic number 0x9e3779b9 chosen to minimize hashing collisions.
+   template <std::size_t N, typename THead, typename... TTail>
+   size_t operator()(std::tuple<KernelParameters...> value) const
+   {
+      constexpr int Index = N - sizeof...(TTail) - 1;
+      auto lhs_hash = std::hash<THead>()(std::get<Index>(value));
+      auto rhs_hash = operator()<N, TTail...>(value);
+      return lhs_hash ^
+             (rhs_hash + 0x9e3779b9 + (lhs_hash << 6) + (lhs_hash >> 2));
+   }
+
+public:
+   /// Returns the hash of the given @a value.
+   size_t operator()(std::tuple<KernelParameters...> value) const
+   {
+      return operator()<sizeof...(KernelParameters), KernelParameters...>(
+         value);
+   }
+};
+
+namespace internal
+{
+template <typename... Types> struct KernelTypeList
+{
+};
+} // namespace internal
+
+template <typename... T> class KernelDispatchTable
+{
+};
+
+template <typename Kernels, typename Signature, typename T, typename... Params>
+class KernelDispatchTable<Kernels,
+                          Signature,
+                          T,
+                          internal::KernelTypeList<Params...>>
+{
+   std::unordered_map<std::tuple<T, Params...>,
+                      Signature,
+                      KernelDispatchKeyHash<T, Params...>>
+      table;
+
+public:
+   /// @brief Run the kernel with the given dispatch parameters and arguments.
+   ///
+   /// If a compile-time specialized version of the kernel with the given
+   /// parameters has been registered, it will be called. Otherwise, the
+   /// fallback kernel will be called.
+   template <typename... Args>
+   static void Run(Params... params, Args &&...args)
+   {
+      const auto &table = Kernels::Get().table;
+      const std::tuple<T, Params...> key = std::make_tuple(T{}, params...);
+      const auto it = table.find(key);
+      if (it != table.end()) { it->second(std::forward<Args>(args)...); }
+      else
+      {
+         KernelReporter::ReportFallback(Kernels::Get().kernel_name, params...);
+         Kernels::Fallback(T{}, params...)(std::forward<Args>(args)...);
+      }
+   }
+
+   /// Register a specialized kernel for dispatch.
+   template <Params... PARAMS>
+   struct Specialization
+   {
+      // Version without optional parameters
+      static void Add()
+      {
+         std::tuple<T, Params...> param_tuple(T{}, PARAMS...);
+         Kernels::Get().table[param_tuple] =
+            Kernels::template Kernel<T, PARAMS...>();
+      };
+   };
+};
+
+} // namespace mfem
+
+#endif // MFEM_KERNEL_DISPATCH_T_HPP
--- fem/tmop/metrics/h2s_001.cpp
+++ fem/tmop/metrics/h2s_001.cpp
@@ -28,25 +28,25 @@ struct hash<mfem::TMOP_PA_Metric_001>
 };
 } // namespace std
 
-#include "../../kernel_dispatch.hpp"
+#include "../../kernel_dispatchT.hpp"
 
 namespace mfem
 {
 
 using kernel_t = void (*)(const TMOP_Integrator *, const Vector &x);
 
-using metric_t = decltype(TMOP_PA_Metric_001{});
+using metric_t = decltype(mfem::TMOP_PA_Metric_001{});
 
-MFEM_REGISTER_KERNELS(Kernels, kernel_t, (int, int, int)); // D, Q
+MFEM_REGISTER_KERNELS_T(Kernels, kernel_t, metric_t, (int, int)); // D, Q
 
-template <int M, int D, int Q>
+template <typename M, int D, int Q>
 kernel_t Kernels::Kernel()
 {
-   dbg("TMOP_PA_Metric_001, M:{}, D:{}, Q:{}", M, D, Q);
+   dbg("TMOP_PA_Metric_001, D:{}, Q:{}", D, Q);
    return TMOPSetupGradPA2D_Kernel<TMOP_PA_Metric_001, D, Q>;
 }
 
-kernel_t Kernels::Fallback(int, int, int)
+kernel_t Kernels::Fallback(metric_t, int, int)
 {
    dbg("TMOP_PA_Metric_001, Fallback");
    return TMOPSetupGradPA2D_Kernel<TMOP_PA_Metric_001>;
@@ -55,8 +55,8 @@ kernel_t Kernels::Fallback(int, int, int)
 // static auto add_kernels = [] { return (TMOPAdd<metric_t, Kernels>(), 0); }();
 static auto add_kernels = []
 {
-   return (Kernels::template Specialization<(int)metric_t{}, 2, 2>::Add(),
-           Kernels::template Specialization<(int)metric_t{}, 2, 3>::Add(), 0);
+   return (Kernels::template Specialization<2, 2>::Add(),
+           Kernels::template Specialization<2, 3>::Add(), 0);
 }();
 
 void TMOPAssembleGradPA_001(TMOPSetupGradPA2D &ker,
@@ -67,7 +67,7 @@ void TMOPAssembleGradPA_001(TMOPSetupGradPA2D &ker,
 
    const int d = ker.Ndof(), q = ker.Nqpt();
    dbg("TMOP_PA_Metric_001, d:{}, q:{}", d, q);
-   Kernels::Run((int)metric_t{}, d, q, ti, x);
+   Kernels::Run(d, q, ti, x);
 }
 
 } // namespace mfem
