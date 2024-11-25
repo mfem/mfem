1aed4df177ece7f7e85bbd6d2304db4c66d7b847
bslazarov <bslazarov@gmail.com>
2024-11-25 15:31:56 -0800

    iuh

---
+++ examples/remap_opt.cpp
@@ -0,0 +1,129 @@
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+
+#include "remap_opt.hpp"
+
+int main(int argc, char *argv[])
+{
+   // 1. Initialize MPI and HYPRE.
+   mfem::Mpi::Init();
+   int num_procs = mfem::Mpi::WorldSize();
+   int myid = mfem::Mpi::WorldRank();
+   mfem::Hypre::Init();
+
+   // 2. Parse command-line options.
+   const char *mesh_file = "../data/inline-quad.mesh";
+   int order = 2;
+   bool static_cond = false;
+   bool pa = false;
+   bool fa = false;
+   const char *device_config = "cpu";
+   bool visualization = true;
+   bool algebraic_ceed = false;
+
+   mfem::OptionsParser args(argc, argv);
+   args.AddOption(&mesh_file, "-m", "--mesh",
+                  "Mesh file to use.");
+   args.AddOption(&order, "-o", "--order",
+                  "Finite element order (polynomial degree) or -1 for"
+                  " isoparametric space.");
+   args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
+                  "--no-static-condensation", "Enable static condensation.");
+   args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
+                  "--no-partial-assembly", "Enable Partial Assembly.");
+   args.AddOption(&fa, "-fa", "--full-assembly", "-no-fa",
+                  "--no-full-assembly", "Enable Full Assembly.");
+   args.AddOption(&device_config, "-d", "--device",
+                  "Device configuration string, see Device::Configure().");
+#ifdef MFEM_USE_CEED
+   args.AddOption(&algebraic_ceed, "-a", "--algebraic",
+                  "-no-a", "--no-algebraic",
+                  "Use algebraic Ceed solver");
+#endif
+   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
+                  "--no-visualization",
+                  "Enable or disable GLVis visualization.");
+   args.Parse();
+   if (!args.Good())
+   {
+      if (myid == 0)
+      {
+         args.PrintUsage(std::cout);
+      }
+      return 1;
+   }
+   if (myid == 0)
+   {
+      args.PrintOptions(std::cout);
+   }
+
+   // 3. Enable hardware devices such as GPUs, and programming models such as
+   //    CUDA, OCCA, RAJA and OpenMP based on command line options.
+   mfem::Device device(device_config);
+   if (myid == 0) { device.Print(); }
+
+   // 4. Read the (serial) mesh from the given mesh file on all processors.  We
+   //    can handle triangular, quadrilateral, tetrahedral, hexahedral, surface
+   //    and volume meshes with the same code.
+   mfem::Mesh mesh(mesh_file, 1, 1);
+   int dim = mesh.Dimension();
+
+
+   // 5. Refine the serial mesh on all processors to increase the resolution. In
+   //    this example we do 'ref_levels' of uniform refinement. We choose
+   //    'ref_levels' to be the largest number that gives a final mesh with no
+   //    more than 10,000 elements.
+   {
+      int ref_levels =
+         (int)floor(log(10000./mesh.GetNE())/log(2.)/dim);
+      for (int l = 0; l < ref_levels; l++)
+      {
+         mesh.UniformRefinement();
+      }
+   }
+
+   // 6. Define a parallel mesh by a partitioning of the serial mesh. Refine
+   //    this mesh further in parallel to increase the resolution. Once the
+   //    parallel mesh is defined, the serial mesh can be deleted.
+   mfem::ParMesh pmesh(MPI_COMM_WORLD, mesh);
+   mesh.Clear();
+   {
+      int par_ref_levels = 2;
+      for (int l = 0; l < par_ref_levels; l++)
+      {
+         pmesh.UniformRefinement();
+      }      
+   }
+
+
+   // Define the discontinuous DG finite element space of the given
+   // polynomial order on the refined mesh.
+   const int btype = mfem::BasisType::Positive;
+   mfem::DG_FECollection fec(order, dim, btype);
+   mfem::ParFiniteElementSpace pfes(&pmesh, &fec);
+
+   mfem::ParGridFunction tgf(&pfes);
+   SphCoefficient sph;
+   tgf.ProjectCoefficient(sph);
+
+   {
+      L2Objective* obj=new L2Objective(pfes,tgf);
+      std::cout<<obj->Eval(tgf.GetTrueVector())<<" "<<std::endl;
+      obj->Test();
+      delete obj;
+   }
+
+   {
+      VolConstr* g=new VolConstr(pfes,0.0);
+      std::cout<<g->Eval(tgf.GetTrueVector())<<" "<<std::endl;
+      g->Test();
+      delete g;
+   }
+
+
+
+
+   return 0;
+}
--- examples/remap_opt.hpp
+++ examples/remap_opt.hpp
@@ -3,6 +3,40 @@
 
 #include "mfem.hpp"
 
+class SphCoefficient:public mfem::Coefficient
+{
+public:
+    SphCoefficient(mfem::real_t r_=1.0):r(r_)
+    {
+
+    }
+
+    ~SphCoefficient()
+    {
+
+
+    }
+
+    virtual
+    mfem::real_t Eval(mfem::ElementTransformation &T,
+                const mfem::IntegrationPoint &ip)
+    {
+        mfem::Vector tmpv;
+        tmpv.SetSize(T.GetSpaceDim());
+        T.Transform(ip,tmpv);
+
+        mfem::real_t rez=tmpv.Norml2();
+
+        if(rez<r){return 1.0;}
+        else{return 0.0;}
+    }
+
+
+private:
+    mfem::real_t r;
+};
+
+
 class L2Objective:public mfem::Operator
 {
 public:
@@ -28,7 +62,7 @@ public:
         cgf.SetFromTrueDofs(x);
         gfc.SetGridFunction(&cgf);
         mfem::real_t vv=tgf->ComputeL2Error(gfc);
-        return vv*vv;
+        return 0.5*vv*vv;
     }
 
     virtual
@@ -36,6 +70,43 @@ public:
     {
         bf->Mult(x,y);
         y-=tgv;
+    }
+
+    void Test()
+    {
+        mfem::ConstantCoefficient cc(0.5);
+        mfem::ParGridFunction lgf(*tgf);
+        lgf.ProjectCoefficient(cc);
+
+        mfem::Vector x=lgf.GetTrueVector();
+        mfem::Vector p; p.SetSize(x.Size()); p.Randomize();
+        mfem::Vector g; g.SetSize(x.Size());
+         mfem::Vector tmpv; tmpv.SetSize(x.Size());
+
+        double lo=this->Eval(x);
+        this->Mult(x,g);
+
+        mfem::real_t nd=mfem::InnerProduct(cgf.ParFESpace()->GetComm(),p,p);
+        mfem::real_t td=mfem::InnerProduct(cgf.ParFESpace()->GetComm(),p,g);
+
+        td=td/nd;
+
+        double lsc=1.0;
+        double lqoi;
+
+        for(int l=0;l<10;l++){
+           lsc/=10.0;
+           p/=10.0;
+
+           add(p,x,tmpv);
+           lqoi=this->Eval(tmpv);
+           mfem::real_t ld=(lqoi-lo)/lsc;
+           if(cgf.ParFESpace()->GetMyRank()==0){
+             std::cout << "dx=" << lsc <<" FD approximation=" << ld/nd
+                        << " gradient=" << td
+                        << " err=" << std::fabs(ld/nd-td) << std::endl;
+           }
+        }
     }
 
 
@@ -53,8 +124,7 @@ private:
 class VolConstr:public mfem::Operator
 {
 public:
-    VolConstr(mfem::ParMesh& mesh, //target mesh
-              mfem::ParFiniteElementSpace& pfes_, //fes of the density field
+    VolConstr(mfem::ParFiniteElementSpace& pfes_, //fes of the density field
               double vol_): //target volume
                 pfes(&pfes_),vol(vol_)
     {
@@ -88,6 +158,45 @@ public:
         l->ParallelAssemble(y);
     }
 
+    void Test()
+    {
+        mfem::ConstantCoefficient cc(0.5);
+        mfem::ParGridFunction lgf(pfes);
+        lgf.ProjectCoefficient(cc);
+
+        mfem::Vector x=lgf.GetTrueVector();
+        mfem::Vector p; p.SetSize(x.Size()); p.Randomize();
+        mfem::Vector g; g.SetSize(x.Size());
+         mfem::Vector tmpv; tmpv.SetSize(x.Size());
+
+        double lo=this->Eval(x);
+        this->Mult(x,g);
+
+        mfem::real_t nd=mfem::InnerProduct(pfes->GetComm(),p,p);
+        mfem::real_t td=mfem::InnerProduct(pfes->GetComm(),p,g);
+
+        td=td/nd;
+
+        double lsc=1.0;
+        double lqoi;
+
+        for(int l=0;l<10;l++){
+           lsc/=10.0;
+           p/=10.0;
+           add(p,x,tmpv);
+           lqoi=this->Eval(tmpv);
+           mfem::real_t ld=(lqoi-lo)/lsc;
+           if(pfes->GetMyRank()==0){
+             std::cout << "dx=" << lsc <<" FD approximation=" << ld/nd
+                        << " gradient=" << td
+                        << " err=" << std::fabs(ld/nd-td) << std::endl;
+           }
+        }
+    }
+
+
+
+
 private:
     mfem::ConstantCoefficient one;
     mfem::ParFiniteElementSpace* pfes;
@@ -96,6 +205,50 @@ private:
     mfem::ParLinearForm* l;
     mfem::ParGridFunction* gf;
 
+};
+
+class MDSolver
+{
+public:
+    MDSolver(mfem::ParFiniteElementSpace& pfes,
+         mfem::real_t vol_,
+         mfem::ParGridFunction& trg,
+         mfem::Vector& u_min_,
+         mfem::Vector& u_max_):cfg(pfes),tfg(pfes)
+    {
+       tfg=u_min_; u_max=tfg.GetTrueVector(); 
+       tfg=u_min_; u_min=tfg.GetTrueVector():
+
+       vol=vol_;
+       obj=new L2Objective(pfes,trg);
+
+       con=new VolConstr(pfes,vol);
+
+       //initialize the current solution
+       for(int i=0;i<cfg.Size();i++){
+        cfg[i]=0.5*(u_min[i]+u_max[i]);
+       }
+    }
+
+    ~MDSolver()
+    {
+        delete obj;
+        delete con;
+    }
+
+    void Optimize(double alpha, double rho)
+    {
+
+    }
+
+private:
+    mfem::ParGridFunction cfg;
+    mfem::ParGridFunction tfg;
+    mfem::Vector u_max;
+    mfem::Vector u_min;
+    mfem::real_t vol;
+    L2Objective* obj;
+    VolConstr* con;
 };
 
 
