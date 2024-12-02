b69da628d2ebf0690ac2418e2a5686d7498dc4bd
Dohyun Kim <dohyun_kim@brown.edu>
2024-12-02 13:40:51 -0500

    sphere tested

---
+++ miniapps/mani-swe/test_sphere.cpp
@@ -0,0 +1,44 @@
+#include "mfem.hpp"
+
+using namespace mfem;
+void sphere(const Vector &x, Vector &y)
+{
+   y = x;
+   y /= y.Norml2();
+}
+
+int main(int argc, char *argv[])
+{
+   Mpi::Init();
+   int num_procs = Mpi::WorldSize();
+   int myid = Mpi::WorldRank();
+
+   int order = 2;
+   int refinement_level = 3;
+
+   Hypre::Init();
+   Mesh mesh("./data/icosahedron.mesh");
+   ParMesh pmesh(MPI_COMM_WORLD, mesh);
+   mesh.Clear();
+
+   pmesh.SetCurvature(order);
+   ParGridFunction &x = static_cast<ParGridFunction&>(*pmesh.GetNodes());
+   VectorFunctionCoefficient sphere_cf(3, sphere);
+   x.ProjectCoefficient(sphere_cf);
+   for(int i=0; i<refinement_level; i++)
+   {
+      pmesh.UniformRefinement();
+      x.ProjectCoefficient(sphere_cf);
+   }
+
+   bool visualization = true;
+   if (visualization)
+   {
+      char vishost[] = "localhost";
+      int  visport   = 19916;
+      socketstream sol_sock(vishost, visport);
+      sol_sock << "parallel " << num_procs << " " << myid << "\n";
+      sol_sock.precision(8);
+      sol_sock << "solution\n" << pmesh << x << std::flush;
+   }
+}
