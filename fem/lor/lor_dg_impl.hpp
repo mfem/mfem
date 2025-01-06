d9bdb34edf0b25d6a357a9c314a5f7534dfaec4e
Toni-ko <anthonykolshorn@lclark.edu>
2025-01-06 15:08:12 -0800

    testing

--- fem/lor/lor_dg_impl.hpp
+++ fem/lor/lor_dg_impl.hpp
@@ -43,7 +43,6 @@ void BatchedLOR_DG::Assemble2D()
 
    const auto w_1d = ir_pp1.GetWeights().Read();
    const auto W = Reshape(ir.GetWeights().Read(), nd1d, nd1d);
-
    const auto X = Reshape(X_vert.Read(), 2, pp2, pp2, nel_ho);
 
    sparse_ij.SetSize(nnz_per_row*ndof_per_el*nel_ho);
@@ -62,6 +61,9 @@ void BatchedLOR_DG::Assemble2D()
       {
          for (int ix = 0; ix < nd1d; ++ix)
          {
+            //std::cout << "element " << iel_ho << std::endl;
+            const real_t A_ref = 0.5*(2*(ir_pp2[ix].x)*(ir_pp2[iy].x) + 2*(ir_pp2[ix+1].x)*(ir_pp2[iy+1].x) - 2*(ir_pp2[ix+1].x)*(ir_pp2[iy].x) - 2*(ir_pp2[ix].x)*(ir_pp2[iy+1].x));
+            const real_t A_el = 0.5*(2*X(0,ix,iy,iel_ho)*X(1,ix,iy,iel_ho) + 2*X(0,ix+1,iy+1,iel_ho)*X(1,ix+1,iy+1,iel_ho) - 2*X(0,ix+1,iy,iel_ho)*X(1,ix+1,iy,iel_ho) - 2*X(0,ix,iy+1,iel_ho)*X(1,ix,iy+1,iel_ho));
             const real_t mq = const_mq ? MQ(0,0,0) : MQ(ix, iy, iel_ho);
             const real_t dq = const_dq ? DQ(0,0,0) : DQ(ix, iy, iel_ho);
             for (int n_idx = 0; n_idx < 2; ++n_idx)
@@ -80,15 +82,27 @@ void BatchedLOR_DG::Assemble2D()
 
                   const bool bdr = (n_idx == 0) ? (i_0 == 0 || i_0 == pp1)
                                    : (j_0 == 0 || j_0 == pp1);
-
-                  const real_t h_face =1.0;
-                  const real_t h_el = 1.0; // from X_vert
-                  const real_t h = h_face/h_el;
                   const int w_idx = (n_idx == 0) ? iy : ix;
                   const int int_idx = (n_idx == 0) ? i_0 : j_0;
+                  const int el_idx = (n_idx == 0) ? j_0 : i_0;
+                  const int xy_idx = (n_idx == 0) ? 1 : 0;
                   if (bdr)
                   {
-                     V(v_idx, ix, iy, iel_ho) = -dq * kappa * w_1d[w_idx] * detJ(ix, iy,iel_ho)/h;
+                     const real_t el_1 = ir_pp2[el_idx+1].x - ir_pp2[el_idx].x;
+                     const real_t el_2 = A_ref / el_1;
+                     const real_t A_face = (n_idx == 0) ? X(1, i_0, j_0+1, iel_ho) - X(1, i_0, j_0, iel_ho) : X(0, i_0+1, j_0, iel_ho) - X(0, i_0, j_0, iel_ho);
+                     const real_t h_prime = A_el / A_face;
+                     const real_t h = (1/h_prime) * el_2;
+                     //std::cout << "A_ref = " << A_ref << std::endl;
+                     //std::cout << "el_1 = " << el_1 << std::endl;
+                     //std::cout << "el_2 = " << el_2 << std::endl;
+                     //std::cout << "A_el = " << A_el << std::endl;
+                     //std::cout << "A_face = " << A_face << std::endl;
+                     //std::cout << "h_prime = " << h_prime << std::endl;
+                     //std::cout << "h = " << h << std::endl;
+                     //std::cout << "x coord= " << X(0,ix,iy,iel_ho) << std::endl;
+                     //std::cout << "y coord= " << X(1,ix,iy,iel_ho) << std::endl;
+                     V(v_idx, ix, iy, iel_ho) = -dq * kappa * w_1d[w_idx] * detJ(ix, iy,iel_ho)*h;
                   }
                   else
                   {
@@ -97,49 +111,11 @@ void BatchedLOR_DG::Assemble2D()
                   }
                }
             }
-            /*
-            const real_t h_face = 1.0;
-            const real_t h_el = 1.0; // from X_vert
-            const real_t h = h_face/h_el;
-            if (ix == 0)
-            {
-               V(4, ix, iy, iel_ho) = -dq*kappa * w_1d[iy] * detJ(ix, iy,iel_ho)/h;
-            }
-            else
-            {
-               V(4, ix, iy, iel_ho) = -dq*w_1d[iy]/(ir[ix].x - ir[ix-1].x);
-            }
-            if (ix == ORDER)
-            {
-               V(2, ix, iy, iel_ho) = -dq*kappa * w_1d[iy] * detJ(ix, iy,iel_ho)/h;
-            }
-            else
-            {
-               V(2, ix, iy, iel_ho) = -dq*w_1d[iy]/(ir[ix+1].x - ir[ix].x);
-            }
-            if (iy == 0)
-            {
-               V(1, ix, iy, iel_ho) = -dq*kappa * w_1d[ix] * detJ(ix, iy,iel_ho)/h;
-            }
-            else
-            {
-               V(1, ix, iy, iel_ho) = -dq*w_1d[ix]/(ir[iy].x - ir[iy-1].x);
-            }
-            if (iy == ORDER)
-            {
-               V(3, ix, iy, iel_ho) = -dq*kappa * w_1d[ix] * detJ(ix, iy,iel_ho)/h;
-            }
-            else
-            {
-               V(3, ix, iy, iel_ho) = -dq*w_1d[ix]/(ir[iy+1].x - ir[iy].x);
-            }
-            */
+            V(0, ix, iy, iel_ho) = mq * detJ(ix, iy, iel_ho) * W(ix, iy);
             for (int i = 1; i < 5; ++i)
             {
                V(0, ix, iy, iel_ho) -= V(i, ix, iy, iel_ho);
             }
-
-            V(0, ix, iy, iel_ho) += mq * detJ(ix, iy, iel_ho) * W(ix, iy);
          }
       }
    });
