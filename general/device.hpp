13b9e14526998f1d1bc49cfff62e85c3c1a5b0a6
Andrew Ho <ho37@llnl.gov>
2025-01-09 14:28:15 -0800

    Moved implementations to cpp file
    removed host/device since this could lead to some ODR violations

--- general/device.hpp
+++ general/device.hpp
@@ -290,64 +290,21 @@ public:
 
    static bool GetGPUAwareMPI() { return Get().mpi_gpu_aware; }
 
-   /** Query the device driver for what memory type a given @a ptr is allocated with. */
+   /** Query the device driver for what memory type a given @a ptr is allocated
+    * with. */
    static MemoryType QueryMemoryType(void* ptr);
 
-   /** @brief The number of hardware compute units/streaming multiprocessors available on a given compute device @a dev. */
-   static int MFEM_HOST_DEVICE NumMultiprocessors(int dev)
-   {
-#if defined(MFEM_USE_CUDA)
-      int res;
-      cudaDeviceGetAttribute(&res, cudaDevAttrMultiProcessorCount, dev);
-      return res;
-#elif defined(MFEM_USE_HIP)
-      int res;
-      hipDeviceGetAttribute(&res, hipDeviceAttributeMultiprocessorCount, dev);
-      return res;
-#else
-      // not compiled with GPU support
-      return 0;
-#endif
-   }
+   /** @brief The number of hardware compute units/streaming multiprocessors
+    * available on a given compute device @a dev. */
+   static int NumMultiprocessors(int dev);
 
-   static int MFEM_HOST_DEVICE NumMultiprocessors()
-   {
-      int dev = 0;
-#if defined(MFEM_USE_CUDA)
-      cudaGetDevice(&dev);
-#elif defined(MFEM_USE_HIP)
-      hipGetDevice(&dev);
-#endif
-      return NumMultiprocessors(dev);
-   }
+   static int NumMultiprocessors();
 
-   /** @brief The number of threads in a warp on a given compute device @a dev. */
-   static int MFEM_HOST_DEVICE WarpSize(int dev)
-   {
-#if defined(MFEM_USE_CUDA)
-      int res;
-      cudaDeviceGetAttribute(&res, cudaDevAttrWarpSize, dev);
-      return res;
-#elif defined(MFEM_USE_HIP)
-      int res;
-      hipDeviceGetAttribute(&res, hipDeviceAttributeWarpSize, dev);
-      return res;
-#else
-      // not compiled with GPU support
-      return 0;
-#endif
-   }
+   /** @brief The number of threads in a warp on a given compute device @a dev.
+    */
+   static int WarpSize(int dev);
 
-   static int MFEM_HOST_DEVICE WarpSize()
-   {
-      int dev = 0;
-#if defined(MFEM_USE_CUDA)
-      cudaGetDevice(&dev);
-#elif defined(MFEM_USE_HIP)
-      hipGetDevice(&dev);
-#endif
-      return WarpSize(dev);
-   }
+   static int WarpSize();
 
    /** @brief Gets the @a free and @a total memory on the device. */
    static void DeviceMem(size_t *free, size_t *total);
