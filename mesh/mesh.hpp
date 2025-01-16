2e326c97bf19b134abdc9c3022297698518cb46e
Stowell, Mark L. <stowell1@llnl.gov>
2025-01-15 16:37:05 -0800

    Adding recommended functions to Mesh class

--- mesh/mesh.hpp
+++ mesh/mesh.hpp
@@ -2070,7 +2070,34 @@ public:
    void GetFaceInfos (int Face, int *Inf1, int *Inf2, int *NCFace) const;
 
    /// @brief Populate a marker array identifying exterior faces
-   virtual void GetExteriorFaceMarker(Array<int> & face_marker) const;
+   ///
+   /// @param[in,out] face_marker Resized if necessary to the number of
+   ///                            local faces. The array entries will be
+   ///                            zero for interior faces and 1 for exterior
+   ///                            faces.
+   virtual void GetExteriorFaceMarker(Array<int> &face_marker) const;
+
+   /// @brief Unmark boundary attributes of internal boundaries
+   ///
+   /// @param[in,out] bdr_marker Array of length bdr_attributes.Max().
+   ///                           Entries associated with internal boundaries
+   ///                           will be set to zero. Other entries will remain
+   ///                           unchanged.
+   /// @param[in]     excl       Only unmark entries which exclusively contain
+   ///                           internal faces [default: true].
+   virtual void RemoveInternalBoundaries(Array<int> &bdr_marker,
+                                         bool excl = true) const;
+
+   /// @brief Mark boundary attributes of external boundaries
+   ///
+   /// @param[in,out] bdr_marker Array will be resized to be at least of length
+   ///                           bdr_attributes.Max(). Entries associated with
+   ///                           external boundaries will be set to one. Other
+   ///                           entries will be set to zero.
+   /// @param[in]     excl       Only mark entries which exclusively contain
+   ///                           external faces [default: true].
+   virtual void MarkExternalBoundaries(Array<int> &bdr_marker,
+                                       bool excl = true) const;
 
    /// @}
 
