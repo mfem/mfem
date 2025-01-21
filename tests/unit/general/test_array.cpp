fb5e610ca50af7207adf7c338114a7251471f8b9
Hugh Carson <hughcars@amazon.com>
2025-01-21 13:54:55 -0500

    Add support and test for back_inserter along with some more stl-style interactions

--- tests/unit/general/test_array.cpp
+++ tests/unit/general/test_array.cpp
@@ -17,20 +17,27 @@ using namespace mfem;
 TEST_CASE("Array init-list and C-style array constructors", "[Array]")
 {
    int ContigData[6] = {6, 5, 4, 3, 2, 1};
-   // Pointer and size construcor
+   // Pointer and size constructor
    Array<int> a(ContigData, 6);
    // Braced-list constructor
    Array<int> b({6, 5, 4, 3, 2, 1});
+   Array<int> c{6, 5, 4, 3, 2, 1};
+   Array<int> d = {6, 5, 4, 3, 2, 1}; // implicit initializer_list
    // Statically sized C-style array constructor
-   Array<int> c(ContigData);
-   // Convertible type constructor
-   Array<int> d({6.0, 5.0, 4.0, 3.0, 2.0, 1.0});
-
+   Array<int> e(ContigData);
+   // Convertible type constructors
+   Array<int> f({6.0, 5.0, 4.0, 3.0, 2.0, 1.0});
+   Array<int> g{6.0, 5.0, 4.0, 3.0, 2.0, 1.0};
+   Array<int> h = {6.0, 5.0, 4.0, 3.0, 2.0, 1.0};
    for (int i = 0; i < a.Size(); i++)
    {
       REQUIRE(a[i] == b[i]);
       REQUIRE(a[i] == c[i]);
       REQUIRE(a[i] == d[i]);
+      REQUIRE(a[i] == e[i]);
+      REQUIRE(a[i] == f[i]);
+      REQUIRE(a[i] == g[i]);
+      REQUIRE(a[i] == h[i]);
    }
 }
 
@@ -74,5 +81,50 @@ TEST_CASE("Array entry strict sorting", "[Array]")
    for (int i = 1; i < b.Size(); i++)
    {
       REQUIRE(b[i] > b[i-1]);
+   }
+}
+
+
+TEST_CASE("Array stl-interactions", "[Array]")
+{
+   Array<int> x{0,1,2,3,4}, y;
+   std::copy(x.begin(), x.end(), std::back_inserter(y));
+   CHECK(y.Size() == 5);
+   for (int i : {0,1,2,3,4})
+   {
+      CHECK(y[i] == i);
+   }
+   y.DeleteAll();
+
+   std::copy_if(x.begin(), x.end(), std::back_inserter(y), [](int x) { return x <= 2; });
+   CHECK(y.Size() == 3);
+   for (int i : {0,1,2})
+   {
+      CHECK(y[i] == i);
+   }
+
+   std::transform(y.begin(), y.end(), y.begin(), [](int x) { return x*x; });
+   CHECK(y.Size() == 3);
+   for (int i : {0,1,2})
+   {
+      CHECK(y[i] == i*i);
+   }
+   y.DeleteAll();
+   for (auto i : x)
+   {
+      y.Append(i);
+   }
+   for (int i : {0,1,2,3,4})
+   {
+      CHECK(x[i] == y[i]);
+   }
+   y.DeleteAll();
+   for (const auto &i : x)
+   {
+      y.Append(i);
+   }
+   for (int i : {0,1,2,3,4})
+   {
+      CHECK(x[i] == y[i]);
    }
 }
