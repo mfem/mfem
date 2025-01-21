8cc47d606e3f30b2b135f6a91dcf1d347f8a7524
bslazarov <bslazarov.dev@gmail.com>
2025-01-21 12:47:11 -0800

    initial randomized eigenvalue solverw

---
+++ miniapps/mtop/rand_eigensolver.cpp
@@ -0,0 +1,2 @@
+#include "rand_eigensolver.hpp"
+
---
+++ miniapps/mtop/rand_eigensolver.hpp
@@ -0,0 +1,50 @@
+#ifndef RAND_EIGENSOLVER_HPP
+#define RAND_EIGENSOLVER_HPP
+
+#include "mfem.hpp"
+
+#include "../config/config.hpp"
+
+namespace mfem{
+
+class RandEigenSolver{
+public:
+
+    RandEigenSolver(MPI_Comm comm);
+
+    ~RandEigenSolver();
+
+    void SetNumModes(int num_);
+
+    void SetMaxIter(int max_it_);
+
+    void SetTol(real_t tol_);
+
+    void SetShift(real_t shift_);
+
+    void SetOperator(const Operator& A_);
+
+    void SetOperators(const Operator& A_, const Operator& B_);
+
+    void Solve();
+
+    int GetNumConverged();
+
+    void GetEigenvalue(unsigned int i, real_t & lr) const;
+
+    void GetEigenvalue(unsigned int i, real_t & lr, real_t & lc) const;
+
+    void GetEigenvector(unsigned int i, Vector & vr) const;
+
+    void GetEigenvector(unsigned int i, Vector & vr, Vector & vc) const;
+
+private:
+
+    Operator* A;
+    Operator* B;
+
+};
+
+
+}
+#endif // RAND_EIGENSOLVER_HPP
