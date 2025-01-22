84c8f41936e55c07c52041c5925ed6464bb0a6b3
Justin Laughlin <laughlin6@llnl.gov>
2025-01-21 17:12:00 -0800

    Naming

--- fem/integ/bilininteg_diffusion_patch.cpp
+++ fem/integ/bilininteg_diffusion_patch.cpp
@@ -22,7 +22,7 @@ namespace mfem
 {
 
 // Adapted from PADiffusionSetup3D
-void SetupPatch3D(const int Q1Dx,
+void PatchDiffusionSetup3D(const int Q1Dx,
                   const int Q1Dy,
                   const int Q1Dz,
                   const int coeffDim,
@@ -145,7 +145,7 @@ void SetupPatch3D(const int Q1Dx,
 
 // Compute a reduced integration rule, using NNLSSolver, for DiffusionIntegrator
 // on a NURBS patch with partial assembly.
-void GetReducedRule(const int nq, const int nd,
+void PatchDiffusionGetReducedRule(const int nq, const int nd,
                     Array2D<real_t> const& B,
                     Array2D<real_t> const& G,
                     std::vector<int> minQ,
@@ -434,7 +434,7 @@ void DiffusionIntegrator::SetupPatchPA(const int patch, Mesh *mesh,
       weights = 1.0;
    }
 
-   SetupPatch3D(Q1D[0], Q1D[1], Q1D[2], coeffDim, symmetric, weights, jac,
+   PatchDiffusionSetup3D(Q1D[0], Q1D[1], Q1D[2], coeffDim, symmetric, weights, jac,
                 coeff, pa_data);
 
    numPatches = mesh->NURBSext->GetNP();
@@ -456,12 +456,12 @@ void DiffusionIntegrator::SetupPatchPA(const int patch, Mesh *mesh,
    {
       // The reduced rules could be cached to avoid repeated computation, but
       // the cost of this setup seems low.
-      GetReducedRule(Q1D[d], D1D[d], B[d], G[d],
+      PatchDiffusionGetReducedRule(Q1D[d], D1D[d], B[d], G[d],
                      minQ[d], maxQ[d],
                      minD[d], maxD[d],
                      minDD[d], maxDD[d], ir1d[d], true,
                      rw(0,d,patch), rid(0,d,patch));
-      GetReducedRule(Q1D[d], D1D[d], B[d], G[d],
+      PatchDiffusionGetReducedRule(Q1D[d], D1D[d], B[d], G[d],
                      minQ[d], maxQ[d],
                      minD[d], maxD[d],
                      minDD[d], maxDD[d], ir1d[d], false,
