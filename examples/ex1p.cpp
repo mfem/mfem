e2ed642f5b42daeeae2137e1e1ab79b68a143796
Stowell, Mark L. <stowell1@llnl.gov>
2025-01-21 20:26:17 -0800

    Correcting the comments in the examples

--- examples/ex1p.cpp
+++ examples/ex1p.cpp
@@ -190,8 +190,8 @@ int main(int argc, char *argv[])
 
    // 8. Determine the list of true (i.e. parallel conforming) essential
    //    boundary dofs. In this example, the boundary conditions are defined
-   //    by marking all the boundary attributes from the mesh as essential
-   //    (Dirichlet) and converting them to a list of true dofs.
+   //    by marking all the external boundary attributes from the mesh as
+   //    essential (Dirichlet) and converting them to a list of true dofs.
    Array<int> ess_tdof_list;
    if (pmesh.bdr_attributes.Size())
    {
