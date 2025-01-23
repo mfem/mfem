33f1150c34689edc943c13b7fd98caca7ec33aeb
Christopher vogl <vogl2@llnl.gov>
2025-01-22 16:14:09 -0800

    updated FindMETIS to also use ExternalProject

--- config/cmake/modules/FindMETIS.cmake
+++ config/cmake/modules/FindMETIS.cmake
@@ -15,34 +15,22 @@
 #   - METIS_INCLUDE_DIRS
 
 if (FETCH_TPLS)
-  message(STATUS "Fetching metis from GitHub ...")
-  include(FetchContent)
-  FetchContent_Declare(METIS
+  # define external project
+  set(PREFIX ${CMAKE_BINARY_DIR}/fetch/metis)
+  include(ExternalProject)
+  ExternalProject_Add(metis
     GIT_REPOSITORY https://github.com/mfem/tpls
     GIT_TAG b60352fbe9675d374b00828055e55be4584c7995 # tag from 1/16/25
     GIT_SHALLOW TRUE
-    BINARY_DIR fetch/metis
-    OVERRIDE_FIND_PACKAGE
-  )
-  # obtain source and create build directory, namely so include directory is
-  # present to pass MFEM checks at end of configuation step
-  FetchContent_MakeAvailable(METIS)
-  execute_process(COMMAND mkdir ${metis_BINARY_DIR}/include)
-  execute_process(COMMAND mkdir ${metis_BINARY_DIR}/Lib)
-  # create custom command and target for building
-  add_custom_command(OUTPUT ${metis_BINARY_DIR}/Lib/libmetis.a
-    COMMAND cd ${metis_SOURCE_DIR} && tar -xzf metis-4.0.3.tar.gz
-    COMMAND cd ${metis_SOURCE_DIR}/metis-4.0.3 && make -j
-    COMMAND cp ${metis_SOURCE_DIR}/metis-4.0.3/Lib/*.h ${metis_BINARY_DIR}/include/
-    COMMAND cp ${metis_SOURCE_DIR}/metis-4.0.3/libmetis.a ${metis_BINARY_DIR}/Lib/
-    COMMENT "Building metis ..."
-  )
-  add_custom_target(METIS_LIBRARY DEPENDS ${metis_BINARY_DIR}/Lib/libmetis.a)
+    PREFIX ${PREFIX}
+    CONFIGURE_COMMAND tar -xzf ../metis/metis-4.0.3.tar.gz
+    BUILD_COMMAND cd metis-4.0.3 && make OPTFLAGS="-Wno-error=implicit-function-declaration"
+    INSTALL_COMMAND mkdir ${PREFIX}/Lib && cp metis-4.0.3/libmetis.a ${PREFIX}/Lib/)
   # create interface library target for linking
-  add_library(METIS INTERFACE IMPORTED)
-  add_dependencies(METIS METIS_LIBRARY)
-  target_link_libraries(METIS INTERFACE ${metis_BINARY_DIR}/Lib/libmetis.a)
-  target_include_directories(METIS INTERFACE ${metis_BINARY_DIR}/include)
+  add_library(METIS STATIC IMPORTED)
+  add_dependencies(METIS metis metis-install)
+  set_target_properties(METIS PROPERTIES
+    IMPORTED_LOCATION ${PREFIX}/Lib/libmetis.a)
   # set cache variables that would otherwise be set after mfem_find_package call
   set(METIS_VERSION_5 FALSE CACHE BOOL "Is METIS version 5?")
   return()
