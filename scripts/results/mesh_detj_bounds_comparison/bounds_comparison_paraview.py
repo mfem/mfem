969f789d8e0cb01f5d84000dad8ce86596990ee1
tarikdzanic <41964501+tarikdzanic@users.noreply.github.com>
2025-02-06 13:54:14 -0800

    Updated script.

--- scripts/results/mesh_detj_bounds_comparison/bounds_comparison_paraview.py
+++ scripts/results/mesh_detj_bounds_comparison/bounds_comparison_paraview.py
@@ -13,53 +13,65 @@ renderView1.Background = [1.0, 1.0, 1.0]
 
 solutionDisplay = Show(solution, renderView1, 'UnstructuredGridRepresentation')
 solutionDisplay.Ambient = 0.25
-solutionDisplay.SetScalarBarVisibility(renderView1, False)
+solutionDisplay.NonlinearSubdivisionLevel = 3
 
-edges = XMLUnstructuredGridReader(FileName=[solution])
-edgesDisplay = Show(bounds_2, renderView1, 'UnstructuredGridRepresentation')
+
+
+
+edges = XMLUnstructuredGridReader(FileName=[path])
+edgesDisplay = Show(edges, renderView1, 'UnstructuredGridRepresentation')
+ColorBy(edgesDisplay, None)
 edgesDisplay.NonlinearSubdivisionLevel = 3
 edgesDisplay.SetRepresentationType('Wireframe')
-edgesDisplay.AmbientColor = [0.0, 0.0, 0.0]
-edgesDisplay.DiffuseColor = [0.0, 0.0, 0.0]
+edgesDisplay.AmbientColor = [1,1,1]
+edgesDisplay.DiffuseColor = [1,1,1]
 edgesDisplay.LineWidth = 1.5
 
 
-
-ColorBy(solutionDisplay, ('POINTS', 'u'))
-uLUT = GetColorTransferFunction('u')
-uLUT.ApplyPreset('Rainbow Desaturated', True)
-uLUT.NumberOfTableValues = 10
-
-# create a new 'Contour'
-contour1 = Contour(Input=solvtu)
-contour1.Isosurfaces = [-7.60548e-05, 0.08307875068, 0.16623355616, 0.24938836164, 0.33254316712, 
-                        0.41569797259999997, 0.49885277808, 0.58200758356, 0.66516238904, 0.74831719452, 0.831472]
-
-contour1Display = Show(contour1, renderView1, 'GeometryRepresentation')
-ColorBy(contour1Display, None)
-contour1Display.LineWidth = 2.0
-
-
-bounds = XMLUnstructuredGridReader(FileName=[boundpath])
-bounds_Display = Show(bounds, renderView1, 'UnstructuredGridRepresentation')
-bounds_Display.NonlinearSubdivisionLevel = 3
-bounds_Display.Opacity = 0.5
-
-
-bounds_3 = XMLUnstructuredGridReader(FileName=[loboundpath])
-bounds_3Display = Show(bounds_3, renderView1, 'UnstructuredGridRepresentation')
-bounds_3Display.NonlinearSubdivisionLevel = 1
-bounds_3Display.SetRepresentationType('Points')
-bounds_3Display.AmbientColor = [0.0, 0.0, 0.0]
-bounds_3Display.DiffuseColor = [0.0, 0.0, 0.0]
-bounds_3Display.PointSize = 6
-
-
 renderView1.OrientationAxesVisibility = 0
-renderView1.CameraPosition = [-1.016570841301479, -1.3554137382029485, 0.9299840088072358]
-renderView1.CameraFocalPoint = [0.38012337357455567, 0.3057432865904366, 0.4010583889725267]
-renderView1.CameraParallelScale = 1
+renderView1.CameraPosition = [3.5, 1.275, 6.55]
+renderView1.CameraFocalPoint = [3.5, 1.275, 0]
+renderView1.CameraParallelScale = 0.5
 renderView1.Background = [1, 1, 1]
-renderView1.CameraViewUp = [0.15031599573568624, 0.1803791948828235, 0.9720434390907711]
+renderView1.CameraViewUp = [0, 1, 0]
 
-SaveScreenshot(outpath, renderView1, ImageResolution=[800, 700])
+for var in ['bruteboundmin', 'bernboundmin', 'cusboundmin', 'bernerrmin', 'cuserrmin']:
+    solutionDisplay.SetScalarBarVisibility(renderView1, False)
+    ColorBy(solutionDisplay, ('POINTS', var))
+    LUT = GetColorTransferFunction(var)
+    LUT.ApplyPreset('Jet', True)
+    LUT.NumberOfTableValues = 1024
+    solutionDisplay.SetScalarBarVisibility(renderView1, True)
+    LUTColorBar = GetScalarBar(LUT, renderView1)
+    LUTColorBar.RangeLabelFormat = '%-#6.0g'
+    LUTColorBar.TextPosition = 'Ticks right/top, annotations left/bottom'
+    LUTColorBar.LabelColor = [0, 0, 0]
+    LUTColorBar.TitleColor = [1, 1, 1]
+    LUTColorBar.LabelBold = 1
+    LUTColorBar.TitleBold = 1
+    LUTColorBar.LabelFontSize = 12
+    LUTColorBar.TitleFontSize = 14
+    LUTColorBar.ComponentTitle = ''
+    LUTColorBar.Orientation = 'Horizontal'
+    LUTColorBar.WindowLocation = 'Any Location'
+    LUTColorBar.ScalarBarLength = 0.6
+    LUTColorBar.ScalarBarThickness = 12
+    LUTColorBar.Position = [0.2, 0.02]
+    LUTColorBar.HorizontalTitle = False
+    LUTColorBar.AddRangeLabels = 1
+    LUTColorBar.RangeLabelFormat = '%g'
+    solutionDisplay.SetScalarBarVisibility(renderView1, True)
+    if 'boundmin' in var:
+        LUTColorBar.Title = 'min det(J)'
+        LUT.RescaleTransferFunction(0, 0.3)
+        pvar = var.replace('boundmin', '')
+        outpath = path.replace('.vtu', f'_{pvar}.jpg')
+    else:
+        LUTColorBar.Title = 'Error'
+        LUT.UseLogScale = 1
+        LUT.MapControlPointsToLogSpace()
+        LUT.RescaleTransferFunction(1e-4, 1e-1)
+        pvar = var.replace('min', '')
+        outpath = path.replace('.vtu', f'_{pvar}.jpg')
+    SaveScreenshot(outpath, renderView1, ImageResolution=[1200, 600])
+    solutionDisplay.SetScalarBarVisibility(renderView1, False)
