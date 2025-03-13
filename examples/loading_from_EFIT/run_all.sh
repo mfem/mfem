cad43c4d87906a097654f72175e045cf5c9f76b4
rzhangbq <rzhangbq@gmail.com>
2025-03-12 22:54:29 -0400

    add run_all bash script

---
+++ examples/loading_from_EFIT/run_all.sh
@@ -0,0 +1,5 @@
+make all -j
+./B_field_projector_B_perp & ./B_field_projector_B_tor
+./J_field_projector_J_perp & ./J_field_projector_J_tor
+./JxB_validator_JxB_perp & ./JxB_validator_JxB_tor
+./JxB_validator_grad_p
