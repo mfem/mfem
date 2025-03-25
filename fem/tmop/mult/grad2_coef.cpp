c25a29cb15bad165926f263a1ea35f8dfe7221e9
camierjs <camierjs@gmail.com>
2025-03-25 12:25:09 -0700

    AddMultGradPA_C0_2D

--- fem/tmop/mult/grad2_coef.cpp
+++ fem/tmop/mult/grad2_coef.cpp
@@ -11,52 +11,49 @@
 
 #include "../pa.hpp"
 #include "../../tmop.hpp"
-#include "../../kernels.hpp"
+#include "../../kernels_regs.hpp"
 #include "../../../general/forall.hpp"
 #include "../../../linalg/kernels.hpp"
+
+using namespace mfem::kernels::internal;
 
 namespace mfem
 {
 
 template <int T_D1D = 0, int T_Q1D = 0>
 void TMOP_AddMultGradPA_C0_2D(const int NE,
-                              const ConstDeviceMatrix &B,
+                              const real_t *b,
                               const DeviceTensor<5, const real_t> &H0,
                               const DeviceTensor<4, const real_t> &X,
                               DeviceTensor<4> &Y,
                               const int d1d,
                               const int q1d)
 {
-   constexpr int NBZ = 1;
    const int D1D = T_D1D ? T_D1D : d1d;
    const int Q1D = T_Q1D ? T_Q1D : q1d;
-   MFEM_VERIFY(D1D <= DeviceDofQuadLimits::Get().MAX_TMOP_1D, "");
-   MFEM_VERIFY(Q1D <= DeviceDofQuadLimits::Get().MAX_TMOP_1D, "");
+   MFEM_VERIFY(D1D <= DeviceDofQuadLimits::Get().MAX_D1D, "");
+   MFEM_VERIFY(Q1D <= DeviceDofQuadLimits::Get().MAX_Q1D, "");
 
-   mfem::forall_2D_batch(NE, Q1D, Q1D, NBZ, [=] MFEM_HOST_DEVICE(int e)
+   mfem::forall_2D(NE, Q1D, Q1D, [=] MFEM_HOST_DEVICE(int e)
    {
-      constexpr int DIM = 2, NBZ = 1;
+      constexpr int DIM = 2;
       constexpr int MQ1 = T_Q1D ? T_Q1D : DofQuadLimits::MAX_TMOP_1D;
       constexpr int MD1 = T_D1D ? T_D1D : DofQuadLimits::MAX_TMOP_1D;
 
-      MFEM_SHARED real_t sB[MQ1 * MD1];
-      MFEM_SHARED real_t XY[2][NBZ][MD1 * MD1];
-      MFEM_SHARED real_t DQ[2][NBZ][MD1 * MQ1];
-      MFEM_SHARED real_t QQ[2][NBZ][MQ1 * MQ1];
+      MFEM_SHARED real_t smem[MQ1][MQ1];
+      MFEM_SHARED real_t sB[MD1][MQ1];
+      regs::LoadMatrix(D1D, Q1D, b, sB);
 
-      kernels::internal::LoadX<MD1, NBZ>(e, D1D, X, XY);
-      kernels::internal::LoadB<MD1, MQ1>(D1D, Q1D, B, sB);
+      regs::regs4d_t<2,1,MQ1> r0, r1;
+      regs::LoadDofs2d(e, D1D, X, r0);
+      regs::Eval2d(D1D, Q1D, smem, sB, r0, r1);
 
-      kernels::internal::EvalX<MD1, MQ1, NBZ>(D1D, Q1D, sB, XY, DQ);
-      kernels::internal::EvalY<MD1, MQ1, NBZ>(D1D, Q1D, sB, DQ, QQ);
-
-      MFEM_FOREACH_THREAD(qy, y, Q1D)
+      mfem::foreach_y_thread(Q1D, [&](int qy)
       {
-         MFEM_FOREACH_THREAD(qx, x, Q1D)
+         mfem::foreach_x_thread(Q1D, [&](int qx)
          {
             // Xh = X^T . Sh
-            real_t Xh[2];
-            kernels::internal::PullEval<MQ1, NBZ>(Q1D, qx, qy, QQ, Xh);
+            const real_t Xh[2] = { r1(0, 0, qy, qx), r1(1, 0, qy, qx) };
 
             real_t H_data[4];
             DeviceMatrix H(H_data, 2, 2);
@@ -68,13 +65,13 @@ void TMOP_AddMultGradPA_C0_2D(const int NE,
             // p2 = H . Xh
             real_t p2[2];
             kernels::Mult(2, 2, H_data, Xh, p2);
-            kernels::internal::PushEval<MQ1, NBZ>(Q1D, qx, qy, p2, QQ);
-         }
-      }
+            r0(0,0, qy,qx) = p2[0];
+            r0(0,1, qy,qx) = p2[1];
+         });
+      });
       MFEM_SYNC_THREAD;
-      kernels::internal::LoadBt<MD1, MQ1>(D1D, Q1D, B, sB);
-      kernels::internal::EvalXt<MD1, MQ1, NBZ>(D1D, Q1D, sB, QQ, DQ);
-      kernels::internal::EvalYt<MD1, MQ1, NBZ>(D1D, Q1D, sB, DQ, Y, e);
+      regs::EvalTranspose2d(D1D, Q1D, smem, sB, r0, r1);
+      regs::WriteDofs2d(e, D1D, r1, Y);
    });
 }
 
@@ -87,11 +84,11 @@ void TMOP_Integrator::AddMultGradPA_C0_2D(const Vector &R, Vector &C) const
    const int NE = PA.ne, d = PA.maps->ndof, q = PA.maps->nqpt;
 
    const auto H0 = Reshape(PA.H0.Read(), DIM, DIM, q, q, NE);
-   const auto B = Reshape(PA.maps->B.Read(), q, d);
+   const auto *b = PA.maps->B.Read();
    const auto X = Reshape(R.Read(), d, d, DIM, NE);
    auto Y = Reshape(C.ReadWrite(), d, d, DIM, NE);
 
-   TMOPMultGradCoefKernels::Run(d, q, NE, B, H0, X, Y, d, q);
+   TMOPMultGradCoefKernels::Run(d, q, NE, b, H0, X, Y, d, q);
 }
 
 } // namespace mfem
