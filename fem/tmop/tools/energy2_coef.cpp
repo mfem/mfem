a07f90e90a1e01dcd46a5275337ccaea0a978e6a
camierjs <camier1@llnl.gov>
2025-03-25 16:10:26 -0700

    TMOP_EnergyPA_C0_2D

--- fem/tmop/tools/energy2_coef.cpp
+++ fem/tmop/tools/energy2_coef.cpp
@@ -11,9 +11,11 @@
 
 #include "../pa.hpp"
 #include "../../tmop.hpp"
-#include "../../kernels.hpp"
+#include "../../kernels_regs.hpp"
 #include "../../../general/forall.hpp"
 #include "../../../linalg/kernels.hpp"
+
+using namespace mfem::kernels::internal;
 
 namespace mfem
 {
@@ -32,64 +34,59 @@ void TMOP_EnergyPA_C0_2D(const real_t lim_normal,
                          const DeviceTensor<4, const real_t> &X1,
                          DeviceTensor<3> &E,
                          const bool exp_lim,
-                         const int d1d,
-                         const int q1d)
+                         const int _d1d,
+                         const int _q1d)
 {
-   constexpr int NBZ = 1;
-   const int D1D = T_D1D ? T_D1D : d1d;
-   const int Q1D = T_Q1D ? T_Q1D : q1d;
-   MFEM_VERIFY(D1D <= DeviceDofQuadLimits::Get().MAX_TMOP_1D, "");
-   MFEM_VERIFY(Q1D <= DeviceDofQuadLimits::Get().MAX_TMOP_1D, "");
+   const int D1D = T_D1D ? T_D1D : _d1d;
+   const int Q1D = T_Q1D ? T_Q1D : _q1d;
+   MFEM_VERIFY(D1D <= DeviceDofQuadLimits::Get().MAX_D1D, "");
+   MFEM_VERIFY(Q1D <= DeviceDofQuadLimits::Get().MAX_Q1D, "");
+   const auto *bld_ptr = (const real_t*) bld;
+   const auto *b_ptr = (const real_t*) b;
 
-   mfem::forall_2D_batch(NE, Q1D, Q1D, NBZ, [=] MFEM_HOST_DEVICE(int e)
+   mfem::forall_2D(NE, Q1D, Q1D, [=] MFEM_HOST_DEVICE(int e)
    {
-      constexpr int NBZ = 1;
-      constexpr int MQ1 = T_Q1D ? T_Q1D : DofQuadLimits::MAX_TMOP_1D;
-      constexpr int MD1 = T_D1D ? T_D1D : DofQuadLimits::MAX_TMOP_1D;
+      constexpr int VDIM = 2;
+      constexpr int MQ1 = T_Q1D ? T_Q1D : DofQuadLimits::MAX_Q1D;
+      constexpr int MD1 = T_D1D ? T_D1D : DofQuadLimits::MAX_D1D;
 
-      MFEM_SHARED real_t B[MQ1 * MD1];
-      MFEM_SHARED real_t BLD[MQ1 * MD1];
+      MFEM_SHARED real_t smem[MQ1][MQ1];
+      MFEM_SHARED real_t sB[MD1][MQ1];
 
-      MFEM_SHARED real_t XY[NBZ][MD1 * MD1];
-      MFEM_SHARED real_t DQ[NBZ][MD1 * MQ1];
-      MFEM_SHARED real_t QQ[NBZ][MQ1 * MQ1];
+      regs::LoadMatrix(D1D, Q1D, bld_ptr, sB);
 
-      MFEM_SHARED real_t XY0[2][NBZ][MD1 * MD1];
-      MFEM_SHARED real_t DQ0[2][NBZ][MD1 * MQ1];
-      MFEM_SHARED real_t QQ0[2][NBZ][MQ1 * MQ1];
+      regs::regs4d_t<1,1,MQ1> rm0, rm1; // scalar LD
+      regs::LoadDofs2d(e, D1D, LD, rm0);
+      regs::Eval2d(D1D, Q1D, smem, sB, rm0, rm1);
 
-      MFEM_SHARED real_t XY1[2][NBZ][MD1 * MD1];
-      MFEM_SHARED real_t DQ1[2][NBZ][MD1 * MQ1];
-      MFEM_SHARED real_t QQ1[2][NBZ][MQ1 * MQ1];
+      regs::LoadMatrix(D1D, Q1D, b_ptr, sB);
 
-      kernels::internal::LoadX<MD1, NBZ>(e, D1D, LD, XY);
-      kernels::internal::LoadX<MD1, NBZ>(e, D1D, X0, XY0);
-      kernels::internal::LoadX<MD1, NBZ>(e, D1D, X1, XY1);
+      regs::regs4d_t<VDIM,1,MQ1> r00, r01; // vector X0
+      regs::LoadDofs2d(e, D1D, X0, r00);
+      regs::Eval2d(D1D, Q1D, smem, sB, r00, r01);
 
-      kernels::internal::LoadB<MD1, MQ1>(D1D, Q1D, b, B);
-      kernels::internal::LoadB<MD1, MQ1>(D1D, Q1D, bld, BLD);
+      regs::regs4d_t<VDIM,1,MQ1> r10, r11; // vector X1
+      regs::LoadDofs2d(e, D1D, X1, r10);
+      regs::Eval2d(D1D, Q1D, smem, sB, r10, r11);
 
-      kernels::internal::EvalX<MD1, MQ1, NBZ>(D1D, Q1D, BLD, XY, DQ);
-      kernels::internal::EvalY<MD1, MQ1, NBZ>(D1D, Q1D, BLD, DQ, QQ);
-
-      kernels::internal::EvalX<MD1, MQ1, NBZ>(D1D, Q1D, B, XY0, DQ0);
-      kernels::internal::EvalY<MD1, MQ1, NBZ>(D1D, Q1D, B, DQ0, QQ0);
-
-      kernels::internal::EvalX<MD1, MQ1, NBZ>(D1D, Q1D, B, XY1, DQ1);
-      kernels::internal::EvalY<MD1, MQ1, NBZ>(D1D, Q1D, B, DQ1, QQ1);
-
-      MFEM_FOREACH_THREAD(qy, y, Q1D)
+      mfem::foreach_y_thread(Q1D, [&](int qy)
       {
-         MFEM_FOREACH_THREAD(qx, x, Q1D)
+         mfem::foreach_x_thread(Q1D, [&](int qx)
          {
-            real_t ld, p0[2], p1[2];
             const real_t *Jtr = &J(0, 0, qx, qy, e);
             const real_t detJtr = kernels::Det<2>(Jtr);
             const real_t weight = W(qx, qy) * detJtr;
             const real_t coeff0 = const_c0 ? C0(0, 0, 0) : C0(qx, qy, e);
-            kernels::internal::PullEval<MQ1, NBZ>(Q1D, qx, qy, QQ, ld);
-            kernels::internal::PullEval<MQ1, NBZ>(Q1D, qx, qy, QQ0, p0);
-            kernels::internal::PullEval<MQ1, NBZ>(Q1D, qx, qy, QQ1, p1);
+
+            // real_t ld, p0[2], p1[2];
+            // kernels::internal::PullEval<MQ1, NBZ>(Q1D, qx, qy, QQ, ld);
+            // kernels::internal::PullEval<MQ1, NBZ>(Q1D, qx, qy, QQ0, p0);
+            // kernels::internal::PullEval<MQ1, NBZ>(Q1D, qx, qy, QQ1, p1);
+
+            const real_t ld = rm1(0, 0, qy, qx);
+            const real_t p0[2] = { r01(0, 0, qy, qx), r01(1, 0, qy, qx) };
+            const real_t p1[2] = { r11(0, 0, qy, qx), r11(1, 0, qy, qx) };
+
             const real_t dist = ld; // GetValues, default comp set to 0
             real_t id2 = 0.0;
             real_t dsq = 0.0;
@@ -103,11 +100,10 @@ void TMOP_EnergyPA_C0_2D(const real_t lim_normal,
             {
                id2 = 1.0 / (dist * dist);
                dsq = kernels::DistanceSquared<2>(p1, p0) * id2;
-               E(qx, qy, e) =
-                  weight * lim_normal * exp(10.0 * (dsq - 1.0)) * coeff0;
+               E(qx, qy, e) = weight * lim_normal * exp(10.0 * (dsq - 1.0)) * coeff0;
             }
-         }
-      }
+         });
+      });
    });
 }
 
