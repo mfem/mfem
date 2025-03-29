cfbaf5a6bf6fdbb6a3942f42b985ec1c5232129f
Gabriel Pinochet-Soto <gpin2@pdx.edu>
2025-03-29 11:02:27 -0700

    Header renaming

--- miniapps/diag-smoothers/ds-common.cpp
+++ miniapps/diag-smoothers/ds-common.cpp
@@ -9,7 +9,7 @@
 // terms of the BSD-3 license. We welcome feedback and contributions, see file
 // CONTRIBUTING.md for details.
 
-#include "lpq-common.hpp"
+#include "ds-common.hpp"
 
 using namespace std;
 using namespace mfem;
