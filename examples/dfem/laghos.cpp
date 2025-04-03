b56e994ecd457ad8dd1750a02b3adcecee01b7c3
camierjs <camierjs@gmail.com>
2025-04-02 17:20:29 -0700

    Copyright header, includes trim & warning fixes

--- examples/dfem/laghos.cpp
+++ examples/dfem/laghos.cpp
@@ -1,3 +1,13 @@
+// Copyright (c) 2010-2025, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
 #include <mfem.hpp>
 
 // TODO: Do we want this to be included from mfem.hpp automatically now?
@@ -84,7 +94,7 @@ mfem::tuple<matd, real_t> qdata_setup(
    real_t p, cs;
    real_t detJ = det(J);
    matd invJ = inv(J);
-   matd stress{0.0};
+   matd stress{{{0.0}}};
    const real_t rho = rho0 * det(J0) / detJ;
    const real_t Ez = fmax(0.0, E);
    real_t visc_coeff = 0.0;
--- fem/dfem/tuple.hpp
+++ fem/dfem/tuple.hpp
@@ -1,11 +1,19 @@
-// This is serac's tuple implementation
-
+// Copyright (c) 2010-2025, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
 #pragma once
 
-#include "general/backends.hpp"
+// This is serac's tuple implementation
+
 #include <utility>
 #include <mfem.hpp>
-#include <tuple>
 
 namespace mfem
 {
