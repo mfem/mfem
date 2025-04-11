75782958499ae39373e677d263c059e351c8e735
Will Pazner <will.e.p@gmail.com>
2025-04-11 06:59:57 -0700

    Handle empty prefix path in ParaViewHDFDataCollection

--- fem/datacollection.cpp
+++ fem/datacollection.cpp
@@ -1178,8 +1178,11 @@ void ParaViewHDFDataCollection::EnsureVTKHDF()
 {
    if (!vtkhdf)
    {
-      const int error_code = create_directory(prefix_path, mesh, myid);
-      MFEM_VERIFY(error_code == 0, "Error creating directory " << prefix_path);
+      if (!prefix_path.empty())
+      {
+         const int error_code = create_directory(prefix_path, mesh, myid);
+         MFEM_VERIFY(error_code == 0, "Error creating directory " << prefix_path);
+      }
 
       std::string fname = prefix_path + name + ".vtkhdf";
       bool use_mpi = false;
