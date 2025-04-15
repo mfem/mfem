3d0bf5a59fd7bec5f2e96a6d0dcf6d3308a0a57a
rzhangbq <rzhangbq@gmail.com>
2025-04-14 23:04:18 -0400

    update run_all.sh

--- examples/loading_from_EFIT/run_all.sh
+++ examples/loading_from_EFIT/run_all.sh
@@ -3,5 +3,5 @@ make all -j 8
 ./B_field_projector_B_tor_DG && ./J_field_projector_J_pol_Hcurl&
 ./B_field_projector_B_pol_Hdiv && ./J_field_projector_J_tor_Hdiv&
 ./B_field_projector_B_pol_Hcurl && ./J_field_projector_J_tor_Hcurl
-./JxB_validator_JxB_tor_Hcurl & ./JxB_validator_JxB_pol_Hcurl
+./JxB_validator_JxB_tor & ./JxB_validator_JxB_pol
 ./JxB_validator_grad_p
