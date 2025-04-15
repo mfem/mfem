e7e00e6e39fa56d820e0f2015b582c3ffc8eb7af
Toni-ko <anthonykolshorn@lclark.edu>
2025-04-14 17:04:40 -0700

    Got rid of shoelace formula for  element area and volume computations

--- fem/lor/lor_dg_impl.hpp
+++ fem/lor/lor_dg_impl.hpp
@@ -75,14 +75,15 @@ void BatchedLOR_DG::Assemble2D()
             const real_t A_ref = (d_vec_ir_pp2_x[ix+1] - d_vec_ir_pp2_x[ix])
                                  * (d_vec_ir_pp2_x[iy+1] - d_vec_ir_pp2_x[iy]);
             // Shoelace formula for area of a quadrilateral
-            const real_t A_el = fabs(0.5*(X(0, ix, iy, iel_ho)*X(1, ix+1, iy, iel_ho)
-                                          - X(0, ix+1, iy, iel_ho)*X(1, ix, iy, iel_ho)
-                                          + X(0, ix+1, iy, iel_ho)*X(1, ix+1, iy+1, iel_ho)
-                                          - X(0, ix+1, iy+1, iel_ho)*X(1, ix+1, iy, iel_ho)
-                                          + X(0, ix+1, iy+1, iel_ho)*X(1, ix, iy+1, iel_ho)
-                                          - X(0, ix, iy+1, iel_ho)*X(1, ix+1, iy+1, iel_ho)
-                                          + X(0, ix, iy+1, iel_ho)*X(1, ix, iy, iel_ho)
-                                          - X(0, ix, iy, iel_ho)*X(1, ix, iy+1, iel_ho)));
+            //const real_t A_el = fabs(0.5*(X(0, ix, iy, iel_ho)*X(1, ix+1, iy, iel_ho)
+            //                              - X(0, ix+1, iy, iel_ho)*X(1, ix, iy, iel_ho)
+            //                              + X(0, ix+1, iy, iel_ho)*X(1, ix+1, iy+1, iel_ho)
+            //                              - X(0, ix+1, iy+1, iel_ho)*X(1, ix+1, iy, iel_ho)
+            //                              + X(0, ix+1, iy+1, iel_ho)*X(1, ix, iy+1, iel_ho)
+            //                              - X(0, ix, iy+1, iel_ho)*X(1, ix+1, iy+1, iel_ho)
+            //                              + X(0, ix, iy+1, iel_ho)*X(1, ix, iy, iel_ho)
+            //                              - X(0, ix, iy, iel_ho)*X(1, ix, iy+1, iel_ho)));
+            const real_t A_el  = A_ref*detJ(ix, iy, iel_ho);
             const real_t mq = const_mq ? MQ(0,0,0) : MQ(ix, iy, iel_ho);
             const real_t dq = const_dq ? DQ(0,0,0) : DQ(ix, iy, iel_ho);
          
@@ -125,20 +126,23 @@ void BatchedLOR_DG::Assemble2D()
                   {
                      const int ix2 = (n_idx == 0) ? ix + (e_i == 0 ? -1 : 1) : ix;
                      const int iy2 = (n_idx == 1) ? iy + (e_i == 0 ? -1 : 1) : iy;
-                     const real_t A_el_2 =
-                        fabs(0.5*(X(0, ix2, iy2, iel_ho)*X(1, ix2+1, iy2, iel_ho)
-                                  - X(0, ix2+1, iy2, iel_ho)*X(1, ix2, iy2, iel_ho)
-                                  + X(0, ix2+1, iy2, iel_ho)*X(1, ix2+1, iy2+1, iel_ho)
-                                  - X(0, ix2+1, iy2+1, iel_ho)*X(1, ix2+1, iy2, iel_ho)
-                                  + X(0, ix2+1, iy2+1, iel_ho)*X(1, ix2, iy2+1, iel_ho)
-                                  - X(0, ix2, iy2+1, iel_ho)*X(1, ix2+1, iy2+1, iel_ho)
-                                  + X(0, ix2, iy2+1, iel_ho)*X(1, ix2, iy2, iel_ho)
-                                  - X(0, ix2, iy2, iel_ho)*X(1, ix2, iy2+1, iel_ho)));
-                     const real_t A_ref_1 = (n_idx == 0) ? d_vec_ir_pp2_x[i_0+1] - d_vec_ir_pp2_x[i_0] :
+                     //const real_t A_el_2 =
+                     //   fabs(0.5*(X(0, ix2, iy2, iel_ho)*X(1, ix2+1, iy2, iel_ho)
+                     //             - X(0, ix2+1, iy2, iel_ho)*X(1, ix2, iy2, iel_ho)
+                     //             + X(0, ix2+1, iy2, iel_ho)*X(1, ix2+1, iy2+1, iel_ho)
+                     //             - X(0, ix2+1, iy2+1, iel_ho)*X(1, ix2+1, iy2, iel_ho)
+                     //             + X(0, ix2+1, iy2+1, iel_ho)*X(1, ix2, iy2+1, iel_ho)
+                     //             - X(0, ix2, iy2+1, iel_ho)*X(1, ix2+1, iy2+1, iel_ho)
+                     //             + X(0, ix2, iy2+1, iel_ho)*X(1, ix2, iy2, iel_ho)
+                     //             - X(0, ix2, iy2, iel_ho)*X(1, ix2, iy2+1, iel_ho)));
+                     const real_t A_ref_2 = (d_vec_ir_pp2_x[ix2+1] - d_vec_ir_pp2_x[ix2])
+                                 * (d_vec_ir_pp2_x[iy2+1] - d_vec_ir_pp2_x[iy2]);
+                     const real_t A_el_2 = A_ref_2*detJ(ix2, iy2, iel_ho); 
+                     const real_t h_ref_1 = (n_idx == 0) ? d_vec_ir_pp2_x[i_0+1] - d_vec_ir_pp2_x[i_0] :
                                             d_vec_ir_pp2_x[j_0+1] - d_vec_ir_pp2_x[j_0];
-                     const real_t A_ref_2 = (n_idx == 0) ? d_vec_ir_pp2_x[i_0] - d_vec_ir_pp2_x[i_0-1] :
+                     const real_t h_ref_2 = (n_idx == 0) ? d_vec_ir_pp2_x[i_0] - d_vec_ir_pp2_x[i_0-1] :
                                             d_vec_ir_pp2_x[j_0] - d_vec_ir_pp2_x[j_0-1];
-                     const real_t h = (0.5*A_el + 0.5*A_el_2) / A_face / (0.5 * (A_ref_1 + A_ref_2)); 
+                     const real_t h = (0.5*A_el + 0.5*A_el_2) / A_face / (0.5 * (h_ref_1 + h_ref_2)); 
 
                      V(v_idx, ix, iy, iel_ho) = -dq * A_face * w_1d[w_idx] / h / el_1 /
                                                 (d_vec_ir_pp1_x[int_idx] - d_vec_ir_pp1_x[int_idx-1]);
@@ -196,47 +200,13 @@ void BatchedLOR_DG::Assemble3D()
                const real_t dq = const_dq ? DQ(0,0,0,0) : DQ(ix, iy, iz, iel_ho);  
 
                //compute A_el and A_ref
-               //For A_el, split element into two tetrahedra, then compute volumes of each of these and add
-               Vector T1_B1(3);
-               Vector T1_B2(3);
-               Vector T1_B3(3);
-               Vector T2_B1(3);
-               Vector T2_B2(3);
-               Vector T2_B3(3);
-
-               T1_B1[0] = X(0, ix, iy, iz, iel_ho) - X(0, ix+1, iy, iz+1, iel_ho); 
-               T1_B2[0] = X(0, ix+1, iy, iz, iel_ho) - X(0, ix+1, iy, iz+1, iel_ho); 
-               T1_B3[0] = X(0, ix, iy+1, iz, iel_ho) - X(0, ix+1, iy, iz+1, iel_ho); 
-               T1_B1[1] = X(1, ix, iy, iz, iel_ho) - X(1, ix+1, iy, iz+1, iel_ho); 
-               T1_B2[1] = X(1, ix+1, iy, iz, iel_ho) - X(1, ix+1, iy, iz+1, iel_ho); 
-               T1_B3[1] = X(1, ix, iy+1, iz, iel_ho) - X(1, ix+1, iy, iz+1, iel_ho); 
-               T1_B1[2] = X(2, ix, iy, iz, iel_ho) - X(2, ix+1, iy, iz+1, iel_ho); 
-               T1_B2[2] = X(2, ix+1, iy, iz, iel_ho) - X(2, ix+1, iy, iz+1, iel_ho); 
-               T1_B3[2] = X(2, ix, iy+1, iz, iel_ho) - X(2, ix+1, iy, iz+1, iel_ho); 
-
-               T2_B1[0] = X(0, ix, iy, iz+1, iel_ho) - X(0, ix+1, iy, iz, iel_ho); 
-               T2_B2[0] = X(0, ix+1, iy+1, iz+1, iel_ho) - X(0, ix, iy, iz, iel_ho); 
-               T2_B3[0] = X(0, ix, iy+1, iz+1, iel_ho) - X(0, ix, iy, iz, iel_ho); 
-               T2_B1[1] = X(1, ix, iy, iz+1, iel_ho) - X(1, ix+1, iy, iz, iel_ho); 
-               T2_B2[1] = X(1, ix+1, iy+1, iz+1, iel_ho) - X(1, ix, iy, iz, iel_ho); 
-               T2_B3[1] = X(1, ix, iy+1, iz+1, iel_ho) - X(1, ix, iy, iz, iel_ho); 
-               T2_B1[2] = X(2, ix, iy, iz+1, iel_ho) - X(2, ix+1, iy, iz, iel_ho); 
-               T2_B2[2] = X(2, ix+1, iy+1, iz+1, iel_ho) - X(2, ix, iy, iz, iel_ho); 
-               T2_B3[2] = X(2, ix, iy+1, iz+1, iel_ho) - X(2, ix, iy, iz, iel_ho);
-               
-               Vector cross1(3);
-               Vector cross2(3);
-
-               T1_B2.cross3D(T1_B3, cross1);
-               T2_B2.cross3D(T2_B3, cross2);
-
-               real_t V1 = fabs(T1_B1*cross1);
-               real_t V2 = fabs(T2_B1*cross2);
-               real_t A_el = (V1+V2)/2;
 
                const real_t A_ref = (ir_pp2[ix+1].x - ir_pp2[ix].x)
                                  * (ir_pp2[iy+1].x - ir_pp2[iy].x)
                                  * (ir_pp2[iz+1].x - ir_pp2[iz].x);
+
+               const real_t A_el = detJ(ix, iy, iz, iel_ho)*A_ref; 
+
                for (int n_idx = 0; n_idx < 3; ++n_idx){
                   for (int e_i = 0; e_i < 2; ++e_i){
                      const int v_idx_lex = e_i + n_idx*2;
@@ -274,44 +244,25 @@ void BatchedLOR_DG::Assemble3D()
                      Vector v2(3);
                      Vector v3(3);
 
-                     v0[0] = X(0, i_0, j_0, k_0, iel_ho); 
-                     v0[1] = X(1, i_0, j_0, k_0, iel_ho); 
-                     v0[2] = X(2, i_0, j_0, k_0, iel_ho); 
-                     v1[0] = X(0, i_1, j_1, k_1, iel_ho); 
-                     v1[1] = X(1, i_1, j_1, k_1, iel_ho); 
-                     v1[2] = X(2, i_1, j_1, k_1, iel_ho); 
-                     v2[0] = X(0, i_2, j_2, k_2, iel_ho); 
-                     v2[1] = X(1, i_2, j_2, k_2, iel_ho); 
-                     v2[2] = X(2, i_2, j_2, k_2, iel_ho); 
-                     v3[0] = X(0, i_3, j_3, k_3, iel_ho); 
-                     v3[1] = X(1, i_3, j_3, k_3, iel_ho); 
-                     v3[2] = X(2, i_3, j_3, k_3, iel_ho); 
+                     const real_t v0x = X(0, i_0, j_0, k_0, iel_ho); 
+                     const real_t v0y = X(1, i_0, j_0, k_0, iel_ho); 
+                     const real_t v0z = X(2, i_0, j_0, k_0, iel_ho); 
+                     const real_t v1x = X(0, i_1, j_1, k_1, iel_ho); 
+                     const real_t v1y = X(1, i_1, j_1, k_1, iel_ho); 
+                     const real_t v1z = X(2, i_1, j_1, k_1, iel_ho); 
+                     const real_t v2x = X(0, i_2, j_2, k_2, iel_ho); 
+                     const real_t v2y = X(1, i_2, j_2, k_2, iel_ho); 
+                     const real_t v2z = X(2, i_2, j_2, k_2, iel_ho); 
+                     const real_t v3x = X(0, i_3, j_3, k_3, iel_ho); 
+                     const real_t v3y = X(1, i_3, j_3, k_3, iel_ho); 
+                     const real_t v3z = X(2, i_3, j_3, k_3, iel_ho); 
+
+                     const real_t cross_sum_1 = (v0y*v1z - v0z*v1y) + (v1y*v2z - v1z*v2y) + (v2y*v3z - v2z*v3y) + (v3y*v0z - v3z*v0y);
+                     const real_t cross_sum_2 = (v0z*v1x - v0x*v1z) + (v1z*v2x - v1x*v2z) + (v2z*v3x - v2x*v3z) + (v3z*v0x - v3x*v0z);
+                     const real_t cross_sum_3 = (v0x*v1y - v0y*v1x) + (v1x*v2y - v1y*v2x) + (v2x*v3y - v2y*v3x) + (v3x*v0y - v3y*v0x);
+
                      
-                     Vector cross_v0_v1(3);
-                     Vector cross_v1_v2(3);
-                     Vector cross_v2_v3(3);
-                     Vector cross_v3_v0(3);
-
-                     v0.cross3D(v1, cross_v0_v1);
-                     v1.cross3D(v2, cross_v1_v2);
-                     v2.cross3D(v3, cross_v2_v3);
-                     v3.cross3D(v0, cross_v3_v0);  
-
-                     Vector sum_vs(3); sum_vs[0] = 0; sum_vs[1] = 0; sum_vs[2] = 0;
-
-                     sum_vs.Add(1, cross_v0_v1);
-                     sum_vs.Add(1, cross_v1_v2);
-                     sum_vs.Add(1, cross_v2_v3);
-                     sum_vs.Add(1, cross_v3_v0);
-                     
-                     const real_t A_face = 0.5*(sum_vs.Norml2());
-                     //std::cout << "A_face =  " << A_face << std::endl;
-                     
-
-                     //const real_t A_face_1 = 0.5*fabs((y0*z1 + y1*z2 + y2*z3 + y3*z0 - z0*y1 - z1*y2 - z2*y3 - z3*y0));
-                     //const real_t A_face_2 = 0.5*fabs((x0*z1 + x1*z2 + x2*z3 + x3*z0 - z0*x1 - z1*x2 - z2*x3 - z3*x0));
-                     //const real_t A_face_3 = 0.5*fabs((y0*x1 + y1*x2 + y2*x3 + y3*x0 - x0*y1 - x1*y2 - x2*y3 - x3*y0));
-                     //const real_t A_face  = (n_idx == 0) ? A_face_1 : ((n_idx == 1) ? A_face_2 : A_face_3);
+                     const real_t A_face = 0.5*sqrt(cross_sum_1*cross_sum_1 + cross_sum_2*cross_sum_2 + cross_sum_3*cross_sum_3);
 
                      bool bdr = true;
                      if (n_idx == 0){bdr = (i_0 == 0 || i_0 == pp1); w_idx_1 = iy; w_idx_2 = iz; int_idx = i_0;}
@@ -332,48 +283,16 @@ void BatchedLOR_DG::Assemble3D()
 
                         //compute A_el_2
                         //For A_el_2, split element into two tetrahedra, then compute volumes of each of these and add
-                        Vector adj_T1_B1(3);
-                        Vector adj_T1_B2(3);
-                        Vector adj_T1_B3(3);
-                        Vector adj_T2_B1(3);
-                        Vector adj_T2_B2(3);
-                        Vector adj_T2_B3(3);
-
-                        adj_T1_B1[0] = X(0, ix2, iy2, iz2, iel_ho) - X(0, ix2+1, iy2, iz2+1, iel_ho); 
-                        adj_T1_B2[0] = X(0, ix2+1, iy2, iz2, iel_ho) - X(0, ix2+1, iy2, iz2+1, iel_ho); 
-                        adj_T1_B3[0] = X(0, ix2, iy2+1, iz2, iel_ho) - X(0, ix2+1, iy2, iz2+1, iel_ho); 
-                        adj_T1_B1[1] = X(1, ix2, iy2, iz2, iel_ho) - X(1, ix2+1, iy2, iz2+1, iel_ho); 
-                        adj_T1_B2[1] = X(1, ix2+1, iy2, iz2, iel_ho) - X(1, ix2+1, iy2, iz2+1, iel_ho); 
-                        adj_T1_B3[1] = X(1, ix2, iy2+1, iz2, iel_ho) - X(1, ix2+1, iy2, iz2+1, iel_ho); 
-                        adj_T1_B1[2] = X(2, ix2, iy2, iz2, iel_ho) - X(2, ix2+1, iy2, iz2+1, iel_ho); 
-                        adj_T1_B2[2] = X(2, ix2+1, iy2, iz2, iel_ho) - X(2, ix2+1, iy2, iz2+1, iel_ho); 
-                        adj_T1_B3[2] = X(2, ix2, iy2+1, iz2, iel_ho) - X(2, ix2+1, iy2, iz2+1, iel_ho); 
-
-                        adj_T2_B1[0] = X(0, ix2, iy2, iz2+1, iel_ho) - X(0, ix2+1, iy2, iz2, iel_ho); 
-                        adj_T2_B2[0] = X(0, ix2+1, iy2+1, iz2+1, iel_ho) - X(0, ix2, iy2, iz2, iel_ho); 
-                        adj_T2_B3[0] = X(0, ix2, iy2+1, iz2+1, iel_ho) - X(0, ix2, iy2, iz2, iel_ho); 
-                        adj_T2_B1[1] = X(1, ix2, iy2, iz2+1, iel_ho) - X(1, ix2+1, iy2, iz2, iel_ho); 
-                        adj_T2_B2[1] = X(1, ix2+1, iy2+1, iz2+1, iel_ho) - X(1, ix2, iy2, iz2, iel_ho); 
-                        adj_T2_B3[1] = X(1, ix2, iy2+1, iz2+1, iel_ho) - X(1, ix2, iy2, iz2, iel_ho); 
-                        adj_T2_B1[2] = X(2, ix2, iy2, iz2+1, iel_ho) - X(2, ix2+1, iy2, iz2, iel_ho); 
-                        adj_T2_B2[2] = X(2, ix2+1, iy2+1, iz2+1, iel_ho) - X(2, ix2, iy2, iz2, iel_ho); 
-                        adj_T2_B3[2] = X(2, ix2, iy2+1, iz2+1, iel_ho) - X(2, ix2, iy2, iz2, iel_ho);
-                        
-                        Vector adj_cross1(3);
-                        Vector adj_cross2(3);
-
-                        adj_T1_B2.cross3D(adj_T1_B3, adj_cross1);
-                        adj_T2_B2.cross3D(adj_T2_B3, adj_cross2);
-
-                        real_t adj_V1 = fabs(adj_T1_B1*adj_cross1);
-                        real_t adj_V2 = fabs(adj_T2_B1*adj_cross2);
-                        const real_t A_el_2 = (adj_V1+adj_V2)/2;
-
-                        const real_t A_el_avg = 0.5*(A_el + A_el_2);
-
+                     
                         const real_t adj_A_ref = (ir_pp2[ix2+1].x - ir_pp2[ix2].x)
                                  * (ir_pp2[iy2+1].x - ir_pp2[iy2].x)
                                  * (ir_pp2[iz2+1].x - ir_pp2[iz2].x);
+                        
+                        const real_t A_el_2 = adj_A_ref*detJ(ix2, iy2, iz2, iel_ho);
+
+                        const real_t A_el_avg = 0.5*(A_el + A_el_2);
+
+                  
                         //const real_t A_ref_avg = 0.5*(A_ref + adj_A_ref);
                         
                         const real_t adj_A_ref_perp = adj_A_ref/A_ref_face;
