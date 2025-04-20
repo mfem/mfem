c21a7bb8b5fd9e91e81713264b14349f5cd2208c
Andrew Ho <ho37@llnl.gov>
2025-04-19 20:18:52 -0700

    Re-arranged DerefineMatrixOpFunctor
    Now it's a common implementation with the indexing order separated.
    
    Still need to make it work for off-diagonals.

--- fem/fes_kernels.hpp
+++ fem/fes_kernels.hpp
@@ -20,7 +20,66 @@ namespace mfem
 namespace internal
 {
 
-struct DerefineMatrixOpFunctorBase
+template <Ordering::Type Order, class Base, bool Diag = true>
+struct DerefineMatrixOpFunctorBase;
+
+template <class Base>
+struct DerefineMatrixOpFunctorBase<Ordering::byNODES, Base, true>
+{
+   /// block col idcs offsets
+   const int *bcptr;
+   /// col idcs
+   const int *cptr;
+
+   int MFEM_HOST_DEVICE BlockWidth(int k) const
+   {
+      return bcptr[k + 1] - bcptr[k];
+   }
+
+   int MFEM_HOST_DEVICE Col(int j, int k) const { return cptr[bcptr[k] + j]; }
+
+   int MFEM_HOST_DEVICE IndexX(int col, int vdim) const
+   {
+      return col + vdim * static_cast<const Base *>(this)->width;
+   }
+   int MFEM_HOST_DEVICE IndexY(int row, int vdim) const
+   {
+      return row + vdim * static_cast<const Base *>(this)->height;
+   }
+};
+
+template <class Base>
+struct DerefineMatrixOpFunctorBase<Ordering::byVDIM, Base, true>
+{
+   /// block col idcs offsets
+   const int *bcptr;
+   /// col idcs
+   const int *cptr;
+
+   int MFEM_HOST_DEVICE BlockWidth(int k) const
+   {
+      return bcptr[k + 1] - bcptr[k];
+   }
+
+   int MFEM_HOST_DEVICE Col(int j, int k) const { return cptr[bcptr[k] + j]; }
+
+   int MFEM_HOST_DEVICE IndexX(int col, int vdim) const
+   {
+      return vdim + col * static_cast<const Base *>(this)->vdims;
+   }
+   int MFEM_HOST_DEVICE IndexY(int row, int vdim) const
+   {
+      return vdim + row * static_cast<const Base *>(this)->vdims;
+   }
+};
+
+/// internally used to implementing the derefinement operator Mult diagonal
+/// block
+template <Ordering::Type Order, bool Atomic, bool Diag = true>
+struct DerefineMatrixOpMultFunctor
+   : public DerefineMatrixOpFunctorBase<
+     Ordering::byNODES, DerefineMatrixOpMultFunctor<Order, Atomic, Diag>,
+     Diag>
 {
    const real_t *xptr;
    real_t *yptr;
@@ -30,12 +89,8 @@ struct DerefineMatrixOpFunctorBase
    const int *boptr;
    /// block row idcs offsets
    const int *brptr;
-   /// block col idcs offsets
-   const int *bcptr;
    /// row idcs
    const int *rptr;
-   /// col idcs
-   const int *cptr;
 
    // number of blocks
    int nblocks;
@@ -45,24 +100,13 @@ struct DerefineMatrixOpFunctorBase
    int height;
    /// overall operator width (for vdim = 1)
    int width;
-};
-
-/// internally used to implementing the derefinement operator Mult diagonal
-/// block
-template <Ordering::Type Order, bool Atomic>
-struct DerefineMatrixOpMultFunctor;
-
-template <bool Atomic>
-struct DerefineMatrixOpMultFunctor<Ordering::byNODES, Atomic>
-   : DerefineMatrixOpFunctorBase
-{
    void MFEM_HOST_DEVICE operator()(int kidx) const
    {
       int k = kidx % nblocks;
       int vdim = kidx / nblocks;
 
       int block_height = brptr[k + 1] - brptr[k];
-      int block_width = bcptr[k + 1] - bcptr[k];
+      int block_width = this->BlockWidth(k);
       MFEM_FOREACH_THREAD(i, x, block_height)
       {
          int row = rptr[brptr[k] + i];
@@ -78,7 +122,7 @@ struct DerefineMatrixOpMultFunctor<Ordering::byNODES, Atomic>
             real_t sum = 0;
             for (int j = 0; j < block_width; ++j)
             {
-               int col = cptr[bcptr[k] + j];
+               int col = this->Col(j, k);
                int sign = rsign;
                if (col < 0)
                {
@@ -86,74 +130,17 @@ struct DerefineMatrixOpMultFunctor<Ordering::byNODES, Atomic>
                   sign *= -1;
                }
                sum += sign * bsptr[boptr[k] + i + j * block_height] *
-                      xptr[col + vdim * width];
+                      xptr[this->IndexX(col, vdim)];
             }
 #if defined(__CUDA_ARCH__) or defined(__HIP_DEVICE_COMPILE__)
             if (Atomic)
             {
-               atomicAdd(yptr + row + vdim * height, sum);
+               atomicAdd(yptr + this->IndexY(row, vdim), sum);
             }
             else
 #endif
             {
-               yptr[row + vdim * height] += sum;
-            }
-         }
-      }
-   }
-
-   /// N is the max block row size (doesn't have to be a power of 2)
-   void Run(int N) const
-   {
-      forall_2D(nblocks * vdims, N, 1, *this);
-   }
-};
-
-template <bool Atomic>
-struct DerefineMatrixOpMultFunctor<Ordering::byVDIM, Atomic>
-   : DerefineMatrixOpFunctorBase
-{
-   void MFEM_HOST_DEVICE operator()(int kidx) const
-   {
-      int k = kidx % nblocks;
-      int vdim = kidx / nblocks;
-
-      int block_height = brptr[k + 1] - brptr[k];
-      int block_width = bcptr[k + 1] - bcptr[k];
-      MFEM_FOREACH_THREAD(i, x, block_height)
-      {
-         int row = rptr[brptr[k] + i];
-         int rsign = 1;
-         if (row < 0)
-         {
-            row = -1 - row;
-            rsign = -1;
-         }
-         if (row < height)
-         {
-            // row not marked as unused
-            real_t sum = 0;
-            for (int j = 0; j < block_width; ++j)
-            {
-               int col = cptr[bcptr[k] + j];
-               int sign = rsign;
-               if (col < 0)
-               {
-                  col = -1 - col;
-                  sign *= -1;
-               }
-               sum += sign * bsptr[boptr[k] + i + j * block_height] *
-                      xptr[vdim + col * vdims];
-            }
-#if defined(__CUDA_ARCH__) or defined(__HIP_DEVICE_COMPILE__)
-            if (Atomic)
-            {
-               atomicAdd(yptr + row + vdim * height, sum);
-            }
-            else
-#endif
-            {
-               yptr[vdim + row * vdims] += sum;
+               yptr[this->IndexY(row, vdim)] += sum;
             }
          }
       }
