904fea97e96e9666232ad8f74c164cdfe4243481
Justin Laughlin <justinglaughlin@gmail.com>
2025-04-28 15:42:27 -0700

    Add new algorithm to compute edge_to_knot map

--- miniapps/nurbs/demo.cpp
+++ miniapps/nurbs/demo.cpp
@@ -32,86 +32,9 @@ int main(int argc, char *argv[])
    Array<NURBSPatch*> patches = mesh.GetNURBSPatches();
 
 
-
-   // cout << endl << "Mesh" << endl;
-   // mesh.Print(mfem::out);
-   // cout << endl << "end Mesh" << endl;
-
-
-   // Print out the vertex indices for each edge of the mesh
-   // cout << endl << "Mesh Edges and their Vertex Indices:" << endl;
-   // for (int i = 0; i < mesh.GetNEdges(); i++) {
-   //    Array<int> v(2); // Array to store the two vertex indices of the edge
-   //    mesh.GetEdgeVertices(i, v); // Get the vertices of edge i
-   //    cout << "Edge " << i << ": Vertex " << v[0] << " - Vertex " << v[1] << endl;
-   // }
-
-
    // Print out edge_to_knot map
    cout << endl << "Original Mesh: edge to knot map:" << endl;
    mesh.NURBSext->PrintEdgeToKnot();
-   // mesh.NURBSext->Che
-   // Array<int> e2k({0, -1, 1, 1});
-
-
-   // Generate edge_to_knot map
-   // cout << endl << "Original Mesh: generated edge to knot map:" << endl;
-   // Array<int> edge_to_knot;
-   // mesh.GeneratePatchTopologyMap(edge_to_knot);
-   // edge_to_knot.Print();
-
-
-   // cout << endl << "PatchTopo" << endl;
-   // patch_topology.Print(mfem::out);
-   // cout << endl << "end PatchTopo" << endl;
-
-   // Patch topology
-   cout << endl << "PatchTopo: Mesh Edges and their Vertex Indices:" << endl;
-   int p = 0; // patch index
-   int kvidx;
-   int gidx=0; // global kv index
-   int ne=patch_topology.GetNEdges();
-   Array<int> kvs(3);
-   Array<int> edges, oedges;
-   Array<int> v(2); // Array to store the two vertex indices of the edge
-   Array<int> e2k(ne);
-   for (int p = 0; p < mesh.NURBSext->GetNP(); p++)
-   {
-      cout << "Patch " << p << ":" << endl;
-      // kvs
-      for (int ikv = 0; ikv < patches[p]->GetNKV(); ++ikv)
-      {
-         kvs[ikv] = gidx;
-         gidx++;
-      }
-      patch_topology.GetElementEdges(p, edges, oedges);
-      // patches[p]->
-      for (int i = 0; i < edges.Size(); i++) {
-         kvidx = (i<8) ? ((i & 1) ? kvs[1] : kvs[0]) : kvs[2];
-         patch_topology.GetEdgeVertices(edges[i], v); // Get the vertices of edge i
-         kvidx = (v[1] > v[0]) ? kvidx : -1 - kvidx;
-
-         e2k[edges[i]] = kvidx;
-
-         // reverse mapping (as used by checkpatches)
-         int kvidx_ = (oedges[i] > 0) ? kvidx : -1 - kvidx;
-
-         cout << "  - i=" << i << ", edge=" << edges[i]
-              << ", v1=" << v[0] << ", v2=" << v[1]
-              << ", oedge=" << oedges[i]
-              << ", kvidx=" << kvidx
-              << ", kvidx_=" << kvidx_
-              << endl;
-      }
-   }
-
-   cout << endl << "Original Mesh: generated edge to knot map:" << endl;
-   e2k.Print();
-
-   Array<int> edge_to_knot;
-   patch_topology.GetEdgeToKnotMapping(edge_to_knot);
-   cout << endl << "GetEdgeToKnotMapping():" << endl;
-   edge_to_knot.Print();
 
 
    // Reconstruct mesh using patch topology and patches
