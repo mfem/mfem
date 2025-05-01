91f648aa950e90fc9b0ee4e37d2ad4ecefa9334f
camierjs <camierjs@gmail.com>
2025-05-01 10:36:43 -0700

    Remove examples leftovers

--- fem/dfem/doperator.hpp
+++ fem/dfem/doperator.hpp
@@ -23,6 +23,10 @@
 #include "qf_derivative_dual.hpp"
 #include "integrate.hpp"
 
+#undef NVTX_COLOR
+#define NVTX_COLOR nvtx::kPurple
+#include "general/nvtx.hpp"
+
 namespace mfem::future
 {
 
@@ -232,10 +236,13 @@ public:
    /// solutions_t. The result is a T-dof vector.
    void Mult(const Vector &solutions_t, Vector &result_t) const override
    {
+      // dbg();
       MFEM_ASSERT(!action_callbacks.empty(), "no integrators have been set");
+      // dbg("prolongation");
       prolongation(solutions, solutions_t, solutions_l);
       for (auto &action : action_callbacks)
       {
+         // dbg("action");
          action(solutions_l, parameters_l, residual_l);
       }
       prolongation_transpose(residual_l, result_t);
@@ -478,6 +485,7 @@ void DifferentiableOperator::AddDomainIntegrator(
    auto &output_e_size = output_e_sz;
 
    output_restriction_transpose = output_rt;
+   residual_e.UseDevice(true);
    residual_e.SetSize(output_e_size);
 
    // The explicit captures are necessary to avoid dependency on
@@ -502,6 +510,13 @@ void DifferentiableOperator::AddDomainIntegrator(
    [[maybe_unused]] const int num_elements = GetNumEntities<Entity::Element>(mesh);
    const int num_entities = GetNumEntities<entity_t>(mesh);
    const int num_qp = integration_rule.GetNPoints();
+   dbg("num_qp:{}", num_qp);
+   dbg("num_entities:{}", num_entities);
+   dbg("dimension:{}", dimension);
+   dbg("num_fields:{}", num_fields);
+   dbg("num_inputs:{}", num_inputs);
+   dbg("num_outputs:{}", num_outputs);
+   dbg("num_elements:{}", num_entities);
 
    if constexpr (is_one_fop<decltype(output_fop)>::value)
    {
@@ -514,9 +529,12 @@ void DifferentiableOperator::AddDomainIntegrator(
       residual_l.SetSize(residual_lsize);
       height = GetTrueVSize(fields[test_space_field_idx]);
    }
+   dbg("residual_lsize:{}", residual_l.Size());
+   dbg("height:{}", height);
 
    // TODO: Is this a hack?
    width = GetTrueVSize(fields[0]);
+   dbg("width:{}", width);
 
    std::vector<const DofToQuad*> dtq;
    for (const auto &field : fields)
@@ -527,10 +545,12 @@ void DifferentiableOperator::AddDomainIntegrator(
                           doftoquad_mode));
    }
    const int q1d = (int)floor(std::pow(num_qp, 1.0/dimension) + 0.5);
+   dbg("q1d:{}", q1d);
 
    const int residual_size_on_qp =
       GetSizeOnQP<entity_t>(output_fop,
                             fields[test_space_field_idx]);
+   dbg("residual_size_on_qp:{}", residual_size_on_qp);
 
    auto input_dtq_maps = create_dtq_maps<entity_t>(inputs, dtq, input_to_field);
    auto output_dtq_maps = create_dtq_maps<entity_t>(outputs, dtq, output_to_field);
@@ -580,6 +600,8 @@ void DifferentiableOperator::AddDomainIntegrator(
    {
       restriction_cb(sol, par, fields_e);
 
+      // MFEM_GPU_CHECK(hipGetLastError());
+      // dbg("residual_e = 0.0");
       residual_e = 0.0;
       auto ye = Reshape(residual_e.ReadWrite(), test_vdim, num_test_dof, num_entities);
 
@@ -610,6 +632,13 @@ void DifferentiableOperator::AddDomainIntegrator(
 
          auto fhat = Reshape(&residual_shmem(0, 0), test_vdim, test_op_dim, num_qp);
          auto y = Reshape(&ye(0, 0, e), num_test_dof, test_vdim);
+
+         const DofToQuadMap &dtq_0 = output_dtq_shmem[0];
+         // dbg("dtq_0.B.GetShape()[0]:{}", dtq_0.B.GetShape()[0]);
+         // dbg("dtq_0.B.GetShape()[1]:{}", dtq_0.B.GetShape()[1]);
+         // dbg("dtq_0.B.GetShape()[2]:{}", dtq_0.B.GetShape()[2]);
+         assert(dtq_0.B.GetShape()[0] == q1d);
+
          map_quadrature_data_to_fields(
             y, fhat, output_fop, output_dtq_shmem[0],
             scratch_shmem, dimension, use_sum_factorization);
