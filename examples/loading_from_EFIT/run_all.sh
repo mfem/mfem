bfd34a21c67be265a3b213bc896e28022adcd3a1
rzhangbq <rzhangbq@gmail.com>
2025-05-15 16:25:37 -0400

    update script

--- examples/loading_from_EFIT/run_all.sh
+++ examples/loading_from_EFIT/run_all.sh
@@ -1,4 +1,6 @@
 make all -j 8
+./J_field_projector_J_tor_direct&
+./J_field_projector_J_pol_direct&
 ./B_field_projector_B_tor_CG && ./J_field_projector_J_pol_Hdiv&
 ./B_field_projector_B_tor_DG && ./J_field_projector_J_pol_Hcurl&
 ./B_field_projector_B_pol_Hdiv && ./J_field_projector_J_tor_Hdiv&
