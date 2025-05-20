c6728595225138f1dbc164d7b169ae9779e7b019
rzhangbq <rzhangbq@gmail.com>
2025-05-20 16:10:58 -0400

    update script and vis

--- examples/loading_from_EFIT/run_all.sh
+++ examples/loading_from_EFIT/run_all.sh
@@ -6,5 +6,6 @@ make all -j 8
 ./B_field_projector_B_pol_Hdiv && ./J_field_projector_J_tor_Hdiv&
 ./B_field_projector_B_pol_Hcurl && ./J_field_projector_J_tor_Hcurl
 ./JxB_validator_JxB_tor_A & ./JxB_validator_JxB_pol_A &
-./JxB_validator_JxB_tor_B & ./JxB_validator_JxB_pol_B
+./JxB_validator_JxB_tor_B & ./JxB_validator_JxB_pol_B &
+./B_field_projector_div_B_pol_Hdiv & B_field_projector_div_B_pol_Hcurl
 ./JxB_validator_grad_p
