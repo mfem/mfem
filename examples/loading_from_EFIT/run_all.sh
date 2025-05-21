d683ea3cc3e17d7d8105028cb03672b4776a3e85
rzhangbq <rzhangbq@gmail.com>
2025-05-21 15:43:40 -0400

    update run_all script

--- examples/loading_from_EFIT/run_all.sh
+++ examples/loading_from_EFIT/run_all.sh
@@ -1,11 +1,19 @@
 make all -j 8
-./J_field_projector_J_tor_direct&
-./J_field_projector_J_pol_direct&
-./B_field_projector_B_tor_CG && ./J_field_projector_J_pol_Hdiv&
-./B_field_projector_B_tor_DG && ./J_field_projector_J_pol_Hcurl&
-./B_field_projector_B_pol_Hdiv && ./J_field_projector_J_tor_Hdiv&
-./B_field_projector_B_pol_Hcurl && ./J_field_projector_J_tor_Hcurl
-./JxB_validator_JxB_tor_A & ./JxB_validator_JxB_pol_A &
-./JxB_validator_JxB_tor_B & ./JxB_validator_JxB_pol_B &
-./B_field_projector_div_B_pol_Hdiv & B_field_projector_div_B_pol_Hcurl
+# first group
+./J_field_projector_J_tor_direct &
+./J_field_projector_J_pol_direct &
+./B_field_projector_B_pol_vec_CG && ./J_field_projector_J_tor_vec_CG &
+./B_field_projector_B_pol_Hcurl && ./J_field_projector_J_tor_Hcurl &
+./B_field_projector_B_pol_Hdiv && ./J_field_projector_J_tor_Hdiv &
+./B_field_projector_B_tor_DG && ./J_field_projector_J_pol_Hcurl &
+./B_field_projector_B_tor_CG && ./J_field_projector_J_pol_Hdiv && ./J_field_projector_J_pol_vec_CG
+# second group
+./JxB_validator_JxB_tor_A &
+./JxB_validator_JxB_pol_A &
+./JxB_validator_JxB_tor_B &
+./JxB_validator_JxB_pol_B &
+./B_field_projector_div_B_pol_vec_CG &
+./B_field_projector_div_B_pol_Hcurl &
+./B_field_projector_div_B_pol_Hdiv
+# third group
 ./JxB_validator_grad_p
