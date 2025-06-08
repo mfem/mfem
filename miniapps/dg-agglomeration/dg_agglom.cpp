9bd1fba97b2d5c70207c8f4aa7298743c61b460a
Will Pazner <will.e.p@gmail.com>
2025-06-08 16:36:55 -0700

    DG agglomeration clean up

--- miniapps/dg-agglomeration/dg_agglom.cpp
+++ miniapps/dg-agglomeration/dg_agglom.cpp
@@ -12,129 +12,187 @@
 //                     -----------------------
 //                     DG Agglomeration Solver
 //                     -----------------------
-//
 
 #include "mfem.hpp"
 #include <iostream>
 #include <memory>
 
-#include "partition.hpp"
+#include "mg_agglom.hpp"
 
 using namespace std;
 using namespace mfem;
 
+// int main(int argc, char *argv[])
+// {
+//    //Mpi::Init(argc, argv);
+//    //Hypre::Init();
+
+//    const char *mesh_file = "../../data/star.mesh";
+//    int ncoarse = 3;
+
+//    OptionsParser args(argc, argv);
+//    args.AddOption(&mesh_file, "-m", "--mesh", "Mesh file to use.");
+//    args.AddOption(&ncoarse, "-n", "--n-coarse", "Number of coarse elements.");
+//    args.ParseCheck();
+
+//    Mesh mesh(mesh_file);
+//    //const Mesh mesh_copy(mesh);
+//    mesh.UniformRefinement();
+//    //mesh.UniformRefinement();
+//    DG_FECollection fec(0, mesh.Dimension());
+//    FiniteElementSpace fes(&mesh, &fec);
+//    GridFunction p(&fes);
+//    const int ne = mesh.GetNE();
+//    const int num_partitions = std::ceil(std::log(ne)/std::log(ncoarse));
+
+//    ParaViewDataCollection pv("Agglomeration", &mesh);
+//    pv.SetPrefixPath("ParaView");
+//    pv.RegisterField("p", &p);
+
+//    std::vector<std::vector<int>> E(num_partitions);
+
+//    p = 0;
+//    pv.SetCycle(0);
+//    pv.SetTime(0.0);
+//    pv.Save();
+
+//    Array<int> partitioning = PartitionMesh(mesh, ncoarse);
+//    for (int i = 0; i < p.Size(); ++i)
+//    {
+//       p[i] = partitioning[i];
+//    }
+//    for (int i = 0; i < ncoarse; ++i)
+//    {
+//       E[0].push_back(0);
+//    }
+//    std::vector<std::vector<int>> macro_elements(ncoarse);
+//    for (int k = 0; k < ne; ++k)
+//    {
+//       const int i = partitioning[k];
+//       macro_elements[i].push_back(k);
+//    }
+//    pv.SetCycle(1);
+//    pv.SetTime(1.0);
+//    pv.Save();
+//    for (int j = 1; j < num_partitions; ++j)
+//    {
+//       std::vector<std::vector<int>> macro_elements(E[j-1].size());
+//       for (int i = 0; i < p.Size(); ++i)
+//       {
+//          const int k = p[i];
+//          macro_elements[k].push_back(i);
+//       }
+//       int num_total_parts = 0;
+//       for (int e = 0; e < E[j-1].size(); ++e)
+//       {
+//          const int num_el_part = macro_elements[e].size();
+//          Array<int> subset(num_el_part);
+//          for (int i=0; i<num_el_part; i++) {subset[i] = macro_elements[e][i];}
+//          Array<int> partitioning = PartitionMesh(mesh, ncoarse, subset);
+//          int num_actual_parts = 0;
+//          for (int ip = 0; ip < partitioning.Size(); ++ip)
+//          {
+//             const int i = partitioning[ip];
+//             num_actual_parts = (i > num_actual_parts) ? i : num_actual_parts;
+//             p[subset[ip]] = i + num_total_parts;
+//          }
+//          for (int k = 0; k <= num_actual_parts; ++k) {E[j].push_back(e);}
+//          num_total_parts = num_total_parts + num_actual_parts + 1;
+//       }
+//       pv.SetCycle(j+1);
+//       pv.SetTime(j+1);
+//       pv.Save();
+//    }
+
+
+//    // // **************TESTS***********
+//    // //Test to make sure final mesh is fully refined
+//    // Array<int> p_arr(p.Size());
+//    // for (int ip = 0; ip < p.Size(); ip++){
+//    //    p_arr[ip] = p[ip];
+//    // }
+//    // p_arr.Sort();
+//    // bool no_duplicates = true;
+//    // for (int ip = 0; ip < p_arr.Size(); ip++){
+//    //    if (p_arr[ip] != ip){
+//    //       no_duplicates = false;
+//    //       break;
+//    //    }
+//    // }
+//    // if (no_duplicates){std::cout << "YES - Refined mesh IS fully refined" << std::endl;}
+//    // else{std::cout << "NO - Refined mesh IS NOT fully refined" << std::endl;}
+
+//    // //Test to make sure that E's have correct sizes
+//    // bool correct_size = true;
+//    // for (int ie = 1; ie < num_partitions; ie++){
+//    //    int max_value = *std::max_element(E[ie].begin(), E[ie].end());
+//    //    if(max_value+1 != E[ie-1].size()){std::cout << "ie with wrong size: " << ie << std::endl; correct_size = false; break;}
+//    //    if (ie == num_partitions - 1)
+//    //    {
+//    //       if(E[ie].size() == ne){std::cout << "CORRECT FINAL SIZE FOR E" << std::endl;}
+//    //       else{std::cout << "WRONG FINAL SIZE FOR E" << std::endl;}
+//    //    }
+//    // }
+//    // if (correct_size){std::cout << "YES - E's have correct sizes" << std::endl;}
+//    // else{std::cout << "NO - an E has an incorrect size" << std::endl;}
+//    return 0;
+// }
+
 int main(int argc, char *argv[])
 {
-   //Mpi::Init(argc, argv);
-   //Hypre::Init();
-
    const char *mesh_file = "../../data/star.mesh";
-   int ncoarse = 3;
+   int ref_levels = 0;
+   int order = 1;
+   real_t kappa_0 = 10.0;
 
    OptionsParser args(argc, argv);
-   args.AddOption(&mesh_file, "-m", "--mesh", "Mesh file to use.");
-   args.AddOption(&ncoarse, "-n", "--n-coarse", "Number of coarse elements.");
+   args.AddOption(&mesh_file, "-m", "--mesh", "Mesh file.");
+   args.AddOption(&ref_levels, "-r", "--refine", "Refinement levels.");
+   args.AddOption(&order, "-o", "--order", "Polynomial degree.");
+   args.AddOption(&kappa_0, "-k", "--kappa", "DG penalty parameter.");
    args.ParseCheck();
 
    Mesh mesh(mesh_file);
-   //const Mesh mesh_copy(mesh);
-   mesh.UniformRefinement();
-   //mesh.UniformRefinement();
-   DG_FECollection fec(0, mesh.Dimension());
-   FiniteElementSpace fes(&mesh, &fec);
-   GridFunction p(&fes);
-   const int ne = mesh.GetNE();
-   const int num_partitions = std::ceil(std::log(ne)/std::log(ncoarse));
+   const int dim = mesh.Dimension();
 
-   ParaViewDataCollection pv("Agglomeration", &mesh);
-   pv.SetPrefixPath("ParaView");
-   pv.RegisterField("p", &p);
+   for (int i = 0; i < ref_levels; ++i) { mesh.UniformRefinement(); }
 
-   std::vector<std::vector<int>> E(num_partitions);
+   DG_FECollection fec(order, dim, BasisType::GaussLobatto);
+   FiniteElementSpace fespace(&mesh, &fec);
+   cout << "Number of unknowns: " << fespace.GetVSize() << endl;
 
-   p = 0;
-   pv.SetCycle(0);
-   pv.SetTime(0.0);
-   pv.Save();
+   const real_t sigma = -1.0;
+   const real_t kappa = kappa_0 * (order + 1) * (order + 1);
 
-   Array<int> partitioning = PartitionMesh(mesh, ncoarse);
-   for (int i = 0; i < p.Size(); ++i)
-   {
-      p[i] = partitioning[i];
-   }
-   for (int i = 0; i < ncoarse; ++i)
-   {
-      E[0].push_back(0);
-   }
-   std::vector<std::vector<int>> macro_elements(ncoarse);
-   for (int k = 0; k < ne; ++k)
-   {
-      const int i = partitioning[k];
-      macro_elements[i].push_back(k);
-   }
-   pv.SetCycle(1);
-   pv.SetTime(1.0);
-   pv.Save();
-   for (int j = 1; j < num_partitions; ++j)
-   {
-      std::vector<std::vector<int>> macro_elements(E[j-1].size());
-      for (int i = 0; i < p.Size(); ++i)
-      {
-         const int k = p[i];
-         macro_elements[k].push_back(i);
-      }
-      int num_total_parts = 0;
-      for (int e = 0; e < E[j-1].size(); ++e)
-      {
-         const int num_el_part = macro_elements[e].size();
-         Array<int> subset(num_el_part);
-         for (int i=0; i<num_el_part; i++) {subset[i] = macro_elements[e][i];}
-         Array<int> partitioning = PartitionMesh(mesh, ncoarse, subset);
-         int num_actual_parts = 0;
-         for (int ip = 0; ip < partitioning.Size(); ++ip)
-         {
-            const int i = partitioning[ip];
-            num_actual_parts = (i > num_actual_parts) ? i : num_actual_parts;
-            p[subset[ip]] = i + num_total_parts;
-         }
-         for (int k = 0; k <= num_actual_parts; ++k) {E[j].push_back(e);}
-         num_total_parts = num_total_parts + num_actual_parts + 1;
-      }
-      pv.SetCycle(j+1);
-      pv.SetTime(j+1);
-      pv.Save();
-   }
+   LinearForm b(&fespace);
+   ConstantCoefficient one(1.0);
+   ConstantCoefficient zero(0.0);
+   b.AddDomainIntegrator(new DomainLFIntegrator(one));
+   b.AddBdrFaceIntegrator(
+      new DGDirichletLFIntegrator(zero, one, sigma, kappa));
+   b.Assemble();
 
+   GridFunction x(&fespace);
+   x = 0.0;
 
-   // // **************TESTS***********
-   // //Test to make sure final mesh is fully refined
-   // Array<int> p_arr(p.Size());
-   // for (int ip = 0; ip < p.Size(); ip++){
-   //    p_arr[ip] = p[ip];
-   // }
-   // p_arr.Sort();
-   // bool no_duplicates = true;
-   // for (int ip = 0; ip < p_arr.Size(); ip++){
-   //    if (p_arr[ip] != ip){
-   //       no_duplicates = false;
-   //       break;
-   //    }
-   // }
-   // if (no_duplicates){std::cout << "YES - Refined mesh IS fully refined" << std::endl;}
-   // else{std::cout << "NO - Refined mesh IS NOT fully refined" << std::endl;}
+   BilinearForm a(&fespace);
+   a.AddDomainIntegrator(new DiffusionIntegrator(one));
+   a.AddInteriorFaceIntegrator(new DGDiffusionIntegrator(one, sigma, kappa));
+   a.AddBdrFaceIntegrator(new DGDiffusionIntegrator(one, sigma, kappa));
+   a.Assemble();
+   a.Finalize();
 
-   // //Test to make sure that E's have correct sizes
-   // bool correct_size = true;
-   // for (int ie = 1; ie < num_partitions; ie++){
-   //    int max_value = *std::max_element(E[ie].begin(), E[ie].end());
-   //    if(max_value+1 != E[ie-1].size()){std::cout << "ie with wrong size: " << ie << std::endl; correct_size = false; break;}
-   //    if (ie == num_partitions - 1)
-   //    {
-   //       if(E[ie].size() == ne){std::cout << "CORRECT FINAL SIZE FOR E" << std::endl;}
-   //       else{std::cout << "WRONG FINAL SIZE FOR E" << std::endl;}
-   //    }
-   // }
-   // if (correct_size){std::cout << "YES - E's have correct sizes" << std::endl;}
-   // else{std::cout << "NO - an E has an incorrect size" << std::endl;}
+   SparseMatrix &A = a.SpMat();
+
+   AgglomerationMultigrid mg(fespace, A);
+
+   CGSolver cg;
+   cg.SetRelTol(1e-12);
+   cg.SetMaxIter(500);
+   cg.SetPrintLevel(1);
+   cg.SetOperator(A);
+   cg.SetPreconditioner(mg);
+   cg.Mult(b, x);
+
    return 0;
 }
--- miniapps/dg-agglomeration/mg_agglom.cpp
+++ miniapps/dg-agglomeration/mg_agglom.cpp
@@ -11,26 +11,26 @@
 
 #include "mg_agglom.hpp"
 #include "partition.hpp"
-#include "mfem.hpp"
-#include <fstream>
-#include <iostream>
-#include <memory>
-
 
 using namespace std;
-using namespace mfem;
+
+namespace mfem
+{
 
 std::vector<std::vector<int>> Agglomerate(Mesh &mesh)
 {
-   std::vector<std::vector<int>> E;
+   const int ne = mesh.GetNE();
+   const int ncoarse = 4;
 
-   // Recursive METIS partitioning to create 'E' data 
-   int ncoarse = 4;
+   const int num_partitions = std::ceil(std::log(ne)/std::log(ncoarse));
+
+   std::vector<std::vector<int>> E(num_partitions);
+
+   // Recursive METIS partitioning to create 'E' data
+
    DG_FECollection fec(0, mesh.Dimension());
    FiniteElementSpace fes(&mesh, &fec);
    GridFunction p(&fes);
-   const int ne = mesh.GetNE();
-   const int num_partitions = std::ceil(std::log(ne)/std::log(ncoarse));
    p = 0;
    Array<int> partitioning = PartitionMesh(mesh, ncoarse);
    for (int i = 0; i < p.Size(); ++i)
@@ -92,17 +92,18 @@ AgglomerationMultigrid::AgglomerationMultigrid(
    operators.SetSize(num_levels);
    smoothers.SetSize(num_levels);
    ownedOperators.SetSize(num_levels);
-   ownedSmoothers.SetSize(num_levels); 
+   ownedSmoothers.SetSize(num_levels);
    prolongations.SetSize(num_levels-1);
-   ownedProlongations.SetSize(num_levels-1); 
+   ownedProlongations.SetSize(num_levels-1);
    //Set the ownership
-   for(int l = 0; l < num_levels-1; ++l){
+   for (int l = 0; l < num_levels-1; ++l)
+   {
       ownedOperators[l] = true;
       ownedSmoothers[l] = true;
       ownedProlongations[l] = true;
    }
    ownedOperators[num_levels-1] = false;
-   ownedSmoothers[num_levels-1] = true; 
+   ownedSmoothers[num_levels-1] = true;
 
    // Populate the arrays: prolongations, ownedProlongations from the Multigrid
    // class. All prolongations are owned.
@@ -110,23 +111,30 @@ AgglomerationMultigrid::AgglomerationMultigrid(
 
    // Make the final prolongation
    operators[num_levels - 1] = &Af;
-   SparseMatrix p_level = SparseMatrix(4*E[num_levels-2].size(), 4*E[num_levels-3].size());
+   SparseMatrix p_level = SparseMatrix(4*E[num_levels-2].size(),
+                                       4*E[num_levels-3].size());
    GridFunction verts(&fes);
    mesh->GetNodes(verts);
    int nnodes = verts.Size()/2;
-   for(int r = 0; r < E[num_levels-2].size(); ++r)
+   for (int r = 0; r < E[num_levels-2].size(); ++r)
    {
-      int c = E[num_levels-2][r]; 
-      double x0 = verts(r*4); double x1 = verts(r*4+1); double x2 = verts(r*4+2); double x3 = verts(r*4+3);
-      double y0 = verts(nnodes+ r*4); double y1 = verts(nnodes + (r*4+1)); double y2 = verts(nnodes + r*4+2); double y3 = verts(nnodes+r*4+3);
+      int c = E[num_levels-2][r];
+      double x0 = verts(r*4); double x1 = verts(r*4+1); double x2 = verts(r*4+2);
+      double x3 = verts(r*4+3);
+      double y0 = verts(nnodes+ r*4); double y1 = verts(nnodes + (r*4+1));
+      double y2 = verts(nnodes + r*4+2); double y3 = verts(nnodes+r*4+3);
       //column 1 of block
-      p_level.Set(4*r, 4*c, 1); p_level.Set(4*r+1, 4*c, 1); p_level.Set(4*r+1, 4*c, 1); p_level.Set(4*r+1, 4*c, 1);
+      p_level.Set(4*r, 4*c, 1); p_level.Set(4*r+1, 4*c, 1);
+      p_level.Set(4*r+1, 4*c, 1); p_level.Set(4*r+1, 4*c, 1);
       //column 2 of block
-      p_level.Set(4*r, 4*c+1, x0); p_level.Set(4*r+1, 4*c+1, x1); p_level.Set(4*r+1, 4*c+1, x2); p_level.Set(4*r+1, 4*c+1, x3);
+      p_level.Set(4*r, 4*c+1, x0); p_level.Set(4*r+1, 4*c+1, x1);
+      p_level.Set(4*r+1, 4*c+1, x2); p_level.Set(4*r+1, 4*c+1, x3);
       //column 3 of block
-      p_level.Set(4*r, 4*c+2, y0); p_level.Set(4*r+1, 4*c+2, y1); p_level.Set(4*r+1, 4*c+2, y2); p_level.Set(4*r+1, 4*c+2, y3);
+      p_level.Set(4*r, 4*c+2, y0); p_level.Set(4*r+1, 4*c+2, y1);
+      p_level.Set(4*r+1, 4*c+2, y2); p_level.Set(4*r+1, 4*c+2, y3);
       //column 3 of block
-      p_level.Set(4*r, 4*c+3, x0*y0); p_level.Set(4*r+1, 4*c+3, x1*y1); p_level.Set(4*r+1, 4*c+3, x2*y2); p_level.Set(4*r+1, 4*c+3, x3*y3);
+      p_level.Set(4*r, 4*c+3, x0*y0); p_level.Set(4*r+1, 4*c+3, x1*y1);
+      p_level.Set(4*r+1, 4*c+3, x2*y2); p_level.Set(4*r+1, 4*c+3, x3*y3);
    }
    SparseMatrix* PT = Transpose(p_level);
    RAPOperator Ac(*PT, *operators[num_levels-1], p_level);
@@ -135,9 +143,10 @@ AgglomerationMultigrid::AgglomerationMultigrid(
    prolongations[num_levels-2] = &p_level;
 
    //Make the rest of the prolongations
-   for(int l = num_levels-3; l > 0; --l){
+   for (int l = num_levels-3; l > 0; --l)
+   {
       SparseMatrix p_level = SparseMatrix(4*E[l].size(), 4*E[l-1].size());
-      for(int r = 1; r < E[l].size(); ++r)
+      for (int r = 1; r < E[l].size(); ++r)
       {
          int c = E[l][r];
          p_level.Set(4*r, 4*c, 1);
@@ -154,9 +163,9 @@ AgglomerationMultigrid::AgglomerationMultigrid(
 
    //Make First Prolongation
    SparseMatrix p_levelc = SparseMatrix(4*E[0].size(), 4);
-   for(int r = 0; r < 4; ++r)
+   for (int r = 0; r < 4; ++r)
    {
-      p_levelc.Set(r, r, 1); 
+      p_levelc.Set(r, r, 1);
       p_levelc.Set(r+4, r, 1);
       p_levelc.Set(r+8, r, 1);
       p_levelc.Set(r+12, r, 1);
@@ -168,180 +177,11 @@ AgglomerationMultigrid::AgglomerationMultigrid(
    prolongations[0] = &p_levelc;
 
    // Create the smoothers (using BlockILU for now) remember block size is num degrees of freedom per element
-   for(int l=0; l < num_levels; ++l)
+   for (int l=0; l < num_levels; ++l)
    {
       BlockILU smooth_l(*operators[l], 4);
       smoothers[l] = &smooth_l;
    }
 }
 
-int main(int argc, char *argv[]){
-   // 1. Parse command-line options.
-   const char *mesh_file = "../data/star.mesh";
-   int ref_levels = -1;
-   int order = 1;
-   real_t sigma = -1.0;
-   real_t kappa = -1.0;
-   real_t eta = 0.0;
-   bool pa = false;
-   bool visualization = 1;
-   const char *device_config = "cpu";
-
-   OptionsParser args(argc, argv);
-   args.AddOption(&mesh_file, "-m", "--mesh",
-                  "Mesh file to use.");
-   args.AddOption(&ref_levels, "-r", "--refine",
-                  "Number of times to refine the mesh uniformly, -1 for auto.");
-   args.AddOption(&order, "-o", "--order",
-                  "Finite element order (polynomial degree) >= 0.");
-   args.AddOption(&sigma, "-s", "--sigma",
-                  "One of the three DG penalty parameters, typically +1/-1."
-                  " See the documentation of class DGDiffusionIntegrator.");
-   args.AddOption(&kappa, "-k", "--kappa",
-                  "One of the three DG penalty parameters, should be positive."
-                  " Negative values are replaced with (order+1)^2.");
-   args.AddOption(&eta, "-e", "--eta", "BR2 penalty parameter.");
-   args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
-                  "--no-partial-assembly", "Enable Partial Assembly.");
-   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
-                  "--no-visualization",
-                  "Enable or disable GLVis visualization.");
-   args.AddOption(&device_config, "-d", "--device",
-                  "Device configuration string, see Device::Configure().");
-   args.Parse();
-   if (!args.Good())
-   {
-      args.PrintUsage(cout);
-      return 1;
-   }
-   if (kappa < 0)
-   {
-      kappa = (order+1)*(order+1);
-   }
-   args.PrintOptions(cout);
-
-     // 3. Read the mesh from the given mesh file. We can handle triangular,
-   //    quadrilateral, tetrahedral and hexahedral meshes with the same code.
-   //    NURBS meshes are projected to second order meshes.
-   Mesh mesh(mesh_file);
-   const int dim = mesh.Dimension();
-
-      // 4. Refine the mesh to increase the resolution. In this example we do
-   //    'ref_levels' of uniform refinement. By default, or if ref_levels < 0,
-   //    we choose it to be the largest number that gives a final mesh with no
-   //    more than 50,000 elements.
-   {
-      if (ref_levels < 0)
-      {
-         ref_levels = (int)floor(log(50000./mesh.GetNE())/log(2.)/dim);
-      }
-      for (int l = 0; l < ref_levels; l++)
-      {
-         mesh.UniformRefinement();
-      }
-   }
-   if (mesh.NURBSext)
-   {
-      mesh.SetCurvature(max(order, 1));
-   }
-
-   // 5. Define a finite element space on the mesh. Here we use discontinuous
-   //    finite elements of the specified order >= 0.
-   const auto bt = pa ? BasisType::GaussLobatto : BasisType::GaussLegendre;
-   DG_FECollection fec(order, dim, bt);
-   FiniteElementSpace fespace(&mesh, &fec);
-   cout << "Number of unknowns: " << fespace.GetVSize() << endl;
-
-   // 6. Set up the linear form b(.) which corresponds to the right-hand side of
-   //    the FEM linear system.
-   LinearForm b(&fespace);
-   ConstantCoefficient one(1.0);
-   ConstantCoefficient zero(0.0);
-   b.AddDomainIntegrator(new DomainLFIntegrator(one));
-   b.AddBdrFaceIntegrator(
-      new DGDirichletLFIntegrator(zero, one, sigma, kappa));
-   b.Assemble();
-
-   // 7. Define the solution vector x as a finite element grid function
-   //    corresponding to fespace. Initialize x with initial guess of zero.
-   GridFunction x(&fespace);
-   x = 0.0;
-
-   // 8. Set up the bilinear form a(.,.) on the finite element space
-   //    corresponding to the Laplacian operator -Delta, by adding the Diffusion
-   //    domain integrator and the interior and boundary DG face integrators.
-   //    Note that boundary conditions are imposed weakly in the form, so there
-   //    is no need for dof elimination. After assembly and finalizing we
-   //    extract the corresponding sparse matrix A.
-   BilinearForm a(&fespace);
-   a.AddDomainIntegrator(new DiffusionIntegrator(one));
-   a.AddInteriorFaceIntegrator(new DGDiffusionIntegrator(one, sigma, kappa));
-   a.AddBdrFaceIntegrator(new DGDiffusionIntegrator(one, sigma, kappa));
-   if (eta > 0)
-   {
-      MFEM_VERIFY(!pa, "BR2 not yet compatible with partial assembly.");
-      a.AddInteriorFaceIntegrator(new DGDiffusionBR2Integrator(fespace, eta));
-      a.AddBdrFaceIntegrator(new DGDiffusionBR2Integrator(fespace, eta));
-   }
-   if (pa) { a.SetAssemblyLevel(AssemblyLevel::PARTIAL); }
-   a.Assemble();
-   a.Finalize();
-
-   // 9. Define a simple symmetric Gauss-Seidel preconditioner and use it to
-   //    solve the system Ax=b with PCG in the symmetric case, and GMRES in the
-   //    non-symmetric one. (Note that tolerances are squared: 1e-12 corresponds
-   //    to a relative tolerance of 1e-6).
-   //
-   //    If MFEM was compiled with SuiteSparse, use UMFPACK to solve the system.
-   if (pa)
-   {
-      MFEM_VERIFY(sigma == -1.0,
-                  "The case of PA with sigma != -1 is not yet supported.");
-      CG(a, b, x, 1, 500, 1e-12, 0.0);
-   }
-   else
-   {
-      SparseMatrix &A = a.SpMat();
-#ifndef MFEM_USE_SUITESPARSE
-      //GSSmoother M(A);
-
-      AgglomerationMultigrid M(fespace, A);
-      if (sigma == -1.0)
-      {
-         PCG(A, M, b, x, 1, 500, 1e-12, 0.0);
-      }
-      else
-      {
-         GMRES(A, M, b, x, 1, 500, 10, 1e-12, 0.0);
-      }
-#else
-      UMFPackSolver umf_solver;
-      umf_solver.Control[UMFPACK_ORDERING] = UMFPACK_ORDERING_METIS;
-      umf_solver.SetOperator(A);
-      umf_solver.Mult(b, x);
-#endif
-   }
-
-   // 10. Save the refined mesh and the solution. This output can be viewed
-   //     later using GLVis: "glvis -m refined.mesh -g sol.gf".
-   ofstream mesh_ofs("refined.mesh");
-   mesh_ofs.precision(8);
-   mesh.Print(mesh_ofs);
-   ofstream sol_ofs("sol.gf");
-   sol_ofs.precision(8);
-   x.Save(sol_ofs);
-
-   // 11. Send the solution by socket to a GLVis server.
-   if (visualization)
-   {
-      char vishost[] = "localhost";
-      int  visport   = 19916;
-      socketstream sol_sock(vishost, visport);
-      sol_sock.precision(8);
-      sol_sock << "solution\n" << mesh << x << flush;
-   }
-
-   return 0;
-}
-
- // namespace mfem
+} // namespace mfem
