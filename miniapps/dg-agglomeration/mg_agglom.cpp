d2740ed46f85dbdd70ba4c06f567b7c3f4db646f
Will Pazner <will.e.p@gmail.com>
2025-06-09 15:38:26 -0700

    Refactor creation of multigrid hierarchy

--- miniapps/dg-agglomeration/mg_agglom.cpp
+++ miniapps/dg-agglomeration/mg_agglom.cpp
@@ -77,11 +77,63 @@ std::vector<std::vector<int>> Agglomerate(Mesh &mesh)
    return E;
 }
 
+SparseMatrix *CreateNodalProlongation(
+   const std::vector<std::vector<int>> &E, FiniteElementSpace &fes)
+{
+   const int n = E.size();
+
+   SparseMatrix *P = new SparseMatrix(4*E[n-1].size(),
+                                      4*E[n-2].size());
+
+   Mesh &mesh = *fes.GetMesh();
+   FiniteElementSpace nodal_fes(&mesh, fes.FEColl());
+   GridFunction nodes(&nodal_fes);
+   mesh.GetNodes(nodes);
+   int nnodes = nodes.Size()/mesh.Dimension();
+   for (int r = 0; r < E[n-1].size(); ++r)
+   {
+      int c = E[n-1][r];
+      double x0 = nodes(r*4); double x1 = nodes(r*4+1); double x2 = nodes(r*4+2);
+      double x3 = nodes(r*4+3);
+      double y0 = nodes(nnodes+ r*4); double y1 = nodes(nnodes + (r*4+1));
+      double y2 = nodes(nnodes + r*4+2); double y3 = nodes(nnodes+r*4+3);
+      //column 1 of block
+      P->Set(4*r, 4*c, 1); P->Set(4*r+1, 4*c, 1);
+      P->Set(4*r+1, 4*c, 1); P->Set(4*r+1, 4*c, 1);
+      //column 2 of block
+      P->Set(4*r, 4*c+1, x0); P->Set(4*r+1, 4*c+1, x1);
+      P->Set(4*r+1, 4*c+1, x2); P->Set(4*r+1, 4*c+1, x3);
+      //column 3 of block
+      P->Set(4*r, 4*c+2, y0); P->Set(4*r+1, 4*c+2, y1);
+      P->Set(4*r+1, 4*c+2, y2); P->Set(4*r+1, 4*c+2, y3);
+      //column 3 of block
+      P->Set(4*r, 4*c+3, x0*y0); P->Set(4*r+1, 4*c+3, x1*y1);
+      P->Set(4*r+1, 4*c+3, x2*y2); P->Set(4*r+1, 4*c+3, x3*y3);
+   }
+   P->Finalize();
+   return P;
+}
+
+SparseMatrix *CreateInclusionProlongation(
+   int l, const std::vector<std::vector<int>> &E)
+{
+   SparseMatrix *P = new SparseMatrix(4*E[l].size(), 4*E[l-1].size());
+   for (int e = 0; e < E[l].size(); ++e)
+   {
+      int c = E[l][e];
+      for (int i = 0; i < 4; ++i)
+      {
+         P->Set(4*e + i, 4*c + i, 1);
+      }
+   }
+   P->Finalize();
+   return P;
+}
+
 AgglomerationMultigrid::AgglomerationMultigrid(
    FiniteElementSpace &fes, SparseMatrix &Af)
 {
    MFEM_VERIFY(fes.GetMaxElementOrder() == 1, "Only linear elements supported.");
-   Mesh* mesh = fes.GetMesh();
    // Create the mesh hierarchy
    auto E = Agglomerate(*fes.GetMesh());
    int num_levels = E.size()+1;
@@ -105,82 +157,35 @@ AgglomerationMultigrid::AgglomerationMultigrid(
    ownedOperators[num_levels-1] = false;
    ownedSmoothers[num_levels-1] = true;
 
+   operators[num_levels - 1] = &Af;
+
    // Populate the arrays: prolongations, ownedProlongations from the Multigrid
    // class. All prolongations are owned.
    // Create the prolongations using 'E' using the SparseMatrix class
-
-   // Make the final prolongation
-   operators[num_levels - 1] = &Af;
-   SparseMatrix p_level = SparseMatrix(4*E[num_levels-2].size(),
-                                       4*E[num_levels-3].size());
-   GridFunction verts(&fes);
-   mesh->GetNodes(verts);
-   int nnodes = verts.Size()/2;
-   for (int r = 0; r < E[num_levels-2].size(); ++r)
+   for (int l = num_levels - 2; l >= 0; --l)
    {
-      int c = E[num_levels-2][r];
-      double x0 = verts(r*4); double x1 = verts(r*4+1); double x2 = verts(r*4+2);
-      double x3 = verts(r*4+3);
-      double y0 = verts(nnodes+ r*4); double y1 = verts(nnodes + (r*4+1));
-      double y2 = verts(nnodes + r*4+2); double y3 = verts(nnodes+r*4+3);
-      //column 1 of block
-      p_level.Set(4*r, 4*c, 1); p_level.Set(4*r+1, 4*c, 1);
-      p_level.Set(4*r+1, 4*c, 1); p_level.Set(4*r+1, 4*c, 1);
-      //column 2 of block
-      p_level.Set(4*r, 4*c+1, x0); p_level.Set(4*r+1, 4*c+1, x1);
-      p_level.Set(4*r+1, 4*c+1, x2); p_level.Set(4*r+1, 4*c+1, x3);
-      //column 3 of block
-      p_level.Set(4*r, 4*c+2, y0); p_level.Set(4*r+1, 4*c+2, y1);
-      p_level.Set(4*r+1, 4*c+2, y2); p_level.Set(4*r+1, 4*c+2, y3);
-      //column 3 of block
-      p_level.Set(4*r, 4*c+3, x0*y0); p_level.Set(4*r+1, 4*c+3, x1*y1);
-      p_level.Set(4*r+1, 4*c+3, x2*y2); p_level.Set(4*r+1, 4*c+3, x3*y3);
-   }
-   SparseMatrix* PT = Transpose(p_level);
-   RAPOperator Ac(*PT, *operators[num_levels-1], p_level);
-   operators[num_levels-2] = &Ac;
-   //SparseMatrix* p_level_p = &p_level;
-   prolongations[num_levels-2] = &p_level;
-
-   //Make the rest of the prolongations
-   for (int l = num_levels-3; l > 0; --l)
-   {
-      SparseMatrix p_level = SparseMatrix(4*E[l].size(), 4*E[l-1].size());
-      for (int r = 1; r < E[l].size(); ++r)
+      SparseMatrix *P;
+      if (l < num_levels - 2)
       {
-         int c = E[l][r];
-         p_level.Set(4*r, 4*c, 1);
-         p_level.Set(4*r+1, 4*c+1, 1);
-         p_level.Set(4*r+2, 4*c+2, 1);
-         p_level.Set(4*r+3, 4*c+3, 1);
+         P = CreateInclusionProlongation(l, E);
       }
-      SparseMatrix* PT = Transpose(p_level);
-      RAPOperator Ac(*PT, *operators[l+1], p_level);
-      operators[l] = &Ac;
-      //SparseMatrix* p_level_p = &p_level;
-      prolongations[l] = &p_level;
-   }
+      else
+      {
+         P = CreateNodalProlongation(E, fes);
+      }
 
-   //Make First Prolongation
-   SparseMatrix p_levelc = SparseMatrix(4*E[0].size(), 4);
-   for (int r = 0; r < 4; ++r)
-   {
-      p_levelc.Set(r, r, 1);
-      p_levelc.Set(r+4, r, 1);
-      p_levelc.Set(r+8, r, 1);
-      p_levelc.Set(r+12, r, 1);
+      SparseMatrix &A_prev = static_cast<SparseMatrix&>(*operators[l + 1]);
+
+      unique_ptr<SparseMatrix> AP(mfem::Mult(A_prev, *P));
+      unique_ptr<SparseMatrix> Pt(Transpose(*P));
+      operators[l] = mfem::Mult(*Pt, *AP);
+      prolongations[l] = P;
    }
-   SparseMatrix* PTc = Transpose(p_levelc);
-   RAPOperator Acc(*PTc, *operators[1], p_levelc);
-   operators[0] = &Acc;
-   //SparseMatrix* p_level_p = &p_level;
-   prolongations[0] = &p_levelc;
 
    // Create the smoothers (using BlockILU for now) remember block size is num degrees of freedom per element
    for (int l=0; l < num_levels; ++l)
    {
-      BlockILU smooth_l(*operators[l], 4);
-      smoothers[l] = &smooth_l;
+      smoothers[l] = new BlockILU(*operators[l], 4);
    }
 }
 
