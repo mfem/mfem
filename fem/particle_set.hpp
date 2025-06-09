65778c415115dbc914dacc62f575ab960afbf864
Joseph Signorelli <jms26@illinois.edu>
2025-06-09 16:46:13 -0700

    Define starting skeleton for ParticleSet

--- fem/particle_set.hpp
+++ fem/particle_set.hpp
@@ -12,55 +12,84 @@
 #ifndef MFEM_PARTICLE_SET
 #define MFEM_PARTICLE_SET
 
+#include "../config/config.hpp"
 
-#ifdef MFEM_USE_GSLIB // Must compile with GSLib!
+#include "../mesh/mesh.hpp"
+#include "fespace.hpp"
+
+#include <vector>
 
 namespace mfem
 {
 
-
 class ParticleSet
 {
-    public:
-        enum class Init
-        {
-            UNIFORM,
-            RANDOM
-        };
+protected:
+   const Ordering::Type ordering;
+#ifdef MFEM_USE_MPI
+   MPI_Comm comm;
+#endif // MFEM_USE_MPI
+   
+   std::vector<Vector> coords; /// Particle coordinate vectors in each spatial dimension
+   std::vector<Vector> real_fields; /// Additional scalar fields per particle, as specified in constructor
 
-    protected:
-        FindPointsGSLib finder;
-        Array<real_t> xs, ys, zs;
+   void SetVCoords() {}; // { v_coords.SetDataAndSize(coords.data(), coords.size()); };
 
-    public:
-        /// Initialize a ParticleSet on Mesh \p m. Note: the input mesh \p m must have Nodes set.
-        explicit ParticleSet(const Mesh &m)
-        { finder.Setup(m); };
+public:
+   ParticleSet(int dim, int num_fields=0, Ordering::Type ordering_=Ordering::Type::byNODES) 
+   : ordering(ordering_), coords(dim), real_fields(num_fields) {};
 
 #ifdef MFEM_USE_MPI
-        
-        // TODO. Will start with serial. Need to consider when a particle leaves one rank and goes to next
-        ParticleSet(const Mesh &m, MPI_Comm comm)
-        : finder(comm) { finder.Setup(m); };
-
+   ParticleSet(MPI_Comm comm_, int dim, int num_fields=0, Ordering::Type ordering_=Ordering::Type::byNODES) 
+   : ParticleSet(dim, num_fields, ordering_) { comm = comm_; };
 #endif // MFEM_USE_MPI
 
-        /** Initialize particles using an initialization method.
+   /// Get the ordering of how particle coordinates are stored.
+   Ordering::Type GetOrdering() const { return ordering; };
 
-            @param[in] init            Initialization method (see ParticleSet::Init for options).
-            @param[in] numParticles    Number of particles to add to ParticleSet.*/
-        void Initialize(ParticleSet::Init init, int numParticles);
+   /// Get the number of particles currently held by this ParticleSet.
+   int GetNumParticles() const  { return coords.size(); };
 
-        /// Add an individual particle at specified location \p coords.
-        void AddParticle(const Array<real_t> &coords);
+   /** Initialize particles randomly within bounding box defined by input \p m . All scalar fields are set to 0.
+       @param[in] m               Mesh defining bounding box to initialize particles on.
+       @param[in] numParticles    Number of particles to add to ParticleSet.
+       @param[in] seed            (Optional) Seed.*/
+   void RandomInitialize(Mesh &m, int numParticles, int seed=0);
 
-        
+   /// Add particle(s) specified by \p in_coords following \ref ordering, with field data given by \p in_fields . Number of fields must match that specified in object construction. */
+   void AddParticles(const Vector &in_coords, const Vector* in_fields[]={});
 
+   /// Remove particle(s) specified by \p list of particle indices.
+   void RemoveParticles(const Array<int> &list);
 
-}
+   /// Get particle \p i 's coordinates.
+   void GetParticle(int i, Vector &pos) const;
+
+   /// Get all particle coordinates.
+   const Vector& GetAllParticles() const;
+
+   /// Get particle \p i , field \p f value.
+   const real_t GetParticleField(int i, int f) const { return real_fields[f][i]; };
+
+   /// Get all field \p f values for all particles.
+   const Vector& GetAllParticlesField(int f) const { return real_fields[f]; };
+   
+   /// Set particle \p i , field \p f value to \p val.
+   void SetParticleField(int i, int f, real_t val) { real_fields[f][i] = val; };
+
+   void SetAllParticlesField(int f, const Vector &field_vals) { real_fields[f] = field_vals; };
+
+   /// Update particle positions using given new coordinates \p new_coords .
+   void UpdateParticlePositions(const Vector &new_coords);
+
+#ifdef MFEM_USE_MPI
+   /// Redistribute particles onto ranks specified in \p rank_list .
+   void Redistribute(const Array<int> &rank_list);
+#endif // MFEM_USE_MPI
+
+};
 
 } // namespace mfem
 
 
-#endif // MFEM_USE_GSLIB
 #endif // MFEM_PARTICLE_SET
