7b41dd6f7b53b12c07584d37b57a5c48c8e42d95
Stowell, Mark L. <stowell1@llnl.gov>
2025-06-12 10:39:23 -0700

    Adding miniapps/plasma and starting files

---
+++ miniapps/plasma/plasma.hpp
@@ -0,0 +1,56 @@
+// Copyright (c) 2010-2025, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#ifndef MFEM_PLASMA_HPP
+#define MFEM_PLASMA_HPP
+
+#include <cmath>
+
+namespace mfem
+{
+
+namespace plasma
+{
+
+// Physical Constants
+
+// Permittivity of Free Space (units F/m)
+static const double epsilon0_ = 8.8541878176e-12;
+
+// Permeability of Free Space (units H/m)
+static const double mu0_ = 4.0e-7 * M_PI;
+
+// Speed of light in Free Space (units m/s)
+static const double c0_ = 1.0 / sqrt(epsilon0_ * mu0_);
+
+static const double q_     = 1.602176634e-19; // Elementary charge in coulombs
+static const double eV_    = 1.602176634e-19; // 1 eV in Joules
+static const double amu_   = 1.660539040e-27; // Atomic mass unit in kilograms
+static const double me_kg_ = 9.10938356e-31;  // Mass of electron in kilograms
+static const double me_u_  = 5.4857990907e-4; // Mass of electron in a.m.u
+
+/**
+   Returns the cyclotron frequency in radians/second
+   m is the mass in a.m.u
+   q is the charge in units of elementary electric charge
+   B is the magnetic field magnitude in tesla
+ */
+inline double cyclotronFrequency(double B, double m, double q)
+{
+   return fabs(q * q_ * B / (m * amu_));
+}
+
+} // namespace plasma
+
+} // namespace mfem
+
+#endif // MFEM_PLASMA_HPP
+
