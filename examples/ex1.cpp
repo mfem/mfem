21df84f3209009c9608f0063c4f26980124342bb
camierjs <camierjs@gmail.com>
2025-06-18 13:03:02 -0700

    ✅ address_sanitizer

--- examples/ex1.cpp
+++ examples/ex1.cpp
@@ -286,17 +286,19 @@ int main(int argc, char *argv[])
 
 #if defined(__has_feature)
 
+   // UndefinedBehaviorSanitizer (UBSan) is a fast undefined behavior detector.
 #if __has_feature(undefined_behavior_sanitizer)
    int k = 0x7fffffff;
    mfem::out << "undefined_behavior_sanitizer" << (k += argc) << std::endl;
 #endif // undefined_behavior_sanitizer
 
-#if __has_feature(address_sanitizer)
-   int *array = new int[1024];
-   delete [] array;
-   mfem::out << "address_sanitizer" << (array[argc]) << std::endl;
-#endif // address_sanitizer
+   // #if __has_feature(address_sanitizer)
+   //    int *array = new int[1024];
+   //    delete [] array;
+   //    mfem::out << "address_sanitizer" << (array[argc]) << std::endl;
+   // #endif // address_sanitizer
 
+   // MemorySanitizer is a detector of uninitialized memory use.
 #if __has_feature(memory_sanitizer)
    int* a = new int[1024];
    a[5] = 0;
