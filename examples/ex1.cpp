9305189601c8cec363d8f3dd274cb4092b3d287a
camierjs <camierjs@gmail.com>
2025-06-18 13:18:18 -0700

    MemorySanitizer code fix

--- examples/ex1.cpp
+++ examples/ex1.cpp
@@ -300,9 +300,9 @@ int main(int argc, char *argv[])
 
    // MemorySanitizer is a detector of uninitialized memory use.
 #if __has_feature(memory_sanitizer)
-   int* a = new int[1024];
-   a[5] = 0;
-   mfem::out << "memory_sanitizer" << (a[argc]) << std::endl;
+   int* array = new int[1024];
+   array[5] = 0;
+   mfem::out << "memory_sanitizer" << (array[argc]) << std::endl;
 #endif // memory_sanitizer
 
 #endif // __has_feature
