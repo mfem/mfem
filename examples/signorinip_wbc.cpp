404f73f184fbb812ef917e1589ea780ef1244126
thatguynoe <noereyesrivas@gmail.com>
2025-06-24 17:20:02 -0400

    modify alpha for faster convergence

--- examples/signorinip_wbc.cpp
+++ examples/signorinip_wbc.cpp
@@ -130,7 +130,7 @@ int main(int argc, char *argv[])
    // 1. Parse command-line options.
    const char *mesh_file = "../data/ref-cube.mesh";
    int order = 1;
-   real_t alpha = 1.0;
+   real_t alpha = 2.0;
    real_t beta = -1.0;
    real_t kappa = -1.0;
    int ref_levels = 0;
