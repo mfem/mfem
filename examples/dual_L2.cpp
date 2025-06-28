e37a466dc3853fd63cb118a8795a75b9e2c6d31b
Alexey Izmailov <alizma@brown.edu>
2025-06-27 17:40:43 -0700

    clean up and add alpha and L2 error against analytic

--- examples/dual_L2.cpp
+++ examples/dual_L2.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
    // 1. Parse command-line options.
    const char *mesh_file = "../data/star.mesh";
    int order = 1;
-   int max_it = 5;
+   int max_it = 10;
    int ref_levels = 3;
    real_t alpha = 1.0;
    real_t growth_rate = 1.0;
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
    }
    args.PrintOptions(cout);
 
-   Mesh mesh = Mesh::MakeCartesian2D(2, 2, Element::Type::QUADRILATERAL, false);
+   Mesh mesh = Mesh::MakeCartesian2D(1, 1, Element::Type::QUADRILATERAL, false);
    const int dim = mesh.Dimension();
    const int sdim = mesh.SpaceDimension();
 
@@ -114,12 +114,13 @@ int main(int argc, char *argv[])
    x = 0.0; rhs = 0.0;
 
    GridFunction u_gf, delta_psi_gf;
-   delta_psi_gf.MakeRef(&RTfes,x,offsets[0]);
-   u_gf.MakeRef(&L2fes,x,offsets[1]);
 
-   GridFunction psi_old_gf(&RTfes);
-   GridFunction psi_gf(&RTfes);
-   GridFunction u_old_gf(&L2fes);
+   u_gf.MakeRef(&RTfes, x, offsets[0]);
+   delta_psi_gf.MakeRef(&L2fes, x, offsets[1]); 
+
+   GridFunction psi_old_gf(&L2fes);
+   GridFunction psi_gf(&L2fes);
+   GridFunction u_old_gf(&RTfes);
 
    delta_psi_gf = 0.0;
    psi_gf = 0.0;
@@ -144,6 +145,8 @@ int main(int argc, char *argv[])
    ConstantCoefficient neg_tichonov_cf(-1.0*tichonov);
 	VectorConstantCoefficient one_vec_cf(Vector({1., 1.})); 
 
+   ConstantCoefficient alpha_cf((real_t) alpha); 
+   ProductCoefficient neg_alpha_cf(neg_one, alpha_cf); 
 
    ZCoefficient Z(sdim, psi_gf, alpha);
    DZCoefficient DZ(sdim, psi_gf, alpha);
@@ -151,60 +154,30 @@ int main(int argc, char *argv[])
 
    VectorSumCoefficient psi_newton_res(psi_old_cf, psi_cf, 1., -1.);
 
-
-   // DivergenceGridFunctionCoefficient div_psi_cf(&psi_gf);
-   // DivergenceGridFunctionCoefficient div_psi_old_cf(&psi_old_gf);
-   // SumCoefficient psi_old_minus_psi(div_psi_old_cf, div_psi_cf, 1.0, -1.0);
-
    LinearForm b0, b1;
    b0.MakeRef(&RTfes,rhs.GetBlock(0),0);
    b1.MakeRef(&L2fes,rhs.GetBlock(1),0);
 
    b0.AddDomainIntegrator(new VectorFEDomainLFIntegrator(psi_newton_res));
+
    FunctionCoefficient f([](const Vector &x) {return -0.5 * ( (1 - 2*x[0]) * x[1] * (1 - x[1]) + x[0] * (1 - x[0]) * (1-2*x[1]));  });
-   b0.AddDomainIntegrator(new VectorFEDomainLFDivIntegrator(f));
-   // VectorFEDomainLFDivIntegrator
+   ProductCoefficient neg_alpha_f_cf(neg_alpha_cf, f); 
+
+   b0.AddDomainIntegrator(new VectorFEDomainLFDivIntegrator(neg_alpha_f_cf));
 
    b1.AddDomainIntegrator(new VectorDomainLFIntegrator(Z));
-   // b1.AddDomainIntegrator(new DomainLFIntegrator(neg_one));
 
    BilinearForm a00(&RTfes);
-   a00.AddDomainIntegrator(new DivDivIntegrator()); 
-   // a00.AddDomainIntegrator(new VectorFEMassIntegrator(neg_DZ));
+   a00.AddDomainIntegrator(new DivDivIntegrator(alpha_cf)); 
    // a00.AddDomainIntegrator(new VectorFEMassIntegrator(tichonov_cf));
 
    MixedBilinearForm a10(&RTfes, &L2fes);
-   // a10.AddDomainIntegrator(new MixedVectorDivergenceIntegrator(one_vec_cf)); 
-   // a10.AddDomainIntegrator(new MixedVectorProductIntegrator(one_vec_cf)); 
-   // a10.AddDomainIntegrator(new MixedScalarMassIntegrator()); 
    a10.AddDomainIntegrator(new VectorFEMassIntegrator()); 
 
-   // a10.AddDomainIntegrator(new MixedVectorMassIntegrator()); 
-   
-   // a10.AddDomainIntegrator(new MixedDotProductIntegrator(one_vec_cf)); 
    a10.Assemble();
    a10.Finalize();
    SparseMatrix &A10 = a10.SpMat();
    SparseMatrix *A01 = Transpose(A10);
-
-   cout << "done with a10, a01" << endl; 
-
-   // MixedBilinearForm a01(&L2fes, &RTfes); 
-   // a01.AddDomainIntegrator(new MixedVectorMassIntegrator()); // NO: must both be H(div) 
-   // a01.AddDomainIntegrator(new MixedVectorProductIntegrator(one_vec_cf)); 
-
-   // a01.AddDomainIntegrator(new VectorFEMassIntegrator()) ; 
-   // a01.AddDomainIntegrator(new MixedDotProductIntegrator(one_vec_cf)); 
-   // a01.SetAssemblyLevel(mfem::AssemblyLevel::LEGACY);
-
-   // a01.AddDomainIntegrator(new VectorMassIntegrator()); 
-   // a01.AddDomainIntegrator(new MixedVector)
-   // a01.AddDomainIntegrator(new MixedVectorProductIntegrator(one_vec_cf)); 
-   
-   // a01.Assemble(); 
-   // a01.Finalize(); 
-   // SparseMatrix &A01 = a01.SpMat(); 
-   // SparseMatrix *A10 = Transpose(A01);
 
 
    BilinearForm a11(&L2fes);
@@ -213,7 +186,6 @@ int main(int argc, char *argv[])
    a11.Assemble();
    a11.Finalize();
    SparseMatrix &A11 = a11.SpMat();
-   cout << "done with a11" << endl;
 
    int k;
    int total_iterations = 0;
@@ -233,14 +205,11 @@ int main(int argc, char *argv[])
          total_iterations++;
 
          b0.Assemble();
-         cout << "assemble b0" << endl; 
          b1.Assemble();
-         cout << "assemble b1" << endl; 
 
          a00.Assemble(false);
          a00.Finalize(false);
          SparseMatrix &A00 = a00.SpMat();
-         cout << "assemble a00" << endl;
 
 
          // Construct Schur-complement preconditioner
@@ -281,8 +250,12 @@ int main(int argc, char *argv[])
 
          if (visualization)
          {
-            sol_sock << "solution\n" << mesh << u_gf << "window_title 'Discrete solution'"
-                     << flush;
+            // GridFunction u_x(&L2fes); 
+            // u_gf.GetNodalValues(u_x, 1);
+            sol_sock << "solution\n" << mesh << u_gf << "window_title 'Discrete solution '" << flush;
+
+            // sol_sock << "solution\n" << mesh << psi_gf << "window_title 'Discrete solution'"
+                     // << flush;
          }
 
          mfem::out << "Newton_update_size = " << Newton_update_size << endl;
@@ -309,6 +282,7 @@ int main(int argc, char *argv[])
       }
 
       alpha *= max(growth_rate, 1_r);
+      alpha_cf.constant = alpha; 
 
    }
 
@@ -316,6 +290,17 @@ int main(int argc, char *argv[])
              << "\n Total iterations: " << total_iterations
              << "\n Total dofs:       " << RTfes.GetTrueVSize() + L2fes.GetTrueVSize()
              << endl;
+
+   VectorFunctionCoefficient exact_coeff(2, [](const Vector &x, Vector &u) {
+      double x_val = x(0);
+      double y_val = x(1);
+      double val = x_val * y_val * (1 - x_val) * (1 - y_val);
+      u(0) = val;
+      u(1) = val;
+   });
+
+   double l2_error = u_gf.ComputeL2Error(exact_coeff); 
+   cout << "L2 error: " << l2_error << endl; 
 
    delete A01;
    // delete A10; 
@@ -348,3 +333,7 @@ void DZCoefficient::Eval(DenseMatrix &K, ElementTransformation &T,
    K.SetSize(2); 
    for (int i = 0; i < 2; ++i) { K(i, i) = (1. - pow(tanh(psi_vals(i) / 2.), 2)) / 2.; }
 }
+
+// void Manufacturedp_vec(const Vector &x, Vector &y) { 
+   // 
+// }
