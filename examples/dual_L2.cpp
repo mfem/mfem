10ed8dc856a25d7f39b16a6f3fc6f47ef26fd1b5
Alexey Izmailov <alizma@brown.edu>
2025-06-29 17:07:27 -0700

    simplify with UMFpack

--- examples/dual_L2.cpp
+++ examples/dual_L2.cpp
@@ -9,37 +9,33 @@ class ZCoefficient : public VectorCoefficient
 {
 protected:
    GridFunction *psi;
-   real_t alpha;
 
 public:
-   ZCoefficient(int vdim, GridFunction &psi_, real_t alpha_ = 1.0)
-      : VectorCoefficient(vdim), psi(&psi_), alpha(alpha_) { }
+   ZCoefficient(int vdim, GridFunction &psi_)
+      : VectorCoefficient(vdim), psi(&psi_) { }
 
    virtual void Eval(Vector &V, ElementTransformation &T,
                      const IntegrationPoint &ip);
-   void SetAlpha(real_t alpha_) { alpha = alpha_; }
 };
 
 class DZCoefficient : public MatrixCoefficient
 {
 protected:
    GridFunction *psi;
-   real_t alpha;
 
 public:
-   DZCoefficient(int height, GridFunction &psi_, real_t alpha_ = 1.0)
-      : MatrixCoefficient(height),  psi(&psi_), alpha(alpha_) { }
+   DZCoefficient(int height, GridFunction &psi_)
+      : MatrixCoefficient(height),  psi(&psi_) { }
 
    virtual void Eval(DenseMatrix &K, ElementTransformation &T,
                      const IntegrationPoint &ip);
-   void SetAlpha(real_t alpha_) { alpha = alpha_; }
 };
 
 int main(int argc, char *argv[])
 {
-   // 1. Parse command-line options.
-   const char *mesh_file = "../data/star.mesh";
+   // const char *mesh_file = "../data/star.mesh";
    int order = 1;
+   int order_l2 = 1; 
    int max_it = 10;
    int ref_levels = 3;
    real_t alpha = 1.0;
@@ -47,13 +43,15 @@ int main(int argc, char *argv[])
    real_t newton_scaling = 0.9;
    real_t tichonov = 1e-1;
    real_t tol = 1e-4;
+
    bool visualization = true;
 
    OptionsParser args(argc, argv);
-   args.AddOption(&mesh_file, "-m", "--mesh",
-                  "Mesh file to use.");
+   // args.AddOption(&mesh_file, "-m", "--mesh",
+                  // "Mesh file to use.");
    args.AddOption(&order, "-o", "--order",
-                  "Finite element order (polynomial degree).");
+                  "Finite element order for RT space");
+   args.AddOption(&order_l2, "-o2", "--order", "FEM order for L2 vec space"); 
    args.AddOption(&ref_levels, "-r", "--refs",
                   "Number of h-refinements.");
    args.AddOption(&max_it, "-mi", "--max-it",
@@ -88,35 +86,24 @@ int main(int argc, char *argv[])
    int curvature_order = max(order,2);
    mesh.SetCurvature(curvature_order);
 
-   // 4. Define the necessary finite element spaces on the mesh.
    RT_FECollection RTfec(order, dim);
    FiniteElementSpace RTfes(&mesh, &RTfec);
 
-   L2_FECollection L2fec(order, dim);
+   L2_FECollection L2fec(order_l2, dim);
    FiniteElementSpace L2fes(&mesh, &L2fec, 2);
-
-   // L2_FECollection L2fec_s(order, 1); 
-   // FiniteElementSpace L2fes_s(&mesh, &L2fec);
-
 
    cout << "Number of H(div) dofs: "
         << RTfes.GetTrueVSize() << endl;
    cout << "Number of L² dofs: "
         << L2fes.GetTrueVSize() << endl;
 
-   Array<int> offsets(3);
-   offsets[0] = 0;
-   offsets[1] = RTfes.GetVSize();
-   offsets[2] = L2fes.GetVSize();
+   Array<int> offsets({0, RTfes.GetVSize(), L2fes.GetVSize()});
    offsets.PartialSum();
 
    BlockVector x(offsets), rhs(offsets);
    x = 0.0; rhs = 0.0;
 
-   GridFunction u_gf, delta_psi_gf;
-
-   u_gf.MakeRef(&RTfes, x, offsets[0]);
-   delta_psi_gf.MakeRef(&L2fes, x, offsets[1]); 
+   GridFunction u_gf(&RTfes, x.GetBlock(0)), delta_psi_gf(&L2fes, x.GetBlock(1));
 
    GridFunction psi_old_gf(&L2fes);
    GridFunction psi_gf(&L2fes);
@@ -128,7 +115,7 @@ int main(int argc, char *argv[])
    psi_old_gf = psi_gf;
    u_old_gf = u_gf;
 
-   VectorGridFunctionCoefficient psi_old_cf(&psi_old_gf), psi_cf(&psi_gf); 
+   VectorGridFunctionCoefficient psi_old_cf(&psi_old_gf), psi_cf(&psi_gf), p_vc(&u_gf); 
 
    char vishost[] = "localhost";
    int  visport   = 19916;
@@ -137,32 +124,40 @@ int main(int argc, char *argv[])
    {
       sol_sock.open(vishost,visport);
       sol_sock.precision(8);
+      // sol_sock  << "keys jlA\n"; // turn off perspective & light 
+      // sol_sock << "keys cmA\n"; // colorbar + mesh + anti-alias
    }
 
    ConstantCoefficient neg_one(-1.0);
    ConstantCoefficient zero(0.0);
+	VectorConstantCoefficient one_vec_cf(Vector({1., 1.})); 
+
    ConstantCoefficient tichonov_cf(tichonov);
    ConstantCoefficient neg_tichonov_cf(-1.0*tichonov);
-	VectorConstantCoefficient one_vec_cf(Vector({1., 1.})); 
+
 
    ConstantCoefficient alpha_cf((real_t) alpha); 
    ProductCoefficient neg_alpha_cf(neg_one, alpha_cf); 
 
-   ZCoefficient Z(sdim, psi_gf, alpha);
-   DZCoefficient DZ(sdim, psi_gf, alpha);
+   ZCoefficient Z(sdim, psi_gf);
+   DZCoefficient DZ(sdim, psi_gf);
    ScalarMatrixProductCoefficient neg_DZ(-1.0, DZ);
 
    VectorSumCoefficient psi_newton_res(psi_old_cf, psi_cf, 1., -1.);
 
-   LinearForm b0, b1;
-   b0.MakeRef(&RTfes,rhs.GetBlock(0),0);
-   b1.MakeRef(&L2fes,rhs.GetBlock(1),0);
+   LinearForm b0(&RTfes, rhs.GetBlock(0).GetData()), b1(&L2fes, rhs.GetBlock(1).GetData());
 
    b0.AddDomainIntegrator(new VectorFEDomainLFIntegrator(psi_newton_res));
 
    // FunctionCoefficient f([](const Vector &x) {return -0.5 * ( (1 - 2*x[0]) * x[1] * (1 - x[1]) + x[0] * (1 - x[0]) * (1-2*x[1]));  });
-   FunctionCoefficient f([](const Vector &x) {return 2. * (x[0] + x[1]);  });
+   // FunctionCoefficient f([](const Vector &x) {return -2. * (x[0] + x[1]);  });
+   // FunctionCoefficient f([](const Vector &x) {return -2. * x[0] - pow(x[0], 2) / 2. + pow(x[0], 3) / 3. - 2 * x[1] - pow(x[1] , 2) / 2. + pow(x[1] , 3) / 3;  });
    
+   // FunctionCoefficient f([](const Vector &x) { return -x[0] - x[1]  + 1; }); 
+
+   FunctionCoefficient f([](const Vector &x) { return  1.; });  // just because this is prettiest 
+
+
    ProductCoefficient neg_alpha_f_cf(neg_alpha_cf, f); 
 
    b0.AddDomainIntegrator(new VectorFEDomainLFDivIntegrator(neg_alpha_f_cf));
@@ -171,7 +166,10 @@ int main(int argc, char *argv[])
 
    BilinearForm a00(&RTfes);
    a00.AddDomainIntegrator(new DivDivIntegrator(alpha_cf)); 
-   // a00.AddDomainIntegrator(new VectorFEMassIntegrator(tichonov_cf));
+   a00.AddDomainIntegrator(new VectorFEMassIntegrator(alpha_cf)); 
+   a00.Assemble();
+   a00.Finalize(); 
+   SparseMatrix &A00 = a00.SpMat(); 
 
    MixedBilinearForm a10(&RTfes, &L2fes);
    a10.AddDomainIntegrator(new VectorFEMassIntegrator()); 
@@ -181,13 +179,8 @@ int main(int argc, char *argv[])
    SparseMatrix &A10 = a10.SpMat();
    SparseMatrix *A01 = Transpose(A10);
 
-
    BilinearForm a11(&L2fes);
-   // a11.AddDomainIntegrator(new MassIntegrator(neg_tichonov_cf));
    a11.AddDomainIntegrator(new VectorMassIntegrator(neg_DZ)); 
-   a11.Assemble();
-   a11.Finalize();
-   SparseMatrix &A11 = a11.SpMat();
 
    int k;
    int total_iterations = 0;
@@ -196,8 +189,6 @@ int main(int argc, char *argv[])
    for (k = 0; k < max_it; k++)
    {
       u_tmp = u_old_gf;
-      Z.SetAlpha(alpha);
-      DZ.SetAlpha(alpha);
 
       mfem::out << "\nOUTER ITERATION " << k+1 << endl;
 
@@ -209,43 +200,20 @@ int main(int argc, char *argv[])
          b0.Assemble();
          b1.Assemble();
 
-         a00.Assemble(false);
-         a00.Finalize(false);
-         SparseMatrix &A00 = a00.SpMat();
+         a11.Assemble(false);
+         a11.Finalize(false);
+         SparseMatrix &A11 = a11.SpMat();
 
 
-         // Construct Schur-complement preconditioner
-         Vector A00_diag(a00.Height());
-         A00.GetDiag(A00_diag);
-         A00_diag.Reciprocal();
-         SparseMatrix *S = Mult_AtDA(*A01, A00_diag);
-         // SparseMatrix *S = Mult_AtDA(*A10, A00_diag);
-// 
-// 
-         BlockDiagonalPreconditioner prec(offsets);
-         prec.SetDiagonalBlock(0,new DSmoother(A00));
-#ifndef MFEM_USE_SUITESPARSE
-         prec.SetDiagonalBlock(1,new GSSmoother(*S));
-#else
-         prec.SetDiagonalBlock(1,new UMFPackSolver(*S));
-#endif
-         prec.owns_blocks = 1;
-
-         BlockOperator A(offsets);
-         // BlockMatrix A(offsets); 
+         BlockMatrix A(offsets); 
          A.SetBlock(0,0,&A00);
          A.SetBlock(1,0,&A10);
          A.SetBlock(0,1,A01);
-         // A.SetBlock(0,1, &A01); 
-         // A.SetBlock(1, 0, A10); 
          A.SetBlock(1,1,&A11);
 
-         GMRES(A,prec,rhs,x,0,2000,500,1e-12,0.0);
-         delete S;
-
-         // SparseMatrix *A_mono = A.CreateMonolithic(); 
-         // UMFPackSolver umf(*A_mono); 
-         // umf.Mult(rhs, x); 
+         SparseMatrix *A_mono = A.CreateMonolithic(); 
+         UMFPackSolver umf(*A_mono); 
+         umf.Mult(rhs, x); 
 
          u_tmp -= u_gf;
          real_t Newton_update_size = u_tmp.ComputeL2Error(zero);
@@ -253,14 +221,17 @@ int main(int argc, char *argv[])
 
          // Damped Newton update
          psi_gf.Add(newton_scaling, delta_psi_gf);
-         a00.Update();
+         a11.Update();
 
          if (visualization)
          {
-            // GridFunction u_x(&L2fes); 
+            GridFunction p_vec(&L2fes); 
+            p_vec.ProjectCoefficient(p_vc); 
             // u_gf.GetNodalValues(u_x, 1);
-            sol_sock << "solution\n" << mesh << u_gf << "window_title 'Discrete solution '" << flush;
+            // sol_sock << "solution\n" << mesh << u_gf << "window_title 'Discrete solution '" << flush;
 
+            sol_sock << "solution\n" << mesh << p_vec << "window_title 'Discrete solution '" << flush;
+            
             // sol_sock << "solution\n" << mesh << psi_gf << "window_title 'Discrete solution'"
                      // << flush;
          }
@@ -302,15 +273,25 @@ int main(int argc, char *argv[])
       double x_val = x(0);
       double y_val = x(1);
       // double val = x_val * y_val * (1 - x_val) * (1 - y_val);
+      
+
       u(0) = x_val * (1. - x_val);
       u(1) = y_val * (1. - y_val);
+
+      // u(0) = 1.; 
+      // u(1) = 1.; 
+
+      // u(0) = 2. * x_val - 1. ; 
+      // u(1) = 2. * y_val - 1.; 
+      
+      // u(0) = 4. * (1 - 2 * x_val) * (y_val - pow(y_val, 2)); 
+      // u(1) = 4. * (x_val - pow(x_val, 2))*(1. - 2.*y_val); 
    });
 
    double l2_error = u_gf.ComputeL2Error(exact_coeff); 
    cout << "L2 error: " << l2_error << endl; 
 
    delete A01;
-   // delete A10; 
    return 0;
 }
 
