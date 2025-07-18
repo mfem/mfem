3d5e23d933594d195100bb9951f41f2fa842dd2b
camierjs <camierjs@gmail.com>
2025-07-18 11:55:21 -0700

    Add glvis visualization

--- miniapps/mtop/test_iso_elast.cpp
+++ miniapps/mtop/test_iso_elast.cpp
@@ -10,6 +10,7 @@
 // CONTRIBUTING.md for details.
 
 #include <iostream>
+#include <memory>
 
 #include "mtop_solvers.hpp"
 
@@ -21,16 +22,14 @@ int main(int argc, char *argv[])
    // 1. Initialize MPI and HYPRE.
    Mpi::Init();
    Hypre::Init();
-   const int myid = Mpi::WorldRank();
 
    // 2. Parse command-line options.
    const char *mesh_file = "canti_2D_6.msh";
    int order = 2;
-   bool static_cond = false;
    bool pa = false;
-   bool fa = false;
    const char *device_config = "cpu";
    bool visualization = true;
+   bool paraview = true;
 
    OptionsParser args(argc, argv);
    args.AddOption(&mesh_file, "-m", "--mesh",
@@ -38,49 +37,40 @@ int main(int argc, char *argv[])
    args.AddOption(&order, "-o", "--order",
                   "Finite element order (polynomial degree) or -1 for"
                   " isoparametric space.");
-   args.AddOption(&static_cond, "-sc", "--static-condensation", "-no-sc",
-                  "--no-static-condensation", "Enable static condensation.");
    args.AddOption(&pa, "-pa", "--partial-assembly", "-no-pa",
                   "--no-partial-assembly", "Enable Partial Assembly.");
-   args.AddOption(&fa, "-fa", "--full-assembly", "-no-fa",
-                  "--no-full-assembly", "Enable Full Assembly.");
    args.AddOption(&device_config, "-d", "--device",
                   "Device configuration string, see Device::Configure().");
-
    args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
                   "--no-visualization",
                   "Enable or disable GLVis visualization.");
+   args.AddOption(&paraview, "-pv", "--paraview", "-no-pv",
+                  "--no-paraview", "Enable or not Paraview visualization");
    args.Parse();
    if (!args.Good())
    {
-      if (myid == 0)
-      {
-         args.PrintUsage(cout);
-      }
-      return 1;
+      if (Mpi::Root()) { args.PrintUsage(cout); }
+      return EXIT_FAILURE;
    }
-   if (myid == 0)
-   {
-      args.PrintOptions(cout);
-   }
+   if (Mpi::Root()) { args.PrintOptions(cout); }
 
    // 3. Enable hardware devices such as GPUs, and programming models such as
    //    CUDA, OCCA, RAJA and OpenMP based on command line options.
    Device device(device_config);
-   if (myid == 0) { device.Print(); }
+   if (Mpi::Root()) { device.Print(); }
 
    // 4. Read the (serial) mesh from the given mesh file on all processors.  We
    //    can handle triangular, quadrilateral, tetrahedral, hexahedral, surface
    //    and volume meshes with the same code.
    Mesh mesh(mesh_file, 1, 1);
-   int dim = mesh.Dimension();
+   const int dim = mesh.Dimension();
 
    // 5. Refine the serial mesh on all processors to increase the resolution. In
    //    this example we do 'ref_levels' of uniform refinement. We choose
    //    'ref_levels' to be the largest number that gives a final mesh with no
    //    more than 10,000 elements.
    {
-      int ref_levels =
+      const int ref_levels =
          (int)floor(log(1000./mesh.GetNE())/log(2.)/dim);
       for (int l = 0; l < ref_levels; l++)
       {
@@ -94,14 +84,14 @@ int main(int argc, char *argv[])
    ParMesh pmesh(MPI_COMM_WORLD, mesh);
    mesh.Clear();
    {
-      int par_ref_levels = 1;
+      const int par_ref_levels = 1;
       for (int l = 0; l < par_ref_levels; l++)
       {
          pmesh.UniformRefinement();
       }
    }
 
-   IsoLinElasticSolver* elsolver=new IsoLinElasticSolver(&pmesh,order);
+   auto elsolver = std::make_unique<IsoLinElasticSolver>(&pmesh, order);
 
    //set BC
    //elsolver->AddDispBC(2,4,0.0);
@@ -123,7 +113,6 @@ int main(int argc, char *argv[])
    ConstantCoefficient nu(0.2);
    elsolver->SetMaterial(E,nu);
 
-
    //set surface load
    elsolver->AddSurfLoad(1,0.0,1.0);
 
@@ -134,6 +123,7 @@ int main(int argc, char *argv[])
    //extract the solution
    ParGridFunction& sol=elsolver->GetDisplacements();
 
+   if (paraview)
    {
       ParaViewDataCollection paraview_dc("isoel", &pmesh);
       paraview_dc.SetPrefixPath("ParaView");
@@ -146,10 +136,19 @@ int main(int argc, char *argv[])
       paraview_dc.Save();
    }
 
-   delete elsolver;
+   if (visualization)
+   {
+      socketstream glvis;
+      glvis.open("localhost", 19916);
+      if (glvis.is_open())
+      {
+         glvis.precision(8);
+         glvis << "parallel " << Mpi::WorldSize() << " " << Mpi::WorldRank() << "\n";
+         glvis << "solution\n" << pmesh << sol << std::flush;
+      }
+   }
 
-   Mpi::Finalize();
-   return 0;
+   return EXIT_SUCCESS;
 }
 
 
