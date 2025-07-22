2951b5aa38f7949d6fbd3dae7800f5c4cecfbebf
Joseph Signorelli <jms26@illinois.edu>
2025-07-22 09:58:35 -0700

    Add Vector::DeleteAt w/ unit test

--- tests/unit/linalg/test_vector.cpp
+++ tests/unit/linalg/test_vector.cpp
@@ -247,3 +247,19 @@ TEST_CASE("Vector Sum", "[Vector],[CUDA]")
 
    REQUIRE(sum_1 == MFEM_Approx(sum_2));
 }
+
+TEST_CASE("Vector delete at indices", "[Vector]")
+{
+   Vector           test({0,1,2,3,4,5,6,7,8});
+   Array<int> rm_indices({0,    3,4,  6,  8});
+   Vector         result({  1,2,    5,  7  });
+
+   test.DeleteAt(rm_indices);
+
+   REQUIRE(test.Size() == result.Size());
+
+   for (int i = 0; i < test.Size(); i++)
+   {
+      CHECK(test[i] == result[i]);
+   }
+}
