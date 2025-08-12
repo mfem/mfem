a579e7f6c19d73352ae544168a747b8cc5e1248b
Toni-ko <kolshorn@pdx.edu>
2025-08-11 18:02:07 -0700

    attempting to resolve failing check

--- linalg/ode.hpp
+++ linalg/ode.hpp
@@ -955,10 +955,11 @@ public:
        - When the dimensions of the associated SplitTimeDependentOperator change.
        - When a time stepping sequence has to be restarted.
        - To change the associated SplitTimeDependentOperator. */
-   virtual void Init(SplitTimeDependentOperator &f);
+   virtual void Init(SplitTimeDependentOperator &f) override;
 
    static MFEM_EXPORT std::unique_ptr<SplitODESolver> Select(
       const int ode_solver_type);
+
    virtual ~SplitODESolver() { }
 };
 
