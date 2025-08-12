3372b2d1a86925412d9cc9f929b82d025784b643
Veselin Dobrev <dobrev@llnl.gov>
2025-08-12 14:06:52 -0700

    Proposed addtions and changes to PR #4028 (branch table-array)
    * Add mfem::swap for the classes Memory, Array, Array2D, and Vector.
    * These mfem::swap functions are for use by the standard library.
    * Re-define mfem::Swap to use mfem::swap, or, if is not defined, std::swap
    * Remove some calls to Memory::Reset after Memory::Delete since the latter
      calls the former
    * Update/improve the definitions of the Vector move-constructor and move-
      assignment; in the copy-assignment, skip the virtual calls to
      v.UseDevice(bool) when they are not needed.
    * In DenseMatrix::SetSize, update the size of the data Array to match
      height x width.
    * Propagate the parameter use_dev in Table::{ReadI,ReadJ} to the respective
      Array::Read calls.
    * In Table::Size_of_connections, return J.Size() instead of I[size].
    * Some small Doxygen tweaks.

--- general/mem_manager.hpp
+++ general/mem_manager.hpp
@@ -280,6 +280,10 @@ public:
    /** @note The destructor will NOT delete the current memory. */
    ~Memory() = default;
 
+   /// Swap without using move assignment, avoiding Reset() calls.
+   void Swap(Memory &other)
+   { Memory tmp(*this); *this = other; other = tmp; }
+
    /** @brief Return true if the host pointer is owned. Ownership indicates
        whether the pointer will be deleted by the method Delete(). */
    bool OwnsHostPtr() const { return flags & OWNS_HOST; }
@@ -599,6 +603,15 @@ private:
       return Alloc<new_align_bytes>::New(size);
    }
 };
+
+
+/** @brief Swap of Memory<T> objects for use with standard library algorithms.
+    Also, used by mfem::Swap(). */
+template <typename T>
+void swap(Memory<T> &a, Memory<T> &b)
+{
+   a.Swap(b);
+}
 
 
 /** The MFEM memory manager class. Host-side pointers are inserted into this
