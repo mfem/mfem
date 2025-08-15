f377c63ea7337a33ff6e8bcaf46e90ff02d05763
Joseph Signorelli <jms26@illinois.edu>
2025-08-15 10:40:56 -0700

    Add Particle class

--- fem/particleset.cpp
+++ fem/particleset.cpp
@@ -15,6 +15,94 @@
 namespace mfem
 {
 
+Particle::Particle(int dim, const Array<int> &field_vdims, int num_tags)
+   : coords(dim),
+     fields(),
+     tags()
+{
+   coords = 0.0;
+
+   for (int f = 0; f < field_vdims.Size(); f++)
+   {
+      fields.emplace_back(field_vdims[f]);
+      fields.back() = 0.0;
+   }
+
+   for (int t = 0; t < num_tags; t++)
+   {
+      tags.emplace_back(1);
+      tags.back()[0] = 0;
+   }
+
+}
+
+bool Particle::operator==(const Particle &rhs) const
+{
+   if (coords.Size() != rhs.coords.Size())
+   {
+      return false;
+   }
+   for (int d = 0; d < coords.Size(); d++)
+   {
+      if (coords[d] != rhs.coords[d])
+      {
+         return false;
+      }
+   }
+
+   if (fields.size() != rhs.fields.size())
+   {
+      return false;
+   }
+   for (int f = 0; f < fields.size(); f++)
+   {
+      if (fields[f].Size() != rhs.fields[f].Size())
+      {
+         return false;
+      }
+      for (int c = 0; c < fields[f].Size(); c++)
+      {
+         if (fields[f][c] != rhs.fields[f][c])
+         {
+            return false;
+         }
+      }
+   }
+   if (tags.size() != rhs.tags.size())
+   {
+      return false;
+   }
+   for (int t = 0; t < tags.size(); t++)
+   {
+      if (tags[t][0] != rhs.tags[t][0])
+      {
+         return false;
+      }
+   }
+
+   return true;
+}
+
+void Particle::Print(std::ostream &out) const
+{
+   out << "Coords: (";
+   for (int d = 0; d < coords.Size(); d++)
+   {
+      out << coords[d] << ( (d+1 < coords.Size()) ? "," : ")\n");
+   }
+   for (int f = 0; f < fields.size(); f++)
+   {
+      out << "Field " << f << ": (";
+      for (int c = 0; c < fields[f].Size(); c++)
+      {
+         out << fields[f][c] << ( (c+1 < fields[f].Size()) ? "," : ")\n");
+      }
+   }
+   for (int t = 0; t < tags.size(); t++)
+   {
+      out << "Tag " << t << ": " << tags[t][0] << "\n";
+   }
+}
 
 
 } // namespace mfem
--- fem/particleset.hpp
+++ fem/particleset.hpp
@@ -12,10 +12,78 @@
 #ifndef MFEM_PARTICLESET
 #define MFEM_PARTICLESET
 
+#include "../config/config.hpp"
+#include "../linalg/linalg.hpp"
+
 
 namespace mfem
 {
 
+
+/// Container for data associated with a single particle. See \ref ParticleSet for more information.
+class Particle
+{
+protected:
+   Vector coords;
+   std::vector<Vector> fields;
+   std::vector<Memory<int>> tags;
+public:
+
+   /** @brief Construct a Particle instance
+    *  @param[in] dim          Spatial dimension.
+    *  @param[in] field_vdims  Vector dimensions of particle fields.
+    *  @param[in] num_tags     Number of integer tags.
+    */
+   Particle(int dim, const Array<int> &field_vdims, int num_tags);
+
+   /// Get the spatial dimension of this particle.
+   int GetDim() const { return coords.Size(); }
+
+   /// Get the number of fields associated with this particle.
+   int GetNF() const { return fields.size(); }
+
+   /// Get the vector dimension of field \p f .
+   int FieldVDim(int f) const { return fields[f].Size(); }
+
+   /// Get the number of tags associated with this particle.
+   int GetNT() const { return tags.size(); }
+
+   /// Get reference to particle coordinates Vector.
+   Vector& Coords() { return coords; }
+
+   /// Get const reference to particle coordinates Vector.
+   const Vector& Coords() const { return coords; }
+
+   /// Get reference to field \p f , component \p c value.
+   real_t& FieldValue(int f, int c=0) { return fields[f][c]; }
+
+   /// Get const reference to field \p f , component \p c value.
+   const real_t& FieldValue(int f, int c=0) const { return fields[f][c]; }
+
+   /// Get reference to field \p f Vector.
+   Vector& Field(int f) { return fields[f]; }
+
+   /// Get const reference to field \p f Vector.
+   const Vector& Field(int f) const { return fields[f]; }
+
+   /// Get reference to tag \p t .
+   int& Tag(int t) { return tags[t][0]; }
+
+   /// Get const reference to tag \p t .
+   const int& Tag(int t) const { return tags[t][0]; }
+
+   /// Get reference to underlying memory object of tag \p t .
+   Memory<int>& TagMemory(int t) { return tags[t]; }
+
+   /// Particle equality operator.
+   bool operator==(const Particle &rhs) const;
+
+   bool operator!=(const Particle &rhs) const { return !operator==(rhs); }
+
+   /// Print all particle data to \p out .
+   void Print(std::ostream &out=mfem::out) const;
+
+};
 
 
 } // namespace mfem
