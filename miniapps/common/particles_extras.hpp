0d0c02b715dc44591064fb21ce2f72d40a3b37cd
Joseph Signorelli <jms26@illinois.edu>
2025-08-15 11:12:34 -0700

    Add miniapp common particle functions + ParticleTrajectories class

--- miniapps/common/particles_extras.hpp
+++ miniapps/common/particles_extras.hpp
@@ -19,6 +19,49 @@ namespace mfem
 namespace common
 {
 
+/// Add a point to a given Mesh, represented as a hex sized \p scale
+void Add3DPoint(const Vector &center, Mesh &m, real_t scale=2e-3);
+
+/// Plot a point cloud of particles, represented as hexes, colored by \p scalar_field
+void VisualizeParticles(socketstream &sock, const char* vishost, int visport,
+                        const ParticleSet &pset, const Vector &scalar_field, real_t psize,
+                        const char* title, int x = 0, int y = 0, int w = 400, int h = 400,
+                        const char* keys=nullptr);
+
+/// Helper class for easily visualizing particle trajectories using GLVis
+class ParticleTrajectories
+{
+protected:
+
+   const ParticleSet &pset;
+
+   socketstream sock;
+   std::vector<Array<unsigned int>>
+                                 segment_ids; /// Track particle IDs that exist at the segment start.
+   std::vector<Mesh> segment_meshes; /// Each segment is stored as a Mesh snapshot
+
+   int tail_size;
+   int x, y, w, h;
+   const char *title, *keys;
+
+   bool newly_opened;
+#ifdef MFEM_USE_MPI
+   MPI_Comm comm;
+#endif // MFEM_USE_MPI
+
+   void AddSegmentStart();
+
+   void SetSegmentEnd();
+
+public:
+
+   ParticleTrajectories(const ParticleSet &particles, int tail_size_,
+                        const char *vishost, int visport, const char *title_, int x_=0, int y_=0,
+                        int w_=400, int h_=400, const char *keys_=nullptr);
+
+   void Visualize();
+
+};
 
 
 } // namespace common
