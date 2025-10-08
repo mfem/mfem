02dfb6319fb8c85ca8061b03787738721bd96ea7
Ido Akkerman <I.Akkerman@tudelft.nl>
2025-10-08 13:10:48 +0200

    Rename projection types3 + change timestep

--- miniapps/nurbs/nurbs_ex10.cpp
+++ miniapps/nurbs/nurbs_ex10.cpp
@@ -3,12 +3,12 @@
 // Compile with: make nurbs_ex10
 //
 // Sample runs:
-//    nurbs_ex10 -m ../../data/beam-quad-nurbs.mesh -s 23 -r 2 -o 2 -dt 3
-//    nurbs_ex10 -m ../../data/beam-quad.mesh -s 23 -r 2 -o 2 -dt 3
-//    nurbs_ex10 -m ../../data/beam-tri.mesh -s 23 -r 2 -o 2 -dt 3
-//    nurbs_ex10 -m ../../data/beam-hex.mesh -s 22 -r 1 -o 2 -dt 3
-//    nurbs_ex10 -m ../../data/beam-tet.mesh -s 22 -r 1 -o 2 -dt 3
-//    nurbs_ex10 -m ../../data/beam-wedge.mesh -s 22 -r 1 -o 2 -dt 3
+//    nurbs_ex10 -m ../../data/beam-quad-nurbs.mesh -s 23 -r 2 -o 2 -dt 0.1
+//    nurbs_ex10 -m ../../data/beam-quad.mesh -s 23 -r 2 -o 2 -dt 0.1
+//    nurbs_ex10 -m ../../data/beam-tri.mesh -s 23 -r 2 -o 2 -dt 0.1
+//    nurbs_ex10 -m ../../data/beam-hex.mesh -s 22 -r 1 -o 2 -dt 0.1
+//    nurbs_ex10 -m ../../data/beam-tet.mesh -s 22 -r 1 -o 2 -dt 0.1
+//    nurbs_ex10 -m ../../data/beam-wedge.mesh -s 22 -r 1 -o 2 -dt 0.1
 //    nurbs_ex10 -m ../../data/beam-quad.mesh -s 4 -r 2 -o 2 -dt 0.03 -vs 20
 //    nurbs_ex10 -m ../../data/beam-hex.mesh -s 4 -r 1 -o 2 -dt 0.05 -vs 20
 //    nurbs_ex10 -m ../../data/beam-quad-amr.mesh -s 23 -r 2 -o 2 -dt 3
@@ -43,8 +43,6 @@
 
 using namespace std;
 using namespace mfem;
-
-typedef GridFunction::ProjType ProjType;
 
 class ReducedSystemOperator;
 
@@ -99,7 +97,7 @@ public:
    real_t KineticEnergy(const Vector &v) const;
    void GetElasticEnergyDensity(const GridFunction &x,
                                 GridFunction &w,
-                                ProjType proj_type) const;
+                                ProjectType proj_type) const;
 
    ~HyperelasticOperator() override;
 };
@@ -213,7 +211,7 @@ int main(int argc, char *argv[])
    }
    args.PrintOptions(cout);
 
-   ProjType proj_type = static_cast<ProjType>(proj_type_int);
+   ProjectType proj_type = static_cast<ProjectType>(proj_type_int);
 
    // 2. Read the mesh from the given mesh file. We can handle triangular,
    //    quadrilateral, tetrahedral and hexahedral meshes with the same code.
@@ -580,7 +578,7 @@ real_t HyperelasticOperator::KineticEnergy(const Vector &v) const
 }
 
 void HyperelasticOperator::GetElasticEnergyDensity(
-   const GridFunction &x, GridFunction &w, ProjType proj_type) const
+   const GridFunction &x, GridFunction &w, ProjectType proj_type) const
 {
    ElasticEnergyCoefficient w_coeff(*model, x);
    w.ProjectCoefficient(w_coeff, proj_type);
