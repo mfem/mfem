5f9f782ad225c98994c6e97dea7c94c5d0566cda
Sohail Reddy <sredd001@fiu.edu>
2025-10-14 17:01:51 -0700

    fixed style

--- linalg/solvers.cpp
+++ linalg/solvers.cpp
@@ -753,7 +753,8 @@ void FPISolver::SetOperator(const Operator &op)
    relax_method->Init(*oper);
 }
 
-void FPISolver::SetRelaxation(real_t rfactor, FPIRelaxation *relaxation, bool own)
+void FPISolver::SetRelaxation(real_t rfactor, FPIRelaxation *relaxation,
+                              bool own)
 {
    relax_factor = rfactor;
    if (relaxation)
