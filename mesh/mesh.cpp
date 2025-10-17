e9ca0960f74b2e69d3f4026a832358bbe97142ee
Will Pazner <will.e.p@gmail.com>
2025-10-17 09:15:45 -0700

    Add fix for Mesh::MakeHigherOrderSimplicial_ on surface meshes

--- mesh/mesh.cpp
+++ mesh/mesh.cpp
@@ -6000,17 +6000,17 @@ void Mesh::MakeHigherOrderSimplicial_(const Mesh &orig_mesh,
                   child_nodes_in_parent[0].x +
                   simplex_node.x * (child_nodes_in_parent[1].x - child_nodes_in_parent[0].x)
                   + simplex_node.y * (child_nodes_in_parent[2].x - child_nodes_in_parent[0].x)
-                  + simplex_node.z * (child_nodes_in_parent[(sdim > 2) ? 3 : 0].x -
+                  + simplex_node.z * (child_nodes_in_parent[(Dim > 2) ? 3 : 0].x -
                                       child_nodes_in_parent[0].x),
                   child_nodes_in_parent[0].y +
                   simplex_node.x * (child_nodes_in_parent[1].y - child_nodes_in_parent[0].y)
                   + simplex_node.y * (child_nodes_in_parent[2].y - child_nodes_in_parent[0].y)
-                  + simplex_node.z * (child_nodes_in_parent[(sdim > 2) ? 3 : 0].y -
+                  + simplex_node.z * (child_nodes_in_parent[(Dim > 2) ? 3 : 0].y -
                                       child_nodes_in_parent[0].y),
                   child_nodes_in_parent[0].z +
                   simplex_node.x * (child_nodes_in_parent[1].z - child_nodes_in_parent[0].z)
                   + simplex_node.y * (child_nodes_in_parent[2].z - child_nodes_in_parent[0].z)
-                  + simplex_node.z * (child_nodes_in_parent[(sdim > 2) ? 3 : 0].z -
+                  + simplex_node.z * (child_nodes_in_parent[(Dim > 2) ? 3 : 0].z -
                                       child_nodes_in_parent[0].z));
                shape.GetColumnReference(j, col);
                orig_FE->CalcShape(simplex_node_in_orig, col);
