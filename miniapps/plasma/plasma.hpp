8e713bfc10732ff93ec5f3042057da16ae6fa3a5
Stowell, Mark L. <stowell1@llnl.gov>
2025-10-20 16:25:14 -0700

    Switching to real_t

--- miniapps/plasma/plasma.hpp
+++ miniapps/plasma/plasma.hpp
@@ -24,22 +24,22 @@ namespace plasma
 // Physical Constants
 
 // Permittivity of Free Space (units F/m)
-static const double epsilon0_ = 8.8541878176e-12;
+static const real_t epsilon0_ = 8.8541878176e-12;
 
 // Permeability of Free Space (units H/m)
-static const double mu0_ = 4.0e-7 * M_PI;
+static const real_t mu0_ = 4.0e-7 * M_PI;
 
 // Speed of light in Free Space (units m/s)
-static const double c0_ = 1.0 / sqrt(epsilon0_ * mu0_);
+static const real_t c0_ = 1.0 / sqrt(epsilon0_ * mu0_);
 
 // Impedance of Free Space (units Ohm)
-static const double Z0_ = sqrt(mu0_ / epsilon0_);
+static const real_t Z0_ = sqrt(mu0_ / epsilon0_);
 
-static const double q_     = 1.602176634e-19; // Elementary charge in coulombs
-static const double eV_    = 1.602176634e-19; // 1 eV in Joules
-static const double amu_   = 1.660539040e-27; // Atomic mass unit in kilograms
-static const double me_kg_ = 9.10938356e-31;  // Mass of electron in kilograms
-static const double me_u_  = 5.4857990907e-4; // Mass of electron in a.m.u
+static const real_t q_     = 1.602176634e-19; // Elementary charge in coulombs
+static const real_t eV_    = 1.602176634e-19; // 1 eV in Joules
+static const real_t amu_   = 1.660539040e-27; // Atomic mass unit in kilograms
+static const real_t me_kg_ = 9.10938356e-31;  // Mass of electron in kilograms
+static const real_t me_u_  = 5.4857990907e-4; // Mass of electron in a.m.u
 
 /**
    Returns the cyclotron frequency in radians/second
@@ -47,7 +47,7 @@ static const double me_u_  = 5.4857990907e-4; // Mass of electron in a.m.u
    q is the charge in units of elementary electric charge
    B is the magnetic field magnitude in tesla
  */
-inline double cyclotronFrequency(double B, double m, double q)
+inline real_t cyclotronFrequency(real_t B, real_t m, real_t q)
 {
    return fabs(q * q_ * B / (m * amu_));
 }
