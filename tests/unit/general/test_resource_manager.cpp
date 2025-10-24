0c9ca1a2f5cb8e90d878dcba109201229297799d
Andrew Ho <ho37@llnl.gov>
2025-10-23 19:00:42 -0700

    unused var

--- tests/unit/general/test_resource_manager.cpp
+++ tests/unit/general/test_resource_manager.cpp
@@ -53,68 +53,65 @@ TEST_CASE("Resource Creation", "[Resource Manager]")
 
 TEST_CASE("Resource Aliasing", "[Resource Manager][GPU]")
 {
-   auto &inst = ResourceManager::instance();
+   Resource<int> tmp(100, ResourceManager::HOST, false);
+   auto hptr = tmp.HostWrite();
+   REQUIRE(hptr != nullptr);
+   for (int i = 0; i < 100; ++i)
    {
-      Resource<int> tmp(100, ResourceManager::HOST, false);
-      auto hptr = tmp.HostWrite();
-      REQUIRE(hptr != nullptr);
+      tmp[i] = i;
+   }
+   // [5, 10)
+   Resource<int> alias0 = tmp.CreateAlias(5, 5);
+   // [8, 19)
+   Resource<int> alias1 = tmp.CreateAlias(8, 11);
+   // [50, 55)
+   Resource<int> alias2 = tmp.CreateAlias(50, 5);
+   {
+      auto ptr = alias0.Write(true);
+      REQUIRE(ptr != nullptr);
+      if (!alias0.ZeroCopy())
+      {
+         REQUIRE(ptr != hptr);
+      }
+      forall(5, [=] MFEM_HOST_DEVICE(int i) { ptr[i] = 0; });
+   }
+   {
+      auto ptr = alias1.Write(true);
+      REQUIRE(ptr != nullptr);
+      if (!alias1.ZeroCopy())
+      {
+         REQUIRE(ptr != hptr);
+      }
+      forall(11, [=] MFEM_HOST_DEVICE(int i) { ptr[i] = 1; });
+   }
+   {
+      auto ptr = alias2.Write(true);
+      REQUIRE(ptr != nullptr);
+      if (!alias1.ZeroCopy())
+      {
+         REQUIRE(ptr != hptr);
+      }
+      forall(5, [=] MFEM_HOST_DEVICE(int i) { ptr[i] = 2; });
+   }
+   {
+      tmp.HostRead();
       for (int i = 0; i < 100; ++i)
       {
-         tmp[i] = i;
-      }
-      // [5, 10)
-      Resource<int> alias0 = tmp.CreateAlias(5, 5);
-      // [8, 19)
-      Resource<int> alias1 = tmp.CreateAlias(8, 11);
-      // [50, 55)
-      Resource<int> alias2 = tmp.CreateAlias(50, 5);
-      {
-         auto ptr = alias0.Write(true);
-         REQUIRE(ptr != nullptr);
-         if (!alias0.ZeroCopy())
+         if (i >= 5 && i < 8)
          {
-            REQUIRE(ptr != hptr);
+            REQUIRE(tmp[i] == 0);
          }
-         forall(5, [=] MFEM_HOST_DEVICE(int i) { ptr[i] = 0; });
-      }
-      {
-         auto ptr = alias1.Write(true);
-         REQUIRE(ptr != nullptr);
-         if (!alias1.ZeroCopy())
+         else if (i >= 8 && i < 19)
          {
-            REQUIRE(ptr != hptr);
+            REQUIRE(tmp[i] == 1);
          }
-         forall(11, [=] MFEM_HOST_DEVICE(int i) { ptr[i] = 1; });
-      }
-      {
-         auto ptr = alias2.Write(true);
-         REQUIRE(ptr != nullptr);
-         if (!alias1.ZeroCopy())
+         else if (i >= 50 && i < 55)
          {
-            REQUIRE(ptr != hptr);
+            REQUIRE(tmp[i] == 2);
          }
-         forall(5, [=] MFEM_HOST_DEVICE(int i) { ptr[i] = 2; });
-      }
-      {
-         tmp.HostRead();
-         for (int i = 0; i < 100; ++i)
+         else
          {
-            if (i >= 5 && i < 8)
-            {
-               REQUIRE(tmp[i] == 0);
-            }
-            else if (i >= 8 && i < 19)
-            {
-               REQUIRE(tmp[i] == 1);
-            }
-            else if (i >= 50 && i < 55)
-            {
-               REQUIRE(tmp[i] == 2);
-            }
-            else
-            {
-               REQUIRE(tmp[i] == i);
-            }
+            REQUIRE(tmp[i] == i);
          }
       }
    }
@@ -122,43 +119,40 @@ TEST_CASE("Resource Aliasing", "[Resource Manager][GPU]")
 
 TEST_CASE("Resource Copy", "[Resource Manager][GPU]")
 {
-   auto &inst = ResourceManager::instance();
-   {
-      Resource<char> tmp0(100, ResourceManager::HOST, false);
-      Resource<char> tmp1(100, ResourceManager::HOST, false);
+   Resource<char> tmp0(100, ResourceManager::HOST, false);
+   Resource<char> tmp1(100, ResourceManager::HOST, false);
 
-      for (int i = 0; i < 100; ++i)
+   for (int i = 0; i < 100; ++i)
+   {
+      tmp0[i] = i;
+   }
+   // put a few parts of tmp0 onto device
+   // [5, 10)
+   {
+      auto dptr = tmp0.CreateAlias(5, 5).Write();
+      forall(5, [=] MFEM_HOST_DEVICE(int i) { dptr[i] = -2; });
+   }
+   // [15, 20)
+   {
+      auto dptr = tmp0.CreateAlias(15, 5).Write();
+      forall(5, [=] MFEM_HOST_DEVICE(int i) { dptr[i] = -3; });
+   }
+   tmp1.CopyFrom(tmp0, tmp0.Capacity());
+   MFEM_DEVICE_SYNC;
+   auto hptr = tmp1.HostRead();
+   for (int i = 0; i < 100; ++i)
+   {
+      if (i >= 5 && i < 10)
       {
-         tmp0[i] = i;
+         REQUIRE(hptr[i] == -2);
       }
-      // put a few parts of tmp0 onto device
-      // [5, 10)
+      else if (i >= 15 && i < 20)
       {
-         auto dptr = tmp0.CreateAlias(5, 5).Write();
-         forall(5, [=] MFEM_HOST_DEVICE(int i) { dptr[i] = -2; });
+         REQUIRE(hptr[i] == -3);
       }
-      // [15, 20)
+      else
       {
-         auto dptr = tmp0.CreateAlias(15, 5).Write();
-         forall(5, [=] MFEM_HOST_DEVICE(int i) { dptr[i] = -3; });
-      }
-      tmp1.CopyFrom(tmp0, tmp0.Capacity());
-      MFEM_DEVICE_SYNC;
-      auto hptr = tmp1.HostRead();
-      for (int i = 0; i < 100; ++i)
-      {
-         if (i >= 5 && i < 10)
-         {
-            REQUIRE(hptr[i] == -2);
-         }
-         else if (i >= 15 && i < 20)
-         {
-            REQUIRE(hptr[i] == -3);
-         }
-         else
-         {
-            REQUIRE(hptr[i] == i);
-         }
+         REQUIRE(hptr[i] == i);
       }
    }
 }
