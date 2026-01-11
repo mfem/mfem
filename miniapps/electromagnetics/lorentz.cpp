bc0ec2e71765795f40c1945bbf85c41c3f0704d2
Mittal, Ketan <mittal3@llnl.gov>
2026-01-11 12:09:01 -0800

    add another sample run

--- miniapps/electromagnetics/lorentz.cpp
+++ miniapps/electromagnetics/lorentz.cpp
@@ -52,7 +52,7 @@
 //      mpirun -np 4 volta -m ../../data/ball-nurbs.mesh -dbcs 1 -cs '0 0 0 0.1 2e-11' -rs 2 -maxit 4
 //      mpirun -np 4 tesla -m ../../data/fichera.mesh -maxit 4 -rs 3 -bm '-0.1 -0.1 -0.1 0.1 0.1 0.1 0.1 -1e10'
 //      mpirun -np 4 lorentz -er Volta-AMR-Parallel -ec 4 -br Tesla-AMR-Parallel -bc 4 -q -10 -dt 1e-3 -nt 1000 -npt 500 -vt 5 -rdf 500 -rdm 1 -vf 5 -pmin '-8 -4 4' -pmax '-8 -4 4' -xmin '-1 -1 -1' -xmax '1 1 1'
-
+//      mpirun -np 4 lorentz -er Volta-AMR-Parallel -ec 4 -br Tesla-AMR-Parallel -bc 4 -q -10 -dt 1e-3 -npt 1 -vt 650 -rdf 500 -rdm 1 -vf 2 -pmin '-8 -4 4' -pmax '-8 -4 4' -xmin '0.8 0 0' -xmax '0.8 0 0' -nt 1300
 
 #include "mfem.hpp"
 #include "../common/particles_extras.hpp"
@@ -349,7 +349,7 @@ int main(int argc, char *argv[])
           boris.GetParticles().GetGlobalNParticles() > 0)
       {
          // Visualize particles pre-redistribute
-         if (ctx.visualization)
+         if (ctx.visualization && boris.GetParticles().GetGlobalNParticles()>1)
          {
             Vector rank_vector(boris.GetParticles().GetNParticles());
             rank_vector = Mpi::WorldRank();
@@ -365,7 +365,7 @@ int main(int argc, char *argv[])
          boris.Redistribute(ctx.redist_mesh, removed_idxs);
 
          // Visualize particles post-redistribute
-         if (ctx.visualization)
+         if (ctx.visualization && boris.GetParticles().GetGlobalNParticles()>1)
          {
             Vector rank_vector(boris.GetParticles().GetNParticles());
             rank_vector = Mpi::WorldRank();
