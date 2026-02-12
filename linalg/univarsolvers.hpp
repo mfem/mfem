303ea9da5620a8a1ed30a985ff1a3b2bf9d7e3da
Brandon Talamini <talamini1@llnl.gov>
2026-02-12 14:39:06 -0800

    Fix bugs to make tests pass

--- linalg/univarsolvers.hpp
+++ linalg/univarsolvers.hpp
@@ -18,8 +18,12 @@
 #ifndef MFEM_UNIVARSOLVERS
 #define MFEM_UNIVARSOLVERS
 
+#include <cmath>
+#include <limits>
+
 #include "../config/config.hpp"
-#include "../general/enzyme.hpp"
+#include "general/enzyme.hpp"
+#include "general/error.hpp"
 
 #ifdef MFEM_USE_ENZYME
 
@@ -38,103 +42,108 @@ struct SolverSettings {
   Bounds bounds{.lower = -std::numeric_limits<real_t>::infinity(), .upper = std::numeric_limits<real_t>::infinity()}; ///< Bounds on root
 };
 
+/**
+ * @brief Implementation detail
+ */
 template <auto f, typename T>
 __attribute__((noinline))
-MFEM_HOST_DEVICE void NewtonBisection_impl(const real_t* x0_ptr, const T* p_ptr, const SolverSettings* settings_ptr, real_t* x_ptr)
+MFEM_HOST_DEVICE void SolveNewtonBisection_impl(const real_t* x0_ptr, const T* p_ptr, const SolverSettings* settings_ptr, real_t* x_ptr)
 {
-  constexpr int max_iters = 50;
+    // It would be better to have the max iterations in the settings instead of
+    // hard-coded.
+    constexpr int max_iters = 50;
 
-  const real_t& x0 = *x0_ptr;
-  const T& p = *p_ptr;
-  const SolverSettings& settings = *settings_ptr;
-  const real_t& left_bracket = settings.bounds.lower;
-  const real_t& right_bracket = settings.bounds.upper;
-  real_t& x = *x_ptr;
-  using std::abs;
+    const real_t& x0 = *x0_ptr;
+    const T& p = *p_ptr;
+    const SolverSettings& settings = *settings_ptr;
+    const real_t& left_bracket = settings.bounds.lower;
+    const real_t& right_bracket = settings.bounds.upper;
+    real_t& x = *x_ptr;
+    using std::abs;
 
-  auto fprime = [&p](real_t x) {
-    real_t x_dot = 1.0;
-    return __enzyme_fwddiff<real_t>((void*)+f, enzyme_dup, x, x_dot, enzyme_const, p);
-  };
+    auto fprime = [&p](real_t x) {
+        real_t x_dot = 1.0;
+        return __enzyme_fwddiff<real_t>((void*)+f, enzyme_dup, x, x_dot, enzyme_const, p);
+    };
 
-  real_t fl = f(left_bracket, p);
-  real_t fh = f(right_bracket, p);
+    real_t fl = f(left_bracket, p);
+    real_t fh = f(right_bracket, p);
 
-   // handle corner cases where one of the brackets is the root
-  if (abs(fl) < settings.residual_abs_tol) {
-    x = left_bracket;
-    return;
-  } else if (abs(fh) < settings.residual_abs_tol) {
-    x = right_bracket;
-    return;
-  }
-
-  MFEM_ASSERT(fl * fh < 0.0, "Root is not bracketed, solve cannot continue.");
-  MFEM_ASSERT(x0 >= left_bracket && x0 <= right_bracket, "Initial guess must be within bounds.");
-
-  // Orient search so that f(xl) < 0
-  real_t xl = left_bracket;
-  real_t xh = right_bracket;
-  if (fl > 0.0) {
-    xl = right_bracket;
-    xh = left_bracket;
-    real_t tmp = fl;
-    fl = fh;
-    fh = tmp;
-  }
-
-  real_t dx_old = abs(right_bracket - left_bracket);
-  real_t dx = dx_old;
-  x = x0;
-  real_t r = f(x, p);
-  real_t dr_dx = fprime(x);
-  real_t r_old = r;
-  for (int i = 0; i < max_iters; i++) {
-    if ((((x - xh) * dr_dx - r)*((x - xl)*dr_dx - r) >= 0.0) || // Newton out of range
-        (std::abs(2.0*r) > std::abs(dx_old*dr_dx))) {           // Newton decreasing bracket slower than bisection
-      // Take bisection step
-      dx_old = dx;
-      dx = 0.5*(xh - xl);
-      real_t x_old = x;
-      x = xl + dx;
-      if (x == x_old) return;
-    } else {
-      // Take Newton step
-      dx_old = dx;
-      dx = -r/dr_dx;
-      real_t x_old = x;
-      x += dx;
-      if (x == x_old) return;
+    // handle corner cases where one of the brackets is the root
+    if (abs(fl) < settings.residual_abs_tol) {
+        x = left_bracket;
+        return;
+    } else if (abs(fh) < settings.residual_abs_tol) {
+        x = right_bracket;
+        return;
     }
 
-    // update residual and jacobian
-    r = f(x, p);
-    dr_dx = fprime(x);
+    MFEM_ASSERT(fl * fh < 0.0, "Root is not bracketed, solve cannot continue.");
+    MFEM_ASSERT(x0 >= left_bracket && x0 <= right_bracket, "Initial guess must be within bounds.");
 
-    // Check convergence
-    if (abs(r) < settings.residual_rel_tol*r_old || abs(r) < settings.residual_abs_tol) {
-      return;
+    // Orient search so that f(xl) < 0
+    real_t xl = left_bracket;
+    real_t xh = right_bracket;
+    if (fl > 0.0) {
+        xl = right_bracket;
+        xh = left_bracket;
+        real_t tmp = fl;
+        fl = fh;
+        fh = tmp;
     }
 
-    // Update bracket
-    if (r < 0.0) {
-      xl = x;
-      fl = r;
-    } else {
-      xh = x;
-      fh = r;
+    real_t dx_old = abs(right_bracket - left_bracket);
+    real_t dx = dx_old;
+    x = x0;
+    real_t r = f(x, p);
+    real_t dr_dx = fprime(x);
+    real_t r_old = r;
+    for (int i = 0; i < max_iters; i++) {
+        if ((((x - xh) * dr_dx - r)*((x - xl)*dr_dx - r) >= 0.0) ||  // Newton out of range
+            (std::abs(2.0*r) > std::abs(dx_old*dr_dx))) {            // Newton decreasing bracket slower than bisection
+            // Take bisection step
+            dx_old = dx;
+            dx = 0.5*(xh - xl);
+            real_t x_old = x;
+            x = xl + dx;
+            if (x == x_old) return;
+        } else {
+            // Take Newton step
+            dx_old = dx;
+            dx = -r/dr_dx;
+            real_t x_old = x;
+            x += dx;
+            if (x == x_old) return;
+        }
+
+        // update residual and jacobian
+        r = f(x, p);
+        dr_dx = fprime(x);
+
+        // Check convergence
+        if (abs(r) < settings.residual_rel_tol*r_old || abs(r) < settings.residual_abs_tol) {
+            return;
+        }
+
+        // Update bracket
+        if (r < 0.0) {
+            xl = x;
+            fl = r;
+        } else {
+            xh = x;
+            fh = r;
+        }
     }
-  }
-  mfem_error("Newton solve did note converge.");
+    mfem_error("Univariate solve did not converge.");
 }
 
 template <auto f, typename T>
-void NewtonBisection_impl_fwddiff(const real_t* x0, const real_t* /* unused shadow */,
-                                   const T* p, const T* dp,
-                                   const SolverSettings* settings, const SolverSettings* /* unused shadow */,
-                                   real_t* x, real_t* dx)
+void SolveNewtonBisection_impl_fwddiff(const real_t* x0, const real_t* /* unused shadow */,
+                                       const T* p, const T* dp,
+                                       const SolverSettings* settings, const SolverSettings* /* unused shadow */,
+                                       real_t* x, real_t* dx)
 {
-  NewtonBisection_impl<f>(x0, p, settings, x);
+  SolveNewtonBisection_impl<f>(x0, p, settings, x);
   real_t dfdx = __enzyme_fwddiff<real_t>((void*)+f, enzyme_dup, *x, 1.0, enzyme_const, *p);
   real_t dfdp = __enzyme_fwddiff<real_t>((void*)+f, enzyme_const, *x, enzyme_dup, *p, *dp);
   *dx = -dfdp/dfdx;
@@ -146,7 +155,7 @@ void NewtonBisection_impl_fwddiff(const real_t* x0, const real_t* /* unused shad
 template<auto f, typename T>
 MFEM_HOST_DEVICE real_t SolveNewtonBisection(real_t x0, T p, SolverSettings settings) {
   real_t x;
-  NewtonBisection_impl<f>(&x0, &p, &settings, &x);
+  SolveNewtonBisection_impl<f>(&x0, &p, &settings, &x);
   return x;
 }
 
--- tests/unit/linalg/test_univarsolver.cpp
+++ tests/unit/linalg/test_univarsolver.cpp
@@ -17,6 +17,121 @@
 using mfem::real_t;
 using namespace mfem::future;
 
+MFEM_HOST_DEVICE inline real_t FlowResistance(real_t eqps, real_t sigma_y, real_t n, real_t ep_0)
+{
+    return sigma_y*(1.0 + std::pow(eqps/ep_0, n));
+}
+
+using J2PlasticityParameters = mfem::future::tuple<real_t, real_t, real_t, real_t, real_t, real_t>;
+
+real_t J2PlasticityResidual(real_t delta_eqps, J2PlasticityParameters p)
+{
+    auto [eqps, q, G, sigma_y, n, ep_0] = p;
+    return q - 3.0*G*delta_eqps - FlowResistance(eqps + delta_eqps, sigma_y, n, ep_0);
+}
+
+struct J2Plasticity {
+  static constexpr int dim = 3;  ///< spatial dimension
+  static constexpr int N_INTERNAL_STATES = 10;
+  static constexpr real_t tol = 1e-10;  ///< relative tolerance on residual mag to judge convergence of return map
+
+  real_t E;        ///< Young's modulus
+  real_t nu;       ///< Poisson's ratio
+  real_t sigma_y;  ///< Yield strength
+  real_t n;        ///< Hardening index
+  real_t ep_0;     ///< Reference plastic strain
+  
+  /// @brief variables required to characterize the hysteresis response
+  struct InternalState {
+    tensor<real_t, dim, dim> plastic_strain;
+    real_t accumulated_plastic_strain;
+  };
+
+  using PackedInternalState = mfem::future::tensor<real_t, N_INTERNAL_STATES>;
+
+  MFEM_HOST_DEVICE static inline InternalState unpack_internal_state(
+      const mfem::future::tensor<real_t, N_INTERNAL_STATES>& packed_state)
+  {
+    auto plastic_strain =
+        mfem::future::make_tensor<dim, dim>([&packed_state](int i, int j) { return packed_state[dim * i + j]; });
+    real_t accumulated_plastic_strain = packed_state[N_INTERNAL_STATES - 1];
+    return {plastic_strain, accumulated_plastic_strain};
+  }
+
+  MFEM_HOST_DEVICE static inline PackedInternalState pack_internal_state(
+      const mfem::future::tensor<real_t, dim, dim>& plastic_strain, real_t accumulated_plastic_strain)
+  {
+    PackedInternalState packed_state{};
+    for (int i = 0, ij = 0; i < dim; i++) {
+      for (int j = 0; j < dim; j++, ij++) {
+        packed_state[ij] = plastic_strain[i][j];
+      }
+    }
+    packed_state[N_INTERNAL_STATES - 1] = accumulated_plastic_strain;
+    return packed_state;
+  }
+
+  MFEM_HOST_DEVICE inline tuple<tensor<real_t, dim, dim>, PackedInternalState>
+  operator()(tensor<real_t, dim, dim> dudxi,
+             PackedInternalState internal_state,
+             tensor<real_t, dim, dim> J,
+             real_t w) const
+  {
+    auto invJ = inv(J);
+    const auto dudX = dudxi * invJ;
+    auto I = IdentityMatrix<dim>();
+    const real_t K = E / (3.0 * (1.0 - 2.0 * nu));
+    const real_t G = 0.5 * E / (1.0 + nu);
+
+    auto [plastic_strain, accumulated_plastic_strain] = unpack_internal_state(internal_state);
+    
+    auto el_strain = sym(dudX) - plastic_strain;
+    auto p = K * tr(el_strain);
+    auto s = 2.0 * G * dev(el_strain);
+    auto q = std::sqrt(1.5) * norm(s);
+    real_t denom = q > 0.0? q : 1.0;
+    auto Np = 1.5 * s / denom;
+
+    if (q > FlowResistance(accumulated_plastic_strain, sigma_y, n, ep_0)) {
+        real_t lb = 0.0;
+        real_t ub = (q - FlowResistance(accumulated_plastic_strain, sigma_y, n, ep_0))/(3*G);
+        SolverSettings settings{.residual_abs_tol = 1e-10*sigma_y, .residual_rel_tol = 1e-10, 
+                                .bounds{.lower = lb, .upper = ub}};
+        real_t delta_eqps = SolveNewtonBisection<J2PlasticityResidual>(
+            0.5*(lb + ub), make_tuple(accumulated_plastic_strain, q, G, sigma_y, n, ep_0), settings);
+        accumulated_plastic_strain += delta_eqps;
+        plastic_strain += delta_eqps * Np;
+        s -= 2.0 * G * delta_eqps * Np;
+    }
+    internal_state = pack_internal_state(plastic_strain, accumulated_plastic_strain);
+    auto stress = s + p * I;
+    const real_t dV = det(J)*w;
+    return make_tuple(stress*transpose(invJ)*dV, internal_state);
+  }
+};
+
+__attribute__((used))
+void *  __enzyme_register_derivative_newton_bisection_on_j2[2] = {
+  (void*) SolveNewtonBisection_impl<J2PlasticityResidual, J2PlasticityParameters>,
+  (void*) SolveNewtonBisection_impl_fwddiff<J2PlasticityResidual, J2PlasticityParameters>
+};
+
+TEST_CASE("Univariate function solver on qfunction", "[univar]")
+{
+    tensor<real_t, 3, 3> H{{{0.947667  , 0.9785799 , 0.33229148},
+                            {0.46866846, 0.5698887 , 0.16550303},
+                            {0.3101946 , 0.68948054, 0.74676657}}};
+    J2Plasticity::PackedInternalState Q{};
+    J2Plasticity material{.E = 70.0e3, .nu = 0.34, .sigma_y = 240.0, .n = 0.15, .ep_0 = 1e-3};
+    auto [stress, Q_new] = material(H, Q, IdentityMatrix<3>(), 1.0);
+    INFO("stress = " << stress << "\ninternal vars =" << Q_new << "\n");
+    real_t mises = std::sqrt(1.5)*norm(dev(stress));
+    real_t eqps = Q_new[9];
+    real_t Y = FlowResistance(eqps, material.sigma_y, material.n, material.ep_0);
+    REQUIRE(mises == MFEM_Approx(Y, 0.0, 1e-8));
+}
+
+
 real_t nthroot_res(real_t x, tuple<real_t, real_t> p)
 {
     auto [index, radicand] = p;
@@ -40,22 +155,22 @@ TEST_CASE("Univariate function solver robustness", "[univar]")
 
     SECTION("Stiff problem")
     {
-        auto f = [](double x, double p) { return std::pow(x, p) - 1.0; };
-        double x0 = 0.1;
-        double p = 50;
+        auto f = [](real_t x, real_t p) { return std::pow(x, p) - 1.0; };
+        real_t x0 = 0.1;
+        real_t p = 50;
         SolverSettings settings{.bounds{.lower = 0.0, .upper = 5.1}};
-        double x = SolveNewtonBisection<+f>(x0, p, settings);
+        real_t x = SolveNewtonBisection<+f>(x0, p, settings);
         REQUIRE(x == MFEM_Approx(1.0));
     }
 
     SECTION("Escapes local minimum")
     {
-        auto f = [](double x, double m) { return std::cos(2*M_PI*x) - m*x + 2.5; };
-        double x0 = 0.1;
-        double m = 2.0;
+        auto f = [](real_t x, real_t m) { return std::cos(2*M_PI*x) - m*x + 2.5; };
+        real_t x0 = 0.1;
+        real_t m = 2.0;
         SolverSettings settings{.bounds{.lower = 0.0, .upper = 2.0}};
-        double x = SolveNewtonBisection<+f>(x0, m, settings);
-        double y = f(x, m);
+        real_t x = SolveNewtonBisection<+f>(x0, m, settings);
+        real_t y = f(x, m);
         mfem::out << "f(x) = " << f(x, m) << "\n";
         INFO("f(x) = " << f(x, m));
         REQUIRE(x == MFEM_Approx(1.25));
@@ -63,11 +178,4 @@ TEST_CASE("Univariate function solver robustness", "[univar]")
 }
 
 
-// TEST_CASE("Univariate function solver", "[dFEM]") {
-//   auto f = [](double x, double p) { return std::pow(x, p) - 1.0; };
-//   double x0 = 0.1;
-//   double p = 50;
-//   SolverSettings settings{.bounds{.lower = 0.0, .upper = 5.1}};
-//   double x = newton_bisection<+f>(x0, p, settings);
-//   EXPECT_NEAR(x, 1.0, 1e-10);
-// }
+
