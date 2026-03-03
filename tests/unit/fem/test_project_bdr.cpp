51bc8037d457bd1756909bf550943ee5b8412061
Jan Nikl <nikl1@llnl.gov>
2026-03-03 12:05:57 -0800

    Added a unit test for vector ProjectBdrCoefficientNormal().

--- tests/unit/fem/test_project_bdr.cpp
+++ tests/unit/fem/test_project_bdr.cpp
@@ -25,15 +25,104 @@ void Func_3D_lin(const Vector &x, Vector &v)
    v[2] = -2.572 * x[0] + 1.321 * x[1] + 3.234 * x[2];
 }
 
+TEST_CASE("3D ProjectBdrCoefficientNormal",
+          "[GridFunction]"
+          "[VectorGridFunctionCoefficient]")
+{
+   const int n = 1;
+   const int dim = 3;
+   const int order = 1;
+
+   const double tol = 1e-6;
+
+   for (int type = (int)Element::TETRAHEDRON;
+        type <= (int)Element::HEXAHEDRON; type++)
+   {
+      Mesh mesh = Mesh::MakeCartesian3D(
+                     n, n, n, (Element::Type)type, 2.0, 3.0, 5.0);
+
+      VectorFunctionCoefficient funcCoef(dim, Func_3D_lin);
+
+      SECTION("3D GetVectorValue tests for element type " +
+              std::to_string(type))
+      {
+         RT_FECollection  rt_fec(order+1, dim);
+
+         FiniteElementSpace  rt_fespace(&mesh,  &rt_fec);
+
+         GridFunction  rt_x( &rt_fespace);
+
+         VectorGridFunctionCoefficient  rt_xCoef( &rt_x);
+
+         Array<int> bdr_marker(6);
+
+         Vector normal(dim);
+         Vector  f_val(dim);
+         Vector rt_val(dim);
+
+         for (int b = 1; b<=6; b++)
+         {
+            bdr_marker = 0;
+            bdr_marker[b-1] = 1;
+
+            rt_x = 0.0;
+            rt_x.ProjectBdrCoefficientNormal(funcCoef, bdr_marker);
+
+            for (int be = 0; be < mesh.GetNBE(); be++)
+            {
+               Element *e = mesh.GetBdrElement(be);
+               if (e->GetAttribute() != b) { continue; }
+
+               ElementTransformation *T = mesh.GetBdrElementTransformation(be);
+               const FiniteElement   *fe = rt_fespace.GetBE(be);
+               const IntegrationRule &ir = IntRules.Get(fe->GetGeomType(),
+                                                        2*order + 2);
+
+               double rt_err = 0.0;
+
+               for (int j=0; j<ir.GetNPoints(); j++)
+               {
+                  const IntegrationPoint &ip = ir.IntPoint(j);
+                  T->SetIntPoint(&ip);
+
+                  CalcOrtho(T->Jacobian(), normal);
+
+                  funcCoef.Eval(f_val, *T, ip);
+                  rt_xCoef.Eval(rt_val, *T, ip);
+
+                  rt_val -= f_val;
+
+                  double rt_dist = rt_val * normal;
+
+                  rt_err += rt_dist;
+
+                  if (verbose_tests && rt_dist > tol)
+                  {
+                     mfem::out << be << ":" << j << " rt ("
+                               << f_val[0] << "," << f_val[1] << "," << f_val[2]
+                               << ") vs. ("
+                               << rt_val[0] << "," << rt_val[1] << ","
+                               << rt_val[2] << ") " << rt_dist << std::endl;
+                  }
+               }
+               rt_err  /= ir.GetNPoints();
+
+               REQUIRE( rt_err == MFEM_Approx(0.0));
+            }
+         }
+      }
+   }
+}
+
 TEST_CASE("3D ProjectBdrCoefficientTangent",
           "[GridFunction]"
           "[VectorGridFunctionCoefficient]")
 {
-   int n = 1;
-   int dim = 3;
-   int order = 1;
+   const int n = 1;
+   const int dim = 3;
+   const int order = 1;
 
-   double tol = 1e-6;
+   const double tol = 1e-6;
 
    for (int type = (int)Element::TETRAHEDRON;
         type <= (int)Element::HEXAHEDRON; type++)
