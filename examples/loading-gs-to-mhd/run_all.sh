c4789f0210cc83de9690b40b3b4c6e8d8224a67c
rzhangbq <rzhangbq@gmail.com>
2026-04-28 20:26:39 -0400

    remove grad p from default run_all

--- examples/loading-gs-to-mhd/run_all.sh
+++ examples/loading-gs-to-mhd/run_all.sh
@@ -17,6 +17,4 @@ make all -j 8
 # third group
 ./B_field_projector_div_B_pol_vec_CG &
 ./B_field_projector_div_B_pol_Hcurl &
-./B_field_projector_div_B_pol_Hdiv &
-./JxB_validator_grad_p_A &
-./JxB_validator_grad_p_B
+./B_field_projector_div_B_pol_Hdiv
