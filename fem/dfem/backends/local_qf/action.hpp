a13ea95d27d9d9a5e06f735e3ec1053e0d212a0d
Julian Andrej <andrej1@llnl.gov>
2026-05-01 17:40:50 -0700

    crude local qf impl

--- fem/dfem/backends/local_qf/action.hpp
+++ fem/dfem/backends/local_qf/action.hpp
@@ -2,7 +2,7 @@
 
 #include "../util.hpp"
 #include "../../integrator_ctx.hpp"
-#include "../fem/kernels3d.hpp"
+#include "../../qfunction_transform.hpp"
 
 #include <utility>
 
@@ -36,126 +36,107 @@ struct Action
       check_consistency(inputs, input_to_infd, ctx.infds);
       check_consistency(outputs, output_to_outfd, ctx.outfds);
 
+      create_fieldbases(inputs, input_to_infd, ctx.infds, ctx.ir, input_bases);
+      create_fieldbases(outputs, output_to_outfd, ctx.outfds, ctx.ir, output_bases);
+
       const int nqp = ctx.ir.GetNPoints();
+      gnqp = nqp * ctx.nentities;
 
-      for_constexpr<ninputs>([&](auto i)
+      xq_offsets.SetSize(ninputs + 1);
+      xq_offsets[0] = 0;
+      constexpr_for<0, ninputs>([&](auto i)
       {
-         const auto &fd = ctx.infds[input_to_infd[i]];
-         idtq[i] = GetDofToQuad<Entity::Element>(fd, ctx.ir,
-                                                 DofToQuad::TENSOR);
+         const auto input = get<i>(inputs);
+         xq_offsets[i + 1] = nqp * input.size_on_qp * ctx.nentities;
       });
+      xq_offsets.PartialSum();
+      xq.Update(xq_offsets);
 
-      for_constexpr<noutputs>([&](auto i)
+      yq_offsets.SetSize(noutputs + 1);
+      yq_offsets[0] = 0;
+      constexpr_for<0, noutputs>([&](auto i)
       {
-         const auto &fd = ctx.outfds[output_to_outfd[i]];
-         odtq[i] = GetDofToQuad<Entity::Element>(fd, ctx.ir,
-                                                 DofToQuad::TENSOR);
+         const auto output = get<i>(outputs);
+         yq_offsets[i + 1] = nqp * output.size_on_qp * ctx.nentities;
       });
+      yq_offsets.PartialSum();
+      yq.Update(yq_offsets);
    }
 
    void operator()(
       const std::vector<Vector *> &xe,
       std::vector<Vector *> &ye) const
    {
-      constexpr int T_Q1D = 0;
-      constexpr int DIM = 3;
-
       if (ctx.attr.Size() == 0) { return; }
 
-      // read all maps into GPU memory
-      for_constexpr<ninputs>([&](auto i)
+      // E -> Q
+      interpolate(input_to_infd, input_bases, xe, xq);
+
+      // Q -> Q
+      using qf_signature = typename get_function_signature<qfunc_t>::type;
+      using qf_param_ts = typename qf_signature::parameter_ts;
+
+      static_assert(tuple_size<qf_param_ts>::value == ninputs + noutputs,
+                    "qfunc parameter count must match inputs+outputs");
+
+      // Create 2D views: [size_on_qp, gnqp] for each input/output block.
+      std::array<DeviceTensor<2>, ninputs> xdt;
+      constexpr_for<0, ninputs>([&](auto i)
       {
-         idtqb[i] = idtq[i]->B.Read();
-         idtqg[i] = idtq[i]->G.Read();
+         const auto input = get<i>(inputs);
+         // Use ReadWrite() to avoid creating a `DeviceTensor<..., const real_t>`,
+         // which would not match the overload set in `qfunction_transform.hpp`.
+         xdt[i] = Reshape(xq.GetBlock(i).ReadWrite(), input.size_on_qp, gnqp);
       });
 
-      for_constexpr<noutputs>([&](auto i)
+      std::array<DeviceTensor<2>, noutputs> ydt;
+      constexpr_for<0, noutputs>([&](auto i)
       {
-         odtqb[i] = odtq[i]->B.Read();
-         odtqg[i] = odtq[i]->G.Read();
+         const auto output = get<i>(outputs);
+         yq.GetBlock(i) = 0.0;
+         ydt[i] = Reshape(yq.GetBlock(i).ReadWrite(), output.size_on_qp, gnqp);
       });
 
-      const auto XE = Reshape(fields_e[0].Read(), d1d, d1d, d1d, VDIM, NE);
-      const real_t *dx_ptr = fields_e[1].Read();
-
-      mfem::future::forall([=] MFEM_HOST_DEVICE (int e, void *)
+      const auto qfunc_local = qfunc;
+      mfem::forall(gnqp, [=] MFEM_HOST_DEVICE (int q)
       {
-         constexpr int MQ1 = T_Q1D > 0 ? T_Q1D : 8;
+         auto qf_args = decay_tuple<qf_param_ts> {};
 
-         MFEM_SHARED real_t sm0[MQ1][MQ1][MQ1][3];
-         MFEM_SHARED real_t sm1[MQ1][MQ1][MQ1][3];
+         for_constexpr<ninputs>([&](auto i)
+         {
+            process_qf_arg(xdt[i], get<i>(qf_args), q);
+         });
 
-         mfem::kernels::internal::low::regs3d_t<DIM, MQ1> reg;
-         const real_t *rd = dx_ptr;
+         // mfem::future::apply() forwards the tuple as `const` due to an
+         // internal `std::move`, which breaks qfuncs with non-const output
+         // parameters. Use a local, no-move tuple invocation instead.
+         call_qfunc_no_move(qfunc_local, qf_args);
 
+         for_constexpr<noutputs>([&](auto o)
+         {
+            constexpr std::size_t arg_idx = ninputs + o;
+            auto out_q = Reshape(&ydt[o](0, q), ydt[o].GetShape()[0]);
+            process_qf_result(out_q, get<arg_idx>(qf_args));
+         });
       });
 
-      //    dfem::forall<T_Q1D*T_Q1D*T_Q1D>([=] MFEM_HOST_DEVICE (int e, void *)
-      //    {
-      //       if (has_attr && !d_attr[d_elem_attr[e] - 1]) { return; }
+      // Q -> E
+      integrate(output_to_outfd, output_bases, yq, ye);
+   }
 
-      //       constexpr int MQ1 = T_Q1D > 0 ? T_Q1D : 8;
+   template <typename func_t, typename args_t, int... Is>
+   MFEM_HOST_DEVICE static void call_qfunc_no_move_impl(
+      const func_t &func, args_t &args, std::integer_sequence<int, Is...>)
+   {
+      (void)func(get<Is>(args)...);
+   }
 
-      //       MFEM_SHARED real_t sm0[MQ1][MQ1][MQ1][3];
-      //       MFEM_SHARED real_t sm1[MQ1][MQ1][MQ1][3];
-
-      //       low::regs3d_t<DIM, MQ1> reg;
-      //       const real_t *rd = dx_ptr;
-
-      //       MFEM_SHARED real_t sB[MQ1][MQ1], sG[MQ1][MQ1];
-      //       {
-      //          low::LoadMatrix(d1d, q1d, B, sB);
-      //          low::LoadMatrix(d1d, q1d, G, sG);
-      //          {
-      //             low::LoadDofs3d(e, d1d, XE, sm0);
-      //             low::Grad3d(d1d, q1d, sB, sG, sm0, sm1, reg);
-      //          }
-      //       }
-      //       // else if constexpr (is_identity_fop<field_operator_t>::value)   // Identity
-      //       {
-      //          // db1("Identity");
-      //          // rd = fields_e_ptr[input_to_field[i]];
-      //          // rd = dx_ptr;
-      //       }
-      //    }
-
-      //    MFEM_FOREACH_THREAD_DIRECT(qz,z,q1d)
-      //    {
-      //       MFEM_FOREACH_THREAD_DIRECT(qy,y,q1d)
-      //       {
-      //          MFEM_FOREACH_THREAD_DIRECT(qx,x,q1d)
-      //          {
-
-      //             auto args = decay_tuple<qf_param_ts> {};
-      //             get<0>(args) = as_tensor<real_t, 3>(&reg[qz][qy][qx][0]);
-      //             if constexpr (T_Q1D > 0)
-      //             {
-      //                get<1>(args) = as_tensor<real_t, 3, 3>(rd + 9*(qx*T_Q1D*T_Q1D + qy*T_Q1D + qz));
-      //             }
-      //             else
-      //             {
-      //                get<1>(args) = as_tensor<real_t, 3, 3>(rd + 9*(qx*q1d*q1d + qy*q1d + qz));
-      //             }
-      //             auto r = get<0>(apply(qfunc, args));
-      //             if constexpr (decltype(r)::ndim == 1)
-      //             {
-      //                as_tensor<real_t, 3>(&reg[qz][qy][qx][0]) = r;
-      //             }
-      //             else { static_assert(false); }
-      //          }
-      //       }
-      //    }
-      //    MFEM_SYNC_THREAD;
-      //    // Integrate
-      //    // if constexpr (is_gradient_fop<std::decay_t<output_fop_t>>::value) // Gradient
-      //    {
-      //       // const auto sB = reinterpret_cast<const real_t (*)[MQ1]>(Bo);
-      //       // const auto sG = reinterpret_cast<const real_t (*)[MQ1]>(Go);
-      //       low::GradTranspose3d(d1d, q1d, sB, sG, reg, sm1, sm0);
-      //       low::WriteDofs3d(d1d, 0, e, reg, YE);
-      //    }
-      // },
-      // num_entities, thread_blocks, 0, nullptr);
+   template <typename func_t, typename args_t>
+   MFEM_HOST_DEVICE static void call_qfunc_no_move(const func_t &func,
+                                                   args_t &args)
+   {
+      constexpr int nargs = static_cast<int>(tuple_size<args_t>::value);
+      call_qfunc_no_move_impl(func, args, std::make_integer_sequence<int, nargs> {});
    }
 
 
@@ -167,14 +148,12 @@ struct Action
    std::array<size_t, ninputs> input_to_infd;
    std::array<size_t, noutputs> output_to_outfd;
 
-   std::array<const DofToQuad*, ninputs> idtq;
-   std::array<const DofToQuad*, noutputs> odtq;
+   std::array<FieldBasis, ninputs> input_bases;
+   std::array<FieldBasis, noutputs> output_bases;
 
-   std::array<const real_t*, ninputs> idtqb;
-   std::array<const real_t*, ninputs> idtqg;
-
-   std::array<const real_t*, ninputs> odtqb;
-   std::array<const real_t*, ninputs> odtqg;
+   int gnqp = 0;
+   Array<int> xq_offsets, yq_offsets;
+   mutable BlockVector xq, yq;
 
 };
 
