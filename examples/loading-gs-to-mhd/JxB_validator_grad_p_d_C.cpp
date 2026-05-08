735fc4a15f84047caca021bfd7e67ff8cedb0cbe
rzhangbq <rzhangbq@gmail.com>
2026-05-08 16:31:36 -0400

    add direct grad p version

---
+++ examples/loading-gs-to-mhd/JxB_validator_grad_p_d_C.cpp
@@ -0,0 +1,77 @@
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+#include "vec_coeffs.hpp"
+
+using namespace std;
+using namespace mfem;
+
+int main(int argc, char *argv[])
+{
+   const char *mesh_file = "mesh/2d_mesh.mesh";
+   bool visualization = true;
+   bool mixed_bilinear_form = false;
+
+   Mesh mesh(mesh_file, 1, 1);
+   int dim = mesh.Dimension();
+
+   ifstream temp_log("input/new_grad_p_cg.gf");
+   GridFunction grad_p(&mesh, temp_log);
+
+   cout << "Mesh loaded" << endl;
+
+   const char *new_mesh_file = "mesh/2d_mesh.mesh";
+   Mesh *new_mesh = new Mesh(new_mesh_file, 1, 1);
+
+   H1_FECollection scaler_fec(1, dim);
+   FiniteElementSpace scaler_fespace(new_mesh, &scaler_fec);
+   ND_FECollection fec(1, dim);
+   FiniteElementSpace fespace(new_mesh, &fec);
+
+
+   LinearForm b(&scaler_fespace);
+   real_t mu = 4.0 * M_PI * 1e-7;
+   real_t beta = mu;
+   grad_p *= beta;
+
+
+   // load JxB_pol on the new mesh
+   temp_log.close(); // close the file
+   temp_log.open("output/JxB_pol_C.gf");
+   GridFunction JxB_pol(new_mesh, temp_log);
+
+   cout << grad_p.FESpace()->GetTrueVSize() << endl;
+   cout << JxB_pol.FESpace()->GetTrueVSize() << endl;
+   grad_p += JxB_pol;
+
+   if (visualization)
+   {
+      char vishost[] = "localhost";
+      int visport = 19916;
+      socketstream sol_sock(vishost, visport);
+      sol_sock.precision(8);
+      sol_sock << "solution\n"
+               << *new_mesh << grad_p << flush;
+   }
+
+   // paraview
+   {
+      ParaViewDataCollection paraview_dc("grad_p_C", new_mesh);
+      paraview_dc.SetPrefixPath("ParaView");
+      paraview_dc.SetLevelsOfDetail(1);
+      paraview_dc.SetCycle(0);
+      paraview_dc.SetDataFormat(VTKFormat::BINARY);
+      paraview_dc.SetHighOrderOutput(true);
+      paraview_dc.SetTime(0.0); // set the time
+      paraview_dc.RegisterField("grad_p_C", &grad_p);
+      paraview_dc.Save();
+   }
+
+   ofstream sol_ofs("output/grad_p_C.gf");
+   sol_ofs.precision(8);
+   grad_p.Save(sol_ofs);
+
+   delete new_mesh;
+
+   return 0;
+}
