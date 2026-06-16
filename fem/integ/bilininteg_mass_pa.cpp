891eaf282918d5d418c3aa27030d005c001adb9f
camierjs <camierjs@gmail.com>
2026-06-16 07:07:34 -0700

    Fix missing B/Bt for OCCA backend

--- fem/integ/bilininteg_mass_pa.cpp
+++ fem/integ/bilininteg_mass_pa.cpp
@@ -159,6 +159,9 @@ void MassIntegrator::AddMultPA(const Vector &x, Vector &y) const
       const int D1D = dofs1D;
       const int Q1D = quad1D;
       const Vector &D = pa_data;
+      const Array<real_t> &B = maps->B;
+      const Array<real_t> &Bt = maps->Bt;
+
 #ifdef MFEM_USE_OCCA
       if (DeviceCanUseOcca())
       {
@@ -196,8 +199,6 @@ void MassIntegrator::AddMultPA(const Vector &x, Vector &y) const
       }
       else
       {
-         const Array<real_t> &B = maps->B;
-         const Array<real_t> &Bt = maps->Bt;
          ApplyPAKernels::Run(dim, D1D, Q1D, ne, B, Bt, D, x, y, D1D, Q1D);
       }
    }
