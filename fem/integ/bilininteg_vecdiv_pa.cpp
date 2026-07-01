5e4b69f3d85671e57241bd3802dc225bcae8406c
camierjs <camierjs@gmail.com>
2026-07-01 14:32:50 -0700

    Cleanup bilininteg_vecdiv_pa

--- fem/integ/bilininteg_vecdiv_pa.cpp
+++ fem/integ/bilininteg_vecdiv_pa.cpp
@@ -13,8 +13,6 @@
 #include "../bilininteg.hpp"
 #include "../kernels.hpp"
 
-using mfem::kernels::internal::SetMaxOf;
-
 namespace mfem
 {
 
@@ -26,24 +24,26 @@ static void PADivergenceSetup2D(const int Q1D,
                                 const real_t COEFF,
                                 Vector &op)
 {
-   const int NQ = Q1D*Q1D;
-   auto W = w.Read();
-   auto J = Reshape(j.Read(), NQ, 2, 2, NE);
-   auto y = Reshape(op.Write(), NQ, 2, 2, NE);
+   const auto W = Reshape(w.Read(), Q1D, Q1D);
+   const auto J = Reshape(j.Read(), Q1D, Q1D, 2, 2, NE);
+   auto y = Reshape(op.Write(), Q1D, Q1D, 2, 2, NE);
 
-   mfem::forall(NE, [=] MFEM_HOST_DEVICE (int e)
+   mfem::forall_2D(NE, Q1D, Q1D, [=] MFEM_HOST_DEVICE (int e)
    {
-      for (int q = 0; q < NQ; ++q)
+      MFEM_FOREACH_THREAD(qx, x, Q1D)
       {
-         const real_t J11 = J(q,0,0,e);
-         const real_t J12 = J(q,0,1,e);
-         const real_t J21 = J(q,1,0,e);
-         const real_t J22 = J(q,1,1,e);
-         // Store wq * Q * adj(J)
-         y(q,0,0,e) = W[q] * COEFF *  J22; // 1,1
-         y(q,0,1,e) = W[q] * COEFF * -J12; // 1,2
-         y(q,1,0,e) = W[q] * COEFF * -J21; // 2,1
-         y(q,1,1,e) = W[q] * COEFF *  J11; // 2,2
+         MFEM_FOREACH_THREAD(qy, y, Q1D)
+         {
+            const real_t J11 = J(qx, qy, 0, 0, e);
+            const real_t J21 = J(qx, qy, 1, 0, e);
+            const real_t J12 = J(qx, qy, 0, 1, e);
+            const real_t J22 = J(qx, qy, 1, 1, e);
+            const real_t cw = W(qx, qy) * COEFF;
+            y(qx, qy, 0, 0, e) = cw *  J22;
+            y(qx, qy, 0, 1, e) = cw * -J12;
+            y(qx, qy, 1, 0, e) = cw * -J21;
+            y(qx, qy, 1, 1, e) = cw *  J11;
+         }
       }
    });
 }
@@ -56,44 +56,50 @@ static void PADivergenceSetup3D(const int Q1D,
                                 const real_t COEFF,
                                 Vector &op)
 {
-   const int NQ = Q1D*Q1D*Q1D;
-   auto W = w.Read();
-   auto J = Reshape(j.Read(), NQ, 3, 3, NE);
-   auto y = Reshape(op.Write(), NQ, 3, 3, NE);
-   mfem::forall(NE, [=] MFEM_HOST_DEVICE (int e)
+   const auto W = Reshape(w.Read(), Q1D, Q1D, Q1D);
+   const auto J = Reshape(j.Read(), Q1D, Q1D, Q1D, 3, 3, NE);
+   auto y = Reshape(op.Write(), Q1D, Q1D, Q1D, 3, 3, NE);
+
+   mfem::forall_3D(NE, Q1D, Q1D, Q1D, [=] MFEM_HOST_DEVICE (int e)
    {
-      for (int q = 0; q < NQ; ++q)
+      MFEM_FOREACH_THREAD(qx, x, Q1D)
       {
-         const real_t J11 = J(q,0,0,e);
-         const real_t J21 = J(q,1,0,e);
-         const real_t J31 = J(q,2,0,e);
-         const real_t J12 = J(q,0,1,e);
-         const real_t J22 = J(q,1,1,e);
-         const real_t J32 = J(q,2,1,e);
-         const real_t J13 = J(q,0,2,e);
-         const real_t J23 = J(q,1,2,e);
-         const real_t J33 = J(q,2,2,e);
-         const real_t cw  = W[q] * COEFF;
-         // adj(J)
-         const real_t A11 = (J22 * J33) - (J23 * J32);
-         const real_t A12 = (J32 * J13) - (J12 * J33);
-         const real_t A13 = (J12 * J23) - (J22 * J13);
-         const real_t A21 = (J31 * J23) - (J21 * J33);
-         const real_t A22 = (J11 * J33) - (J13 * J31);
-         const real_t A23 = (J21 * J13) - (J11 * J23);
-         const real_t A31 = (J21 * J32) - (J31 * J22);
-         const real_t A32 = (J31 * J12) - (J11 * J32);
-         const real_t A33 = (J11 * J22) - (J12 * J21);
-         // Store wq * Q * adj(J)
-         y(q,0,0,e) = cw * A11; // 1,1
-         y(q,0,1,e) = cw * A12; // 1,2
-         y(q,0,2,e) = cw * A13; // 1,3
-         y(q,1,0,e) = cw * A21; // 2,1
-         y(q,1,1,e) = cw * A22; // 2,2
-         y(q,1,2,e) = cw * A23; // 2,3
-         y(q,2,0,e) = cw * A31; // 3,1
-         y(q,2,1,e) = cw * A32; // 3,2
-         y(q,2,2,e) = cw * A33; // 3,3
+         MFEM_FOREACH_THREAD(qy, y, Q1D)
+         {
+            MFEM_FOREACH_THREAD(qz, z, Q1D)
+            {
+               const real_t J11 = J(qx, qy, qz, 0, 0, e);
+               const real_t J21 = J(qx, qy, qz, 1, 0, e);
+               const real_t J31 = J(qx, qy, qz, 2, 0, e);
+               const real_t J12 = J(qx, qy, qz, 0, 1, e);
+               const real_t J22 = J(qx, qy, qz, 1, 1, e);
+               const real_t J32 = J(qx, qy, qz, 2, 1, e);
+               const real_t J13 = J(qx, qy, qz, 0, 2, e);
+               const real_t J23 = J(qx, qy, qz, 1, 2, e);
+               const real_t J33 = J(qx, qy, qz, 2, 2, e);
+               const real_t cw = W(qx, qy, qz) * COEFF;
+               // adj(J)
+               const real_t A11 = (J22 * J33) - (J23 * J32);
+               const real_t A12 = (J32 * J13) - (J12 * J33);
+               const real_t A13 = (J12 * J23) - (J22 * J13);
+               const real_t A21 = (J31 * J23) - (J21 * J33);
+               const real_t A22 = (J11 * J33) - (J13 * J31);
+               const real_t A23 = (J21 * J13) - (J11 * J23);
+               const real_t A31 = (J21 * J32) - (J31 * J22);
+               const real_t A32 = (J31 * J12) - (J11 * J32);
+               const real_t A33 = (J11 * J22) - (J12 * J21);
+               // Store wq * Q * adj(J)
+               y(qx, qy, qz, 0, 0, e) = cw * A11;
+               y(qx, qy, qz, 0, 1, e) = cw * A12;
+               y(qx, qy, qz, 0, 2, e) = cw * A13;
+               y(qx, qy, qz, 1, 0, e) = cw * A21;
+               y(qx, qy, qz, 1, 1, e) = cw * A22;
+               y(qx, qy, qz, 1, 2, e) = cw * A23;
+               y(qx, qy, qz, 2, 0, e) = cw * A31;
+               y(qx, qy, qz, 2, 1, e) = cw * A32;
+               y(qx, qy, qz, 2, 2, e) = cw * A33;
+            }
+         }
       }
    });
 }
@@ -123,14 +129,13 @@ void VectorDivergenceIntegrator::AssemblePA(const FiniteElementSpace &trial_fes,
    // Assumes tensor-product elements ordered by nodes
    MFEM_ASSERT(trial_fes.GetOrdering() == Ordering::byNODES,
                "PA Only supports Ordering::byNODES!");
-   Mesh *mesh = trial_fes.GetMesh();
-   const FiniteElement &trial_fe = *trial_fes.GetTypicalFE();
-   const FiniteElement &test_fe = *test_fes.GetTypicalFE();
-   ElementTransformation *trans = mesh->GetTypicalElementTransformation();
-   const IntegrationRule *ir = IntRule ? IntRule : &GetRule(trial_fe, test_fe,
-                                                            *trans);
+   auto *mesh = trial_fes.GetMesh();
+   const auto &trial_fe = *trial_fes.GetTypicalFE();
+   const auto &test_fe = *test_fes.GetTypicalFE();
+   const auto *ir = IntRule ? IntRule :
+                    &GetRule(trial_fe, test_fe,
+                             *mesh->GetTypicalElementTransformation());
    const int dims = trial_fe.GetDim();
-   const int dimsToStore = dims * dims;
    nq = ir->GetNPoints();
    dim = mesh->Dimension();
    ne = trial_fes.GetNE();
@@ -138,16 +143,18 @@ void VectorDivergenceIntegrator::AssemblePA(const FiniteElementSpace &trial_fes,
    trial_maps = &trial_fe.GetDofToQuad(*ir, DofToQuad::TENSOR);
    trial_dofs1D = trial_maps->ndof;
    quad1D = trial_maps->nqpt;
-   test_maps  = &test_fe.GetDofToQuad(*ir, DofToQuad::TENSOR);
+   test_maps = &test_fe.GetDofToQuad(*ir, DofToQuad::TENSOR);
    test_dofs1D = test_maps->ndof;
    MFEM_ASSERT(quad1D == test_maps->nqpt,
-               "PA requires test and trial space to have same number of quadrature points!");
-   pa_data.SetSize(nq * dimsToStore * ne, Device::GetMemoryType());
+               "PA requires test and trial space to have same number of "
+               "quadrature points!");
+   pa_data.SetSize(nq * dims * dims * ne, Device::GetMemoryType());
+
    real_t coeff = 1.0;
    if (Q)
    {
-      ConstantCoefficient *cQ = dynamic_cast<ConstantCoefficient*>(Q);
-      MFEM_VERIFY(cQ != NULL, "only ConstantCoefficient is supported!");
+      auto *cQ = dynamic_cast<ConstantCoefficient *>(Q);
+      MFEM_VERIFY(cQ, "only ConstantCoefficient is supported!");
       coeff = cQ->constant;
    }
    PADivergenceSetup(dim, quad1D, ne, ir->GetWeights(), geom->J, coeff, pa_data);
@@ -181,7 +188,7 @@ static void SmemPADivergenceApply2D(const int NE,
 
    mfem::forall_2D<T_Q1D * T_Q1D>(NE, Q1D, Q1D, [=] MFEM_HOST_DEVICE (int e)
    {
-      constexpr int MQ1 = T_Q1D > 0 ? SetMaxOf(T_Q1D) : DofQuadLimits::MAX_T1D;
+      constexpr int MQ1 = T_Q1D ? T_Q1D : DofQuadLimits::MAX_T1D;
 
       MFEM_SHARED real_t smem[MQ1][MQ1];
       MFEM_SHARED real_t sB[MQ1][MQ1], sG[MQ1][MQ1];
@@ -417,82 +424,86 @@ static void SmemPADivergenceApply3D(const int NE,
 // PA Divergence Apply kernel
 void VectorDivergenceIntegrator::AddMultPA(const Vector &x, Vector &y) const
 {
-   static const auto vector_divergence_kernel_specializations =
-      ( // 2D
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 2, 2, 2>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 2, 2, 3>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 2, 2, 4>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 3, 3, 3>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 3, 3, 4>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 3, 3, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 4, 4, 4>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 4, 4, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 4, 4, 6>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 5, 5, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 5, 5, 6>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<2, 5, 5, 7>::Add(),
-         // 3D
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 2, 2, 3>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 2, 2, 4>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 2, 2, 6>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 3, 2, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 3, 3, 4>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 3, 3, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 3, 3, 7>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 4, 4, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 4, 4, 6>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 4, 4, 8>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 5, 5, 6>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 5, 5, 7>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultPA::Specialization<3, 5, 5, 9>::Add(),
-         true);
-   MFEM_CONTRACT_VAR(vector_divergence_kernel_specializations);
+   using Kernels = VectorDivergenceAddMultPA;
 
-   VectorDivergenceAddMultPA::Run(dim, trial_dofs1D, test_dofs1D, quad1D, ne,
-                                  trial_maps->B, trial_maps->G, test_maps->Bt,
-                                  pa_data, x, y,
-                                  trial_dofs1D, test_dofs1D, quad1D);
+   static const auto specializations =
+      ( // 2D
+         Kernels::Specialization<2, 2, 2, 2>::Add(),
+         Kernels::Specialization<2, 2, 2, 3>::Add(),
+         Kernels::Specialization<2, 2, 2, 4>::Add(),
+         Kernels::Specialization<2, 3, 3, 3>::Add(),
+         Kernels::Specialization<2, 3, 3, 4>::Add(),
+         Kernels::Specialization<2, 3, 3, 5>::Add(),
+         Kernels::Specialization<2, 4, 4, 4>::Add(),
+         Kernels::Specialization<2, 4, 4, 5>::Add(),
+         Kernels::Specialization<2, 4, 4, 6>::Add(),
+         Kernels::Specialization<2, 5, 5, 5>::Add(),
+         Kernels::Specialization<2, 5, 5, 6>::Add(),
+         Kernels::Specialization<2, 5, 5, 7>::Add(),
+         // 3D
+         Kernels::Specialization<3, 2, 2, 3>::Add(),
+         Kernels::Specialization<3, 2, 2, 4>::Add(),
+         Kernels::Specialization<3, 2, 2, 6>::Add(),
+         Kernels::Specialization<3, 3, 2, 5>::Add(),
+         Kernels::Specialization<3, 3, 3, 4>::Add(),
+         Kernels::Specialization<3, 3, 3, 5>::Add(),
+         Kernels::Specialization<3, 3, 3, 7>::Add(),
+         Kernels::Specialization<3, 4, 4, 5>::Add(),
+         Kernels::Specialization<3, 4, 4, 6>::Add(),
+         Kernels::Specialization<3, 4, 4, 8>::Add(),
+         Kernels::Specialization<3, 5, 5, 6>::Add(),
+         Kernels::Specialization<3, 5, 5, 7>::Add(),
+         Kernels::Specialization<3, 5, 5, 9>::Add(),
+         true);
+   MFEM_CONTRACT_VAR(specializations);
+
+   Kernels::Run(dim, trial_dofs1D, test_dofs1D, quad1D, ne,
+                trial_maps->B, trial_maps->G, test_maps->Bt,
+                pa_data, x, y,
+                trial_dofs1D, test_dofs1D, quad1D);
 }
 
-// PA Divergence Apply kernel
+// PA Divergence Apply kernel transpose
 void VectorDivergenceIntegrator::AddMultTransposePA(const Vector &x,
                                                     Vector &y) const
 {
-   static const auto vector_divergence_transposed_kernel_specializations =
-      ( // 2D
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 2, 2, 2>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 2, 2, 3>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 2, 2, 4>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 3, 3, 3>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 3, 3, 4>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 3, 3, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 4, 4, 4>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 4, 4, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 4, 4, 6>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 5, 5, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 5, 5, 6>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<2, 5, 5, 7>::Add(),
-         // 3D
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 2, 2, 3>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 2, 2, 4>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 2, 2, 6>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 3, 2, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 3, 3, 4>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 3, 3, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 3, 3, 7>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 4, 4, 5>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 4, 4, 6>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 4, 4, 8>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 5, 5, 6>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 5, 5, 7>::Add(),
-         VectorDivergenceIntegrator::VectorDivergenceAddMultTransposePA::Specialization<3, 5, 5, 9>::Add(),
-         true);
-   MFEM_CONTRACT_VAR(vector_divergence_transposed_kernel_specializations);
+   using Kernels = VectorDivergenceAddMultTransposePA;
 
-   VectorDivergenceAddMultTransposePA::Run(dim, trial_dofs1D, test_dofs1D, quad1D,
-                                           ne, trial_maps->Bt, trial_maps->Gt, test_maps->B,
-                                           pa_data, x, y,
-                                           trial_dofs1D, test_dofs1D, quad1D);
+   static const auto specializations =
+      ( // 2D
+         Kernels::Specialization<2, 2, 2, 2>::Add(),
+         Kernels::Specialization<2, 2, 2, 3>::Add(),
+         Kernels::Specialization<2, 2, 2, 4>::Add(),
+         Kernels::Specialization<2, 3, 3, 3>::Add(),
+         Kernels::Specialization<2, 3, 3, 4>::Add(),
+         Kernels::Specialization<2, 3, 3, 5>::Add(),
+         Kernels::Specialization<2, 4, 4, 4>::Add(),
+         Kernels::Specialization<2, 4, 4, 5>::Add(),
+         Kernels::Specialization<2, 4, 4, 6>::Add(),
+         Kernels::Specialization<2, 5, 5, 5>::Add(),
+         Kernels::Specialization<2, 5, 5, 6>::Add(),
+         Kernels::Specialization<2, 5, 5, 7>::Add(),
+         // 3D
+         Kernels::Specialization<3, 2, 2, 3>::Add(),
+         Kernels::Specialization<3, 2, 2, 4>::Add(),
+         Kernels::Specialization<3, 2, 2, 6>::Add(),
+         Kernels::Specialization<3, 3, 2, 5>::Add(),
+         Kernels::Specialization<3, 3, 3, 4>::Add(),
+         Kernels::Specialization<3, 3, 3, 5>::Add(),
+         Kernels::Specialization<3, 3, 3, 7>::Add(),
+         Kernels::Specialization<3, 4, 4, 5>::Add(),
+         Kernels::Specialization<3, 4, 4, 6>::Add(),
+         Kernels::Specialization<3, 4, 4, 8>::Add(),
+         Kernels::Specialization<3, 5, 5, 6>::Add(),
+         Kernels::Specialization<3, 5, 5, 7>::Add(),
+         Kernels::Specialization<3, 5, 5, 9>::Add(),
+         true);
+   MFEM_CONTRACT_VAR(specializations);
+
+   Kernels::Run(dim, trial_dofs1D, test_dofs1D, quad1D, ne,
+                trial_maps->Bt, trial_maps->Gt, test_maps->B,
+                pa_data, x, y,
+                trial_dofs1D, test_dofs1D, quad1D);
 }
 
 /// \cond DO_NOT_DOCUMENT
