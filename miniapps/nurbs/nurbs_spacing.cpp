0e12f953eb7c5b969351d8f0a04f54f72ac64199
Dylan Copeland <copeland11@llnl.gov>
2026-07-14 13:11:55 -0700

    Added circles and cylinders examples to nurbs_spacing miniapp.

--- miniapps/nurbs/nurbs_spacing.cpp
+++ miniapps/nurbs/nurbs_spacing.cpp
@@ -1,18 +1,56 @@
+// Copyright (c) 2010-2025, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+//
+//          -------------------------------------------------------------------
+//          NURBS Spacing: Adjust the physical element spacing in NURBS patches
+//          -------------------------------------------------------------------
+//
+// Compile with: make nurbs_spacing
+//
+// Sample runs:  nurbs_spacing -ex 1
+//               nurbs_spacing -ex 2
+//
+// Description:  This miniapp inputs a NURBS mesh and modifies its control points
+//               (degrees of freedom) so that the relative physical element
+//               spacing in each direction in each patch approximates the
+//               relative spacing of the corresponding knot vector in reference
+//               space. An example NC-patch NURBS mesh can optionally be
+//               generated as a demonstration.
+
 #include "mfem.hpp"
 #include <fstream>
 #include <iostream>
 
+#include "circlesNCpatch.hpp"
+
 using namespace std;
 using namespace mfem;
 
+enum class ExampleMesh
+{
+   FileInput = 0,  // Mesh loaded from file
+   CirclesNC = 1,  // 2D NC-patch NURBS circles mesh
+   CylindersNC = 2 // 3D NC-patch NURBS mesh extruded from CirclesNC
+};
+
 int main(int argc, char *argv[])
 {
-   // 1. Parse command-line options.
-   const char *mesh_file = "../data/nc3-nurbs.mesh";
+   int example = 0;  // ExampleMesh
+   const char *mesh_file = "../../data/nc3-nurbs.mesh";
    bool sweep1D = true;
    bool visualization = true;
 
    OptionsParser args(argc, argv);
+   args.AddOption(&example, "-ex", "--example",
+                  "Example mesh");
    args.AddOption(&mesh_file, "-m", "--mesh",
                   "Mesh file to use.");
    args.AddOption(&sweep1D, "-s", "--sweep-1D", "-fs",
@@ -28,23 +66,39 @@ int main(int argc, char *argv[])
    }
    args.PrintOptions(cout);
 
-   Mesh mesh(mesh_file, 1, 1);
-   int dim = mesh.Dimension();
+   const ExampleMesh example_type = static_cast<ExampleMesh>(example);
 
-   mesh.NURBSext->ConvertToPatches(*mesh.GetNodes());
+   Mesh *mesh;
+   if (example_type == ExampleMesh::CirclesNC)
+   {
+      mesh = CirclesMesh(4);
+   }
+   else if (example_type == ExampleMesh::CylindersNC)
+   {
+      Mesh *mesh2D = CirclesMesh(4);
+      mesh = ExtrudeNURBS2D(*mesh2D, 2, 3, 1.0);
+   }
+   else
+   {
+      mesh = new Mesh(mesh_file, 1, 1);
+   }
 
-   Array<NURBSPatch*> patches;
-   mesh.NURBSext->GetPatches(patches);
+   mesh->NURBSext->PhysicalSpacing(*mesh->GetNodes(), sweep1D);
 
-   mesh.NURBSext->PhysicalSpacing(*mesh.GetNodes(), sweep1D);
-   mesh.NURBSext->SetCoordsFromPatches(*mesh.GetNodes(), dim);
+   ofstream mesh_ofs("spaced.mesh");
+   mesh_ofs.precision(8);
+   mesh->Print(mesh_ofs);
 
-   mesh.NURBSext->ConvertToPatches(*mesh.GetNodes());
-   mesh.NURBSext->GetPatches(patches);
+   if (visualization)
+   {
+      char vishost[] = "localhost";
+      constexpr int visport = 19916;
+      socketstream sol_sock(vishost, visport);
+      sol_sock.precision(8);
+      sol_sock << "mesh\n" << *mesh << flush;
+   }
 
-   ofstream mesh_ofss("spaced.mesh");
-   mesh_ofss.precision(8);
-   mesh.Print(mesh_ofss);
+   delete mesh;
 
    return 0;
 }
