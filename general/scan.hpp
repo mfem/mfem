fa006da71e5fdf406b68558aa828d196d6c3452f
Veselin Dobrev <dobrev@llnl.gov>
2026-07-14 04:38:48 -0700

    Modifications allowing the use of 'mfem.hpp' in pure c++ source files when the library is built with CUDA or HIP support.

--- general/scan.hpp
+++ general/scan.hpp
@@ -15,6 +15,12 @@
 #include "backends.hpp"
 #include "forall.hpp"
 
+#if defined(MFEM_USE_CUDA_OR_HIP) && !defined(MFEM_USE_CUDA_OR_HIP_LANG)
+#error "This header requires compilation with CUDA/HIP language!"
+#endif
+
+#ifdef MFEM_USE_CUDA_OR_HIP_LANG
+
 #ifdef MFEM_USE_CUDA
 #include <cub/device/device_scan.cuh>
 #include <cub/device/device_select.cuh>
@@ -405,5 +411,7 @@ void CopyUnique(bool use_dev, InputIt d_in, OutputIt d_out,
 } // namespace mfem
 
 #undef MFEM_CUB_NAMESPACE
+
+#endif // #ifdef MFEM_USE_CUDA_OR_HIP_LANG
 
 #endif
