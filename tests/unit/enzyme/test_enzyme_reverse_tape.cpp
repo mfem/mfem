8262a9837a9e0c31086585cd8dbd145a3a48103c
molinari2 <molinari2@llnl.gov>
2026-07-15 10:17:37 -0700

    Added version with qvector-like wrapper.

--- tests/unit/enzyme/test_enzyme_reverse_tape.cpp
+++ tests/unit/enzyme/test_enzyme_reverse_tape.cpp
@@ -11,6 +11,7 @@
 
 #include "mfem.hpp"
 #include "unit_tests.hpp"
+#include "../../../linalg/tensor_arrays.hpp"
 
 #ifdef MFEM_USE_ENZYME
 
@@ -18,6 +19,7 @@ namespace enzyme_test_reversetape
 {
 
 using namespace mfem;
+using namespace mfem::future;
 
 void myQF(real_t x[2])
 {
@@ -26,9 +28,31 @@ void myQF(real_t x[2])
    x[0] = sin(u) * cos(v) * (u + v);
 }
 
-void * __enzyme_augmentfwd(void *, real_t *, real_t *);
-void __enzyme_reverse(void *, real_t *, real_t *, void *);
-void __enzyme_autodiff(void *, real_t *, real_t *);
+struct WrappedQF
+{
+   void operator()(tensor_array<real_t> &x) const
+   {
+      const real_t u = x(0);
+      const real_t v = x(1);
+      x(0) = sin(u) * cos(v) * (u + v);
+   }
+};
+
+template <int N>
+void qfunction_wrapper(real_t *x)
+{
+   auto x_t = make_tensor_array(x, N);
+
+   WrappedQF qf;
+   qf(x_t);
+}
+
+
+template <typename return_type, typename... Args>
+return_type  __enzyme_augmentfwd(Args...);
+
+template <typename return_type, typename... Args>
+return_type  __enzyme_reverse(Args...);
 
 } // namespace enzyme_test_reversetape
 
@@ -42,10 +66,41 @@ TEST_CASE("Enzyme split reverse mode point qfunction VJP",
    real_t y[2] = {x[0], x[1]};
    real_t y_bar[2] = {x_bar[0], x_bar[1]};
 
-   void *tape = __enzyme_augmentfwd((void *)myQF, x, x_bar);
-   __enzyme_reverse((void *)myQF, x, x_bar, tape);
+   void *tape = __enzyme_augmentfwd<void *>((void *)myQF, x, x_bar);
+   __enzyme_reverse<void>((void *)myQF, x, x_bar, tape);
 
-   __enzyme_autodiff((void *)myQF, y, y_bar);
+   __enzyme_autodiff<void>((void *)myQF, y, y_bar);
+
+   const real_t u = 0.7;
+   const real_t v = 0.2;
+   const real_t residual = sin(u) * cos(v) * (u + v);
+   REQUIRE(x[0] == MFEM_Approx(residual));
+   REQUIRE(y[0] == MFEM_Approx(residual));
+   REQUIRE(x[1] == MFEM_Approx(v));
+   REQUIRE(y[1] == MFEM_Approx(v));
+
+   const real_t exact_u_bar = 1.5 * (cos(u) * cos(v) * (u + v) + sin(u) * cos(v));
+   const real_t exact_v_bar = 1.5 * (-sin(u) * sin(v) * (u + v) + sin(u) * cos(v));
+   REQUIRE(x_bar[0] == MFEM_Approx(exact_u_bar));
+   REQUIRE(x_bar[1] == MFEM_Approx(exact_v_bar));
+   REQUIRE(y_bar[0] == MFEM_Approx(x_bar[0]));
+   REQUIRE(y_bar[1] == MFEM_Approx(x_bar[1]));
+}
+
+TEST_CASE("Enzyme split reverse mode wrapped point qfunction VJP",
+          "[Enzyme][SplitReverse][QFunctionWrapper]")
+{
+   using namespace enzyme_test_reversetape;
+
+   real_t x[2] = {0.7, 0.2};
+   real_t x_bar[2] = {1.5, 0.0};
+   real_t y[2] = {x[0], x[1]};
+   real_t y_bar[2] = {x_bar[0], x_bar[1]};
+
+   void *tape = __enzyme_augmentfwd<void *>((void *)qfunction_wrapper<2>, x, x_bar);
+   __enzyme_reverse<void>((void *)qfunction_wrapper<2>, x, x_bar, tape);
+
+   __enzyme_autodiff<void>((void *)qfunction_wrapper<2>, y, y_bar);
 
    const real_t u = 0.7;
    const real_t v = 0.2;
