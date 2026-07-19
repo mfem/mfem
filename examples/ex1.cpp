698cfcd877742c8a44a59761f8d3e7491dcf6905
camierjs <camierjs@gmail.com>
2026-07-18 20:26:38 -0700

    Simplices MMA meld back ex1/ex1p to master

--- examples/ex1.cpp
+++ examples/ex1.cpp
@@ -53,9 +53,6 @@
 // Device simplices sample runs:
 //               ex1 -pa -d gpu -m ../data/inline-tet.mesh
 //               ex1 -pa -d gpu -m ../data/inline-tri.mesh
-//               ex1 -pa -d gpu -m ../data/inline-tet.mesh -b P
-//               ex1 -pa -d cuda -m ../data/inline-tet.mesh -b G
-//               ex1 -pa -d cuda -m ../data/inline-tri.mesh -b G
 //
 // Description:  This example code demonstrates the use of MFEM to define a
 //               simple finite element discretization of the Poisson problem
@@ -88,7 +85,6 @@ int main(int argc, char *argv[])
    bool pa = false;
    bool fa = false;
    const char *device_config = "cpu";
-   const char *basis_type = "auto";
    bool visualization = true;
    bool algebraic_ceed = false;
 
@@ -106,9 +102,6 @@ int main(int argc, char *argv[])
                   "--no-full-assembly", "Enable Full Assembly.");
    args.AddOption(&device_config, "-d", "--device",
                   "Device configuration string, see Device::Configure().");
-   args.AddOption(&basis_type, "-b", "--basis-type",
-                  "Basis: auto | G (Gauss-Lobatto) | P (Positive). "
-                  "auto: Positive if -pa and simplex mesh, else Gauss-Lobatto.");
 #ifdef MFEM_USE_CEED
    args.AddOption(&algebraic_ceed, "-a", "--algebraic", "-no-a", "--no-algebraic",
                   "Use algebraic Ceed solver");
@@ -151,23 +144,14 @@ int main(int argc, char *argv[])
    // 5. Define a finite element space on the mesh. Here we use continuous
    //    Lagrange finite elements of the specified order.
    //    - If order < 1, we instead use an isoparametric/isogeometric space.
-   //    - With -b auto (default): Positive basis if -pa and simplex mesh,
-   //      else Gauss-Lobatto. Use -b G / -b P to force a basis.
+   //    - If the mesh is simplicial and partial assembly is requested,
+   //      we use the positive basis, which supports device execution.
    FiniteElementCollection *fec;
-   int basis;
-   if (!strcmp(basis_type, "auto"))
-   {
-      basis = (pa && mesh.IsSimplexMesh()) ?
-              BasisType::Positive : BasisType::GaussLobatto;
-   }
-   else
-   {
-      basis = BasisType::GetType(basis_type[0]);
-   }
-   cout << "Using " << BasisType::Name(basis) << " basis ..." << endl;
+   auto basis_type = (pa && mesh.IsSimplexMesh()) ?
+                     BasisType::Positive : BasisType::GaussLobatto;
    if (order > 0)
    {
-      fec = new H1_FECollection(order, dim, basis);
+      fec = new H1_FECollection(order, dim, basis_type);
    }
    else if (mesh.GetNodes())
    {
@@ -176,7 +160,7 @@ int main(int argc, char *argv[])
    }
    else
    {
-      fec = new H1_FECollection(order = 1, dim, basis);
+      fec = new H1_FECollection(order = 1, dim, basis_type);
    }
    FiniteElementSpace fespace(&mesh, fec);
    cout << "Number of finite element unknowns: "
@@ -226,7 +210,6 @@ int main(int argc, char *argv[])
       // bit-for-bit deterministic at the cost of somewhat longer run time.
       a.EnableSparseMatrixSorting(Device::IsEnabled());
    }
-   // a.AddDomainIntegrator(new MassIntegrator(one));
    a.AddDomainIntegrator(new DiffusionIntegrator(one));
 
    // 10. Assemble the bilinear form and the corresponding linear system,
