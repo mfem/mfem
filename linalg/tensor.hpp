f9ff7a24414ed8e34539c3cd34f1cd14375ace3b
Veselin Dobrev <dobrev@llnl.gov>
2026-08-19 12:04:58 -0700

    In class tensor: * make the data arrays 'value'/'values' private * add some MFEM_HOST_DEVICE annotations * with MSVC, don't use std::array since in MSVC std::array<T,0> is not trivial   even when T is trivial * for a bit more consistency rename 'c_subarray_type' -> 'c_sub_array_type'

--- linalg/tensor.hpp
+++ linalg/tensor.hpp
@@ -40,8 +40,8 @@ struct tensor<T>
    static constexpr auto sizes_array() { return std::array<int,0> {}; }
    static constexpr int size(int) { return 1; }
 
-   constexpr tensor() = default;
-   constexpr tensor(const T &val) : value(val) { }
+   MFEM_HOST_DEVICE constexpr tensor() = default;
+   MFEM_HOST_DEVICE constexpr tensor(const T &val) : value(val) { }
 
    MFEM_HOST_DEVICE constexpr T& operator[](int) { return value; }
    MFEM_HOST_DEVICE constexpr const T& operator[](int) const { return value; }
@@ -53,6 +53,7 @@ struct tensor<T>
 
    MFEM_HOST_DEVICE constexpr operator T() const { return value; }
 
+private:
    T value;
 };
 
@@ -64,11 +65,11 @@ struct tensor<T, N0, Ns...>
    using type = T;
    using sub_tensor_type = std::conditional_t<
                            (sizeof...(Ns) > 0), tensor<T, Ns...>, T>;
-   using c_subarray_type = std::conditional_t<
-                           (sizeof...(Ns) > 0),
-                           typename tensor<T, Ns...>::c_array_type,
-                           T>;
-   using c_array_type = c_subarray_type[N0 ? N0 : 1];
+   using c_sub_array_type = std::conditional_t<
+                            (sizeof...(Ns) > 0),
+                            typename tensor<T, Ns...>::c_array_type,
+                            T>;
+   using c_array_type = c_sub_array_type[N0 ? N0 : 1];
 
    static constexpr int rank() { return 1 + sizeof...(Ns); }
 
@@ -79,12 +80,13 @@ struct tensor<T, N0, Ns...>
 
    static constexpr int size(int k) { return sizes_array()[k]; }
 
-   constexpr tensor() = default;
+   MFEM_HOST_DEVICE constexpr tensor() = default;
 
-   constexpr tensor(const c_array_type &a)
+   MFEM_HOST_DEVICE constexpr tensor(const c_array_type &a)
       : tensor(a, std::make_index_sequence<N0> {}) { }
 
-   constexpr tensor(std::initializer_list<c_subarray_type> ilist)
+   MFEM_HOST_DEVICE constexpr
+   tensor(std::initializer_list<c_sub_array_type> ilist)
       : tensor(ilist, std::make_index_sequence<N0> {}) { }
 
    MFEM_HOST_DEVICE constexpr auto& operator[](int i) { return values[i]; }
@@ -112,18 +114,24 @@ struct tensor<T, N0, Ns...>
       else { return values[i0](is...); }
    }
 
+private:
+#ifndef _MSC_VER
    // Use std::array to support N0 = 0.
    std::array<sub_tensor_type, N0> values;
+#else
+   // In MSVC, std::array<T,0> is not trivial even if T is trivial, so we don't
+   // use std::array.
+   sub_tensor_type values[N0 ? N0 : 1];
+#endif
 
-private:
    // Helper constructor from c_array_type
-   template <std::size_t... Is>
+   template <std::size_t... Is> MFEM_HOST_DEVICE
    constexpr tensor(const c_array_type &a, std::index_sequence<Is...>)
       : values{a[Is]...} { }
 
-   // Helper constructor from std::initializer_list<c_subarray_type>
-   template <std::size_t... Is>
-   constexpr tensor(std::initializer_list<c_subarray_type> a,
+   // Helper constructor from std::initializer_list<c_sub_array_type>
+   template <std::size_t... Is> MFEM_HOST_DEVICE
+   constexpr tensor(std::initializer_list<c_sub_array_type> a,
                     std::index_sequence<Is...>)
       : values{a.begin()[Is]...} { }
 };
