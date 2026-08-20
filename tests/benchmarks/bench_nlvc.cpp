d4a534e954ab7c4472d6991eb10aab6e27f689d6
Tara Drwenski <drwenski1@llnl.gov>
2026-08-19 20:25:36 -0700

    Use bmi::Benchmark in bench_nlvc for CodSpeed compatibility

--- tests/benchmarks/bench_nlvc.cpp
+++ tests/benchmarks/bench_nlvc.cpp
@@ -22,7 +22,7 @@
 using namespace mfem;
 
 // Custom benchmark arguments generator ///////////////////////////////////////
-static void CustomArguments(bm::Benchmark *b) noexcept
+static void CustomArguments(bmi::Benchmark *b) noexcept
 {
    constexpr int MAX_NDOFS = 8 * 1024 * (mfem_use_gpu ? 1024 : 8);
 
