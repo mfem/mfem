64a866cb4c39ae48288a131d648c17680541cc3f
Ido Akkerman <I.Akkerman@tudelft.nl>
2026-09-03 14:51:10 +0200

    Make style

--- tests/unit/fem/test_transfer.cpp
+++ tests/unit/fem/test_transfer.cpp
@@ -735,7 +735,7 @@ TEST_CASE("Generic Transfer Operator -- Vector", "[Dimension][Order][LOR]")
    int order = GENERATE(2, 3, 4);
    int lor = GENERATE(2, 3, 4, 5);
    auto vectorspace = GENERATE(VecSpace::VectorH1vdim, VecSpace::VectorH1nodes);
-                              // tbd --> VecSpace::ND,VecSpace::RT);
+   // tbd --> VecSpace::ND,VecSpace::RT);
 
    INFO("dimension = " << dimension);
    INFO("order = " << order);
